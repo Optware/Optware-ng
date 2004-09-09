2115d5e6c26802e0919a6509e93f4e56bcfecaa8
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2004-09-09 12:21:10 +0000

    Bumped zlib to 1.2.1

--- make/zlib.mk
+++ make/zlib.mk
@@ -3,74 +3,78 @@
 # zlib
 #
 #############################################################
-ZLIB_SOURCE=zlib-1.1.4.tar.bz2
+
+ZLIB_DIR=$(BUILD_DIR)/zlib
+
+ZLIB_VERSION:=1.2.1
 ZLIB_SITE=http://aleron.dl.sourceforge.net/sourceforge/libpng
-ZLIB_DIR=$(BUILD_DIR)/zlib-1.1.4
+ZLIB_SOURCE=zlib-$(ZLIB_VERSION).tar.bz2
 ZLIB_CFLAGS= $(TARGET_CFLAGS) -fPIC
 ifeq ($(strip $(BUILD_WITH_LARGEFILE)),true)
 ZLIB_CFLAGS+= -D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64
 endif
 
-ZLIB_IPK_DIR=$(BUILD_DIR)/zlib-1.1.4-ipk
-ZLIB_IPK=$(BUILD_DIR)/zlib_1.1.4-1_armeb.ipk
+ZLIB_IPK_DIR=$(BUILD_DIR)/zlib-$(ZLIB_VERSION)-ipk
+ZLIB_IPK=$(BUILD_DIR)/zlib_$(ZLIB_VERSION)-1_armeb.ipk
 
 $(DL_DIR)/$(ZLIB_SOURCE):
 	$(WGET) -P $(DL_DIR) $(ZLIB_SITE)/$(ZLIB_SOURCE)
 
 $(ZLIB_DIR)/.source: $(DL_DIR)/$(ZLIB_SOURCE)
 	bzcat $(DL_DIR)/$(ZLIB_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+	mv $(BUILD_DIR)/zlib-$(ZLIB_VERSION) $(ZLIB_DIR)
 	touch $(ZLIB_DIR)/.source
 
 $(ZLIB_DIR)/.configured: $(ZLIB_DIR)/.source
 	(cd $(ZLIB_DIR); \
 		./configure \
 		--shared \
-		--prefix=/usr \
+		--prefix=/opt \
 		--exec-prefix=$(STAGING_DIR)/usr/bin \
 		--libdir=$(STAGING_DIR)/lib \
 		--includedir=$(STAGING_DIR)/include \
 	);
 	touch $(ZLIB_DIR)/.configured;
 
-$(ZLIB_DIR)/libz.so.1.1.4: $(ZLIB_DIR)/.configured
+$(ZLIB_DIR)/libz.so.$(ZLIB_VERSION): $(ZLIB_DIR)/.configured
 	$(MAKE) LDSHARED="$(TARGET_CROSS)ld -shared -soname,libz.so.1" \
 		CFLAGS="$(ZLIB_CFLAGS)" CC=$(TARGET_CC) -C $(ZLIB_DIR) all libz.a;
-	touch -c $(ZLIB_DIR)/libz.so.1.1.4
+	touch -c $(ZLIB_DIR)/libz.so.$(ZLIB_VERSION)
 
-$(STAGING_DIR)/lib/libz.so.1.1.4: $(ZLIB_DIR)/libz.so.1.1.4
+$(STAGING_DIR)/lib/libz.so.$(ZLIB_VERSION): $(ZLIB_DIR)/libz.so.$(ZLIB_VERSION)
 	cp -dpf $(ZLIB_DIR)/libz.a $(STAGING_DIR)/lib;
 	cp -dpf $(ZLIB_DIR)/zlib.h $(STAGING_DIR)/include;
 	cp -dpf $(ZLIB_DIR)/zconf.h $(STAGING_DIR)/include;
 	cp -dpf $(ZLIB_DIR)/libz.so* $(STAGING_DIR)/lib;
-	(cd $(STAGING_DIR)/lib; ln -fs libz.so.1.1.4 libz.so.1);
-	chmod a-x $(STAGING_DIR)/lib/libz.so.1.1.4
-	touch -c $(STAGING_DIR)/lib/libz.so.1.1.4
+	(cd $(STAGING_DIR)/lib; ln -fs libz.so.$(ZLIB_VERSION) libz.so.1);
+	chmod a-x $(STAGING_DIR)/lib/libz.so.$(ZLIB_VERSION)
+	touch -c $(STAGING_DIR)/lib/libz.so.$(ZLIB_VERSION)
 
-$(TARGET_DIR)/lib/libz.so.1.1.4: $(STAGING_DIR)/lib/libz.so.1.1.4
+$(TARGET_DIR)/lib/libz.so.$(ZLIB_VERSION): $(STAGING_DIR)/lib/libz.so.$(ZLIB_VERSION)
 	cp -dpf $(STAGING_DIR)/lib/libz.so* $(TARGET_DIR)/lib;
 	-$(STRIP) --strip-unneeded $(TARGET_DIR)/lib/libz.so*
-	touch -c $(TARGET_DIR)/lib/libz.so.1.1.4
+	touch -c $(TARGET_DIR)/lib/libz.so.$(ZLIB_VERSION)
 
-$(TARGET_DIR)/usr/lib/libz.a: $(STAGING_DIR)/lib/libz.so.1.1.4
+$(TARGET_DIR)/usr/lib/libz.a: $(STAGING_DIR)/lib/libz.so.$(ZLIB_VERSION)
 	mkdir -p $(TARGET_DIR)/usr/include
 	cp -dpf $(STAGING_DIR)/include/zlib.h $(TARGET_DIR)/usr/include/
 	cp -dpf $(STAGING_DIR)/include/zconf.h $(TARGET_DIR)/usr/include/
 	cp -dpf $(STAGING_DIR)/lib/libz.a $(TARGET_DIR)/usr/lib/
 	rm -f $(TARGET_DIR)/lib/libz.so
-	(cd $(TARGET_DIR)/usr/lib; ln -fs /lib/libz.so.1.1.4 libz.so)
+	(cd $(TARGET_DIR)/usr/lib; ln -fs /lib/libz.so.$(ZLIB_VERSION) libz.so)
 	touch -c $(TARGET_DIR)/usr/lib/libz.a
 
 zlib-headers: $(TARGET_DIR)/usr/lib/libz.a
 
-zlib: $(TARGET_DIR)/lib/libz.so.1.1.4
+zlib: $(TARGET_DIR)/lib/libz.so.$(ZLIB_VERSION)
 
-$(ZLIB_IPK): $(STAGING_DIR)/lib/libz.so.1.1.4
+$(ZLIB_IPK): $(STAGING_DIR)/lib/libz.so.$(ZLIB_VERSION)
 	mkdir -p $(ZLIB_IPK_DIR)/CONTROL
 	cp $(SOURCE_DIR)/zlib.control $(ZLIB_IPK_DIR)/CONTROL/control
 	mkdir -p $(ZLIB_IPK_DIR)/opt/lib
 	cp -dpf $(STAGING_DIR)/lib/libz.so* $(ZLIB_IPK_DIR)/opt/lib;
 	-$(STRIP) --strip-unneeded $(ZLIB_IPK_DIR)/opt/lib/libz.so*
-	touch -c $(ZLIB_IPK_DIR)/opt/lib/libz.so.1.1.4
+	touch -c $(ZLIB_IPK_DIR)/opt/lib/libz.so.$(ZLIB_VERSION)
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(ZLIB_IPK_DIR)
 
 zlib-ipk: $(ZLIB_IPK)
