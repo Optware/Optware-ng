5224c61b946c39fbdae109654e4424fe055995df
codewhacker <codewhacker@1f1dca69-5516-0410-b834-e485effd04c3>
2004-09-10 08:31:23 +0000

    portmap daemon and nfs server daemons, adapted from Jim's article on tomsnetworking.com

---
+++ make/nfs-server.mk
@@ -0,0 +1,67 @@
+#############################################################
+#
+# nfs-server
+#
+#############################################################
+
+NFS-SERVER_DIR:=$(BUILD_DIR)/nfs-server
+
+NFS-SERVER_VERSION=2.2beta47
+NFS-SERVER=nfs-server-$(NFS-SERVER_VERSION)
+NFS-SERVER_SITE=http://linux.mathematik.tu-darmstadt.de/linux/people/okir
+NFS-SERVER_SOURCE:=$(NFS-SERVER).tar.gz
+NFS-SERVER_UNZIP=zcat
+NFS-SERVER_PATCH:=$(SOURCE_DIR)/nfs-server.patch
+NFS-SERVER_IPK=$(BUILD_DIR)/nfs-server_$(NFS-SERVER_VERSION)-1_armeb.ipk
+NFS-SERVER_IPK_DIR:=$(BUILD_DIR)/nfs-server-$(NFS-SERVER_VERSION)-ipk
+
+$(DL_DIR)/$(NFS-SERVER_SOURCE):
+	$(WGET) -P $(DL_DIR) $(NFS-SERVER_SITE)/$(NFS-SERVER_SOURCE)
+
+nfs-server-source: $(DL_DIR)/$(NFS-SERVER_SOURCE) $(NFS-SERVER_PATCH)
+
+
+# make changes to the BUILD options below.  If you are using TCP Wrappers, 
+# set --libwrap-directory=pathname 
+
+$(NFS-SERVER_DIR): $(DL_DIR)/$(NFS-SERVER_SOURCE) $(NFS-SERVER_PATCH)
+	@rm -rf $(BUILD_DIR)/$(NFS-SERVER) $(NFS-SERVER_DIR)
+	$(NFS-SERVER_UNZIP) $(DL_DIR)/$(NFS-SERVER_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+	(cd $(BUILD_DIR)/$(NFS-SERVER) && \
+	./BUILD --batch \
+		--multi \
+		--devtab=no \
+		--ugidd=no \
+		--nis=no \
+		--hosts-access=no \
+		--exports-uid=0 \
+		--exports-gid=0 \
+		--log-mounts=yes)
+	patch -d $(BUILD_DIR)/$(NFS-SERVER) -p1 < $(NFS-SERVER_PATCH)
+	mv $(BUILD_DIR)/$(NFS-SERVER) $(NFS-SERVER_DIR)
+	make -C $(NFS-SERVER_DIR) 
+
+nfs-server: $(NFS-SERVER_DIR)
+
+nfs-server-diff: #$(NFS-SERVER_DIR)/config.h
+	@rm -rf $(BUILD_DIR)/$(NFS-SERVER)
+	$(NFS-SERVER_UNZIP) $(DL_DIR)/$(NFS-SERVER_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+	-make -C $(NFS-SERVER_DIR) distclean
+	-cd $(BUILD_DIR) && diff -BurN $(NFS-SERVER) nfs-server | grep -v ^Only > $(NFS-SERVER_PATCH)
+
+$(NFS-SERVER_IPK): $(NFS-SERVER_DIR)
+	install -d $(NFS-SERVER_IPK_DIR)/CONTROL
+	install -d $(NFS-SERVER_IPK_DIR)/opt/sbin $(NFS-SERVER_IPK_DIR)/opt/etc/init.d
+	$(STRIP) $(NFS-SERVER_DIR)/rpc.nfsd -o $(NFS-SERVER_IPK_DIR)/opt/sbin/rpc.nfsd
+	$(STRIP) $(NFS-SERVER_DIR)/rpc.mountd -o $(NFS-SERVER_IPK_DIR)/opt/sbin/rpc.mountd
+	install -m 755 $(SOURCE_DIR)/nfs-server.rc $(NFS-SERVER_IPK_DIR)/opt/etc/init.d/S5nfsd
+	install -m 644 $(SOURCE_DIR)/nfs-server.control  $(NFS-SERVER_IPK_DIR)/CONTROL/control
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(NFS-SERVER_IPK_DIR)
+
+nfs-server-ipk: $(NFS-SERVER_IPK)
+
+nfs-server-clean:
+	-make -C $(NFS-SERVER_DIR) clean
+
+nfs-server-dirclean:
+	rm -rf $(NFS-SERVER_DIR) $(NFS-SERVER_IPK_DIR)
---
+++ make/portmap.mk
@@ -0,0 +1,52 @@
+#############################################################
+#
+# portmap
+#
+#############################################################
+
+PORTMAP_DIR:=$(BUILD_DIR)/portmap
+
+PORTMAP_VERSION=4
+PORTMAP=portmap_$(PORTMAP_VERSION)
+PORTMAP_SITE=http://ftp.surfnet.nl/security/tcpwrappers
+PORTMAP_SOURCE:=$(PORTMAP).tar.gz
+PORTMAP_UNZIP=zcat
+PORTMAP_PATCH:=$(SOURCE_DIR)/portmap.patch
+PORTMAP_IPK=$(BUILD_DIR)/portmap_$(PORTMAP_VERSION)-1_armeb.ipk
+PORTMAP_IPK_DIR:=$(BUILD_DIR)/portmap-$(PORTMAP_VERSION)-ipk
+
+$(DL_DIR)/$(PORTMAP_SOURCE):
+	$(WGET) -P $(DL_DIR) $(PORTMAP_SITE)/$(PORTMAP_SOURCE)
+
+portmap-source: $(DL_DIR)/$(PORTMAP_SOURCE) $(PORTMAP_PATCH)
+
+$(PORTMAP_DIR): $(DL_DIR)/$(PORTMAP_SOURCE) $(PORTMAP_PATCH)
+	@rm -rf $(BUILD_DIR)/$(PORTMAP) $(PORTMAP_DIR)
+	$(PORTMAP_UNZIP) $(DL_DIR)/$(PORTMAP_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+	patch -d $(BUILD_DIR)/$(PORTMAP) -p1 < $(PORTMAP_PATCH)
+	mv $(BUILD_DIR)/$(PORTMAP) $(PORTMAP_DIR)
+	make -C $(PORTMAP_DIR) 
+
+portmap: $(PORTMAP_DIR)
+
+portmap-diff: #$(PORTMAP_DIR)/config.h
+	@rm -rf $(BUILD_DIR)/$(PORTMAP)
+	$(PORTMAP_UNZIP) $(DL_DIR)/$(PORTMAP_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+	-make -C $(PORTMAP_DIR) distclean
+	-cd $(BUILD_DIR) && diff -BurN $(PORTMAP) portmap | grep -v ^Only > $(PORTMAP_PATCH)
+
+$(PORTMAP_IPK): $(PORTMAP_DIR)
+	install -d $(PORTMAP_IPK_DIR)/CONTROL
+	install -d $(PORTMAP_IPK_DIR)/opt/sbin $(PORTMAP_IPK_DIR)/opt/etc/init.d
+	$(STRIP) $(PORTMAP_DIR)/portmap -o $(PORTMAP_IPK_DIR)/opt/sbin/portmap
+	install -m 755 $(SOURCE_DIR)/portmap.rc $(PORTMAP_IPK_DIR)/opt/etc/init.d/S50portmap
+	install -m 644 $(SOURCE_DIR)/portmap.control  $(PORTMAP_IPK_DIR)/CONTROL/control
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(PORTMAP_IPK_DIR)
+
+portmap-ipk: $(PORTMAP_IPK)
+
+portmap-clean:
+	-make -C $(PORTMAP_DIR) clean
+
+portmap-dirclean:
+	rm -rf $(PORTMAP_DIR) $(PORTMAP_IPK_DIR)
