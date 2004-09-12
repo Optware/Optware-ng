ce72879c05c3f0db618c8e1fc238d13633c9c2bb
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2004-09-12 00:15:55 +0000

    Changed variables and targets from mtdaapd to mt-daapd to follow conventions

--- trunk/make/mt-daapd.mk
+++ trunk/make/mt-daapd.mk
@@ -1,40 +1,40 @@
 ###########################################################
 #
-# mtdaapd
+# mt-daapd
 #
 ###########################################################
 
-MTDAAPD_DIR=$(BUILD_DIR)/mtdaapd
+MT_DAAPD_DIR=$(BUILD_DIR)/mt-daapd
 
-MTDAAPD_VERSION=0.2.0
-MTDAAPD=mt-daapd-$(MTDAAPD_VERSION)
-MTDAAPD_SITE=http://belnet.dl.sourceforge.net/sourceforge/mt-daapd
-MTDAAPD_SOURCE=$(MTDAAPD).tar.gz
-MTDAAPD_UNZIP=zcat
-MTDAAPD_CFLAGS=$(TARGET_CFLAGS) -I$(STAGING_DIR)/include -fPIC
-MTDAAPD_LDFLAGS=$(TARGET_LDFLAGS) -L$(STAGING_DIR)/lib
+MT_DAAPD_VERSION=0.2.0
+MT_DAAPD=mt-daapd-$(MT_DAAPD_VERSION)
+MT_DAAPD_SITE=http://belnet.dl.sourceforge.net/sourceforge/mt-daapd
+MT_DAAPD_SOURCE=$(MT_DAAPD).tar.gz
+MT_DAAPD_UNZIP=zcat
+MT_DAAPD_CFLAGS=$(TARGET_CFLAGS) -I$(STAGING_DIR)/include -fPIC
+MT_DAAPD_LDFLAGS=$(TARGET_LDFLAGS) -L$(STAGING_DIR)/lib
 
-MTDAAPD_PATCH:=$(SOURCE_DIR)/mt-daapd.patch
+MT_DAAPD_PATCH:=$(SOURCE_DIR)/mt-daapd.patch
 
-MTDAAPD_IPK=$(BUILD_DIR)/mt-daapd_$(MTDAAPD_VERSION)-1_armeb.ipk
-MTDAAPD_IPK_DIR=$(BUILD_DIR)/mt-daapd-$(MTDAAPD_VERSION)-ipk
+MT_DAAPD_IPK=$(BUILD_DIR)/mt-daapd_$(MT_DAAPD_VERSION)-1_armeb.ipk
+MT_DAAPD_IPK_DIR=$(BUILD_DIR)/mt-daapd-$(MT_DAAPD_VERSION)-ipk
 
-$(DL_DIR)/$(MTDAAPD_SOURCE):
-	$(WGET) -P $(DL_DIR) $(MTDAAPD_SITE)/$(MTDAAPD_SOURCE)
+$(DL_DIR)/$(MT_DAAPD_SOURCE):
+	$(WGET) -P $(DL_DIR) $(MT_DAAPD_SITE)/$(MT_DAAPD_SOURCE)
 
-mtdaapd-source: $(DL_DIR)/$(MTDAAPD_SOURCE) $(MTDAAPD_PATCH)
+mt-daapd-source: $(DL_DIR)/$(MT_DAAPD_SOURCE) $(MT_DAAPD_PATCH)
 
-$(MTDAAPD_DIR)/.source: $(DL_DIR)/$(MTDAAPD_SOURCE)
-	$(MTDAAPD_UNZIP) $(DL_DIR)/$(MTDAAPD_SOURCE) | tar -C $(BUILD_DIR) -xvf -
-	cat $(MTDAAPD_PATCH) | patch -d $(BUILD_DIR)/$(MTDAAPD) -p1
-	mv $(BUILD_DIR)/$(MTDAAPD) $(MTDAAPD_DIR)
-	touch $(MTDAAPD_DIR)/.source
+$(MT_DAAPD_DIR)/.source: $(DL_DIR)/$(MT_DAAPD_SOURCE)
+	$(MT_DAAPD_UNZIP) $(DL_DIR)/$(MT_DAAPD_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+	cat $(MT_DAAPD_PATCH) | patch -d $(BUILD_DIR)/$(MT_DAAPD) -p1
+	mv $(BUILD_DIR)/$(MT_DAAPD) $(MT_DAAPD_DIR)
+	touch $(MT_DAAPD_DIR)/.source
 
-$(MTDAAPD_DIR)/.configured: $(MTDAAPD_DIR)/.source
-	(cd $(MTDAAPD_DIR); \
+$(MT_DAAPD_DIR)/.configured: $(MT_DAAPD_DIR)/.source
+	(cd $(MT_DAAPD_DIR); \
         export CC=$(TARGET_CC) ;\
-        export CFLAGS="$(MTDAAPD_CFLAGS)" ;\
-        export LDFLAGS="$(MTDAAPD_LDFLAGS)" ;\
+        export CFLAGS="$(MT_DAAPD_CFLAGS)" ;\
+        export LDFLAGS="$(MT_DAAPD_LDFLAGS)" ;\
 		./configure \
         --host=arm-linux \
 		--target=$(GNU_TARGET_NAME) \
@@ -44,42 +44,42 @@ $(MTDAAPD_DIR)/.configured: $(MTDAAPD_DIR)/.source
         LIBS="-lgdbm -lid3tag -lz -lpthread" \
         ac_cv_func_setpgrp_void=yes \
 	);
-	touch $(MTDAAPD_DIR)/.configured
+	touch $(MT_DAAPD_DIR)/.configured
 
-$(STAGING_DIR)/sbin/mt-daapd: $(MTDAAPD_DIR)/.configured
-	$(MAKE) LDFLAGS="$(MTDAAPD_LDFLAGS)" CC="$(TARGET_CC)" -C $(MTDAAPD_DIR) install
+$(STAGING_DIR)/sbin/mt-daapd: $(MT_DAAPD_DIR)/.configured
+	$(MAKE) LDFLAGS="$(MT_DAAPD_LDFLAGS)" CC="$(TARGET_CC)" -C $(MT_DAAPD_DIR) install
 
-#mtdaapd-headers: $(STAGING_DIR)/sbin/mt-daapd
+#mt-daapd-headers: $(STAGING_DIR)/sbin/mt-daapd
 
-mtdaapd: zlib gdbm libid3tag $(STAGING_DIR)/sbin/mt-daapd
+mt-daapd: zlib gdbm libid3tag $(STAGING_DIR)/sbin/mt-daapd
 
-mtdaapd-diff: #$(MTDAAPD_DIR)/.configured
-	@rm -rf $(BUILD_DIR)/$(MTDAAPD)
-	$(MTDAAPD_UNZIP) $(DL_DIR)/$(MTDAAPD_SOURCE) | tar -C $(BUILD_DIR) -xvf -
-	-make -C $(MTDAAPD_DIR) distclean
-	-cd $(BUILD_DIR) && diff -BurN $(MTDAAPD) mtdaapd | grep -v ^Only > $(MTDAAPD_PATCH)
+mt-daapd-diff: #$(MT_DAAPD_DIR)/.configured
+	@rm -rf $(BUILD_DIR)/$(MT_DAAPD)
+	$(MT_DAAPD_UNZIP) $(DL_DIR)/$(MT_DAAPD_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+	-make -C $(MT_DAAPD_DIR) distclean
+	-cd $(BUILD_DIR) && diff -BurN $(MT_DAAPD) mt-daapd | grep -v ^Only > $(MT_DAAPD_PATCH)
 
-$(MTDAAPD_IPK): $(STAGING_DIR)/sbin/mt-daapd
-	install -d $(MTDAAPD_IPK_DIR)/CONTROL
-	install -d $(MTDAAPD_IPK_DIR)/opt/sbin $(MTDAAPD_IPK_DIR)/opt/etc/init.d
-	install -d $(MTDAAPD_IPK_DIR)/opt/share/mt-daapd/admin-root
-	install -d $(MTDAAPD_IPK_DIR)/opt/var/mt-daapd
-	$(STRIP) --strip-unneeded $(STAGING_DIR)/sbin/mt-daapd -o $(MTDAAPD_IPK_DIR)/opt/sbin/mt-daapd
-	install -m 644 $(STAGING_DIR)/share/mt-daapd/admin-root/* $(MTDAAPD_IPK_DIR)/opt/share/mt-daapd/admin-root
-	install -m 755 $(SOURCE_DIR)/mt-daapd.rc        $(MTDAAPD_IPK_DIR)/opt/etc/init.d/S60mt-daapd
-	install -m 644 $(SOURCE_DIR)/mt-daapd.conf      $(MTDAAPD_IPK_DIR)/opt/etc
-	install -m 644 $(SOURCE_DIR)/mt-daapd.playlist  $(MTDAAPD_IPK_DIR)/opt/etc
-	install -m 644 $(SOURCE_DIR)/mt-daapd.control    $(MTDAAPD_IPK_DIR)/CONTROL/control
-	cd $(BUILD_DIR); $(IPKG_BUILD) $(MTDAAPD_IPK_DIR)
+$(MT_DAAPD_IPK): $(STAGING_DIR)/sbin/mt-daapd
+	install -d $(MT_DAAPD_IPK_DIR)/CONTROL
+	install -d $(MT_DAAPD_IPK_DIR)/opt/sbin $(MT_DAAPD_IPK_DIR)/opt/etc/init.d
+	install -d $(MT_DAAPD_IPK_DIR)/opt/share/mt-daapd/admin-root
+	install -d $(MT_DAAPD_IPK_DIR)/opt/var/mt-daapd
+	$(STRIP) --strip-unneeded $(STAGING_DIR)/sbin/mt-daapd -o $(MT_DAAPD_IPK_DIR)/opt/sbin/mt-daapd
+	install -m 644 $(STAGING_DIR)/share/mt-daapd/admin-root/* $(MT_DAAPD_IPK_DIR)/opt/share/mt-daapd/admin-root
+	install -m 755 $(SOURCE_DIR)/mt-daapd.rc        $(MT_DAAPD_IPK_DIR)/opt/etc/init.d/S60mt-daapd
+	install -m 644 $(SOURCE_DIR)/mt-daapd.conf      $(MT_DAAPD_IPK_DIR)/opt/etc
+	install -m 644 $(SOURCE_DIR)/mt-daapd.playlist  $(MT_DAAPD_IPK_DIR)/opt/etc
+	install -m 644 $(SOURCE_DIR)/mt-daapd.control    $(MT_DAAPD_IPK_DIR)/CONTROL/control
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(MT_DAAPD_IPK_DIR)
 
-mtdaapd-ipk: $(MTDAAPD_IPK)
+mt-daapd-ipk: $(MT_DAAPD_IPK)
 
-mtdaapd-source: $(DL_DIR)/$(MTDAAPD_SOURCE)
+mt-daapd-source: $(DL_DIR)/$(MT_DAAPD_SOURCE)
 
-mtdaapd-clean:
-	-$(MAKE) -C $(MTDAAPD_DIR) uninstall
-	-$(MAKE) -C $(MTDAAPD_DIR) clean
+mt-daapd-clean:
+	-$(MAKE) -C $(MT_DAAPD_DIR) uninstall
+	-$(MAKE) -C $(MT_DAAPD_DIR) clean
 
-mtdaapd-dirclean: mtdaapd-clean
-	rm -rf $(MTDAAPD_DIR) $(MTDAAPD_IPK_DIR)
+mt-daapd-dirclean: mt-daapd-clean
+	rm -rf $(MT_DAAPD_DIR) $(MT_DAAPD_IPK_DIR) $(MT_DAAPD_IPK)
 
