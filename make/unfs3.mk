e1343ac8e9f4ae3d9d283e90ab5e1e7821db04e8
blunck2 <blunck2@1f1dca69-5516-0410-b834-e485effd04c3>
2004-09-19 23:40:39 +0000

    added.  a Makefile used to download and build the unfs3 server.  it's pretty trivial and fortunately does not require any patches

---
+++ make/unfs3.mk
@@ -0,0 +1,57 @@
+#############################################################
+#
+# unfs3
+#
+#############################################################
+
+UNFS3_DIR:=$(BUILD_DIR)/unfs3
+
+UNFS3_VERSION=0.9.10
+UNFS3=unfs3-$(UNFS3_VERSION)
+UNFS3_SITE=http://aleron.dl.sourceforge.net/sourceforge/unfs3
+UNFS3_SOURCE:=$(UNFS3).tar.gz
+UNFS3_UNZIP=zcat
+UNFS3_PATCH:=$(SOURCE_DIR)/unfs3.patch
+UNFS3_IPK=$(BUILD_DIR)/unfs3_$(UNFS3_VERSION)-1_armeb.ipk
+UNFS3_IPK_DIR:=$(BUILD_DIR)/unfs3-$(UNFS3_VERSION)-ipk
+
+$(DL_DIR)/$(UNFS3_SOURCE):
+	$(WGET) -P $(DL_DIR) $(UNFS3_SITE)/$(UNFS3_SOURCE)
+
+unfs3-source: $(DL_DIR)/$(UNFS3_SOURCE) $(UNFS3_PATCH)
+
+
+# make changes to the BUILD options below.  If you are using TCP Wrappers, 
+# set --libwrap-directory=pathname 
+
+$(UNFS3_DIR)/.configured: $(DL_DIR)/$(UNFS3_SOURCE)
+	@rm -rf $(BUILD_DIR)/$(UNFS3) $(UNFS3_DIR)
+	$(UNFS3_UNZIP) $(DL_DIR)/$(UNFS3_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+	(cd $(BUILD_DIR)/$(UNFS3) && \
+   ./configure)
+#	patch -d $(BUILD_DIR)/$(UNFS3) -p1 < $(UNFS3_PATCH)
+	mv $(BUILD_DIR)/$(UNFS3) $(UNFS3_DIR)
+	touch $(UNFS3_DIR)/.configured
+
+unfs3-unpack: $(UNFS3_DIR)/.configured
+
+$(UNFS3_DIR)/unfsd: $(UNFS3_DIR)/.configured
+	make -C $(UNFS3_DIR) CC=$(TARGET_CC) AR=$(TARGET_AR) RANLIB=$(TARGET_RANLIB) LDFLAGS="-L$(STAGING_DIR)/lib/lib -lfl"
+
+unfs3: $(UNFS3_DIR)/unfsd
+
+$(UNFS3_IPK): $(UNFS3_DIR)/unfsd
+	install -d $(UNFS3_IPK_DIR)/CONTROL
+	install -d $(UNFS3_IPK_DIR)/opt/sbin $(UNFS3_IPK_DIR)/opt/etc/init.d
+	$(STRIP) $(UNFS3_DIR)/unfsd -o $(UNFS3_IPK_DIR)/opt/sbin/unfsd
+	install -m 755 $(SOURCE_DIR)/unfs3.rc $(UNFS3_IPK_DIR)/opt/etc/init.d/S56unfsd
+	install -m 644 $(SOURCE_DIR)/unfs3.control  $(UNFS3_IPK_DIR)/CONTROL/control
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(UNFS3_IPK_DIR)
+
+unfs3-ipk: $(UNFS3_IPK)
+
+unfs3-clean:
+	-make -C $(UNFS3_DIR) clean
+
+unfs3-dirclean:
+	rm -rf $(UNFS3_DIR) $(UNFS3_IPK_DIR) $(UNFS3_IPK)
