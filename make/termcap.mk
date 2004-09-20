2e6237a405e32312a3434a8446e7b698fc414d8c
blunck2 <blunck2@1f1dca69-5516-0410-b834-e485effd04c3>
2004-09-20 05:02:27 +0000

    added.  makefile for building termcap

---
+++ make/termcap.mk
@@ -0,0 +1,54 @@
+#############################################################
+#
+# libtermcap
+#
+#############################################################
+
+TERMCAP_DIR:=$(BUILD_DIR)/termcap
+
+TERMCAP_VERSION=1.3.1
+TERMCAP=termcap-$(TERMCAP_VERSION)
+TERMCAP_SITE=ftp://ftp.gnu.org/gnu/termcap/
+TERMCAP_SOURCE:=$(TERMCAP).tar.gz
+TERMCAP_UNZIP=zcat
+TERMCAP_IPK=$(BUILD_DIR)/termcap_$(TERMCAP_VERSION)-1_armeb.ipk
+TERMCAP_IPK_DIR:=$(BUILD_DIR)/termcap-$(TERMCAP_VERSION)-ipk
+
+$(DL_DIR)/$(TERMCAP_SOURCE):
+	$(WGET) -P $(DL_DIR) $(TERMCAP_SITE)/$(TERMCAP_SOURCE)
+
+termcap-source: $(DL_DIR)/$(TERMCAP_SOURCE) $(TERMCAP_PATCH)
+
+
+# make changes to the BUILD options below.  If you are using TCP Wrappers, 
+# set --libwrap-directory=pathname 
+
+$(TERMCAP_DIR)/.configured: $(DL_DIR)/$(TERMCAP_SOURCE)
+	@rm -rf $(BUILD_DIR)/$(TERMCAP) $(TERMCAP_DIR)
+	$(TERMCAP_UNZIP) $(DL_DIR)/$(TERMCAP_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+	mv $(BUILD_DIR)/$(TERMCAP) $(TERMCAP_DIR)
+	(cd $(TERMCAP_DIR) && \
+   ./configure --disable-tv --without-tv)
+	touch $(TERMCAP_DIR)/.configured
+
+termcap-unpack: $(TERMCAP_DIR)/.configured
+
+$(TERMCAP_DIR)/termcap: $(TERMCAP_DIR)/.configured
+	make -C $(TERMCAP_DIR) CC=$(TARGET_CC) AR=$(TARGET_AR) RANLIB=$(TARGET_RANLIB) 
+
+termcap: $(TERMCAP_DIR)/termcap
+
+$(TERMCAP_IPK): $(TERMCAP_DIR)/termcap
+	install -d $(TERMCAP_IPK_DIR)/CONTROL
+	mkdir -p $(TERMCAP_IPK_DIR)/opt/lib 
+	cp $(TERMCAP_DIR)/libtermcap.a $(TERMCAP_IPK_DIR)/opt/lib 
+	install -m 644 $(SOURCE_DIR)/termcap.control  $(TERMCAP_IPK_DIR)/CONTROL/control
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(TERMCAP_IPK_DIR)
+
+termcap-ipk: $(TERMCAP_IPK)
+
+termcap-clean:
+	-make -C $(TERMCAP_DIR) clean
+
+termcap-dirclean:
+	rm -rf $(TERMCAP_DIR) $(TERMCAP_IPK_DIR) $(TERMCAP_IPK)
