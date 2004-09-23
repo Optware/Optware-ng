8ee2c486d075030389375a19877763e7f313976e
blunck2 <blunck2@1f1dca69-5516-0410-b834-e485effd04c3>
2004-09-23 05:22:32 +0000

    added.  makefile for that trusty little backup/restore program

---
+++ trunk/make/dump.mk
@@ -0,0 +1,87 @@
+###########################################################
+#
+# dump
+#
+###########################################################
+
+DUMP_DIR=$(BUILD_DIR)/dump
+
+DUMP_VERSION=0.4b37
+DUMP=dump-$(DUMP_VERSION)
+DUMP_SITE=http://aleron.dl.sourceforge.net/sourceforge/dump/
+DUMP_SOURCE=$(DUMP).tar.gz
+DUMP_UNZIP=zcat
+
+DUMP_IPK=$(BUILD_DIR)/dump_$(DUMP_VERSION)-1_armeb.ipk
+DUMP_IPK_DIR=$(BUILD_DIR)/dump-$(DUMP_VERSION)-ipk
+
+CFLAGS="-I $(STAGING_DIR)/include"
+LDFLAGS="-L $(STAGING_DIR)/lib"
+
+
+$(DL_DIR)/$(DUMP_SOURCE):
+	$(WGET) -P $(DL_DIR) $(DUMP_SITE)/$(DUMP_SOURCE)
+
+dump-source: $(DL_DIR)/$(DUMP_SOURCE)
+
+$(DUMP_DIR)/.source: $(DL_DIR)/$(DUMP_SOURCE)
+	$(DUMP_UNZIP) $(DL_DIR)/$(DUMP_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+	mv $(BUILD_DIR)/dump-$(DUMP_VERSION) $(DUMP_DIR)
+	touch $(DUMP_DIR)/.source
+
+$(DUMP_DIR)/.configured: $(DUMP_DIR)/.source
+	(cd $(DUMP_DIR); \
+		./configure \
+	  --disable-readline \
+		--prefix=$(DUMP_IPK_DIR)/opt \
+	);
+	touch $(DUMP_DIR)/.configured
+
+$(DUMP_IPK_DIR): $(DUMP_DIR)/.configured
+	$(MAKE) \
+	  -C $(DUMP_DIR) \
+	  CC_FOR_BUILD=$(CC) \
+	  CC=$(TARGET_CC) \
+	  OPT=$(CFLAGS) \
+	  ALL_LDFLAGS=$(LDFLAGS) \
+	  RANLIB=$(TARGET_RANLIB) \
+	  AR=$(TARGET_AR) \
+	  LD=$(TARGET_CC)
+
+
+dump-headers: $(DUMP_IPK_DIR)
+
+dump: $(DUMP_IPK_DIR)
+
+$(DUMP_IPK): $(DUMP_IPK_DIR)
+	mkdir -p $(DUMP_IPK_DIR)/CONTROL
+	cp $(SOURCE_DIR)/dump.control $(DUMP_IPK_DIR)/CONTROL/control
+	mkdir -p $(DUMP_IPK_DIR)/opt/sbin
+
+	$(STRIP) $(DUMP_DIR)/dump/dump
+	cp $(DUMP_DIR)/dump/dump $(DUMP_IPK_DIR)/opt/sbin
+
+	$(STRIP) $(DUMP_DIR)/restore/restore
+	cp $(DUMP_DIR)/restore/restore $(DUMP_IPK_DIR)/opt/sbin
+
+	$(STRIP) $(DUMP_DIR)/rmt/rmt
+	cp $(DUMP_DIR)/rmt/rmt $(DUMP_IPK_DIR)/opt/sbin
+
+	rm -rf $(STAGING_DIR)/CONTROL
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(DUMP_IPK_DIR)
+
+$(DUMP_IPK)/staging: $(DUMP_IPK)
+
+
+dump-ipk: $(DUMP_IPK)/staging $(DUMP_IPK)
+
+dump-source: $(DL_DIR)/$(DUMP_SOURCE)
+
+dump-clean:
+	-$(MAKE) -C $(DUMP_DIR) uninstall
+	-$(MAKE) -C $(DUMP_DIR) clean
+
+dump-distclean:
+	-rm $(DUMP_DIR)/.configured
+	-$(MAKE) -C $(DUMP_DIR) distclean
+
