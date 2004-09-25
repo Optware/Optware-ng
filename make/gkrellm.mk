a641dc0ac3ee3f762d5dca5036f0e47bed5e4565
blunck2 <blunck2@1f1dca69-5516-0410-b834-e485effd04c3>
2004-09-25 05:43:23 +0000

    added.  a makefile for the gkrellm server!!!

---
+++ make/gkrellm.mk
@@ -0,0 +1,68 @@
+###########################################################
+#
+# gkrellm
+#
+###########################################################
+
+GKRELLM_DIR=$(BUILD_DIR)/gkrellm
+
+GKRELLM_VERSION=2.2.4
+GKRELLM=gkrellm-$(GKRELLM_VERSION)
+GKRELLM_SITE=http://web.wt.net/~billw/gkrellm/
+GKRELLM_SOURCE=$(GKRELLM).tar.gz
+GKRELLM_UNZIP=zcat
+
+GKRELLM_IPK=$(BUILD_DIR)/gkrellm_$(GKRELLM_VERSION)-1_armeb.ipk
+GKRELLM_IPK_DIR=$(BUILD_DIR)/gkrellm-$(GKRELLM_VERSION)-ipk
+
+SYSLIBS="-I $(STAGING_DIR)/include/glib-2.0 \
+	-I $(STAGING_DIR)/include/gmodule-2.0 \
+	-L $(STAGING_DIR)/lib/ \
+	-lz \
+	-lgmodule-2.0 \
+	-lglib-2.0 \
+	-lgthread-2.0 \
+	-Wl \
+	--export-dynamic \
+	-pthread"
+
+$(DL_DIR)/$(GKRELLM_SOURCE):
+	$(WGET) -P $(DL_DIR) $(GKRELLM_SITE)/$(GKRELLM_SOURCE)
+
+gkrellm-source: $(DL_DIR)/$(GKRELLM_SOURCE)
+
+$(GKRELLM_DIR)/.source: $(DL_DIR)/$(GKRELLM_SOURCE)
+	$(GKRELLM_UNZIP) $(DL_DIR)/$(GKRELLM_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+	mv $(BUILD_DIR)/gkrellm-$(GKRELLM_VERSION) $(GKRELLM_DIR)
+	touch $(GKRELLM_DIR)/.source
+
+$(GKRELLM_DIR)/.configured: $(GKRELLM_DIR)/.source
+	touch $(GKRELLM_DIR)/.configured
+
+$(GKRELLM_IPK_DIR): $(GKRELLM_DIR)/.configured
+	$(MAKE) -C $(GKRELLM_DIR)/server CC=$(TARGET_CC) \
+	RANLIB=$(TARGET_RANLIB) AR=$(TARGET_AR) LD=$(TARGET_LD) SYS_LIBS=$(SYSLIBS) 
+
+gkrellm-headers: $(GKRELLM_IPK_DIR)
+
+gkrellm: $(GKRELLM_IPK_DIR)
+
+$(GKRELLM_IPK): $(GKRELLM_IPK_DIR)
+	mkdir -p $(GKRELLM_IPK_DIR)/CONTROL
+	cp $(SOURCE_DIR)/gkrellm.control $(GKRELLM_IPK_DIR)/CONTROL/control
+	$(STRIP) $(GKRELLM_DIR)/gkrellm
+	rm -rf $(STAGING_DIR)/CONTROL
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(GKRELLM_IPK_DIR)
+
+gkrellm-ipk: $(GKRELLM_IPK) $(GKRELLM_IPK)/staging
+
+gkrellm-source: $(DL_DIR)/$(GKRELLM_SOURCE)
+
+gkrellm-clean:
+	-$(MAKE) -C $(GKRELLM_DIR) uninstall
+	-$(MAKE) -C $(GKRELLM_DIR) clean
+
+gkrellm-distclean:
+	-rm $(GKRELLM_DIR)/.configured
+	-$(MAKE) -C $(GKRELLM_DIR) distclean
+
