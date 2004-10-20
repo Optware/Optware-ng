5661c0fd0f5f2ddd53f90dfb0cee21cbe7601eab
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2004-10-20 14:25:43 +0000

    Updated to match latest template

--- make/slugtool.mk
+++ make/slugtool.mk
@@ -4,45 +4,46 @@
 #
 #############################################################
 
-SLUGTOOL_DIR:=$(TOOL_BUILD_DIR)/slugtool
-SLUGTOOL_SOURCE_DIR:=$(SOURCE_DIR)/slugtool
+SLUGTOOL_SITE=http://www.lantz.com/filemgmt_data/files/
+SLUGTOOL_SOURCE=slugtool.tar.gz
+SLUGTOOL_DIR=slugtool
+SLUGTOOL_UNZIP=zcat
 
-SLUGTOOL_SITE:=http://www.lantz.com/filemgmt_data/files/
-SLUGTOOL_SOURCE:=slugtool.tar.gz
-SLUGTOOL_UNZIP:=zcat
+SLUGTOOL_PATCH=$(SLUGTOOL_SOURCE_DIR)/redboot_typo.patch
 
-SLUGTOOL_PATCH:=$(SLUGTOOL_SOURCE_DIR)/redboot_typo.patch
+SLUGTOOL_BUILD_DIR=$(TOOL_BUILD_DIR)/slugtool
+SLUGTOOL_SOURCE_DIR=$(SOURCE_DIR)/slugtool
 
 $(DL_DIR)/$(SLUGTOOL_SOURCE):
 	$(WGET) -P $(DL_DIR) $(SLUGTOOL_SITE)/$(SLUGTOOL_SOURCE)
 
 slugtool-source: $(DL_DIR)/$(SLUGTOOL_SOURCE) $(SLUGTOOL_PATCH)
 
-$(SLUGTOOL_DIR)/.configured: $(DL_DIR)/$(SLUGTOOL_SOURCE) $(SLUGTOOL_PATCH)
-	@rm -rf $(SLUGTOOL_DIR)
-	mkdir -p $(SLUGTOOL_DIR)
-	$(SLUGTOOL_UNZIP) $(DL_DIR)/$(SLUGTOOL_SOURCE) | tar -C $(SLUGTOOL_DIR) -xvf -
-	cat $(SLUGTOOL_PATCH) | patch -d $(SLUGTOOL_DIR) -p1
-	touch $(SLUGTOOL_DIR)/.configured
+$(SLUGTOOL_BUILD_DIR)/.configured: $(DL_DIR)/$(SLUGTOOL_SOURCE) $(SLUGTOOL_PATCH)
+	@rm -rf $(SLUGTOOL_BUILD_DIR)
+	mkdir -p $(SLUGTOOL_BUILD_DIR)
+	$(SLUGTOOL_UNZIP) $(DL_DIR)/$(SLUGTOOL_SOURCE) | tar -C $(SLUGTOOL_BUILD_DIR) -xvf -
+	cat $(SLUGTOOL_PATCH) | patch -d $(SLUGTOOL_BUILD_DIR) -p1
+	touch $(SLUGTOOL_BUILD_DIR)/.configured
 
-slugtool-unpack: $(SLUGTOOL_DIR)/.configured
+slugtool-unpack: $(SLUGTOOL_BUILD_DIR)/.configured
 
-$(SLUGTOOL_DIR)/slugtool: $(SLUGTOOL_DIR)/.configured
-	make -C $(SLUGTOOL_DIR) slugtool
+$(SLUGTOOL_BUILD_DIR)/slugtool: $(SLUGTOOL_BUILD_DIR)/.configured
+	make -C $(SLUGTOOL_BUILD_DIR) slugtool
 
-$(STAGING_DIR)/bin/slugtool: $(SLUGTOOL_DIR)/slugtool
-	mkdir -p $(STAGING_DIR)/bin
-	install -m 755 $(SLUGTOOL_DIR)/slugtool $(STAGING_DIR)/bin/slugtool
+$(STAGING_DIR)/bin/slugtool: $(SLUGTOOL_BUILD_DIR)/slugtool
+	install -d $(STAGING_DIR)/bin
+	install -m 755 $(SLUGTOOL_BUILD_DIR)/slugtool $(STAGING_DIR)/bin/slugtool
 
 slugtool: $(STAGING_DIR)/bin/slugtool
 
 slugtool-install: slugtool
 
 slugtool-clean:
-	-make -C $(SLUGTOOL_DIR) clean
-	rm -f $(STAGING_DIR)/bin/slugtool
+	-make -C $(SLUGTOOL_BUILD_DIR) clean
 
 slugtool-dirclean:
-	rm -rf $(SLUGTOOL_DIR)
+	rm -rf $(SLUGTOOL_BUILD_DIR)
+	rm -f $(STAGING_DIR)/bin/slugtool
 
-SLUGTOOL := PATH=$(TARGET_PATH) slugtool
+SLUGTOOL = PATH=$(TARGET_PATH) slugtool
