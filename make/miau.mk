6431927627f88da74a3714da07e15975bac5478e
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2004-10-20 14:38:40 +0000

    Updated to match latest template

--- make/miau.mk
+++ make/miau.mk
@@ -4,61 +4,38 @@
 #
 ###########################################################
 
-MIAU_DIR=$(BUILD_DIR)/miau
-MIAU_SOURCE_DIR=$(SOURCE_DIR)/miau
-MIAU_VERSION=0.5.3
-MIAU=miau-$(MIAU_VERSION)
 MIAU_SITE=http://aleron.dl.sourceforge.net/sourceforge/miau
-MIAU_SOURCE_ARCHIVE=$(MIAU).tar.gz
+MIAU_VERSION=0.5.3
+MIAU_SOURCE=miau-$(MIAU_VERSION).tar.gz
+MIAU_DIR=miau-$(MIAU_VERSION)
 MIAU_UNZIP=zcat
 
-MIAU_IPK=$(BUILD_DIR)/miau_$(MIAU_VERSION)-2_armeb.ipk
+MIAU_IPK_VERSION=2
+
+MIAU_CPPFLAGS=
+MIAU_LDFLAGS=
+
+MIAU_BUILD_DIR=$(BUILD_DIR)/miau
+MIAU_SOURCE_DIR=$(SOURCE_DIR)/miau
 MIAU_IPK_DIR=$(BUILD_DIR)/miau-$(MIAU_VERSION)-ipk
+MIAU_IPK=$(BUILD_DIR)/miau_$(MIAU_VERSION)-$(MIAU_IPK_VERSION)_armeb.ipk
 
-#
-# Setting these up because Flex includes and libraries get put in these
-# locations and IRCD Hybrid needs them.
-#
-MY_STAGING_CPPFLAGS="$(STAGING_CPPFLAGS) -I$(STAGING_DIR)/include/include"
-MY_STAGING_LDFLAGS="$(STAGING_LDFLAGS) -L$(STAGING_DIR)/lib/lib"
+$(DL_DIR)/$(MIAU_SOURCE):
+	$(WGET) -P $(DL_DIR) $(MIAU_SITE)/$(MIAU_SOURCE)
 
-#
-# This is the dependency on the source code.  If the source is missing,
-# then it will be fetched from the site using wget.
-#
-$(DL_DIR)/$(MIAU_SOURCE_ARCHIVE):
-	$(WGET) -P $(DL_DIR) $(MIAU_SITE)/$(MIAU_SOURCE_ARCHIVE)
+miau-source: $(DL_DIR)/$(MIAU_SOURCE)
 
-#
-# The IRCD Hybrid source code depends on it existing within the
-# download directory.  This target will be called by the top level
-# Makefile to download the source code's archive (.tar.gz, .bz2, etc.)
-#
-miau-source: $(DL_DIR)/$(MIAU_SOURCE_ARCHIVE)
-
-#
-# This target unpacks the source code into the build directory.
-#
-$(MIAU_DIR)/.source: $(DL_DIR)/$(MIAU_SOURCE_ARCHIVE)
-	$(MIAU_UNZIP) $(DL_DIR)/$(MIAU_SOURCE_ARCHIVE) | tar -C $(BUILD_DIR) -xvf -
-	mv $(BUILD_DIR)/miau-$(MIAU_VERSION) $(MIAU_DIR)
-	touch $(MIAU_DIR)/.source
-
-#
-# This target configures the build within the build directory.
-# This is a fairly important note (cuz I wasted about 5 hours on it).
-# Flags usch as LDFLAGS and CPPFLAGS should be passed into configure
-# and NOT $(MAKE) below.  Passing it to configure causes configure to
-# correctly BUILD the Makefile with the right paths, where passing it
-# to Make causes it to override the default search paths of the compiler.
-#
-$(MIAU_DIR)/.configured: $(MIAU_DIR)/.source
-	(cd $(MIAU_DIR); \
-	export LDFLAGS=$(MY_STAGING_LDFLAGS); \
-	export CPPFLAGS=$(MY_STAGING_CPPFLAGS); \
-	./configure \
-		--host=$(GNU_TARGET_NAME) \
+$(MIAU_BUILD_DIR)/.configured: $(DL_DIR)/$(MIAU_SOURCE)
+	$(MIAU_UNZIP) $(DL_DIR)/$(MIAU_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+	mv $(BUILD_DIR)/$(MIAU_DIR) $(MIAU_BUILD_DIR)
+	(cd $(MIAU_BUILD_DIR); \
+		$(TARGET_CONFIGURE_OPTS) \
+		CPPFLAGS="$(STAGING_CPPFLAGS) $(MIAU_CPPFLAGS)" \
+		LDFLAGS="$(STAGING_LDFLAGS) $(MIAU_LDFLAGS)" \
+		./configure \
 		--build=$(GNU_HOST_NAME) \
+		--host=$(GNU_TARGET_NAME) \
+		--target=$(GNU_TARGET_NAME) \
 		--prefix=/opt	\
 		--enable-dccbounce \
 		--enable-automode \
@@ -74,59 +51,30 @@ $(MIAU_DIR)/.configured: $(MIAU_DIR)/.source
 		--enable-pingstat \
 		--enable-dumpstatus \
 	);
-	touch $(MIAU_DIR)/.configured
+	touch $(MIAU_BUILD_DIR)/.configured
 
-#
-# This builds the actual binary (ircd).  IRCD Hybrid drops the final binary
-# in the src directory once built.
-#
-$(MIAU_DIR)/src/miau: $(MIAU_DIR)/.configured
-	$(MAKE) -C $(MIAU_DIR)	\
-	RANLIB=$(TARGET_RANLIB)
+miau-unpack: $(MIAU_BUILD_DIR)/.configured
 
-#
-# These are the dependencies for the binary.  IRCD Hybrid requires Zlib,
-# Flex, and the actual binary to exist before we're done.
-#
-miau: zlib flex $(MIAU_DIR)/src/miau
+$(MIAU_BUILD_DIR)/src/miau: $(MIAU_BUILD_DIR)/.configured
+	$(MAKE) -C $(MIAU_BUILD_DIR)
 
-#
-# This builds the IPK file.
-#
-$(MIAU_IPK): $(MIAU_DIR)/src/miau
-	install -d $(MIAU_IPK_DIR)/CONTROL
+miau: $(MIAU_BUILD_DIR)/src/miau
+
+$(MIAU_IPK): $(MIAU_BUILD_DIR)/src/miau
 	install -d $(MIAU_IPK_DIR)/opt/bin $(MIAU_IPK_DIR)/opt/etc/init.d
-	$(STRIP) $(MIAU_DIR)/src/miau -o $(MIAU_IPK_DIR)/opt/bin/miau
-	cp $(MIAU_DIR)/misc/miaurc $(MIAU_IPK_DIR)/opt/etc/miaurc
-	cp $(MIAU_SOURCE_DIR)/rc.miau $(MIAU_IPK_DIR)/opt/etc/init.d/S52miau
-	cp $(MIAU_SOURCE_DIR)/control $(MIAU_IPK_DIR)/CONTROL/control
-	cp $(MIAU_SOURCE_DIR)/postinst $(MIAU_IPK_DIR)/CONTROL/postinst
-	cp $(MIAU_SOURCE_DIR)/prerm $(MIAU_IPK_DIR)/CONTROL/prerm
+	$(STRIP) $(MIAU_BUILD_DIR)/src/miau -o $(MIAU_IPK_DIR)/opt/bin/miau
+	install -m 644 $(MIAU_BUILD_DIR)/misc/miaurc $(MIAU_IPK_DIR)/opt/etc/miaurc
+	install -m 755 $(MIAU_SOURCE_DIR)/rc.miau $(MIAU_IPK_DIR)/opt/etc/init.d/S52miau
+	install -d $(MIAU_IPK_DIR)/CONTROL
+	install -m 644 $(MIAU_SOURCE_DIR)/control $(MIAU_IPK_DIR)/CONTROL/control
+	install -m 644 $(MIAU_SOURCE_DIR)/postinst $(MIAU_IPK_DIR)/CONTROL/postinst
+	install -m 644 $(MIAU_SOURCE_DIR)/prerm $(MIAU_IPK_DIR)/CONTROL/prerm
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(MIAU_IPK_DIR)
 
-#
-# This is called from the top level makefile to create the IPK file.
-#
 miau-ipk: $(MIAU_IPK)
 
-#
-# This is called from the top level makefile to clean all of the built files.
-#
 miau-clean:
-	-$(MAKE) -C $(MIAU_DIR) uninstall
-	-$(MAKE) -C $(MIAU_DIR) clean
+	-$(MAKE) -C $(MIAU_BUILD_DIR) clean
 
-#
-# This is called from the top level makefile to clean ALL files, including
-# downloaded source.
-#
-miau-distclean:
-	-rm $(MIAU_DIR)/.configured
-	-$(MAKE) -C $(MIAU_DIR) distclean
-
-#
-# This is called from the top level makefile to clean all dynamically created
-# directories.
-#
 miau-dirclean:
-	rm -rf $(MIAU_DIR) $(MIAU_IPK_DIR) $(MIAU_IPK)
+	rm -rf $(MIAU_BUILD_DIR) $(MIAU_IPK_DIR) $(MIAU_IPK)
