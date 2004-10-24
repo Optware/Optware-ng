ff69e4cda3dd74dec3eab1d8abd9364970671abf
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2004-10-24 03:37:49 +0000

    Fixed the site URL

--- make/dropbear.mk
+++ make/dropbear.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-DROPBEAR_SITE=http://matt.ucc.asn.au/dropbear/releases
+DROPBEAR_SITE=http://matt.ucc.asn.au/dropbear/testing
 DROPBEAR_VERSION=0.44test4
 DROPBEAR_SOURCE=dropbear-$(DROPBEAR_VERSION).tar.bz2
 DROPBEAR_DIR=dropbear-$(DROPBEAR_VERSION)
