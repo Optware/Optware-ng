a1f081492836bd7fedd16fd9365109939ef34644
rpedde <rpedde@1f1dca69-5516-0410-b834-e485effd04c3>
2004-10-26 19:10:27 +0000

    slugbug#9 - move config files to /opt/etc/mt-daapd, bring sources in line with template packaging

--- make/mt-daapd.mk
+++ make/mt-daapd.mk
@@ -4,67 +4,91 @@
 #
 ###########################################################
 
-MT_DAAPD_DIR=$(BUILD_DIR)/mt-daapd
-
-MT_DAAPD_VERSION=0.2.1-pre1
-MT_DAAPD=mt-daapd-$(MT_DAAPD_VERSION)
 MT_DAAPD_SITE=http://belnet.dl.sourceforge.net/sourceforge/mt-daapd
-MT_DAAPD_SOURCE=$(MT_DAAPD).tar.gz
+MT_DAAPD_VERSION=0.2.1-pre1
+MT_DAAPD_SOURCE=mt-daapd-$(MT_DAAPD_VERSION).tar.gz
+MT_DAAPD_DIR=mt-daapd-$(MT_DAAPD_VERSION)
 MT_DAAPD_UNZIP=zcat
-MT_DAAPD_IPK=$(BUILD_DIR)/mt-daapd_$(MT_DAAPD_VERSION)-1_armeb.ipk
+
+MT_DAAPD_IPK_VERSION=2
+
+MT_DAAPD_PATCHES=
+
+MT_DAAPD_CPPFLAGS=-DSTRSEP
+MT_DAAPD_LDFLAGS=
+
+MT_DAAPD_BUILD_DIR=$(BUILD_DIR)/mt-daapd
+MT_DAAPD_SOURCE_DIR=$(SOURCE_DIR)/mt-daapd
 MT_DAAPD_IPK_DIR=$(BUILD_DIR)/mt-daapd-$(MT_DAAPD_VERSION)-ipk
+MT_DAAPD_IPK=$(BUILD_DIR)/mt-daapd_$(MT_DAAPD_VERSION)-$(MT_DAAPD_IPK_VERSION)_armeb.ipk
 
 $(DL_DIR)/$(MT_DAAPD_SOURCE):
 	$(WGET) -P $(DL_DIR) $(MT_DAAPD_SITE)/$(MT_DAAPD_SOURCE)
 
 mt-daapd-source: $(DL_DIR)/$(MT_DAAPD_SOURCE)
 
-$(MT_DAAPD_DIR)/.source: $(DL_DIR)/$(MT_DAAPD_SOURCE)
+$(MT_DAAPD_BUILD_DIR)/.configured: $(DL_DIR)/$(MT_DAAPD_SOURCE)
+	rm -rf $(BUILD_DIR)/$(MT_DAAPD_DIR) $(MT_DAAPD_BUILD_DIR)
 	$(MT_DAAPD_UNZIP) $(DL_DIR)/$(MT_DAAPD_SOURCE) | tar -C $(BUILD_DIR) -xvf -
-	mv $(BUILD_DIR)/$(MT_DAAPD) $(MT_DAAPD_DIR)
-	touch $(MT_DAAPD_DIR)/.source
+#	cat $(MT_DAAPD_PATCHES) | patch -d $(BUILD_DIR)/$(MT_DAAPD_DIR) -p1
+	mv $(BUILD_DIR)/$(MT_DAAPD_DIR) $(MT_DAAPD_BUILD_DIR)
 
-$(MT_DAAPD_DIR)/.configured: $(MT_DAAPD_DIR)/.source
-	export LDFLAGS=$(STAGING_LDFLAGS) CPPFLAGS=$(STAGING_CPPFLAGS); \
-	(cd $(MT_DAAPD_DIR); \
-	./configure \
-	--prefix=/opt \
-	--host=$(GNU_TARGET_NAME) \
-	--build=$(GNU_HOST_NAME) \
-	--target=$(GNU_TARGET_NAME) \
-        --with-static-libs=$(STAGING_DIR)/lib \
-	--with-gdbm-include=$(STAGING_DIR)/include \
-	LIBS="-lgdbm -lid3tag -lz" \
-	ac_cv_func_setpgrp_void=yes \
-	); \
-	touch $(MT_DAAPD_DIR)/.configured
+	(cd $(MT_DAAPD_BUILD_DIR); \
+		$(TARGET_CONFIGURE_OPTS) \
+		CPPFLAGS="$(STAGING_CPPFLAGS) $(MT_DAAPD_CPPFLAGS)" \
+		LDFLAGS="$(STAGING_LDFLAGS) $(MT_DAAPD_LDFLAGS)" \
+		./configure \
+		--build=$(GNU_HOST_NAME) \
+		--host=$(GNU_TARGET_NAME) \
+		--target=$(GNU_TARGET_NAME) \
+		--prefix=/opt \
+	        --with-static-libs=$(STAGING_DIR)/lib \
+		--with-gdbm-include=$(STAGING_DIR)/include \
+		LIBS="-lgdbm -lid3tag -lz" \
+		ac_cv_func_setpgrp_void=yes \
+	)
+	touch $(MT_DAAPD_BUILD_DIR)/.configured
 
-$(MT_DAAPD_DIR)/src/mt-daapd: $(MT_DAAPD_DIR)/.configured
-	$(MAKE) -C $(MT_DAAPD_DIR) CFLAGS="-DSTRSEP"
+mt-daapd-unpack: $(MT_DAAPD_BUILD_DIR)/.configured
 
-mt-daapd: zlib gdbm libid3tag $(MT_DAAPD_DIR)/src/mt-daapd
+$(MT_DAAPD_BUILD_DIR)/src/mt-daapd: $(MT_DAAPD_BUILD_DIR)/.configured
+	$(MAKE) -C $(MT_DAAPD_BUILD_DIR) CFLAGS="-DSTRSEP"
 
-$(MT_DAAPD_IPK): $(MT_DAAPD_DIR)/src/mt-daapd
-	-mkdir -p $(MT_DAAPD_IPK_DIR)	
-	$(MAKE) -C $(MT_DAAPD_DIR) DESTDIR=$(MT_DAAPD_IPK_DIR) install
+#	$(MAKE) -C $(MT_DAAPD_BUILD_DIR) CFLAGS="-DSTRSEP"
+
+mt-daapd: zlib gdbm libid3tag $(MT_DAAPD_BUILD_DIR)/src/mt-daapd
+
+$(MT_DAAPD_IPK): $(MT_DAAPD_BUILD_DIR)/src/mt-daapd
+	install -d $(MT_DAAPD_IPK_DIR)/opt/sbin
+	$(STRIP) --strip-unneeded $(MT_DAAPD_BUILD_DIR)/src/mt-daapd -o $(MT_DAAPD_IPK_DIR)/opt/sbin/mt-daapd
+	install -d $(MT_DAAPD_IPK_DIR)/opt/etc/init.d
+	install -m 755 $(MT_DAAPD_SOURCE_DIR)/rc.mt-daapd $(MT_DAAPD_IPK_DIR)/opt/etc/init.d/S60mt-daapd
 	install -d $(MT_DAAPD_IPK_DIR)/CONTROL
-	install -d $(MT_DAAPD_IPK_DIR)/opt/sbin $(MT_DAAPD_IPK_DIR)/opt/etc/init.d
+	install -m 644 $(MT_DAAPD_SOURCE_DIR)/control $(MT_DAAPD_IPK_DIR)/CONTROL/control
+	install -m 644 $(MT_DAAPD_SOURCE_DIR)/postinst $(MT_DAAPD_IPK_DIR)/CONTROL/postinst
+	install -m 644 $(MT_DAAPD_SOURCE_DIR)/prerm $(MT_DAAPD_IPK_DIR)/CONTROL/prerm
+	install -m 644 $(MT_DAAPD_SOURCE_DIR)/conffiles $(MT_DAAPD_IPK_DIR)/CONTROL/conffiles
+
+	install -d $(MT_DAAPD_IPK_DIR)/opt/etc/mt-daapd
+	install -m 644 $(MT_DAAPD_SOURCE_DIR)/mt-daapd.conf $(MT_DAAPD_IPK_DIR)/opt/etc/mt-daapd
+	install -m 644 $(MT_DAAPD_SOURCE_DIR)/mt-daapd.playlist $(MT_DAAPD_IPK_DIR)/opt/etc/mt-daapd
+
+	install -d $(MT_DAAPD_IPK_DIR)/opt/doc/mt-daapd
+	install -m 644 $(MT_DAAPD_SOURCE_DIR)/mt-daapd.conf $(MT_DAAPD_IPK_DIR)/opt/doc/mt-daapd
+	install -m 644 $(MT_DAAPD_SOURCE_DIR)/mt-daapd.playlist $(MT_DAAPD_IPK_DIR)/opt/doc/mt-daapd
+
 	install -d $(MT_DAAPD_IPK_DIR)/opt/share/mt-daapd/admin-root
+	install -m 644 $(MT_DAAPD_BUILD_DIR)/admin-root/* $(MT_DAAPD_IPK_DIR)/opt/share/mt-daapd/admin-root
+	rm -f $(MT_DAAPD_IPK_DIR)/opt/share/mt-daapd/admin-root/Makefile*
 	install -d $(MT_DAAPD_IPK_DIR)/opt/var/mt-daapd
-	$(STRIP) --strip-unneeded $(MT_DAAPD_IPK_DIR)/sbin/mt-daapd -o $(MT_DAAPD_IPK_DIR)/opt/sbin/mt-daapd
-	install -m 644 $(MT_DAAPD_DIR)/admin-root/* $(MT_DAAPD_IPK_DIR)/opt/share/mt-daapd/admin-root
-	install -m 755 $(SOURCE_DIR)/mt-daapd.rc $(MT_DAAPD_IPK_DIR)/opt/etc/init.d/S60mt-daapd
-	install -m 644 $(SOURCE_DIR)/mt-daapd.conf $(MT_DAAPD_IPK_DIR)/opt/etc
-	install -m 644 $(SOURCE_DIR)/mt-daapd.playlist $(MT_DAAPD_IPK_DIR)/opt/etc
-	install -m 644 $(SOURCE_DIR)/mt-daapd.control $(MT_DAAPD_IPK_DIR)/CONTROL/control
+
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(MT_DAAPD_IPK_DIR)
 
 mt-daapd-ipk: $(MT_DAAPD_IPK)
 
 mt-daapd-clean:
-	-$(MAKE) -C $(MT_DAAPD_DIR) uninstall
-	-$(MAKE) -C $(MT_DAAPD_DIR) clean
+	-$(MAKE) -C $(MT_DAAPD_BUILD_DIR) clean
 
 mt-daapd-dirclean: mt-daapd-clean
-	rm -rf $(MT_DAAPD_DIR) $(MT_DAAPD_IPK_DIR) $(MT_DAAPD_IPK)
+	rm -rf $(MT_DAAPD_BUILD_DIR) $(MT_DAAPD_IPK_DIR) $(MT_DAAPD_IPK)
 
