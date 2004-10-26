e5a1bc4d00f94abb015ee3874e85a07b14a9a7d8
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2004-10-26 13:35:32 +0000

    Updated to match template

--- make/ntpclient.mk
+++ make/ntpclient.mk
@@ -4,52 +4,54 @@
 #
 ###########################################################
 
-NTPCLIENT_DIR=$(BUILD_DIR)/ntpclient
-NTPCLIENT_SOURCE_DIR=$(SOURCE_DIR)/ntpclient
-
+#NTPCLIENT_SITE=http://doolittle.faludi.com/ntpclient
+NTPCLIENT_SITE=http://ipkg.nslu2-linux.org/downloads
 NTPCLIENT_VERSION=2003_194
-NTPCLIENT=ntpclient_$(NTPCLIENT_VERSION)
-#NTPCLIENT_SITE=http://doolittle.faludi.com/ntpclient/
-NTPCLIENT_SITE=ipkg.nslu2-linux.org/downloads
-NTPCLIENT_SOURCE=$(NTPCLIENT).tar.gz
+NTPCLIENT_SOURCE=ntpclient_$(NTPCLIENT_VERSION).tar.gz
+NTPCLIENT_DIR=ntpclient
 NTPCLIENT_UNZIP=zcat
 
-NTPCLIENT_IPK=$(BUILD_DIR)/ntpclient_$(NTPCLIENT_VERSION)-1_armeb.ipk
+NTPCLIENT_IPK_VERSION=1
+
+NTPCLIENT_CPPFLAGS=
+NTPCLIENT_LDFLAGS=
+
+NTPCLIENT_BUILD_DIR=$(BUILD_DIR)/ntpclient
+NTPCLIENT_SOURCE_DIR=$(SOURCE_DIR)/ntpclient
 NTPCLIENT_IPK_DIR=$(BUILD_DIR)/ntpclient-$(NTPCLIENT_VERSION)-ipk
+NTPCLIENT_IPK=$(BUILD_DIR)/ntpclient_$(NTPCLIENT_VERSION)-$(NTPCLIENT_IPK_VERSION)_armeb.ipk
 
 $(DL_DIR)/$(NTPCLIENT_SOURCE):
 	$(WGET) -P $(DL_DIR) $(NTPCLIENT_SITE)/$(NTPCLIENT_SOURCE)
 
 ntpclient-source: $(DL_DIR)/$(NTPCLIENT_SOURCE)
 
-$(NTPCLIENT_DIR)/.configured: $(DL_DIR)/$(NTPCLIENT_SOURCE)
+$(NTPCLIENT_BUILD_DIR)/.configured: $(DL_DIR)/$(NTPCLIENT_SOURCE)
 	$(NTPCLIENT_UNZIP) $(DL_DIR)/$(NTPCLIENT_SOURCE) | tar -C $(BUILD_DIR) -xvf -
-	touch $(NTPCLIENT_DIR)/.configured
+#	mv $(BUILD_DIR)/$(NTPCLIENT_DIR) $(NTPCLIENT_BUILD_DIR)
+	touch $(NTPCLIENT_BUILD_DIR)/.configured
 
-ntpclient-unpack: $(NTPCLIENT_DIR)/.configured
+ntpclient-unpack: $(NTPCLIENT_BUILD_DIR)/.configured
 
-$(NTPCLIENT_DIR)/ntpclient: $(NTPCLIENT_DIR)/.configured
-	$(MAKE) -C $(NTPCLIENT_DIR) ntpclient adjtimex CC=$(TARGET_CC) \
+$(NTPCLIENT_BUILD_DIR)/ntpclient: $(NTPCLIENT_BUILD_DIR)/.configured
+	$(MAKE) -C $(NTPCLIENT_BUILD_DIR) ntpclient adjtimex CC=$(TARGET_CC) \
 	RANLIB=$(TARGET_RANLIB) AR=$(TARGET_AR) LD=$(TARGET_LD) 
 
-ntpclient: $(NTPCLIENT_DIR)/ntpclient
+ntpclient: $(NTPCLIENT_BUILD_DIR)/ntpclient
 
-$(NTPCLIENT_IPK): $(NTPCLIENT_DIR)/ntpclient
-	mkdir -p $(NTPCLIENT_IPK_DIR)/CONTROL
-	install -d $(NTPCLIENT_IPK_DIR)/opt/bin $(NTPCLIENT_IPK_DIR)/opt/sbin
-	cp $(NTPCLIENT_SOURCE_DIR)/control $(NTPCLIENT_IPK_DIR)/CONTROL/control
-	$(STRIP) $(NTPCLIENT_DIR)/ntpclient -o $(NTPCLIENT_IPK_DIR)/opt/bin/ntpclient
-	$(STRIP) $(NTPCLIENT_DIR)/adjtimex -o $(NTPCLIENT_IPK_DIR)/opt/sbin/adjtimex
+$(NTPCLIENT_IPK): $(NTPCLIENT_BUILD_DIR)/ntpclient
+	install -d $(NTPCLIENT_IPK_DIR)/opt/bin
+	$(STRIP) $(NTPCLIENT_BUILD_DIR)/ntpclient -o $(NTPCLIENT_IPK_DIR)/opt/bin/ntpclient
+	install -d $(NTPCLIENT_IPK_DIR)/opt/sbin
+	$(STRIP) $(NTPCLIENT_BUILD_DIR)/adjtimex -o $(NTPCLIENT_IPK_DIR)/opt/sbin/adjtimex
+	install -d $(NTPCLIENT_IPK_DIR)/CONTROL
+	install -m 644 $(NTPCLIENT_SOURCE_DIR)/control $(NTPCLIENT_IPK_DIR)/CONTROL/control
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(NTPCLIENT_IPK_DIR)
 
 ntpclient-ipk: $(NTPCLIENT_IPK)
 
 ntpclient-clean:
-	-$(MAKE) -C $(NTPCLIENT_DIR) clean
+	-$(MAKE) -C $(NTPCLIENT_BUILD_DIR) clean
 
-ntpclient-distclean:
-	-rm $(NTPCLIENT_DIR)/.configured
-	-$(MAKE) -C $(NTPCLIENT_DIR) clean
-
-ntpclient-dirclean:
-	rm -rf $(NTPCLIENT_DIR) $(NTPCLIENT_IPK_DIR) $(NTPCLIENT_IPK)
+ntpclient-dirclean: ntpclient-clean
+	rm -rf $(BUILD_DIR)/$(NTPCLIENT_DIR) $(NTPCLIENT_BUILD_DIR) $(NTPCLIENT_IPK_DIR) $(NTPCLIENT_IPK)
