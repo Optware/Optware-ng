b8bb14f7bcf94d389c6569b16f53d5f7a085abb0
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2004-10-27 12:02:02 +0000

    Updated to match template and staging_dir policy.

--- make/gdbm.mk
+++ make/gdbm.mk
@@ -4,67 +4,70 @@
 #
 ###########################################################
 
-GDBM_DIR=$(BUILD_DIR)/gdbm
-
-GDBM_VERSION=1.8.3
-GDBM_LIBVERSION=3.0.0
-GDBM=gdbm-$(GDBM_VERSION)
 GDBM_SITE=ftp://ftp.gnu.org/gnu/gdbm
-GDBM_SOURCE=$(GDBM).tar.gz
+GDBM_VERSION=1.8.3
+GDBM_LIB_VERSION=3.0.0
+GDBM_SOURCE=gdbm-$(GDBM_VERSION).tar.gz
+GDBM_DIR=gdbm-$(GDBM_VERSION)
 GDBM_UNZIP=zcat
-ZLIB_CFLAGS= $(TARGET_CFLAGS) -fPIC
 
-GDBM_PATCH:=$(SOURCE_DIR)/gdbm.patch
+GDBM_IPK_VERSION=1
 
-GDBM_IPK=$(BUILD_DIR)/gdbm_$(GDBM_VERSION)-1_armeb.ipk
+GDBM_PATCHES=$(GDBM_SOURCE_DIR)/Makefile.patch
+
+GDBM_BUILD_DIR=$(BUILD_DIR)/gdbm
+GDBM_SOURCE_DIR=$(SOURCE_DIR)/gdbm
+GDBM_IPK=$(BUILD_DIR)/gdbm_$(GDBM_VERSION)-$(GDBM_IPK_VERSION)_armeb.ipk
 GDBM_IPK_DIR=$(BUILD_DIR)/gdbm-$(GDBM_VERSION)-ipk
 
 $(DL_DIR)/$(GDBM_SOURCE):
 	$(WGET) -P $(DL_DIR) $(GDBM_SITE)/$(GDBM_SOURCE)
 
-gdbm-source: $(DL_DIR)/$(GDBM_SOURCE) $(GDBM_PATCH)
+gdbm-source: $(DL_DIR)/$(GDBM_SOURCE) $(GDBM_PATCHES)
 
-$(GDBM_DIR)/.source: $(DL_DIR)/$(GDBM_SOURCE)
+$(GDBM_BUILD_DIR)/.configured: $(DL_DIR)/$(GDBM_SOURCE)
+	rm -rf $(BUILD_DIR)/$(GDBM_DIR) $(GDBM_BUILD_DIR)
 	$(GDBM_UNZIP) $(DL_DIR)/$(GDBM_SOURCE) | tar -C $(BUILD_DIR) -xvf -
-	cat $(GDBM_PATCH) | patch -d $(BUILD_DIR)/$(GDBM) -p1
-	mv $(BUILD_DIR)/$(GDBM) $(GDBM_DIR)
-	touch $(GDBM_DIR)/.source
-
-$(GDBM_DIR)/.configured: $(GDBM_DIR)/.source
-	(cd $(GDBM_DIR); \
+	cat $(GDBM_PATCHES) | patch -d $(BUILD_DIR)/$(GDBM_DIR) -p1
+	mv $(BUILD_DIR)/$(GDBM_DIR) $(GDBM_BUILD_DIR)
+	(cd $(GDBM_BUILD_DIR); \
+		$(TARGET_CONFIGURE_OPTS) \
 		./configure \
-		--host=$(GNU_TARGET_NAME) \
 		--build=$(GNU_HOST_NAME) \
-		--prefix=$(STAGING_DIR) \
+		--host=$(GNU_TARGET_NAME) \
+		--target=$(GNU_TARGET_NAME) \
+		--prefix=/opt \
 	);
-	touch $(GDBM_DIR)/.configured
+	touch $(GDBM_BUILD_DIR)/.configured
 
-$(STAGING_DIR)/lib/libgdbm.so.$(GDBM_LIBVERSION): $(GDBM_DIR)/.configured
-	$(MAKE) -C $(GDBM_DIR) install
+gdbm-unpack: $(GDBM_BUILD_DIR)/.configured
 
-gdbm-headers: $(STAGING_DIR)/lib/libgdbm.a
+$(GDBM_BUILD_DIR)/.libs/libgdbm.a: $(GDBM_BUILD_DIR)/.configured
+	$(MAKE) -C $(GDBM_BUILD_DIR)
 
-gdbm: $(STAGING_DIR)/lib/libgdbm.so.$(GDBM_LIBVERSION)
+gdbm: $(GDBM_BUILD_DIR)/.libs/libgdbm.a
 
-$(GDBM_IPK): $(STAGING_DIR)/lib/libgdbm.so.$(GDBM_LIBVERSION)
-	mkdir -p $(GDBM_IPK_DIR)/CONTROL
-	cp $(SOURCE_DIR)/gdbm.control $(GDBM_IPK_DIR)/CONTROL/control
-	mkdir -p $(GDBM_IPK_DIR)/opt/include
-	cp -dpf $(STAGING_DIR)/include/gdbm.h $(GDBM_IPK_DIR)/opt/include
-	mkdir -p $(GDBM_IPK_DIR)/opt/lib
-	cp -dpf $(STAGING_DIR)/lib/libgdbm.so* $(GDBM_IPK_DIR)/opt/lib
-	-$(STRIP) --strip-unneeded $(GDBM_IPK_DIR)/opt/lib/libgdbm.so*
-	touch -c $(GDBM_IPK_DIR)/opt/lib/libgdbm.so.$(GDBM_LIBVERSION)
+$(STAGING_DIR)/opt/lib/libgdbm.a: $(GDBM_BUILD_DIR)/.libs/libgdbm.a
+	$(MAKE) -C $(GDBM_BUILD_DIR) INSTALL_ROOT=$(STAGING_DIR) install install-compat
+	rm -rf $(STAGING_DIR)/opt/{man,info}
+
+gdbm-stage: $(STAGING_DIR)/opt/lib/libgdbm.a
+
+$(GDBM_IPK): $(GDBM_BUILD_DIR)/.libs/libgdbm.a
+	rm -rf $(GDBM_IPK_DIR) $(GDBM_IPK)
+	$(MAKE) -C $(GDBM_BUILD_DIR) INSTALL_ROOT=$(GDBM_IPK_DIR) install install-compat
+	$(STRIP) --strip-unneeded $(GDBM_IPK_DIR)/opt/lib/*.so.*
+	rm -rf $(GDBM_IPK_DIR)/opt/{man,info}
+	rm -f $(GDBM_IPK_DIR)/opt/lib/*.{la,a}
+	install -d $(GDBM_IPK_DIR)/CONTROL
+	install -m 644 $(GDBM_SOURCE_DIR)/control $(GDBM_IPK_DIR)/CONTROL/control
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(GDBM_IPK_DIR)
 
 gdbm-ipk: $(GDBM_IPK)
 
-gdbm-source: $(DL_DIR)/$(GDBM_SOURCE)
-
 gdbm-clean:
-	-$(MAKE) -C $(GDBM_DIR) uninstall
-	-$(MAKE) -C $(GDBM_DIR) clean
+	-$(MAKE) -C $(GDBM_BUILD_DIR) clean
 
 gdbm-dirclean: gdbm-clean
-	rm -rf $(GDBM_DIR) $(GDBM_IPK_DIR) $(GDBM_IPK)
+	rm -rf $(BUILD_DIR)/$(GDBM_DIR) $(GDBM_BUILD_DIR) $(GDBM_IPK_DIR) $(GDBM_IPK)
 
