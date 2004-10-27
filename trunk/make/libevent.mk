f33b7a9252fd852fb142d75a67aa0aec3eaf5170
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2004-10-27 12:53:37 +0000

    Updated to match template

--- trunk/make/libevent.mk
+++ trunk/make/libevent.mk
@@ -4,66 +4,69 @@
 #
 #############################################################
 
-LIBEVENT_DIR=$(BUILD_DIR)/libevent
-
-LIBEVENT_VERSION:=0.9
 LIBEVENT_SITE=http://www.monkey.org/~provos/
+LIBEVENT_VERSION=0.9
 LIBEVENT_SOURCE=libevent-$(LIBEVENT_VERSION).tar.gz
-LIBEVENT_CFLAGS= $(TARGET_CFLAGS) -fPIC
+LIBEVENT_DIR=libevent-$(LIBEVENT_VERSION)
+LIBEVENT_UNZIP=zcat
 
+LIBEVENT_IPK_VERSION=1
+
+LIBEVENT_CPPFLAGS= -fPIC
+
+LIBEVENT_BUILD_DIR=$(BUILD_DIR)/libevent
+LIBEVENT_SOURCE_DIR=$(SOURCE_DIR)/libevent
 LIBEVENT_IPK_DIR=$(BUILD_DIR)/libevent-$(LIBEVENT_VERSION)-ipk
-LIBEVENT_IPK=$(BUILD_DIR)/libevent_$(LIBEVENT_VERSION)-1_armeb.ipk
+LIBEVENT_IPK=$(BUILD_DIR)/libevent_$(LIBEVENT_VERSION)-$(LIBEVENT_IPK_VERSION)_armeb.ipk
 
 $(DL_DIR)/$(LIBEVENT_SOURCE):
 	$(WGET) -P $(DL_DIR) $(LIBEVENT_SITE)/$(LIBEVENT_SOURCE)
 
-$(LIBEVENT_DIR)/.source: $(DL_DIR)/$(LIBEVENT_SOURCE)
-	zcat $(DL_DIR)/$(LIBEVENT_SOURCE) | tar -C $(BUILD_DIR) -xvf -
-	mv $(BUILD_DIR)/libevent-$(LIBEVENT_VERSION) $(LIBEVENT_DIR)
-	touch $(LIBEVENT_DIR)/.source
+libevent-source: $(DL_DIR)/$(LIBEVENT_SOURCE)
 
-$(LIBEVENT_DIR)/.configured: $(LIBEVENT_DIR)/.source
-	(cd $(LIBEVENT_DIR); \
+$(LIBEVENT_BUILD_DIR)/.configured: $(DL_DIR)/$(LIBEVENT_SOURCE)
+	rm -rf $(BUILD_DIR)/$(LIBEVENT_DIR) $(LIBEVENT_BUILD_DIR)
+	$(LIBEVENT_UNZIP) $(DL_DIR)/$(LIBEVENT_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+	mv $(BUILD_DIR)/libevent-$(LIBEVENT_VERSION) $(LIBEVENT_BUILD_DIR)
+	(cd $(LIBEVENT_BUILD_DIR); \
+		$(TARGET_CONFIGURE_OPTS) \
+		CPPFLAGS="$(LIBEVENT_CPPFLAGS)" \
 		./configure \
-		--prefix=/opt \
-		--exec-prefix=$(STAGING_DIR)/usr/bin \
-		--libdir=$(STAGING_DIR)/lib \
-		--includedir=$(STAGING_DIR)/include \
+		--build=$(GNU_HOST_NAME) \
 		--host=$(GNU_TARGET_NAME) \
+		--target=$(GNU_TARGET_NAME) \
+		--prefix=/opt \
 	);
-	touch $(LIBEVENT_DIR)/.configured;
+	touch $(LIBEVENT_BUILD_DIR)/.configured
 
-$(LIBEVENT_DIR)/libevent.a: $(LIBEVENT_DIR)/.configured
-	$(MAKE) -C $(LIBEVENT_DIR) RANLIB="$(TARGET_RANLIB)" AR="$(TARGET_AR)"
+libevent-unpack: $(LIBEVENT_BUILD_DIR)/.configured
 
-$(STAGING_DIR)/lib/libevent.a: $(LIBEVENT_DIR)/libevent.a
-	cp -dpf $(LIBEVENT_DIR)/libevent.a $(STAGING_DIR)/lib/libevent.a
-	cp -dpf $(LIBEVENT_DIR)/event.h $(STAGING_DIR)/include/event.h
-	cp -dpf $(LIBEVENT_DIR)/event.3 $(STAGING_DIR)/man/man3/
+$(LIBEVENT_BUILD_DIR)/libevent.a: $(LIBEVENT_BUILD_DIR)/.configured
+	$(MAKE) -C $(LIBEVENT_BUILD_DIR)
 
-libevent-headers: $(STAGING_DIR)/lib/libevent.so.$(LIBEVENT_VERSION)
+libevent: $(LIBEVENT_BUILD_DIR)/libevent.a
 
-libevent: $(STAGING_DIR)/lib/libevent.so.$(LIBEVENT_VERSION)
+$(STAGING_DIR)/opt/lib/libevent.a: $(LIBEVENT_BUILD_DIR)/libevent.a
+	install -d $(STAGING_DIR)/opt/include
+	install -m 644 $(LIBEVENT_BUILD_DIR)/event.h $(STAGING_DIR)/opt/include/event.h
+	install -d $(STAGING_DIR)/opt/lib
+	install -m 644 $(LIBEVENT_BUILD_DIR)/libevent.a $(STAGING_DIR)/opt/lib/libevent.a
 
-$(LIBEVENT_IPK): $(STAGING_DIR)/lib/libevent.a
-	mkdir -p $(LIBEVENT_IPK_DIR)/CONTROL
-	cp $(SOURCE_DIR)/libevent.control $(LIBEVENT_IPK_DIR)/CONTROL/control
-	mkdir -p $(LIBEVENT_IPK_DIR)/opt/include
-	cp -dpf $(STAGING_DIR)/include/event.h $(LIBEVENT_IPK_DIR)/opt/include
-	mkdir -p $(LIBEVENT_IPK_DIR)/opt/lib
-	cp -dpf $(STAGING_DIR)/lib/libevent.a $(LIBEVENT_IPK_DIR)/opt/lib
-	mkdir -p $(LIBEVENT_IPK_DIR)/opt/man/man3
-	cp -dpf $(STAGING_DIR)/man/man3/event.3 $(LIBEVENT_IPK_DIR)/opt/man/man3
+libevent-stage: $(STAGING_DIR)/opt/lib/libevent.a
+
+$(LIBEVENT_IPK): $(LIBEVENT_BUILD_DIR)/libevent.a
+	install -d $(LIBEVENT_IPK_DIR)/opt/include
+	install -m 644 $(LIBEVENT_BUILD_DIR)/event.h $(LIBEVENT_IPK_DIR)/opt/include
+	install -d $(LIBEVENT_IPK_DIR)/opt/lib
+	install -m 644 $(LIBEVENT_BUILD_DIR)/libevent.a $(LIBEVENT_IPK_DIR)/opt/lib
+	install -d $(LIBEVENT_IPK_DIR)/CONTROL
+	install -m 644 $(LIBEVENT_SOURCE_DIR)/control $(LIBEVENT_IPK_DIR)/CONTROL/control
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(LIBEVENT_IPK_DIR)
 
 libevent-ipk: $(LIBEVENT_IPK)
 
-libevent-source: $(DL_DIR)/$(LIBEVENT_SOURCE)
-
 libevent-clean:
-	rm -f $(STAGING_DIR)/lib/libevent.*
-	rm -f $(STAGING_DIR)/include/event.h
-	-$(MAKE) -C $(LIBEVENT_DIR) clean
+	-$(MAKE) -C $(LIBEVENT_BUILD_DIR) clean
 
 libevent-dirclean:
-	rm -rf $(LIBEVENT_DIR) $(LIBEVENT_IPK_DIR) $(LIBEVENT_IPK)
+	rm -rf $(BUILD_DIR)/$(LIBEVENT_DIR) $(LIBEVENT_BUILD_DIR) $(LIBEVENT_IPK_DIR) $(LIBEVENT_IPK)
