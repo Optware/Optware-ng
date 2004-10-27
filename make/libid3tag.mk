8b3ffd44a2b040dbb8edebf4e610c308290bf7b1
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2004-10-27 12:28:26 +0000

    Updated to match template

--- make/libid3tag.mk
+++ make/libid3tag.mk
@@ -4,64 +4,68 @@
 #
 ###########################################################
 
-LIBID3TAG_DIR=$(BUILD_DIR)/libid3tag
-
-LIBID3TAG_VERSION=0.15.1b
-LIBID3TAG_SHLIBVERSION=0.3.0
-LIBID3TAG=libid3tag-$(LIBID3TAG_VERSION)
 LIBID3TAG_SITE=http://belnet.dl.sourceforge.net/sourceforge/mad
-LIBID3TAG_SOURCE=$(LIBID3TAG).tar.gz
+LIBID3TAG_VERSION=0.15.1b
+LIBID3TAG_LIB_VERSION=0.3.0
+LIBID3TAG_SOURCE=libid3tag-$(LIBID3TAG_VERSION).tar.gz
+LIBID3TAG_DIR=libid3tag-$(LIBID3TAG_VERSION)
 LIBID3TAG_UNZIP=zcat
 
-LIBID3TAG_IPK=$(BUILD_DIR)/libid3tag_$(LIBID3TAG_VERSION)-1_armeb.ipk
+LIBID3TAG_IPK_VERSION=1
+
+LIBID3TAG_BUILD_DIR=$(BUILD_DIR)/libid3tag
+LIBID3TAG_SOURCE_DIR=$(SOURCE_DIR)/libid3tag
 LIBID3TAG_IPK_DIR=$(BUILD_DIR)/libid3tag-$(LIBID3TAG_VERSION)-ipk
+LIBID3TAG_IPK=$(BUILD_DIR)/libid3tag_$(LIBID3TAG_VERSION)-$(LIBID3TAG_IPK_VERSION)_armeb.ipk
 
 $(DL_DIR)/$(LIBID3TAG_SOURCE):
 	$(WGET) -P $(DL_DIR) $(LIBID3TAG_SITE)/$(LIBID3TAG_SOURCE)
 
-$(LIBID3TAG_DIR)/.source: $(DL_DIR)/$(LIBID3TAG_SOURCE)
-	$(LIBID3TAG_UNZIP) $(DL_DIR)/$(LIBID3TAG_SOURCE) | tar -C $(BUILD_DIR) -xvf -
-	mv $(BUILD_DIR)/$(LIBID3TAG) $(LIBID3TAG_DIR)
-	touch $(LIBID3TAG_DIR)/.source
-
 libid3tag-source: $(DL_DIR)/$(LIBID3TAG_SOURCE)
 
-$(LIBID3TAG_DIR)/.configured: $(LIBID3TAG_DIR)/.source
-	(cd $(LIBID3TAG_DIR); \
-        export CC=$(TARGET_CC) ;\
-        export CPPFLAGS="$(STAGING_CPPFLAGS)" ;\
-        export LDFLAGS="$(STAGING_LDFLAGS)" ;\
+$(LIBID3TAG_BUILD_DIR)/.configured: $(DL_DIR)/$(LIBID3TAG_SOURCE)
+	$(MAKE) zlib-stage
+	rm -rf $(BUILD_DIR)/$(LIBID3TAG_DIR) $(LIBID3TAG_BUILD_DIR)
+	$(LIBID3TAG_UNZIP) $(DL_DIR)/$(LIBID3TAG_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+	mv $(BUILD_DIR)/$(LIBID3TAG_DIR) $(LIBID3TAG_BUILD_DIR)
+	(cd $(LIBID3TAG_BUILD_DIR); \
+		$(TARGET_CONFIGURE_OPTS) \
+		CPPFLAGS="$(STAGING_CPPFLAGS)" \
+		LDFLAGS="$(STAGING_LDFLAGS)" \
 		./configure \
-		--host=$(GNU_TARGET_NAME) \
 		--build=$(GNU_HOST_NAME) \
-		--prefix=$(STAGING_DIR) \
+		--host=$(GNU_TARGET_NAME) \
+		--target=$(GNU_TARGET_NAME) \
+		--prefix=/opt \
 	);
-	touch $(LIBID3TAG_DIR)/.configured
+	touch $(LIBID3TAG_BUILD_DIR)/.configured
 
-$(STAGING_DIR)/lib/libid3tag.so.$(LIBID3TAG_SHLIBVERSION): $(LIBID3TAG_DIR)/.configured
-	$(MAKE) -C $(LIBID3TAG_DIR) install
+libid3tag-unpack: $(LIBID3TAG_BUILD_DIR)/.configured
 
-libid3tag-headers: $(STAGING_DIR)/lib/libid3tag.a
+$(LIBID3TAG_BUILD_DIR)/.libs/libid3tag.a: $(LIBID3TAG_BUILD_DIR)/.configured
+	$(MAKE) -C $(LIBID3TAG_BUILD_DIR)
 
-libid3tag: zlib $(STAGING_DIR)/lib/libid3tag.so.$(LIBID3TAG_SHLIBVERSION)
+libid3tag: $(LIBID3TAG_BUILD_DIR)/.libs/libid3tag.a
 
-$(LIBID3TAG_IPK): $(STAGING_DIR)/lib/libid3tag.so.$(LIBID3TAG_SHLIBVERSION)
-	mkdir -p $(LIBID3TAG_IPK_DIR)/CONTROL
-	cp $(SOURCE_DIR)/libid3tag.control $(LIBID3TAG_IPK_DIR)/CONTROL/control
-	mkdir -p $(LIBID3TAG_IPK_DIR)/opt/include
-	cp -dpf $(STAGING_DIR)/include/id3tag.h $(LIBID3TAG_IPK_DIR)/opt/include
-	mkdir -p $(LIBID3TAG_IPK_DIR)/opt/lib
-	cp -dpf $(STAGING_DIR)/lib/libid3tag.so* $(LIBID3TAG_IPK_DIR)/opt/lib
-	-$(STRIP) --strip-unneeded $(LIBID3TAG_IPK_DIR)/opt/lib/libid3tag.so*
-	touch -c $(LIBID3TAG_IPK_DIR)/opt/lib/libid3tag.so.$(LIBID3TAG_SHLIBVERSION)
+$(STAGING_DIR)/opt/lib/libid3tag.a: $(LIBID3TAG_BUILD_DIR)/.libs/libid3tag.a
+	$(MAKE) -C $(LIBID3TAG_BUILD_DIR) DESTDIR=$(STAGING_DIR) install
+
+libid3tag-stage: $(STAGING_DIR)/opt/lib/libid3tag.a
+
+$(LIBID3TAG_IPK): $(LIBID3TAG_BUILD_DIR)/.libs/libid3tag.a
+	rm -rf $(LIBID3TAG_IPK_DIR) $(LIBID3TAG_IPK)
+	$(MAKE) -C $(LIBID3TAG_BUILD_DIR) DESTDIR=$(LIBID3TAG_IPK_DIR) install
+	$(STRIP) --strip-unneeded $(LIBID3TAG_IPK_DIR)/opt/lib/*.so.*
+	rm -f $(LIBID3TAG_IPK_DIR)/opt/lib/*.{la,a}
+	install -d $(LIBID3TAG_IPK_DIR)/CONTROL
+	install -m 644 $(LIBID3TAG_SOURCE_DIR)/control $(LIBID3TAG_IPK_DIR)/CONTROL/control
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(LIBID3TAG_IPK_DIR)
 
 libid3tag-ipk: $(LIBID3TAG_IPK)
 
 libid3tag-clean:
-	-$(MAKE) -C $(LIBID3TAG_DIR) uninstall
-	-$(MAKE) -C $(LIBID3TAG_DIR) clean
+	-$(MAKE) -C $(LIBID3TAG_BUILD_DIR) clean
 
 libid3tag-dirclean: libid3tag-clean
-	rm -rf $(LIBID3TAG_DIR) $(LIBID3TAG_IPK_DIR) $(LIBID3TAG_IPK)
+	rm -rf $(BUILD_DIR)/$(LIBID3TAG_DIR) $(LIBID3TAG_BUILD_DIR) $(LIBID3TAG_IPK_DIR) $(LIBID3TAG_IPK)
 
