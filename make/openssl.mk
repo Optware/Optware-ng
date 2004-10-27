6c106afcc67836af7795f059b01b65af561c86b3
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2004-10-27 10:59:41 +0000

    Added .../opt/... to STAGING_DIR references.

--- make/openssl.mk
+++ make/openssl.mk
@@ -4,16 +4,16 @@
 
 OPENSSL_SITE=http://www.openssl.org/source
 OPENSSL_VERSION=0.9.7d
-OPENSSL_LIB_VERSION:=0.9.7
-OPENSSL_SOURCE:=openssl-$(OPENSSL_VERSION).tar.gz
-OPENSSL_DIR:=openssl-$(OPENSSL_VERSION)
-OPENSSL_UNZIP:=zcat
+OPENSSL_LIB_VERSION=0.9.7
+OPENSSL_SOURCE=openssl-$(OPENSSL_VERSION).tar.gz
+OPENSSL_DIR=openssl-$(OPENSSL_VERSION)
+OPENSSL_UNZIP=zcat
 
 OPENSSL_IPK_VERSION=1
 
-OPENSSL_BUILD_DIR:=$(BUILD_DIR)/openssl
-OPENSSL_SOURCE_DIR:=$(SOURCE_DIR)/openssl
-OPENSSL_IPK_DIR:=$(BUILD_DIR)/openssl-$(OPENSSL_VERSION)-ipk
+OPENSSL_BUILD_DIR=$(BUILD_DIR)/openssl
+OPENSSL_SOURCE_DIR=$(SOURCE_DIR)/openssl
+OPENSSL_IPK_DIR=$(BUILD_DIR)/openssl-$(OPENSSL_VERSION)-ipk
 OPENSSL_IPK=$(BUILD_DIR)/openssl_$(OPENSSL_VERSION)-$(OPENSSL_IPK_VERSION)_armeb.ipk
 
 OPENSSL_PATCHES=$(OPENSSL_SOURCE_DIR)/Configure.patch
@@ -42,6 +42,7 @@ $(OPENSSL_BUILD_DIR)/.configured: $(DL_DIR)/$(OPENSSL_SOURCE) $(OPENSSL_PATCHES)
 openssl-unpack: $(OPENSSL_BUILD_DIR)/.configured
 
 $(OPENSSL_BUILD_DIR)/libssl.so.$(OPENSSL_LIB_VERSION): $(OPENSSL_BUILD_DIR)/.configured
+	$(MAKE) zlib-stage
 	$(MAKE) -C $(OPENSSL_BUILD_DIR) \
 		$(TARGET_CONFIGURE_OPTS) \
 		AR="${TARGET_AR} r" \
@@ -51,14 +52,19 @@ $(OPENSSL_BUILD_DIR)/libssl.so.$(OPENSSL_LIB_VERSION): $(OPENSSL_BUILD_DIR)/.con
 
 openssl: $(OPENSSL_BUILD_DIR)/libssl.so.$(OPENSSL_LIB_VERSION)
 
-$(STAGING_DIR)/lib/libssl.so.$(OPENSSL_LIB_VERSION): $(OPENSSL_BUILD_DIR)/libssl.so.$(OPENSSL_LIB_VERSION)
-	rm -rf $(STAGING_DIR)/include/openssl
-	install -d $(STAGING_DIR)/include/openssl
-	install -m 644 $(OPENSSL_BUILD_DIR)/include/openssl/*.h $(STAGING_DIR)/include/openssl
-	install -d $(STAGING_DIR)/lib
-	install -m 644 $(OPENSSL_BUILD_DIR)/lib{crypto,ssl}.* $(STAGING_DIR)/lib
+$(STAGING_DIR)/opt/lib/libssl.so.$(OPENSSL_LIB_VERSION): $(OPENSSL_BUILD_DIR)/libssl.so.$(OPENSSL_LIB_VERSION)
+	rm -rf $(STAGING_DIR)/opt/include/openssl
+	install -d $(STAGING_DIR)/opt/include/openssl
+	install -m 644 $(OPENSSL_BUILD_DIR)/include/openssl/*.h $(STAGING_DIR)/opt/include/openssl
+	install -d $(STAGING_DIR)/opt/lib
+	install -m 644 $(OPENSSL_BUILD_DIR)/lib{crypto,ssl}.a $(STAGING_DIR)/opt/lib
+	install -m 644 $(OPENSSL_BUILD_DIR)/lib{crypto,ssl}.so.$(OPENSSL_LIB_VERSION) $(STAGING_DIR)/opt/lib
+	cd $(STAGING_DIR)/opt/lib && ln -fs libcrypto.so.$(OPENSSL_LIB_VERSION) libcrypto.so.0
+	cd $(STAGING_DIR)/opt/lib && ln -fs libcrypto.so.$(OPENSSL_LIB_VERSION) libcrypto.so
+	cd $(STAGING_DIR)/opt/lib && ln -fs libssl.so.$(OPENSSL_LIB_VERSION) libssl.so.0
+	cd $(STAGING_DIR)/opt/lib && ln -fs libssl.so.$(OPENSSL_LIB_VERSION) libssl.so
 
-openssl-stage: $(STAGING_DIR)/lib/libssl.so.$(OPENSSL_LIB_VERSION)
+openssl-stage: $(STAGING_DIR)/opt/lib/libssl.so.$(OPENSSL_LIB_VERSION)
 
 $(OPENSSL_IPK): $(OPENSSL_BUILD_DIR)/libssl.so.$(OPENSSL_LIB_VERSION)
 	install -d $(OPENSSL_IPK_DIR)/opt/bin
@@ -69,8 +75,12 @@ $(OPENSSL_IPK): $(OPENSSL_BUILD_DIR)/libssl.so.$(OPENSSL_LIB_VERSION)
 	install -d $(OPENSSL_IPK_DIR)/opt/include/openssl
 	install -m 644 $(OPENSSL_BUILD_DIR)/include/openssl/*.h $(OPENSSL_IPK_DIR)/opt/include/openssl
 	install -d $(OPENSSL_IPK_DIR)/opt/lib
-	install -m 644 $(OPENSSL_BUILD_DIR)/lib{crypto,ssl}.* $(OPENSSL_IPK_DIR)/opt/lib
+	install -m 644 $(OPENSSL_BUILD_DIR)/lib{crypto,ssl}.so.$(OPENSSL_LIB_VERSION) $(OPENSSL_IPK_DIR)/opt/lib
 	$(STRIP) --strip-unneeded $(OPENSSL_IPK_DIR)/opt/lib/lib{ssl,crypto}.so*
+	cd $(OPENSSL_IPK_DIR)/opt/lib && ln -fs libcrypto.so.$(OPENSSL_LIB_VERSION) libcrypto.so.0
+	cd $(OPENSSL_IPK_DIR)/opt/lib && ln -fs libcrypto.so.$(OPENSSL_LIB_VERSION) libcrypto.so
+	cd $(OPENSSL_IPK_DIR)/opt/lib && ln -fs libssl.so.$(OPENSSL_LIB_VERSION) libssl.so.0
+	cd $(OPENSSL_IPK_DIR)/opt/lib && ln -fs libssl.so.$(OPENSSL_LIB_VERSION) libssl.so
 	install -d $(OPENSSL_IPK_DIR)/CONTROL
 	install -m 644 $(OPENSSL_SOURCE_DIR)/control $(OPENSSL_IPK_DIR)/CONTROL/control
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(OPENSSL_IPK_DIR)
