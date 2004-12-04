58173bc87d68036448afdb3ff5f12dd8e8fe845b
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2004-12-04 10:44:38 +0000

    Removed my email address.

--- trunk/make/libdb.mk
+++ trunk/make/libdb.mk
@@ -5,7 +5,7 @@
 ###########################################################
 
 # Copyright (C) 2004 by Tom King <ka6sox@gmail.com>
-# Copyright (C) 2004 by Rod Whitby <unslung@gmail.com>
+# Copyright (C) 2004 by Rod Whitby
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
