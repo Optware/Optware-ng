76347189ca7b4e3601651396f3caeeb8cc1f08ce
edmondsc <edmondsc@1f1dca69-5516-0410-b834-e485effd04c3>
2004-12-08 07:50:23 +0000

    netatalk check in, ka6sox please finish

---
+++ make/netatalk.mk
@@ -0,0 +1,179 @@
+###########################################################
+#
+# netatalk
+#
+###########################################################
+
+# You must replace "netatalk" and "NETATALK" with the lower case name and
+# upper case name of your new package.  Some places below will say
+# "Do not change this" - that does not include this global change,
+# which must always be done to ensure we have unique names.
+
+#
+# NETATALK_VERSION, NETATALK_SITE and NETATALK_SOURCE define
+# the upstream location of the source code for the package.
+# NETATALK_DIR is the directory which is created when the source
+# archive is unpacked.
+# NETATALK_UNZIP is the command used to unzip the source.
+# It is usually "zcat" (for .gz) or "bzcat" (for .bz2)
+#
+# You should change all these variables to suit your package.
+#
+NETATALK_SITE=http://unc.dl.sourceforge.net/sourceforge/netatalk
+NETATALK_VERSION=2.0.1
+NETATALK_SOURCE=netatalk-$(NETATALK_VERSION).tar.gz
+NETATALK_DIR=netatalk-$(NETATALK_VERSION)
+NETATALK_UNZIP=zcat
+
+#
+# NETATALK_IPK_VERSION should be incremented when the ipk changes.
+#
+NETATALK_IPK_VERSION=1
+
+#
+# NETATALK_PATCHES should list any patches, in the the order in
+# which they should be applied to the source code.
+#
+NETATALK_PATCHES=
+
+#
+# If the compilation of the package requires additional
+# compilation or linking flags, then list them here.
+#
+NETATALK_CPPFLAGS=
+NETATALK_LDFLAGS=
+
+#
+# NETATALK_BUILD_DIR is the directory in which the build is done.
+# NETATALK_SOURCE_DIR is the directory which holds all the
+# patches and ipkg control files.
+# NETATALK_IPK_DIR is the directory in which the ipk is built.
+# NETATALK_IPK is the name of the resulting ipk files.
+#
+# You should not change any of these variables.
+#
+NETATALK_BUILD_DIR=$(BUILD_DIR)/netatalk
+NETATALK_SOURCE_DIR=$(SOURCE_DIR)/netatalk
+NETATALK_IPK_DIR=$(BUILD_DIR)/netatalk-$(NETATALK_VERSION)-ipk
+NETATALK_IPK=$(BUILD_DIR)/netatalk_$(NETATALK_VERSION)-$(NETATALK_IPK_VERSION)_armeb.ipk
+
+#
+# This is the dependency on the source code.  If the source is missing,
+# then it will be fetched from the site using wget.
+#
+$(DL_DIR)/$(NETATALK_SOURCE):
+	$(WGET) -P $(DL_DIR) $(NETATALK_SITE)/$(NETATALK_SOURCE)
+
+#
+# The source code depends on it existing within the download directory.
+# This target will be called by the top level Makefile to download the
+# source code's archive (.tar.gz, .bz2, etc.)
+#
+netatalk-source: $(DL_DIR)/$(NETATALK_SOURCE) $(NETATALK_PATCHES)
+
+#
+# This target unpacks the source code in the build directory.
+# If the source archive is not .tar.gz or .tar.bz2, then you will need
+# to change the commands here.  Patches to the source code are also
+# applied in this target as required.
+#
+# This target also configures the build within the build directory.
+# Flags such as LDFLAGS and CPPFLAGS should be passed into configure
+# and NOT $(MAKE) below.  Passing it to configure causes configure to
+# correctly BUILD the Makefile with the right paths, where passing it
+# to Make causes it to override the default search paths of the compiler.
+#
+# If the compilation of the package requires other packages to be staged
+# first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
+#
+$(NETATALK_BUILD_DIR)/.configured: $(DL_DIR)/$(NETATALK_SOURCE) $(NETATALK_PATCHES)
+	$(MAKE) libdb-stage
+	rm -rf $(BUILD_DIR)/$(NETATALK_DIR) $(NETATALK_BUILD_DIR)
+	$(NETATALK_UNZIP) $(DL_DIR)/$(NETATALK_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+	mv $(BUILD_DIR)/$(NETATALK_DIR) $(NETATALK_BUILD_DIR)
+	(cd $(NETATALK_BUILD_DIR); \
+		$(TARGET_CONFIGURE_OPTS) \
+		CPPFLAGS="$(STAGING_CPPFLAGS) $(NETATALK_CPPFLAGS)" \
+		LDFLAGS="$(STAGING_LDFLAGS) $(NETATALK_LDFLAGS)" \
+		./configure \
+		--build=$(GNU_HOST_NAME) \
+		--host=$(GNU_TARGET_NAME) \
+		--target=$(GNU_TARGET_NAME) \
+		--prefix=$(NETATALK_BUILD_DIR)/opt \
+		--with-bdb=$(STAGING_DIR)/opt/include \
+	)
+	touch $(NETATALK_BUILD_DIR)/.configured
+
+netatalk-unpack: $(NETATALK_BUILD_DIR)/.configured
+
+#
+# This builds the actual binary.  You should change the target to refer
+# directly to the main binary which is built.
+#
+$(NETATALK_BUILD_DIR)/opt/sbin/atalkd: $(NETATALK_BUILD_DIR)/.configured
+	$(MAKE) install-strip -C $(NETATALK_BUILD_DIR)
+	install -m 755 $(NETATALK_SOURCE_DIR)/rename.sh $(NETATALK_BUILD_DIR)/rename.sh
+	cd $(NETATALK_BUILD_DIR) && ./rename.sh
+
+
+#
+# You should change the dependency to refer directly to the main binary
+# which is built.
+#
+netatalk: $(NETATALK_BUILD_DIR)/opt/sbin/atalkd
+
+#
+# If you are building a library, then you need to stage it too.
+#
+$(STAGING_DIR)/opt/lib/libnetatalk.so.$(NETATALK_VERSION): $(NETATALK_BUILD_DIR)/libnetatalk.so.$(NETATALK_VERSION)
+	install -d $(STAGING_DIR)/opt/include
+	install -m 644 $(NETATALK_BUILD_DIR)/netatalk.h $(STAGING_DIR)/opt/include
+	install -d $(STAGING_DIR)/opt/lib
+	install -m 644 $(NETATALK_BUILD_DIR)/libnetatalk.a $(STAGING_DIR)/opt/lib
+	install -m 644 $(NETATALK_BUILD_DIR)/libnetatalk.so.$(NETATALK_VERSION) $(STAGING_DIR)/opt/lib
+	cd $(STAGING_DIR)/opt/lib && ln -fs libnetatalk.so.$(NETATALK_VERSION) libnetatalk.so.1
+	cd $(STAGING_DIR)/opt/lib && ln -fs libnetatalk.so.$(NETATALK_VERSION) libnetatalk.so
+
+netatalk-stage: $(STAGING_DIR)/opt/lib/libnetatalk.so.$(NETATALK_VERSION)
+
+#
+# This builds the IPK file.
+#
+# Binaries should be installed into $(NETATALK_IPK_DIR)/opt/sbin or $(NETATALK_IPK_DIR)/opt/bin
+# (use the location in a well-known Linux distro as a guide for choosing sbin or bin).
+# Libraries and include files should be installed into $(NETATALK_IPK_DIR)/opt/{lib,include}
+# Configuration files should be installed in $(NETATALK_IPK_DIR)/opt/etc/netatalk/...
+# Documentation files should be installed in $(NETATALK_IPK_DIR)/opt/doc/netatalk/...
+# Daemon startup scripts should be installed in $(NETATALK_IPK_DIR)/opt/etc/init.d/S??netatalk
+#
+# You may need to patch your application to make it use these locations.
+#
+$(NETATALK_IPK): $(NETATALK_BUILD_DIR)/opt/sbin/atalkd
+	rm -rf $(NETATALK_IPK_DIR) $(NETATALK_IPK)
+	cp -rf $(NETATALK_BUILD_DIR)/opt $(NETATALK_IPK_DIR)/
+	rm -f $(NETATALK_IPK_DIR)/opt/lib/*.a
+	install -d $(NETATALK_IPK_DIR)/opt/etc/init.d
+	#install -m 755 $(NETATALK_SOURCE_DIR)/rc.netatalk $(NETATALK_IPK_DIR)/opt/etc/init.d/SXXnetatalk
+	install -d $(NETATALK_IPK_DIR)/CONTROL
+	install -m 644 $(NETATALK_SOURCE_DIR)/control $(NETATALK_IPK_DIR)/CONTROL/control
+	#install -m 644 $(NETATALK_SOURCE_DIR)/postinst $(NETATALK_IPK_DIR)/CONTROL/postinst
+	#install -m 644 $(NETATALK_SOURCE_DIR)/prerm $(NETATALK_IPK_DIR)/CONTROL/prerm
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(NETATALK_IPK_DIR)
+
+#
+# This is called from the top level makefile to create the IPK file.
+#
+netatalk-ipk: $(NETATALK_IPK)
+
+#
+# This is called from the top level makefile to clean all of the built files.
+#
+netatalk-clean:
+	-$(MAKE) -C $(NETATALK_BUILD_DIR) clean
+
+#
+# This is called from the top level makefile to clean all dynamically created
+# directories.
+#
+netatalk-dirclean:
+	rm -rf $(BUILD_DIR)/$(NETATALK_DIR) $(NETATALK_BUILD_DIR) $(NETATALK_IPK_DIR) $(NETATALK_IPK)
