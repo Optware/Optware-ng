ee3fafa791433196058467daa38bcb61ee8e9f3b
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2004-12-08 13:52:03 +0000

    Changed some variables to match the template, so we could reuse the CVS variable for grabbing source from CVS.

--- make/cvs.mk
+++ make/cvs.mk
@@ -4,12 +4,12 @@
 #
 ###########################################################
 
-CVS_DIR=$(BUILD_DIR)/cvs
+CVS_BUILD_DIR=$(BUILD_DIR)/cvs
 
 CVS_VERSION=1.12.9
-CVS=cvs-$(CVS_VERSION)
+CVS_DIR=cvs-$(CVS_VERSION)
 CVS_SITE=https://ccvs.cvshome.org/files/documents/19/201
-CVS_SOURCE=$(CVS).tar.gz
+CVS_SOURCE=$(CVS_DIR).tar.gz
 CVS_UNZIP=zcat
 
 CVS_IPK=$(BUILD_DIR)/cvs_$(CVS_VERSION)-1_armeb.ipk
@@ -20,13 +20,13 @@ $(DL_DIR)/$(CVS_SOURCE):
 
 cvs-source: $(DL_DIR)/$(CVS_SOURCE)
 
-$(CVS_DIR)/.source: $(DL_DIR)/$(CVS_SOURCE)
+$(CVS_BUILD_DIR)/.source: $(DL_DIR)/$(CVS_SOURCE)
 	$(CVS_UNZIP) $(DL_DIR)/$(CVS_SOURCE) | tar -C $(BUILD_DIR) -xvf -
-	mv $(BUILD_DIR)/cvs-$(CVS_VERSION) $(CVS_DIR)
-	touch $(CVS_DIR)/.source
+	mv $(BUILD_DIR)/cvs-$(CVS_VERSION) $(CVS_BUILD_DIR)
+	touch $(CVS_BUILD_DIR)/.source
 
-$(CVS_DIR)/.configured: $(CVS_DIR)/.source
-	(cd $(CVS_DIR); \
+$(CVS_BUILD_DIR)/.configured: $(CVS_BUILD_DIR)/.source
+	(cd $(CVS_BUILD_DIR); \
 		./configure \
 		--host=$(GNU_TARGET_NAME) \
 		--build=$(GNU_HOST_NAME) \
@@ -35,31 +35,31 @@ $(CVS_DIR)/.configured: $(CVS_DIR)/.source
 		cvs_cv_func_printf=yes \
 		cvs_cv_func_printf_ptr=yes \
 	);
-	touch $(CVS_DIR)/.configured
+	touch $(CVS_BUILD_DIR)/.configured
 
-$(CVS_DIR)/src/cvs: $(CVS_DIR)/.configured
-	$(MAKE) -C $(CVS_DIR) \
+$(CVS_BUILD_DIR)/src/cvs: $(CVS_BUILD_DIR)/.configured
+	$(MAKE) -C $(CVS_BUILD_DIR) \
 	CC=$(TARGET_CC) AR=$(TARGET_AR) RANLIB=$(TARGET_RANLIB)
 
-cvs: $(CVS_DIR)/src/cvs
+cvs: $(CVS_BUILD_DIR)/src/cvs
 
-$(CVS_IPK): $(CVS_DIR)/src/cvs
+$(CVS_IPK): $(CVS_BUILD_DIR)/src/cvs
 	mkdir -p $(CVS_IPK_DIR)/CONTROL
 	mkdir -p $(CVS_IPK_DIR)/opt
 	mkdir -p $(CVS_IPK_DIR)/opt/bin
-	$(STRIP) $(CVS_DIR)/src/cvs -o $(CVS_IPK_DIR)/opt/bin/cvs
+	$(STRIP) $(CVS_BUILD_DIR)/src/cvs -o $(CVS_IPK_DIR)/opt/bin/cvs
 	cp $(SOURCE_DIR)/cvs.control $(CVS_IPK_DIR)/CONTROL/control
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(CVS_IPK_DIR)
 
 cvs-ipk: $(CVS_IPK)
 
 cvs-clean:
-	-$(MAKE) -C $(CVS_DIR) uninstall
-	-$(MAKE) -C $(CVS_DIR) clean
+	-$(MAKE) -C $(CVS_BUILD_DIR) uninstall
+	-$(MAKE) -C $(CVS_BUILD_DIR) clean
 
 cvs-distclean:
-	-rm $(CVS_DIR)/.configured
-	-$(MAKE) -C $(CVS_DIR) distclean
+	-rm $(CVS_BUILD_DIR)/.configured
+	-$(MAKE) -C $(CVS_BUILD_DIR) distclean
 
 cvs-dirclean:
-	rm -rf $(CVS_DIR) $(CVS_IPK_DIR) $(CVS_IPK)
+	rm -rf $(CVS_BUILD_DIR) $(CVS_IPK_DIR) $(CVS_IPK)
