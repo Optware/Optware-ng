560ea5699f8a8eaeddc23288bd10594c42a4435e
jieglen <jieglen@1f1dca69-5516-0410-b834-e485effd04c3>
2004-12-14 03:22:29 +0000

    initial addition

---
+++ make/m4.mk
@@ -0,0 +1,176 @@
+###########################################################
+#
+# m4
+#
+###########################################################
+
+# You must replace "m4" and "M4" with the lower case name and
+# upper case name of your new package.  Some places below will say
+# "Do not change this" - that does not include this global change,
+# which must always be done to ensure we have unique names.
+
+#
+# M4_VERSION, M4_SITE and M4_SOURCE define
+# the upstream location of the source code for the package.
+# M4_DIR is the directory which is created when the source
+# archive is unpacked.
+# M4_UNZIP is the command used to unzip the source.
+# It is usually "zcat" (for .gz) or "bzcat" (for .bz2)
+#
+# You should change all these variables to suit your package.
+#
+M4_SITE=http://ftp.gnu.org/pub/gnu/m4/
+M4_VERSION=1.4.1
+M4_SOURCE=m4-$(M4_VERSION).tar.gz
+M4_DIR=m4-$(M4_VERSION)
+M4_UNZIP=zcat
+
+#
+# M4_IPK_VERSION should be incremented when the ipk changes.
+#
+M4_IPK_VERSION=1
+
+#
+# M4_PATCHES should list any patches, in the the order in
+# which they should be applied to the source code.
+#
+#M4_PATCHES=$(M4_SOURCE_DIR)/configure.patch
+
+#
+# If the compilation of the package requires additional
+# compilation or linking flags, then list them here.
+#
+M4_CPPFLAGS=
+M4_LDFLAGS=
+
+#
+# M4_BUILD_DIR is the directory in which the build is done.
+# M4_SOURCE_DIR is the directory which holds all the
+# patches and ipkg control files.
+# M4_IPK_DIR is the directory in which the ipk is built.
+# M4_IPK is the name of the resulting ipk files.
+#
+# You should not change any of these variables.
+#
+M4_BUILD_DIR=$(BUILD_DIR)/m4
+M4_SOURCE_DIR=$(SOURCE_DIR)/m4
+M4_IPK_DIR=$(BUILD_DIR)/m4-$(M4_VERSION)-ipk
+M4_IPK=$(BUILD_DIR)/m4_$(M4_VERSION)-$(M4_IPK_VERSION)_armeb.ipk
+
+#
+# This is the dependency on the source code.  If the source is missing,
+# then it will be fetched from the site using wget.
+#
+$(DL_DIR)/$(M4_SOURCE):
+	$(WGET) -P $(DL_DIR) $(M4_SITE)/$(M4_SOURCE)
+
+#
+# The source code depends on it existing within the download directory.
+# This target will be called by the top level Makefile to download the
+# source code's archive (.tar.gz, .bz2, etc.)
+#
+m4-source: $(DL_DIR)/$(M4_SOURCE) $(M4_PATCHES)
+
+#
+# This target unpacks the source code in the build directory.
+# If the source archive is not .tar.gz or .tar.bz2, then you will need
+# to change the commands here.  Patches to the source code are also
+# applied in this target as required.
+#
+# This target also configures the build within the build directory.
+# Flags such as LDFLAGS and CPPFLAGS should be passed into configure
+# and NOT $(MAKE) below.  Passing it to configure causes configure to
+# correctly BUILD the Makefile with the right paths, where passing it
+# to Make causes it to override the default search paths of the compiler.
+#
+# If the compilation of the package requires other packages to be staged
+# first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
+#
+$(M4_BUILD_DIR)/.configured: $(DL_DIR)/$(M4_SOURCE) $(M4_PATCHES)
+#	$(MAKE) <bar>-stage <baz>-stage
+	rm -rf $(BUILD_DIR)/$(M4_DIR) $(M4_BUILD_DIR)
+	$(M4_UNZIP) $(DL_DIR)/$(M4_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+#	cat $(M4_PATCHES) | patch -d $(BUILD_DIR)/$(M4_DIR) -p1
+	mv $(BUILD_DIR)/$(M4_DIR) $(M4_BUILD_DIR)
+	(cd $(M4_BUILD_DIR); \
+		$(TARGET_CONFIGURE_OPTS) \
+		CPPFLAGS="$(STAGING_CPPFLAGS) $(M4_CPPFLAGS)" \
+		LDFLAGS="$(STAGING_LDFLAGS) $(M4_LDFLAGS)" \
+		./configure \
+		--build=$(GNU_HOST_NAME) \
+		--host=$(GNU_TARGET_NAME) \
+		--target=$(GNU_TARGET_NAME) \
+		--prefix=/opt \
+	)
+	touch $(M4_BUILD_DIR)/.configured
+
+m4-unpack: $(M4_BUILD_DIR)/.configured
+
+#
+# This builds the actual binary.  You should change the target to refer
+# directly to the main binary which is built.
+#
+$(M4_BUILD_DIR)/m4: $(M4_BUILD_DIR)/.configured
+	$(MAKE) -C $(M4_BUILD_DIR)
+
+#
+# You should change the dependency to refer directly to the main binary
+# which is built.
+#
+m4: $(M4_BUILD_DIR)/m4
+
+#
+# If you are building a library, then you need to stage it too.
+#
+$(STAGING_DIR)/opt/lib/libm4.so.$(M4_VERSION): $(M4_BUILD_DIR)/libm4.so.$(M4_VERSION)
+	install -d $(STAGING_DIR)/opt/include
+	install -m 644 $(M4_BUILD_DIR)/m4.h $(STAGING_DIR)/opt/include
+	install -d $(STAGING_DIR)/opt/lib
+	install -m 644 $(M4_BUILD_DIR)/libm4.a $(STAGING_DIR)/opt/lib
+	install -m 644 $(M4_BUILD_DIR)/libm4.so.$(M4_VERSION) $(STAGING_DIR)/opt/lib
+	cd $(STAGING_DIR)/opt/lib && ln -fs libm4.so.$(M4_VERSION) libm4.so.1
+	cd $(STAGING_DIR)/opt/lib && ln -fs libm4.so.$(M4_VERSION) libm4.so
+
+m4-stage: $(STAGING_DIR)/opt/lib/libm4.so.$(M4_VERSION)
+
+#
+# This builds the IPK file.
+#
+# Binaries should be installed into $(M4_IPK_DIR)/opt/sbin or $(M4_IPK_DIR)/opt/bin
+# (use the location in a well-known Linux distro as a guide for choosing sbin or bin).
+# Libraries and include files should be installed into $(M4_IPK_DIR)/opt/{lib,include}
+# Configuration files should be installed in $(M4_IPK_DIR)/opt/etc/m4/...
+# Documentation files should be installed in $(M4_IPK_DIR)/opt/doc/m4/...
+# Daemon startup scripts should be installed in $(M4_IPK_DIR)/opt/etc/init.d/S??m4
+#
+# You may need to patch your application to make it use these locations.
+#
+$(M4_IPK): $(M4_BUILD_DIR)/m4
+	rm -rf $(M4_IPK_DIR) $(M4_IPK)
+	install -d $(M4_IPK_DIR)/opt/bin
+	$(STRIP) $(M4_BUILD_DIR)/m4 -o $(M4_IPK_DIR)/opt/bin/m4
+#	install -d $(M4_IPK_DIR)/opt/etc/init.d
+#	install -m 755 $(M4_SOURCE_DIR)/rc.m4 $(M4_IPK_DIR)/opt/etc/init.d/SXXm4
+	install -d $(M4_IPK_DIR)/CONTROL
+	install -m 644 $(M4_SOURCE_DIR)/control $(M4_IPK_DIR)/CONTROL/control
+#	install -m 644 $(M4_SOURCE_DIR)/postinst $(M4_IPK_DIR)/CONTROL/postinst
+#	install -m 644 $(M4_SOURCE_DIR)/prerm $(M4_IPK_DIR)/CONTROL/prerm
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(M4_IPK_DIR)
+
+#
+# This is called from the top level makefile to create the IPK file.
+#
+m4-ipk: $(M4_IPK)
+
+#
+# This is called from the top level makefile to clean all of the built files.
+#
+m4-clean:
+	-$(MAKE) -C $(M4_BUILD_DIR) clean
+
+#
+# This is called from the top level makefile to clean all dynamically created
+# directories.
+#
+m4-dirclean:
+	rm -rf $(BUILD_DIR)/$(M4_DIR) $(M4_BUILD_DIR) $(M4_IPK_DIR) $(M4_IPK)
---
+++ make/patch.mk
@@ -0,0 +1,176 @@
+###########################################################
+#
+# patch
+#
+###########################################################
+
+# You must replace "patch" and "PATCH" with the lower case name and
+# upper case name of your new package.  Some places below will say
+# "Do not change this" - that does not include this global change,
+# which must always be done to ensure we have unique names.
+
+#
+# PATCH_VERSION, PATCH_SITE and PATCH_SOURCE define
+# the upstream location of the source code for the package.
+# PATCH_DIR is the directory which is created when the source
+# archive is unpacked.
+# PATCH_UNZIP is the command used to unzip the source.
+# It is usually "zcat" (for .gz) or "bzcat" (for .bz2)
+#
+# You should change all these variables to suit your package.
+#
+PATCH_SITE=ftp://ftp.gnu.org/gnu/patch
+PATCH_VERSION=2.5.4
+PATCH_SOURCE=patch-$(PATCH_VERSION).tar.gz
+PATCH_DIR=patch-$(PATCH_VERSION)
+PATCH_UNZIP=zcat
+
+#
+# PATCH_IPK_VERSION should be incremented when the ipk changes.
+#
+PATCH_IPK_VERSION=1
+
+#
+# PATCH_PATCHES should list any patches, in the the order in
+# which they should be applied to the source code.
+#
+#PATCH_PATCHES=$(PATCH_SOURCE_DIR)/configure.patch
+
+#
+# If the compilation of the package requires additional
+# compilation or linking flags, then list them here.
+#
+PATCH_CPPFLAGS=
+PATCH_LDFLAGS=
+
+#
+# PATCH_BUILD_DIR is the directory in which the build is done.
+# PATCH_SOURCE_DIR is the directory which holds all the
+# patches and ipkg control files.
+# PATCH_IPK_DIR is the directory in which the ipk is built.
+# PATCH_IPK is the name of the resulting ipk files.
+#
+# You should not change any of these variables.
+#
+PATCH_BUILD_DIR=$(BUILD_DIR)/patch
+PATCH_SOURCE_DIR=$(SOURCE_DIR)/patch
+PATCH_IPK_DIR=$(BUILD_DIR)/patch-$(PATCH_VERSION)-ipk
+PATCH_IPK=$(BUILD_DIR)/patch_$(PATCH_VERSION)-$(PATCH_IPK_VERSION)_armeb.ipk
+
+#
+# This is the dependency on the source code.  If the source is missing,
+# then it will be fetched from the site using wget.
+#
+$(DL_DIR)/$(PATCH_SOURCE):
+	$(WGET) -P $(DL_DIR) $(PATCH_SITE)/$(PATCH_SOURCE)
+
+#
+# The source code depends on it existing within the download directory.
+# This target will be called by the top level Makefile to download the
+# source code's archive (.tar.gz, .bz2, etc.)
+#
+patch-source: $(DL_DIR)/$(PATCH_SOURCE) $(PATCH_PATCHES)
+
+#
+# This target unpacks the source code in the build directory.
+# If the source archive is not .tar.gz or .tar.bz2, then you will need
+# to change the commands here.  Patches to the source code are also
+# applied in this target as required.
+#
+# This target also configures the build within the build directory.
+# Flags such as LDFLAGS and CPPFLAGS should be passed into configure
+# and NOT $(MAKE) below.  Passing it to configure causes configure to
+# correctly BUILD the Makefile with the right paths, where passing it
+# to Make causes it to override the default search paths of the compiler.
+#
+# If the compilation of the package requires other packages to be staged
+# first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
+#
+$(PATCH_BUILD_DIR)/.configured: $(DL_DIR)/$(PATCH_SOURCE) $(PATCH_PATCHES)
+#	$(MAKE) <bar>-stage <baz>-stage
+	rm -rf $(BUILD_DIR)/$(PATCH_DIR) $(PATCH_BUILD_DIR)
+	$(PATCH_UNZIP) $(DL_DIR)/$(PATCH_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+#	cat $(PATCH_PATCHES) | patch -d $(BUILD_DIR)/$(PATCH_DIR) -p1
+	mv $(BUILD_DIR)/$(PATCH_DIR) $(PATCH_BUILD_DIR)
+	(cd $(PATCH_BUILD_DIR); \
+		$(TARGET_CONFIGURE_OPTS) \
+		CPPFLAGS="$(STAGING_CPPFLAGS) $(PATCH_CPPFLAGS)" \
+		LDFLAGS="$(STAGING_LDFLAGS) $(PATCH_LDFLAGS)" \
+		./configure \
+		--build=$(GNU_HOST_NAME) \
+		--host=$(GNU_TARGET_NAME) \
+		--target=$(GNU_TARGET_NAME) \
+		--prefix=/opt \
+	)
+	touch $(PATCH_BUILD_DIR)/.configured
+
+patch-unpack: $(PATCH_BUILD_DIR)/.configured
+
+#
+# This builds the actual binary.  You should change the target to refer
+# directly to the main binary which is built.
+#
+$(PATCH_BUILD_DIR)/patch: $(PATCH_BUILD_DIR)/.configured
+	$(MAKE) -C $(PATCH_BUILD_DIR)
+
+#
+# You should change the dependency to refer directly to the main binary
+# which is built.
+#
+patch: $(PATCH_BUILD_DIR)/patch
+
+#
+# If you are building a library, then you need to stage it too.
+#
+$(STAGING_DIR)/opt/lib/libpatch.so.$(PATCH_VERSION): $(PATCH_BUILD_DIR)/libpatch.so.$(PATCH_VERSION)
+#	install -d $(STAGING_DIR)/opt/include
+#	install -m 644 $(PATCH_BUILD_DIR)/patch.h $(STAGING_DIR)/opt/include
+#	install -d $(STAGING_DIR)/opt/lib
+#	install -m 644 $(PATCH_BUILD_DIR)/libpatch.a $(STAGING_DIR)/opt/lib
+#	install -m 644 $(PATCH_BUILD_DIR)/libpatch.so.$(PATCH_VERSION) $(STAGING_DIR)/opt/lib
+#	cd $(STAGING_DIR)/opt/lib && ln -fs libpatch.so.$(PATCH_VERSION) libpatch.so.1
+#	cd $(STAGING_DIR)/opt/lib && ln -fs libpatch.so.$(PATCH_VERSION) libpatch.so
+
+patch-stage: $(STAGING_DIR)/opt/lib/libpatch.so.$(PATCH_VERSION)
+
+#
+# This builds the IPK file.
+#
+# Binaries should be installed into $(PATCH_IPK_DIR)/opt/sbin or $(PATCH_IPK_DIR)/opt/bin
+# (use the location in a well-known Linux distro as a guide for choosing sbin or bin).
+# Libraries and include files should be installed into $(PATCH_IPK_DIR)/opt/{lib,include}
+# Configuration files should be installed in $(PATCH_IPK_DIR)/opt/etc/patch/...
+# Documentation files should be installed in $(PATCH_IPK_DIR)/opt/doc/patch/...
+# Daemon startup scripts should be installed in $(PATCH_IPK_DIR)/opt/etc/init.d/S??patch
+#
+# You may need to patch your application to make it use these locations.
+#
+$(PATCH_IPK): $(PATCH_BUILD_DIR)/patch
+	rm -rf $(PATCH_IPK_DIR) $(PATCH_IPK)
+	install -d $(PATCH_IPK_DIR)/opt/bin
+	$(STRIP) $(PATCH_BUILD_DIR)/patch -o $(PATCH_IPK_DIR)/opt/bin/patch
+#	install -d $(PATCH_IPK_DIR)/opt/etc/init.d
+#	install -m 755 $(PATCH_SOURCE_DIR)/rc.patch $(PATCH_IPK_DIR)/opt/etc/init.d/SXXpatch
+	install -d $(PATCH_IPK_DIR)/CONTROL
+	install -m 644 $(PATCH_SOURCE_DIR)/control $(PATCH_IPK_DIR)/CONTROL/control
+#	install -m 644 $(PATCH_SOURCE_DIR)/postinst $(PATCH_IPK_DIR)/CONTROL/postinst
+#	install -m 644 $(PATCH_SOURCE_DIR)/prerm $(PATCH_IPK_DIR)/CONTROL/prerm
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(PATCH_IPK_DIR)
+
+#
+# This is called from the top level makefile to create the IPK file.
+#
+patch-ipk: $(PATCH_IPK)
+
+#
+# This is called from the top level makefile to clean all of the built files.
+#
+patch-clean:
+	-$(MAKE) -C $(PATCH_BUILD_DIR) clean
+
+#
+# This is called from the top level makefile to clean all dynamically created
+# directories.
+#
+patch-dirclean:
+	rm -rf $(BUILD_DIR)/$(PATCH_DIR) $(PATCH_BUILD_DIR) $(PATCH_IPK_DIR) $(PATCH_IPK)
