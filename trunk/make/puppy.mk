8c0a002b114f89750b7c0d3b1f315ef1efb9f947
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2004-12-23 21:43:56 +0000

    Updated to Puppy 1.3

--- trunk/make/puppy.mk
+++ trunk/make/puppy.mk
@@ -6,9 +6,9 @@
 
 # PUPPY_REPOSITORY=:ext:$(LOGNAME)@cvs.sf.net:/cvsroot/puppy
 PUPPY_REPOSITORY=:pserver:anonymous@cvs.sf.net:/cvsroot/puppy
-PUPPY_VERSION=1.2
+PUPPY_VERSION=1.3
 PUPPY_SOURCE=puppy-$(PUPPY_VERSION).tar.gz
-PUPPY_TAG=-r PUPPY_1_2
+PUPPY_TAG=-r PUPPY_1_3
 PUPPY_MODULE=puppy
 PUPPY_DIR=puppy-$(PUPPY_VERSION)
 PUPPY_UNZIP=zcat
