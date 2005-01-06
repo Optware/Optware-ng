0359803adab4fe7f6d5a8048a3cdb59f65585af5
dyoung8888 <dyoung8888@1f1dca69-5516-0410-b834-e485effd04c3>
2005-01-06 10:01:58 +0000

    Added file

---
+++ trunk/make/file.mk
@@ -0,0 +1,179 @@
+###########################################################
+#
+# file
+#
+###########################################################
+
+# You must replace "file" and "FILE" with the lower case name and
+# upper case name of your new package.  Some places below will say
+# "Do not change this" - that does not include this global change,
+# which must always be done to ensure we have unique names.
+
+#
+# FILE_VERSION, FILE_SITE and FILE_SOURCE define
+# the upstream location of the source code for the package.
+# FILE_DIR is the directory which is created when the source
+# archive is unpacked.
+# FILE_UNZIP is the command used to unzip the source.
+# It is usually "zcat" (for .gz) or "bzcat" (for .bz2)
+#
+# You should change all these variables to suit your package.
+#
+FILE_SITE=ftp://ftp.astron.com/pub/file
+FILE_VERSION=4.12
+FILE_SOURCE=file-$(FILE_VERSION).tar.gz
+FILE_DIR=file-$(FILE_VERSION)
+FILE_UNZIP=zcat
+
+#
+# FILE_IPK_VERSION should be incremented when the ipk changes.
+#
+FILE_IPK_VERSION=1
+
+#
+# FILE_PATCHES should list any patches, in the the order in
+# which they should be applied to the source code.
+#
+#FILE_PATCHES=$(FILE_SOURCE_DIR)/configure.patch
+
+#
+# If the compilation of the package requires additional
+# compilation or linking flags, then list them here.
+#
+FILE_CPPFLAGS=
+FILE_LDFLAGS=
+
+#
+# FILE_BUILD_DIR is the directory in which the build is done.
+# FILE_SOURCE_DIR is the directory which holds all the
+# patches and ipkg control files.
+# FILE_IPK_DIR is the directory in which the ipk is built.
+# FILE_IPK is the name of the resulting ipk files.
+#
+# You should not change any of these variables.
+#
+FILE_BUILD_DIR=$(BUILD_DIR)/file
+FILE_SOURCE_DIR=$(SOURCE_DIR)/file
+FILE_IPK_DIR=$(BUILD_DIR)/file-$(FILE_VERSION)-ipk
+FILE_IPK=$(BUILD_DIR)/file_$(FILE_VERSION)-$(FILE_IPK_VERSION)_armeb.ipk
+
+#
+# This is the dependency on the source code.  If the source is missing,
+# then it will be fetched from the site using wget.
+#
+$(DL_DIR)/$(FILE_SOURCE):
+	$(WGET) -P $(DL_DIR) $(FILE_SITE)/$(FILE_SOURCE)
+
+#
+# The source code depends on it existing within the download directory.
+# This target will be called by the top level Makefile to download the
+# source code's archive (.tar.gz, .bz2, etc.)
+#
+file-source: $(DL_DIR)/$(FILE_SOURCE) $(FILE_PATCHES)
+
+#
+# This target unpacks the source code in the build directory.
+# If the source archive is not .tar.gz or .tar.bz2, then you will need
+# to change the commands here.  Patches to the source code are also
+# applied in this target as required.
+#
+# This target also configures the build within the build directory.
+# Flags such as LDFLAGS and CPPFLAGS should be passed into configure
+# and NOT $(MAKE) below.  Passing it to configure causes configure to
+# correctly BUILD the Makefile with the right paths, where passing it
+# to Make causes it to override the default search paths of the compiler.
+#
+# If the compilation of the package requires other packages to be staged
+# first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
+#
+$(FILE_BUILD_DIR)/.configured: $(DL_DIR)/$(FILE_SOURCE) $(FILE_PATCHES)
+#	$(MAKE) <bar>-stage <baz>-stage
+	rm -rf $(BUILD_DIR)/$(FILE_DIR) $(FILE_BUILD_DIR)
+	$(FILE_UNZIP) $(DL_DIR)/$(FILE_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+#	cat $(FILE_PATCHES) | patch -d $(BUILD_DIR)/$(FILE_DIR) -p1
+	mv $(BUILD_DIR)/$(FILE_DIR) $(FILE_BUILD_DIR)
+	(cd $(FILE_BUILD_DIR); \
+		$(TARGET_CONFIGURE_OPTS) \
+		CPPFLAGS="$(STAGING_CPPFLAGS) $(FILE_CPPFLAGS)" \
+		LDFLAGS="$(STAGING_LDFLAGS) $(FILE_LDFLAGS)" \
+		./configure \
+		--build=$(GNU_HOST_NAME) \
+		--host=$(GNU_TARGET_NAME) \
+		--target=$(GNU_TARGET_NAME) \
+		--prefix=/opt \
+		--disable-nls \
+	)
+	touch $(FILE_BUILD_DIR)/.configured
+
+file-unpack: $(FILE_BUILD_DIR)/.configured
+
+#
+# This builds the actual binary.  You should change the target to refer
+# directly to the main binary which is built.
+#
+$(FILE_BUILD_DIR)/.built: $(FILE_BUILD_DIR)/.configured
+	rm -f $(FILE_BUILD_DIR)/.built
+	$(MAKE) -C $(FILE_BUILD_DIR)
+	touch $(FILE_BUILD_DIR)/.built
+
+#
+# You should change the dependency to refer directly to the main binary
+# which is built.
+#
+file: $(FILE_BUILD_DIR)/.built
+
+#
+# If you are building a library, then you need to stage it too.
+#
+$(STAGING_DIR)/opt/lib/libfile.so.$(FILE_VERSION): $(FILE_BUILD_DIR)/.built
+	install -d $(STAGING_DIR)/opt/include
+	install -m 644 $(FILE_BUILD_DIR)/file.h $(STAGING_DIR)/opt/include
+	install -d $(STAGING_DIR)/opt/lib
+	install -m 644 $(FILE_BUILD_DIR)/libfile.a $(STAGING_DIR)/opt/lib
+	install -m 644 $(FILE_BUILD_DIR)/libfile.so.$(FILE_VERSION) $(STAGING_DIR)/opt/lib
+	cd $(STAGING_DIR)/opt/lib && ln -fs libfile.so.$(FILE_VERSION) libfile.so.1
+	cd $(STAGING_DIR)/opt/lib && ln -fs libfile.so.$(FILE_VERSION) libfile.so
+
+file-stage: $(STAGING_DIR)/opt/lib/libfile.so.$(FILE_VERSION)
+
+#
+# This builds the IPK file.
+#
+# Binaries should be installed into $(FILE_IPK_DIR)/opt/sbin or $(FILE_IPK_DIR)/opt/bin
+# (use the location in a well-known Linux distro as a guide for choosing sbin or bin).
+# Libraries and include files should be installed into $(FILE_IPK_DIR)/opt/{lib,include}
+# Configuration files should be installed in $(FILE_IPK_DIR)/opt/etc/file/...
+# Documentation files should be installed in $(FILE_IPK_DIR)/opt/doc/file/...
+# Daemon startup scripts should be installed in $(FILE_IPK_DIR)/opt/etc/init.d/S??file
+#
+# You may need to patch your application to make it use these locations.
+#
+$(FILE_IPK): $(FILE_BUILD_DIR)/.built
+	rm -rf $(FILE_IPK_DIR) $(BUILD_DIR)/file_*_armeb.ipk
+	install -d $(FILE_IPK_DIR)/opt/bin
+	$(TARGET_STRIP) $(FILE_BUILD_DIR)/file -o $(FILE_IPK_DIR)/opt/bin/file
+	install -d $(FILE_IPK_DIR)/opt/etc/init.d
+	install -m 755 $(FILE_SOURCE_DIR)/rc.file $(FILE_IPK_DIR)/opt/etc/init.d/SXXfile
+	install -d $(FILE_IPK_DIR)/CONTROL
+	install -m 644 $(FILE_SOURCE_DIR)/control $(FILE_IPK_DIR)/CONTROL/control
+	install -m 644 $(FILE_SOURCE_DIR)/postinst $(FILE_IPK_DIR)/CONTROL/postinst
+	install -m 644 $(FILE_SOURCE_DIR)/prerm $(FILE_IPK_DIR)/CONTROL/prerm
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(FILE_IPK_DIR)
+
+#
+# This is called from the top level makefile to create the IPK file.
+#
+file-ipk: $(FILE_IPK)
+
+#
+# This is called from the top level makefile to clean all of the built files.
+#
+file-clean:
+	-$(MAKE) -C $(FILE_BUILD_DIR) clean
+
+#
+# This is called from the top level makefile to clean all dynamically created
+# directories.
+#
+file-dirclean:
+	rm -rf $(BUILD_DIR)/$(FILE_DIR) $(FILE_BUILD_DIR) $(FILE_IPK_DIR) $(FILE_IPK)
