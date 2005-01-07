720cfbf5ddaa99fd74ccc46a9ff9b19c2a7a20ef
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2005-01-07 23:01:55 +0000

    Made openssl respect rpath /opt/lib

--- trunk/make/openssl.mk
+++ trunk/make/openssl.mk
@@ -9,7 +9,7 @@ OPENSSL_SOURCE=openssl-$(OPENSSL_VERSION).tar.gz
 OPENSSL_DIR=openssl-$(OPENSSL_VERSION)
 OPENSSL_UNZIP=zcat
 
-OPENSSL_IPK_VERSION=1
+OPENSSL_IPK_VERSION=2
 
 OPENSSL_BUILD_DIR=$(BUILD_DIR)/openssl
 OPENSSL_SOURCE_DIR=$(SOURCE_DIR)/openssl
@@ -47,7 +47,7 @@ $(OPENSSL_BUILD_DIR)/libssl.so.$(OPENSSL_LIB_VERSION): $(OPENSSL_BUILD_DIR)/.con
 		$(TARGET_CONFIGURE_OPTS) \
 		AR="${TARGET_AR} r" \
 		MANDIR=/opt/man \
-		EX_LIBS=-ldl \
+		EX_LIBS="$(STAGING_LDFLAGS) -ldl" \
 		DIRS="crypto ssl apps"
 
 openssl: $(OPENSSL_BUILD_DIR)/libssl.so.$(OPENSSL_LIB_VERSION)
@@ -67,6 +67,7 @@ $(STAGING_DIR)/opt/lib/libssl.so.$(OPENSSL_LIB_VERSION): $(OPENSSL_BUILD_DIR)/li
 openssl-stage: $(STAGING_DIR)/opt/lib/libssl.so.$(OPENSSL_LIB_VERSION)
 
 $(OPENSSL_IPK): $(OPENSSL_BUILD_DIR)/libssl.so.$(OPENSSL_LIB_VERSION)
+	rm -rf $(OPENSSL_IPK_DIR) $(BUILD_DIR)/openssl_*_armeb.ipk
 	install -d $(OPENSSL_IPK_DIR)/opt/bin
 	install -m 755 $(OPENSSL_BUILD_DIR)/apps/openssl $(OPENSSL_IPK_DIR)/opt/bin/openssl
 	$(TARGET_STRIP) --strip-unneeded $(OPENSSL_IPK_DIR)/opt/bin/opensll
@@ -90,5 +91,5 @@ openssl-ipk: $(OPENSSL_IPK)
 openssl-clean:
 	-$(MAKE) -C $(OPENSSL_BUILD_DIR) clean
 
-openssl-dirclean: openssl-clean
+openssl-dirclean:
 	rm -rf $(BUILD_DIR)/$(OPENSSL_DIR) $(OPENSSL_BUILD_DIR) $(OPENSSL_IPK_DIR) $(OPENSSL_IPK)
