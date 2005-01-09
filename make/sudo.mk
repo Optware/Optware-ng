d5825f3f9f64853f7c3b487b1f0abd03db654b3a
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2005-01-09 12:43:57 +0000

    Updated sudo to match the template

--- make/sudo.mk
+++ make/sudo.mk
@@ -2,71 +2,75 @@
 # Make file for sudo
 #
 
-SUDO_DIR:=$(BUILD_DIR)/sudo
-SUDO_SOURCE_DIR:=$(SOURCE_DIR)/sudo
-SUDO_VERSION:=1.6.8p1
-SUDO:=sudo-$(SUDO_VERSION)
 #SUDO_SITE=http://probsd.org/sudoftp
-#SUDO_SOURCE:=$(SUDO).tar.gz
 SUDO_SITE=http://ipkg.nslu2-linux.org/downloads/
-SUDO_SOURCE:=$(SUDO).tar.gz
+SUDO_VERSION=1.6.8p1
+SUDO_SOURCE=sudo-$(SUDO_VERSION).tar.gz
+SUDO_DIR=sudo-$(SUDO_VERSION)
+SUDO_UNZIP=zcat
 
-SUDO_IPK_VERSION:=3
+SUDO_IPK_VERSION=4
 
-SUDO_IPK:=$(BUILD_DIR)/sudo_$(SUDO_VERSION)-$(SUDO_IPK_VERSION)_armeb.ipk
-SUDO_IPK_DIR:=$(BUILD_DIR)/sudo-$(SUDO_VERSION)-ipk
-SUDO_PATCH:=$(SUDO_SOURCE_DIR)/configure.patch
+SUDO_CONFFILES=/opt/etc/sudoers
+
+SUDO_PATCHES=$(SUDO_SOURCE_DIR)/configure.patch
+
+SUDO_BUILD_DIR:=$(BUILD_DIR)/sudo
+SUDO_SOURCE_DIR=$(SOURCE_DIR)/sudo
+SUDO_IPK_DIR=$(BUILD_DIR)/sudo-$(SUDO_VERSION)-ipk
+SUDO_IPK=$(BUILD_DIR)/sudo_$(SUDO_VERSION)-$(SUDO_IPK_VERSION)_armeb.ipk
 
 
 $(DL_DIR)/$(SUDO_SOURCE):
 	cd $(DL_DIR) && $(WGET) $(SUDO_SITE)/$(SUDO_SOURCE)
 
-sudo-source: $(DL_DIR)/$(SUDO_SOURCE) $(SUDO_PATCH)
+sudo-source: $(DL_DIR)/$(SUDO_SOURCE) $(SUDO_PATCHES)
 
-$(SUDO_DIR)/.configured: $(DL_DIR)/$(SUDO_SOURCE) $(SUDO_PATCH)
-	@rm -rf $(SUDO_DIR)
-	tar xzf $(DL_DIR)/$(SUDO_SOURCE) -C $(BUILD_DIR)
-	mv $(BUILD_DIR)/$(SUDO) $(SUDO_DIR)
-	patch -d $(SUDO_DIR) -p1 < $(SUDO_PATCH)
-	cd $(SUDO_DIR) && \
+$(SUDO_BUILD_DIR)/.configured: $(DL_DIR)/$(SUDO_SOURCE) $(SUDO_PATCHES)
+	rm -rf $(BUILD_DIR)/$(SUDO_DIR) $(SUDO_BUILD_DIR)
+	$(SUDO_UNZIP) $(DL_DIR)/$(SUDO_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+	cat $(SUDO_PATCHES) | patch -d $(BUILD_DIR)/$(SUDO_DIR) -p1
+	mv $(BUILD_DIR)/$(SUDO_DIR) $(SUDO_BUILD_DIR)
+	cd $(SUDO_BUILD_DIR) && \
 		$(TARGET_CONFIGURE_OPTS) \
 		./configure \
 			--host=$(GNU_TARGET_NAME) \
 			--target=$(GNU_TARGET_NAME) \
 			--build=$(GNU_HOST_NAME) \
-			--prefix=/opt/sudo \
-			--exec-prefix=/opt/sudo \
+			--prefix=/opt \
 			--enable-authentication \
 			--without-insults \
 			--with-editor=/bin/vi \
 			--sysconfdir=/opt/etc
-	touch $(SUDO_DIR)/.configured
+	touch $(SUDO_BUILD_DIR)/.configured
 
-sudo-unpack: $(SUDO_DIR)/.configured
+sudo-unpack: $(SUDO_BUILD_DIR)/.configured
 
-$(SUDO_DIR)/sudo: $(SUDO_DIR)/.configured
-	make -C $(BUILD_DIR)/sudo
+$(SUDO_BUILD_DIR)/sudo: $(SUDO_BUILD_DIR)/.configured
+	make -C $(SUDO_BUILD_DIR)
 
-sudo: $(SUDO_DIR)/sudo
+sudo: $(SUDO_BUILD_DIR)/sudo
 
-$(SUDO_IPK): $(SUDO_DIR)/sudo
+$(SUDO_IPK): $(SUDO_BUILD_DIR)/sudo
+	rm -rf $(SUDO_IPK_DIR) $(BUILD_DIR)/sudo_*_armeb.ipk
 	install -d $(SUDO_IPK_DIR)/opt/bin
-	$(TARGET_STRIP) $(SUDO_DIR)/sudo -o $(SUDO_IPK_DIR)/opt/bin/sudo
-	$(TARGET_STRIP) $(SUDO_DIR)/visudo -o $(SUDO_IPK_DIR)/opt/bin/visudo
+	$(TARGET_STRIP) $(SUDO_BUILD_DIR)/sudo -o $(SUDO_IPK_DIR)/opt/bin/sudo
+	$(TARGET_STRIP) $(SUDO_BUILD_DIR)/visudo -o $(SUDO_IPK_DIR)/opt/bin/visudo
 	install -d $(SUDO_IPK_DIR)/opt/etc
-	install -m 600 $(SUDO_DIR)/sudoers $(SUDO_IPK_DIR)/opt/etc/sudoers
+	install -m 600 $(SUDO_BUILD_DIR)/sudoers $(SUDO_IPK_DIR)/opt/etc/sudoers
 	install -d $(SUDO_IPK_DIR)/opt/doc/sudo
-	install -m 644 $(SUDO_DIR)/sample.sudoers $(SUDO_IPK_DIR)/opt/doc/sudo/sample.sudoers
+	install -m 644 $(SUDO_BUILD_DIR)/sample.sudoers $(SUDO_IPK_DIR)/opt/doc/sudo/sample.sudoers
 	install -d $(SUDO_IPK_DIR)/CONTROL
 	install -m 644 $(SUDO_SOURCE_DIR)/control $(SUDO_IPK_DIR)/CONTROL/control
 	install -m 644 $(SUDO_SOURCE_DIR)/postinst $(SUDO_IPK_DIR)/CONTROL/postinst
+	echo $(SUDO_CONFFILES) | sed -e 's/ /\n/g' > $(SUDO_IPK_DIR)/CONTROL/conffiles
 	cd $(BUILD_DIR) && $(IPKG_BUILD) $(SUDO_IPK_DIR)
 
 
 sudo-ipk: $(SUDO_IPK)
 
 sudo-clean:
-	-make -C $(BUILD_DIR)/sudo clean
+	-make -C $(SUDO_BUILD_DIR) clean
 
 sudo-dirclean:
-	rm -rf $(SUDO_DIR) $(SUDO_IPK_DIR) $(SUDO_IPK)
+	rm -rf $(BUILD_DIR)/$(SUDO_DIR) $(SUDO_BUILD_DIR) $(SUDO_IPK_DIR) $(SUDO_IPK)
