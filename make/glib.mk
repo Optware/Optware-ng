8a7a59cb7c8fee034e0df22fe332be83f8f563a7
gieldn <gieldn@1f1dca69-5516-0410-b834-e485effd04c3>
2005-01-14 20:01:11 +0000

    Revamped to match template.mk. Updated to version 2.6.1. We're using the make install of glib now to get all the files to the right location. Locales are handled by a variable in the makefile.

--- make/glib.mk
+++ make/glib.mk
@@ -4,91 +4,173 @@
 #
 ###########################################################
 
-GLIB_DIR=$(BUILD_DIR)/glib
-
-GLIB_VERSION=2.2.0
-GLIB=glib-$(GLIB_VERSION)
-GLIB_SITE=ftp://ftp.gtk.org/pub/gtk/v2.2/
-GLIB_SOURCE=$(GLIB).tar.gz
+#
+# GLIB_VERSION, GLIB_SITE and GLIB_SOURCE define
+# the upstream location of the source code for the package.
+# GLIB_DIR is the directory which is created when the source
+# archive is unpacked.
+# GLIB_UNZIP is the command used to unzip the source.
+# It is usually "zcat" (for .gz) or "bzcat" (for .bz2)
+#
+GLIB_SITE=ftp://ftp.gtk.org/pub/gtk/v2.6/
+GLIB_VERSION=2.6.1
+GLIB_SOURCE=glib-$(GLIB_VERSION).tar.gz
+GLIB_DIR=glib-$(GLIB_VERSION)
 GLIB_UNZIP=zcat
 
-GLIB_IPK=$(BUILD_DIR)/glib_$(GLIB_VERSION)-1_armeb.ipk
-GLIB_IPK_DIR=$(BUILD_DIR)/glib-$(GLIB_VERSION)-ipk
+#
+# GLIB_IPK_VERSION should be incremented when the ipk changes.
+#
+GLIB_IPK_VERSION=1
 
+#
+# GLIB_LOCALES defines which locales get installed
+#
+GLIB_LOCALES=
+
+#
+# GLIB_CONFFILES should be a list of user-editable files
+#GLIB_CONFFILES=/opt/etc/glib.conf /opt/etc/init.d/SXXglib
+
+#
+# GLIB_PATCHES should list any patches, in the the order in
+# which they should be applied to the source code.
+#
+#GLIB_PATCHES=$(GLIB_SOURCE_DIR)/configure.patch
+
+#
+# If the compilation of the package requires additional
+# compilation or linking flags, then list them here.
+#
+GLIB_CPPFLAGS=
+GLIB_LDFLAGS=
+
+#
+# GLIB_BUILD_DIR is the directory in which the build is done.
+# GLIB_SOURCE_DIR is the directory which holds all the
+# patches and ipkg control files.
+# GLIB_IPK_DIR is the directory in which the ipk is built.
+# GLIB_IPK is the name of the resulting ipk files.
+#
+# You should not change any of these variables.
+#
+GLIB_BUILD_DIR=$(BUILD_DIR)/glib
+GLIB_SOURCE_DIR=$(SOURCE_DIR)/glib
+GLIB_IPK_DIR=$(BUILD_DIR)/glib-$(GLIB_VERSION)-ipk
+GLIB_IPK=$(BUILD_DIR)/glib_$(GLIB_VERSION)-$(GLIB_IPK_VERSION)_armeb.ipk
+
+#
+# This is the dependency on the source code.  If the source is missing,
+# then it will be fetched from the site using wget.
+#
 $(DL_DIR)/$(GLIB_SOURCE):
 	$(WGET) -P $(DL_DIR) $(GLIB_SITE)/$(GLIB_SOURCE)
 
-glib-source: $(DL_DIR)/$(GLIB_SOURCE)
+#
+# The source code depends on it existing within the download directory.
+# This target will be called by the top level Makefile to download the
+# source code's archive (.tar.gz, .bz2, etc.)
+#
+glib-source: $(DL_DIR)/$(GLIB_SOURCE) $(GLIB_PATCHES)
 
-$(GLIB_DIR)/.source: $(DL_DIR)/$(GLIB_SOURCE)
+#
+# This target unpacks the source code in the build directory.
+# If the source archive is not .tar.gz or .tar.bz2, then you will need
+# to change the commands here.  Patches to the source code are also
+# applied in this target as required.
+#
+# This target also configures the build within the build directory.
+# Flags such as LDFLAGS and CPPFLAGS should be passed into configure
+# and NOT $(MAKE) below.  Passing it to configure causes configure to
+# correctly BUILD the Makefile with the right paths, where passing it
+# to Make causes it to override the default search paths of the compiler.
+#
+# If the compilation of the package requires other packages to be staged
+# first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
+#
+$(GLIB_BUILD_DIR)/.configured: $(DL_DIR)/$(GLIB_SOURCE) $(GLIB_PATCHES)
+	rm -rf $(BUILD_DIR)/$(GLIB_DIR) $(GLIB_BUILD_DIR)
 	$(GLIB_UNZIP) $(DL_DIR)/$(GLIB_SOURCE) | tar -C $(BUILD_DIR) -xvf -
-	mv $(BUILD_DIR)/glib-$(GLIB_VERSION) $(GLIB_DIR)
-	touch $(GLIB_DIR)/.source
-
-$(GLIB_DIR)/.configured: $(GLIB_DIR)/.source
-	cp $(SOURCE_DIR)/glib/glib.cache $(GLIB_DIR)/arm.cache
-	(cd $(GLIB_DIR); \
+	#cat $(GLIB_PATCHES) | patch -d $(BUILD_DIR)/$(GLIB_DIR) -p1
+	mv $(BUILD_DIR)/$(GLIB_DIR) $(GLIB_BUILD_DIR)
+	cp $(SOURCE_DIR)/glib/glib.cache $(GLIB_BUILD_DIR)/arm.cache
+	sed -i -e '/^ALL_LINGUAS=/s/"[^"]\+"$$/$(GLIB_LOCALES)/;' $(GLIB_BUILD_DIR)/configure
+	(cd $(GLIB_BUILD_DIR); \
+		$(TARGET_CONFIGURE_OPTS) \
 		./configure \
-		--host=$(GNU_TARGET_NAME) \
 		--build=$(GNU_HOST_NAME) \
+		--host=$(GNU_TARGET_NAME) \
+		--target=$(GNU_TARGET_NAME) \
 		--cache-file=arm.cache \
-		--disable-rebuilds \
 		--prefix=/opt \
-	);
-	touch $(GLIB_DIR)/.configured
+		--disable-nls \
+	)
+	touch $(GLIB_BUILD_DIR)/.configured
 
-$(GLIB_DIR)/glib/libglib-2.0.la: $(GLIB_DIR)/.configured
-	$(MAKE) -C $(GLIB_DIR) CC=$(TARGET_CC) CCLD=$(TARGET_CC) \
-	RANLIB=$(TARGET_RANLIB) AR=$(TARGET_AR) LD=$(TARGET_LD) 
+glib-unpack: $(GLIB_BUILD_DIR)/.configured
 
+#
+# This builds the actual binary.  You should change the target to refer
+# directly to the main binary which is built.
+#
+$(GLIB_BUILD_DIR)/.built: $(GLIB_BUILD_DIR)/.configured
+	rm -f $(GLIB_BUILD_DIR)/.built
+	$(MAKE) -C $(GLIB_BUILD_DIR)
+	touch $(GLIB_BUILD_DIR)/.built
 
-glib-headers: glib-ipk
+#
+# You should change the dependency to refer directly to the main binary
+# which is built.
+#
+glib: $(GLIB_BUILD_DIR)/.built
 
-glib: glib-ipk
+#
+# If you are building a library, then you need to stage it too.
+#
+$(STAGING_DIR)/opt/lib/libglib-2.0.so: $(GLIB_BUILD_DIR)/.built
+	$(MAKE) -C $(GLIB_BUILD_DIR) install-strip prefix=$(STAGING_DIR)/opt
+	rm -rf $(STAGING_DIR)/opt/lib/libglib-2.0.la
+	rm -rf $(STAGING_DIR)/opt/lib/libgmodule-2.0.la
+	rm -rf $(STAGING_DIR)/opt/lib/libgobject-2.0.la
+	rm -rf $(STAGING_DIR)/opt/lib/libgthread-2.0.la
 
-$(GLIB_IPK): $(GLIB_DIR)/glib/libglib-2.0.la
-	mkdir -p $(GLIB_IPK_DIR)/CONTROL
-	cp $(SOURCE_DIR)/glib/control $(GLIB_IPK_DIR)/CONTROL/control
-	mkdir -p $(GLIB_IPK_DIR)/opt/lib
-	cp $(GLIB_DIR)/gmodule/.libs/* $(GLIB_IPK_DIR)/opt/lib
-	cp $(GLIB_DIR)/gthread/.libs/* $(GLIB_IPK_DIR)/opt/lib
-	cp $(GLIB_DIR)/glib/.libs/* $(GLIB_IPK_DIR)/opt/lib
-	rm -rf $(STAGING_DIR)/CONTROL
+glib-stage: $(STAGING_DIR)/opt/lib/libglib-2.0.so
+
+#
+# This builds the IPK file.
+#
+# Binaries should be installed into $(GLIB_IPK_DIR)/opt/sbin or $(GLIB_IPK_DIR)/opt/bin
+# (use the location in a well-known Linux distro as a guide for choosing sbin or bin).
+# Libraries and include files should be installed into $(GLIB_IPK_DIR)/opt/{lib,include}
+# Configuration files should be installed in $(GLIB_IPK_DIR)/opt/etc/glib/...
+# Documentation files should be installed in $(GLIB_IPK_DIR)/opt/doc/glib/...
+# Daemon startup scripts should be installed in $(GLIB_IPK_DIR)/opt/etc/init.d/S??glib
+#
+# You may need to patch your application to make it use these locations.
+#
+$(GLIB_IPK): $(GLIB_BUILD_DIR)/.built
+	rm -rf $(GLIB_IPK_DIR) $(BUILD_DIR)/glib_*_armeb.ipk
+	$(MAKE) -C $(GLIB_BUILD_DIR) install-strip prefix=$(GLIB_IPK_DIR)/opt
+	rm -rf $(GLIB_IPK_DIR)/opt/share/gtk-doc
+	rm -rf $(GLIB_IPK_DIR)/opt/man
+	install -d $(GLIB_IPK_DIR)/CONTROL
+	install -m 644 $(GLIB_SOURCE_DIR)/control $(GLIB_IPK_DIR)/CONTROL/control
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(GLIB_IPK_DIR)
 
-glib-stage:  $(GLIB_IPK)
-	cp $(GLIB_DIR)/gmodule/.libs/* $(STAGING_LIB_DIR)
-	cp $(GLIB_DIR)/gthread/.libs/* $(STAGING_LIB_DIR)
-	cp $(GLIB_DIR)/glib/.libs/* $(STAGING_LIB_DIR)
-
-	cd $(STAGING_LIB_DIR)
-	rm -f $(STAGING_LIB_DIR)/libglib-2.0.a
-	ln -s $(STAGING_LIB_DIR)/libglib-2.0.so $(STAGING_LIB_DIR)/libglib-2.0.a
-	rm -f $(STAGING_LIB_DIR)/libgthread-2.0.a
-	ln -s $(STAGING_LIB_DIR)/libgthread-2.0.so $(STAGING_LIB_DIR)/libgthread-2.0.a
-	ls -l $(STAGING_LIB_DIR)/libgthread-2.0*
-	rm -f $(STAGING_LIB_DIR)/libgmodule-2.0.a
-	ln -s $(STAGING_LIB_DIR)/libgmodule-2.0.so $(STAGING_LIB_DIR)/libgmodule-2.0.a
-
-	mkdir -p $(STAGING_INCLUDE_DIR)/gmodule-2.0
-	cp $(GLIB_DIR)/gmodule/*.h $(STAGING_INCLUDE_DIR)/gmodule-2.0
-
-	mkdir -p $(STAGING_INCLUDE_DIR)/glib-2.0
-	cp $(GLIB_DIR)/glib/*.h $(STAGING_INCLUDE_DIR)/glib-2.0
-
-
-
+#
+# This is called from the top level makefile to create the IPK file.
+#
 glib-ipk: $(GLIB_IPK)
 
-glib-source: $(DL_DIR)/$(GLIB_SOURCE)
-
+#
+# This is called from the top level makefile to clean all of the built files.
+#
 glib-clean:
-	-$(MAKE) -C $(GLIB_DIR) uninstall
-	-$(MAKE) -C $(GLIB_DIR) clean
+	-$(MAKE) -C $(GLIB_BUILD_DIR) clean
 
-glib-distclean:
-	-rm $(GLIB_DIR)/.configured
-	-$(MAKE) -C $(GLIB_DIR) distclean
-
+#
+# This is called from the top level makefile to clean all dynamically created
+# directories.
+#
 glib-dirclean:
-	rm -rf $(GLIB_DIR) $(GLIB_IPK_DIR) $(GLIB_IPK)
+	rm -rf $(BUILD_DIR)/$(GLIB_DIR) $(GLIB_BUILD_DIR) $(GLIB_IPK_DIR) $(GLIB_IPK)
