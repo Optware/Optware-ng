48928eb208fe37bf8a24af3cc7fd1d9e81e68534
purbanec <purbanec@1f1dca69-5516-0410-b834-e485effd04c3>
2005-01-19 12:31:48 +0000

    Released 1.6 - about 170% performance improvement in turbo gets.

--- trunk/make/puppy.mk
+++ trunk/make/puppy.mk
@@ -6,9 +6,9 @@
 
 # PUPPY_REPOSITORY=:ext:$(LOGNAME)@cvs.sf.net:/cvsroot/puppy
 PUPPY_REPOSITORY=:pserver:anonymous@cvs.sf.net:/cvsroot/puppy
-PUPPY_VERSION=1.5
+PUPPY_VERSION=1.6
 PUPPY_SOURCE=puppy-$(PUPPY_VERSION).tar.gz
-PUPPY_TAG=-r PUPPY_1_5
+PUPPY_TAG=-r PUPPY_1_6
 PUPPY_MODULE=puppy
 PUPPY_DIR=puppy-$(PUPPY_VERSION)
 PUPPY_UNZIP=zcat
