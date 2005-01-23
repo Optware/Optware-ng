ca5769e141e4986bb3346217367cb092a0772a25
ptweety <ptweety@1f1dca69-5516-0410-b834-e485effd04c3>
2005-01-23 14:42:00 +0000

    added groff; needs still some work on libstdc++.so.5 problem

---
+++ make/groff.mk
@@ -0,0 +1,192 @@
+###########################################################
+#
+# groff
+#
+###########################################################
+
+# You must replace "groff" and "GROFF" with the lower case name and
+# upper case name of your new package.  Some places below will say
+# "Do not change this" - that does not include this global change,
+# which must always be done to ensure we have unique names.
+
+#
+# GROFF_VERSION, GROFF_SITE and GROFF_SOURCE define
+# the upstream location of the source code for the package.
+# GROFF_DIR is the directory which is created when the source
+# archive is unpacked.
+# GROFF_UNZIP is the command used to unzip the source.
+# It is usually "zcat" (for .gz) or "bzcat" (for .bz2)
+#
+# You should change all these variables to suit your package.
+#
+GROFF_SITE=http://ftp.gnu.org/gnu/groff
+GROFF_VERSION=1.19.1
+GROFF_SOURCE=groff-$(GROFF_VERSION).tar.gz
+GROFF_DIR=groff-$(GROFF_VERSION)
+GROFF_UNZIP=zcat
+
+#
+# GROFF_IPK_VERSION should be incremented when the ipk changes.
+#
+GROFF_IPK_VERSION=1
+
+#
+# GROFF_CONFFILES should be a list of user-editable files
+#GROFF_CONFFILES=
+
+#
+# GROFF_PATCHES should list any patches, in the the order in
+# which they should be applied to the source code.
+#
+GROFF_PATCHES=$(GROFF_SOURCE_DIR)/groff.patch
+
+#
+# If the compilation of the package requires additional
+# compilation or linking flags, then list them here.
+#
+GROFF_CPPFLAGS=
+GROFF_LDFLAGS=
+
+#
+# GROFF_BUILD_DIR is the directory in which the build is done.
+# GROFF_SOURCE_DIR is the directory which holds all the
+# patches and ipkg control files.
+# GROFF_IPK_DIR is the directory in which the ipk is built.
+# GROFF_IPK is the name of the resulting ipk files.
+#
+# You should not change any of these variables.
+#
+GROFF_BUILD_DIR=$(BUILD_DIR)/groff
+GROFF_SOURCE_DIR=$(SOURCE_DIR)/groff
+GROFF_IPK_DIR=$(BUILD_DIR)/groff-$(GROFF_VERSION)-ipk
+GROFF_IPK=$(BUILD_DIR)/groff_$(GROFF_VERSION)-$(GROFF_IPK_VERSION)_armeb.ipk
+
+#
+# This is the dependency on the source code.  If the source is missing,
+# then it will be fetched from the site using wget.
+#
+$(DL_DIR)/$(GROFF_SOURCE):
+	$(WGET) -P $(DL_DIR) $(GROFF_SITE)/$(GROFF_SOURCE)
+
+#
+# The source code depends on it existing within the download directory.
+# This target will be called by the top level Makefile to download the
+# source code's archive (.tar.gz, .bz2, etc.)
+#
+groff-source: $(DL_DIR)/$(GROFF_SOURCE) $(GROFF_PATCHES)
+
+#
+# This target unpacks the source code in the build directory.
+# If the source archive is not .tar.gz or .tar.bz2, then you will need
+# to change the commands here.  Patches to the source code are also
+# applied in this target as required.
+#
+# This target also configures the build within the build directory.
+# Flags such as LDFLAGS and CPPFLAGS should be passed into configure
+# and NOT $(MAKE) below.  Passing it to configure causes configure to
+# correctly BUILD the Makefile with the right paths, where passing it
+# to Make causes it to override the default search paths of the compiler.
+#
+# If the compilation of the package requires other packages to be staged
+# first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
+#
+$(GROFF_BUILD_DIR)/.configured: $(DL_DIR)/$(GROFF_SOURCE) $(GROFF_PATCHES)
+#	$(MAKE) <bar>-stage <baz>-stage
+	rm -rf $(BUILD_DIR)/$(GROFF_DIR) $(GROFF_BUILD_DIR)
+	$(GROFF_UNZIP) $(DL_DIR)/$(GROFF_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+	cat $(GROFF_PATCHES) | patch -d $(BUILD_DIR)/$(GROFF_DIR) -p1
+	mv $(BUILD_DIR)/$(GROFF_DIR) $(GROFF_BUILD_DIR)
+	(cd $(GROFF_BUILD_DIR); \
+		$(TARGET_CONFIGURE_OPTS) \
+		CPPFLAGS="$(STAGING_CPPFLAGS) $(GROFF_CPPFLAGS)" \
+		LDFLAGS="$(STAGING_LDFLAGS) $(GROFF_LDFLAGS)" \
+		./configure \
+		--build=$(GNU_HOST_NAME) \
+		--host=$(GNU_TARGET_NAME) \
+		--target=$(GNU_TARGET_NAME) \
+		--prefix=\$$\(DESTDIR\)/opt \
+		--disable-nls \
+	)
+	touch $(GROFF_BUILD_DIR)/.configured
+
+groff-unpack: $(GROFF_BUILD_DIR)/.configured
+
+#
+# This builds the actual binary.
+#
+$(GROFF_BUILD_DIR)/.built: $(GROFF_BUILD_DIR)/.configured
+	rm -f $(GROFF_BUILD_DIR)/.built
+	$(MAKE) -C $(GROFF_BUILD_DIR)
+	touch $(GROFF_BUILD_DIR)/.built
+
+#
+# This is the build convenience target.
+#
+groff: $(GROFF_BUILD_DIR)/.built
+
+#
+# If you are building a library, then you need to stage it too.
+#
+$(STAGING_DIR)/opt/lib/libgroff.so.$(GROFF_VERSION): $(GROFF_BUILD_DIR)/.built
+	install -d $(STAGING_DIR)/opt/include
+	install -m 644 $(GROFF_BUILD_DIR)/groff.h $(STAGING_DIR)/opt/include
+	install -d $(STAGING_DIR)/opt/lib
+	install -m 644 $(GROFF_BUILD_DIR)/libgroff.a $(STAGING_DIR)/opt/lib
+	install -m 644 $(GROFF_BUILD_DIR)/libgroff.so.$(GROFF_VERSION) $(STAGING_DIR)/opt/lib
+	cd $(STAGING_DIR)/opt/lib && ln -fs libgroff.so.$(GROFF_VERSION) libgroff.so.1
+	cd $(STAGING_DIR)/opt/lib && ln -fs libgroff.so.$(GROFF_VERSION) libgroff.so
+
+groff-stage: $(STAGING_DIR)/opt/lib/libgroff.so.$(GROFF_VERSION)
+
+#
+# This builds the IPK file.
+#
+# Binaries should be installed into $(GROFF_IPK_DIR)/opt/sbin or $(GROFF_IPK_DIR)/opt/bin
+# (use the location in a well-known Linux distro as a guide for choosing sbin or bin).
+# Libraries and include files should be installed into $(GROFF_IPK_DIR)/opt/{lib,include}
+# Configuration files should be installed in $(GROFF_IPK_DIR)/opt/etc/groff/...
+# Documentation files should be installed in $(GROFF_IPK_DIR)/opt/doc/groff/...
+# Daemon startup scripts should be installed in $(GROFF_IPK_DIR)/opt/etc/init.d/S??groff
+#
+# You may need to patch your application to make it use these locations.
+#
+$(GROFF_IPK): $(GROFF_BUILD_DIR)/.built
+	rm -rf $(GROFF_IPK_DIR) $(BUILD_DIR)/groff_*_armeb.ipk
+	install -d $(GROFF_IPK_DIR)/opt/bin
+	install -d $(GROFF_IPK_DIR)/opt/info
+	install -d $(GROFF_IPK_DIR)/opt/lib/groff/site-tmac
+	install -d $(GROFF_IPK_DIR)/opt/man/man1
+	install -d $(GROFF_IPK_DIR)/opt/man/man5
+	install -d $(GROFF_IPK_DIR)/opt/man/man7
+	install -d $(GROFF_IPK_DIR)/opt/share/groff/1.19.1
+	install -d $(GROFF_IPK_DIR)/opt/share/doc/groff/1.19.1
+	$(MAKE) -C $(GROFF_BUILD_DIR) DESTDIR=$(GROFF_IPK_DIR) install
+#	$(STRIP_COMMAND) $(GROFF_BUILD_DIR)/groff -o $(GROFF_IPK_DIR)/opt/bin/groff
+#	install -d $(GROFF_IPK_DIR)/opt/etc/
+#	install -m 755 $(GROFF_SOURCE_DIR)/groff.conf $(GROFF_IPK_DIR)/opt/etc/groff.conf
+#	install -d $(GROFF_IPK_DIR)/opt/etc/init.d
+#	install -m 755 $(GROFF_SOURCE_DIR)/rc.groff $(GROFF_IPK_DIR)/opt/etc/init.d/SXXgroff
+	install -d $(GROFF_IPK_DIR)/CONTROL
+	install -m 644 $(GROFF_SOURCE_DIR)/control $(GROFF_IPK_DIR)/CONTROL/control
+#	install -m 644 $(GROFF_SOURCE_DIR)/postinst $(GROFF_IPK_DIR)/CONTROL/postinst
+#	install -m 644 $(GROFF_SOURCE_DIR)/prerm $(GROFF_IPK_DIR)/CONTROL/prerm
+	echo $(GROFF_CONFFILES) | sed -e 's/ /\n/g' > $(GROFF_IPK_DIR)/CONTROL/conffiles
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(GROFF_IPK_DIR)
+
+#
+# This is called from the top level makefile to create the IPK file.
+#
+groff-ipk: $(GROFF_IPK)
+
+#
+# This is called from the top level makefile to clean all of the built files.
+#
+groff-clean:
+	-$(MAKE) -C $(GROFF_BUILD_DIR) clean
+
+#
+# This is called from the top level makefile to clean all dynamically created
+# directories.
+#
+groff-dirclean:
+	rm -rf $(BUILD_DIR)/$(GROFF_DIR) $(GROFF_BUILD_DIR) $(GROFF_IPK_DIR) $(GROFF_IPK)
