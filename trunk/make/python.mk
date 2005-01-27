38704da342ff8271085549d1fa5b82221b923996
edmondsc <edmondsc@1f1dca69-5516-0410-b834-e485effd04c3>
2005-01-27 06:54:01 +0000

    python checkin, not complete out there for others to work on

---
+++ trunk/make/python.mk
@@ -0,0 +1,176 @@
+###########################################################
+#
+# python
+#
+###########################################################
+
+# You must replace "python" and "PYTHON" with the lower case name and
+# upper case name of your new package.  Some places below will say
+# "Do not change this" - that does not include this global change,
+# which must always be done to ensure we have unique names.
+
+#
+# PYTHON_VERSION, PYTHON_SITE and PYTHON_SOURCE define
+# the upstream location of the source code for the package.
+# PYTHON_DIR is the directory which is created when the source
+# archive is unpacked.
+# PYTHON_UNZIP is the command used to unzip the source.
+# It is usually "zcat" (for .gz) or "bzcat" (for .bz2)
+#
+# You should change all these variables to suit your package.
+#
+PYTHON_SITE=http://www.python.org/ftp/python/2.4/
+PYTHON_VERSION=2.4
+PYTHON_SOURCE=Python-$(PYTHON_VERSION).tgz
+PYTHON_DIR=Python-$(PYTHON_VERSION)
+PYTHON_UNZIP=zcat
+
+#
+# PYTHON_IPK_VERSION should be incremented when the ipk changes.
+#
+PYTHON_IPK_VERSION=1
+
+#
+# PYTHON_PATCHES should list any patches, in the the order in
+# which they should be applied to the source code.
+#
+PYTHON_PATCHES=$(PYTHON_SOURCE_DIR)/python-cross-compile.patch
+
+#
+# If the compilation of the package requires additional
+# compilation or linking flags, then list them here.
+#
+PYTHON_CPPFLAGS=
+PYTHON_LDFLAGS=
+
+#
+# PYTHON_BUILD_DIR is the directory in which the build is done.
+# PYTHON_SOURCE_DIR is the directory which holds all the
+# patches and ipkg control files.
+# PYTHON_IPK_DIR is the directory in which the ipk is built.
+# PYTHON_IPK is the name of the resulting ipk files.
+#
+# You should not change any of these variables.
+#
+PYTHON_BUILD_DIR=$(BUILD_DIR)/python
+PYTHON_SOURCE_DIR=$(SOURCE_DIR)/python
+PYTHON_IPK_DIR=$(BUILD_DIR)/python-$(PYTHON_VERSION)-ipk
+PYTHON_IPK=$(BUILD_DIR)/python_$(PYTHON_VERSION)-$(PYTHON_IPK_VERSION)_armeb.ipk
+
+#
+# This is the dependency on the source code.  If the source is missing,
+# then it will be fetched from the site using wget.
+#
+$(DL_DIR)/$(PYTHON_SOURCE):
+	$(WGET) -P $(DL_DIR) $(PYTHON_SITE)/$(PYTHON_SOURCE)
+
+#
+# The source code depends on it existing within the download directory.
+# This target will be called by the top level Makefile to download the
+# source code's archive (.tar.gz, .bz2, etc.)
+#
+python-source: $(DL_DIR)/$(PYTHON_SOURCE) $(PYTHON_PATCHES)
+
+#
+# This target unpacks the source code in the build directory.
+# If the source archive is not .tar.gz or .tar.bz2, then you will need
+# to change the commands here.  Patches to the source code are also
+# applied in this target as required.
+#
+# This target also configures the build within the build directory.
+# Flags such as LDFLAGS and CPPFLAGS should be passed into configure
+# and NOT $(MAKE) below.  Passing it to configure causes configure to
+# correctly BUILD the Makefile with the right paths, where passing it
+# to Make causes it to override the default search paths of the compiler.
+#
+# If the compilation of the package requires other packages to be staged
+# first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
+#
+$(PYTHON_BUILD_DIR)/.configured: $(DL_DIR)/$(PYTHON_SOURCE) $(PYTHON_PATCHES)
+	rm -rf $(BUILD_DIR)/$(PYTHON_DIR) $(PYTHON_BUILD_DIR)
+	$(PYTHON_UNZIP) $(DL_DIR)/$(PYTHON_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+	cat $(PYTHON_PATCHES) | patch -d $(BUILD_DIR)/$(PYTHON_DIR) -p1
+	mv $(BUILD_DIR)/$(PYTHON_DIR) $(PYTHON_BUILD_DIR)
+	mkdir $(PYTHON_BUILD_DIR)/cross_dir
+	(cd $(PYTHON_BUILD_DIR); \
+		$(TARGET_CONFIGURE_OPTS) \
+		CPPFLAGS="$(STAGING_CPPFLAGS) $(PYTHON_CPPFLAGS)" \
+		LDFLAGS="$(STAGING_LDFLAGS) $(PYTHON_LDFLAGS)" \
+		./configure \
+		--build=$(GNU_HOST_NAME) \
+		--host=$(GNU_TARGET_NAME) \
+		--target=$(GNU_TARGET_NAME) \
+		--prefix=/opt \
+	)
+	touch $(PYTHON_BUILD_DIR)/.configured
+
+python-unpack: $(PYTHON_BUILD_DIR)/.configured
+
+#
+# This builds the actual binary.  You should change the target to refer
+# directly to the main binary which is built.
+#
+$(PYTHON_BUILD_DIR)/python: $(PYTHON_BUILD_DIR)/.configured
+	$(MAKE) -C $(PYTHON_BUILD_DIR)
+
+#
+# You should change the dependency to refer directly to the main binary
+# which is built.
+#
+python: $(PYTHON_BUILD_DIR)/python
+
+#
+# If you are building a library, then you need to stage it too.
+#
+$(STAGING_DIR)/opt/lib/libpython.so.$(PYTHON_VERSION): $(PYTHON_BUILD_DIR)/libpython.so.$(PYTHON_VERSION)
+	install -d $(STAGING_DIR)/opt/include
+	install -m 644 $(PYTHON_BUILD_DIR)/python.h $(STAGING_DIR)/opt/include
+	install -d $(STAGING_DIR)/opt/lib
+	install -m 644 $(PYTHON_BUILD_DIR)/libpython.a $(STAGING_DIR)/opt/lib
+	install -m 644 $(PYTHON_BUILD_DIR)/libpython.so.$(PYTHON_VERSION) $(STAGING_DIR)/opt/lib
+	cd $(STAGING_DIR)/opt/lib && ln -fs libpython.so.$(PYTHON_VERSION) libpython.so.1
+	cd $(STAGING_DIR)/opt/lib && ln -fs libpython.so.$(PYTHON_VERSION) libpython.so
+
+python-stage: $(STAGING_DIR)/opt/lib/libpython.so.$(PYTHON_VERSION)
+
+#
+# This builds the IPK file.
+#
+# Binaries should be installed into $(PYTHON_IPK_DIR)/opt/sbin or $(PYTHON_IPK_DIR)/opt/bin
+# (use the location in a well-known Linux distro as a guide for choosing sbin or bin).
+# Libraries and include files should be installed into $(PYTHON_IPK_DIR)/opt/{lib,include}
+# Configuration files should be installed in $(PYTHON_IPK_DIR)/opt/etc/python/...
+# Documentation files should be installed in $(PYTHON_IPK_DIR)/opt/doc/python/...
+# Daemon startup scripts should be installed in $(PYTHON_IPK_DIR)/opt/etc/init.d/S??python
+#
+# You may need to patch your application to make it use these locations.
+#
+$(PYTHON_IPK): $(PYTHON_BUILD_DIR)/python
+	rm -rf $(PYTHON_IPK_DIR) $(PYTHON_IPK)
+	install -d $(PYTHON_IPK_DIR)/opt/bin
+	$(STRIP) $(PYTHON_BUILD_DIR)/python -o $(PYTHON_IPK_DIR)/opt/bin/python
+	install -d $(PYTHON_IPK_DIR)/opt/etc/init.d
+	install -m 755 $(PYTHON_SOURCE_DIR)/rc.python $(PYTHON_IPK_DIR)/opt/etc/init.d/SXXpython
+	install -d $(PYTHON_IPK_DIR)/CONTROL
+	install -m 644 $(PYTHON_SOURCE_DIR)/control $(PYTHON_IPK_DIR)/CONTROL/control
+	install -m 644 $(PYTHON_SOURCE_DIR)/postinst $(PYTHON_IPK_DIR)/CONTROL/postinst
+	install -m 644 $(PYTHON_SOURCE_DIR)/prerm $(PYTHON_IPK_DIR)/CONTROL/prerm
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(PYTHON_IPK_DIR)
+
+#
+# This is called from the top level makefile to create the IPK file.
+#
+python-ipk: $(PYTHON_IPK)
+
+#
+# This is called from the top level makefile to clean all of the built files.
+#
+python-clean:
+	-$(MAKE) -C $(PYTHON_BUILD_DIR) clean
+
+#
+# This is called from the top level makefile to clean all dynamically created
+# directories.
+#
+python-dirclean:
+	rm -rf $(BUILD_DIR)/$(PYTHON_DIR) $(PYTHON_BUILD_DIR) $(PYTHON_IPK_DIR) $(PYTHON_IPK)
