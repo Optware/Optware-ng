a26019b5b0c12c92486cf92bc02d7071c46d87d0
ptweety <ptweety@1f1dca69-5516-0410-b834-e485effd04c3>
2005-01-31 21:36:02 +0000

    updated the postfix-ipk target. CONFFILES still missing.

--- trunk/make/postfix.mk
+++ trunk/make/postfix.mk
@@ -49,7 +49,7 @@ $(POSTFIX_BUILD_DIR)/.configured: $(DL_DIR)/$(POSTFIX_SOURCE) $(POSTFIX_PATCHES)
 			-DDEF_HTML_DIR=\"/opt/share/doc/postfix/html\" \
 			-DDEF_MANPAGE_DIR=\"/opt/man\" \
 			-DDEF_NEWALIAS_PATH=\"/opt/bin/newaliases\" \
-			-DDEF_QUEUE_DIR=\"/var/spool/postfix\" \
+			-DDEF_QUEUE_DIR=\"/opt/var/spool/postfix\" \
 			-DDEF_README_DIR=\"/opt/share/doc/postfix/readme\" \
 			-DDEF_SENDMAIL_PATH=\"/opt/sbin/sendmail\" \
 			-DUSE_SASL_AUTH \
@@ -65,6 +65,23 @@ postfix-unpack: $(POSTFIX_BUILD_DIR)/.configured
 $(POSTFIX_BUILD_DIR)/.built: $(POSTFIX_BUILD_DIR)/.configured
 	rm -f $(POSTFIX_BUILD_DIR)/.built
 	$(MAKE) -C $(POSTFIX_BUILD_DIR)
+	(cd $(POSTFIX_BUILD_DIR); \
+		sed -i 's/fmt/\/opt\/bin\/fmt/g' postfix-install; \
+		sed -i 's/cmp/\/opt\/bin\/cmp/g' postfix-install; \
+		rm -f conf/LICENSE; \
+		cp LICENSE conf/; \
+		rm -f README_FILES/RELEASE_NOTES; \
+		cp RELEASE_NOTES README_FILES/; \
+		cd html; \
+		rm -f defer.8.html; \
+		cp bounce.8.html defer.8.html; \
+		rm -f mailq.1.html; \
+		cp sendmail.1.html mailq.1.html; \
+		rm -f newaliases.1.html; \
+		cp sendmail.1.html newaliases.1.html; \
+		rm -f trace.8.html; \
+		cp bounce.8.html trace.8.html; \
+	)
 	touch $(POSTFIX_BUILD_DIR)/.built
 
 postfix: $(POSTFIX_BUILD_DIR)/.built
@@ -72,30 +89,30 @@ postfix: $(POSTFIX_BUILD_DIR)/.built
 $(POSTFIX_BUILD_DIR)/.staged: $(POSTFIX_BUILD_DIR)/.built
 	rm -f $(POSTFIX_BUILD_DIR)/.staged
 #	$(MAKE) -C $(POSTFIX_BUILD_DIR) DESTDIR=$(STAGING_DIR) install
-	echo "The makefile target 'postfix-stage' is still empty."
+	echo "The makefile target 'postfix-stage' is not available."
 	touch $(POSTFIX_BUILD_DIR)/.staged
 
 postfix-stage: $(POSTFIX_BUILD_DIR)/.staged
 
 $(POSTFIX_IPK): $(POSTFIX_BUILD_DIR)/.built
 	echo "The makefile target 'postfix-ipk' is still empty."
-#	rm -rf $(POSTFIX_IPK_DIR) $(BUILD_DIR)/postfix_*_armeb.ipk
-#	$(MAKE) -C $(POSTFIX_BUILD_DIR) DESTDIR=$(POSTFIX_IPK_DIR) install
+	rm -rf $(POSTFIX_IPK_DIR) $(BUILD_DIR)/postfix_*_armeb.ipk
+	$(MAKE) -C $(POSTFIX_BUILD_DIR) install_root=$(POSTFIX_IPK_DIR) upgrade
 #	install -d $(POSTFIX_IPK_DIR)/opt/etc/
 #	install -m 755 $(POSTFIX_SOURCE_DIR)/postfix.conf $(POSTFIX_IPK_DIR)/opt/etc/postfix.conf
 #	install -d $(POSTFIX_IPK_DIR)/opt/etc/init.d
 #	install -m 755 $(POSTFIX_SOURCE_DIR)/rc.postfix $(POSTFIX_IPK_DIR)/opt/etc/init.d/SXXpostfix
-#	install -d $(POSTFIX_IPK_DIR)/CONTROL
-#	install -m 644 $(POSTFIX_SOURCE_DIR)/control $(POSTFIX_IPK_DIR)/CONTROL/control
+	install -d $(POSTFIX_IPK_DIR)/CONTROL
+	install -m 644 $(POSTFIX_SOURCE_DIR)/control $(POSTFIX_IPK_DIR)/CONTROL/control
 #	install -m 644 $(POSTFIX_SOURCE_DIR)/postinst $(POSTFIX_IPK_DIR)/CONTROL/postinst
 #	install -m 644 $(POSTFIX_SOURCE_DIR)/prerm $(POSTFIX_IPK_DIR)/CONTROL/prerm
-#	echo $(POSTFIX_CONFFILES) | sed -e 's/ /\n/g' > $(POSTFIX_IPK_DIR)/CONTROL/conffiles
-#	cd $(BUILD_DIR); $(IPKG_BUILD) $(POSTFIX_IPK_DIR)
+	echo $(POSTFIX_CONFFILES) | sed -e 's/ /\n/g' > $(POSTFIX_IPK_DIR)/CONTROL/conffiles
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(POSTFIX_IPK_DIR)
 
 postfix-ipk: $(POSTFIX_IPK)
 
 postfix-clean:
-	-$(MAKE) -C $(POSTFIX_BUILD_DIR) clean
+	-$(MAKE) -C $(POSTFIX_BUILD_DIR) tidy
 
 postfix-dirclean:
 	rm -rf $(BUILD_DIR)/$(POSTFIX_DIR) $(POSTFIX_BUILD_DIR) $(POSTFIX_IPK_DIR) $(POSTFIX_IPK)
