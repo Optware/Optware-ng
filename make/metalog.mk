85a08439c9303be3489618e75d581f30931d732b
ingeba <ingeba@1f1dca69-5516-0410-b834-e485effd04c3>
2005-02-03 16:13:33 +0000

    Fixed wrong path for metalog startup script as standalone server

--- make/metalog.mk
+++ make/metalog.mk
@@ -153,7 +153,7 @@ $(METALOG_IPK): $(METALOG_BUILD_DIR)/.built
 	install -d $(METALOG_IPK_DIR)/opt/etc/
 	install -m 755 $(METALOG_SOURCE_DIR)/metalog.conf $(METALOG_IPK_DIR)/opt/etc/metalog.conf
 	install -d $(METALOG_IPK_DIR)/opt/doc/metalog
-	install -m 755 $(METALOG_SOURCE_DIR)/S01metalog $(METALOG_IPK_DIR)/opt/doc/S01metalog
+	install -m 755 $(METALOG_SOURCE_DIR)/S01metalog $(METALOG_IPK_DIR)/opt/doc/metalog
 	# Make log directory on HD
 	install -d $(METALOG_IPK_DIR)/opt/var/log
 	install -d $(METALOG_IPK_DIR)/CONTROL
