ce2f326f0e4c055752463bcd8345de8217584dd5
djf <djf@1f1dca69-5516-0410-b834-e485effd04c3>
2005-02-05 00:51:52 +0000

    initial checkin - still undergoing tuning - *not* ready for testing

---
+++ trunk/make/mutt.mk
@@ -0,0 +1,180 @@
+###########################################################
+#
+# mutt
+#
+###########################################################
+
+#
+# MUTT_VERSION, MUTT_SITE and MUTT_SOURCE define
+# the upstream location of the source code for the package.
+# MUTT_DIR is the directory which is created when the source
+# archive is unpacked.
+# MUTT_UNZIP is the command used to unzip the source.
+# It is usually "zcat" (for .gz) or "bzcat" (for .bz2)
+#
+# You should change all these variables to suit your package.
+#
+MUTT_SITE=ftp://ftp.mutt.org/mutt/devel
+MUTT_VERSION=1.5.7
+MUTT_SOURCE=mutt-$(MUTT_VERSION)i.tar.gz
+MUTT_DIR=mutt-$(MUTT_VERSION)
+MUTT_UNZIP=zcat
+
+#
+# MUTT_IPK_VERSION should be incremented when the ipk changes.
+#
+MUTT_IPK_VERSION=1
+
+#
+# MUTT_CONFFILES should be a list of user-editable files
+#MUTT_CONFFILES=/opt/etc/mutt.conf /opt/etc/init.d/SXXmutt
+
+#
+# MUTT_PATCHES should list any patches, in the the order in
+# which they should be applied to the source code.
+#
+#MUTT_PATCHES=$(MUTT_SOURCE_DIR)/configure.patch
+
+#
+# If the compilation of the package requires additional
+# compilation or linking flags, then list them here.
+#
+MUTT_CPPFLAGS=
+MUTT_LDFLAGS=
+
+#
+# MUTT_BUILD_DIR is the directory in which the build is done.
+# MUTT_SOURCE_DIR is the directory which holds all the
+# patches and ipkg control files.
+# MUTT_IPK_DIR is the directory in which the ipk is built.
+# MUTT_IPK is the name of the resulting ipk files.
+#
+# You should not change any of these variables.
+#
+MUTT_BUILD_DIR=$(BUILD_DIR)/mutt
+MUTT_SOURCE_DIR=$(SOURCE_DIR)/mutt
+MUTT_IPK_DIR=$(BUILD_DIR)/mutt-$(MUTT_VERSION)-ipk
+MUTT_IPK=$(BUILD_DIR)/mutt_$(MUTT_VERSION)-$(MUTT_IPK_VERSION)_armeb.ipk
+
+#
+# This is the dependency on the source code.  If the source is missing,
+# then it will be fetched from the site using wget.
+#
+$(DL_DIR)/$(MUTT_SOURCE):
+	$(WGET) -P $(DL_DIR) $(MUTT_SITE)/$(MUTT_SOURCE)
+
+#
+# The source code depends on it existing within the download directory.
+# This target will be called by the top level Makefile to download the
+# source code's archive (.tar.gz, .bz2, etc.)
+#
+mutt-source: $(DL_DIR)/$(MUTT_SOURCE) $(MUTT_PATCHES)
+
+#
+# This target unpacks the source code in the build directory.
+# If the source archive is not .tar.gz or .tar.bz2, then you will need
+# to change the commands here.  Patches to the source code are also
+# applied in this target as required.
+#
+# This target also configures the build within the build directory.
+# Flags such as LDFLAGS and CPPFLAGS should be passed into configure
+# and NOT $(MAKE) below.  Passing it to configure causes configure to
+# correctly BUILD the Makefile with the right paths, where passing it
+# to Make causes it to override the default search paths of the compiler.
+#
+# If the compilation of the package requires other packages to be staged
+# first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
+#
+$(MUTT_BUILD_DIR)/.configured: $(DL_DIR)/$(MUTT_SOURCE) $(MUTT_PATCHES)
+#	$(MAKE) <bar>-stage <baz>-stage
+	rm -rf $(BUILD_DIR)/$(MUTT_DIR) $(MUTT_BUILD_DIR)
+	$(MUTT_UNZIP) $(DL_DIR)/$(MUTT_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+#	cat $(MUTT_PATCHES) | patch -d $(BUILD_DIR)/$(MUTT_DIR) -p1
+	mv $(BUILD_DIR)/$(MUTT_DIR) $(MUTT_BUILD_DIR)
+	# change mutt.h and lib.h to find posix1_lim.h in <bits/...>
+	sed -e 's:posix1_lim.h:bits/posix1_lim.h:g' $(MUTT_BUILD_DIR)/mutt.h > $(MUTT_BUILD_DIR)/mutt.h.new
+	sed -e 's:posix1_lim.h:bits/posix1_lim.h:g' $(MUTT_BUILD_DIR)/lib.h  > $(MUTT_BUILD_DIR)/lib.h.new
+	mv $(MUTT_BUILD_DIR)/mutt.h.new $(MUTT_BUILD_DIR)/mutt.h
+	mv $(MUTT_BUILD_DIR)/lib.h.new  $(MUTT_BUILD_DIR)/lib.h
+	(cd $(MUTT_BUILD_DIR); \
+		$(TARGET_CONFIGURE_OPTS) \
+		CPPFLAGS="$(STAGING_CPPFLAGS) $(MUTT_CPPFLAGS)" \
+		LDFLAGS="$(STAGING_LDFLAGS) $(MUTT_LDFLAGS)" \
+		./configure \
+		--build=$(GNU_HOST_NAME) \
+		--host=$(GNU_TARGET_NAME) \
+		--target=$(GNU_TARGET_NAME) \
+		--prefix=/opt \
+		--disable-nls \
+		--with-mailpath=/opt/var/spool/mail \
+	)
+	touch $(MUTT_BUILD_DIR)/.configured
+
+mutt-unpack: $(MUTT_BUILD_DIR)/.configured
+
+#
+# This builds the actual binary.
+#
+$(MUTT_BUILD_DIR)/.built: $(MUTT_BUILD_DIR)/.configured
+	rm -f $(MUTT_BUILD_DIR)/.built
+	$(MAKE) -C $(MUTT_BUILD_DIR)
+	touch $(MUTT_BUILD_DIR)/.built
+
+#
+# This is the build convenience target.
+#
+mutt: $(MUTT_BUILD_DIR)/.built
+
+#
+# If you are building a library, then you need to stage it too.
+#
+$(MUTT_BUILD_DIR)/.staged: $(MUTT_BUILD_DIR)/.built
+	rm -f $(MUTT_BUILD_DIR)/.staged
+	$(MAKE) -C $(MUTT_BUILD_DIR) DESTDIR=$(STAGING_DIR) install
+	touch $(MUTT_BUILD_DIR)/.staged
+
+mutt-stage: $(MUTT_BUILD_DIR)/.staged
+
+#
+# This builds the IPK file.
+#
+# Binaries should be installed into $(MUTT_IPK_DIR)/opt/sbin or $(MUTT_IPK_DIR)/opt/bin
+# (use the location in a well-known Linux distro as a guide for choosing sbin or bin).
+# Libraries and include files should be installed into $(MUTT_IPK_DIR)/opt/{lib,include}
+# Configuration files should be installed in $(MUTT_IPK_DIR)/opt/etc/mutt/...
+# Documentation files should be installed in $(MUTT_IPK_DIR)/opt/doc/mutt/...
+# Daemon startup scripts should be installed in $(MUTT_IPK_DIR)/opt/etc/init.d/S??mutt
+#
+# You may need to patch your application to make it use these locations.
+#
+$(MUTT_IPK): $(MUTT_BUILD_DIR)/.built
+	rm -rf $(MUTT_IPK_DIR) $(BUILD_DIR)/mutt_*_armeb.ipk
+	$(MAKE) -C $(MUTT_BUILD_DIR) DESTDIR=$(MUTT_IPK_DIR) install-strip
+#	install -d $(MUTT_IPK_DIR)/opt/etc/
+#	install -m 755 $(MUTT_SOURCE_DIR)/mutt.conf $(MUTT_IPK_DIR)/opt/etc/mutt.conf
+#	install -d $(MUTT_IPK_DIR)/opt/etc/init.d
+#	install -m 755 $(MUTT_SOURCE_DIR)/rc.mutt $(MUTT_IPK_DIR)/opt/etc/init.d/SXXmutt
+	install -d $(MUTT_IPK_DIR)/CONTROL
+	install -m 644 $(MUTT_SOURCE_DIR)/control $(MUTT_IPK_DIR)/CONTROL/control
+#	install -m 644 $(MUTT_SOURCE_DIR)/postinst $(MUTT_IPK_DIR)/CONTROL/postinst
+#	install -m 644 $(MUTT_SOURCE_DIR)/prerm $(MUTT_IPK_DIR)/CONTROL/prerm
+#	echo $(MUTT_CONFFILES) | sed -e 's/ /\n/g' > $(MUTT_IPK_DIR)/CONTROL/conffiles
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(MUTT_IPK_DIR)
+
+#
+# This is called from the top level makefile to create the IPK file.
+#
+mutt-ipk: $(MUTT_IPK)
+
+#
+# This is called from the top level makefile to clean all of the built files.
+#
+mutt-clean:
+	-$(MAKE) -C $(MUTT_BUILD_DIR) clean
+
+#
+# This is called from the top level makefile to clean all dynamically created
+# directories.
+#
+mutt-dirclean:
+	rm -rf $(BUILD_DIR)/$(MUTT_DIR) $(MUTT_BUILD_DIR) $(MUTT_IPK_DIR) $(MUTT_IPK)
