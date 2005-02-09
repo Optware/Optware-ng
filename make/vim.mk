fd32be9b9535968112745e617753d371b70855d5
paulhargreaves <paulhargreaves@1f1dca69-5516-0410-b834-e485effd04c3>
2005-02-09 17:45:31 +0000

    Initial version of vim

---
+++ make/vim.mk
@@ -0,0 +1,186 @@
+###########################################################
+#
+# vim
+#
+###########################################################
+
+# You must replace "vim" and "VIM" with the lower case name and
+# upper case name of your new package.  Some places below will say
+# "Do not change this" - that does not include this global change,
+# which must always be done to ensure we have unique names.
+
+#
+# VIM_VERSION, VIM_SITE and VIM_SOURCE define
+# the upstream location of the source code for the package.
+# VIM_DIR is the directory which is created when the source
+# archive is unpacked.
+# VIM_UNZIP is the command used to unzip the source.
+# It is usually "zcat" (for .gz) or "bzcat" (for .bz2)
+#
+# You should change all these variables to suit your package.
+#
+VIM_SITE=ftp://ftp.vim.org/pub/vim/unix
+VIM_VERSION=6.3
+VIM_SOURCE=vim-$(VIM_VERSION).tar.bz2
+VIM_DIR=vim-$(VIM_VERSION)
+VIM_UNZIP=bzcat
+
+#
+# VIM_IPK_VERSION should be incremented when the ipk changes.
+#
+VIM_IPK_VERSION=1
+
+#
+# VIM_CONFFILES should be a list of user-editable files
+#VIM_CONFFILES=/opt/etc/vim.conf /opt/etc/init.d/SXXvim
+VIM_CONFFILES=""
+
+#
+# VIM_PATCHES should list any patches, in the the order in
+# which they should be applied to the source code.
+#
+#VIM_PATCHES=$(VIM_SOURCE_DIR)/configure.patch
+
+#
+# If the compilation of the package requires additional
+# compilation or linking flags, then list them here.
+#
+VIM_CPPFLAGS=
+VIM_LDFLAGS=
+
+#
+# VIM_BUILD_DIR is the directory in which the build is done.
+# VIM_SOURCE_DIR is the directory which holds all the
+# patches and ipkg control files.
+# VIM_IPK_DIR is the directory in which the ipk is built.
+# VIM_IPK is the name of the resulting ipk files.
+#
+# You should not change any of these variables.
+#
+VIM_BUILD_DIR=$(BUILD_DIR)/vim
+VIM_SOURCE_DIR=$(SOURCE_DIR)/vim
+VIM_IPK_DIR=$(BUILD_DIR)/vim-$(VIM_VERSION)-ipk
+VIM_IPK=$(BUILD_DIR)/vim_$(VIM_VERSION)-$(VIM_IPK_VERSION)_armeb.ipk
+
+#
+# This is the dependency on the source code.  If the source is missing,
+# then it will be fetched from the site using wget.
+#
+$(DL_DIR)/$(VIM_SOURCE):
+	$(WGET) -P $(DL_DIR) $(VIM_SITE)/$(VIM_SOURCE)
+
+#
+# The source code depends on it existing within the download directory.
+# This target will be called by the top level Makefile to download the
+# source code's archive (.tar.gz, .bz2, etc.)
+#
+vim-source: $(DL_DIR)/$(VIM_SOURCE) $(VIM_PATCHES)
+
+#
+# This target unpacks the source code in the build directory.
+# If the source archive is not .tar.gz or .tar.bz2, then you will need
+# to change the commands here.  Patches to the source code are also
+# applied in this target as required.
+#
+# This target also configures the build within the build directory.
+# Flags such as LDFLAGS and CPPFLAGS should be passed into configure
+# and NOT $(MAKE) below.  Passing it to configure causes configure to
+# correctly BUILD the Makefile with the right paths, where passing it
+# to Make causes it to override the default search paths of the compiler.
+#
+# If the compilation of the package requires other packages to be staged
+# first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
+#
+$(VIM_BUILD_DIR)/.configured: $(DL_DIR)/$(VIM_SOURCE) $(VIM_PATCHES)
+	# $(MAKE) <bar>-stage <baz>-stage
+	rm -rf $(BUILD_DIR)/$(VIM_DIR) $(VIM_BUILD_DIR)
+	$(VIM_UNZIP) $(DL_DIR)/$(VIM_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+	# Rename the directory since they seem to include version numbers
+	# in the unpacked file. 
+	mv $(BUILD_DIR)/`echo $(VIM_DIR)|sed 's/[-.]//g'` $(BUILD_DIR)/$(VIM_DIR)
+	#cat $(VIM_PATCHES) | patch -d $(BUILD_DIR)/$(VIM_DIR) -p1
+	mv $(BUILD_DIR)/$(VIM_DIR) $(VIM_BUILD_DIR)
+	(cd $(VIM_BUILD_DIR)/src; \
+		$(TARGET_CONFIGURE_OPTS) \
+		CPPFLAGS="$(STAGING_CPPFLAGS) $(VIM_CPPFLAGS)" \
+		CFLAGS="$(STAGING_CPPFLAGS) $(VIM_CPPFLAGS)" \
+		LDFLAGS="$(STAGING_LDFLAGS) $(VIM_LDFLAGS)" \
+		LIBS="$(STAGING_LDFLAGS) $(VIM_LDFLAGS)" \
+		./configure \
+		--build=$(GNU_HOST_NAME) \
+		--host=$(GNU_TARGET_NAME) \
+		--target=$(GNU_TARGET_NAME) \
+		--prefix=/opt \
+		--disable-nls \
+	)
+	touch $(VIM_BUILD_DIR)/.configured
+
+vim-unpack: $(VIM_BUILD_DIR)/.configured
+
+#
+# This builds the actual binary.
+#
+$(VIM_BUILD_DIR)/.built: $(VIM_BUILD_DIR)/.configured
+	rm -f $(VIM_BUILD_DIR)/.built
+	$(MAKE) -C $(VIM_BUILD_DIR)
+	touch $(VIM_BUILD_DIR)/.built
+
+#
+# This is the build convenience target.
+#
+vim: $(VIM_BUILD_DIR)/.built
+
+#
+# If you are building a library, then you need to stage it too.
+#
+$(VIM_BUILD_DIR)/.staged: $(VIM_BUILD_DIR)/.built
+	rm -f $(VIM_BUILD_DIR)/.staged
+	$(MAKE) -C $(VIM_BUILD_DIR) DESTDIR=$(STAGING_DIR) install
+	touch $(VIM_BUILD_DIR)/.staged
+
+vim-stage: $(VIM_BUILD_DIR)/.staged
+
+#
+# This builds the IPK file.
+#
+# Binaries should be installed into $(VIM_IPK_DIR)/opt/sbin or $(VIM_IPK_DIR)/opt/bin
+# (use the location in a well-known Linux distro as a guide for choosing sbin or bin).
+# Libraries and include files should be installed into $(VIM_IPK_DIR)/opt/{lib,include}
+# Configuration files should be installed in $(VIM_IPK_DIR)/opt/etc/vim/...
+# Documentation files should be installed in $(VIM_IPK_DIR)/opt/doc/vim/...
+# Daemon startup scripts should be installed in $(VIM_IPK_DIR)/opt/etc/init.d/S??vim
+#
+# You may need to patch your application to make it use these locations.
+#
+$(VIM_IPK): $(VIM_BUILD_DIR)/.built
+	rm -rf $(VIM_IPK_DIR) $(BUILD_DIR)/vim_*_armeb.ipk
+	cd $(VIM_BUILD_DIR)/src
+	$(MAKE) -C $(VIM_BUILD_DIR) DESTDIR=$(VIM_IPK_DIR) install
+	# Fix the $VIM directory
+	mv $(VIM_IPK_DIR)/opt/share/vim $(VIM_IPK_DIR)/opt/share/vim-temp
+	mv $(VIM_IPK_DIR)/opt/share/vim-temp/vim* $(VIM_IPK_DIR)/opt/share/vim
+	rm -rf $(VIM_IPK_DIR)/opt/share/vim-temp
+	install -d $(VIM_IPK_DIR)/CONTROL
+	install -m 644 $(VIM_SOURCE_DIR)/control $(VIM_IPK_DIR)/CONTROL/control
+	#install -m 644 $(VIM_SOURCE_DIR)/prerm $(VIM_IPK_DIR)/CONTROL/prerm
+	#install -m 644 $(VIM_SOURCE_DIR)/postinst $(VIM_IPK_DIR)/CONTROL/postinst
+	echo $(VIM_CONFFILES) | sed -e 's/ /\n/g' > $(VIM_IPK_DIR)/CONTROL/conffiles
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(VIM_IPK_DIR)
+
+#
+# This is called from the top level makefile to create the IPK file.
+#
+vim-ipk: $(VIM_IPK)
+
+#
+# This is called from the top level makefile to clean all of the built files.
+#
+vim-clean:
+	-$(MAKE) -C $(VIM_BUILD_DIR) clean
+
+#
+# This is called from the top level makefile to clean all dynamically created
+# directories.
+#
+vim-dirclean:
+	rm -rf $(BUILD_DIR)/$(VIM_DIR) $(VIM_BUILD_DIR) $(VIM_IPK_DIR) $(VIM_IPK)
---
+++ make/xmail.mk
@@ -0,0 +1,209 @@
+###########################################################
+#
+# xmail
+#
+###########################################################
+
+# You must replace "xmail" and "XMAIL" with the lower case name and
+# upper case name of your new package.  Some places below will say
+# "Do not change this" - that does not include this global change,
+# which must always be done to ensure we have unique names.
+
+# paulhar NOTES
+# 1) When the package is removed, I can't understand why it doesn't remove
+#    everything it installed. e.g /opt/var/MailRoot
+# 2) For any form of logging to work syslogd needs replacing; the native one
+#    doesn't appear to write any logs. This should be double-checked by someone
+#    other than me since I've just replaced mine :)
+
+#
+# XMAIL_VERSION, XMAIL_SITE and XMAIL_SOURCE define
+# the upstream location of the source code for the package.
+# XMAIL_DIR is the directory which is created when the source
+# archive is unpacked.
+# XMAIL_UNZIP is the command used to unzip the source.
+# It is usually "zcat" (for .gz) or "bzcat" (for .bz2)
+#
+# You should change all these variables to suit your package.
+#
+XMAIL_SITE=http://www.xmailserver.org/
+XMAIL_VERSION=1.21
+XMAIL_SOURCE=xmail-$(XMAIL_VERSION).tar.gz
+XMAIL_DIR=xmail-$(XMAIL_VERSION)
+XMAIL_UNZIP=zcat
+
+#
+# XMAIL_IPK_VERSION should be incremented when the ipk changes.
+#
+XMAIL_IPK_VERSION=1
+
+#
+# XMAIL_CONFFILES should be a list of user-editable files
+#XMAIL_CONFFILES=/opt/etc/xmail.conf /opt/etc/init.d/S70xmail
+XMAIL_CONFFILES=
+
+#
+# XMAIL_PATCHES should list any patches, in the the order in
+# which they should be applied to the source code.
+#
+#XMAIL_PATCHES=$(XMAIL_SOURCE_DIR)/configure.patch
+
+#
+# If the compilation of the package requires additional
+# compilation or linking flags, then list them here.
+#
+XMAIL_CPPFLAGS=
+XMAIL_LDFLAGS=-R /opt/lib
+
+#
+# XMAIL_BUILD_DIR is the directory in which the build is done.
+# XMAIL_SOURCE_DIR is the directory which holds all the
+# patches and ipkg control files.
+# XMAIL_IPK_DIR is the directory in which the ipk is built.
+# XMAIL_IPK is the name of the resulting ipk files.
+#
+# You should not change any of these variables.
+#
+XMAIL_BUILD_DIR=$(BUILD_DIR)/xmail
+XMAIL_SOURCE_DIR=$(SOURCE_DIR)/xmail
+XMAIL_IPK_DIR=$(BUILD_DIR)/xmail-$(XMAIL_VERSION)-ipk
+XMAIL_IPK=$(BUILD_DIR)/xmail_$(XMAIL_VERSION)-$(XMAIL_IPK_VERSION)_armeb.ipk
+
+#
+# This is the dependency on the source code.  If the source is missing,
+# then it will be fetched from the site using wget.
+#
+$(DL_DIR)/$(XMAIL_SOURCE):
+	$(WGET) -P $(DL_DIR) $(XMAIL_SITE)/$(XMAIL_SOURCE)
+
+#
+# The source code depends on it existing within the download directory.
+# This target will be called by the top level Makefile to download the
+# source code's archive (.tar.gz, .bz2, etc.)
+#
+xmail-source: $(DL_DIR)/$(XMAIL_SOURCE) $(XMAIL_PATCHES)
+
+#
+# This target unpacks the source code in the build directory.
+# If the source archive is not .tar.gz or .tar.bz2, then you will need
+# to change the commands here.  Patches to the source code are also
+# applied in this target as required.
+#
+# This target also configures the build within the build directory.
+# Flags such as LDFLAGS and CPPFLAGS should be passed into configure
+# and NOT $(MAKE) below.  Passing it to configure causes configure to
+# correctly BUILD the Makefile with the right paths, where passing it
+# to Make causes it to override the default search paths of the compiler.
+#
+# If the compilation of the package requires other packages to be staged
+# first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
+#
+$(XMAIL_BUILD_DIR)/.configured: $(DL_DIR)/$(XMAIL_SOURCE) $(XMAIL_PATCHES)
+	# The dependency is already installed as part of the core installation
+	# but is here just in case. Uncomment if needed
+	# $(MAKE) libstdc++-stage
+	rm -rf $(BUILD_DIR)/$(XMAIL_DIR) $(XMAIL_BUILD_DIR)
+	$(XMAIL_UNZIP) $(DL_DIR)/$(XMAIL_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+	# cat $(XMAIL_PATCHES) | patch -d $(BUILD_DIR)/$(XMAIL_DIR) -p1
+	mv $(BUILD_DIR)/$(XMAIL_DIR) $(XMAIL_BUILD_DIR)
+	# Fix the compilation so that /opt/lib is in the path
+	sed -ie 's/lpthread/lpthread -Wl,-rpath,\/opt\/lib/' $(XMAIL_BUILD_DIR)/Makefile.lnx
+	(cd $(XMAIL_BUILD_DIR); \
+		$(TARGET_CONFIGURE_OPTS) \
+		CPPFLAGS="$(STAGING_CPPFLAGS) $(XMAIL_CPPFLAGS)" \
+		LDFLAGS="$(STAGING_LDFLAGS) $(XMAIL_LDFLAGS)" \
+	)
+	touch $(XMAIL_BUILD_DIR)/.configured
+
+xmail-unpack: $(XMAIL_BUILD_DIR)/.configured
+
+#
+# This builds the actual binary.
+#
+$(XMAIL_BUILD_DIR)/.built: $(XMAIL_BUILD_DIR)/.configured
+	rm -f $(XMAIL_BUILD_DIR)/.built
+	#$(MAKE) -C $(XMAIL_BUILD_DIR)
+	$(MAKE) -C $(XMAIL_BUILD_DIR) -f Makefile.lnx
+	# Lower the ulimit
+	sed -ie 's/ulimit -c 20000/ulimit -c 10000/' $(XMAIL_BUILD_DIR)/xmail
+	# Reduce the number of threads xmail uses
+	sed -ie 's/XMAIL_CMD_LINE=""/XMAIL_CMD_LINE="-SX 1 -Qn 1 -Yt 1 -Ln 1 -PX 1 -CX 1"/' $(XMAIL_BUILD_DIR)/xmail
+	# Change the hard-coded directory to MailRoot
+	sed -ie 's/var\/MailRoot/opt\/var\/MailRoot/' $(XMAIL_BUILD_DIR)/xmail
+	touch $(XMAIL_BUILD_DIR)/.built
+
+#
+# This is the build convenience target.
+#
+xmail: $(XMAIL_BUILD_DIR)/.built
+
+#
+# If you are building a library, then you need to stage it too.
+#
+$(XMAIL_BUILD_DIR)/.staged: $(XMAIL_BUILD_DIR)/.built
+	rm -f $(XMAIL_BUILD_DIR)/.staged
+	$(MAKE) -C $(XMAIL_BUILD_DIR) DESTDIR=$(STAGING_DIR) install
+	touch $(XMAIL_BUILD_DIR)/.staged
+
+xmail-stage: $(XMAIL_BUILD_DIR)/.staged
+
+#
+# This builds the IPK file.
+#
+# Binaries should be installed into $(XMAIL_IPK_DIR)/opt/sbin or $(XMAIL_IPK_DIR)/opt/bin
+# (use the location in a well-known Linux distro as a guide for choosing sbin or bin).
+# Libraries and include files should be installed into $(XMAIL_IPK_DIR)/opt/{lib,include}
+# Configuration files should be installed in $(XMAIL_IPK_DIR)/opt/etc/xmail/...
+# Documentation files should be installed in $(XMAIL_IPK_DIR)/opt/doc/xmail/...
+# Daemon startup scripts should be installed in $(XMAIL_IPK_DIR)/opt/etc/init.d/S??xmail
+#
+# You may need to patch your application to make it use these locations.
+#
+$(XMAIL_IPK): $(XMAIL_BUILD_DIR)/.built
+	rm -rf $(XMAIL_IPK_DIR) $(BUILD_DIR)/xmail_*_armeb.ipk
+	#$(MAKE) -C $(XMAIL_BUILD_DIR) DESTDIR=$(XMAIL_IPK_DIR) install
+	# Main configuration and temporary stores - the MailRoot folder
+	install -d $(XMAIL_IPK_DIR)/opt/var/MailRoot
+	cp -R $(XMAIL_BUILD_DIR)/MailRoot/* $(XMAIL_IPK_DIR)/opt/var/MailRoot
+	#chown root $(XMAIL_IPK_DIR)/opt/var/MailRoot
+	#chgrp root $(XMAIL_IPK_DIR)/opt/var/MailRoot
+	install -m 755 $(XMAIL_BUILD_DIR)/xmail $(XMAIL_IPK_DIR)/opt/var/MailRoot
+	chmod 700 $(XMAIL_IPK_DIR)/opt/var/MailRoot
+	# The binaries (/opt/bin)
+	install -d $(XMAIL_IPK_DIR)/opt/bin
+	install -m 755 $(XMAIL_BUILD_DIR)/bin/CtrlClnt $(XMAIL_IPK_DIR)/opt/bin
+	install -m 700 $(XMAIL_BUILD_DIR)/bin/MkUsers $(XMAIL_IPK_DIR)/opt/bin
+	install -m 700 $(XMAIL_BUILD_DIR)/bin/sendmail $(XMAIL_IPK_DIR)/opt/bin
+	install -m 700 $(XMAIL_BUILD_DIR)/bin/XMail $(XMAIL_IPK_DIR)/opt/bin
+	install -m 700 $(XMAIL_BUILD_DIR)/bin/XMCrypt $(XMAIL_IPK_DIR)/opt/bin
+	# The docs (/opt/doc)
+	install -d $(XMAIL_IPK_DIR)/opt/doc/xmail
+	install -m 755 $(XMAIL_BUILD_DIR)/docs/Readme.txt $(XMAIL_IPK_DIR)/opt/doc/xmail
+	install -m 755 $(XMAIL_BUILD_DIR)/docs/Readme.html $(XMAIL_IPK_DIR)/opt/doc/xmail
+	# rc  (/opt/etc/init.d)
+	# This is handled by the postinst script
+	# Rest of the stuff
+	install -d $(XMAIL_IPK_DIR)/CONTROL
+	install -m 644 $(XMAIL_SOURCE_DIR)/control $(XMAIL_IPK_DIR)/CONTROL/control
+	install -m 644 $(XMAIL_SOURCE_DIR)/postinst $(XMAIL_IPK_DIR)/CONTROL/postinst
+	install -m 644 $(XMAIL_SOURCE_DIR)/prerm $(XMAIL_IPK_DIR)/CONTROL/prerm
+	echo $(XMAIL_CONFFILES) | sed -e 's/ /\n/g' > $(XMAIL_IPK_DIR)/CONTROL/conffiles
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(XMAIL_IPK_DIR)
+
+#
+# This is called from the top level makefile to create the IPK file.
+#
+xmail-ipk: $(XMAIL_IPK)
+
+#
+# This is called from the top level makefile to clean all of the built files.
+#
+xmail-clean:
+	-$(MAKE) -C $(XMAIL_BUILD_DIR) -f Makefile.lnx clean
+
+#
+# This is called from the top level makefile to clean all dynamically created
+# directories.
+#
+xmail-dirclean:
+	rm -rf $(BUILD_DIR)/$(XMAIL_DIR) $(XMAIL_BUILD_DIR) $(XMAIL_IPK_DIR) $(XMAIL_IPK)
