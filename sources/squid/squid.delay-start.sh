0c47755ad28c62a3c23c09414ebab18966a7be0a
kin-kwok <kin-kwok@1f1dca69-5516-0410-b834-e485effd04c3>
2005-02-18 04:54:59 +0000

    add more comment

--- sources/squid/squid.delay-start.sh
+++ sources/squid/squid.delay-start.sh
@@ -3,10 +3,13 @@
 # wait 300s and start squid.
 #
 # Normally, the cache dir is on the data partition.
-# The data partition is not writable when it boot up.
+# The data partition is not writable when it boot up,
+# if you have quota enable.
 # If you start the squid right away, it will
 # quit and report 'no premission' to write cache.
 # The dely-start script is used to start squid with delay.
+
+# if you have a large HD, you may need more than 300s.
 
 echo "wait 300 seconds:"
 sleep 300
