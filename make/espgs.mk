1d5fed1e649627904fd8ab35add7464238476484
garpinc <garpinc@1f1dca69-5516-0410-b834-e485effd04c3>
2005-02-25 17:27:00 +0000

    native printing attempt

---
+++ make/espgs.mk
@@ -0,0 +1,175 @@
+###########################################################
+#
+# espgs
+#
+###########################################################
+
+# You must replace "espgs" and "ESPGS" with the lower case name and
+# upper case name of your new package.  Some places below will say
+# "Do not change this" - that does not include this global change,
+# which must always be done to ensure we have unique names.
+
+#
+# ESPGS_VERSION, ESPGS_SITE and ESPGS_SOURCE define
+# the upstream location of the source code for the package.
+# ESPGS_DIR is the directory which is created when the source
+# archive is unpacked.
+# ESPGS_UNZIP is the command used to unzip the source.
+# It is usually "zcat" (for .gz) or "bzcat" (for .bz2)
+#
+# You should change all these variables to suit your package.
+#
+ESPGS_SITE=http://ftp.easysw.com/pub/ghostscript/test
+ESPGS_VERSION=8.15rc2
+ESPGS_SOURCE=espgs-$(ESPGS_VERSION)-source.tar.bz2
+ESPGS_DIR=espgs-$(ESPGS_VERSION)
+ESPGS_UNZIP=bzcat
+
+#
+# ESPGS_IPK_VERSION should be incremented when the ipk changes.
+#
+ESPGS_IPK_VERSION=1
+
+#
+# ESPGS_CONFFILES should be a list of user-editable files
+ESPGS_CONFFILES=/opt/etc/espgs.conf /opt/etc/init.d/SXXespgs
+
+#
+## ESPGS_PATCHES should list any patches, in the the order in
+# which they should be applied to the source code.
+#
+ESPGS_PATCHES=$(ESPGS_SOURCE_DIR)/patch
+
+#
+# If the compilation of the package requires additional
+# compilation or linking flags, then list them here.
+#
+ESPGS_CPPFLAGS=
+ESPGS_LDFLAGS=
+
+#
+# ESPGS_BUILD_DIR is the directory in which the build is done.
+# ESPGS_SOURCE_DIR is the directory which holds all the
+# patches and ipkg control files.
+# ESPGS_IPK_DIR is the directory in which the ipk is built.
+# ESPGS_IPK is the name of the resulting ipk files.
+#
+# You should not change any of these variables.
+#
+ESPGS_BUILD_DIR=$(BUILD_DIR)/espgs
+ESPGS_SOURCE_DIR=$(SOURCE_DIR)/espgs
+ESPGS_IPK_DIR=$(BUILD_DIR)/espgs-$(ESPGS_VERSION)-ipk
+ESPGS_IPK=$(BUILD_DIR)/espgs_$(ESPGS_VERSION)-$(ESPGS_IPK_VERSION)_armeb.ipk
+
+#
+# This is the dependency on the source code.  If the source is missing,
+# then it will be fetched from the site using wget.
+#
+$(DL_DIR)/$(ESPGS_SOURCE):
+	$(WGET) -P $(DL_DIR) $(ESPGS_SITE)/$(ESPGS_SOURCE)
+
+#
+# The source code depends on it existing within the download directory.
+# This target will be called by the top level Makefile to download the
+# source code's archive (.tar.gz, .bz2, etc.)
+#
+#espgs-source: $(DL_DIR)/$(ESPGS_SOURCE) $(ESPGS_PATCHES)
+
+#
+# This target unpacks the source code in the build directory.
+# If the source archive is not .tar.gz or .tar.bz2, then you will need
+# to change the commands here.  Patches to the source code are also
+# applied in this target as required.
+#
+# This target also configures the build within the build directory.
+# Flags such as LDFLAGS and CPPFLAGS should be passed into configure
+# and NOT $(MAKE) below.  Passing it to configure causes configure to
+# correctly BUILD the Makefile with the right paths, where passing it
+# to Make causes it to override the default search paths of the compiler.
+#
+# If the compilation of the package requires other packages to be staged
+## first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
+#
+$(ESPGS_BUILD_DIR)/.configured: $(DL_DIR)/$(ESPGS_SOURCE) $(ESPGS_PATCHES)
+	$(MAKE) libjpeg-stage zlib-stage libpng-stage
+	rm -rf $(BUILD_DIR)/$(ESPGS_DIR) $(ESPGS_BUILD_DIR)
+	$(ESPGS_UNZIP) $(DL_DIR)/$(ESPGS_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+	cat $(ESPGS_PATCHES) | patch -d $(BUILD_DIR)/$(ESPGS_DIR) -p1
+	mv $(BUILD_DIR)/$(ESPGS_DIR) $(ESPGS_BUILD_DIR)
+	(cd $(ESPGS_BUILD_DIR); \
+		ln -s src/unix-gcc.mak Makefile ; \
+		mkdir obj; \
+		$(TARGET_CONFIGURE_OPTS) \
+		CPPFLAGS="$(STAGING_CPPFLAGS) $(ESPGS_CPPFLAGS)" \
+		LDFLAGS="$(STAGING_LDFLAGS) $(ESPGS_LDFLAGS)" \
+		$(MAKE) obj/arch.h ; \
+		cp obj/arch.h obj/arch.h.orig; \
+		cp $(ESPGS_SOURCE_DIR)/arch.h obj/arch.h; \
+		$(MAKE) obj/genconf obj/echogs; \
+		ln -s ../../builds/libjpeg jpeg; \
+		ln -s ../../builds/zlib zlib; \
+		ln -s ../../builds/libpng libpng; \
+	)
+	touch $(ESPGS_BUILD_DIR)/.configured
+
+espgs-unpack: $(ESPGS_BUILD_DIR)/.configured
+
+#
+# This builds the actual binary.
+#
+$(ESPGS_BUILD_DIR)/.built: $(ESPGS_BUILD_DIR)/.configured
+	rm -f $(ESPGS_BUILD_DIR)/.built
+	$(MAKE) prefix=/opt CC=$(TARGET_CC) LD=$(TARGET_LD) -C $(ESPGS_BUILD_DIR)
+	touch $(ESPGS_BUILD_DIR)/.built
+
+#
+# This is the build convenience target.
+#
+espgs: $(ESPGS_BUILD_DIR)/.built
+
+#
+# If you are building a library, then you need to stage it too.
+#
+$(ESPGS_BUILD_DIR)/.staged: $(ESPGS_BUILD_DIR)/.built
+	rm -f $(ESPGS_BUILD_DIR)/.staged
+	$(MAKE) -C $(ESPGS_BUILD_DIR) DESTDIR=$(STAGING_DIR) install
+	touch $(ESPGS_BUILD_DIR)/.staged
+
+espgs-stage: $(ESPGS_BUILD_DIR)/.staged
+
+#
+# This builds the IPK file.
+#
+# Binaries should be installed into $(ESPGS_IPK_DIR)/opt/sbin or $(ESPGS_IPK_DIR)/opt/bin
+# (use the location in a well-known Linux distro as a guide for choosing sbin or bin).
+# Libraries and include files should be installed into $(ESPGS_IPK_DIR)/opt/{lib,include}
+# Configuration files should be installed in $(ESPGS_IPK_DIR)/opt/etc/espgs/...
+# Documentation files should be installed in $(ESPGS_IPK_DIR)/opt/doc/espgs/...
+# Daemon startup scripts should be installed in $(ESPGS_IPK_DIR)/opt/etc/init.d/S??espgs
+#
+# You may need to patch your application to make it use these locations.
+#
+$(ESPGS_IPK): $(ESPGS_BUILD_DIR)/.built
+	rm -rf $(ESPGS_IPK_DIR) $(BUILD_DIR)/espgs_*_armeb.ipk
+	$(MAKE) -C $(ESPGS_BUILD_DIR) prefix=$(ESPGS_IPK_DIR)/opt DESTDIR=$(ESPGS_IPK_DIR) install
+	install -d $(ESPGS_IPK_DIR)/CONTROL
+	install -m 644 $(ESPGS_SOURCE_DIR)/control $(ESPGS_IPK_DIR)/CONTROL/control
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(ESPGS_IPK_DIR)
+
+#
+# This is called from the top level makefile to create the IPK file.
+#
+espgs-ipk: $(ESPGS_IPK)
+
+#
+# This is called from the top level makefile to clean all of the built files.
+#
+espgs-clean:
+	-$(MAKE) -C $(ESPGS_BUILD_DIR) clean
+
+#
+# This is called from the top level makefile to clean all dynamically created
+# directories.
+#
+espgs-dirclean:
+	rm -rf $(BUILD_DIR)/$(ESPGS_DIR) $(ESPGS_BUILD_DIR) $(ESPGS_IPK_DIR) $(ESPGS_IPK)
---
+++ make/gimp-print.mk
@@ -0,0 +1,172 @@
+###########################################################
+#
+# gimp-print
+#
+###########################################################
+
+# You must replace "gimp-print" and "GIMP-PRINT" with the lower case name and
+# upper case name of your new package.  Some places below will say
+# "Do not change this" - that does not include this global change,
+# which must always be done to ensure we have unique names.
+
+#
+# GIMP-PRINT_VERSION, GIMP-PRINT_SITE and GIMP-PRINT_SOURCE define
+# the upstream location of the source code for the package.
+# GIMP-PRINT_DIR is the directory which is created when the source
+# archive is unpacked.
+# GIMP-PRINT_UNZIP is the command used to unzip the source.
+# It is usually "bzcat" (for .bz2) or "bbzcat" (for .bz2)
+#
+# You should change all these variables to suit your package.
+#
+GIMP-PRINT_SITE=http://unc.dl.sourceforge.net/sourceforge/gimp-print
+GIMP-PRINT_VERSION=5.0.0-beta2
+GIMP-PRINT_SOURCE=gimp-print-$(GIMP-PRINT_VERSION).tar.bz2
+GIMP-PRINT_DIR=gimp-print-$(GIMP-PRINT_VERSION)
+GIMP-PRINT_UNZIP=bzcat
+
+#
+# GIMP-PRINT_IPK_VERSION should be incremented when the ipk changes.
+#
+GIMP-PRINT_IPK_VERSION=1
+
+#
+# GIMP-PRINT_CONFFILES should be a list of user-editable files
+GIMP-PRINT_CONFFILES=/opt/etc/gimp-print.conf /opt/etc/init.d/SXXgimp-print
+
+#
+## GIMP-PRINT_PATCHES should list any patches, in the the order in
+# which they should be applied to the source code.
+#
+#GIMP-PRINT_PATCHES=$(GIMP-PRINT_SOURCE_DIR)/configure.patch
+
+#
+# If the compilation of the package requires additional
+# compilation or linking flags, then list them here.
+#
+GIMP-PRINT_CPPFLAGS=
+GIMP-PRINT_LDFLAGS=
+
+#
+# GIMP-PRINT_BUILD_DIR is the directory in which the build is done.
+# GIMP-PRINT_SOURCE_DIR is the directory which holds all the
+# patches and ipkg control files.
+# GIMP-PRINT_IPK_DIR is the directory in which the ipk is built.
+# GIMP-PRINT_IPK is the name of the resulting ipk files.
+#
+# You should not change any of these variables.
+#
+GIMP-PRINT_BUILD_DIR=$(BUILD_DIR)/gimp-print
+GIMP-PRINT_SOURCE_DIR=$(SOURCE_DIR)/gimp-print
+GIMP-PRINT_IPK_DIR=$(BUILD_DIR)/gimp-print-$(GIMP-PRINT_VERSION)-ipk
+GIMP-PRINT_IPK=$(BUILD_DIR)/gimp-print_$(GIMP-PRINT_VERSION)-$(GIMP-PRINT_IPK_VERSION)_armeb.ipk
+
+#
+# This is the dependency on the source code.  If the source is missing,
+# then it will be fetched from the site using wget.
+#
+$(DL_DIR)/$(GIMP-PRINT_SOURCE):
+	$(WGET) -P $(DL_DIR) $(GIMP-PRINT_SITE)/$(GIMP-PRINT_SOURCE)
+
+#
+# The source code depends on it existing within the download directory.
+# This target will be called by the top level Makefile to download the
+# source code's archive (.tar.bz2, .bz2, etc.)
+#
+#gimp-print-source: $(DL_DIR)/$(GIMP-PRINT_SOURCE) $(GIMP-PRINT_PATCHES)
+
+#
+# This target unpacks the source code in the build directory.
+# If the source archive is not .tar.bz2 or .tar.bz2, then you will need
+# to change the commands here.  Patches to the source code are also
+# applied in this target as required.
+#
+# This target also configures the build within the build directory.
+# Flags such as LDFLAGS and CPPFLAGS should be passed into configure
+# and NOT $(MAKE) below.  Passing it to configure causes configure to
+# correctly BUILD the Makefile with the right paths, where passing it
+# to Make causes it to override the default search paths of the compiler.
+#
+# If the compilation of the package requires other packages to be staged
+## first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
+#
+$(GIMP-PRINT_BUILD_DIR)/.configured: $(DL_DIR)/$(GIMP-PRINT_SOURCE) $(GIMP-PRINT_PATCHES)
+#	$(MAKE) <bar>-stage <baz>-stage
+	rm -rf $(BUILD_DIR)/$(GIMP-PRINT_DIR) $(GIMP-PRINT_BUILD_DIR)
+	$(GIMP-PRINT_UNZIP) $(DL_DIR)/$(GIMP-PRINT_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+#	cat $(GIMP-PRINT_PATCHES) | patch -d $(BUILD_DIR)/$(GIMP-PRINT_DIR) -p1
+	mv $(BUILD_DIR)/$(GIMP-PRINT_DIR) $(GIMP-PRINT_BUILD_DIR)
+	(cd $(GIMP-PRINT_BUILD_DIR); \
+		$(TARGET_CONFIGURE_OPTS) \
+		CPPFLAGS="$(STAGING_CPPFLAGS) $(GIMP-PRINT_CPPFLAGS)" \
+		LDFLAGS="$(STAGING_LDFLAGS) $(GIMP-PRINT_LDFLAGS)" \
+		./configure \
+		--build=$(GNU_HOST_NAME) \
+		--host=$(GNU_TARGET_NAME) \
+		--target=$(GNU_TARGET_NAME) \
+		--prefix=/opt \
+		--disable-nls \
+	)
+	touch $(GIMP-PRINT_BUILD_DIR)/.configured
+
+gimp-print-unpack: $(GIMP-PRINT_BUILD_DIR)/.configured
+
+#
+# This builds the actual binary.
+#
+$(GIMP-PRINT_BUILD_DIR)/.built: $(GIMP-PRINT_BUILD_DIR)/.configured
+	rm -f $(GIMP-PRINT_BUILD_DIR)/.built
+	$(MAKE) -C $(GIMP-PRINT_BUILD_DIR)
+	touch $(GIMP-PRINT_BUILD_DIR)/.built
+
+#
+# This is the build convenience target.
+#
+gimp-print: $(GIMP-PRINT_BUILD_DIR)/.built
+
+#
+# If you are building a library, then you need to stage it too.
+#
+$(GIMP-PRINT_BUILD_DIR)/.staged: $(GIMP-PRINT_BUILD_DIR)/.built
+	rm -f $(GIMP-PRINT_BUILD_DIR)/.staged
+	$(MAKE) -C $(GIMP-PRINT_BUILD_DIR) DESTDIR=$(STAGING_DIR) install
+	touch $(GIMP-PRINT_BUILD_DIR)/.staged
+
+gimp-print-stage: $(GIMP-PRINT_BUILD_DIR)/.staged
+
+#
+# This builds the IPK file.
+#
+# Binaries should be installed into $(GIMP-PRINT_IPK_DIR)/opt/sbin or $(GIMP-PRINT_IPK_DIR)/opt/bin
+# (use the location in a well-known Linux distro as a guide for choosing sbin or bin).
+# Libraries and include files should be installed into $(GIMP-PRINT_IPK_DIR)/opt/{lib,include}
+# Configuration files should be installed in $(GIMP-PRINT_IPK_DIR)/opt/etc/gimp-print/...
+# Documentation files should be installed in $(GIMP-PRINT_IPK_DIR)/opt/doc/gimp-print/...
+# Daemon startup scripts should be installed in $(GIMP-PRINT_IPK_DIR)/opt/etc/init.d/S??gimp-print
+#
+# You may need to patch your application to make it use these locations.
+#
+$(GIMP-PRINT_IPK): $(GIMP-PRINT_BUILD_DIR)/.built
+	rm -rf $(GIMP-PRINT_IPK_DIR) $(BUILD_DIR)/gimp-print_*_armeb.ipk
+	$(MAKE) -C $(GIMP-PRINT_BUILD_DIR) DESTDIR=$(GIMP-PRINT_IPK_DIR) install-strip
+	install -d $(GIMP-PRINT_IPK_DIR)/CONTROL
+	install -m 644 $(GIMP-PRINT_SOURCE_DIR)/control $(GIMP-PRINT_IPK_DIR)/CONTROL/control
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(GIMP-PRINT_IPK_DIR)
+
+#
+# This is called from the top level makefile to create the IPK file.
+#
+gimp-print-ipk: $(GIMP-PRINT_IPK)
+
+#
+# This is called from the top level makefile to clean all of the built files.
+#
+gimp-print-clean:
+	-$(MAKE) -C $(GIMP-PRINT_BUILD_DIR) clean
+
+#
+# This is called from the top level makefile to clean all dynamically created
+# directories.
+#
+gimp-print-dirclean:
+	rm -rf $(BUILD_DIR)/$(GIMP-PRINT_DIR) $(GIMP-PRINT_BUILD_DIR) $(GIMP-PRINT_IPK_DIR) $(GIMP-PRINT_IPK)
