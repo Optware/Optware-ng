1706797073fdd9e0abfb61fa97c2c297c5e170b4
jp30 <jp30@1f1dca69-5516-0410-b834-e485effd04c3>
2005-02-27 09:18:56 +0000

    initial checkin: xchat

---
+++ make/xchat.mk
@@ -0,0 +1,204 @@
+###########################################################
+#
+# xchat
+#
+###########################################################
+
+#
+# XCHAT_VERSION, XCHAT_SITE and XCHAT_SOURCE define
+# the upstream location of the source code for the package.
+# XCHAT_DIR is the directory which is created when the source
+# archive is unpacked.
+# XCHAT_UNZIP is the command used to unzip the source.
+# It is usually "zcat" (for .gz) or "bzcat" (for .bz2)
+#
+XCHAT_SITE=http://www.xchat.org/files/source/2.4
+XCHAT_VERSION=2.4.1
+XCHAT_SOURCE=xchat-$(XCHAT_VERSION).tar.bz2
+XCHAT_DIR=xchat-$(XCHAT_VERSION)
+XCHAT_UNZIP=bzcat
+XCHAT_MAINTAINER=Josh Parsons <jbparsons@ucdavis.edu>
+XCHAT_DESCRIPTION=Gtk+ based IRC client
+XCHAT_SECTION=lib
+XCHAT_PRIORITY=optional
+XCHAT_DEPENDS=gtk
+
+#
+# XCHAT_IPK_VERSION should be incremented when the ipk changes.
+#
+XCHAT_IPK_VERSION=1
+
+#
+# XCHAT_LOCALES defines which locales get installed
+#
+XCHAT_LOCALES=
+
+#
+# XCHAT_CONFFILES should be a list of user-editable files
+#XCHAT_CONFFILES=/opt/etc/xchat.conf /opt/etc/init.d/SXXxchat
+
+#
+# XCHAT_PATCHES should list any patches, in the the order in
+# which they should be applied to the source code.
+#
+XCHAT_PATCHES=
+
+#
+# If the compilation of the package requires additional
+# compilation or linking flags, then list them here.
+#
+XCHAT_CPPFLAGS=-I$(STAGING_INCLUDE_DIR)/glib-2.0 -I$(STAGING_LIB_DIR)/glib-2.0/include -I$(STAGING_INCLUDE_DIR)/freetype2 -I$(STAGING_INCLUDE_DIR)/gtk-2.0 -I$(STAGING_LIB_DIR)/gtk-2.0/include -I$(STAGING_INCLUDE_DIR)/pango-1.0 -I$(STAGING_INCLUDE_DIR)/atk-1.0
+XCHAT_LDFLAGS=-Wl,-rpath-link=$(STAGING_LIB_DIR)
+
+#
+# XCHAT_BUILD_DIR is the directory in which the build is done.
+# XCHAT_SOURCE_DIR is the directory which holds all the
+# patches and ipkg control files.
+# XCHAT_IPK_DIR is the directory in which the ipk is built.
+# XCHAT_IPK is the name of the resulting ipk files.
+#
+# You should not change any of these variables.
+#
+XCHAT_BUILD_DIR=$(BUILD_DIR)/xchat
+XCHAT_SOURCE_DIR=$(SOURCE_DIR)/xchat
+XCHAT_IPK_DIR=$(BUILD_DIR)/xchat-$(XCHAT_VERSION)-ipk
+XCHAT_IPK=$(BUILD_DIR)/xchat_$(XCHAT_VERSION)-$(XCHAT_IPK_VERSION)_armeb.ipk
+
+#
+# Automatically create a ipkg control file
+#
+$(XCHAT_IPK_DIR)/CONTROL/control:
+	@install -d $(XCHAT_IPK_DIR)/CONTROL
+	@rm -f $@
+	@echo "Package: xchat" >>$@
+	@echo "Architecture: armeb" >>$@
+	@echo "Priority: $(XCHAT_PRIORITY)" >>$@
+	@echo "Section: $(XCHAT_SECTION)" >>$@
+	@echo "Version: $(XCHAT_VERSION)-$(XCHAT_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(XCHAT_MAINTAINER)" >>$@
+	@echo "Source: $(XCHAT_SITE)/$(XCHAT_SOURCE)" >>$@
+	@echo "Description: $(XCHAT_DESCRIPTION)" >>$@
+	@echo "Depends: $(XCHAT_DEPENDS)" >>$@
+
+#
+# This is the dependency on the source code.  If the source is missing,
+# then it will be fetched from the site using wget.
+#
+$(DL_DIR)/$(XCHAT_SOURCE):
+	$(WGET) -P $(DL_DIR) $(XCHAT_SITE)/$(XCHAT_SOURCE)
+
+#
+# The source code depends on it existing within the download directory.
+# This target will be called by the top level Makefile to download the
+# source code's archive (.tar.gz, .bz2, etc.)
+#
+xchat-source: $(DL_DIR)/$(XCHAT_SOURCE) $(XCHAT_PATCHES)
+
+#
+# This target unpacks the source code in the build directory.
+# If the source archive is not .tar.gz or .tar.bz2, then you will need
+# to change the commands here.  Patches to the source code are also
+# applied in this target as required.
+#
+# This target also configures the build within the build directory.
+# Flags such as LDFLAGS and CPPFLAGS should be passed into configure
+# and NOT $(MAKE) below.  Passing it to configure causes configure to
+# correctly BUILD the Makefile with the right paths, where passing it
+# to Make causes it to override the default search paths of the compiler.
+#
+# If the compilation of the package requires other packages to be staged
+# first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
+#
+$(XCHAT_BUILD_DIR)/.configured: $(DL_DIR)/$(XCHAT_SOURCE) \
+		$(STAGING_LIB_DIR)/libgtk-x11-2.0.so \
+		$(XCHAT_PATCHES)
+	rm -rf $(BUILD_DIR)/$(XCHAT_DIR) $(XCHAT_BUILD_DIR)
+	$(XCHAT_UNZIP) $(DL_DIR)/$(XCHAT_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+	mv $(BUILD_DIR)/$(XCHAT_DIR) $(XCHAT_BUILD_DIR)
+	#cat $(XCHAT_PATCHES) |patch -p0 -d$(XCHAT_BUILD_DIR)
+	(cd $(XCHAT_BUILD_DIR); \
+		$(TARGET_CONFIGURE_OPTS) \
+		PATH="$(STAGING_DIR)/opt/bin:$$PATH" \
+		CPPFLAGS="$(STAGING_CPPFLAGS) $(XCHAT_CPPFLAGS)" \
+		LDFLAGS="$(STAGING_LDFLAGS) $(XCHAT_LDFLAGS)" \
+		PKG_CONFIG_PATH="$(STAGING_LIB_DIR)/pkgconfig" \
+		PKG_CONFIG_LIBDIR="$(STAGING_LIB_DIR)/pkgconfig" \
+		./configure \
+		--build=$(GNU_HOST_NAME) \
+		--host=$(GNU_TARGET_NAME) \
+		--target=$(GNU_TARGET_NAME) \
+		--x-includes=$(STAGING_INCLUDE_DIR) \
+		--x-libraries=$(STAGING_LIB_DIR) \
+		--prefix=/opt \
+		--disable-gtk-doc \
+		--disable-static \
+		--disable-glibtest \
+		--disable-gtktest \
+		--disable-perl \
+		--disable-python \
+		--disable-tcl \
+	)
+	touch $(XCHAT_BUILD_DIR)/.configured
+
+xchat-unpack: $(XCHAT_BUILD_DIR)/.configured
+
+#
+# This builds the actual binary.  You should change the target to refer
+# directly to the main binary which is built.
+#
+$(XCHAT_BUILD_DIR)/.built: $(XCHAT_BUILD_DIR)/.configured
+	rm -f $(XCHAT_BUILD_DIR)/.built
+	cp $(XCHAT_SOURCE_DIR)/inline_pngs.h $(XCHAT_BUILD_DIR)/src/pixmaps
+	$(MAKE) -C $(XCHAT_BUILD_DIR)
+	touch $(XCHAT_BUILD_DIR)/.built
+
+#
+# You should change the dependency to refer directly to the main binary
+# which is built.
+#
+xchat: $(XCHAT_BUILD_DIR)/.built
+
+#
+# If you are building a library, then you need to stage it too.
+#
+$(STAGING_DIR)/opt/lib/libxchat.so: $(XCHAT_BUILD_DIR)/.built
+	$(MAKE) -C $(XCHAT_BUILD_DIR) install-strip DESTDIR=$(STAGING_DIR)
+	rm -rf $(STAGING_DIR)/opt/lib/libxchat.la
+
+xchat-stage: $(STAGING_DIR)/opt/lib/libxchat.so
+
+#
+# This builds the IPK file.
+#
+# Binaries should be installed into $(XCHAT_IPK_DIR)/opt/sbin or $(XCHAT_IPK_DIR)/opt/bin
+# (use the location in a well-known Linux distro as a guide for choosing sbin or bin).
+# Libraries and include files should be installed into $(XCHAT_IPK_DIR)/opt/{lib,include}
+# Configuration files should be installed in $(XCHAT_IPK_DIR)/opt/etc/xchat/...
+# Documentation files should be installed in $(XCHAT_IPK_DIR)/opt/doc/xchat/...
+# Daemon startup scripts should be installed in $(XCHAT_IPK_DIR)/opt/etc/init.d/S??xchat
+#
+# You may need to patch your application to make it use these locations.
+#
+$(XCHAT_IPK): $(XCHAT_BUILD_DIR)/.built
+	rm -rf $(XCHAT_IPK_DIR) $(BUILD_DIR)/xchat_*_armeb.ipk
+	$(MAKE) -C $(XCHAT_BUILD_DIR) DESTDIR=$(XCHAT_IPK_DIR) install-strip
+	$(MAKE) $(XCHAT_IPK_DIR)/CONTROL/control
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(XCHAT_IPK_DIR)
+
+#
+# This is called from the top level makefile to create the IPK file.
+#
+xchat-ipk: $(XCHAT_IPK)
+
+#
+# This is called from the top level makefile to clean all of the built files.
+#
+xchat-clean:
+	-$(MAKE) -C $(XCHAT_BUILD_DIR) clean
+
+#
+# This is called from the top level makefile to clean all dynamically created
+# directories.
+#
+xchat-dirclean:
+	rm -rf $(BUILD_DIR)/$(XCHAT_DIR) $(XCHAT_BUILD_DIR) $(XCHAT_IPK_DIR) $(XCHAT_IPK)
