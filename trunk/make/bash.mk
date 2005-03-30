6e007df9e61de4ca4533958d05c14dbaa1027305
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2005-03-30 16:02:02 +0000

    Updated to generate the control file.

--- trunk/make/bash.mk
+++ trunk/make/bash.mk
@@ -9,8 +9,14 @@ BASH_VERSION=2.05b
 BASH_SOURCE=bash-$(BASH_VERSION).tar.gz
 BASH_DIR=bash-$(BASH_VERSION)
 BASH_UNZIP=zcat
+BASH_MAINTAINER=Christopher Blunck <christopher.blunck@gmail.com>
+BASH_DESCRIPTION=A bourne style shell
+BASH_SECTION=shell
+BASH_PRIORITY=optional
+BASH_DEPENDS=ncurses
+BASH_CONFLICTS=
 
-BASH_IPK_VERSION=3
+BASH_IPK_VERSION=4
 
 BASH_CPPFLAGS=
 BASH_LDFLAGS=
@@ -57,6 +63,24 @@ $(BASH_BUILD_DIR)/bash: $(BASH_BUILD_DIR)/.configured
 
 bash: termcap-stage $(BASH_BUILD_DIR)/bash
 
+#
+# This rule creates a control file for ipkg.  It is no longer
+# necessary to create a seperate control file under sources/bash
+#
+$(BASH_IPK_DIR)/CONTROL/control:
+	@install -d $(BASH_IPK_DIR)/CONTROL
+	@rm -f $@
+	@echo "Package: bash" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(BASH_PRIORITY)" >>$@
+	@echo "Section: $(BASH_SECTION)" >>$@
+	@echo "Version: $(BASH_VERSION)-$(BASH_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(BASH_MAINTAINER)" >>$@
+	@echo "Source: $(BASH_SITE)/$(BASH_SOURCE)" >>$@
+	@echo "Description: $(BASH_DESCRIPTION)" >>$@
+	@echo "Depends: $(BASH_DEPENDS)" >>$@
+	@echo "Conflicts: $(BASH_CONFLICTS)" >>$@
+
 $(BASH_IPK): $(BASH_BUILD_DIR)/bash
 	rm -rf $(BASH_IPK_DIR) $(BUILD_DIR)/bash_*_$(TARGET_ARCH).ipk
 	install -d $(BASH_IPK_DIR)/opt/bin
@@ -64,10 +88,8 @@ $(BASH_IPK): $(BASH_BUILD_DIR)/bash
 	install -d $(BASH_IPK_DIR)/opt/etc 
 	install -m 644 $(BASH_SOURCE_DIR)/profile $(BASH_IPK_DIR)/opt/etc/profile
 	install -d $(BASH_IPK_DIR)/bin
-	ln -s /opt/bin/bash \
-		$(BASH_IPK_DIR)/bin/bash
-	install -d $(BASH_IPK_DIR)/CONTROL
-	install -m 644 $(BASH_SOURCE_DIR)/control $(BASH_IPK_DIR)/CONTROL/control
+	ln -s /opt/bin/bash $(BASH_IPK_DIR)/bin/bash
+	$(MAKE) $(BASH_IPK_DIR)/CONTROL/control
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(BASH_IPK_DIR)
 
 bash-ipk: $(BASH_IPK)
