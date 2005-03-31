b118bd27a26511d42f4f265d2f5f867b2607bada
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2005-03-31 22:13:57 +0000

    Updated for control file generation.

--- make/libevent.mk
+++ make/libevent.mk
@@ -9,8 +9,14 @@ LIBEVENT_VERSION=0.9
 LIBEVENT_SOURCE=libevent-$(LIBEVENT_VERSION).tar.gz
 LIBEVENT_DIR=libevent-$(LIBEVENT_VERSION)
 LIBEVENT_UNZIP=zcat
+LIBEVENT_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
+LIBEVENT_DESCRIPTION=libevent to implement an event loop
+LIBEVENT_SECTION=libs
+LIBEVENT_PRIORITY=optional
+LIBEVENT_DEPENDS=
+LIBEVENT_CONFLICTS=
 
-LIBEVENT_IPK_VERSION=1
+LIBEVENT_IPK_VERSION=2
 
 LIBEVENT_CPPFLAGS= -fPIC
 
@@ -54,13 +60,30 @@ $(STAGING_DIR)/opt/lib/libevent.a: $(LIBEVENT_BUILD_DIR)/libevent.a
 
 libevent-stage: $(STAGING_DIR)/opt/lib/libevent.a
 
+#
+# This rule creates a control file for ipkg.  It is no longer
+# necessary to create a seperate control file under sources/libevent
+#
+$(LIBEVENT_IPK_DIR)/CONTROL/control:
+	@install -d $(LIBEVENT_IPK_DIR)/CONTROL
+	@rm -f $@
+	@echo "Package: libevent" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(LIBEVENT_PRIORITY)" >>$@
+	@echo "Section: $(LIBEVENT_SECTION)" >>$@
+	@echo "Version: $(LIBEVENT_VERSION)-$(LIBEVENT_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(LIBEVENT_MAINTAINER)" >>$@
+	@echo "Source: $(LIBEVENT_SITE)/$(LIBEVENT_SOURCE)" >>$@
+	@echo "Description: $(LIBEVENT_DESCRIPTION)" >>$@
+	@echo "Depends: $(LIBEVENT_DEPENDS)" >>$@
+	@echo "Conflicts: $(LIBEVENT_CONFLICTS)" >>$@
+
 $(LIBEVENT_IPK): $(LIBEVENT_BUILD_DIR)/libevent.a
 	install -d $(LIBEVENT_IPK_DIR)/opt/include
 	install -m 644 $(LIBEVENT_BUILD_DIR)/event.h $(LIBEVENT_IPK_DIR)/opt/include
 	install -d $(LIBEVENT_IPK_DIR)/opt/lib
 	install -m 644 $(LIBEVENT_BUILD_DIR)/libevent.a $(LIBEVENT_IPK_DIR)/opt/lib
-	install -d $(LIBEVENT_IPK_DIR)/CONTROL
-	install -m 644 $(LIBEVENT_SOURCE_DIR)/control $(LIBEVENT_IPK_DIR)/CONTROL/control
+	$(MAKE) $(LIBEVENT_IPK_DIR)/CONTROL/control
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(LIBEVENT_IPK_DIR)
 
 libevent-ipk: $(LIBEVENT_IPK)
