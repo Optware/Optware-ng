3a6854459b15c273a97780698eaf4d573192c741
dyoung8888 <dyoung8888@1f1dca69-5516-0410-b834-e485effd04c3>
2005-04-06 12:51:38 +0000

    Changed it to the new template

--- trunk/make/libcurl.mk
+++ trunk/make/libcurl.mk
@@ -18,13 +18,25 @@
 # It is usually "zcat" (for .gz) or "bzcat" (for .bz2)
 #
 # You should change all these variables to suit your package.
+# Please make sure that you add a description, and that you
+# list all your packages' dependencies, seperated by commas.
+# 
+# If you list yourself as MAINTAINER, please give a valid email
+# address, and indicate your irc nick if it cannot be easily deduced
+# from your name or email address.  If you leave MAINTAINER set to
+# "NSLU2 Linux" other developers will feel free to edit.
 #
 LIBCURL_SITE= http://curl.haxx.se/download
 LIBCURL_VERSION=7.12.2
-LIBCURL_SO_VERSION=3.0.0
 LIBCURL_SOURCE=curl-$(LIBCURL_VERSION).tar.gz
 LIBCURL_DIR=curl-$(LIBCURL_VERSION)
 LIBCURL_UNZIP=zcat
+LIBCURL_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
+LIBCURL_DESCRIPTION=Curl is a command line tool for transferring files with URL syntax, supporting FTP, FTPS, HTTP, HTTPS, GOPHER, TELNET, DICT, FILE and LDAP. Curl supports HTTPS certificates, HTTP POST, HTTP PUT, FTP uploading, kerberos, HTTP form based upload, proxies, cookies, user+password authentication, file transfer resume, http proxy tunneling and a busload of other useful tricks.
+LIBCURL_SECTION=
+LIBCURL_PRIORITY=optional
+LIBCURL_DEPENDS=
+LIBCURL_CONFLICTS=
 
 #
 # LIBCURL_IPK_VERSION should be incremented when the ipk changes.
@@ -32,10 +44,14 @@ LIBCURL_UNZIP=zcat
 LIBCURL_IPK_VERSION=1
 
 #
+# LIBCURL_CONFFILES should be a list of user-editable files
+LIBCURL_CONFFILES=#/opt/etc/libcurl.conf /opt/etc/init.d/SXXlibcurl
+
+#
 # LIBCURL_PATCHES should list any patches, in the the order in
 # which they should be applied to the source code.
 #
-#LIBCURL_PATCHES=$(LIBCURL_SOURCE_DIR)/configure.patch
+LIBCURL_PATCHES=#$(LIBCURL_SOURCE_DIR)/configure.patch
 
 #
 # If the compilation of the package requires additional
@@ -88,8 +104,10 @@ libcurl-source: $(DL_DIR)/$(LIBCURL_SOURCE) $(LIBCURL_PATCHES)
 # first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
 #
 $(LIBCURL_BUILD_DIR)/.configured: $(DL_DIR)/$(LIBCURL_SOURCE) $(LIBCURL_PATCHES)
+#	$(MAKE) <bar>-stage <baz>-stage
 	rm -rf $(BUILD_DIR)/$(LIBCURL_DIR) $(LIBCURL_BUILD_DIR)
 	$(LIBCURL_UNZIP) $(DL_DIR)/$(LIBCURL_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+#	cat $(LIBCURL_PATCHES) | patch -d $(BUILD_DIR)/$(LIBCURL_DIR) -p1
 	mv $(BUILD_DIR)/$(LIBCURL_DIR) $(LIBCURL_BUILD_DIR)
 	(cd $(LIBCURL_BUILD_DIR); \
 		$(TARGET_CONFIGURE_OPTS) \
@@ -99,42 +117,54 @@ $(LIBCURL_BUILD_DIR)/.configured: $(DL_DIR)/$(LIBCURL_SOURCE) $(LIBCURL_PATCHES)
 		--build=$(GNU_HOST_NAME) \
 		--host=$(GNU_TARGET_NAME) \
 		--target=$(GNU_TARGET_NAME) \
-		--with-random="/dev/urandom" \
 		--prefix=/opt \
+		--disable-nls \
+		--with-random="/dev/urandom" \
 	)
 	touch $(LIBCURL_BUILD_DIR)/.configured
 
 libcurl-unpack: $(LIBCURL_BUILD_DIR)/.configured
 
 #
-# This builds the actual binary.  You should change the target to refer
-# directly to the main binary which is built.
+# This builds the actual binary.
 #
-$(LIBCURL_BUILD_DIR)/lib/.libs/libcurl.la: $(LIBCURL_BUILD_DIR)/.configured
+$(LIBCURL_BUILD_DIR)/.built: $(LIBCURL_BUILD_DIR)/.configured
+	rm -f $(LIBCURL_BUILD_DIR)/.built
 	$(MAKE) -C $(LIBCURL_BUILD_DIR)
+	touch $(LIBCURL_BUILD_DIR)/.built
 
 #
-# You should change the dependency to refer directly to the main binary
-# which is built.
+# This is the build convenience target.
 #
-libcurl: $(LIBCURL_BUILD_DIR)/lib/.libs/libcurl.la
+libcurl: $(LIBCURL_BUILD_DIR)/.built
 
 #
 # If you are building a library, then you need to stage it too.
 #
-$(STAGING_DIR)/opt/lib/libcurl.so.$(LIBCURL_SO_VERSION): $(LIBCURL_BUILD_DIR)/lib/.libs/libcurl.la
-	install -d $(STAGING_DIR)/opt/include/curl
-	install -m 644 $(LIBCURL_BUILD_DIR)/include/curl/*.h $(STAGING_DIR)/opt/include/curl
-	install -d $(STAGING_DIR)/bin
-	install -m 755 $(LIBCURL_BUILD_DIR)/curl-config $(STAGING_DIR)/bin
-	install -d $(STAGING_DIR)/opt/lib
-	install -m 644 $(LIBCURL_BUILD_DIR)/lib/.libs/libcurl.a $(STAGING_DIR)/opt/lib
-	install -m 644 $(LIBCURL_BUILD_DIR)/lib/libcurl.la $(STAGING_DIR)/opt/lib
-	install -m 644 $(LIBCURL_BUILD_DIR)/lib/.libs/libcurl.so.$(LIBCURL_SO_VERSION) $(STAGING_DIR)/opt/lib
-	cd $(STAGING_DIR)/opt/lib && ln -fs libcurl.so.$(LIBCURL_SO_VERSION) libcurl.so.3
-	cd $(STAGING_DIR)/opt/lib && ln -fs libcurl.so.$(LIBCURL_SO_VERSION) libcurl.so
+$(LIBCURL_BUILD_DIR)/.staged: $(LIBCURL_BUILD_DIR)/.built
+	rm -f $(LIBCURL_BUILD_DIR)/.staged
+	$(MAKE) -C $(LIBCURL_BUILD_DIR) DESTDIR=$(STAGING_DIR) install
+	touch $(LIBCURL_BUILD_DIR)/.staged
 
-libcurl-stage: $(STAGING_DIR)/opt/lib/libcurl.so.$(LIBCURL_SO_VERSION)
+libcurl-stage: $(LIBCURL_BUILD_DIR)/.staged
+
+#
+# This rule creates a control file for ipkg.  It is no longer
+# necessary to create a seperate control file under sources/libcurl
+#
+$(LIBCURL_IPK_DIR)/CONTROL/control:
+	@install -d $(LIBCURL_IPK_DIR)/CONTROL
+	@rm -f $@
+	@echo "Package: libcurl" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(LIBCURL_PRIORITY)" >>$@
+	@echo "Section: $(LIBCURL_SECTION)" >>$@
+	@echo "Version: $(LIBCURL_VERSION)-$(LIBCURL_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(LIBCURL_MAINTAINER)" >>$@
+	@echo "Source: $(LIBCURL_SITE)/$(LIBCURL_SOURCE)" >>$@
+	@echo "Description: $(LIBCURL_DESCRIPTION)" >>$@
+	@echo "Depends: $(LIBCURL_DEPENDS)" >>$@
+	@echo "Conflicts: $(LIBCURL_CONFLICTS)" >>$@
 
 #
 # This builds the IPK file.
@@ -148,12 +178,17 @@ libcurl-stage: $(STAGING_DIR)/opt/lib/libcurl.so.$(LIBCURL_SO_VERSION)
 #
 # You may need to patch your application to make it use these locations.
 #
-$(LIBCURL_IPK): $(LIBCURL_BUILD_DIR)/lib/.libs/libcurl.la
-	rm -rf $(LIBCURL_IPK_DIR) $(LIBCURL_IPK)
+$(LIBCURL_IPK): $(LIBCURL_BUILD_DIR)/.built
+	rm -rf $(LIBCURL_IPK_DIR) $(BUILD_DIR)/libcurl_*_$(TARGET_ARCH).ipk
 	$(MAKE) -C $(LIBCURL_BUILD_DIR) DESTDIR=$(LIBCURL_IPK_DIR) install
-	rm -rf $(LIBCURL_IPK_DIR)/opt/lib/lib*.a
-	mkdir -p $(LIBCURL_IPK_DIR)/CONTROL
-	cp $(SOURCE_DIR)/libcurl/control $(LIBCURL_IPK_DIR)/CONTROL/control
+	#install -d $(LIBCURL_IPK_DIR)/opt/etc/
+	#install -m 644 $(LIBCURL_SOURCE_DIR)/libcurl.conf $(LIBCURL_IPK_DIR)/opt/etc/libcurl.conf
+	#install -d $(LIBCURL_IPK_DIR)/opt/etc/init.d
+	#install -m 755 $(LIBCURL_SOURCE_DIR)/rc.libcurl $(LIBCURL_IPK_DIR)/opt/etc/init.d/SXXlibcurl
+	$(MAKE) $(LIBCURL_IPK_DIR)/CONTROL/control
+	#install -m 755 $(LIBCURL_SOURCE_DIR)/postinst $(LIBCURL_IPK_DIR)/CONTROL/postinst
+	#install -m 755 $(LIBCURL_SOURCE_DIR)/prerm $(LIBCURL_IPK_DIR)/CONTROL/prerm
+	echo $(LIBCURL_CONFFILES) | sed -e 's/ /\n/g' > $(LIBCURL_IPK_DIR)/CONTROL/conffiles
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(LIBCURL_IPK_DIR)
 
 #
