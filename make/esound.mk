e906c26c6458df733e01c2af8a6752e21c90a216
jp30 <jp30@1f1dca69-5516-0410-b834-e485effd04c3>
2005-04-06 23:03:15 +0000

    esound: stage esd-config into STAGING/bin

--- make/esound.mk
+++ make/esound.mk
@@ -146,6 +146,7 @@ esound: $(ESOUND_BUILD_DIR)/.built
 $(ESOUND_BUILD_DIR)/.staged: $(ESOUND_BUILD_DIR)/.built
 	rm -f $(ESOUND_BUILD_DIR)/.staged
 	$(MAKE) -C $(ESOUND_BUILD_DIR) DESTDIR=$(STAGING_DIR) install
+	cp $(STAGING_DIR)/opt/bin/esd-config $(STAGING_DIR)/bin/esd-config
 	touch $(ESOUND_BUILD_DIR)/.staged
 
 esound-stage: $(ESOUND_BUILD_DIR)/.staged
