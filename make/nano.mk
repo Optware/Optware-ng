e2e1d6c777348aafbb68524aa948fea75268a1c1
ptweety <ptweety@1f1dca69-5516-0410-b834-e485effd04c3>
2005-04-11 18:50:30 +0000

    set --enable-all in configure and install nanorc

--- make/nano.mk
+++ make/nano.mk
@@ -4,85 +4,38 @@
 #
 ###########################################################
 
-#
-# NANO_VERSION, NANO_SITE and NANO_SOURCE define
-# the upstream location of the source code for the package.
-# NANO_DIR is the directory which is created when the source
-# archive is unpacked.
-# NANO_UNZIP is the command used to unzip the source.
-# It is usually "zcat" (for .gz) or "bzcat" (for .bz2)
-#
 NANO_SITE=http://www.nano-editor.org/dist/v1.2
 NANO_VERSION=1.2.4
 NANO_SOURCE=nano-$(NANO_VERSION).tar.gz
 NANO_DIR=nano-$(NANO_VERSION)
 NANO_UNZIP=zcat
 NANO_MAINTAINER=Mark Donszelmann <mark@donszelmann.com>
+NANO_DESCRIPTION=A pico like editor
 NANO_SECTION=editor
 NANO_PRIORITY=optional
 NANO_DEPENDS=ncurses
-NANO_DESCRIPTION=A pico like editor
+NANO_CONFLICTS=
 
-#
-# NANO_IPK_VERSION should be incremented when the ipk changes.
-#
-NANO_IPK_VERSION=1
+NANO_IPK_VERSION=2
 
-#
-# NANO_PATCHES should list any patches, in the the order in
-# which they should be applied to the source code.
-#
-NANO_PATCHES="$(NANO_SOURCE_DIR)/configure.patch"
+NANO_CONFFILES=/opt/etc/nanorc
 
-#
-# If the compilation of the package requires additional
-# compilation or linking flags, then list them here.
-#
+NANO_PATCHES=$(NANO_SOURCE_DIR)/configure.patch
+
 NANO_CPPFLAGS=-I$(STAGING_PREFIX)/include/ncurses
 NANO_LDFLAGS=
 
-#
-# NANO_BUILD_DIR is the directory in which the build is done.
-# NANO_SOURCE_DIR is the directory which holds all the
-# patches and ipkg control files.
-# NANO_IPK_DIR is the directory in which the ipk is built.
-# NANO_IPK is the name of the resulting ipk files.
-#
-# You should not change any of these variables.
-#
 NANO_BUILD_DIR=$(BUILD_DIR)/nano
 NANO_SOURCE_DIR=$(SOURCE_DIR)/nano
 NANO_IPK_DIR=$(BUILD_DIR)/nano-$(NANO_VERSION)-ipk
 NANO_IPK=$(BUILD_DIR)/nano_$(NANO_VERSION)-$(NANO_IPK_VERSION)_$(TARGET_ARCH).ipk
 
-#
-# This is the dependency on the source code.  If the source is missing,
-# then it will be fetched from the site using wget.
-#
 $(DL_DIR)/$(NANO_SOURCE):
 	$(WGET) -P $(DL_DIR) $(NANO_SITE)/$(NANO_SOURCE)
 
-#
-# The source code depends on it existing within the download directory.
-# This target will be called by the top level Makefile to download the
-# source code's archive (.tar.gz, .bz2, etc.)
-#
 nano-source: $(DL_DIR)/$(NANO_SOURCE) $(NANO_PATCHES)
 
-#
-# This target unpacks the source code in the build directory.
-# If the source archive is not .tar.gz or .tar.bz2, then you will need
-# to change the commands here.  Patches to the source code are also
-# applied in this target as required.
-# This target also configures the build within the build directory.
-# Flags such as LDFLAGS and CPPFLAGS should be passed into configure
-# and NOT $(MAKE) below.  Passing it to configure causes configure to
-# correctly BUILD the Makefile with the right paths, where passing it
-# to Make causes it to override the default search paths of the compiler.
-#
-# we run a fake configure and then copy the correct makefile in...
-#
-$(NANO_BUILD_DIR)/.configured: $(DL_DIR)/$(NANO_SOURCE)
+$(NANO_BUILD_DIR)/.configured: $(DL_DIR)/$(NANO_SOURCE) $(NANO_PATCHES)
 	$(MAKE) ncurses-stage
 	rm -rf $(BUILD_DIR)/$(NANO_DIR) $(NANO_BUILD_DIR)
 	$(NANO_UNZIP) $(DL_DIR)/$(NANO_SOURCE) | tar -C $(BUILD_DIR) -xvf -
@@ -97,42 +50,28 @@ $(NANO_BUILD_DIR)/.configured: $(DL_DIR)/$(NANO_SOURCE)
 		--host=$(GNU_TARGET_NAME) \
 		--target=$(GNU_TARGET_NAME) \
 		--prefix=/opt \
+		--enable-all \
+		--without-libiconv-prefix \
+		--disable-nls \
 	)
 	touch $(NANO_BUILD_DIR)/.configured
 
 nano-unpack: $(NANO_BUILD_DIR)/.configured
 
-#
-# This builds the actual binary.  You should change the target to refer
-# directly to the main binary which is built.
-#
-$(NANO_BUILD_DIR)/nano: $(NANO_BUILD_DIR)/.configured
+$(NANO_BUILD_DIR)/.built: $(NANO_BUILD_DIR)/.configured
+	rm -f $(NANO_BUILD_DIR)/.built
 	$(MAKE) -C $(NANO_BUILD_DIR)
+	touch $(NANO_BUILD_DIR)/.built
 
-#
-# These are the dependencies for the package.  Again, you should change
-# the final dependency to refer directly to the main binary which is built.
-#
-nano: ncurses $(NANO_BUILD_DIR)/nano
+nano: $(NANO_BUILD_DIR)/.built
 
-#
-# If you are building a library, then you need to stage it too.
-#
-$(STAGING_DIR)/lib/libnano.so.$(NANO_VERSION): $(NANO_BUILD_DIR)/libnano.so.$(NANO_VERSION)
-	install -d $(STAGING_DIR)/include
-	install -m 644 $(NANO_BUILD_DIR)/nano.h $(STAGING_DIR)/include
-	install -d $(STAGING_DIR)/lib
-	install -m 644 $(NANO_BUILD_DIR)/libnano.a $(STAGING_DIR)/lib
-	install -m 644 $(NANO_BUILD_DIR)/libnano.so.$(NANO_VERSION) $(STAGING_DIR)/lib
-	cd $(STAGING_DIR)/lib && ln -fs libnano.so.$(NANO_VERSION) libnano.so.1
-	cd $(STAGING_DIR)/lib && ln -fs libnano.so.$(NANO_VERSION) libnano.so
+$(NANO_BUILD_DIR)/.staged: $(NANO_BUILD_DIR)/.built
+	rm -f $(NANO_BUILD_DIR)/.staged
+	$(MAKE) -C $(NANO_BUILD_DIR) DESTDIR=$(STAGING_DIR) install
+	touch $(NANO_BUILD_DIR)/.staged
 
-nano-stage: $(STAGING_DIR)/lib/libnano.so.$(NANO_VERSION)
+nano-stage: $(NANO_BUILD_DIR)/.staged
 
-#
-# This rule creates a control file for ipkg.  It is no longer
-# necessary to create a seperate control file under sources/nano
-#
 $(NANO_IPK_DIR)/CONTROL/control:
 	@install -d $(NANO_IPK_DIR)/CONTROL
 	@rm -f $@
@@ -147,45 +86,24 @@ $(NANO_IPK_DIR)/CONTROL/control:
 	@echo "Depends: $(NANO_DEPENDS)" >>$@
 	@echo "Conflicts: $(NANO_CONFLICTS)" >>$@
 
-#
-# This builds the IPK file.
-#
-# Binaries should be installed into $(NANO_IPK_DIR)/opt/sbin or $(NANO_IPK_DIR)/opt/bin
-# (use the location in a well-known Linux distro as a guide for choosing sbin or bin).
-# Libraries and include files should be installed into $(NANO_IPK_DIR)/opt/{lib,include}
-# Configuration files should be installed in $(NANO_IPK_DIR)/opt/etc/nano/...
-# Documentation files should be installed in $(NANO_IPK_DIR)/opt/doc/nano/...
-# Daemon startup scripts should be installed in $(NANO_IPK_DIR)/opt/etc/init.d/S??nano
-#
-# You may need to patch your application to make it use these locations.
-#
-#	install -d $(NANO_IPK_DIR)/opt/etc/init.d
-#	install -m 755 $(NANO_SOURCE_DIR)/rc.nano $(NANO_IPK_DIR)/opt/etc/init.d/SXXnano
-#	install -m 644 $(NANO_SOURCE_DIR)/postinst $(NANO_IPK_DIR)/CONTROL/postinst
-#	install -m 644 $(NANO_SOURCE_DIR)/prerm $(NANO_IPK_DIR)/CONTROL/prerm
-#
-#
-$(NANO_IPK): $(NANO_BUILD_DIR)/nano
-	install -d $(NANO_IPK_DIR)/opt/bin
-	$(STRIP_COMMAND) $(NANO_BUILD_DIR)/nano -o $(NANO_IPK_DIR)/opt/bin/nano
-	install -d $(NANO_IPK_DIR)/CONTROL
+$(NANO_IPK): $(NANO_BUILD_DIR)/.built
+	rm -rf $(NANO_IPK_DIR) $(BUILD_DIR)/nano_*_$(TARGET_ARCH).ipk
+	install -d $(NANO_IPK_DIR)/opt/bin/
+	install -d $(NANO_IPK_DIR)/opt/info/
+	install -d $(NANO_IPK_DIR)/opt/man/man1/
+	install -d $(NANO_IPK_DIR)/opt/man/man5/
+	$(MAKE) -C $(NANO_BUILD_DIR) DESTDIR=$(NANO_IPK_DIR) install
+	$(STRIP_COMMAND) $(NANO_IPK_DIR)/opt/bin/nano
+	install -d $(NANO_IPK_DIR)/opt/etc/
+	install -m 644 $(NANO_BUILD_DIR)/nanorc.sample $(NANO_IPK_DIR)/opt/etc/nanorc
 	$(MAKE) $(NANO_IPK_DIR)/CONTROL/control
+	echo $(NANO_CONFFILES) | sed -e 's/ /\n/g' > $(NANO_IPK_DIR)/CONTROL/conffiles
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(NANO_IPK_DIR)
 
-#
-# This is called from the top level makefile to create the IPK file.
-#
 nano-ipk: $(NANO_IPK)
 
-#
-# This is called from the top level makefile to clean all of the built files.
-#
 nano-clean:
 	-$(MAKE) -C $(NANO_BUILD_DIR) clean
 
-#
-# This is called from the top level makefile to clean all dynamically created
-# directories.
-#
 nano-dirclean:
 	rm -rf $(BUILD_DIR)/$(NANO_DIR) $(NANO_BUILD_DIR) $(NANO_IPK_DIR) $(NANO_IPK)
