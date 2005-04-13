b342a22367514d5f80a1feb3e937660ccc82d510
rpedde <rpedde@1f1dca69-5516-0410-b834-e485effd04c3>
2005-04-13 04:55:45 +0000

    Initial commit of sqlite 2.x, to coexist with sqlite3.

---
+++ make/sqlite2.mk
@@ -0,0 +1,171 @@
+###########################################################
+#
+# sqlite2
+#
+###########################################################
+
+# You must replace "sqlite2" and "SQLITE2" with the lower case name and
+# upper case name of your new package.  Some places below will say
+# "Do not change this" - that does not include this global change,
+# which must always be done to ensure we have unique names.
+
+#
+# SQLITE2_VERSION, SQLITE2_SITE and SQLITE2_SOURCE define
+# the upstream location of the source code for the package.
+# SQLITE2_DIR is the directory which is created when the source
+# archive is unpacked.
+# SQLITE2_UNZIP is the command used to unzip the source.
+# It is usually "zcat" (for .gz) or "bzcat" (for .bz2)
+#
+# You should change all these variables to suit your package.
+#
+SQLITE2_SITE=http://www.sqlite.org/
+SQLITE2_VERSION=2.8.16
+SQLITE2_SOURCE=sqlite-$(SQLITE2_VERSION).tar.gz
+SQLITE2_DIR=sqlite-$(SQLITE2_VERSION)
+SQLITE2_UNZIP=zcat
+
+#
+# SQLITE2_IPK_VERSION should be incremented when the ipk changes.
+#
+SQLITE2_IPK_VERSION=1
+
+#
+# SQLITE2_CONFFILES should be a list of user-editable files
+
+#
+# SQLITE2_PATCHES should list any patches, in the the order in
+# which they should be applied to the source code.
+#
+SQLITE2_PATCHES=$(SQLITE2_SOURCE_DIR)/configure.patch
+#
+# If the compilation of the package requires additional
+# compilation or linking flags, then list them here.
+#
+SQLITE2_CPPFLAGS=
+SQLITE2_LDFLAGS=
+
+#
+# SQLITE2_BUILD_DIR is the directory in which the build is done.
+# SQLITE2_SOURCE_DIR is the directory which holds all the
+# patches and ipkg control files.
+# SQLITE2_IPK_DIR is the directory in which the ipk is built.
+# SQLITE2_IPK is the name of the resulting ipk files.
+#
+# You should not change any of these variables.
+#
+SQLITE2_BUILD_DIR=$(BUILD_DIR)/sqlite2
+SQLITE2_SOURCE_DIR=$(SOURCE_DIR)/sqlite2
+SQLITE2_IPK_DIR=$(BUILD_DIR)/sqlite2-$(SQLITE2_VERSION)-ipk
+SQLITE2_IPK=$(BUILD_DIR)/sqlite2_$(SQLITE2_VERSION)-$(SQLITE2_IPK_VERSION)_armeb.ipk
+
+#
+# This is the dependency on the source code.  If the source is missing,
+# then it will be fetched from the site using wget.
+#
+$(DL_DIR)/$(SQLITE2_SOURCE):
+	$(WGET) -P $(DL_DIR) $(SQLITE2_SITE)/$(SQLITE2_SOURCE)
+
+#
+# The source code depends on it existing within the download directory.
+# This target will be called by the top level Makefile to download the
+# source code's archive (.tar.gz, .bz2, etc.)
+#
+sqlite2-source: $(DL_DIR)/$(SQLITE2_SOURCE) $(SQLITE2_PATCHES)
+
+#
+# This target unpacks the source code in the build directory.
+# If the source archive is not .tar.gz or .tar.bz2, then you will need
+# to change the commands here.  Patches to the source code are also
+# applied in this target as required.
+#
+# This target also configures the build within the build directory.
+# Flags such as LDFLAGS and CPPFLAGS should be passed into configure
+# and NOT $(MAKE) below.  Passing it to configure causes configure to
+# correctly BUILD the Makefile with the right paths, where passing it
+# to Make causes it to override the default search paths of the compiler.
+#
+# If the compilation of the package requires other packages to be staged
+# first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
+#
+$(SQLITE2_BUILD_DIR)/.configured: $(DL_DIR)/$(SQLITE2_SOURCE) $(SQLITE2_PATCHES)
+	$(MAKE) readline-stage
+	rm -rf $(BUILD_DIR)/$(SQLITE2_DIR) $(SQLITE2_BUILD_DIR)
+	$(SQLITE2_UNZIP) $(DL_DIR)/$(SQLITE2_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+	cat $(SQLITE2_PATCHES) | patch -d $(BUILD_DIR)/$(SQLITE2_DIR) -p1
+	mv $(BUILD_DIR)/$(SQLITE2_DIR) $(SQLITE2_BUILD_DIR)
+	(cd $(SQLITE2_BUILD_DIR); \
+		$(TARGET_CONFIGURE_OPTS) \
+		CPPFLAGS="$(STAGING_CPPFLAGS) $(SQLITE2_CPPFLAGS)" \
+		LDFLAGS="$(STAGING_LDFLAGS) $(SQLITE2_LDFLAGS)" \
+		config_TARGET_READLINE_INC=-I$(STAGING_DIR)/opt/include \
+		./configure \
+		--build=$(GNU_HOST_NAME) \
+		--host=$(GNU_TARGET_NAME) \
+		--target=$(GNU_TARGET_NAME) \
+		--prefix=/opt \
+		--disable-nls \
+	)
+	touch $(SQLITE2_BUILD_DIR)/.configured
+
+sqlite2-unpack: $(SQLITE2_BUILD_DIR)/.configured
+
+#
+# This builds the actual binary.
+#
+$(SQLITE2_BUILD_DIR)/.built: $(SQLITE2_BUILD_DIR)/.configured
+	rm -f $(SQLITE2_BUILD_DIR)/.built
+	$(MAKE) -C $(SQLITE2_BUILD_DIR)
+	touch $(SQLITE2_BUILD_DIR)/.built
+
+#
+# This is the build convenience target.
+#
+sqlite2: $(SQLITE2_BUILD_DIR)/.built
+
+#
+# If you are building a library, then you need to stage it too.
+#
+$(SQLITE2_BUILD_DIR)/.staged: $(SQLITE2_BUILD_DIR)/.built
+	rm -f $(SQLITE2_BUILD_DIR)/.staged
+	$(MAKE) -C $(SQLITE2_BUILD_DIR) DESTDIR=$(STAGING_DIR) install
+	touch $(SQLITE2_BUILD_DIR)/.staged
+
+sqlite2-stage: $(SQLITE2_BUILD_DIR)/.staged
+
+#
+# This builds the IPK file.
+#
+# Binaries should be installed into $(SQLITE2_IPK_DIR)/opt/sbin or $(SQLITE2_IPK_DIR)/opt/bin
+# (use the location in a well-known Linux distro as a guide for choosing sbin or bin).
+# Libraries and include files should be installed into $(SQLITE2_IPK_DIR)/opt/{lib,include}
+# Configuration files should be installed in $(SQLITE2_IPK_DIR)/opt/etc/sqlite2/...
+# Documentation files should be installed in $(SQLITE2_IPK_DIR)/opt/doc/sqlite2/...
+# Daemon startup scripts should be installed in $(SQLITE2_IPK_DIR)/opt/etc/init.d/S??sqlite2
+#
+# You may need to patch your application to make it use these locations.
+#
+$(SQLITE2_IPK): $(SQLITE2_BUILD_DIR)/.built
+	rm -rf $(SQLITE2_IPK_DIR) $(BUILD_DIR)/sqlite2_*_armeb.ipk
+	$(MAKE) -C $(SQLITE2_BUILD_DIR) DESTDIR=$(SQLITE2_IPK_DIR) install
+	install -d $(SQLITE2_IPK_DIR)/CONTROL
+	install -m 644 $(SQLITE2_SOURCE_DIR)/control $(SQLITE2_IPK_DIR)/CONTROL/control
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(SQLITE2_IPK_DIR)
+
+#
+# This is called from the top level makefile to create the IPK file.
+#
+sqlite2-ipk: $(SQLITE2_IPK)
+
+#
+# This is called from the top level makefile to clean all of the built files.
+#
+sqlite2-clean:
+	-$(MAKE) -C $(SQLITE2_BUILD_DIR) clean
+
+#
+# This is called from the top level makefile to clean all dynamically created
+# directories.
+#
+sqlite2-dirclean:
+	rm -rf $(BUILD_DIR)/$(SQLITE2_DIR) $(SQLITE2_BUILD_DIR) $(SQLITE2_IPK_DIR) $(SQLITE2_IPK)
