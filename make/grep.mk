51e1770f27741b5bd34080d0ef3ca1d66a9477f1
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2005-04-16 11:48:10 +0000

    Now generates the control file.

--- make/grep.mk
+++ make/grep.mk
@@ -11,8 +11,14 @@ GREP=grep-$(GREP_VERSION)
 GREP_SITE=ftp://ftp.gnu.org/pub/gnu/grep
 GREP_SOURCE=$(GREP).tar.gz
 GREP_UNZIP=zcat
+GREP_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
+GREP_DESCRIPTION=Global regular expression parser
+GREP_SECTION=util
+GREP_PRIORITY=optional
+GREP_DEPENDS=
+GREP_CONFLICTS=busybox
 
-GREP_IPK_VERSION=3
+GREP_IPK_VERSION=4
 
 GREP_IPK=$(BUILD_DIR)/grep_$(GREP_VERSION)-$(GREP_IPK_VERSION)_$(TARGET_ARCH).ipk
 GREP_IPK_DIR=$(BUILD_DIR)/grep-$(GREP_VERSION)-ipk
@@ -48,14 +54,31 @@ $(GREP_DIR)/src/grep: $(GREP_DIR)/.configured
 
 grep: $(GREP_DIR)/src/grep
 
+#
+# This rule creates a control file for ipkg.  It is no longer
+# necessary to create a seperate control file under sources/grep
+#
+$(GREP_IPK_DIR)/CONTROL/control:
+	@install -d $(GREP_IPK_DIR)/CONTROL
+	@rm -f $@
+	@echo "Package: grep" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(GREP_PRIORITY)" >>$@
+	@echo "Section: $(GREP_SECTION)" >>$@
+	@echo "Version: $(GREP_VERSION)-$(GREP_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(GREP_MAINTAINER)" >>$@
+	@echo "Source: $(GREP_SITE)/$(GREP_SOURCE)" >>$@
+	@echo "Description: $(GREP_DESCRIPTION)" >>$@
+	@echo "Depends: $(GREP_DEPENDS)" >>$@
+	@echo "Conflicts: $(GREP_CONFLICTS)" >>$@
+
 $(GREP_IPK): $(GREP_DIR)/src/grep
 	rm -rf $(GREP_IPK_DIR) $(BUILD_DIR)/grep_*_$(TARGET_ARCH).ipk
 	install -d $(GREP_IPK_DIR)/opt/bin
 	$(STRIP_COMMAND) $(GREP_DIR)/src/grep -o $(GREP_IPK_DIR)/opt/bin/grep
 	$(STRIP_COMMAND) $(GREP_DIR)/src/egrep -o $(GREP_IPK_DIR)/opt/bin/egrep
 	$(STRIP_COMMAND) $(GREP_DIR)/src/fgrep -o $(GREP_IPK_DIR)/opt/bin/fgrep
-	install -d $(GREP_IPK_DIR)/CONTROL
-	install -m 644 $(SOURCE_DIR)/grep/control $(GREP_IPK_DIR)/CONTROL/control
+	$(MAKE) $(GREP_IPK_DIR)/CONTROL/control
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(GREP_IPK_DIR)
 
 grep-ipk: $(GREP_IPK)
