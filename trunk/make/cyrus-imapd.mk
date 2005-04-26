06b794d00eb2bdf0ef605d0e4ab5edce1210eff6
ptweety <ptweety@1f1dca69-5516-0410-b834-e485effd04c3>
2005-04-26 17:44:58 +0000

    cyrus-imapd.mk generates control files; first steps for cross-compilation.

--- trunk/make/cyrus-imapd.mk
+++ trunk/make/cyrus-imapd.mk
@@ -9,16 +9,28 @@ CYRUS-IMAPD_VERSION=2.2.10
 CYRUS-IMAPD_SOURCE=cyrus-imapd-$(CYRUS-IMAPD_VERSION).tar.gz
 CYRUS-IMAPD_DIR=cyrus-imapd-$(CYRUS-IMAPD_VERSION)
 CYRUS-IMAPD_UNZIP=zcat
+CYRUS-IMAPD_MAINTAINER=Matthias Appel <private_tweety@gmx.net>
+CYRUS-IMAPD_DESCRIPTION=The Carnegie Mellon University Cyrus IMAP Server
+CYRUS-IMAPD_SECTION=util
+CYRUS-IMAPD_PRIORITY=optional
+CYRUS-IMAPD_DEPENDS=openssl, libdb, cyrus-sasl, perl
+CYRUS-IMAPD_SUGGESTS=
+CYRUS-IMAPD_CONFLICTS=
 
-CYRUS-IMAPD_IPK_VERSION=2
+CYRUS-IMAPD_IPK_VERSION=3
 
 CYRUS-IMAPD_CONFFILES=/opt/etc/cyrus.conf /opt/etc/imapd.conf /opt/etc/init.d/S59cyrus-imapd
 
-CYRUS-IMAPD_PATCHES=$(CYRUS-IMAPD_SOURCE_DIR)/perl.Makefile.in.patch $(CYRUS-IMAPD_SOURCE_DIR)/perl.Makefile.PL.patch
-#CYRUS-IMAPD_PATCHES=
+CYRUS-IMAPD_PATCHES=$(CYRUS-IMAPD_SOURCE_DIR)/cyrus.cross.patch $(CYRUS-IMAPD_SOURCE_DIR)/perl.Makefile.in.patch $(CYRUS-IMAPD_SOURCE_DIR)/perl.Makefile.PL.patch
 
 CYRUS-IMAPD_CPPFLAGS=
 CYRUS-IMAPD_LDFLAGS=
+
+ifeq ($(HOST_MACHINE),armv5b)
+  CYRUS-IMAPD_CONFIGURE_OPTS=""
+else
+  CYRUS-IMAPD_CONFIGURE_OPTS="--without-perl"
+endif
 
 CYRUS-IMAPD_BUILD_DIR=$(BUILD_DIR)/cyrus-imapd
 CYRUS-IMAPD_SOURCE_DIR=$(SOURCE_DIR)/cyrus-imapd
@@ -31,14 +43,17 @@ $(DL_DIR)/$(CYRUS-IMAPD_SOURCE):
 cyrus-imapd-source: $(DL_DIR)/$(CYRUS-IMAPD_SOURCE) $(CYRUS-IMAPD_PATCHES)
 
 $(CYRUS-IMAPD_BUILD_DIR)/.configured: $(DL_DIR)/$(CYRUS-IMAPD_SOURCE) $(CYRUS-IMAPD_PATCHES)
-	$(MAKE) libdb-stage openssl-stage e2fsprogs-stage
+	$(MAKE) libdb-stage openssl-stage
 	$(MAKE) cyrus-sasl-stage
 	rm -rf $(BUILD_DIR)/$(CYRUS-IMAPD_DIR) $(CYRUS-IMAPD_BUILD_DIR)
 	$(CYRUS-IMAPD_UNZIP) $(DL_DIR)/$(CYRUS-IMAPD_SOURCE) | tar -C $(BUILD_DIR) -xvf -
 	cat $(CYRUS-IMAPD_PATCHES) | patch -d $(BUILD_DIR)/$(CYRUS-IMAPD_DIR) -p1
 	mv $(BUILD_DIR)/$(CYRUS-IMAPD_DIR) $(CYRUS-IMAPD_BUILD_DIR)
 	(cd $(CYRUS-IMAPD_BUILD_DIR); \
+		autoconf; \
 		$(TARGET_CONFIGURE_OPTS) \
+		CC_FOR_BUILD=$(HOSTCC) \
+		BUILD_CFLAGS="$(STAGING_CPPFLAGS) $(CYRUS-IMAPD_CPPFLAGS) -I.. -I../et" \
 		CPPFLAGS="$(STAGING_CPPFLAGS) $(CYRUS-IMAPD_CPPFLAGS)" \
 		LDFLAGS="$(STAGING_LDFLAGS) $(CYRUS-IMAPD_LDFLAGS)" \
 		./configure \
@@ -59,6 +74,7 @@ $(CYRUS-IMAPD_BUILD_DIR)/.configured: $(DL_DIR)/$(CYRUS-IMAPD_SOURCE) $(CYRUS-IM
 		--with-cyrus-group=mail \
 		--with-checkapop \
 		--disable-nls \
+		$(CYRUS-IMAPD_CONFIGURE_OPTS) \
 	)
 	touch $(CYRUS-IMAPD_BUILD_DIR)/.configured
 
@@ -78,6 +94,51 @@ $(CYRUS-IMAPD_BUILD_DIR)/.staged: $(CYRUS-IMAPD_BUILD_DIR)/.built
 	touch $(CYRUS-IMAPD_BUILD_DIR)/.staged
 
 cyrus-imapd-stage: $(CYRUS-IMAPD_BUILD_DIR)/.staged
+
+$(CYRUS-IMAPD_IPK_DIR)/CONTROL/control:
+	@install -d $(CYRUS-IMAPD_IPK_DIR)/CONTROL
+	@rm -f $@
+	@echo "Package: cyrus-imapd" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(CYRUS-IMAPD_PRIORITY)" >>$@
+	@echo "Section: $(CYRUS-IMAPD_SECTION)" >>$@
+	@echo "Version: $(CYRUS-IMAPD_VERSION)-$(CYRUS-IMAPD_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(CYRUS-IMAPD_MAINTAINER)" >>$@
+	@echo "Source: $(CYRUS-IMAPD_SITE)/$(CYRUS-IMAPD_SOURCE)" >>$@
+	@echo "Description: $(CYRUS-IMAPD_DESCRIPTION)" >>$@
+	@echo "Depends: cyrus-imapd-devel (= $(CYRUS-IMAPD_VERSION)-$(CYRUS-IMAPD_IPK_VERSION))" >>$@
+	@echo "Suggests: cyrus-imapd-doc, $(CYRUS-IMAPD_SUGGESTS)" >>$@
+	@echo "Conflicts: $(CYRUS-IMAPD_CONFLICTS)" >>$@
+
+$(CYRUS-IMAPD_IPK_DIR)-doc/CONTROL/control:
+	@install -d $(CYRUS-IMAPD_IPK_DIR)-doc/CONTROL
+	@rm -f $@
+	@echo "Package: cyrus-imapd-doc" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(CYRUS-IMAPD_PRIORITY)" >>$@
+	@echo "Section: $(CYRUS-IMAPD_SECTION)" >>$@
+	@echo "Version: $(CYRUS-IMAPD_VERSION)-$(CYRUS-IMAPD_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(CYRUS-IMAPD_MAINTAINER)" >>$@
+	@echo "Source: $(CYRUS-IMAPD_SITE)/$(CYRUS-IMAPD_SOURCE)" >>$@
+	@echo "Description: $(CYRUS-IMAPD_DESCRIPTION)" >>$@
+	@echo "Depends: " >>$@
+	@echo "Suggests: man" >>$@
+	@echo "Conflicts: " >>$@
+
+$(CYRUS-IMAPD_IPK_DIR)-devel/CONTROL/control:
+	@install -d $(CYRUS-IMAPD_IPK_DIR)-devel/CONTROL
+	@rm -f $@
+	@echo "Package: cyrus-imapd-devel" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(CYRUS-IMAPD_PRIORITY)" >>$@
+	@echo "Section: $(CYRUS-IMAPD_SECTION)" >>$@
+	@echo "Version: $(CYRUS-IMAPD_VERSION)-$(CYRUS-IMAPD_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(CYRUS-IMAPD_MAINTAINER)" >>$@
+	@echo "Source: $(CYRUS-IMAPD_SITE)/$(CYRUS-IMAPD_SOURCE)" >>$@
+	@echo "Description: $(CYRUS-IMAPD_DESCRIPTION)" >>$@
+	@echo "Depends: $(CYRUS-IMAPD_DEPENDS)" >>$@
+	@echo "Suggests: $(CYRUS-IMAPD_SUGGESTS)" >>$@
+	@echo "Conflicts: $(CYRUS-IMAPD_CONFLICTS)" >>$@
 
 $(CYRUS-IMAPD_IPK): $(CYRUS-IMAPD_BUILD_DIR)/.built
 	rm -rf $(CYRUS-IMAPD_IPK_DIR)* $(BUILD_DIR)/cyrus-imapd_*_$(TARGET_ARCH).ipk
@@ -144,20 +205,23 @@ $(CYRUS-IMAPD_IPK): $(CYRUS-IMAPD_BUILD_DIR)/.built
 	mv $(CYRUS-IMAPD_IPK_DIR)/opt/man $(CYRUS-IMAPD_IPK_DIR)-doc/opt/man
 	mv $(CYRUS-IMAPD_IPK_DIR)-doc/opt/man/man8/idled.8 $(CYRUS-IMAPD_IPK_DIR)-doc/opt/man/man8/cyrus_idled.8
 	mv $(CYRUS-IMAPD_IPK_DIR)-doc/opt/man/man8/master.8 $(CYRUS-IMAPD_IPK_DIR)-doc/opt/man/man8/cyrus_master.8
-	install -d $(CYRUS-IMAPD_IPK_DIR)-doc/CONTROL
-	install -m 644 $(CYRUS-IMAPD_SOURCE_DIR)/control-doc $(CYRUS-IMAPD_IPK_DIR)-doc/CONTROL/control
+	$(MAKE) $(CYRUS-IMAPD_IPK_DIR)-doc/CONTROL/control
+#	install -d $(CYRUS-IMAPD_IPK_DIR)-doc/CONTROL
+#	install -m 644 $(CYRUS-IMAPD_SOURCE_DIR)/control-doc $(CYRUS-IMAPD_IPK_DIR)-doc/CONTROL/control
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(CYRUS-IMAPD_IPK_DIR)-doc
 
 	rm -rf $(CYRUS-IMAPD_IPK_DIR)-devel
 	install -d $(CYRUS-IMAPD_IPK_DIR)-devel/opt/lib
 	mv $(CYRUS-IMAPD_IPK_DIR)/opt/lib/*.a $(CYRUS-IMAPD_IPK_DIR)-devel/opt/lib
 	mv $(CYRUS-IMAPD_IPK_DIR)/opt/include $(CYRUS-IMAPD_IPK_DIR)-devel/opt/include
-	install -d $(CYRUS-IMAPD_IPK_DIR)-devel/CONTROL
-	install -m 644 $(CYRUS-IMAPD_SOURCE_DIR)/control-devel $(CYRUS-IMAPD_IPK_DIR)-devel/CONTROL/control
+	$(MAKE) $(CYRUS-IMAPD_IPK_DIR)-devel/CONTROL/control
+#	install -d $(CYRUS-IMAPD_IPK_DIR)-devel/CONTROL
+#	install -m 644 $(CYRUS-IMAPD_SOURCE_DIR)/control-devel $(CYRUS-IMAPD_IPK_DIR)-devel/CONTROL/control
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(CYRUS-IMAPD_IPK_DIR)-devel
 
-	install -d $(CYRUS-IMAPD_IPK_DIR)/CONTROL
-	install -m 644 $(CYRUS-IMAPD_SOURCE_DIR)/control $(CYRUS-IMAPD_IPK_DIR)/CONTROL/control
+	$(MAKE) $(CYRUS-IMAPD_IPK_DIR)/CONTROL/control
+#	install -d $(CYRUS-IMAPD_IPK_DIR)/CONTROL
+#	install -m 644 $(CYRUS-IMAPD_SOURCE_DIR)/control $(CYRUS-IMAPD_IPK_DIR)/CONTROL/control
 	install -m 644 $(CYRUS-IMAPD_SOURCE_DIR)/postinst $(CYRUS-IMAPD_IPK_DIR)/CONTROL/postinst
 #	install -m 644 $(CYRUS-IMAPD_SOURCE_DIR)/prerm $(CYRUS-IMAPD_IPK_DIR)/CONTROL/prerm
 	echo $(CYRUS-IMAPD_CONFFILES) | sed -e 's/ /\n/g' > $(CYRUS-IMAPD_IPK_DIR)/CONTROL/conffiles
