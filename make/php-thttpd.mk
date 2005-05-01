1ec5f88e5b52450ab29c501c88d9affa101a0402
jp30 <jp30@1f1dca69-5516-0410-b834-e485effd04c3>
2005-05-01 01:23:15 +0000

    patch to build a working php-thttpd with thread safety

--- make/php-thttpd.mk
+++ make/php-thttpd.mk
@@ -57,7 +57,7 @@ PHP_THTTPD_CONFFILES=/opt/etc/init.d/S80thttpd /opt/etc/thttpd.conf
 # PHP_THTTPD_PATCHES should list any patches, in the the order in
 # which they should be applied to the source code.
 #
-PHP_THTTPD_LIBPHP_PATCHES=$(PHP_THTTPD_SOURCE_DIR)/php-5.0.3.patch $(PHP_THTTPD_SOURCE_DIR)/config.m4.patch $(PHP_THTTPD_SOURCE_DIR)/thttpd.c.patch
+PHP_THTTPD_LIBPHP_PATCHES=$(PHP_THTTPD_SOURCE_DIR)/php-5.0.3.patch $(PHP_THTTPD_SOURCE_DIR)/config.m4.patch $(PHP_THTTPD_SOURCE_DIR)/thttpd.c.patch $(PHP_THTTPD_SOURCE_DIR)/zts-without-threaded-thttpd.patch
 PHP_THTTPD_PATCHES=$(THTTPD_PATCHES)
 
 #
@@ -131,6 +131,7 @@ $(PHP_THTTPD_LIBPHP_BUILD_DIR)/.configured: $(DL_DIR)/$(PHP_THTTPD_SOURCE) $(DL_
 		--disable-nls \
 		--with-config-file-scan-dir=/opt/etc/php.d \
 		--with-layout=GNU \
+		$(PHP_CONFIGURE_THREAD_ARGS) \
 		--disable-static \
 		--disable-dom \
 		--disable-xml \
