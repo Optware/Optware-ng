3df325e2c3ae39eee3ef8588488ec6359506b77e
mkoster1605 <mkoster1605@1f1dca69-5516-0410-b834-e485effd04c3>
2005-05-04 09:47:49 +0000

    Added CGI::Application and its dependance HTML::Template

---
+++ make/perl-html-template.mk
@@ -0,0 +1,108 @@
+###########################################################
+#
+# perl-html-template
+#
+###########################################################
+
+PERL-HTML-TEMPLATE_SITE=http://search.cpan.org/CPAN/authors/id/S/SA/SAMTREGAR
+PERL-HTML-TEMPLATE_VERSION=2.7
+PERL-HTML-TEMPLATE_SOURCE=HTML-Template-$(PERL-HTML-TEMPLATE_VERSION).tar.gz
+PERL-HTML-TEMPLATE_DIR=HTML-Template-$(PERL-HTML-TEMPLATE_VERSION)
+PERL-HTML-TEMPLATE_UNZIP=zcat
+
+PERL-HTML-TEMPLATE_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
+PERL-HTML-TEMPLATE_DESCRIPTION=Perl module to use HTML Templates from CGI scripts
+PERL-HTML-TEMPLATE_SECTION=util
+PERL-HTML-TEMPLATE_PRIORITY=optional
+PERL-HTML-TEMPLATE_DEPENDS=perl
+
+PERL-HTML-TEMPLATE_IPK_VERSION=1
+
+PERL-HTML-TEMPLATE_CONFFILES=
+
+PERL-HTML-TEMPLATE_PATCHES=
+
+PERL-HTML-TEMPLATE_BUILD_DIR=$(BUILD_DIR)/perl-html-template
+PERL-HTML-TEMPLATE_SOURCE_DIR=$(SOURCE_DIR)/perl-html-template
+PERL-HTML-TEMPLATE_IPK_DIR=$(BUILD_DIR)/perl-html-template-$(PERL-HTML-TEMPLATE_VERSION)-ipk
+PERL-HTML-TEMPLATE_IPK=$(BUILD_DIR)/perl-html-template_$(PERL-HTML-TEMPLATE_VERSION)-$(PERL-HTML-TEMPLATE_IPK_VERSION)_$(TARGET_ARCH).ipk
+
+PERL-HTML-TEMPLATE_BUILD_DIR=$(BUILD_DIR)/perl-html-template
+PERL-HTML-TEMPLATE_SOURCE_DIR=$(SOURCE_DIR)/perl-html-template
+PERL-HTML-TEMPLATE_IPK_DIR=$(BUILD_DIR)/perl-html-template-$(PERL-HTML-TEMPLATE_VERSION)-ipk
+PERL-HTML-TEMPLATE_IPK=$(BUILD_DIR)/perl-html-template_$(PERL-HTML-TEMPLATE_VERSION)-$(PERL-HTML-TEMPLATE_IPK_VERSION)_$(TARGET_ARCH).ipk
+
+$(DL_DIR)/$(PERL-HTML-TEMPLATE_SOURCE):
+	$(WGET) -P $(DL_DIR) $(PERL-HTML-TEMPLATE_SITE)/$(PERL-HTML-TEMPLATE_SOURCE)
+
+perl-html-template-source: $(DL_DIR)/$(PERL-HTML-TEMPLATE_SOURCE) $(PERL-HTML-TEMPLATE_PATCHES)
+
+$(PERL-HTML-TEMPLATE_BUILD_DIR)/.configured: $(DL_DIR)/$(PERL-HTML-TEMPLATE_SOURCE) $(PERL-HTML-TEMPLATE_PATCHES)
+#	$(MAKE) perl-...-stage
+	rm -rf $(BUILD_DIR)/$(PERL-HTML-TEMPLATE_DIR) $(PERL-HTML-TEMPLATE_BUILD_DIR)
+	$(PERL-HTML-TEMPLATE_UNZIP) $(DL_DIR)/$(PERL-HTML-TEMPLATE_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+#	cat $(PERL-HTML-TEMPLATE_PATCHES) | patch -d $(BUILD_DIR)/$(PERL-HTML-TEMPLATE_DIR) -p1
+	mv $(BUILD_DIR)/$(PERL-HTML-TEMPLATE_DIR) $(PERL-HTML-TEMPLATE_BUILD_DIR)
+	(cd $(PERL-HTML-TEMPLATE_BUILD_DIR); \
+		$(TARGET_CONFIGURE_OPTS) \
+		CPPFLAGS="$(STAGING_CPPFLAGS)" \
+		LDFLAGS="$(STAGING_LDFLAGS)" \
+		PERL5LIB="$(STAGING_DIR)/opt/lib/perl5/site_perl" \
+		perl Makefile.PL \
+		PREFIX=/opt \
+	)
+	touch $(PERL-HTML-TEMPLATE_BUILD_DIR)/.configured
+
+perl-html-template-unpack: $(PERL-HTML-TEMPLATE_BUILD_DIR)/.configured
+
+$(PERL-HTML-TEMPLATE_BUILD_DIR)/.built: $(PERL-HTML-TEMPLATE_BUILD_DIR)/.configured
+	rm -f $(PERL-HTML-TEMPLATE_BUILD_DIR)/.built
+	$(MAKE) -C $(PERL-HTML-TEMPLATE_BUILD_DIR) \
+	PERL5LIB="$(STAGING_DIR)/opt/lib/perl5/site_perl"
+	touch $(PERL-HTML-TEMPLATE_BUILD_DIR)/.built
+
+perl-html-template: $(PERL-HTML-TEMPLATE_BUILD_DIR)/.built
+
+$(PERL-HTML-TEMPLATE_BUILD_DIR)/.staged: $(PERL-HTML-TEMPLATE_BUILD_DIR)/.built
+	rm -f $(PERL-HTML-TEMPLATE_BUILD_DIR)/.staged
+	$(MAKE) -C $(PERL-HTML-TEMPLATE_BUILD_DIR) DESTDIR=$(STAGING_DIR) install
+	touch $(PERL-HTML-TEMPLATE_BUILD_DIR)/.staged
+
+perl-html-template-stage: $(PERL-HTML-TEMPLATE_BUILD_DIR)/.staged
+
+$(PERL-HTML-TEMPLATE_IPK_DIR)/CONTROL/control:
+	@install -d $(PERL-HTML-TEMPLATE_IPK_DIR)/CONTROL
+	@rm -f $@
+	@echo "Package: perl-html-template" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(PERL-HTML-TEMPLATE_PRIORITY)" >>$@
+	@echo "Section: $(PERL-HTML-TEMPLATE_SECTION)" >>$@
+	@echo "Version: $(PERL-HTML-TEMPLATE_VERSION)-$(PERL-HTML-TEMPLATE_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(PERL-HTML-TEMPLATE_MAINTAINER)" >>$@
+	@echo "Source: $(PERL-HTML-TEMPLATE_SITE)/$(PERL-HTML-TEMPLATE_SOURCE)" >>$@
+	@echo "Description: $(PERL-HTML-TEMPLATE_DESCRIPTION)" >>$@
+	@echo "Depends: $(PERL-HTML-TEMPLATE_DEPENDS)" >>$@
+
+$(PERL-HTML-TEMPLATE_IPK): $(PERL-HTML-TEMPLATE_BUILD_DIR)/.built
+	rm -rf $(PERL-HTML-TEMPLATE_IPK_DIR) $(BUILD_DIR)/perl-html-template_*_$(TARGET_ARCH).ipk
+	$(MAKE) -C $(PERL-HTML-TEMPLATE_BUILD_DIR) DESTDIR=$(PERL-HTML-TEMPLATE_IPK_DIR) install
+	find $(PERL-HTML-TEMPLATE_IPK_DIR)/opt -name 'perllocal.pod' -exec rm -f {} \;
+	(cd $(PERL-HTML-TEMPLATE_IPK_DIR)/opt/lib/perl5 ; \
+		find . -name '*.so' -exec chmod +w {} \; ; \
+		find . -name '*.so' -exec $(STRIP_COMMAND) {} \; ; \
+		find . -name '*.so' -exec chmod -w {} \; ; \
+	)
+	find $(PERL-HTML-TEMPLATE_IPK_DIR)/opt -type d -exec chmod go+rx {} \;
+	$(MAKE) $(PERL-HTML-TEMPLATE_IPK_DIR)/CONTROL/control
+#	install -m 755 $(PERL-HTML-TEMPLATE_SOURCE_DIR)/postinst $(PERL-HTML-TEMPLATE_IPK_DIR)/CONTROL/postinst
+#	install -m 755 $(PERL-HTML-TEMPLATE_SOURCE_DIR)/prerm $(PERL-HTML-TEMPLATE_IPK_DIR)/CONTROL/prerm
+	echo $(PERL-HTML-TEMPLATE_CONFFILES) | sed -e 's/ /\n/g' > $(PERL-HTML-TEMPLATE_IPK_DIR)/CONTROL/conffiles
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(PERL-HTML-TEMPLATE_IPK_DIR)
+
+perl-html-template-ipk: $(PERL-HTML-TEMPLATE_IPK)
+
+perl-html-template-clean:
+	-$(MAKE) -C $(PERL-HTML-TEMPLATE_BUILD_DIR) clean
+
+perl-html-template-dirclean:
+	rm -rf $(BUILD_DIR)/$(PERL-HTML-TEMPLATE_DIR) $(PERL-HTML-TEMPLATE_BUILD_DIR) $(PERL-HTML-TEMPLATE_IPK_DIR) $(PERL-HTML-TEMPLATE_IPK)
