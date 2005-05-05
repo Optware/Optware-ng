b7f1404305fcf7b0fab95bc66a967acdb23963c8
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2005-05-05 10:44:36 +0000

    Added LIBUSB_PATCHES for msteveb to try.

--- make/libusb.mk
+++ make/libusb.mk
@@ -40,6 +40,12 @@ LIBUSB_CONFLICTS=
 LIBUSB_IPK_VERSION=2
 
 #
+# LIBUSB_PATCHES should list any patches, in the the order in
+# which they should be applied to the source code.
+#
+# LIBUSB_PATCHES=$(LIBUSB_SOURCE_DIR)/foo.patch
+
+#
 # If the compilation of the package requires additional
 # compilation or linking flags, then list them here.
 #
@@ -90,9 +96,10 @@ libusb-source: $(DL_DIR)/$(LIBUSB_SOURCE)
 # If the compilation of the package requires other packages to be staged
 # first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
 #
-$(LIBUSB_BUILD_DIR)/.configured: $(DL_DIR)/$(LIBUSB_SOURCE)
+$(LIBUSB_BUILD_DIR)/.configured: $(DL_DIR)/$(LIBUSB_SOURCE) $(LIBUSB_PATCHES)
 	rm -rf $(BUILD_DIR)/$(LIBUSB_DIR) $(LIBUSB_BUILD_DIR)
 	$(LIBUSB_UNZIP) $(DL_DIR)/$(LIBUSB_SOURCE) | tar -C $(BUILD_DIR) -xf -
+	cat $(LIBUSB_PATCHES) | patch -d $(BUILD_DIR)/$(LIBUSB_DIR) -p1
 	mv $(BUILD_DIR)/$(LIBUSB_DIR) $(LIBUSB_BUILD_DIR)
 	(cd $(LIBUSB_BUILD_DIR); \
 		$(TARGET_CONFIGURE_OPTS) \
