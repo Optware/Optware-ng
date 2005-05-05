88fb0454f3f735af74b1711f4c2fb8622044c2b3
ptweety <ptweety@1f1dca69-5516-0410-b834-e485effd04c3>
2005-05-05 11:03:05 +0000

    convenience package for unslung developers

---
+++ trunk/make/unslung-devel.mk
@@ -0,0 +1,54 @@
+###########################################################
+#
+# unslung-devel
+#
+###########################################################
+
+UNSLUNG-DEVEL_VERSION=3.18
+UNSLUNG-DEVEL_DIR=unslung-devel-$(UNSLUNG-DEVEL_VERSION)
+UNSLUNG-DEVEL_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
+UNSLUNG-DEVEL_DESCRIPTION=This is a meta package that bundels all the utils required for unslung development.
+UNSLUNG-DEVEL_SECTION=util
+UNSLUNG-DEVEL_PRIORITY=optional
+UNSLUNG-DEVEL_DEPENDS=ncurses, bash, bison, coreutils, findutils, flex, gawk, libstdc++, groff, libtool, make, m4, autoconf, automake, patch, perl, sed, bzip2, tar, openssl, wget-ssl, crosstool
+UNSLUNG-DEVEL_SUGGESTS=
+UNSLUNG-DEVEL_CONFLICTS=
+
+UNSLUNG-DEVEL_IPK_VERSION=1
+
+UNSLUNG-DEVEL_IPK_DIR=$(BUILD_DIR)/unslung-devel-$(UNSLUNG-DEVEL_VERSION)-ipk
+UNSLUNG-DEVEL_IPK=$(BUILD_DIR)/unslung-devel_$(UNSLUNG-DEVEL_VERSION)-$(UNSLUNG-DEVEL_IPK_VERSION)_$(TARGET_ARCH).ipk
+
+unslung-devel-unpack:
+
+unslung-devel:
+
+$(UNSLUNG-DEVEL_IPK_DIR)/CONTROL/control:
+	@install -d $(UNSLUNG-DEVEL_IPK_DIR)/CONTROL
+	@rm -f $@
+	@echo "Package: unslung-devel" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(UNSLUNG-DEVEL_PRIORITY)" >>$@
+	@echo "Section: $(UNSLUNG-DEVEL_SECTION)" >>$@
+	@echo "Version: $(UNSLUNG-DEVEL_VERSION)-$(UNSLUNG-DEVEL_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(UNSLUNG-DEVEL_MAINTAINER)" >>$@
+	@echo "Source: $(UNSLUNG-DEVEL_SITE)/$(UNSLUNG-DEVEL_SOURCE)" >>$@
+	@echo "Description: $(UNSLUNG-DEVEL_DESCRIPTION)" >>$@
+	@echo "Depends: $(UNSLUNG-DEVEL_DEPENDS)" >>$@
+	@echo "Suggests: $(UNSLUNG-DEVEL_SUGGESTS)" >>$@
+	@echo "Conflicts: $(UNSLUNG-DEVEL_CONFLICTS)" >>$@
+
+$(UNSLUNG-DEVEL_IPK):
+	rm -rf $(UNSLUNG-DEVEL_IPK_DIR) $(BUILD_DIR)/unslung-devel_*_$(TARGET_ARCH).ipk
+	$(MAKE) $(UNSLUNG-DEVEL_IPK_DIR)/CONTROL/control
+#	install -m 755 $(UNSLUNG-DEVEL_SOURCE_DIR)/postinst $(UNSLUNG-DEVEL_IPK_DIR)/CONTROL/postinst
+#	install -m 755 $(UNSLUNG-DEVEL_SOURCE_DIR)/prerm $(UNSLUNG-DEVEL_IPK_DIR)/CONTROL/prerm
+#	echo $(UNSLUNG-DEVEL_CONFFILES) | sed -e 's/ /\n/g' > $(UNSLUNG-DEVEL_IPK_DIR)/CONTROL/conffiles
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(UNSLUNG-DEVEL_IPK_DIR)
+
+unslung-devel-ipk: $(UNSLUNG-DEVEL_IPK)
+
+unslung-devel-clean:
+
+unslung-devel-dirclean:
+	rm -rf $(UNSLUNG-DEVEL_IPK_DIR) $(UNSLUNG-DEVEL_IPK)
