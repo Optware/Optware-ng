809e2319ecc9b39e748745e27855ae34ff854d6d
jp30 <jp30@1f1dca69-5516-0410-b834-e485effd04c3>
2005-05-06 01:42:55 +0000

    coreutils, procps: use update-alternatives for kill, install ps

--- make/procps.mk
+++ make/procps.mk
@@ -4,7 +4,6 @@
 #
 ###########################################################
 
-PROCPS_DIR=$(BUILD_DIR)/procps
 PROCPS_VERSION=3.2.3
 PROCPS=procps-$(PROCPS_VERSION)
 PROCPS_SITE=http://procps.sourceforge.net
@@ -17,8 +16,10 @@ PROCPS_PRIORITY=optional
 PROCPS_DEPENDS=ncurses
 PROCPS_CONFLICTS=busybox
 
-PROCPS_IPK_VERSION=4
+PROCPS_IPK_VERSION=5
 
+PROCPS_BUILD_DIR=$(BUILD_DIR)/procps
+PROCPS_SOURCE_DIR=$(SOURCE_DIR)/procps
 PROCPS_IPK=$(BUILD_DIR)/procps_$(PROCPS_VERSION)-$(PROCPS_IPK_VERSION)_$(TARGET_ARCH).ipk
 PROCPS_IPK_DIR=$(BUILD_DIR)/procps-$(PROCPS_VERSION)-ipk
 
@@ -42,10 +43,10 @@ procps-source: $(DL_DIR)/$(PROCPS_SOURCE_ARCHIVE)
 #
 # This target unpacks the source code into the build directory.
 #
-$(PROCPS_DIR)/.source: $(DL_DIR)/$(PROCPS_SOURCE_ARCHIVE)
+$(PROCPS_BUILD_DIR)/.source: $(DL_DIR)/$(PROCPS_SOURCE_ARCHIVE)
 	$(PROCPS_UNZIP) $(DL_DIR)/$(PROCPS_SOURCE_ARCHIVE) | tar -C $(BUILD_DIR) -xvf -
-	mv $(BUILD_DIR)/procps-$(PROCPS_VERSION) $(PROCPS_DIR)
-	touch $(PROCPS_DIR)/.source
+	mv $(BUILD_DIR)/procps-$(PROCPS_VERSION) $(PROCPS_BUILD_DIR)
+	touch $(PROCPS_BUILD_DIR)/.source
 
 #
 # This target configures the build within the build directory.
@@ -55,15 +56,17 @@ $(PROCPS_DIR)/.source: $(DL_DIR)/$(PROCPS_SOURCE_ARCHIVE)
 # correctly BUILD the Makefile with the right paths, where passing it
 # to Make causes it to override the default search paths of the compiler.
 #
-$(PROCPS_DIR)/.configured: $(PROCPS_DIR)/.source
+$(PROCPS_BUILD_DIR)/.configured: $(PROCPS_BUILD_DIR)/.source
 	$(MAKE) ncurses-stage
-	touch $(PROCPS_DIR)/.configured
+	touch $(PROCPS_BUILD_DIR)/.configured
+
+procps-unpack: $(PROCPS_BUILD_DIR)/.configured
 
 #
 # This builds the actual binary.
 #
-$(PROCPS_DIR)/watch: $(PROCPS_DIR)/.configured
-	$(MAKE) -C $(PROCPS_DIR)	\
+$(PROCPS_BUILD_DIR)/watch: $(PROCPS_BUILD_DIR)/.configured
+	$(MAKE) -C $(PROCPS_BUILD_DIR)	\
 	CC=$(TARGET_CC)			\
 	CPPFLAGS=$(PROCPS_CPPFLAGS)	\
 	LDFLAGS=$(PROCPS_LDFLAGS)	\
@@ -72,7 +75,7 @@ $(PROCPS_DIR)/watch: $(PROCPS_DIR)/.configured
 #
 # These are the dependencies for the binary.  
 #
-procps: ncurses $(PROCPS_DIR)/watch
+procps: ncurses $(PROCPS_BUILD_DIR)/watch
 
 #
 # This rule creates a control file for ipkg.  It is no longer
@@ -95,29 +98,32 @@ $(PROCPS_IPK_DIR)/CONTROL/control:
 #
 # This builds the IPK file.
 #
-$(PROCPS_IPK): $(PROCPS_DIR)/watch
+$(PROCPS_IPK): $(PROCPS_BUILD_DIR)/watch
 	rm -rf $(PROCPS_IPK_DIR) $(BUILD_DIR)/procps_*_$(TARGET_ARCH).ipk
 	mkdir -p $(PROCPS_IPK_DIR)/opt
 	mkdir -p $(PROCPS_IPK_DIR)/opt/bin
-	$(STRIP_COMMAND) $(PROCPS_DIR)/free -o $(PROCPS_IPK_DIR)/opt/bin/free
-	$(STRIP_COMMAND) $(PROCPS_DIR)/kill -o $(PROCPS_IPK_DIR)/opt/bin/kill
-	$(STRIP_COMMAND) $(PROCPS_DIR)/pgrep -o $(PROCPS_IPK_DIR)/opt/bin/pgrep
-	$(STRIP_COMMAND) $(PROCPS_DIR)/pmap -o $(PROCPS_IPK_DIR)/opt/bin/pmap
-	$(STRIP_COMMAND) $(PROCPS_DIR)/skill -o $(PROCPS_IPK_DIR)/opt/bin/skill
-	$(STRIP_COMMAND) $(PROCPS_DIR)/slabtop -o $(PROCPS_IPK_DIR)/opt/bin/slabtop
-	$(STRIP_COMMAND) $(PROCPS_DIR)/snice -o $(PROCPS_IPK_DIR)/opt/bin/snice
-	$(STRIP_COMMAND) $(PROCPS_DIR)/sysctl -o $(PROCPS_IPK_DIR)/opt/bin/sysctl
-	cp $(PROCPS_DIR)/t $(PROCPS_IPK_DIR)/opt/bin
-	$(STRIP_COMMAND) $(PROCPS_DIR)/tload -o $(PROCPS_IPK_DIR)/opt/bin/tload
-	$(STRIP_COMMAND) $(PROCPS_DIR)/top -o $(PROCPS_IPK_DIR)/opt/bin/top
-	$(STRIP_COMMAND) $(PROCPS_DIR)/uptime -o $(PROCPS_IPK_DIR)/opt/bin/uptime
-	cp $(PROCPS_DIR)/v $(PROCPS_IPK_DIR)/opt/bin
-	$(STRIP_COMMAND) $(PROCPS_DIR)/vmstat -o $(PROCPS_IPK_DIR)/opt/bin/vmstat
-	$(STRIP_COMMAND) $(PROCPS_DIR)/w -o $(PROCPS_IPK_DIR)/opt/bin/w
-	$(STRIP_COMMAND) $(PROCPS_DIR)/watch -o $(PROCPS_IPK_DIR)/opt/bin/watch
+	$(STRIP_COMMAND) $(PROCPS_BUILD_DIR)/free -o $(PROCPS_IPK_DIR)/opt/bin/free
+	$(STRIP_COMMAND) $(PROCPS_BUILD_DIR)/kill -o $(PROCPS_IPK_DIR)/opt/bin/procps-kill
+	$(STRIP_COMMAND) $(PROCPS_BUILD_DIR)/pgrep -o $(PROCPS_IPK_DIR)/opt/bin/pgrep
+	$(STRIP_COMMAND) $(PROCPS_BUILD_DIR)/pmap -o $(PROCPS_IPK_DIR)/opt/bin/pmap
+	$(STRIP_COMMAND) $(PROCPS_BUILD_DIR)/ps/ps -o $(PROCPS_IPK_DIR)/opt/bin/ps
+	$(STRIP_COMMAND) $(PROCPS_BUILD_DIR)/skill -o $(PROCPS_IPK_DIR)/opt/bin/skill
+	$(STRIP_COMMAND) $(PROCPS_BUILD_DIR)/slabtop -o $(PROCPS_IPK_DIR)/opt/bin/slabtop
+	$(STRIP_COMMAND) $(PROCPS_BUILD_DIR)/snice -o $(PROCPS_IPK_DIR)/opt/bin/snice
+	$(STRIP_COMMAND) $(PROCPS_BUILD_DIR)/sysctl -o $(PROCPS_IPK_DIR)/opt/bin/sysctl
+	cp $(PROCPS_BUILD_DIR)/t $(PROCPS_IPK_DIR)/opt/bin
+	$(STRIP_COMMAND) $(PROCPS_BUILD_DIR)/tload -o $(PROCPS_IPK_DIR)/opt/bin/tload
+	$(STRIP_COMMAND) $(PROCPS_BUILD_DIR)/top -o $(PROCPS_IPK_DIR)/opt/bin/top
+	$(STRIP_COMMAND) $(PROCPS_BUILD_DIR)/uptime -o $(PROCPS_IPK_DIR)/opt/bin/procps-uptime
+	cp $(PROCPS_BUILD_DIR)/v $(PROCPS_IPK_DIR)/opt/bin
+	$(STRIP_COMMAND) $(PROCPS_BUILD_DIR)/vmstat -o $(PROCPS_IPK_DIR)/opt/bin/vmstat
+	$(STRIP_COMMAND) $(PROCPS_BUILD_DIR)/w -o $(PROCPS_IPK_DIR)/opt/bin/w
+	$(STRIP_COMMAND) $(PROCPS_BUILD_DIR)/watch -o $(PROCPS_IPK_DIR)/opt/bin/watch
 	mkdir -p $(PROCPS_IPK_DIR)/opt/lib
-	cp $(PROCPS_DIR)/proc/libproc-3.2.3.so $(PROCPS_IPK_DIR)/opt/lib
+	$(STRIP_COMMAND) $(PROCPS_BUILD_DIR)/proc/libproc-3.2.3.so -o $(PROCPS_IPK_DIR)/opt/lib/libproc-3.2.3.so
 	$(MAKE) $(PROCPS_IPK_DIR)/CONTROL/control
+	install -m 644 $(PROCPS_SOURCE_DIR)/postinst $(PROCPS_IPK_DIR)/CONTROL/postinst
+	install -m 644 $(PROCPS_SOURCE_DIR)/prerm $(PROCPS_IPK_DIR)/CONTROL/prerm
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(PROCPS_IPK_DIR)
 
 #
@@ -129,20 +135,20 @@ procps-ipk: $(PROCPS_IPK)
 # This is called from the top level makefile to clean all of the built files.
 #
 procps-clean:
-	-$(MAKE) -C $(PROCPS_DIR) uninstall
-	-$(MAKE) -C $(PROCPS_DIR) clean
+	-$(MAKE) -C $(PROCPS_BUILD_DIR) uninstall
+	-$(MAKE) -C $(PROCPS_BUILD_DIR) clean
 
 #
 # This is called from the top level makefile to clean ALL files, including
 # downloaded source.
 #
 procps-distclean:
-	-rm $(PROCPS_DIR)/.configured
-	-$(MAKE) -C $(PROCPS_DIR) distclean
+	-rm $(PROCPS_BUILD_DIR)/.configured
+	-$(MAKE) -C $(PROCPS_BUILD_DIR) distclean
 
 #
 # This is called from the top level makefile to clean all dynamically created
 # directories.
 #
 procps-dirclean:
-	rm -rf $(PROCPS_DIR) $(PROCPS_IPK_DIR) $(PROCPS_IPK)
+	rm -rf $(PROCPS_BUILD_DIR) $(PROCPS_IPK_DIR) $(PROCPS_IPK)
