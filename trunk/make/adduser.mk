afde6d709ea88a99fa30f3c8d166210d293e8957
jeanfabrice <jeanfabrice@1f1dca69-5516-0410-b834-e485effd04c3>
2005-05-10 09:27:40 +0000

    Added su applet to adduser optware

--- trunk/make/adduser.mk
+++ trunk/make/adduser.mk
@@ -37,7 +37,7 @@ ADDUSER_VERSION:=$(shell sed -n -e 's/^BUSYBOX_VERSION *=\([0-9]\)/\1/p' make/bu
 #
 # ADDUSER_IPK_VERSION should be incremented when the ipk changes.
 #
-ADDUSER_IPK_VERSION=3
+ADDUSER_IPK_VERSION=4
 
 #
 # ADDUSER_CONFFILES should be a list of user-editable files
@@ -164,6 +164,7 @@ $(ADDUSER_IPK): $(ADDUSER_BUILD_DIR)/.built
 	cd $(ADDUSER_IPK_DIR)/opt/bin && ln -fs adduser addgroup
 	cd $(ADDUSER_IPK_DIR)/opt/bin && ln -fs adduser delgroup
 	cd $(ADDUSER_IPK_DIR)/opt/bin && ln -fs adduser deluser
+	cd $(ADDUSER_IPK_DIR)/opt/bin && ln -fs adduser su
 	$(MAKE) $(ADDUSER_IPK_DIR)/CONTROL/control
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(ADDUSER_IPK_DIR)
 
