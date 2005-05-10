48a721e3c9d785c44b92cf5b4d4a50f4e14b7d88
cdoban <cdoban@1f1dca69-5516-0410-b834-e485effd04c3>
2005-05-10 06:14:43 +0000

    builds from CVS

--- make/asterisk.mk
+++ make/asterisk.mk
@@ -4,26 +4,44 @@
 #
 ###########################################################
 
-# You must replace "asterisk" and "ASTERISK" with the lower case name and
-# upper case name of your new package.  Some places below will say
-# "Do not change this" - that does not include this global change,
-# which must always be done to ensure we have unique names.
+#
+# ASTERISK_REPOSITORY defines the upstream location of the source code
+# for the package.  ASTERISK_DIR is the directory which is created when
+# this cvs module is checked out.
+#
+
+ASTERISK_REPOSITORY=:pserver:anoncvs@cvs.digium.com:/usr/cvsroot
+ASTERISK_DIR=asterisk
+ASTERISK_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
+ASTERISK_DESCRIPTION=Open Source VoIP PBX System
+ASTERISK_SECTION=util
+ASTERISK_PRIORITY=optional
+ASTERISK_DEPENDS=openssl ncurses
+ASTERISK_SUGGESTS=
+ASTERISK_CONFLICTS=
 
 #
-# ASTERISK_VERSION, ASTERISK_SITE and ASTERISK_SOURCE define
-# the upstream location of the source code for the package.
-# ASTERISK_DIR is the directory which is created when the source
-# archive is unpacked.
-# ASTERISK_UNZIP is the command used to unzip the source.
-# It is usually "zcat" (for .gz) or "bzcat" (for .bz2)
+# Software downloaded from CVS repositories must either use a tag or a
+# date to ensure that the same sources can be downloaded later.
 #
-# You should change all these variables to suit your package.
+
 #
-ASTERISK_SITE=http://www.asterisk.org/html/downloads
-ASTERISK_VERSION=1.0.6
-ASTERISK_SOURCE=asterisk-$(ASTERISK_VERSION).tar.gz
-ASTERISK_DIR=asterisk-$(ASTERISK_VERSION)
-ASTERISK_UNZIP=zcat
+# If you want to use a date, uncomment the variables below and modify
+# ASTERISK_CVS_DATE
+#
+
+ASTERISK_CVS_DATE=20050508
+ASTERISK_VERSION=cvs$(ASTERISK_CVS_DATE)
+ASTERISK_CVS_OPTS=-D $(ASTERISK_CVS_DATE)
+
+#
+# If you want to use a tag, uncomment the variables below and modify
+# ASTERISK_CVS_TAG and ASTERISK_CVS_VERSION
+#
+
+#ASTERISK_CVS_TAG=version_1_2_3
+#ASTERISK_VERSION=1.2.3
+#ASTERISK_CVS_OPTS=-r $(ASTERISK_CVS_TAG)
 
 #
 # ASTERISK_IPK_VERSION should be incremented when the ipk changes.
@@ -31,15 +49,17 @@ ASTERISK_UNZIP=zcat
 ASTERISK_IPK_VERSION=1
 
 #
+# ASTERISK_CONFFILES should be a list of user-editable files
+ASTERISK_CONFFILES=
+
+#
 # ASTERISK_PATCHES should list any patches, in the the order in
 # which they should be applied to the source code.
 #
 ASTERISK_PATCHES=$(ASTERISK_SOURCE_DIR)/Makefile.patch \
 		 $(ASTERISK_SOURCE_DIR)/editline.makelist.patch \
-		 $(ASTERISK_SOURCE_DIR)/codecs.gsm.Makefile.patch \
-		 $(ASTERISK_SOURCE_DIR)/codecs.ilbc.Makefile.patch \
-		 $(ASTERISK_SOURCE_DIR)/codecs.lpc10.Makefile.patch \
-		 $(ASTERISK_SOURCE_DIR)/db1-ast.Makefile.patch
+		 $(ASTERISK_SOURCE_DIR)/codecs.gsm.Makefile.patch
+                                  
 
 #
 # If the compilation of the package requires additional
@@ -57,7 +77,7 @@ ASTERISK_LDFLAGS=
 #
 # You should not change any of these variables.
 #
-ASTERISK_BUILD_DIR=$(BUILD_DIR)/asterisk
+ASTERISK_BUILD_DIR=$(BUILD_DIR)/asterisk-cvs
 ASTERISK_SOURCE_DIR=$(SOURCE_DIR)/asterisk
 ASTERISK_IPK_DIR=$(BUILD_DIR)/asterisk-$(ASTERISK_VERSION)-ipk
 ASTERISK_IPK=$(BUILD_DIR)/asterisk_$(ASTERISK_VERSION)-$(ASTERISK_IPK_VERSION)_$(TARGET_ARCH).ipk
@@ -76,26 +96,21 @@ ASTERISK_INFO_DIR=$(ASTERISK_INST_DIR)/info
 ASTERISK_MAN_DIR=$(ASTERISK_INST_DIR)/man
 ASTERISK_SYSCONF_SAMPLE_DIR=$(ASTERISK_INST_DIR)/etc/asterisk/sample
 
+#
+# In this case there is no tarball, instead we fetch the sources
+# directly to the builddir with CVS
+#
+$(ASTERISK_BUILD_DIR)/.fetched: $(ASTERISK_PATCHES)
+	rm -rf $(ASTERISK_BUILD_DIR) $(BUILD_DIR)/$(ASTERISK_DIR)
+	( cd $(BUILD_DIR); \
+		cvs -d $(ASTERISK_REPOSITORY) -z3 co $(ASTERISK_CVS_OPTS) $(ASTERISK_DIR); \
+	)
+	mv $(BUILD_DIR)/$(ASTERISK_DIR) $(ASTERISK_BUILD_DIR)
+	cat $(ASTERISK_PATCHES) | patch -d $(ASTERISK_BUILD_DIR) -p1
+	touch $@
 
-#
-# This is the dependency on the source code.  If the source is missing,
-# then it will be fetched from the site using wget.
-#
-$(DL_DIR)/$(ASTERISK_SOURCE):
-	$(WGET) -P $(DL_DIR) $(ASTERISK_SITE)/$(ASTERISK_SOURCE)
+asterisk-source: $(ASTERISK_BUILD_DIR)/.fetched
 
-#
-# The source code depends on it existing within the download directory.
-# This target will be called by the top level Makefile to download the
-# source code's archive (.tar.gz, .bz2, etc.)
-#
-asterisk-source: $(DL_DIR)/$(ASTERISK_SOURCE) $(ASTERISK_PATCHES)
-
-#
-# This target unpacks the source code in the build directory.
-# If the source archive is not .tar.gz or .tar.bz2, then you will need
-# to change the commands here.  Patches to the source code are also
-# applied in this target as required.
 #
 # This target also configures the build within the build directory.
 # Flags such as LDFLAGS and CPPFLAGS should be passed into configure
@@ -104,14 +119,21 @@ asterisk-source: $(DL_DIR)/$(ASTERISK_SOURCE) $(ASTERISK_PATCHES)
 # to Make causes it to override the default search paths of the compiler.
 #
 # If the compilation of the package requires other packages to be staged
-# first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
+# first, then do that first (e.g. "$(MAKE) <foo>-stage <baz>-stage").
 #
-$(ASTERISK_BUILD_DIR)/.configured: $(DL_DIR)/$(ASTERISK_SOURCE) $(ASTERISK_PATCHES)
+$(ASTERISK_BUILD_DIR)/.configured: $(ASTERISK_BUILD_DIR)/.fetched
 	$(MAKE) ncurses-stage openssl-stage
-	rm -rf $(BUILD_DIR)/$(ASTERISK_DIR) $(ASTERISK_BUILD_DIR)
-	$(ASTERISK_UNZIP) $(DL_DIR)/$(ASTERISK_SOURCE) | tar -C $(BUILD_DIR) -xvf -
-	cat $(ASTERISK_PATCHES) | patch -d $(BUILD_DIR)/$(ASTERISK_DIR) -p1
-	mv $(BUILD_DIR)/$(ASTERISK_DIR) $(ASTERISK_BUILD_DIR)
+#	(cd $(ASTERISK_BUILD_DIR); \
+#		$(TARGET_CONFIGURE_OPTS) \
+#		CPPFLAGS="$(STAGING_CPPFLAGS) $(ASTERISK_CPPFLAGS)" \
+#		LDFLAGS="$(STAGING_LDFLAGS) $(ASTERISK_LDFLAGS)" \
+#		./configure \
+#		--build=$(GNU_HOST_NAME) \
+#		--host=$(GNU_TARGET_NAME) \
+#		--target=$(GNU_TARGET_NAME) \
+#		--prefix=/opt \
+#		--disable-nls \
+#	)
 	touch $(ASTERISK_BUILD_DIR)/.configured
 
 asterisk-unpack: $(ASTERISK_BUILD_DIR)/.configured
@@ -124,6 +146,7 @@ $(ASTERISK_BUILD_DIR)/.built: $(ASTERISK_BUILD_DIR)/.configured
 	CPPFLAGS="$(STAGING_CPPFLAGS) $(ASTERISK_CPPFLAGS)" \
 	LDFLAGS="$(STAGING_LDFLAGS) $(ASTERISK_LDFLAGS)" \
 	$(MAKE) -C $(ASTERISK_BUILD_DIR) INSTALL_PREFIX=$(ASTERISK_INST_DIR) \
+	PROC=arm SUB_PROC=xscale \
 	$(TARGET_CONFIGURE_OPTS)
 	touch $(ASTERISK_BUILD_DIR)/.built
 
@@ -137,10 +160,32 @@ asterisk: $(ASTERISK_BUILD_DIR)/.built
 #
 $(ASTERISK_BUILD_DIR)/.staged: $(ASTERISK_BUILD_DIR)/.built
 	rm -f $(ASTERISK_BUILD_DIR)/.staged
-	$(MAKE) -C $(ASTERISK_BUILD_DIR) DESTDIR=$(STAGING_DIR) INSTALL_PREFIX=$(ASTERISK_INST_DIR) install
+	$(MAKE) -C $(ASTERISK_BUILD_DIR) DESTDIR=$(STAGING_DIR) \
+	 INSTALL_PREFIX=$(ASTERISK_INST_DIR) \
+	PROC=arm SUB_PROC=xscale \
+	install
 	touch $(ASTERISK_BUILD_DIR)/.staged
 
 asterisk-stage: $(ASTERISK_BUILD_DIR)/.staged
+
+#
+# This rule creates a control file for ipkg.  It is no longer
+# necessary to create a seperate control file under sources/asterisk
+#
+$(ASTERISK_IPK_DIR)/CONTROL/control:
+	@install -d $(ASTERISK_IPK_DIR)/CONTROL
+	@rm -f $@
+	@echo "Package: asterisk" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(ASTERISK_PRIORITY)" >>$@
+	@echo "Section: $(ASTERISK_SECTION)" >>$@
+	@echo "Version: $(ASTERISK_VERSION)-$(ASTERISK_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(ASTERISK_MAINTAINER)" >>$@
+	@echo "Source: $(ASTERISK_REPOSITORY)" >>$@
+	@echo "Description: $(ASTERISK_DESCRIPTION)" >>$@
+	@echo "Depends: $(ASTERISK_DEPENDS)" >>$@
+	@echo "Suggests: $(ASTERISK_SUGGESTS)" >>$@
+	@echo "Conflicts: $(ASTERISK_CONFLICTS)" >>$@
 
 #
 # This builds the IPK file.
@@ -163,18 +208,20 @@ $(ASTERISK_IPK): $(ASTERISK_BUILD_DIR)/.built
 		ASTSBINDIR=$(ASTERISK_SBIN_DIR) \
 		ASTMANDIR=$(ASTERISK_MAN_DIR) \
 		ASTLIBDIR=$(ASTERISK_LIB_DIR) \
+		PROC=arm SUB_PROC=xscale \
 		install )
+	install -d $(ASTERISK_IPK_DIR)/opt/etc/
 	($(MAKE) -C $(ASTERISK_BUILD_DIR) DESTDIR=$(ASTERISK_IPK_DIR) \
 		INSTALL_PREFIX=$(ASTERISK_INST_DIR) \
-                ASTHEADERDIR=$(ASTERISK_INCLUDE_DIR) \
-                ASTBINDIR=$(ASTERISK_BIN_DIR) \
-                ASTSBINDIR=$(ASTERISK_SBIN_DIR) \
-                ASTMANDIR=$(ASTERISK_MAN_DIR) \
-                ASTLIBDIR=$(ASTERISK_LIB_DIR) \
+		ASTHEADERDIR=$(ASTERISK_INCLUDE_DIR) \
+		ASTBINDIR=$(ASTERISK_BIN_DIR) \
+		ASTSBINDIR=$(ASTERISK_SBIN_DIR) \
+		ASTMANDIR=$(ASTERISK_MAN_DIR) \
+		ASTLIBDIR=$(ASTERISK_LIB_DIR) \
 		ASTETCDIR=$(ASTERISK_SYSCONF_SAMPLE_DIR) \
+		PROC=arm SUB_PROC=xscale \
 		samples )
-	install -d $(ASTERISK_IPK_DIR)/CONTROL
-	install -m 644 $(ASTERISK_SOURCE_DIR)/control $(ASTERISK_IPK_DIR)/CONTROL/control
+	$(MAKE) $(ASTERISK_IPK_DIR)/CONTROL/control
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(ASTERISK_IPK_DIR)
 
 #
