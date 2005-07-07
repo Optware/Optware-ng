afb19ccf4001949fb3fb008db365d5cbeb3ce679
aabaker <aabaker@1f1dca69-5516-0410-b834-e485effd04c3>
2005-07-07 22:19:22 +0000

    Add a make file for the IM to IRC gateway Bitlbee, GNUTLS (the preferred option for it to do SSL) and all of the libraries GNUTLS needs

---
+++ make/bitlbee.mk
@@ -0,0 +1,212 @@
+###########################################################
+#
+# bitlbee
+#
+###########################################################
+
+#
+# BITLBEE_VERSION, BITLBEE_SITE and BITLBEE_SOURCE define
+# the upstream location of the source code for the package.
+# BITLBEE_DIR is the directory which is created when the source
+# archive is unpacked.
+# BITLBEE_UNZIP is the command used to unzip the source.
+# It is usually "zcat" (for .gz) or "bzcat" (for .bz2)
+#
+# You should change all these variables to suit your package.
+# Please make sure that you add a description, and that you
+# list all your packages' dependencies, seperated by commas.
+# 
+# If you list yourself as MAINTAINER, please give a valid email
+# address, and indicate your irc nick if it cannot be easily deduced
+# from your name or email address.  If you leave MAINTAINER set to
+# "NSLU2 Linux" other developers will feel free to edit.
+#
+BITLBEE_SITE=http://get.bitlbee.org/src/
+BITLBEE_VERSION=0.92
+BITLBEE_SOURCE=bitlbee-$(BITLBEE_VERSION).tar.gz
+BITLBEE_DIR=bitlbee-$(BITLBEE_VERSION)
+BITLBEE_UNZIP=zcat
+BITLBEE_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
+BITLBEE_DESCRIPTION=A gateway between IRC and proprietary IM networks
+BITLBEE_SECTION=net
+BITLBEE_PRIORITY=optional
+BITLBEE_DEPENDS=glib, gnutls, xinetd
+BITLBEE_SUGGESTS=
+BITLBEE_CONFLICTS=
+
+#
+# BITLBEE_IPK_VERSION should be incremented when the ipk changes.
+#
+BITLBEE_IPK_VERSION=1
+
+#
+# BITLBEE_CONFFILES should be a list of user-editable files
+BITLBEE_CONFFILES=/opt/etc/bitlbee/bitlbee.conf /opt/etc/xinetd.d/bitlbee
+
+#
+# BITLBEE_PATCHES should list any patches, in the the order in
+# which they should be applied to the source code.
+#
+BITLBEE_PATCHES=$(BITLBEE_SOURCE_DIR)/configure.patch
+
+#
+# If the compilation of the package requires additional
+# compilation or linking flags, then list them here.
+#
+BITLBEE_CPPFLAGS=
+BITLBEE_LDFLAGS=
+
+#
+# BITLBEE_BUILD_DIR is the directory in which the build is done.
+# BITLBEE_SOURCE_DIR is the directory which holds all the
+# patches and ipkg control files.
+# BITLBEE_IPK_DIR is the directory in which the ipk is built.
+# BITLBEE_IPK is the name of the resulting ipk files.
+#
+# You should not change any of these variables.
+#
+BITLBEE_BUILD_DIR=$(BUILD_DIR)/bitlbee
+BITLBEE_SOURCE_DIR=$(SOURCE_DIR)/bitlbee
+BITLBEE_IPK_DIR=$(BUILD_DIR)/bitlbee-$(BITLBEE_VERSION)-ipk
+BITLBEE_IPK=$(BUILD_DIR)/bitlbee_$(BITLBEE_VERSION)-$(BITLBEE_IPK_VERSION)_$(TARGET_ARCH).ipk
+
+#
+# This is the dependency on the source code.  If the source is missing,
+# then it will be fetched from the site using wget.
+#
+$(DL_DIR)/$(BITLBEE_SOURCE):
+	$(WGET) -P $(DL_DIR) $(BITLBEE_SITE)/$(BITLBEE_SOURCE)
+
+#
+# The source code depends on it existing within the download directory.
+# This target will be called by the top level Makefile to download the
+# source code's archive (.tar.gz, .bz2, etc.)
+#
+bitlbee-source: $(DL_DIR)/$(BITLBEE_SOURCE) $(BITLBEE_PATCHES)
+
+#
+# This target unpacks the source code in the build directory.
+# If the source archive is not .tar.gz or .tar.bz2, then you will need
+# to change the commands here.  Patches to the source code are also
+# applied in this target as required.
+#
+# This target also configures the build within the build directory.
+# Flags such as LDFLAGS and CPPFLAGS should be passed into configure
+# and NOT $(MAKE) below.  Passing it to configure causes configure to
+# correctly BUILD the Makefile with the right paths, where passing it
+# to Make causes it to override the default search paths of the compiler.
+#
+# If the compilation of the package requires other packages to be staged
+# first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
+#
+$(BITLBEE_BUILD_DIR)/.configured: $(DL_DIR)/$(BITLBEE_SOURCE) $(BITLBEE_PATCHES)
+	$(MAKE) glib-stage gnutls-stage
+	rm -rf $(BUILD_DIR)/$(BITLBEE_DIR) $(BITLBEE_BUILD_DIR)
+	$(BITLBEE_UNZIP) $(DL_DIR)/$(BITLBEE_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+	cat $(BITLBEE_PATCHES) | patch -d $(BUILD_DIR)/$(BITLBEE_DIR) -p1
+	mv $(BUILD_DIR)/$(BITLBEE_DIR) $(BITLBEE_BUILD_DIR)
+	(cd $(BITLBEE_BUILD_DIR); \
+		$(TARGET_CONFIGURE_OPTS) \
+		CPPFLAGS="$(STAGING_CPPFLAGS) $(BITLBEE_CPPFLAGS)" \
+		LDFLAGS="$(STAGING_LDFLAGS) $(BITLBEE_LDFLAGS)" \
+                PKG_CONFIG_PATH="$(STAGING_LIB_DIR)/pkgconfig" \
+                PKG_CONFIG_LIBDIR="$(STAGING_LIB_DIR)/pkgconfig" \
+		STAGING_DIR=$(STAGING_DIR) \
+		./configure \
+		--prefix=/opt \
+		--cpu=armv5b \
+		--ssl=gnutls \
+		--msn=1 \
+		--yahoo=1 \
+		--mandir=/opt/man \
+		--datadir=/opt/var/bitlbee \
+		--config=/opt/var/bitlbee \
+	)
+	touch $(BITLBEE_BUILD_DIR)/.configured
+
+bitlbee-unpack: $(BITLBEE_BUILD_DIR)/.configured
+
+#
+# This builds the actual binary.
+#
+$(BITLBEE_BUILD_DIR)/.built: $(BITLBEE_BUILD_DIR)/.configured
+	rm -f $(BITLBEE_BUILD_DIR)/.built
+	$(MAKE) -C $(BITLBEE_BUILD_DIR)
+	touch $(BITLBEE_BUILD_DIR)/.built
+
+#
+# This is the build convenience target.
+#
+bitlbee: $(BITLBEE_BUILD_DIR)/.built
+
+#
+# If you are building a library, then you need to stage it too.
+#
+$(BITLBEE_BUILD_DIR)/.staged: $(BITLBEE_BUILD_DIR)/.built
+	rm -f $(BITLBEE_BUILD_DIR)/.staged
+	$(MAKE) -C $(BITLBEE_BUILD_DIR) DESTDIR=$(STAGING_DIR) install
+	touch $(BITLBEE_BUILD_DIR)/.staged
+
+bitlbee-stage: $(BITLBEE_BUILD_DIR)/.staged
+
+#
+# This rule creates a control file for ipkg.  It is no longer
+# necessary to create a seperate control file under sources/bitlbee
+#
+$(BITLBEE_IPK_DIR)/CONTROL/control:
+	@install -d $(BITLBEE_IPK_DIR)/CONTROL
+	@rm -f $@
+	@echo "Package: bitlbee" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(BITLBEE_PRIORITY)" >>$@
+	@echo "Section: $(BITLBEE_SECTION)" >>$@
+	@echo "Version: $(BITLBEE_VERSION)-$(BITLBEE_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(BITLBEE_MAINTAINER)" >>$@
+	@echo "Source: $(BITLBEE_SITE)/$(BITLBEE_SOURCE)" >>$@
+	@echo "Description: $(BITLBEE_DESCRIPTION)" >>$@
+	@echo "Depends: $(BITLBEE_DEPENDS)" >>$@
+	@echo "Suggests: $(BITLBEE_SUGGESTS)" >>$@
+	@echo "Conflicts: $(BITLBEE_CONFLICTS)" >>$@
+
+#
+# This builds the IPK file.
+#
+# Binaries should be installed into $(BITLBEE_IPK_DIR)/opt/sbin or $(BITLBEE_IPK_DIR)/opt/bin
+# (use the location in a well-known Linux distro as a guide for choosing sbin or bin).
+# Libraries and include files should be installed into $(BITLBEE_IPK_DIR)/opt/{lib,include}
+# Configuration files should be installed in $(BITLBEE_IPK_DIR)/opt/etc/bitlbee/...
+# Documentation files should be installed in $(BITLBEE_IPK_DIR)/opt/doc/bitlbee/...
+# Daemon startup scripts should be installed in $(BITLBEE_IPK_DIR)/opt/etc/init.d/S??bitlbee
+#
+# You may need to patch your application to make it use these locations.
+#
+$(BITLBEE_IPK): $(BITLBEE_BUILD_DIR)/.built
+	rm -rf $(BITLBEE_IPK_DIR) $(BUILD_DIR)/bitlbee_*_$(TARGET_ARCH).ipk
+	$(MAKE) -C $(BITLBEE_BUILD_DIR) DESTDIR=$(BITLBEE_IPK_DIR) install
+	install -d $(BITLBEE_IPK_DIR)/opt/etc/bitlbee
+	install -m 644 $(BITLBEE_BUILD_DIR)/bitlbee.conf $(BITLBEE_IPK_DIR)/opt/etc/bitlbee/bitlbee.conf
+	install -d $(BITLBEE_IPK_DIR)/opt/etc/xinetd.d
+	install -m 755 $(BITLBEE_SOURCE_DIR)/xinetd.bitlbee $(BITLBEE_IPK_DIR)/opt/etc/xinetd.d/bitlbee
+	$(MAKE) $(BITLBEE_IPK_DIR)/CONTROL/control
+	install -m 755 $(BITLBEE_SOURCE_DIR)/postinst $(BITLBEE_IPK_DIR)/CONTROL/postinst
+	#install -m 755 $(BITLBEE_SOURCE_DIR)/prerm $(BITLBEE_IPK_DIR)/CONTROL/prerm
+	echo $(BITLBEE_CONFFILES) | sed -e 's/ /\n/g' > $(BITLBEE_IPK_DIR)/CONTROL/conffiles
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(BITLBEE_IPK_DIR)
+
+#
+# This is called from the top level makefile to create the IPK file.
+#
+bitlbee-ipk: $(BITLBEE_IPK)
+
+#
+# This is called from the top level makefile to clean all of the built files.
+#
+bitlbee-clean:
+	-$(MAKE) -C $(BITLBEE_BUILD_DIR) clean
+
+#
+# This is called from the top level makefile to clean all dynamically created
+# directories.
+#
+bitlbee-dirclean:
+	rm -rf $(BUILD_DIR)/$(BITLBEE_DIR) $(BITLBEE_BUILD_DIR) $(BITLBEE_IPK_DIR) $(BITLBEE_IPK)
---
+++ make/libtasn1.mk
@@ -0,0 +1,214 @@
+###########################################################
+#
+# libtasn1
+#
+###########################################################
+
+# You must replace "libtasn1" and "LIBTASN1" with the lower case name and
+# upper case name of your new package.  Some places below will say
+# "Do not change this" - that does not include this global change,
+# which must always be done to ensure we have unique names.
+
+#
+# LIBTASN1_VERSION, LIBTASN1_SITE and LIBTASN1_SOURCE define
+# the upstream location of the source code for the package.
+# LIBTASN1_DIR is the directory which is created when the source
+# archive is unpacked.
+# LIBTASN1_UNZIP is the command used to unzip the source.
+# It is usually "zcat" (for .gz) or "bzcat" (for .bz2)
+#
+# You should change all these variables to suit your package.
+# Please make sure that you add a description, and that you
+# list all your packages' dependencies, seperated by commas.
+# 
+# If you list yourself as MAINTAINER, please give a valid email
+# address, and indicate your irc nick if it cannot be easily deduced
+# from your name or email address.  If you leave MAINTAINER set to
+# "NSLU2 Linux" other developers will feel free to edit.
+#
+LIBTASN1_SITE=ftp://ftp.gnutls.org/pub/gnutls/libtasn1
+LIBTASN1_VERSION=0.2.13
+LIBTASN1_SOURCE=libtasn1-$(LIBTASN1_VERSION).tar.gz
+LIBTASN1_DIR=libtasn1-$(LIBTASN1_VERSION)
+LIBTASN1_UNZIP=zcat
+LIBTASN1_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
+LIBTASN1_DESCRIPTION=Describe libtasn1 here.
+LIBTASN1_SECTION=libs
+LIBTASN1_PRIORITY=optional
+LIBTASN1_DEPENDS=
+LIBTASN1_SUGGESTS=
+LIBTASN1_CONFLICTS=
+
+#
+# LIBTASN1_IPK_VERSION should be incremented when the ipk changes.
+#
+LIBTASN1_IPK_VERSION=1
+
+#
+# LIBTASN1_CONFFILES should be a list of user-editable files
+LIBTASN1_CONFFILES=#/opt/etc/libtasn1.conf /opt/etc/init.d/SXXlibtasn1
+
+#
+# LIBTASN1_PATCHES should list any patches, in the the order in
+# which they should be applied to the source code.
+#
+LIBTASN1_PATCHES=#$(LIBTASN1_SOURCE_DIR)/configure.patch
+
+#
+# If the compilation of the package requires additional
+# compilation or linking flags, then list them here.
+#
+LIBTASN1_CPPFLAGS=
+LIBTASN1_LDFLAGS=
+
+#
+# LIBTASN1_BUILD_DIR is the directory in which the build is done.
+# LIBTASN1_SOURCE_DIR is the directory which holds all the
+# patches and ipkg control files.
+# LIBTASN1_IPK_DIR is the directory in which the ipk is built.
+# LIBTASN1_IPK is the name of the resulting ipk files.
+#
+# You should not change any of these variables.
+#
+LIBTASN1_BUILD_DIR=$(BUILD_DIR)/libtasn1
+LIBTASN1_SOURCE_DIR=$(SOURCE_DIR)/libtasn1
+LIBTASN1_IPK_DIR=$(BUILD_DIR)/libtasn1-$(LIBTASN1_VERSION)-ipk
+LIBTASN1_IPK=$(BUILD_DIR)/libtasn1_$(LIBTASN1_VERSION)-$(LIBTASN1_IPK_VERSION)_$(TARGET_ARCH).ipk
+
+#
+# This is the dependency on the source code.  If the source is missing,
+# then it will be fetched from the site using wget.
+#
+$(DL_DIR)/$(LIBTASN1_SOURCE):
+	$(WGET) -P $(DL_DIR) $(LIBTASN1_SITE)/$(LIBTASN1_SOURCE)
+
+#
+# The source code depends on it existing within the download directory.
+# This target will be called by the top level Makefile to download the
+# source code's archive (.tar.gz, .bz2, etc.)
+#
+libtasn1-source: $(DL_DIR)/$(LIBTASN1_SOURCE) $(LIBTASN1_PATCHES)
+
+#
+# This target unpacks the source code in the build directory.
+# If the source archive is not .tar.gz or .tar.bz2, then you will need
+# to change the commands here.  Patches to the source code are also
+# applied in this target as required.
+#
+# This target also configures the build within the build directory.
+# Flags such as LDFLAGS and CPPFLAGS should be passed into configure
+# and NOT $(MAKE) below.  Passing it to configure causes configure to
+# correctly BUILD the Makefile with the right paths, where passing it
+# to Make causes it to override the default search paths of the compiler.
+#
+# If the compilation of the package requires other packages to be staged
+# first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
+#
+$(LIBTASN1_BUILD_DIR)/.configured: $(DL_DIR)/$(LIBTASN1_SOURCE) $(LIBTASN1_PATCHES)
+	#$(MAKE) <bar>-stage <baz>-stage
+	rm -rf $(BUILD_DIR)/$(LIBTASN1_DIR) $(LIBTASN1_BUILD_DIR)
+	$(LIBTASN1_UNZIP) $(DL_DIR)/$(LIBTASN1_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+	#cat $(LIBTASN1_PATCHES) | patch -d $(BUILD_DIR)/$(LIBTASN1_DIR) -p1
+	mv $(BUILD_DIR)/$(LIBTASN1_DIR) $(LIBTASN1_BUILD_DIR)
+	(cd $(LIBTASN1_BUILD_DIR); \
+		$(TARGET_CONFIGURE_OPTS) \
+		CPPFLAGS="$(STAGING_CPPFLAGS) $(LIBTASN1_CPPFLAGS)" \
+		LDFLAGS="$(STAGING_LDFLAGS) $(LIBTASN1_LDFLAGS)" \
+		./configure \
+		--build=$(GNU_HOST_NAME) \
+		--host=$(GNU_TARGET_NAME) \
+		--target=$(GNU_TARGET_NAME) \
+		--prefix=/opt \
+		--disable-nls \
+	)
+	touch $(LIBTASN1_BUILD_DIR)/.configured
+
+libtasn1-unpack: $(LIBTASN1_BUILD_DIR)/.configured
+
+#
+# This builds the actual binary.
+#
+$(LIBTASN1_BUILD_DIR)/.built: $(LIBTASN1_BUILD_DIR)/.configured
+	rm -f $(LIBTASN1_BUILD_DIR)/.built
+	$(MAKE) -C $(LIBTASN1_BUILD_DIR)
+	touch $(LIBTASN1_BUILD_DIR)/.built
+
+#
+# This is the build convenience target.
+#
+libtasn1: $(LIBTASN1_BUILD_DIR)/.built
+
+#
+# If you are building a library, then you need to stage it too.
+#
+$(LIBTASN1_BUILD_DIR)/.staged: $(LIBTASN1_BUILD_DIR)/.built
+	rm -f $(LIBTASN1_BUILD_DIR)/.staged
+	$(MAKE) -C $(LIBTASN1_BUILD_DIR) DESTDIR=$(STAGING_DIR) install
+	QUOTED_STAGING_DIR=`echo $(STAGING_DIR) | sed -e "s/\\\//\\\\\\\\\//g"`; sed -e "s/'\/opt\/lib/'$$QUOTED_STAGING_DIR\/opt\/lib/" <$(STAGING_DIR)/opt/lib/libtasn1.la >$(STAGING_DIR)/opt/lib/libtasn1.la.tmp
+	mv $(STAGING_DIR)/opt/lib/libtasn1.la.tmp $(STAGING_DIR)/opt/lib/libtasn1.la
+	touch $(LIBTASN1_BUILD_DIR)/.staged
+
+libtasn1-stage: $(LIBTASN1_BUILD_DIR)/.staged
+
+#
+# This rule creates a control file for ipkg.  It is no longer
+# necessary to create a seperate control file under sources/libtasn1
+#
+$(LIBTASN1_IPK_DIR)/CONTROL/control:
+	@install -d $(LIBTASN1_IPK_DIR)/CONTROL
+	@rm -f $@
+	@echo "Package: libtasn1" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(LIBTASN1_PRIORITY)" >>$@
+	@echo "Section: $(LIBTASN1_SECTION)" >>$@
+	@echo "Version: $(LIBTASN1_VERSION)-$(LIBTASN1_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(LIBTASN1_MAINTAINER)" >>$@
+	@echo "Source: $(LIBTASN1_SITE)/$(LIBTASN1_SOURCE)" >>$@
+	@echo "Description: $(LIBTASN1_DESCRIPTION)" >>$@
+	@echo "Depends: $(LIBTASN1_DEPENDS)" >>$@
+	@echo "Suggests: $(LIBTASN1_SUGGESTS)" >>$@
+	@echo "Conflicts: $(LIBTASN1_CONFLICTS)" >>$@
+
+#
+# This builds the IPK file.
+#
+# Binaries should be installed into $(LIBTASN1_IPK_DIR)/opt/sbin or $(LIBTASN1_IPK_DIR)/opt/bin
+# (use the location in a well-known Linux distro as a guide for choosing sbin or bin).
+# Libraries and include files should be installed into $(LIBTASN1_IPK_DIR)/opt/{lib,include}
+# Configuration files should be installed in $(LIBTASN1_IPK_DIR)/opt/etc/libtasn1/...
+# Documentation files should be installed in $(LIBTASN1_IPK_DIR)/opt/doc/libtasn1/...
+# Daemon startup scripts should be installed in $(LIBTASN1_IPK_DIR)/opt/etc/init.d/S??libtasn1
+#
+# You may need to patch your application to make it use these locations.
+#
+$(LIBTASN1_IPK): $(LIBTASN1_BUILD_DIR)/.built
+	rm -rf $(LIBTASN1_IPK_DIR) $(BUILD_DIR)/libtasn1_*_$(TARGET_ARCH).ipk
+	$(MAKE) -C $(LIBTASN1_BUILD_DIR) DESTDIR=$(LIBTASN1_IPK_DIR) install
+	rm -r $(LIBTASN1_IPK_DIR)/opt/info
+	#install -d $(LIBTASN1_IPK_DIR)/opt/etc/
+	#install -m 644 $(LIBTASN1_SOURCE_DIR)/libtasn1.conf $(LIBTASN1_IPK_DIR)/opt/etc/libtasn1.conf
+	#install -d $(LIBTASN1_IPK_DIR)/opt/etc/init.d
+	#install -m 755 $(LIBTASN1_SOURCE_DIR)/rc.libtasn1 $(LIBTASN1_IPK_DIR)/opt/etc/init.d/SXXlibtasn1
+	$(MAKE) $(LIBTASN1_IPK_DIR)/CONTROL/control
+	#install -m 755 $(LIBTASN1_SOURCE_DIR)/postinst $(LIBTASN1_IPK_DIR)/CONTROL/postinst
+	#install -m 755 $(LIBTASN1_SOURCE_DIR)/prerm $(LIBTASN1_IPK_DIR)/CONTROL/prerm
+	echo $(LIBTASN1_CONFFILES) | sed -e 's/ /\n/g' > $(LIBTASN1_IPK_DIR)/CONTROL/conffiles
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(LIBTASN1_IPK_DIR)
+
+#
+# This is called from the top level makefile to create the IPK file.
+#
+libtasn1-ipk: $(LIBTASN1_IPK)
+
+#
+# This is called from the top level makefile to clean all of the built files.
+#
+libtasn1-clean:
+	-$(MAKE) -C $(LIBTASN1_BUILD_DIR) clean
+
+#
+# This is called from the top level makefile to clean all dynamically created
+# directories.
+#
+libtasn1-dirclean:
+	rm -rf $(BUILD_DIR)/$(LIBTASN1_DIR) $(LIBTASN1_BUILD_DIR) $(LIBTASN1_IPK_DIR) $(LIBTASN1_IPK)
