cc454290b215116accf7f25104a42586fd3b3538
jp30 <jp30@1f1dca69-5516-0410-b834-e485effd04c3>
2005-07-19 17:18:04 +0000

    libtool fixes, strip

--- make/cyrus-sasl.mk
+++ make/cyrus-sasl.mk
@@ -16,7 +16,7 @@ CYRUS-SASL_PRIORITY=optional
 CYRUS-SASL_DEPENDS=
 CYRUS-SASL_CONFLICTS=
 
-CYRUS-SASL_IPK_VERSION=7
+CYRUS-SASL_IPK_VERSION=8
 
 CYRUS-SASL_CONFFILES=/opt/etc/init.d/S52saslauthd
 
@@ -63,7 +63,9 @@ $(CYRUS-SASL_BUILD_DIR)/.configured: $(DL_DIR)/$(CYRUS-SASL_SOURCE) $(CYRUS-SASL
 		--disable-otp \
 		--disable-krb4 \
 		--disable-nls \
+		--disable-static \
 	)
+	$(PATCH_LIBTOOL) $(CYRUS-SASL_BUILD_DIR)/libtool
 	touch $(CYRUS-SASL_BUILD_DIR)/.configured
 
 cyrus-sasl-unpack: $(CYRUS-SASL_BUILD_DIR)/.configured
@@ -78,6 +80,7 @@ cyrus-sasl: $(CYRUS-SASL_BUILD_DIR)/.built
 $(CYRUS-SASL_BUILD_DIR)/.staged: $(CYRUS-SASL_BUILD_DIR)/.built
 	rm -f $(CYRUS-SASL_BUILD_DIR)/.staged
 	$(MAKE) -C $(CYRUS-SASL_BUILD_DIR) DESTDIR=$(STAGING_DIR) install
+	rm -f $(STAGING_LIB_DIR)/libsasl2.la
 	touch $(CYRUS-SASL_BUILD_DIR)/.staged
 
 cyrus-sasl-stage: $(CYRUS-SASL_BUILD_DIR)/.staged
@@ -118,8 +121,12 @@ $(CYRUS-SASL_IPK): $(CYRUS-SASL_BUILD_DIR)/.built
 	rm -rf $(CYRUS-SASL_IPK_DIR) $(BUILD_DIR)/cyrus-sasl_*_$(TARGET_ARCH).ipk
 	rm -rf $(CYRUS-SASL-LIBS_IPK_DIR) $(BUILD_DIR)/cyrus-sasl-libs_*_$(TARGET_ARCH).ipk
 	$(MAKE) -C $(CYRUS-SASL_BUILD_DIR) DESTDIR=$(CYRUS-SASL_IPK_DIR) install-strip
+	rm -f $(CYRUS-SASL_IPK_DIR)/opt/lib/libsasl2.la
+	rm -f $(CYRUS-SASL_IPK_DIR)/opt/lib/sasl2/*.la
 	find $(CYRUS-SASL_IPK_DIR) -type d -exec chmod go+rx {} \;
 	$(STRIP_COMMAND) $(CYRUS-SASL_IPK_DIR)/opt/sbin/*
+	$(STRIP_COMMAND) $(CYRUS-SASL_IPK_DIR)/opt/lib/*.so
+	$(STRIP_COMMAND) $(CYRUS-SASL_IPK_DIR)/opt/lib/sasl2/*.so
 	install -d $(CYRUS-SASL_IPK_DIR)/opt/var/state/saslauthd
 	install -d $(CYRUS-SASL_IPK_DIR)/opt/etc/init.d
 	install -m 755 $(CYRUS-SASL_SOURCE_DIR)/rc.saslauthd $(CYRUS-SASL_IPK_DIR)/opt/etc/init.d/S52saslauthd
