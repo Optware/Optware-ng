059b64c2f29f164272f701b0a873a5c28cc55f67
stripwax <stripwax@1f1dca69-5516-0410-b834-e485effd04c3>
2005-08-10 07:45:20 +0000

    First cut of a package for UnRAR

---
+++ trunk/make/unrar.mk
@@ -0,0 +1,85 @@
+#############################################################
+#
+# unrar
+#
+#############################################################
+
+UNRAR_SITE=http://www.rarlab.com/rar
+UNRAR_VERSION:=3.5.3
+UNRAR_SOURCE=unrarsrc-$(UNRAR_VERSION).tar.gz
+UNRAR_DIR=unrarsrc-$(UNRAR_VERSION)
+UNRAR_UNZIP=gunzip
+UNRAR_MAINTAINER=stripwax <stripwax@sourceforge.net>
+UNRAR_DESCRIPTION=unrar is an application that can decompress files and archives created using the RAR compression scheme
+UNRAR_SECTION=apps
+UNRAR_PRIORITY=optional
+UNRAR_DEPENDS=libstdc++
+UNRAR_CONFLICTS=
+
+UNRAR_IPK_VERSION=1
+
+UNRAR_CFLAGS= $(TARGET_CFLAGS) -fPIC -DBIG_ENDIAN
+
+UNRAR_BUILD_DIR=$(BUILD_DIR)/unrar
+UNRAR_SOURCE_DIR=$(SOURCE_DIR)/unrar
+UNRAR_IPK_DIR=$(BUILD_DIR)/unrar-$(UNRAR_VERSION)-ipk
+UNRAR_IPK=$(BUILD_DIR)/unrar_$(UNRAR_VERSION)-$(UNRAR_IPK_VERSION)_$(TARGET_ARCH).ipk
+
+$(DL_DIR)/$(UNRAR_SOURCE):
+	$(WGET) -P $(DL_DIR) $(UNRAR_SITE)/$(UNRAR_SOURCE)
+
+unrar-source: $(DL_DIR)/$(UNRAR_SOURCE)
+
+$(UNRAR_BUILD_DIR)/.configured: $(DL_DIR)/$(UNRAR_SOURCE)
+	$(MAKE) libstdc++-stage
+	rm -rf $(BUILD_DIR)/$(UNRAR_DIR) $(UNRAR_BUILD_DIR)
+	tar -C $(BUILD_DIR) -xzvf $(DL_DIR)/$(UNRAR_SOURCE)
+	(cd $(UNRAR_BUILD_DIR); \
+	 	sed -ir 's/#define( *)LITTLE_ENDIAN/#define BIG_ENDIAN/' os.hpp ; \
+		cp makefile.unix Makefile ; \
+		sed -ir 's/^CXXFLAGS=(.*)$$/CXXFLAGS=$$\(CFLAGS\)/' Makefile \
+	)
+	touch $(UNRAR_BUILD_DIR)/.configured
+
+unrar-unpack: $(UNRAR_BUILD_DIR)/.configured
+
+$(UNRAR_BUILD_DIR)/unrar: $(UNRAR_BUILD_DIR)/.configured
+	$(MAKE) RANLIB="$(TARGET_RANLIB)" AR="$(TARGET_AR) rc" \
+		CFLAGS="$(UNRAR_CFLAGS)" CC=$(TARGET_CC) -C $(UNRAR_BUILD_DIR) \
+		LDFLAGS="$(STAGING_LDFLAGS)"
+
+unrar: $(UNRAR_BUILD_DIR)/unrar
+
+#
+# This rule creates a control file for ipkg.  It is no longer
+# necessary to create a seperate control file under sources/nylon
+#
+$(UNRAR_IPK_DIR)/CONTROL/control:
+	@install -d $(UNRAR_IPK_DIR)/CONTROL
+	@rm -f $@
+	@echo "Package: unrar" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(UNRAR_PRIORITY)" >>$@
+	@echo "Section: $(UNRAR_SECTION)" >>$@
+	@echo "Version: $(UNRAR_VERSION)-$(UNRAR_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(UNRAR_MAINTAINER)" >>$@
+	@echo "Source: $(UNRAR_SITE)/$(UNRAR_SOURCE)" >>$@
+	@echo "Description: $(UNRAR_DESCRIPTION)" >>$@
+	@echo "Depends: $(UNRAR_DEPENDS)" >>$@
+	@echo "Conflicts: $(UNRAR_CONFLICTS)" >>$@
+
+$(UNRAR_IPK): $(UNRAR_BUILD_DIR)/unrar
+	rm -rf $(UNRAR_IPK_DIR) $(BUILD_DIR)/unrar_*_$(TARGET_ARCH).ipk
+	install -d $(UNRAR_IPK_DIR)/opt/bin
+	install -m 755 $(UNRAR_BUILD_DIR)/unrar $(UNRAR_IPK_DIR)/opt/bin
+	$(STRIP_COMMAND) $(UNRAR_IPK_DIR)/opt/bin/unrar
+	$(MAKE) $(UNRAR_IPK_DIR)/CONTROL/control
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(UNRAR_IPK_DIR)
+
+unrar-ipk: $(UNRAR_IPK)
+
+unrar-clean:
+	-$(MAKE) -C $(UNRAR_BUILD_DIR) clean
+
+unrar-dirclean:
+	rm -rf $(BUILD_DIR)/$(UNRAR_DIR) $(UNRAR_BUILD_DIR) $(UNRAR_IPK_DIR) $(UNRAR_IPK)
