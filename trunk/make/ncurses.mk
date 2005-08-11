b8cc05f934a6a240bcb02fb13853377fc7e97094
jp30 <jp30@1f1dca69-5516-0410-b834-e485effd04c3>
2005-08-11 16:32:21 +0000

    ncurses: use ln -sf to avoid error when restaging

--- trunk/make/ncurses.mk
+++ trunk/make/ncurses.mk
@@ -61,7 +61,7 @@ ncurses: $(NCURSES_DIR)/lib/libncurses.so.$(NCURSES_SHLIBVERSION)
 
 $(STAGING_DIR)/opt/lib/libncurses.so.$(NCURSES_SHLIBVERSION): $(NCURSES_DIR)/lib/libncurses.so.$(NCURSES_SHLIBVERSION)
 	$(MAKE) -C $(NCURSES_DIR) DESTDIR=$(STAGING_DIR) install.includes install.libs
-	ln -s ncurses/ncurses.h $(STAGING_INCLUDE_DIR)
+	ln -sf ncurses/ncurses.h $(STAGING_INCLUDE_DIR)
 
 ncurses-stage: $(STAGING_DIR)/opt/lib/libncurses.so.$(NCURSES_SHLIBVERSION)
 
