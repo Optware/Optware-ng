9fa28b6388004a34052737dca18ab2819ce3d5d8
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2005-08-19 13:51:20 +0000

    Fixed /dev/loop -> /dev/loop0

--- trunk/make/ufsd.mk
+++ trunk/make/ufsd.mk
@@ -33,7 +33,7 @@ UFSD_CONFLICTS=
 #
 # UFSD_IPK_VERSION should be incremented when the ipk changes.
 #
-UFSD_IPK_VERSION=2
+UFSD_IPK_VERSION=3
 
 #
 # <FOO>_SOURCE_DIR is the directory which holds all the
