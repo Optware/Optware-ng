8f2dc2a949f16d9a6f836281331efdf891b5038e
orepvik <orepvik@1f1dca69-5516-0410-b834-e485effd04c3>
2005-09-02 07:38:27 +0000

    Add basic "error-checking" to bootstrap.sh

--- sources/ds101-bootstrap/bootstrap.sh
+++ sources/ds101-bootstrap/bootstrap.sh
@@ -13,10 +13,10 @@ export IPKG_CONF_DIR=$BSDIR
 export IPKG_DIR_PREFIX=$BSDIR 
 
 echo "Installing DS101(g)-bootstrap package..."
-sh ./ipkg.sh install ds101*-bootstrap_0.1-1_*.ipk
+sh ./ipkg.sh install ds101*-bootstrap_0.1-1_*.ipk || exit 1
 
 echo "Installing IPKG package..."
-sh ./ipkg.sh install ipkg_0.99-148-1_*.ipk
+sh ./ipkg.sh install ipkg_0.99-148-1_*.ipk || exit 1
 
 echo "Removing temporary ipkg repository..."
 rm -rf $BSDIR
