3fb766dd603d951d48ba65ce6cc9c2601a41cc46
orepvik <orepvik@1f1dca69-5516-0410-b834-e485effd04c3>
2005-09-02 07:21:29 +0000

    Added ds101 bootstrap files

---
+++ trunk/sources/ds101-bootstrap/bootstrap.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+BSDIR="/tmp/ipkg-bootstrap"
+
+echo "Creating temporary ipkg repository..."
+rm -rf $BSDIR
+mkdir -p $BSDIR
+cat >>$BSDIR/ipkg.conf <<EOF
+dest root /
+lists_dir ext /$BSDIR/ipkg
+EOF
+
+export IPKG_CONF_DIR=$BSDIR 
+export IPKG_DIR_PREFIX=$BSDIR 
+
+echo "Installing DS101(g)-bootstrap package..."
+sh ./ipkg.sh install ds101*-bootstrap_0.1-1_*.ipk
+
+echo "Installing IPKG package..."
+sh ./ipkg.sh install ipkg_0.99-148-1_*.ipk
+
+echo "Removing temporary ipkg repository..."
+rm -rf $BSDIR
+
+echo "OK, now call . /etc/profile"
