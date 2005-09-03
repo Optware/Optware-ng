fb50a0e06da821a38f50f7cab00da7addfc9870c
orepvik <orepvik@1f1dca69-5516-0410-b834-e485effd04c3>
2005-09-03 22:36:24 +0000

    Fix so it'll handle IPK_VERSION bumps

--- sources/ds101-bootstrap/bootstrap.sh
+++ sources/ds101-bootstrap/bootstrap.sh
@@ -13,7 +13,7 @@ export IPKG_CONF_DIR=$BSDIR
 export IPKG_DIR_PREFIX=$BSDIR 
 
 echo "Installing DS101(g)-bootstrap package..."
-sh ./ipkg.sh install ds101-bootstrap_0.1-1_*.ipk || exit 1
+sh ./ipkg.sh install ds101-bootstrap_0.1-?_*.ipk || exit 1
 
 echo "Installing IPKG package..."
 sh ./ipkg.sh install ipkg_0.99-148-1_*.ipk || exit 1
