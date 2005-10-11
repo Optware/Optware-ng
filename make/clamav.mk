a17e8fe8c128c19bc59eefcac2339ddc1f2956f1
caplink811 <caplink811@1f1dca69-5516-0410-b834-e485effd04c3>
2005-10-11 09:22:46 +0000

    changed the sourceversion to 0.87

--- make/clamav.mk
+++ make/clamav.mk
@@ -27,7 +27,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 CLAMAV_SITE=http://puzzle.dl.sourceforge.net/clamav
-CLAMAV_VERSION=0.84
+CLAMAV_VERSION=0.87
 CLAMAV_SOURCE=clamav-$(CLAMAV_VERSION).tar.gz
 CLAMAV_DIR=clamav-$(CLAMAV_VERSION)
 CLAMAV_UNZIP=zcat
@@ -42,7 +42,7 @@ CLAMAV_CONFLICTS=
 #
 # CLAMAV_IPK_VERSION should be incremented when the ipk changes.
 #
-CLAMAV_IPK_VERSION=2
+CLAMAV_IPK_VERSION=3
 
 #
 # CLAMAV_CONFFILES should be a list of user-editable files
