2acfa9eff8cbef8c63303f99279a9dda25fe0805
marceln <marceln@1f1dca69-5516-0410-b834-e485effd04c3>
2005-11-23 21:07:09 +0000

    Added as a request of "Otmar Spoettel"

---
+++ trunk/make/digitemp.mk
@@ -0,0 +1,193 @@
+###########################################################
+#
+# digitemp
+#
+###########################################################
+#
+# $Header$
+#
+# If you list yourself as MAINTAINER, please give a valid email
+# address, and indicate your irc nick if it cannot be easily deduced
+# from your name or email address.  If you leave MAINTAINER set to
+# "NSLU2 Linux" other developers will feel free to edit.
+#
+DIGITEMP_SITE=http://www.digitemp.com/software/linux
+DIGITEMP_VERSION=1.7
+DIGITEMP_SOURCE=digitemp-$(DIGITEMP_VERSION).tar.gz
+DIGITEMP_DIR=digitemp-$(DIGITEMP_VERSION)
+DIGITEMP_UNZIP=zcat
+DIGITEMP_MAINTAINER=Marcel Nijenhof <nslu2@pion.xs4all.nl>
+DIGITEMP_DESCRIPTION=Reads 1-Wire Temperature sensor PCB (http://www.digitemp.com)
+DIGITEMP_SECTION=misc
+DIGITEMP_PRIORITY=optional
+DIGITEMP_DEPENDS=
+DIGITEMP_SUGGESTS=
+DIGITEMP_CONFLICTS=
+
+#
+# DIGITEMP_IPK_VERSION should be incremented when the ipk changes.
+#
+DIGITEMP_IPK_VERSION=1
+
+#
+# DIGITEMP_CONFFILES should be a list of user-editable files
+# DIGITEMP_CONFFILES=/opt/etc/digitemp.conf /opt/etc/init.d/SXXdigitemp
+
+#
+# DIGITEMP_PATCHES should list any patches, in the the order in
+# which they should be applied to the source code.
+#
+# DIGITEMP_PATCHES=$(DIGITEMP_SOURCE_DIR)/configure.patch
+
+#
+# If the compilation of the package requires additional
+# compilation or linking flags, then list them here.
+#
+DIGITEMP_CPPFLAGS=
+DIGITEMP_LDFLAGS=
+
+#
+# DIGITEMP_BUILD_DIR is the directory in which the build is done.
+# DIGITEMP_SOURCE_DIR is the directory which holds all the
+# patches and ipkg control files.
+# DIGITEMP_IPK_DIR is the directory in which the ipk is built.
+# DIGITEMP_IPK is the name of the resulting ipk files.
+#
+# You should not change any of these variables.
+#
+DIGITEMP_BUILD_DIR=$(BUILD_DIR)/digitemp
+DIGITEMP_SOURCE_DIR=$(SOURCE_DIR)/digitemp
+DIGITEMP_IPK_DIR=$(BUILD_DIR)/digitemp-$(DIGITEMP_VERSION)-ipk
+DIGITEMP_IPK=$(BUILD_DIR)/digitemp_$(DIGITEMP_VERSION)-$(DIGITEMP_IPK_VERSION)_$(TARGET_ARCH).ipk
+
+#
+# This is the dependency on the source code.  If the source is missing,
+# then it will be fetched from the site using wget.
+#
+$(DL_DIR)/$(DIGITEMP_SOURCE):
+	$(WGET) -P $(DL_DIR) $(DIGITEMP_SITE)/$(DIGITEMP_SOURCE)
+
+#
+# The source code depends on it existing within the download directory.
+# This target will be called by the top level Makefile to download the
+# source code's archive (.tar.gz, .bz2, etc.)
+#
+digitemp-source: $(DL_DIR)/$(DIGITEMP_SOURCE) $(DIGITEMP_PATCHES)
+
+#
+# This target unpacks the source code in the build directory.
+# If the source archive is not .tar.gz or .tar.bz2, then you will need
+# to change the commands here.  Patches to the source code are also
+# applied in this target as required.
+#
+# This target also configures the build within the build directory.
+# Flags such as LDFLAGS and CPPFLAGS should be passed into configure
+# and NOT $(MAKE) below.  Passing it to configure causes configure to
+# correctly BUILD the Makefile with the right paths, where passing it
+# to Make causes it to override the default search paths of the compiler.
+#
+# If the compilation of the package requires other packages to be staged
+# first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
+#
+# If the package uses  GNU libtool, you should invoke $(PATCH_LIBTOOL) as
+# shown below to make various patches to it.
+#
+$(DIGITEMP_BUILD_DIR)/.configured: $(DL_DIR)/$(DIGITEMP_SOURCE) $(DIGITEMP_PATCHES)
+	# $(MAKE) <bar>-stage <baz>-stage
+	rm -rf $(BUILD_DIR)/$(DIGITEMP_DIR) $(DIGITEMP_BUILD_DIR)
+	$(DIGITEMP_UNZIP) $(DL_DIR)/$(DIGITEMP_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+	if test -n "$(DIGITEMP_PATCHES)" ; \
+		then cat $(DIGITEMP_PATCHES) | \
+		patch -d $(BUILD_DIR)/$(DIGITEMP_DIR) -p0 ; \
+	fi
+	if test "$(BUILD_DIR)/$(DIGITEMP_DIR)" != "$(DIGITEMP_BUILD_DIR)" ; \
+		then mv $(BUILD_DIR)/$(DIGITEMP_DIR) $(DIGITEMP_BUILD_DIR) ; \
+	fi
+	touch $(DIGITEMP_BUILD_DIR)/.configured
+	sed -i '/^CC/d' $(DIGITEMP_BUILD_DIR)/Makefile
+
+digitemp-unpack: $(DIGITEMP_BUILD_DIR)/.configured
+
+#
+# This builds the actual binary.
+#
+$(DIGITEMP_BUILD_DIR)/.built: $(DIGITEMP_BUILD_DIR)/.configured
+	echo
+	echo $(TARGET_CONFIGURE_OPTS)
+	echo
+	rm -f $(DIGITEMP_BUILD_DIR)/.built
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(DIGITEMP_BUILD_DIR) digitemp
+	touch $(DIGITEMP_BUILD_DIR)/.built
+
+#
+# This is the build convenience target.
+#
+digitemp: $(DIGITEMP_BUILD_DIR)/.built
+
+#
+# If you are building a library, then you need to stage it too.
+#
+$(DIGITEMP_BUILD_DIR)/.staged: $(DIGITEMP_BUILD_DIR)/.built
+	rm -f $(DIGITEMP_BUILD_DIR)/.staged
+	$(MAKE) -C $(DIGITEMP_BUILD_DIR) DESTDIR=$(STAGING_DIR) install
+	touch $(DIGITEMP_BUILD_DIR)/.staged
+
+digitemp-stage: $(DIGITEMP_BUILD_DIR)/.staged
+
+#
+# This rule creates a control file for ipkg.  It is no longer
+# necessary to create a seperate control file under sources/digitemp
+#
+$(DIGITEMP_IPK_DIR)/CONTROL/control:
+	@install -d $(DIGITEMP_IPK_DIR)/CONTROL
+	@rm -f $@
+	@echo "Package: digitemp" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(DIGITEMP_PRIORITY)" >>$@
+	@echo "Section: $(DIGITEMP_SECTION)" >>$@
+	@echo "Version: $(DIGITEMP_VERSION)-$(DIGITEMP_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(DIGITEMP_MAINTAINER)" >>$@
+	@echo "Source: $(DIGITEMP_SITE)/$(DIGITEMP_SOURCE)" >>$@
+	@echo "Description: $(DIGITEMP_DESCRIPTION)" >>$@
+	@echo "Depends: $(DIGITEMP_DEPENDS)" >>$@
+	@echo "Suggests: $(DIGITEMP_SUGGESTS)" >>$@
+	@echo "Conflicts: $(DIGITEMP_CONFLICTS)" >>$@
+
+#
+# This builds the IPK file.
+#
+# Binaries should be installed into $(DIGITEMP_IPK_DIR)/opt/sbin or $(DIGITEMP_IPK_DIR)/opt/bin
+# (use the location in a well-known Linux distro as a guide for choosing sbin or bin).
+# Libraries and include files should be installed into $(DIGITEMP_IPK_DIR)/opt/{lib,include}
+# Configuration files should be installed in $(DIGITEMP_IPK_DIR)/opt/etc/digitemp/...
+# Documentation files should be installed in $(DIGITEMP_IPK_DIR)/opt/doc/digitemp/...
+# Daemon startup scripts should be installed in $(DIGITEMP_IPK_DIR)/opt/etc/init.d/S??digitemp
+#
+# You may need to patch your application to make it use these locations.
+#
+$(DIGITEMP_IPK): $(DIGITEMP_BUILD_DIR)/.built
+	rm -rf $(DIGITEMP_IPK_DIR) $(BUILD_DIR)/digitemp_*_$(TARGET_ARCH).ipk
+	install -d $(DIGITEMP_IPK_DIR)/opt/bin
+	install -m 755 $(DIGITEMP_BUILD_DIR)/digitemp $(DIGITEMP_IPK_DIR)/opt/bin
+	$(STRIP_COMMAND) $(DIGITEMP_IPK_DIR)/opt/bin/digitemp
+	$(MAKE) $(DIGITEMP_IPK_DIR)/CONTROL/control
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(DIGITEMP_IPK_DIR)
+
+#
+# This is called from the top level makefile to create the IPK file.
+#
+digitemp-ipk: $(DIGITEMP_IPK)
+
+#
+# This is called from the top level makefile to clean all of the built files.
+#
+digitemp-clean:
+	rm -f $(DIGITEMP_BUILD_DIR)/.built
+	-$(MAKE) -C $(DIGITEMP_BUILD_DIR) clean
+
+#
+# This is called from the top level makefile to clean all dynamically created
+# directories.
+#
+digitemp-dirclean:
+	rm -rf $(BUILD_DIR)/$(DIGITEMP_DIR) $(DIGITEMP_BUILD_DIR) $(DIGITEMP_IPK_DIR) $(DIGITEMP_IPK)
