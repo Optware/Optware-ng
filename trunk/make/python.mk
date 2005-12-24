ef357ceffdde9341eb96914734fc35fddfe28944
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2005-12-24 04:01:49 +0000

    added zlib-stage and zlib as runtime dependency

--- trunk/make/python.mk
+++ trunk/make/python.mk
@@ -37,12 +37,12 @@ PYTHON_MAINTAINER=Brian Zhou<bzhou@users.sf.net>
 PYTHON_DESCRIPTION=Python is an interpreted, interactive, object-oriented programming language.
 PYTHON_SECTION=misc
 PYTHON_PRIORITY=optional
-PYTHON_DEPENDS=libstdc++, readline, ncurses, openssl, libdb
+PYTHON_DEPENDS=libstdc++, readline, ncurses, openssl, libdb, zlib
 
 #
 # PYTHON_IPK_VERSION should be incremented when the ipk changes.
 #
-PYTHON_IPK_VERSION=1
+PYTHON_IPK_VERSION=2
 
 #
 # PYTHON_CONFFILES should be a list of user-editable files
@@ -112,7 +112,7 @@ python-source: $(DL_DIR)/$(PYTHON_SOURCE) $(PYTHON_PATCHES)
 # first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
 #
 $(PYTHON_BUILD_DIR)/.configured: $(DL_DIR)/$(PYTHON_SOURCE) $(PYTHON_PATCHES)
-	make readline-stage ncurses-stage openssl-stage libdb-stage
+	make readline-stage ncurses-stage openssl-stage libdb-stage zlib-stage
 	rm -rf $(BUILD_DIR)/$(PYTHON_DIR) $(PYTHON_BUILD_DIR)
 	$(PYTHON_UNZIP) $(DL_DIR)/$(PYTHON_SOURCE) | tar -C $(BUILD_DIR) -xvf -
 	cd $(BUILD_DIR)/$(PYTHON_DIR); \
