ae9db975e9fd20a9576ac5b83c438a4674e924a6
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2006-01-23 21:25:41 +0000

    librsync and py-rdiff-backup ready for cross test

---
+++ trunk/make/py-rdiff-backup.mk
@@ -0,0 +1,203 @@
+###########################################################
+#
+# py-rdiff-backup
+#
+###########################################################
+
+#
+# PY-RDIFF-BACKUP_VERSION, PY-RDIFF-BACKUP_SITE and PY-RDIFF-BACKUP_SOURCE define
+# the upstream location of the source code for the package.
+# PY-RDIFF-BACKUP_DIR is the directory which is created when the source
+# archive is unpacked.
+# PY-RDIFF-BACKUP_UNZIP is the command used to unzip the source.
+# It is usually "zcat" (for .gz) or "bzcat" (for .bz2)
+#
+# You should change all these variables to suit your package.
+# Please make sure that you add a description, and that you
+# list all your packages' dependencies, seperated by commas.
+# 
+# If you list yourself as MAINTAINER, please give a valid email
+# address, and indicate your irc nick if it cannot be easily deduced
+# from your name or email address.  If you leave MAINTAINER set to
+# "NSLU2 Linux" other developers will feel free to edit.
+#
+PY-RDIFF-BACKUP_VERSION=1.1.5
+PY-RDIFF-BACKUP_SITE=http://savannah.nongnu.org/download/rdiff-backup
+PY-RDIFF-BACKUP_SOURCE=rdiff-backup-$(PY-RDIFF-BACKUP_VERSION).tar.gz
+PY-RDIFF-BACKUP_DIR=rdiff-backup-$(PY-RDIFF-BACKUP_VERSION)
+PY-RDIFF-BACKUP_UNZIP=zcat
+PY-RDIFF-BACKUP_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
+PY-RDIFF-BACKUP_DESCRIPTION=rdiff-backup backs up one directory to another, possibly over a network.
+PY-RDIFF-BACKUP_SECTION=misc
+PY-RDIFF-BACKUP_PRIORITY=optional
+PY-RDIFF-BACKUP_DEPENDS=python, librsync
+PY-RDIFF-BACKUP_CONFLICTS=
+
+#
+# PY-RDIFF-BACKUP_IPK_VERSION should be incremented when the ipk changes.
+#
+PY-RDIFF-BACKUP_IPK_VERSION=1
+
+#
+# PY-RDIFF-BACKUP_CONFFILES should be a list of user-editable files
+#PY-RDIFF-BACKUP_CONFFILES=/opt/etc/py-rdiff-backup.conf /opt/etc/init.d/SXXpy-rdiff-backup
+
+#
+# PY-RDIFF-BACKUP_PATCHES should list any patches, in the the order in
+# which they should be applied to the source code.
+#
+#PY-RDIFF-BACKUP_PATCHES=$(PY-RDIFF-BACKUP_SOURCE_DIR)/configure.patch
+
+#
+# If the compilation of the package requires additional
+# compilation or linking flags, then list them here.
+#
+PY-RDIFF-BACKUP_CPPFLAGS=
+PY-RDIFF-BACKUP_LDFLAGS=
+
+#
+# PY-RDIFF-BACKUP_BUILD_DIR is the directory in which the build is done.
+# PY-RDIFF-BACKUP_SOURCE_DIR is the directory which holds all the
+# patches and ipkg control files.
+# PY-RDIFF-BACKUP_IPK_DIR is the directory in which the ipk is built.
+# PY-RDIFF-BACKUP_IPK is the name of the resulting ipk files.
+#
+# You should not change any of these variables.
+#
+PY-RDIFF-BACKUP_BUILD_DIR=$(BUILD_DIR)/py-rdiff-backup
+PY-RDIFF-BACKUP_SOURCE_DIR=$(SOURCE_DIR)/py-rdiff-backup
+PY-RDIFF-BACKUP_IPK_DIR=$(BUILD_DIR)/py-rdiff-backup-$(PY-RDIFF-BACKUP_VERSION)-ipk
+PY-RDIFF-BACKUP_IPK=$(BUILD_DIR)/py-rdiff-backup_$(PY-RDIFF-BACKUP_VERSION)-$(PY-RDIFF-BACKUP_IPK_VERSION)_$(TARGET_ARCH).ipk
+
+#
+# This is the dependency on the source code.  If the source is missing,
+# then it will be fetched from the site using wget.
+#
+$(DL_DIR)/$(PY-RDIFF-BACKUP_SOURCE):
+	$(WGET) -P $(DL_DIR) $(PY-RDIFF-BACKUP_SITE)/$(PY-RDIFF-BACKUP_SOURCE)
+
+#
+# The source code depends on it existing within the download directory.
+# This target will be called by the top level Makefile to download the
+# source code's archive (.tar.gz, .bz2, etc.)
+#
+py-rdiff-backup-source: $(DL_DIR)/$(PY-RDIFF-BACKUP_SOURCE) $(PY-RDIFF-BACKUP_PATCHES)
+
+#
+# This target unpacks the source code in the build directory.
+# If the source archive is not .tar.gz or .tar.bz2, then you will need
+# to change the commands here.  Patches to the source code are also
+# applied in this target as required.
+#
+# This target also configures the build within the build directory.
+# Flags such as LDFLAGS and CPPFLAGS should be passed into configure
+# and NOT $(MAKE) below.  Passing it to configure causes configure to
+# correctly BUILD the Makefile with the right paths, where passing it
+# to Make causes it to override the default search paths of the compiler.
+#
+# If the compilation of the package requires other packages to be staged
+# first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
+#
+$(PY-RDIFF-BACKUP_BUILD_DIR)/.configured: $(DL_DIR)/$(PY-RDIFF-BACKUP_SOURCE) $(PY-RDIFF-BACKUP_PATCHES)
+	$(MAKE) librsync-stage
+	rm -rf $(BUILD_DIR)/$(PY-RDIFF-BACKUP_DIR) $(PY-RDIFF-BACKUP_BUILD_DIR)
+	$(PY-RDIFF-BACKUP_UNZIP) $(DL_DIR)/$(PY-RDIFF-BACKUP_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+#	cat $(PY-RDIFF-BACKUP_PATCHES) | patch -d $(BUILD_DIR)/$(PY-RDIFF-BACKUP_DIR) -p1
+	mv $(BUILD_DIR)/$(PY-RDIFF-BACKUP_DIR) $(PY-RDIFF-BACKUP_BUILD_DIR)
+	(cd $(PY-RDIFF-BACKUP_BUILD_DIR); \
+	    ( \
+		echo "[build_ext]"; \
+	        echo "include-dirs=$(STAGING_DIR)/opt/include"; \
+	        echo "library-dirs=$(STAGING_DIR)/opt/lib"; \
+	        echo "rpath=/opt/lib"; \
+		echo "[build_scripts]"; \
+		echo "executable=/opt/bin/python" \
+	    ) >> setup.cfg; \
+	)
+	touch $(PY-RDIFF-BACKUP_BUILD_DIR)/.configured
+
+py-rdiff-backup-unpack: $(PY-RDIFF-BACKUP_BUILD_DIR)/.configured
+
+#
+# This builds the actual binary.
+#
+$(PY-RDIFF-BACKUP_BUILD_DIR)/.built: $(PY-RDIFF-BACKUP_BUILD_DIR)/.configured
+	rm -f $(PY-RDIFF-BACKUP_BUILD_DIR)/.built
+	(cd $(PY-RDIFF-BACKUP_BUILD_DIR); \
+	 CC='$(TARGET_CC)' LDSHARED='$(TARGET_CC) -shared' \
+	    python2.4 setup.py build; \
+	)
+	touch $(PY-RDIFF-BACKUP_BUILD_DIR)/.built
+
+#
+# This is the build convenience target.
+#
+py-rdiff-backup: $(PY-RDIFF-BACKUP_BUILD_DIR)/.built
+
+#
+# If you are building a library, then you need to stage it too.
+#
+$(PY-RDIFF-BACKUP_BUILD_DIR)/.staged: $(PY-RDIFF-BACKUP_BUILD_DIR)/.built
+	rm -f $(PY-RDIFF-BACKUP_BUILD_DIR)/.staged
+	#$(MAKE) -C $(PY-RDIFF-BACKUP_BUILD_DIR) DESTDIR=$(STAGING_DIR) install
+	touch $(PY-RDIFF-BACKUP_BUILD_DIR)/.staged
+
+py-rdiff-backup-stage: $(PY-RDIFF-BACKUP_BUILD_DIR)/.staged
+
+#
+# This rule creates a control file for ipkg.  It is no longer
+# necessary to create a seperate control file under sources/py-rdiff-backup
+#
+$(PY-RDIFF-BACKUP_IPK_DIR)/CONTROL/control:
+	@install -d $(PY-RDIFF-BACKUP_IPK_DIR)/CONTROL
+	@rm -f $@
+	@echo "Package: py-rdiff-backup" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(PY-RDIFF-BACKUP_PRIORITY)" >>$@
+	@echo "Section: $(PY-RDIFF-BACKUP_SECTION)" >>$@
+	@echo "Version: $(PY-RDIFF-BACKUP_VERSION)-$(PY-RDIFF-BACKUP_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(PY-RDIFF-BACKUP_MAINTAINER)" >>$@
+	@echo "Source: $(PY-RDIFF-BACKUP_SITE)/$(PY-RDIFF-BACKUP_SOURCE)" >>$@
+	@echo "Description: $(PY-RDIFF-BACKUP_DESCRIPTION)" >>$@
+	@echo "Depends: $(PY-RDIFF-BACKUP_DEPENDS)" >>$@
+	@echo "Conflicts: $(PY-RDIFF-BACKUP_CONFLICTS)" >>$@
+
+#
+# This builds the IPK file.
+#
+# Binaries should be installed into $(PY-RDIFF-BACKUP_IPK_DIR)/opt/sbin or $(PY-RDIFF-BACKUP_IPK_DIR)/opt/bin
+# (use the location in a well-known Linux distro as a guide for choosing sbin or bin).
+# Libraries and include files should be installed into $(PY-RDIFF-BACKUP_IPK_DIR)/opt/{lib,include}
+# Configuration files should be installed in $(PY-RDIFF-BACKUP_IPK_DIR)/opt/etc/py-rdiff-backup/...
+# Documentation files should be installed in $(PY-RDIFF-BACKUP_IPK_DIR)/opt/doc/py-rdiff-backup/...
+# Daemon startup scripts should be installed in $(PY-RDIFF-BACKUP_IPK_DIR)/opt/etc/init.d/S??py-rdiff-backup
+#
+# You may need to patch your application to make it use these locations.
+#
+$(PY-RDIFF-BACKUP_IPK): $(PY-RDIFF-BACKUP_BUILD_DIR)/.built
+	rm -rf $(PY-RDIFF-BACKUP_IPK_DIR) $(BUILD_DIR)/py-rdiff-backup_*_$(TARGET_ARCH).ipk
+	(cd $(PY-RDIFF-BACKUP_BUILD_DIR); \
+	 CC='$(TARGET_CC)' LDSHARED='$(TARGET_CC) -shared' \
+	    python2.4 setup.py install --prefix=$(PY-RDIFF-BACKUP_IPK_DIR)/opt; \
+	)
+	$(STRIP_COMMAND) $(PY-RDIFF-BACKUP_IPK_DIR)/opt/lib/python2.4/site-packages/*/*.so
+	$(MAKE) $(PY-RDIFF-BACKUP_IPK_DIR)/CONTROL/control
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(PY-RDIFF-BACKUP_IPK_DIR)
+
+#
+# This is called from the top level makefile to create the IPK file.
+#
+py-rdiff-backup-ipk: $(PY-RDIFF-BACKUP_IPK)
+
+#
+# This is called from the top level makefile to clean all of the built files.
+#
+py-rdiff-backup-clean:
+	-$(MAKE) -C $(PY-RDIFF-BACKUP_BUILD_DIR) clean
+
+#
+# This is called from the top level makefile to clean all dynamically created
+# directories.
+#
+py-rdiff-backup-dirclean:
+	rm -rf $(BUILD_DIR)/$(PY-RDIFF-BACKUP_DIR) $(PY-RDIFF-BACKUP_BUILD_DIR) $(PY-RDIFF-BACKUP_IPK_DIR) $(PY-RDIFF-BACKUP_IPK)
