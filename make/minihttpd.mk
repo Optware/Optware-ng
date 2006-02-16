f282a4455d7cfcad0d54d3c1242668d53299ca26
oleon <oleon@1f1dca69-5516-0410-b834-e485effd04c3>
2006-02-16 09:30:24 +0000

    minihttpd: wrapper startup bugfixes

--- make/minihttpd.mk
+++ make/minihttpd.mk
@@ -37,7 +37,7 @@ MINIHTTPD_CONFLICTS=
 #
 # MINIHTTPD_IPK_VERSION should be incremented when the ipk changes.
 #
-MINIHTTPD_IPK_VERSION=1
+MINIHTTPD_IPK_VERSION=2
 
 #
 # MINIHTTPD_CONFFILES should be a list of user-editable files
