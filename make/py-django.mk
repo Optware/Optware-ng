4e2802fc02938a23bccc7876190ceff049615ef0
Brian Zhou <bzhou@users.noreply.github.com>
2006-02-27 04:48:43 +0000

    django easily built

---
+++ make/py-django.mk
@@ -0,0 +1,197 @@
+###########################################################
+#
+# py-django
+#
+###########################################################
+
+#
+# PY-DJANGO_VERSION, PY-DJANGO_SITE and PY-DJANGO_SOURCE define
+# the upstream location of the source code for the package.
+# PY-DJANGO_DIR is the directory which is created when the source
+# archive is unpacked.
+# PY-DJANGO_UNZIP is the command used to unzip the source.
+# It is usually "zcat" (for .gz) or "bzcat" (for .bz2)
+#
+# You should change all these variables to suit your package.
+# Please make sure that you add a description, and that you
+# list all your packages' dependencies, seperated by commas.
+# 
+# If you list yourself as MAINTAINER, please give a valid email
+# address, and indicate your irc nick if it cannot be easily deduced
+# from your name or email address.  If you leave MAINTAINER set to
+# "NSLU2 Linux" other developers will feel free to edit.
+#
+PY-DJANGO_VERSION=0.91
+PY-DJANGO_SITE=http://www.djangoproject.com/download/$(PY-DJANGO_VERSION)/tarball/
+PY-DJANGO_SOURCE=Django-$(PY-DJANGO_VERSION).tar.gz
+PY-DJANGO_DIR=Django-$(PY-DJANGO_VERSION)
+PY-DJANGO_UNZIP=zcat
+PY-DJANGO_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
+PY-DJANGO_DESCRIPTION=A high-level Python Web framework that encourages rapid development and clean, pragmatic design.
+PY-DJANGO_SECTION=misc
+PY-DJANGO_PRIORITY=optional
+PY-DJANGO_DEPENDS=python
+PY-DJANGO_CONFLICTS=
+
+#
+# PY-DJANGO_IPK_VERSION should be incremented when the ipk changes.
+#
+PY-DJANGO_IPK_VERSION=1
+
+#
+# PY-DJANGO_CONFFILES should be a list of user-editable files
+#PY-DJANGO_CONFFILES=/opt/etc/py-django.conf /opt/etc/init.d/SXXpy-django
+
+#
+# PY-DJANGO_PATCHES should list any patches, in the the order in
+# which they should be applied to the source code.
+#
+#PY-DJANGO_PATCHES=$(PY-DJANGO_SOURCE_DIR)/configure.patch
+
+#
+# If the compilation of the package requires additional
+# compilation or linking flags, then list them here.
+#
+PY-DJANGO_CPPFLAGS=
+PY-DJANGO_LDFLAGS=
+
+#
+# PY-DJANGO_BUILD_DIR is the directory in which the build is done.
+# PY-DJANGO_SOURCE_DIR is the directory which holds all the
+# patches and ipkg control files.
+# PY-DJANGO_IPK_DIR is the directory in which the ipk is built.
+# PY-DJANGO_IPK is the name of the resulting ipk files.
+#
+# You should not change any of these variables.
+#
+PY-DJANGO_BUILD_DIR=$(BUILD_DIR)/py-django
+PY-DJANGO_SOURCE_DIR=$(SOURCE_DIR)/py-django
+PY-DJANGO_IPK_DIR=$(BUILD_DIR)/py-django-$(PY-DJANGO_VERSION)-ipk
+PY-DJANGO_IPK=$(BUILD_DIR)/py-django$(PY-DJANGO_VERSION)-$(PY-DJANGO_IPK_VERSION)_$(TARGET_ARCH).ipk
+
+#
+# This is the dependency on the source code.  If the source is missing,
+# then it will be fetched from the site using wget.
+#
+$(DL_DIR)/$(PY-DJANGO_SOURCE):
+	$(WGET) -P $(DL_DIR) $(PY-DJANGO_SITE)
+
+#
+# The source code depends on it existing within the download directory.
+# This target will be called by the top level Makefile to download the
+# source code's archive (.tar.gz, .bz2, etc.)
+#
+py-django-source: $(DL_DIR)/$(PY-DJANGO_SOURCE) $(PY-DJANGO_PATCHES)
+
+#
+# This target unpacks the source code in the build directory.
+# If the source archive is not .tar.gz or .tar.bz2, then you will need
+# to change the commands here.  Patches to the source code are also
+# applied in this target as required.
+#
+# This target also configures the build within the build directory.
+# Flags such as LDFLAGS and CPPFLAGS should be passed into configure
+# and NOT $(MAKE) below.  Passing it to configure causes configure to
+# correctly BUILD the Makefile with the right paths, where passing it
+# to Make causes it to override the default search paths of the compiler.
+#
+# If the compilation of the package requires other packages to be staged
+# first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
+#
+$(PY-DJANGO_BUILD_DIR)/.configured: $(DL_DIR)/$(PY-DJANGO_SOURCE) $(PY-DJANGO_PATCHES)
+	$(MAKE) py-setuptools-stage
+	rm -rf $(BUILD_DIR)/$(PY-DJANGO_DIR) $(PY-DJANGO_BUILD_DIR)
+	$(PY-DJANGO_UNZIP) $(DL_DIR)/$(PY-DJANGO_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+#	cat $(PY-DJANGO_PATCHES) | patch -d $(BUILD_DIR)/$(PY-DJANGO_DIR) -p1
+	mv $(BUILD_DIR)/$(PY-DJANGO_DIR) $(PY-DJANGO_BUILD_DIR)
+	(cd $(PY-DJANGO_BUILD_DIR); \
+	    ( \
+	    echo "[build_scripts]"; \
+	    echo "executable=/opt/bin/python"; \
+	    echo "[install]"; \
+	    echo "install_scripts=/opt/bin"; \
+	    ) >> setup.cfg \
+	)
+	touch $(PY-DJANGO_BUILD_DIR)/.configured
+
+py-django-unpack: $(PY-DJANGO_BUILD_DIR)/.configured
+
+#
+# This builds the actual binary.
+#
+$(PY-DJANGO_BUILD_DIR)/.built: $(PY-DJANGO_BUILD_DIR)/.configured
+	rm -f $(PY-DJANGO_BUILD_DIR)/.built
+#	$(MAKE) -C $(PY-DJANGO_BUILD_DIR)
+	touch $(PY-DJANGO_BUILD_DIR)/.built
+
+#
+# This is the build convenience target.
+#
+py-django: $(PY-DJANGO_BUILD_DIR)/.built
+
+#
+# If you are building a library, then you need to stage it too.
+#
+$(PY-DJANGO_BUILD_DIR)/.staged: $(PY-DJANGO_BUILD_DIR)/.built
+	rm -f $(PY-DJANGO_BUILD_DIR)/.staged
+#	$(MAKE) -C $(PY-DJANGO_BUILD_DIR) DESTDIR=$(STAGING_DIR) install
+	touch $(PY-DJANGO_BUILD_DIR)/.staged
+
+py-django-stage: $(PY-DJANGO_BUILD_DIR)/.staged
+
+#
+# This rule creates a control file for ipkg.  It is no longer
+# necessary to create a seperate control file under sources/py-django
+#
+$(PY-DJANGO_IPK_DIR)/CONTROL/control:
+	@install -d $(PY-DJANGO_IPK_DIR)/CONTROL
+	@rm -f $@
+	@echo "Package: py-django" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(PY-DJANGO_PRIORITY)" >>$@
+	@echo "Section: $(PY-DJANGO_SECTION)" >>$@
+	@echo "Version: $(PY-DJANGO_VERSION)-$(PY-DJANGO_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(PY-DJANGO_MAINTAINER)" >>$@
+	@echo "Source: $(PY-DJANGO_SITE)/$(PY-DJANGO_SOURCE)" >>$@
+	@echo "Description: $(PY-DJANGO_DESCRIPTION)" >>$@
+	@echo "Depends: $(PY-DJANGO_DEPENDS)" >>$@
+	@echo "Conflicts: $(PY-DJANGO_CONFLICTS)" >>$@
+
+#
+# This builds the IPK file.
+#
+# Binaries should be installed into $(PY-DJANGO_IPK_DIR)/opt/sbin or $(PY-DJANGO_IPK_DIR)/opt/bin
+# (use the location in a well-known Linux distro as a guide for choosing sbin or bin).
+# Libraries and include files should be installed into $(PY-DJANGO_IPK_DIR)/opt/{lib,include}
+# Configuration files should be installed in $(PY-DJANGO_IPK_DIR)/opt/etc/py-django/...
+# Documentation files should be installed in $(PY-DJANGO_IPK_DIR)/opt/doc/py-django/...
+# Daemon startup scripts should be installed in $(PY-DJANGO_IPK_DIR)/opt/etc/init.d/S??py-django
+#
+# You may need to patch your application to make it use these locations.
+#
+$(PY-DJANGO_IPK): $(PY-DJANGO_BUILD_DIR)/.built
+	rm -rf $(PY-DJANGO_IPK_DIR) $(BUILD_DIR)/py-django_*_$(TARGET_ARCH).ipk
+	(cd $(PY-DJANGO_BUILD_DIR); \
+	PYTHONPATH=$(STAGING_LIB_DIR)/python2.4/site-packages \
+	python2.4 setup.py install --root=$(PY-DJANGO_IPK_DIR) --prefix=/opt --single-version-externally-managed)
+	$(MAKE) $(PY-DJANGO_IPK_DIR)/CONTROL/control
+	echo $(PY-DJANGO_CONFFILES) | sed -e 's/ /\n/g' > $(PY-DJANGO_IPK_DIR)/CONTROL/conffiles
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(PY-DJANGO_IPK_DIR)
+
+#
+# This is called from the top level makefile to create the IPK file.
+#
+py-django-ipk: $(PY-DJANGO_IPK)
+
+#
+# This is called from the top level makefile to clean all of the built files.
+#
+py-django-clean:
+	-$(MAKE) -C $(PY-DJANGO_BUILD_DIR) clean
+
+#
+# This is called from the top level makefile to clean all dynamically created
+# directories.
+#
+py-django-dirclean:
+	rm -rf $(BUILD_DIR)/$(PY-DJANGO_DIR) $(PY-DJANGO_BUILD_DIR) $(PY-DJANGO_IPK_DIR) $(PY-DJANGO_IPK)
