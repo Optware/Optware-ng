1da9a0a5b1177e230d0fa915222dafb31d7b7c2d
Brian Zhou <bzhou@users.noreply.github.com>
2006-04-05 02:52:55 +0000

    upstream upgrade to 2.1.7

--- make/mailman.mk
+++ make/mailman.mk
@@ -27,7 +27,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 MAILMAN_SITE=http://dl.sourceforge.net/sourceforge/mailman
-MAILMAN_VERSION=2.1.6
+MAILMAN_VERSION=2.1.7
 MAILMAN_SOURCE=mailman-$(MAILMAN_VERSION).tgz
 MAILMAN_DIR=mailman-$(MAILMAN_VERSION)
 MAILMAN_UNZIP=zcat
