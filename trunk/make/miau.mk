1d9947e957ab0b392b242fdc07e4db42da337869
caplink811 <caplink811@1f1dca69-5516-0410-b834-e485effd04c3>
2006-04-13 17:55:37 +0000

    changing to upstream version 0.6.0.2, bumped ipkg version

--- trunk/make/miau.mk
+++ trunk/make/miau.mk
@@ -5,8 +5,8 @@
 ###########################################################
 
 MIAU_SITE=http://dl.sourceforge.net/sourceforge/miau
-MIAU_VERSION=0.5.3
-#MIAU_VERSION=0.6.0.2
+#MIAU_VERSION=0.5.3
+MIAU_VERSION=0.6.0.2
 MIAU_SOURCE=miau-$(MIAU_VERSION).tar.gz
 MIAU_DIR=miau-$(MIAU_VERSION)
 MIAU_UNZIP=zcat
@@ -17,14 +17,14 @@ MIAU_PRIORITY=optional
 MIAU_DEPENDS=
 MIAU_CONFLICTS=
 
-MIAU_IPK_VERSION=13
+MIAU_IPK_VERSION=14
 
 MIAU_CONFFILES= /opt/etc/miau.conf \
 		/opt/etc/init.d/S52miau \
 		/opt/etc/logrotate.d/miau
 
-MIAU_PATCHES=$(MIAU_SOURCE_DIR)/paths.patch
-#MIAU_PATCHES=$(MIAU_SOURCE_DIR)/paths.patch.0.6
+#MIAU_PATCHES=$(MIAU_SOURCE_DIR)/paths.patch
+MIAU_PATCHES=$(MIAU_SOURCE_DIR)/paths.patch.0.6
 
 MIAU_CPPFLAGS=
 MIAU_LDFLAGS=
@@ -101,8 +101,8 @@ $(MIAU_IPK): $(MIAU_BUILD_DIR)/src/miau
 	install -d $(MIAU_IPK_DIR)/opt/bin
 	$(STRIP_COMMAND) $(MIAU_BUILD_DIR)/src/miau -o $(MIAU_IPK_DIR)/opt/bin/miau
 	install -d $(MIAU_IPK_DIR)/opt/etc
-	install -m 644 $(MIAU_SOURCE_DIR)/miau.conf $(MIAU_IPK_DIR)/opt/etc/miau.conf
-#	install -m 644 $(MIAU_SOURCE_DIR)/miau.conf.0.6 $(MIAU_IPK_DIR)/opt/etc/miau.con
+#	install -m 644 $(MIAU_SOURCE_DIR)/miau.conf $(MIAU_IPK_DIR)/opt/etc/miau.conf
+	install -m 644 $(MIAU_SOURCE_DIR)/miau.conf.0.6 $(MIAU_IPK_DIR)/opt/etc/miau.conf
 	install -d $(MIAU_IPK_DIR)/opt/etc/init.d
 	install -m 755 $(MIAU_SOURCE_DIR)/rc.miau $(MIAU_IPK_DIR)/opt/etc/init.d/S52miau
 	install -d $(MIAU_IPK_DIR)/opt/etc/logrotate.d
