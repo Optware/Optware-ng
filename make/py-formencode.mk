bd05ab73fafed9499cff5e2acd1398b1ad787c79
Brian Zhou <bzhou@users.noreply.github.com>
2006-04-19 20:52:26 +0000

    upstream upgrade to 0.5

--- make/py-formencode.mk
+++ make/py-formencode.mk
@@ -22,7 +22,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 PY-FORMENCODE_SITE=http://cheeseshop.python.org/packages/source/F/FormEncode
-PY-FORMENCODE_VERSION=0.4
+PY-FORMENCODE_VERSION=0.5
 PY-FORMENCODE_SOURCE=FormEncode-$(PY-FORMENCODE_VERSION).tar.gz
 PY-FORMENCODE_DIR=FormEncode-$(PY-FORMENCODE_VERSION)
 PY-FORMENCODE_UNZIP=zcat
