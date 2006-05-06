553a8c6345688198e75089eebb9242db1d0e8f37
placid <placid@1f1dca69-5516-0410-b834-e485effd04c3>
2006-05-06 01:07:52 +0000

    doxygen: tweak make

--- make/doxygen.mk
+++ make/doxygen.mk
@@ -42,7 +42,7 @@ DOXYGEN_CONFLICTS=
 #
 # DOXYGEN_IPK_VERSION should be incremented when the ipk changes.
 #
-DOXYGEN_IPK_VERSION=1
+DOXYGEN_IPK_VERSION=2
 
 #
 # DOXYGEN_CONFFILES should be a list of user-editable files
