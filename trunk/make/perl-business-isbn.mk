89a2fcc23669554d88bcfdc166566a792b4a1e8f
mkoster1605 <mkoster1605@1f1dca69-5516-0410-b834-e485effd04c3>
2006-05-24 16:10:17 +0000

    perl-business-isbn: added for converting, checking, making barcode for ISBN stuff.

---
+++ trunk/make/perl-business-isbn.mk
@@ -0,0 +1,102 @@
+###########################################################
+#
+# perl-business-isbn
+#
+###########################################################
+
+PERL-BUSINESS-ISBN_SITE=http://search.cpan.org/CPAN/authors/id/B/BD/BDFOY
+PERL-BUSINESS-ISBN_VERSION=1.82
+PERL-BUSINESS-ISBN_SOURCE=Business-ISBN-$(PERL-BUSINESS-ISBN_VERSION).tar.gz
+PERL-BUSINESS-ISBN_DIR=Business-ISBN-$(PERL-BUSINESS-ISBN_VERSION)
+PERL-BUSINESS-ISBN_UNZIP=zcat
+PERL-BUSINESS-ISBN_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
+PERL-BUSINESS-ISBN_DESCRIPTION=Business-ISBN - work with International Standard Book Numbers
+PERL-BUSINESS-ISBN_SECTION=util
+PERL-BUSINESS-ISBN_PRIORITY=optional
+PERL-BUSINESS-ISBN_DEPENDS=perl, perl-gd-barcode, perl-business-isbn-data
+PERL-BUSINESS-ISBN_SUGGESTS=
+PERL-BUSINESS-ISBN_CONFLICTS=
+
+PERL-BUSINESS-ISBN_IPK_VERSION=1
+
+PERL-BUSINESS-ISBN_CONFFILES=
+
+PERL-BUSINESS-ISBN_BUILD_DIR=$(BUILD_DIR)/perl-business-isbn
+PERL-BUSINESS-ISBN_SOURCE_DIR=$(SOURCE_DIR)/perl-business-isbn
+PERL-BUSINESS-ISBN_IPK_DIR=$(BUILD_DIR)/perl-business-isbn-$(PERL-BUSINESS-ISBN_VERSION)-ipk
+PERL-BUSINESS-ISBN_IPK=$(BUILD_DIR)/perl-business-isbn_$(PERL-BUSINESS-ISBN_VERSION)-$(PERL-BUSINESS-ISBN_IPK_VERSION)_$(TARGET_ARCH).ipk
+
+$(DL_DIR)/$(PERL-BUSINESS-ISBN_SOURCE):
+	$(WGET) -P $(DL_DIR) $(PERL-BUSINESS-ISBN_SITE)/$(PERL-BUSINESS-ISBN_SOURCE)
+
+perl-business-isbn-source: $(DL_DIR)/$(PERL-BUSINESS-ISBN_SOURCE) $(PERL-BUSINESS-ISBN_PATCHES)
+
+$(PERL-BUSINESS-ISBN_BUILD_DIR)/.configured: $(DL_DIR)/$(PERL-BUSINESS-ISBN_SOURCE) $(PERL-BUSINESS-ISBN_PATCHES)
+	$(MAKE) perl-gd-barcode-stage perl-business-isbn-data-stage
+	rm -rf $(BUILD_DIR)/$(PERL-BUSINESS-ISBN_DIR) $(PERL-BUSINESS-ISBN_BUILD_DIR)
+	$(PERL-BUSINESS-ISBN_UNZIP) $(DL_DIR)/$(PERL-BUSINESS-ISBN_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+#	cat $(PERL-BUSINESS-ISBN_PATCHES) | patch -d $(BUILD_DIR)/$(PERL-BUSINESS-ISBN_DIR) -p1
+	mv $(BUILD_DIR)/$(PERL-BUSINESS-ISBN_DIR) $(PERL-BUSINESS-ISBN_BUILD_DIR)
+	(cd $(PERL-BUSINESS-ISBN_BUILD_DIR); \
+		$(TARGET_CONFIGURE_OPTS) \
+		CPPFLAGS="$(STAGING_CPPFLAGS)" \
+		LDFLAGS="$(STAGING_LDFLAGS)" \
+		PERL5LIB="$(STAGING_DIR)/opt/lib/perl5/site_perl" \
+		perl Makefile.PL \
+		PREFIX=/opt \
+	)
+	touch $(PERL-BUSINESS-ISBN_BUILD_DIR)/.configured
+
+perl-business-isbn-unpack: $(PERL-BUSINESS-ISBN_BUILD_DIR)/.configured
+
+$(PERL-BUSINESS-ISBN_BUILD_DIR)/.built: $(PERL-BUSINESS-ISBN_BUILD_DIR)/.configured
+	rm -f $(PERL-BUSINESS-ISBN_BUILD_DIR)/.built
+	$(MAKE) -C $(PERL-BUSINESS-ISBN_BUILD_DIR) \
+	PERL5LIB="$(STAGING_DIR)/opt/lib/perl5/site_perl"
+	touch $(PERL-BUSINESS-ISBN_BUILD_DIR)/.built
+
+perl-business-isbn: $(PERL-BUSINESS-ISBN_BUILD_DIR)/.built
+
+$(PERL-BUSINESS-ISBN_BUILD_DIR)/.staged: $(PERL-BUSINESS-ISBN_BUILD_DIR)/.built
+	rm -f $(PERL-BUSINESS-ISBN_BUILD_DIR)/.staged
+	$(MAKE) -C $(PERL-BUSINESS-ISBN_BUILD_DIR) DESTDIR=$(STAGING_DIR) install
+	touch $(PERL-BUSINESS-ISBN_BUILD_DIR)/.staged
+
+perl-business-isbn-stage: $(PERL-BUSINESS-ISBN_BUILD_DIR)/.staged
+
+$(PERL-BUSINESS-ISBN_IPK_DIR)/CONTROL/control:
+	@install -d $(PERL-BUSINESS-ISBN_IPK_DIR)/CONTROL
+	@rm -f $@
+	@echo "Package: perl-business-isbn" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(PERL-BUSINESS-ISBN_PRIORITY)" >>$@
+	@echo "Section: $(PERL-BUSINESS-ISBN_SECTION)" >>$@
+	@echo "Version: $(PERL-BUSINESS-ISBN_VERSION)-$(PERL-BUSINESS-ISBN_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(PERL-BUSINESS-ISBN_MAINTAINER)" >>$@
+	@echo "Source: $(PERL-BUSINESS-ISBN_SITE)/$(PERL-BUSINESS-ISBN_SOURCE)" >>$@
+	@echo "Description: $(PERL-BUSINESS-ISBN_DESCRIPTION)" >>$@
+	@echo "Depends: $(PERL-BUSINESS-ISBN_DEPENDS)" >>$@
+	@echo "Suggests: $(PERL-BUSINESS-ISBN_SUGGESTS)" >>$@
+	@echo "Conflicts: $(PERL-BUSINESS-ISBN_CONFLICTS)" >>$@
+
+$(PERL-BUSINESS-ISBN_IPK): $(PERL-BUSINESS-ISBN_BUILD_DIR)/.built
+	rm -rf $(PERL-BUSINESS-ISBN_IPK_DIR) $(BUILD_DIR)/perl-business-isbn_*_$(TARGET_ARCH).ipk
+	$(MAKE) -C $(PERL-BUSINESS-ISBN_BUILD_DIR) DESTDIR=$(PERL-BUSINESS-ISBN_IPK_DIR) install
+	find $(PERL-BUSINESS-ISBN_IPK_DIR)/opt -name 'perllocal.pod' -exec rm -f {} \;
+	(cd $(PERL-BUSINESS-ISBN_IPK_DIR)/opt/lib/perl5 ; \
+		find . -name '*.so' -exec chmod +w {} \; ; \
+		find . -name '*.so' -exec $(STRIP_COMMAND) {} \; ; \
+		find . -name '*.so' -exec chmod -w {} \; ; \
+	)
+	find $(PERL-BUSINESS-ISBN_IPK_DIR)/opt -type d -exec chmod go+rx {} \;
+	$(MAKE) $(PERL-BUSINESS-ISBN_IPK_DIR)/CONTROL/control
+	echo $(PERL-BUSINESS-ISBN_CONFFILES) | sed -e 's/ /\n/g' > $(PERL-BUSINESS-ISBN_IPK_DIR)/CONTROL/conffiles
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(PERL-BUSINESS-ISBN_IPK_DIR)
+
+perl-business-isbn-ipk: $(PERL-BUSINESS-ISBN_IPK)
+
+perl-business-isbn-clean:
+	-$(MAKE) -C $(PERL-BUSINESS-ISBN_BUILD_DIR) clean
+
+perl-business-isbn-dirclean:
+	rm -rf $(BUILD_DIR)/$(PERL-BUSINESS-ISBN_DIR) $(PERL-BUSINESS-ISBN_BUILD_DIR) $(PERL-BUSINESS-ISBN_IPK_DIR) $(PERL-BUSINESS-ISBN_IPK)
---
+++ trunk/make/perl-gd-barcode.mk
@@ -0,0 +1,102 @@
+###########################################################
+#
+# perl-gd-barcode
+#
+###########################################################
+
+PERL-GD-BARCODE_SITE=http://search.cpan.org/CPAN/authors/id/K/KW/KWITKNR
+PERL-GD-BARCODE_VERSION=1.15
+PERL-GD-BARCODE_SOURCE=GD-Barcode-$(PERL-GD-BARCODE_VERSION).tar.gz
+PERL-GD-BARCODE_DIR=GD-Barcode-$(PERL-GD-BARCODE_VERSION)
+PERL-GD-BARCODE_UNZIP=zcat
+PERL-GD-BARCODE_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
+PERL-GD-BARCODE_DESCRIPTION=GD-Barcode - Create barcode image with GD 
+PERL-GD-BARCODE_SECTION=util
+PERL-GD-BARCODE_PRIORITY=optional
+PERL-GD-BARCODE_DEPENDS=perl, perl-gd
+PERL-GD-BARCODE_SUGGESTS=
+PERL-GD-BARCODE_CONFLICTS=
+
+PERL-GD-BARCODE_IPK_VERSION=1
+
+PERL-GD-BARCODE_CONFFILES=
+
+PERL-GD-BARCODE_BUILD_DIR=$(BUILD_DIR)/perl-gd-barcode
+PERL-GD-BARCODE_SOURCE_DIR=$(SOURCE_DIR)/perl-gd-barcode
+PERL-GD-BARCODE_IPK_DIR=$(BUILD_DIR)/perl-gd-barcode-$(PERL-GD-BARCODE_VERSION)-ipk
+PERL-GD-BARCODE_IPK=$(BUILD_DIR)/perl-gd-barcode_$(PERL-GD-BARCODE_VERSION)-$(PERL-GD-BARCODE_IPK_VERSION)_$(TARGET_ARCH).ipk
+
+$(DL_DIR)/$(PERL-GD-BARCODE_SOURCE):
+	$(WGET) -P $(DL_DIR) $(PERL-GD-BARCODE_SITE)/$(PERL-GD-BARCODE_SOURCE)
+
+perl-gd-barcode-source: $(DL_DIR)/$(PERL-GD-BARCODE_SOURCE) $(PERL-GD-BARCODE_PATCHES)
+
+$(PERL-GD-BARCODE_BUILD_DIR)/.configured: $(DL_DIR)/$(PERL-GD-BARCODE_SOURCE) $(PERL-GD-BARCODE_PATCHES)
+	$(MAKE) perl-gd-stage
+	rm -rf $(BUILD_DIR)/$(PERL-GD-BARCODE_DIR) $(PERL-GD-BARCODE_BUILD_DIR)
+	$(PERL-GD-BARCODE_UNZIP) $(DL_DIR)/$(PERL-GD-BARCODE_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+#	cat $(PERL-GD-BARCODE_PATCHES) | patch -d $(BUILD_DIR)/$(PERL-GD-BARCODE_DIR) -p1
+	mv $(BUILD_DIR)/$(PERL-GD-BARCODE_DIR) $(PERL-GD-BARCODE_BUILD_DIR)
+	(cd $(PERL-GD-BARCODE_BUILD_DIR); \
+		$(TARGET_CONFIGURE_OPTS) \
+		CPPFLAGS="$(STAGING_CPPFLAGS)" \
+		LDFLAGS="$(STAGING_LDFLAGS)" \
+		PERL5LIB="$(STAGING_DIR)/opt/lib/perl5/site_perl" \
+		perl Makefile.PL \
+		PREFIX=/opt \
+	)
+	touch $(PERL-GD-BARCODE_BUILD_DIR)/.configured
+
+perl-gd-barcode-unpack: $(PERL-GD-BARCODE_BUILD_DIR)/.configured
+
+$(PERL-GD-BARCODE_BUILD_DIR)/.built: $(PERL-GD-BARCODE_BUILD_DIR)/.configured
+	rm -f $(PERL-GD-BARCODE_BUILD_DIR)/.built
+	$(MAKE) -C $(PERL-GD-BARCODE_BUILD_DIR) \
+	PERL5LIB="$(STAGING_DIR)/opt/lib/perl5/site_perl"
+	touch $(PERL-GD-BARCODE_BUILD_DIR)/.built
+
+perl-gd-barcode: $(PERL-GD-BARCODE_BUILD_DIR)/.built
+
+$(PERL-GD-BARCODE_BUILD_DIR)/.staged: $(PERL-GD-BARCODE_BUILD_DIR)/.built
+	rm -f $(PERL-GD-BARCODE_BUILD_DIR)/.staged
+	$(MAKE) -C $(PERL-GD-BARCODE_BUILD_DIR) DESTDIR=$(STAGING_DIR) install
+	touch $(PERL-GD-BARCODE_BUILD_DIR)/.staged
+
+perl-gd-barcode-stage: $(PERL-GD-BARCODE_BUILD_DIR)/.staged
+
+$(PERL-GD-BARCODE_IPK_DIR)/CONTROL/control:
+	@install -d $(PERL-GD-BARCODE_IPK_DIR)/CONTROL
+	@rm -f $@
+	@echo "Package: perl-gd-barcode" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(PERL-GD-BARCODE_PRIORITY)" >>$@
+	@echo "Section: $(PERL-GD-BARCODE_SECTION)" >>$@
+	@echo "Version: $(PERL-GD-BARCODE_VERSION)-$(PERL-GD-BARCODE_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(PERL-GD-BARCODE_MAINTAINER)" >>$@
+	@echo "Source: $(PERL-GD-BARCODE_SITE)/$(PERL-GD-BARCODE_SOURCE)" >>$@
+	@echo "Description: $(PERL-GD-BARCODE_DESCRIPTION)" >>$@
+	@echo "Depends: $(PERL-GD-BARCODE_DEPENDS)" >>$@
+	@echo "Suggests: $(PERL-GD-BARCODE_SUGGESTS)" >>$@
+	@echo "Conflicts: $(PERL-GD-BARCODE_CONFLICTS)" >>$@
+
+$(PERL-GD-BARCODE_IPK): $(PERL-GD-BARCODE_BUILD_DIR)/.built
+	rm -rf $(PERL-GD-BARCODE_IPK_DIR) $(BUILD_DIR)/perl-gd-barcode_*_$(TARGET_ARCH).ipk
+	$(MAKE) -C $(PERL-GD-BARCODE_BUILD_DIR) DESTDIR=$(PERL-GD-BARCODE_IPK_DIR) install
+	find $(PERL-GD-BARCODE_IPK_DIR)/opt -name 'perllocal.pod' -exec rm -f {} \;
+	(cd $(PERL-GD-BARCODE_IPK_DIR)/opt/lib/perl5 ; \
+		find . -name '*.so' -exec chmod +w {} \; ; \
+		find . -name '*.so' -exec $(STRIP_COMMAND) {} \; ; \
+		find . -name '*.so' -exec chmod -w {} \; ; \
+	)
+	find $(PERL-GD-BARCODE_IPK_DIR)/opt -type d -exec chmod go+rx {} \;
+	$(MAKE) $(PERL-GD-BARCODE_IPK_DIR)/CONTROL/control
+	echo $(PERL-GD-BARCODE_CONFFILES) | sed -e 's/ /\n/g' > $(PERL-GD-BARCODE_IPK_DIR)/CONTROL/conffiles
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(PERL-GD-BARCODE_IPK_DIR)
+
+perl-gd-barcode-ipk: $(PERL-GD-BARCODE_IPK)
+
+perl-gd-barcode-clean:
+	-$(MAKE) -C $(PERL-GD-BARCODE_BUILD_DIR) clean
+
+perl-gd-barcode-dirclean:
+	rm -rf $(BUILD_DIR)/$(PERL-GD-BARCODE_DIR) $(PERL-GD-BARCODE_BUILD_DIR) $(PERL-GD-BARCODE_IPK_DIR) $(PERL-GD-BARCODE_IPK)
