d8ac120598c3fa8d612b98d1da0a7cd8f96606cd
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2006-05-28 00:05:10 +0000

    py-sqlalchemy: upstream upgrade to 0.2.0

--- trunk/make/py-sqlalchemy.mk
+++ trunk/make/py-sqlalchemy.mk
@@ -21,7 +21,7 @@
 # from your name or email address.  If you leave MAINTAINER set to
 # "NSLU2 Linux" other developers will feel free to edit.
 #
-PY-SQLALCHEMY_VERSION=0.1.7
+PY-SQLALCHEMY_VERSION=0.2.0
 PY-SQLALCHEMY_IPK_VERSION=1
 
 PY-SQLALCHEMY_SVN_REV=
@@ -203,7 +203,7 @@ $(PY-SQLALCHEMY_IPK): $(PY-SQLALCHEMY_BUILD_DIR)/.built
 	rm -rf $(PY-SQLALCHEMY_IPK_DIR) $(BUILD_DIR)/py-sqlalchemy_*_$(TARGET_ARCH).ipk
 	(cd $(PY-SQLALCHEMY_BUILD_DIR); \
 	PYTHONPATH=$(STAGING_LIB_DIR)/python2.4/site-packages \
-	python2.4 setup.py install --root=$(PY-SQLALCHEMY_IPK_DIR) --prefix=/opt --single-version-externally-managed)
+	python2.4 setup.py install --root=$(PY-SQLALCHEMY_IPK_DIR) --prefix=/opt)
 	$(MAKE) $(PY-SQLALCHEMY_IPK_DIR)/CONTROL/control
 	echo $(PY-SQLALCHEMY_CONFFILES) | sed -e 's/ /\n/g' > $(PY-SQLALCHEMY_IPK_DIR)/CONTROL/conffiles
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(PY-SQLALCHEMY_IPK_DIR)
