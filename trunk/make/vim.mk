6a251cf858cf2ba0f4cf1ba710915252b0a533af
placid <placid@1f1dca69-5516-0410-b834-e485effd04c3>
2006-06-28 00:18:08 +0000

    vim: revert to 6.4

--- trunk/make/vim.mk
+++ trunk/make/vim.mk
@@ -19,18 +19,16 @@
 #
 # You should change all these variables to suit your package.
 #
-VIM_SITE=http://ftp.vim.org/pub/vim/unix
-VIM_VERSION=7.0
+VIM_SITE=ftp://ftp.vim.org/pub/vim/unix
+VIM_VERSION=6.4
 VIM_SOURCE=vim-$(VIM_VERSION).tar.bz2
-VIM_DIR=vim70
+VIM_DIR=vim-$(VIM_VERSION)
 VIM_UNZIP=bzcat
 VIM_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
 VIM_DESCRIPTION=Yet another version of the vi editor.
 VIM_SECTION=util
 VIM_PRIORITY=optional
 VIM_DEPENDS=ncurses
-
-VIM_SED=sed -ie
 
 #
 # VIM_IPK_VERSION should be incremented when the ipk changes.
@@ -40,13 +38,13 @@ VIM_IPK_VERSION=1
 #
 # VIM_CONFFILES should be a list of user-editable files
 #VIM_CONFFILES=/opt/etc/vim.conf /opt/etc/init.d/SXXvim
+VIM_CONFFILES=""
 
 #
 # VIM_PATCHES should list any patches, in the the order in
 # which they should be applied to the source code.
 #
-#VIM_PATCHES=$(VIM_SOURCE_DIR)/configure.in.patch
-VIM_PATCHES=$(VIM_SOURCE_DIR)/Makefile.patch
+VIM_PATCHES=$(VIM_SOURCE_DIR)/configure.in.patch
 
 #
 # If the compilation of the package requires additional
@@ -102,15 +100,18 @@ $(VIM_BUILD_DIR)/.configured: $(DL_DIR)/$(VIM_SOURCE) $(VIM_PATCHES)
 	$(MAKE) ncurses-stage
 	rm -rf $(BUILD_DIR)/$(VIM_DIR) $(VIM_BUILD_DIR)
 	$(VIM_UNZIP) $(DL_DIR)/$(VIM_SOURCE) | tar -C $(BUILD_DIR) -xvf -
-	if test -n "$(VIM_PATCHES)" ; \
-		then cat $(VIM_PATCHES) | \
-		patch -d $(BUILD_DIR)/$(VIM_DIR) -p0 ; \
-	fi
-	if test "$(BUILD_DIR)/$(VIM_DIR)" != "$(VIM_BUILD_DIR)" ; \
-		then mv $(BUILD_DIR)/$(VIM_DIR) $(VIM_BUILD_DIR) ; \
-	fi
-# Vim calls src/Makefile to build and run the configure script.
-### Not working - target configure flags are not flowing through!
+#	Rename the directory since they seem to include version numbers
+#	in the unpacked file. 
+	mv $(BUILD_DIR)/`echo $(VIM_DIR)|sed 's/[-.]//g'` $(BUILD_DIR)/$(VIM_DIR)
+ifneq ($(HOSTCC), $(TARGET_CC))
+	cat $(VIM_PATCHES) | patch -d $(BUILD_DIR)/$(VIM_DIR) -p1
+endif
+	mv $(BUILD_DIR)/$(VIM_DIR) $(VIM_BUILD_DIR)
+ifneq ($(HOSTCC), $(TARGET_CC))
+	(cd $(VIM_BUILD_DIR); \
+		autoconf src/configure.in > src/auto/configure; \
+	)
+endif
 	(cd $(VIM_BUILD_DIR)/src; \
 		$(TARGET_CONFIGURE_OPTS) \
 		CPPFLAGS="$(STAGING_CPPFLAGS) $(VIM_CPPFLAGS)" \
@@ -118,7 +119,14 @@ $(VIM_BUILD_DIR)/.configured: $(DL_DIR)/$(VIM_SOURCE) $(VIM_PATCHES)
 		LDFLAGS="$(STAGING_LDFLAGS) $(VIM_LDFLAGS)" \
 		LIBS="$(STAGING_LDFLAGS) $(VIM_LDFLAGS)" \
 		ac_cv_sizeof_int=4 \
-		$(MAKE) \
+		./configure \
+		--build=$(GNU_HOST_NAME) \
+		--host=$(GNU_TARGET_NAME) \
+		--target=$(GNU_TARGET_NAME) \
+		--prefix=/opt \
+		--enable-gui=no \
+		--without-x \
+		--disable-nls \
 	)
 	touch $(VIM_BUILD_DIR)/.configured
 
@@ -181,9 +189,9 @@ $(VIM_IPK): $(VIM_BUILD_DIR)/.built
 	cd $(VIM_BUILD_DIR)/src
 	$(MAKE) -C $(VIM_BUILD_DIR) DESTDIR=$(VIM_IPK_DIR) install
 #	Fix the $VIM directory
-#	mv $(VIM_IPK_DIR)/opt/share/vim $(VIM_IPK_DIR)/opt/share/vim-temp
-#	mv $(VIM_IPK_DIR)/opt/share/vim-temp/vim* $(VIM_IPK_DIR)/opt/share/vim
-#	rm -rf $(VIM_IPK_DIR)/opt/share/vim-temp
+	mv $(VIM_IPK_DIR)/opt/share/vim $(VIM_IPK_DIR)/opt/share/vim-temp
+	mv $(VIM_IPK_DIR)/opt/share/vim-temp/vim* $(VIM_IPK_DIR)/opt/share/vim
+	rm -rf $(VIM_IPK_DIR)/opt/share/vim-temp
 	$(MAKE) $(VIM_IPK_DIR)/CONTROL/control
 #	install -m 644 $(VIM_SOURCE_DIR)/prerm $(VIM_IPK_DIR)/CONTROL/prerm
 #	install -m 644 $(VIM_SOURCE_DIR)/postinst $(VIM_IPK_DIR)/CONTROL/postinst
