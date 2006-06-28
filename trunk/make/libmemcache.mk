47d26fb83f8449fdb21affa9b03eb0aaedf7f2cc
gda <gda@1f1dca69-5516-0410-b834-e485effd04c3>
2006-06-28 19:14:43 +0000

    libmemcache: added -D__linux to CPPFLAGS to allow build on ds101g

--- trunk/make/libmemcache.mk
+++ trunk/make/libmemcache.mk
@@ -37,7 +37,7 @@ LIBMEMCACHE_CONFLICTS=
 #
 # LIBMEMCACHE_IPK_VERSION should be incremented when the ipk changes.
 #
-LIBMEMCACHE_IPK_VERSION=1
+LIBMEMCACHE_IPK_VERSION=2
 
 #
 # LIBMEMCACHE_CONFFILES should be a list of user-editable files
