b17456f27c737df20e2d40ef44f85dd1fe226c9f
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2006-07-06 05:21:37 +0000

    swi-prolog: upstream upgrade to 5.6.16

--- trunk/make/swi-prolog.mk
+++ trunk/make/swi-prolog.mk
@@ -23,7 +23,7 @@
 #
 SWI-PROLOG_SITE=ftp://gollem.science.uva.nl/SWI-Prolog
 ifneq ($(OPTWARE_TARGET),wl500g)
-SWI-PROLOG_VERSION=5.6.15
+SWI-PROLOG_VERSION=5.6.16
 else
 # 5.6.x requires wchar
 SWI-PROLOG_VERSION=5.4.7
