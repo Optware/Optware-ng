55eebad0927f7c164848cd1ba4f18a137e370ee2
oleo <oleo@1f1dca69-5516-0410-b834-e485effd04c3>
2006-07-16 23:30:49 +0000

    optware: tiny script for searching broken packages

---
+++ trunk/scripts/report-broken.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# This script will  try to build all common cross packages one by one and 
+# at the end will output broken packages in Makefile format
+# Example batch usage: nohup sh scripts/report-broken.sh &
+
+PACKAGES=`make query-COMMON_CROSS_PACKAGES`
+
+i=0
+NL="\\\\\\n"
+
+for package in ${PACKAGES} ; do
+	make ${package}-ipk
+	status=$?
+	if [ ${status} != 0 ]; then
+		BROKEN="${BROKEN} ${package}"
+		i=$((${i}+1))
+		if [ ${i} = 7 ]; then
+			BROKEN="${BROKEN} ${NL}\t"
+			i=0
+		fi
+	fi
+done
+
+echo -e "BROKEN_PACKAGES = ${NL}\t${BROKEN}"
