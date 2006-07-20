7df1df6fa7ea98f286e8387d622c5497a8be74ee
oleo <oleo@1f1dca69-5516-0410-b834-e485effd04c3>
2006-07-20 20:42:12 +0000

    lsof: upstream upgrade to 4.77 and package cleanup

--- trunk/make/lsof.mk
+++ trunk/make/lsof.mk
@@ -3,29 +3,96 @@
 # lsof
 #
 #############################################################
+#
+# LSOF_VERSION, LSOF_SITE and LSOF_SOURCE define
+# the upstream location of the source code for the package.
+# LSOF_DIR is the directory which is created when the source
+# archive is unpacked.
+# LSOF_UNZIP is the command used to unzip the source.
+# It is usually "zcat" (for .gz) or "bzcat" (for .bz2)
+#
+# You should change all these variables to suit your package.
+# Please make sure that you add a description, and that you
+# list all your packages' dependencies, seperated by commas.
+# 
+# If you list yourself as MAINTAINER, please give a valid email
+# address, and indicate your irc nick if it cannot be easily deduced
+# from your name or email address.  If you leave MAINTAINER set to
+# "NSLU2 Linux" other developers will feel free to edit.
+#
 
 LSOF_DIR:=$(BUILD_DIR)/lsof
 LSOF_SOURCE_DIR:=$(SOURCE_DIR)/lsof
-LSOF_VERSION:=4.74.dfsg.3
+LSOF_VERSION:=4.77.dfsg.1
 LSOF:=lsof-$(LSOF_VERSION).orig
 LSOF_FILE:=lsof_$(LSOF_VERSION).orig
-LSOF_DSC=lsof_$(LSOF_VERSION)-2.dsc
+LSOF_DSC=lsof_$(LSOF_VERSION)-3.dsc
 LSOF_SITE=http://http.us.debian.org/debian/pool/main/l/lsof
 LSOF_SOURCE:=$(LSOF_FILE).tar.gz
+LSOF_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
+LSOF_DESCRIPTION=LiSt Open Files - a diagnostic tool.
+LSOF_SECTION=admin
+LSOF_PRIORITY=optional
+LSOF_DEPENDS=
+LSOF_SUGGESTS=
+LSOF_CONFLICTS=
+
+
+#
+# LSOF_IPK_VERSION should be incremented when the ipk changes.
+#
 LSOF_IPK_VERSION=1
+
+#
+# LSOF_BUILD_DIR is the directory in which the build is done.
+# LSOF_SOURCE_DIR is the directory which holds all the
+# patches and ipkg control files.
+# LSOF_IPK_DIR is the directory in which the ipk is built.
+# LSOF_IPK is the name of the resulting ipk files.
+#
+# You should not change any of these variables.
+#
 LSOF_IPK:=$(BUILD_DIR)/lsof_$(LSOF_VERSION)-$(LSOF_IPK_VERSION)_$(TARGET_ARCH).ipk
 LSOF_IPK_DIR:=$(BUILD_DIR)/lsof-$(LSOF_VERSION)-ipk
 LSOF_PATCH:=$(LSOF_SOURCE_DIR)/Makefile-lib.patch
 LSOF_UNZIP:=gunzip
 
+
+#
+# This is the dependency on the source code.  If the source is missing,
+# then it will be fetched from the site using wget.
+#
 $(DL_DIR)/$(LSOF_SOURCE):
 	$(WGET) -P $(DL_DIR) $(LSOF_SITE)/$(LSOF_SOURCE)
 
 $(DL_DIR)/$(LSOF_DSC):
 	$(WGET) -P $(DL_DIR) $(LSOF_SITE)/$(LSOF_DSC)
 
+#
+# The source code depends on it existing within the download directory.
+# This target will be called by the top level Makefile to download the
+# source code's archive (.tar.gz, .bz2, etc.)
+#
 lsof-source: $(DL_DIR)/$(LSOF_SOURCE) $(DL_DIR)/$(LSOF_DSC) $(LSOF_PATCH)
 
+#
+# This target unpacks the source code in the build directory.
+# If the source archive is not .tar.gz or .tar.bz2, then you will need
+# to change the commands here.  Patches to the source code are also
+# applied in this target as required.
+#
+# This target also configures the build within the build directory.
+# Flags such as LDFLAGS and CPPFLAGS should be passed into configure
+# and NOT $(MAKE) below.  Passing it to configure causes configure to
+# correctly BUILD the Makefile with the right paths, where passing it
+# to Make causes it to override the default search paths of the compiler.
+#
+# If the compilation of the package requires other packages to be staged
+# first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
+#
+# If the package uses  GNU libtool, you should invoke $(PATCH_LIBTOOL) as
+# shown below to make various patches to it.
+#
 $(LSOF_DIR)/.configured: $(DL_DIR)/$(LSOF_SOURCE) $(DL_DIR)/$(LSOF_DSC) $(LSOF_PATCHES)
 	@rm -rf $(BUILD_DIR)/$(LSOF) $(LSOF_DIR)
 	cd $(DL_DIR) && \
@@ -44,23 +111,70 @@ $(LSOF_DIR)/.configured: $(DL_DIR)/$(LSOF_SOURCE) $(DL_DIR)/$(LSOF_DSC) $(LSOF_P
 
 lsof-unpack: $(LSOF_DIR)/.configured
 
+#
+# This builds the actual binary.
+#
 $(LSOF_DIR)/lsof: $(LSOF_DIR)/.configured
 	make -C $(LSOF_DIR) $(TARGET_CONFIGURE_OPTS) CFLAGS="$(TARGET_CFLAGS)"
 
+#
+# This is the build convenience target.
+#
 lsof: $(LSOF_DIR)/lsof
+
+#
+# This rule creates a control file for ipkg.  It is no longer
+# necessary to create a seperate control file under sources/lsof
+#
+$(LSOF_IPK_DIR)/CONTROL/control:
+	@install -d $(LSOF_IPK_DIR)/CONTROL
+	@rm -f $@
+	@echo "Package: lsof" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(LSOF_PRIORITY)" >>$@
+	@echo "Section: $(LSOF_SECTION)" >>$@
+	@echo "Version: $(LSOF_VERSION)-$(LSOF_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(LSOF_MAINTAINER)" >>$@
+	@echo "Source: $(LSOF_SITE)/$(LSOF_SOURCE)" >>$@
+	@echo "Description: $(LSOF_DESCRIPTION)" >>$@
+	@echo "Depends: $(LSOF_DEPENDS)" >>$@
+	@echo "Suggests: $(LSOF_SUGGESTS)" >>$@
+	@echo "Conflicts: $(LSOF_CONFLICTS)" >>$@
+
+#
+# This builds the IPK file.
+#
+# Binaries should be installed into $(LSOF_IPK_DIR)/opt/sbin or $(LSOF_IPK_DIR)/opt/bin
+# (use the location in a well-known Linux distro as a guide for choosing sbin or bin).
+# Libraries and include files should be installed into $(LSOF_IPK_DIR)/opt/{lib,include}
+# Configuration files should be installed in $(LSOF_IPK_DIR)/opt/etc/lsof/...
+# Documentation files should be installed in $(LSOF_IPK_DIR)/opt/doc/lsof/...
+# Daemon startup scripts should be installed in $(LSOF_IPK_DIR)/opt/etc/init.d/S??lsof
+#
+# You may need to patch your application to make it use these locations.
 
 $(LSOF_IPK): $(LSOF_DIR)/lsof
 	rm -rf $(LSOF_IPK_DIR) $(BUILD_DIR)/lsof_*_$(TARGET_ARCH).ipk
 	install -d $(LSOF_IPK_DIR)/CONTROL
 	install -d $(LSOF_IPK_DIR)/opt/sbin
 	$(STRIP_COMMAND) $(LSOF_DIR)/lsof -o $(LSOF_IPK_DIR)/opt/sbin/lsof
-	install -m 644 $(LSOF_SOURCE_DIR)/control  $(LSOF_IPK_DIR)/CONTROL/control
+	$(MAKE) $(LSOF_IPK_DIR)/CONTROL/control
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(LSOF_IPK_DIR)
 
+#
+# This is called from the top level makefile to create the IPK file.
+#
 lsof-ipk: $(LSOF_IPK)
 
+#
+# This is called from the top level makefile to clean all of the built files.
+#
 lsof-clean:
 	-make -C $(LSOF_DIR) clean
 
+#
+# This is called from the top level makefile to clean all dynamically created
+# directories.
+#
 lsof-dirclean:
 	rm -rf $(LSOF_DIR) $(LSOF_IPK_DIR) $(LSOF_IPK)
