e8a865d5b58f4f4ebaac87710e2e4b567c972807
oleo <oleo@1f1dca69-5516-0410-b834-e485effd04c3>
2006-07-21 10:29:17 +0000

    bash: upstream upgrade to 3.1 with latest patches

--- trunk/make/bash.mk
+++ trunk/make/bash.mk
@@ -3,9 +3,25 @@
 # bash
 #
 ###########################################################
-
+#
+# BASH_VERSION, BASH_SITE and BASH_SOURCE define
+# the upstream location of the source code for the package.
+# BASH_DIR is the directory which is created when the source
+# archive is unpacked.
+# BASH_UNZIP is the command used to unzip the source.
+# It is usually "zcat" (for .gz) or "bzcat" (for .bz2)
+#
+# You should change all these variables to suit your package.
+# Please make sure that you add a description, and that you
+# list all your packages' dependencies, seperated by commas.
+# 
+# If you list yourself as MAINTAINER, please give a valid email
+# address, and indicate your irc nick if it cannot be easily deduced
+# from your name or email address.  If you leave MAINTAINER set to
+# "NSLU2 Linux" other developers will feel free to edit.
+#
 BASH_SITE=http://ftp.gnu.org/gnu/bash/
-BASH_VERSION=2.05b
+BASH_VERSION=3.1
 BASH_SOURCE=bash-$(BASH_VERSION).tar.gz
 BASH_DIR=bash-$(BASH_VERSION)
 BASH_UNZIP=zcat
@@ -13,29 +29,84 @@ BASH_MAINTAINER=Christopher Blunck <christopher.blunck@gmail.com>
 BASH_DESCRIPTION=A bourne style shell
 BASH_SECTION=shell
 BASH_PRIORITY=optional
-BASH_DEPENDS=ncurses
+BASH_DEPENDS=readline
+BASH_CONFLICTS=
+BASH_SUGGESTS=
 BASH_CONFLICTS=
 
-BASH_IPK_VERSION=5
+#
+# BASH_IPK_VERSION should be incremented when the ipk changes.
+#
+BASH_IPK_VERSION=1
+#
+# BASH_PATCHES should list any patches, in the the order in
+# which they should be applied to the source code.
+#
+BASH_PATCHES=$(BASH_SOURCE_DIR)/bash-3.1-patches/bash31-*
 
+#
+# If the compilation of the package requires additional
+# compilation or linking flags, then list them here.
+#
 BASH_CPPFLAGS=
 BASH_LDFLAGS=
 
+#
+# BASH_BUILD_DIR is the directory in which the build is done.
+# BASH_SOURCE_DIR is the directory which holds all the
+# patches and ipkg control files.
+# BASH_IPK_DIR is the directory in which the ipk is built.
+# BASH_IPK is the name of the resulting ipk files.
+#
+# You should not change any of these variables.
+#
 BASH_BUILD_DIR=$(BUILD_DIR)/bash
 BASH_SOURCE_DIR=$(SOURCE_DIR)/bash
 BASH_IPK_DIR=$(BUILD_DIR)/bash-$(BASH_VERSION)-ipk
 BASH_IPK=$(BUILD_DIR)/bash_$(BASH_VERSION)-$(BASH_IPK_VERSION)_$(TARGET_ARCH).ipk
 
+#
+# This is the dependency on the source code.  If the source is missing,
+# then it will be fetched from the site using wget.
+#
 $(DL_DIR)/$(BASH_SOURCE):
 	$(WGET) -P $(DL_DIR) $(BASH_SITE)/$(BASH_SOURCE)
 
+#
+# The source code depends on it existing within the download directory.
+# This target will be called by the top level Makefile to download the
+# source code's archive (.tar.gz, .bz2, etc.)
+#
 bash-source: $(DL_DIR)/$(BASH_SOURCE)
-
+#
+# This target unpacks the source code in the build directory.
+# If the source archive is not .tar.gz or .tar.bz2, then you will need
+# to change the commands here.  Patches to the source code are also
+# applied in this target as required.
+#
+# This target also configures the build within the build directory.
+# Flags such as LDFLAGS and CPPFLAGS should be passed into configure
+# and NOT $(MAKE) below.  Passing it to configure causes configure to
+# correctly BUILD the Makefile with the right paths, where passing it
+# to Make causes it to override the default search paths of the compiler.
+#
+# If the compilation of the package requires other packages to be staged
+# first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
+#
+# If the package uses  GNU libtool, you should invoke $(PATCH_LIBTOOL) as
+# shown below to make various patches to it.
+#
 $(BASH_BUILD_DIR)/.configured: $(DL_DIR)/$(BASH_SOURCE)
 	$(MAKE) termcap-stage
 	rm -rf $(BUILD_DIR)/$(BASH_DIR) $(BASH_BUILD_DIR)
 	$(BASH_UNZIP) $(DL_DIR)/$(BASH_SOURCE) | tar -C $(BUILD_DIR) -xvf -
-	mv $(BUILD_DIR)/$(BASH_DIR) $(BASH_BUILD_DIR)
+	if test -n "$(BASH_PATCHES)" ; \
+		then cat $(BASH_PATCHES) | \
+		patch -d $(BUILD_DIR)/$(BASH_DIR) -p0 ; \
+	fi
+	if test "$(BUILD_DIR)/$(BASH_DIR)" != "$(BASH_BUILD_DIR)" ; \
+		then mv $(BUILD_DIR)/$(BASH_DIR) $(BASH_BUILD_DIR) ; \
+	fi
 	(cd $(BASH_BUILD_DIR); \
 		$(TARGET_CONFIGURE_OPTS) \
 		CPPFLAGS="$(STAGING_CPPFLAGS) $(BASH_CPPFLAGS)" \
@@ -61,6 +132,9 @@ $(BASH_BUILD_DIR)/.configured: $(DL_DIR)/$(BASH_SOURCE)
 
 bash-unpack: $(BASH_BUILD_DIR)/.configured
 
+#
+# This builds the actual binary.
+#
 $(BASH_BUILD_DIR)/bash: $(BASH_BUILD_DIR)/.configured
 	$(MAKE) -C $(BASH_BUILD_DIR)
 
@@ -84,6 +158,18 @@ $(BASH_IPK_DIR)/CONTROL/control:
 	@echo "Depends: $(BASH_DEPENDS)" >>$@
 	@echo "Conflicts: $(BASH_CONFLICTS)" >>$@
 
+#
+# This builds the IPK file.
+#
+# Binaries should be installed into $(BASH_IPK_DIR)/opt/sbin or $(BASH_IPK_DIR)/opt/bin
+# (use the location in a well-known Linux distro as a guide for choosing sbin or bin).
+# Libraries and include files should be installed into $(BASH_IPK_DIR)/opt/{lib,include}
+# Configuration files should be installed in $(BASH_IPK_DIR)/opt/etc/bash/...
+# Documentation files should be installed in $(BASH_IPK_DIR)/opt/doc/bash/...
+# Daemon startup scripts should be installed in $(BASH_IPK_DIR)/opt/etc/init.d/S??bash
+#
+# You may need to patch your application to make it use these locations.
+#
 $(BASH_IPK): $(BASH_BUILD_DIR)/bash
 	rm -rf $(BASH_IPK_DIR) $(BUILD_DIR)/bash_*_$(TARGET_ARCH).ipk
 	install -d $(BASH_IPK_DIR)/opt/bin
