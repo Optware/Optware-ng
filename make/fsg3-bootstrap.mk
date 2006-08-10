dda2acdf6b2c17e765b7e82c56ec74b0a5e5639d
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2006-08-10 15:04:31 +0000

    Initial fsg3-bootstrap (untested)

---
+++ make/fsg3-bootstrap.mk
@@ -0,0 +1,136 @@
+###########################################################
+#
+# fsg3-bootstrap
+#
+# Creates an ipk for bootstrapping the DS-101. Also
+# includes missing GLIBC libraries
+#
+###########################################################
+
+FSG3_GLIBC_VERSION=2.2.5
+FSG3_BOOTSTRAP_VERSION=0.1
+FSG3_BOOTSTRAP_DIR=fsg3-bootstrap-$(FSG3_BOOTSTRAP_VERSION)
+FSG3_BOOTSTRAP_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
+FSG3_BOOTSTRAP_DESCRIPTION=Bootstrap package for the Freecom FSG-3
+FSG3_BOOTSTRAP_SECTION=util
+FSG3_BOOTSTRAP_PRIORITY=optional
+FSG3_BOOTSTRAP_DEPENDS=
+FSG3_BOOTSTRAP_CONFLICTS=
+
+FSG3_BOOTSTRAP_IPK_VERSION=0
+
+FSG3_BOOTSTRAP_BUILD_DIR=$(BUILD_DIR)/fsg3-bootstrap
+FSG3_BOOTSTRAP_SOURCE_DIR=$(SOURCE_DIR)/fsg3-bootstrap
+FSG3_BOOTSTRAP_IPK_DIR=$(BUILD_DIR)/fsg3-bootstrap-$(FSG3_BOOTSTRAP_VERSION)-ipk
+FSG3_BOOTSTRAP_IPK=$(BUILD_DIR)/fsg3-bootstrap_$(FSG3_BOOTSTRAP_VERSION)-$(FSG3_BOOTSTRAP_IPK_VERSION)_$(TARGET_ARCH).ipk
+FSG3_BOOTSTRAP_XSH=$(BUILD_DIR)/fsg3-bootstrap_$(FSG3_BOOTSTRAP_VERSION)-$(FSG3_BOOTSTRAP_IPK_VERSION)_$(TARGET_ARCH).xsh
+
+# Additional ipk's we require
+FSG3_IPKG_IPK=ipkg_0.99-148-?_$(TARGET_ARCH).ipk
+FSG3_OPENSSL_IPK=openssl_0.9.7d-?_$(TARGET_ARCH).ipk
+FSG3_WGET_SSL_IPK=wget-ssl_1.10-?_$(TARGET_ARCH).ipk
+
+
+$(FSG3_BOOTSTRAP_BUILD_DIR)/.configured: $(FSG3_BOOTSTRAP_PATCHES)
+	rm -rf $(BUILD_DIR)/$(FSG3_BOOTSTRAP_DIR) $(FSG3_BOOTSTRAP_BUILD_DIR)
+	mkdir -p $(FSG3_BOOTSTRAP_BUILD_DIR)
+	touch $(FSG3_BOOTSTRAP_BUILD_DIR)/.configured
+
+fsg3-bootstrap-unpack: $(FSG3_BOOTSTRAP_BUILD_DIR)/.configured
+
+$(FSG3_BOOTSTRAP_BUILD_DIR)/.built: $(FSG3_BOOTSTRAP_BUILD_DIR)/.configured
+	rm -f $(FSG3_BOOTSTRAP_BUILD_DIR)/.built
+	cp -R $(TARGET_LIBDIR)/* $(FSG3_BOOTSTRAP_BUILD_DIR)/
+	find $(FSG3_BOOTSTRAP_BUILD_DIR)/ -type l |xargs rm
+	rm $(FSG3_BOOTSTRAP_BUILD_DIR)/libc.so
+	cp $(TARGET_LIBDIR)/../sbin/ldconfig $(FSG3_BOOTSTRAP_BUILD_DIR)/
+	cp $(SOURCE_DIR)/ipkg/rc.optware $(FSG3_BOOTSTRAP_BUILD_DIR)/
+	touch $(FSG3_BOOTSTRAP_BUILD_DIR)/.built
+
+fsg3-bootstrap: $(FSG3_BOOTSTRAP_BUILD_DIR)/.built
+
+$(FSG3_BOOTSTRAP_BUILD_DIR)/.staged: $(FSG3_BOOTSTRAP_BUILD_DIR)/.built
+	rm -f $(FSG3_BOOTSTRAP_BUILD_DIR)/.staged
+	install -d $(STAGING_DIR)/opt/lib
+	install -d $(STAGING_DIR)/opt/sbin
+	install -d $(STAGING_DIR)/opt/etc
+	install -d $(STAGING_DIR)/writeable/lib
+	install -d $(STAGING_DIR)/opt/lib/gconv
+	install -d $(STAGING_DIR)/opt/lib/ldscripts
+	install -m 755 $(FSG3_BOOTSTRAP_BUILD_DIR)/*crt* $(STAGING_DIR)/opt/lib
+	install -m 755 $(FSG3_BOOTSTRAP_BUILD_DIR)/lib* $(STAGING_DIR)/opt/lib
+	install -m 755 $(FSG3_BOOTSTRAP_BUILD_DIR)/gconv/* $(STAGING_DIR)/opt/lib/gconv
+	install -m 755 $(FSG3_BOOTSTRAP_BUILD_DIR)/ldscripts/* $(STAGING_DIR)/opt/lib/ldscripts	
+	install -m 755 $(FSG3_BOOTSTRAP_BUILD_DIR)/ldconfig $(STAGING_DIR)/opt/sbin
+	install -m 755 $(FSG3_BOOTSTRAP_BUILD_DIR)/rc.optware $(STAGING_DIR)/opt/etc
+	touch $(FSG3_BOOTSTRAP_BUILD_DIR)/.staged
+
+fsg3-bootstrap-stage: $(FSG3_BOOTSTRAP_BUILD_DIR)/.staged
+
+$(FSG3_BOOTSTRAP_IPK_DIR)/CONTROL/control:
+	@install -d $(FSG3_BOOTSTRAP_IPK_DIR)/CONTROL
+	@rm -f $@
+	@echo "Package: fsg3-bootstrap" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(FSG3_BOOTSTRAP_PRIORITY)" >>$@
+	@echo "Section: $(FSG3_BOOTSTRAP_SECTION)" >>$@
+	@echo "Version: $(FSG3_BOOTSTRAP_VERSION)-$(FSG3_BOOTSTRAP_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(FSG3_BOOTSTRAP_MAINTAINER)" >>$@
+	@echo "Source: $(FSG3_BOOTSTRAP_SITE)/$(FSG3_BOOTSTRAP_SOURCE)" >>$@
+	@echo "Description: $(FSG3_BOOTSTRAP_DESCRIPTION)" >>$@
+	@echo "Depends: $(FSG3_BOOTSTRAP_DEPENDS)" >>$@
+	@echo "Conflicts: $(FSG3_BOOTSTRAP_CONFLICTS)" >>$@
+
+$(FSG3_BOOTSTRAP_IPK): $(FSG3_BOOTSTRAP_BUILD_DIR)/.built
+	rm -rf $(FSG3_BOOTSTRAP_IPK_DIR) $(BUILD_DIR)/fsg3-bootstrap_*_$(TARGET_ARCH).ipk
+	install -d $(FSG3_BOOTSTRAP_IPK_DIR)/opt/sbin
+	install -d $(FSG3_BOOTSTRAP_IPK_DIR)/opt/etc
+	install -d $(FSG3_BOOTSTRAP_IPK_DIR)/writeable/lib
+	install -d $(FSG3_BOOTSTRAP_IPK_DIR)/opt/lib
+	install -d $(FSG3_BOOTSTRAP_IPK_DIR)/opt/lib/gconv
+	install -d $(FSG3_BOOTSTRAP_IPK_DIR)/opt/lib/ldscripts
+	install -m 755 $(FSG3_BOOTSTRAP_BUILD_DIR)/*crt* $(FSG3_BOOTSTRAP_IPK_DIR)/opt/lib/
+	install -m 755 $(FSG3_BOOTSTRAP_BUILD_DIR)/lib* $(FSG3_BOOTSTRAP_IPK_DIR)/opt/lib/
+	install -m 755 $(FSG3_BOOTSTRAP_BUILD_DIR)/gconv/* $(FSG3_BOOTSTRAP_IPK_DIR)/opt/lib/gconv/
+	install -m 755 $(FSG3_BOOTSTRAP_BUILD_DIR)/ldscripts/* $(FSG3_BOOTSTRAP_IPK_DIR)/opt/lib/ldscripts/
+	install -m 755 $(FSG3_BOOTSTRAP_BUILD_DIR)/ldconfig $(FSG3_BOOTSTRAP_IPK_DIR)/opt/sbin
+	install -m 755 $(FSG3_BOOTSTRAP_BUILD_DIR)/rc.optware $(FSG3_BOOTSTRAP_IPK_DIR)/opt/etc
+
+	$(STRIP_COMMAND) $(FSG3_BOOTSTRAP_IPK_DIR)/opt/lib/*.so
+	$(MAKE) $(FSG3_BOOTSTRAP_IPK_DIR)/CONTROL/control
+	install -m 644 $(FSG3_BOOTSTRAP_SOURCE_DIR)/preinst $(FSG3_BOOTSTRAP_IPK_DIR)/CONTROL/preinst
+	install -m 644 $(FSG3_BOOTSTRAP_SOURCE_DIR)/postinst $(FSG3_BOOTSTRAP_IPK_DIR)/CONTROL/postinst
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(FSG3_BOOTSTRAP_IPK_DIR)
+
+$(FSG3_BOOTSTRAP_XSH): $(FSG3_BOOTSTRAP_IPK) ipkg-ipk openssl-ipk wget-ssl-ipk
+	rm -rf $(FSG3_BOOTSTRAP_BUILD_DIR)/bootstrap
+	mkdir -p $(FSG3_BOOTSTRAP_BUILD_DIR)/bootstrap
+	cp $(FSG3_BOOTSTRAP_IPK) $(FSG3_BOOTSTRAP_BUILD_DIR)/bootstrap/bootstrap.ipk
+	cp $(BUILD_DIR)/$(FSG3_IPKG_IPK) $(FSG3_BOOTSTRAP_BUILD_DIR)/bootstrap/ipkg.ipk
+	cp $(BUILD_DIR)/$(FSG3_OPENSSL_IPK) $(FSG3_BOOTSTRAP_BUILD_DIR)/bootstrap/openssl.ipk
+	cp $(BUILD_DIR)/$(FSG3_WGET_SSL_IPK) $(FSG3_BOOTSTRAP_BUILD_DIR)/bootstrap/wget-ssl.ipk
+	cp $(FSG3_BOOTSTRAP_SOURCE_DIR)/bootstrap.sh $(FSG3_BOOTSTRAP_BUILD_DIR)/bootstrap
+	cp $(FSG3_BOOTSTRAP_SOURCE_DIR)/ipkg.sh $(FSG3_BOOTSTRAP_BUILD_DIR)/bootstrap
+	
+	# If you should ever change the archive header (echo lines below), 
+	# make sure to recalculate dd's bs= argument, otherwise the self-
+	# extracting archive will break! Using tail+n would be much simpler
+	# but the tail command available on the DS-101 doesn't support this.
+	echo "#!/bin/sh" >$@
+	echo 'echo "FSG-3 Bootstrap extracting archive... please wait"' >>$@
+	echo 'dd if=$$0 bs=180 skip=1| tar xvz' >>$@
+	echo "cd bootstrap && sh bootstrap.sh && cd .. && rm -r bootstrap" >>$@
+	echo 'exec /bin/sh --login' >>$@
+	tar -C $(FSG3_BOOTSTRAP_BUILD_DIR) -czf - bootstrap >>$@
+	chmod 755 $@
+
+fsg3-bootstrap-ipk: $(FSG3_BOOTSTRAP_IPK)
+
+fsg3-bootstrap-xsh: $(FSG3_BOOTSTRAP_XSH)
+
+fsg3-bootstrap-clean:
+	rm -rf $(FSG3_BOOTSTRAP_BUILD_DIR)/*
+
+fsg3-bootstrap-dirclean:
+	rm -rf $(BUILD_DIR)/$(FSG3_BOOTSTRAP_DIR) $(FSG3_BOOTSTRAP_BUILD_DIR) $(FSG3_BOOTSTRAP_IPK_DIR) $(FSG3_BOOTSTRAP_IPK) $(FSG3_BOOTSTRAP_XSH)
+	rm -rf $(FSG3_BOOTSTRAP_BUILD_DIR)/bootstrap
