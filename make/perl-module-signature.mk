be990ee566eebe873410a568162f80a3695b6030
Brian Zhou <bzhou@users.noreply.github.com>
2006-08-19 06:59:29 +0000

    perl-module-signature: cross buildable

--- make/perl-module-signature.mk
+++ make/perl-module-signature.mk
@@ -46,7 +46,7 @@ $(PERL-MODULE-SIGNATURE_BUILD_DIR)/.configured: $(DL_DIR)/$(PERL-MODULE-SIGNATUR
 		CPPFLAGS="$(STAGING_CPPFLAGS)" \
 		LDFLAGS="$(STAGING_LDFLAGS)" \
 		PERL5LIB="$(STAGING_DIR)/opt/lib/perl5/site_perl" \
-		perl Makefile.PL \
+		$(PERL_HOSTPERL) Makefile.PL \
 		PREFIX=/opt \
 	)
 	touch $(PERL-MODULE-SIGNATURE_BUILD_DIR)/.configured
