afefc3db44f5efa87ed48ec95166728573a8c51f
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2006-08-22 23:54:54 +0000

    irssi: cross buildable

--- trunk/make/irssi.mk
+++ trunk/make/irssi.mk
@@ -3,7 +3,6 @@
 # irssi
 #
 ###########################################################
-
 #
 # IRSSI_VERSION, IRSSI_SITE and IRSSI_SOURCE define
 # the upstream location of the source code for the package.
@@ -13,20 +12,31 @@
 # It is usually "zcat" (for .gz) or "bzcat" (for .bz2)
 #
 # You should change all these variables to suit your package.
+# Please make sure that you add a description, and that you
+# list all your packages' dependencies, seperated by commas.
+# 
+# If you list yourself as MAINTAINER, please give a valid email
+# address, and indicate your irc nick if it cannot be easily deduced
+# from your name or email address.  If you leave MAINTAINER set to
+# "NSLU2 Linux" other developers will feel free to edit.
 #
-IRSSI_SITE=http://irssi.org/
-IRSSI_VERSION=20050206
+IRSSI_SITE=http://www.irssi.org/files
+IRSSI_VERSION=0.8.10a
 IRSSI_SOURCE=irssi-$(IRSSI_VERSION).tar.gz
-IRSSI_DIR=irssi-$(IRSSI_VERSION)
+IRSSI_DIR=irssi-0.8.10
 IRSSI_UNZIP=zcat
-IRSSI_REPOSITORY=:pserver:anonymous@cvs.irssi.org:/home/cvs
-IRSSI_TAG="-D 2005-02-06"
-IRSSI_MODULE=irssi
+IRSSI_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
+IRSSI_DESCRIPTION=A terminal based IRC client for UNIX systems.
+IRSSI_SECTION=net
+IRSSI_PRIORITY=optional
+IRSSI_DEPENDS=
+IRSSI_SUGGESTS=
+IRSSI_CONFLICTS=
 
 #
 # IRSSI_IPK_VERSION should be incremented when the ipk changes.
 #
-IRSSI_IPK_VERSION=2
+IRSSI_IPK_VERSION=1
 
 #
 # IRSSI_CONFFILES should be a list of user-editable files
@@ -36,7 +46,7 @@ IRSSI_IPK_VERSION=2
 # IRSSI_PATCHES should list any patches, in the the order in
 # which they should be applied to the source code.
 #
-#IRSSI_PATCHES=$(IRSSI_SOURCE_DIR)/configure.patch
+IRSSI_PATCHES=$(IRSSI_SOURCE_DIR)/configure.in.patch
 
 #
 # If the compilation of the package requires additional
@@ -64,10 +74,7 @@ IRSSI_IPK=$(BUILD_DIR)/irssi_$(IRSSI_VERSION)-$(IRSSI_IPK_VERSION)_$(TARGET_ARCH
 # then it will be fetched from the site using wget.
 #
 $(DL_DIR)/$(IRSSI_SOURCE):
-	cd $(DL_DIR) ; $(CVS) -d $(IRSSI_REPOSITORY) co $(IRSSI_TAG) $(IRSSI_MODULE)
-	mv $(DL_DIR)/$(IRSSI_MODULE) $(DL_DIR)/$(IRSSI_DIR)
-	cd $(DL_DIR) ; tar zcvf $(IRSSI_SOURCE) $(IRSSI_DIR)
-	rm -rf $(DL_DIR)/$(IRSSI_DIR)
+	$(WGET) -P $(DL_DIR) $(IRSSI_SITE)/$(IRSSI_SOURCE)
 
 #
 # The source code depends on it existing within the download directory.
@@ -91,25 +98,36 @@ irssi-source: $(DL_DIR)/$(IRSSI_SOURCE) $(IRSSI_PATCHES)
 # If the compilation of the package requires other packages to be staged
 # first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
 #
-$(IRSSI_BUILD_DIR)/.configured: $(DL_DIR)/$(IRSSI_SOURCE) $(IRSSI_PATCHES)
+# If the package uses  GNU libtool, you should invoke $(PATCH_LIBTOOL) as
+# shown below to make various patches to it.
+#
+$(IRSSI_BUILD_DIR)/.configured: $(DL_DIR)/$(IRSSI_SOURCE) $(IRSSI_PATCHES) make/irssi.mk
 	$(MAKE) glib-stage ncurses-stage openssl-stage
 	rm -rf $(BUILD_DIR)/$(IRSSI_DIR) $(IRSSI_BUILD_DIR)
 	$(IRSSI_UNZIP) $(DL_DIR)/$(IRSSI_SOURCE) | tar -C $(BUILD_DIR) -xvf -
-#	cat $(IRSSI_PATCHES) | patch -d $(BUILD_DIR)/$(IRSSI_DIR) -p1
-	mv $(BUILD_DIR)/$(IRSSI_DIR) $(IRSSI_BUILD_DIR)
+	if test -n "$(IRSSI_PATCHES)" ; \
+		then cat $(IRSSI_PATCHES) | \
+		patch -d $(BUILD_DIR)/$(IRSSI_DIR) -p1 ; \
+	fi
+	if test "$(BUILD_DIR)/$(IRSSI_DIR)" != "$(IRSSI_BUILD_DIR)" ; \
+		then mv $(BUILD_DIR)/$(IRSSI_DIR) $(IRSSI_BUILD_DIR) ; \
+	fi
 	(cd $(IRSSI_BUILD_DIR); \
+		autoconf; \
 		$(TARGET_CONFIGURE_OPTS) \
 		CPPFLAGS="$(STAGING_CPPFLAGS) $(IRSSI_CPPFLAGS)" \
 		LDFLAGS="$(STAGING_LDFLAGS) $(IRSSI_LDFLAGS)" \
-		./autogen.sh \
+		./configure \
 		--build=$(GNU_HOST_NAME) \
 		--host=$(GNU_TARGET_NAME) \
 		--target=$(GNU_TARGET_NAME) \
 		--prefix=/opt \
+		--disable-nls \
 		--without-perl \
 		--with-ncurses=/opt \
 		--enable-ipv6 \
 	)
+	$(PATCH_LIBTOOL) $(IRSSI_BUILD_DIR)/libtool
 	touch $(IRSSI_BUILD_DIR)/.configured
 
 irssi-unpack: $(IRSSI_BUILD_DIR)/.configured
@@ -138,6 +156,25 @@ $(IRSSI_BUILD_DIR)/.staged: $(IRSSI_BUILD_DIR)/.built
 irssi-stage: $(IRSSI_BUILD_DIR)/.staged
 
 #
+# This rule creates a control file for ipkg.  It is no longer
+# necessary to create a seperate control file under sources/irssi
+#
+$(IRSSI_IPK_DIR)/CONTROL/control:
+	@install -d $(IRSSI_IPK_DIR)/CONTROL
+	@rm -f $@
+	@echo "Package: irssi" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(IRSSI_PRIORITY)" >>$@
+	@echo "Section: $(IRSSI_SECTION)" >>$@
+	@echo "Version: $(IRSSI_VERSION)-$(IRSSI_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(IRSSI_MAINTAINER)" >>$@
+	@echo "Source: $(IRSSI_SITE)/$(IRSSI_SOURCE)" >>$@
+	@echo "Description: $(IRSSI_DESCRIPTION)" >>$@
+	@echo "Depends: $(IRSSI_DEPENDS)" >>$@
+	@echo "Suggests: $(IRSSI_SUGGESTS)" >>$@
+	@echo "Conflicts: $(IRSSI_CONFLICTS)" >>$@
+
+#
 # This builds the IPK file.
 #
 # Binaries should be installed into $(IRSSI_IPK_DIR)/opt/sbin or $(IRSSI_IPK_DIR)/opt/bin
@@ -151,16 +188,16 @@ irssi-stage: $(IRSSI_BUILD_DIR)/.staged
 #
 $(IRSSI_IPK): $(IRSSI_BUILD_DIR)/.built
 	rm -rf $(IRSSI_IPK_DIR) $(BUILD_DIR)/irssi_*_$(TARGET_ARCH).ipk
-	$(MAKE) -C $(IRSSI_BUILD_DIR) DESTDIR=$(IRSSI_IPK_DIR) install-strip
-#	install -d $(IRSSI_IPK_DIR)/opt/etc/
+	$(MAKE) -C $(IRSSI_BUILD_DIR) DESTDIR=$(IRSSI_IPK_DIR) install
+	$(STRIP_COMMAND) $(IRSSI_IPK_DIR)/opt/bin/irssi
+	install -d $(IRSSI_IPK_DIR)/opt/etc/
 #	install -m 644 $(IRSSI_SOURCE_DIR)/irssi.conf $(IRSSI_IPK_DIR)/opt/etc/irssi.conf
 #	install -d $(IRSSI_IPK_DIR)/opt/etc/init.d
 #	install -m 755 $(IRSSI_SOURCE_DIR)/rc.irssi $(IRSSI_IPK_DIR)/opt/etc/init.d/SXXirssi
-	install -d $(IRSSI_IPK_DIR)/CONTROL
-	install -m 644 $(IRSSI_SOURCE_DIR)/control $(IRSSI_IPK_DIR)/CONTROL/control
-#	install -m 644 $(IRSSI_SOURCE_DIR)/postinst $(IRSSI_IPK_DIR)/CONTROL/postinst
-#	install -m 644 $(IRSSI_SOURCE_DIR)/prerm $(IRSSI_IPK_DIR)/CONTROL/prerm
-#	echo $(IRSSI_CONFFILES) | sed -e 's/ /\n/g' > $(IRSSI_IPK_DIR)/CONTROL/conffiles
+	$(MAKE) $(IRSSI_IPK_DIR)/CONTROL/control
+#	install -m 755 $(IRSSI_SOURCE_DIR)/postinst $(IRSSI_IPK_DIR)/CONTROL/postinst
+#	install -m 755 $(IRSSI_SOURCE_DIR)/prerm $(IRSSI_IPK_DIR)/CONTROL/prerm
+	echo $(IRSSI_CONFFILES) | sed -e 's/ /\n/g' > $(IRSSI_IPK_DIR)/CONTROL/conffiles
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(IRSSI_IPK_DIR)
 
 #
@@ -172,6 +209,7 @@ irssi-ipk: $(IRSSI_IPK)
 # This is called from the top level makefile to clean all of the built files.
 #
 irssi-clean:
+	rm -f $(IRSSI_BUILD_DIR)/.built
 	-$(MAKE) -C $(IRSSI_BUILD_DIR) clean
 
 #
