1d1420486c22629fa575e9edd5a920929aacc1ac
Brian Zhou <bzhou@users.noreply.github.com>
2006-08-24 13:31:42 +0000

    bumped IPK_VERSION of perl cross packages

--- make/perl-class-dbi.mk
+++ make/perl-class-dbi.mk
@@ -42,7 +42,7 @@ $(PERL-CLASS-DBI_BUILD_DIR)/.configured: $(DL_DIR)/$(PERL-CLASS-DBI_SOURCE) $(PE
 		CPPFLAGS="$(STAGING_CPPFLAGS)" \
 		LDFLAGS="$(STAGING_LDFLAGS)" \
 		PERL5LIB="$(STAGING_DIR)/opt/lib/perl5/site_perl" \
-		perl Makefile.PL \
+		$(PERL_HOSTPERL) Makefile.PL \
 		PREFIX=/opt \
 	)
 	touch $(PERL-CLASS-DBI_BUILD_DIR)/.configured
