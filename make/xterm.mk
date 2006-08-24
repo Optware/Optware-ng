52296dddeecefcdba44391c4f17b690d9c7365ce
Brian Zhou <bzhou@users.noreply.github.com>
2006-08-24 17:12:43 +0000

    xterm: upstream upgrade to 217

--- make/xterm.mk
+++ make/xterm.mk
@@ -27,7 +27,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 XTERM_SITE=ftp://invisible-island.net/xterm
-XTERM_VERSION=216
+XTERM_VERSION=217
 XTERM_SOURCE=xterm-$(XTERM_VERSION).tgz
 XTERM_DIR=xterm-$(XTERM_VERSION)
 XTERM_UNZIP=zcat
