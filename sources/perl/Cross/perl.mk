748b7234dcbb8269f6857fedc97fd35856eedd26
gda <gda@1f1dca69-5516-0410-b834-e485effd04c3>
2006-08-26 18:25:29 +0000

    tried to get DB_File extension without success

--- sources/perl/Cross/perl.mk
+++ sources/perl/Cross/perl.mk
@@ -30,7 +30,7 @@ PERL_CONFLICTS=
 #
 # PERL_IPK_VERSION should be incremented when the ipk changes.
 #
-PERL_IPK_VERSION=4
+PERL_IPK_VERSION=5
 
 #
 # PERL_CONFFILES should be a list of user-editable files
