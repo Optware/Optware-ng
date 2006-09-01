57e4c1dbede850eef95aa67fe4329432d7a80e6c
Brian Zhou <bzhou@users.noreply.github.com>
2006-09-01 14:00:09 +0000

    py-curl: upstream upgrade to 7.15.5.1

--- make/py-curl.mk
+++ make/py-curl.mk
@@ -22,7 +22,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 PY-CURL_SITE=http://pycurl.sourceforge.net/download
-PY-CURL_VERSION=7.15.5
+PY-CURL_VERSION=7.15.5.1
 PY-CURL_SOURCE=pycurl-$(PY-CURL_VERSION).tar.gz
 PY-CURL_DIR=pycurl-$(PY-CURL_VERSION)
 PY-CURL_UNZIP=zcat
