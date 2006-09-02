bea48b5a360bb39416e6241c0f59df283ceb54a8
Brian Zhou <bzhou@users.noreply.github.com>
2006-09-02 14:51:10 +0000

    py-psycopg2: upstream upgrade to 2.0.5.1

--- make/py-psycopg2.mk
+++ make/py-psycopg2.mk
@@ -22,7 +22,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 PY-PSYCOPG2_SITE=http://initd.org/pub/software/psycopg
-PY-PSYCOPG2_VERSION=2.0.5
+PY-PSYCOPG2_VERSION=2.0.5.1
 PY-PSYCOPG2_SOURCE=psycopg2-$(PY-PSYCOPG2_VERSION).tar.gz
 PY-PSYCOPG2_DIR=psycopg2-$(PY-PSYCOPG2_VERSION)
 PY-PSYCOPG2_UNZIP=zcat
@@ -117,6 +117,7 @@ $(PY-PSYCOPG2_BUILD_DIR)/.configured: $(DL_DIR)/$(PY-PSYCOPG2_SOURCE) $(PY-PSYCO
 		echo "install_scripts=/opt/bin"; \
 	    ) >> setup.cfg; \
 	    sed -i -e '/datetime\.h/s/^/if True: #/' $(PY-PSYCOPG2_BUILD_DIR)/setup.py; \
+	    sed -i -e '/^def get_pg_config/a\    return ""' $(PY-PSYCOPG2_BUILD_DIR)/setup.py; \
 	)
 	touch $(PY-PSYCOPG2_BUILD_DIR)/.configured
 
