a33100a06b61d55002383091d73e6350f1cb0635
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2006-09-08 14:01:25 +0000

    py-bazaar-ng: upstream upgrade to 0.10

--- trunk/make/py-bazaar-ng.mk
+++ trunk/make/py-bazaar-ng.mk
@@ -21,8 +21,8 @@
 # from your name or email address.  If you leave MAINTAINER set to
 # "NSLU2 Linux" other developers will feel free to edit.
 #
-PY-BAZAAR-NG_VERSION=0.9
-PY-BAZAAR-NG_SITE=http://bazaar-vcs.org/pkg
+PY-BAZAAR-NG_VERSION=0.10
+PY-BAZAAR-NG_SITE=http://bazaar-vcs.org/releases/src
 PY-BAZAAR-NG_SOURCE=bzr-$(PY-BAZAAR-NG_VERSION).tar.gz
 PY-BAZAAR-NG_DIR=bzr-$(PY-BAZAAR-NG_VERSION)
 PY-BAZAAR-NG_UNZIP=zcat
