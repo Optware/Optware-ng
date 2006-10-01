d9ceffc6487111146a1881e0c31374170eb7c894
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2006-10-01 23:27:16 +0000

    py-turbogears: upstream upgrade to 1.0b1

--- trunk/make/py-turbogears.mk
+++ trunk/make/py-turbogears.mk
@@ -22,7 +22,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 PY-TURBOGEARS_SITE=http://files.turbogears.org/eggs
-PY-TURBOGEARS_VERSION=0.9a7
+PY-TURBOGEARS_VERSION=1.0b1
 PY-TURBOGEARS_SOURCE=TurboGears-$(PY-TURBOGEARS_VERSION).tar.gz
 PY-TURBOGEARS_DIR=TurboGears-$(PY-TURBOGEARS_VERSION)
 PY-TURBOGEARS_UNZIP=zcat
@@ -30,7 +30,23 @@ PY-TURBOGEARS_MAINTAINER=Brian Zhou <bzhou@users.sf.net>
 PY-TURBOGEARS_DESCRIPTION=Rapid web development megaframework in Python.
 PY-TURBOGEARS_SECTION=misc
 PY-TURBOGEARS_PRIORITY=optional
-PY-TURBOGEARS_DEPENDS=python, py-celementtree (>=1.0.5), py-cherrypy (>=2.1.1), py-configobj (>=4.3.0), py-elementtree (>=1.2.6), py-formencode (>=0.5.1), py-nose (>=0.8), py-ruledispatch, py-simplejson (>=1.3), py-sqlobject (>=0.7.1b1), py-turbocheetah (0.9.5), py-turbojson (>=0.9.2), py-turbokid (>=0.9.5)
+
+PY-TURBOGEARS_DEPENDS=python, \
+	py-celementtree (>=1.0.5), \
+	py-cherrypy (>=2.2.1), \
+	py-configobj (>=4.3.2), \
+	py-elementtree (>=1.2.6), \
+	py-formencode (>=0.5.1), \
+	py-nose (>=0.9), \
+	py-pastescript (>=0.9.7), \
+	py-ruledispatch, \
+	py-simplejson (>=1.3), \
+	py-sqlobject (>=0.7.1), \
+	py-turbocheetah (>=0.9.5), \
+	py-turbojson (>=0.9.9), \
+	py-turbokid (>=0.9.8) \
+	findutils \
+
 PY-TURBOGEARS_CONFLICTS=
 
 #
@@ -171,6 +187,9 @@ $(PY-TURBOGEARS_IPK): $(PY-TURBOGEARS_BUILD_DIR)/.built
 	PYTHONPATH=$(STAGING_LIB_DIR)/python2.4/site-packages \
 	python2.4 setup.py install --root=$(PY-TURBOGEARS_IPK_DIR) --prefix=/opt --single-version-externally-managed)
 	$(MAKE) $(PY-TURBOGEARS_IPK_DIR)/CONTROL/control
+	(echo '#!/bin/sh'; \
+echo /opt/bin/find /opt/lib/python2.4/site-packages -maxdepth 1 -empty -type d -name \'*.egg-info\' -printf \"rmdir %p\\n\" -exec rmdir {} + ; \
+	) > $(PY-TURBOGEARS_IPK_DIR)/CONTROL/postinst
 	echo $(PY-TURBOGEARS_CONFFILES) | sed -e 's/ /\n/g' > $(PY-TURBOGEARS_IPK_DIR)/CONTROL/conffiles
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(PY-TURBOGEARS_IPK_DIR)
 
