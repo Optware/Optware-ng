d25431e8e14944d448085fc6b81b8908e9c61de2
dyoung <dyoung@1f1dca69-5516-0410-b834-e485effd04c3>
2006-10-02 08:48:02 +0000

    First run configuration for the Technologic TS-72xx arm-linux family of boards.

---
+++ sources/crosstool/ts72xx-cross334.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -ex
+TARBALLS_DIR=$HOME/downloads
+export TARBALLS_DIR RESULT_TOP PREFIX
+GCC_LANGUAGES="c,c++"
+export GCC_LANGUAGES
+
+# Really, you should do the mkdir before running this,
+# and chown /opt/crosstool to yourself so you don't need to run as root.
+mkdir -p $RESULT_TOP
+
+# Build the toolchain.  Takes a couple hours and a couple gigabytes.
+
+eval `cat arm-ts72xx.dat gcc-3.3.4-glibc-2.3.2.dat` BINUTILS_DIR=binutils-2.15.94.0.2 LINUX_DIR=linux-2.4.26 sh all.sh --notest
+
+echo Done.
+
