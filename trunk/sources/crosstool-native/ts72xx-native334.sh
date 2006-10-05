7239b0a219f56f152492e2b628578e3e1c6fa314
dyoung <dyoung@1f1dca69-5516-0410-b834-e485effd04c3>
2006-10-05 10:57:40 +0000

    crosstool-native: preliminary conifigurations for ts72xx native compile tools.

---
+++ trunk/sources/crosstool-native/ts72xx-native334.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -ex
+TARBALLS_DIR=$HOME/downloads
+export TARBALLS_DIR RESULT_TOP
+GCC_LANGUAGES="c,c++"
+export GCC_LANGUAGES
+
+HOST=$GCC_HOST
+
+# Really, you should do the mkdir before running this,
+# and chown /opt/crosstool to yourself so you don't need to run as root.
+mkdir -p $RESULT_TOP
+
+export GCC_HOST AR AS LD NM CC GCC CXX RANLIB PATH GPROF HOST
+
+# Build the toolchain. Takes a couple hours and a couple gigabytes.
+
+eval `cat arm-72xx.dat gcc-3.3.4-glibc-2.3.2.dat` BINUTILS_DIR=binutils-2.15.94.0.2 LINUX_DIR=linux-2.4.26 sh all.sh --notest
+
+echo Done.
