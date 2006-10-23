0d7d5146e2ec1e43d9d83f963fe56de5f919209d
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2006-10-23 14:10:30 +0000

    optware-devel: Added - renamed from unslung-devel

---
+++ trunk/make/optware-devel.mk
@@ -0,0 +1,54 @@
+###########################################################
+#
+# optware-devel
+#
+###########################################################
+
+OPTWARE-DEVEL_VERSION=6.8
+OPTWARE-DEVEL_DIR=optware-devel-$(OPTWARE-DEVEL_VERSION)
+OPTWARE-DEVEL_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
+OPTWARE-DEVEL_DESCRIPTION=This is a meta package that bundles all the packages required for optware native development.
+OPTWARE-DEVEL_SECTION=util
+OPTWARE-DEVEL_PRIORITY=optional
+OPTWARE-DEVEL_DEPENDS=autoconf, automake, bash, bison, bzip2, coreutils, crosstool-native, cvs, diffutils, file, findutils, flex, gawk, libstdc++, groff, libtool, make, m4, ncurses, openssl, patch, perl, pkgconfig, python, sed, tar, wget-ssl
+OPTWARE-DEVEL_SUGGESTS=
+OPTWARE-DEVEL_CONFLICTS=
+
+OPTWARE-DEVEL_IPK_VERSION=1
+
+OPTWARE-DEVEL_IPK_DIR=$(BUILD_DIR)/optware-devel-$(OPTWARE-DEVEL_VERSION)-ipk
+OPTWARE-DEVEL_IPK=$(BUILD_DIR)/optware-devel_$(OPTWARE-DEVEL_VERSION)-$(OPTWARE-DEVEL_IPK_VERSION)_$(TARGET_ARCH).ipk
+
+optware-devel-unpack:
+
+optware-devel:
+
+$(OPTWARE-DEVEL_IPK_DIR)/CONTROL/control:
+	@install -d $(OPTWARE-DEVEL_IPK_DIR)/CONTROL
+	@rm -f $@
+	@echo "Package: optware-devel" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(OPTWARE-DEVEL_PRIORITY)" >>$@
+	@echo "Section: $(OPTWARE-DEVEL_SECTION)" >>$@
+	@echo "Version: $(OPTWARE-DEVEL_VERSION)-$(OPTWARE-DEVEL_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(OPTWARE-DEVEL_MAINTAINER)" >>$@
+	@echo "Source: $(OPTWARE-DEVEL_SITE)/$(OPTWARE-DEVEL_SOURCE)" >>$@
+	@echo "Description: $(OPTWARE-DEVEL_DESCRIPTION)" >>$@
+	@echo "Depends: $(OPTWARE-DEVEL_DEPENDS)" >>$@
+	@echo "Suggests: $(OPTWARE-DEVEL_SUGGESTS)" >>$@
+	@echo "Conflicts: $(OPTWARE-DEVEL_CONFLICTS)" >>$@
+
+$(OPTWARE-DEVEL_IPK):
+	rm -rf $(OPTWARE-DEVEL_IPK_DIR) $(BUILD_DIR)/optware-devel_*_$(TARGET_ARCH).ipk
+	$(MAKE) $(OPTWARE-DEVEL_IPK_DIR)/CONTROL/control
+#	install -m 755 $(OPTWARE-DEVEL_SOURCE_DIR)/postinst $(OPTWARE-DEVEL_IPK_DIR)/CONTROL/postinst
+#	install -m 755 $(OPTWARE-DEVEL_SOURCE_DIR)/prerm $(OPTWARE-DEVEL_IPK_DIR)/CONTROL/prerm
+#	echo $(OPTWARE-DEVEL_CONFFILES) | sed -e 's/ /\n/g' > $(OPTWARE-DEVEL_IPK_DIR)/CONTROL/conffiles
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(OPTWARE-DEVEL_IPK_DIR)
+
+optware-devel-ipk: $(OPTWARE-DEVEL_IPK)
+
+optware-devel-clean:
+
+optware-devel-dirclean:
+	rm -rf $(OPTWARE-DEVEL_IPK_DIR) $(OPTWARE-DEVEL_IPK)
