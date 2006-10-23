75895b12bd082215550674e02c706dea0766920a
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2006-10-23 12:16:43 +0000

    clamav: Move the log file back up to where it was, and fix the real problem (permissions on /opt/var/log)

--- make/clamav.mk
+++ make/clamav.mk
@@ -42,7 +42,7 @@ CLAMAV_CONFLICTS=
 #
 # CLAMAV_IPK_VERSION should be incremented when the ipk changes.
 #
-CLAMAV_IPK_VERSION=9
+CLAMAV_IPK_VERSION=10
 
 #
 # CLAMAV_CONFFILES should be a list of user-editable files
