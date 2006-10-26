d4953f3abf47dfed049891a4dcf97822e28484ad
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2006-10-26 13:12:38 +0000

    optware-devel: Removed bison until it builds on fsg3

--- trunk/make/optware-devel.mk
+++ trunk/make/optware-devel.mk
@@ -10,11 +10,12 @@ OPTWARE-DEVEL_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
 OPTWARE-DEVEL_DESCRIPTION=This is a meta package that bundles all the packages required for optware native development.
 OPTWARE-DEVEL_SECTION=util
 OPTWARE-DEVEL_PRIORITY=optional
-OPTWARE-DEVEL_DEPENDS=autoconf, automake, bash, bison, bzip2, coreutils, crosstool-native, cvs, diffutils, file, findutils, flex, gawk, libstdc++, groff, libtool, make, m4, ncurses, openssl, patch, perl, pkgconfig, python, sed, tar, wget-ssl
+# Need to add bison, but it doesn't build on fsg3.
+OPTWARE-DEVEL_DEPENDS=autoconf, automake, bash, bzip2, coreutils, crosstool-native, cvs, diffutils, file, findutils, flex, gawk, libstdc++, groff, libtool, make, m4, ncurses, openssl, patch, perl, pkgconfig, python, sed, tar, wget-ssl
 OPTWARE-DEVEL_SUGGESTS=
 OPTWARE-DEVEL_CONFLICTS=
 
-OPTWARE-DEVEL_IPK_VERSION=1
+OPTWARE-DEVEL_IPK_VERSION=2
 
 OPTWARE-DEVEL_IPK_DIR=$(BUILD_DIR)/optware-devel-$(OPTWARE-DEVEL_VERSION)-ipk
 OPTWARE-DEVEL_IPK=$(BUILD_DIR)/optware-devel_$(OPTWARE-DEVEL_VERSION)-$(OPTWARE-DEVEL_IPK_VERSION)_$(TARGET_ARCH).ipk
