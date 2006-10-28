952c5221cb733817e360143a801a96960e219922
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2006-10-28 23:09:42 +0000

    denyhosts: unbreak the build

--- trunk/make/denyhosts.mk
+++ trunk/make/denyhosts.mk
@@ -126,7 +126,8 @@ $(DENYHOSTS_IPK): $(DENYHOSTS_BUILD_DIR)/.built
 	rm -rf $(DENYHOSTS_IPK_DIR) $(BUILD_DIR)/denyhosts_*_$(TARGET_ARCH).ipk
 	install -d $(DENYHOSTS_IPK_DIR)/var/run/
 	install -d $(DENYHOSTS_IPK_DIR)/opt/etc/
-	install -m 644 $(DENYHOSTS_SOURCE_DIR)/denyhosts.cfg $(DENYHOSTS_IPK_DIR)/opt/etc/denyhosts.cfg
+	install -m 644 $(DENYHOSTS_BUILD_DIR)/denyhosts.cfg-dist $(DENYHOSTS_IPK_DIR)/opt/etc/denyhosts.cfg
+#	install -m 644 $(DENYHOSTS_SOURCE_DIR)/denyhosts.cfg $(DENYHOSTS_IPK_DIR)/opt/etc/denyhosts.cfg
 	install -d $(DENYHOSTS_IPK_DIR)/opt/etc/init.d
 	install -m 755 $(DENYHOSTS_SOURCE_DIR)/rc.denyhosts $(DENYHOSTS_IPK_DIR)/opt/etc/init.d/S01denyhosts
 	install -dv $(DENYHOSTS_IPK_DIR)/opt/share/denyhosts
