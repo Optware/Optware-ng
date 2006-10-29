8b14bdf74f82b85b2fab73195dd8033d1d03b5c0
pawsa <pawsa@1f1dca69-5516-0410-b834-e485effd04c3>
2006-10-29 20:27:33 +0000

    sendmail: fix queue location in default config file

--- make/sendmail.mk
+++ make/sendmail.mk
@@ -20,7 +20,7 @@ SENDMAIL_CONFLICTS=postfix
 #
 # SENDMAIL_IPK_VERSION should be incremented when the ipk changes.
 #
-SENDMAIL_IPK_VERSION=1
+SENDMAIL_IPK_VERSION=2
 
 #
 # SENDMAIL_CONFFILES should be a list of user-editable files
