d01a8e7b8b92c41882f2b3c7cd145fa757e32aa9
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2006-11-01 00:51:49 +0000

    mtr: upstream upgrade to 0.72, generate control file

--- trunk/make/mtr.mk
+++ trunk/make/mtr.mk
@@ -3,7 +3,6 @@
 # mtr
 #
 ###########################################################
-
 #
 # MTR_VERSION, MTR_SITE and MTR_SOURCE define
 # the upstream location of the source code for the package.
@@ -13,12 +12,26 @@
 # It is usually "zcat" (for .gz) or "bzcat" (for .bz2)
 #
 # You should change all these variables to suit your package.
+# Please make sure that you add a description, and that you
+# list all your packages' dependencies, seperated by commas.
+# 
+# If you list yourself as MAINTAINER, please give a valid email
+# address, and indicate your irc nick if it cannot be easily deduced
+# from your name or email address.  If you leave MAINTAINER set to
+# "NSLU2 Linux" other developers will feel free to edit.
 #
 MTR_SITE=ftp://ftp.bitwizard.nl/mtr
-MTR_VERSION=0.69
+MTR_VERSION=0.72
 MTR_SOURCE=mtr-$(MTR_VERSION).tar.gz
 MTR_DIR=mtr-$(MTR_VERSION)
 MTR_UNZIP=zcat
+MTR_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
+MTR_DESCRIPTION=mtr combines the functionality of the 'traceroute' and 'ping' programs in a single network diagnostic tool.
+MTR_SECTION=net
+MTR_PRIORITY=optional
+MTR_DEPENDS=ncurses
+MTR_SUGGESTS=
+MTR_CONFLICTS=
 
 #
 # MTR_IPK_VERSION should be incremented when the ipk changes.
@@ -27,14 +40,13 @@ MTR_IPK_VERSION=1
 
 #
 # MTR_CONFFILES should be a list of user-editable files
-MTR_CONFFILES=
+#MTR_CONFFILES=/opt/etc/mtr.conf /opt/etc/init.d/SXXmtr
 
 #
 # MTR_PATCHES should list any patches, in the the order in
 # which they should be applied to the source code.
 #
 #MTR_PATCHES=$(MTR_SOURCE_DIR)/configure.patch
-MTR_PATCHES=
 
 #
 # If the compilation of the package requires additional
@@ -56,6 +68,8 @@ MTR_BUILD_DIR=$(BUILD_DIR)/mtr
 MTR_SOURCE_DIR=$(SOURCE_DIR)/mtr
 MTR_IPK_DIR=$(BUILD_DIR)/mtr-$(MTR_VERSION)-ipk
 MTR_IPK=$(BUILD_DIR)/mtr_$(MTR_VERSION)-$(MTR_IPK_VERSION)_$(TARGET_ARCH).ipk
+
+.PHONY: mtr-source mtr-unpack mtr mtr-stage mtr-ipk mtr-clean mtr-dirclean mtr-check
 
 #
 # This is the dependency on the source code.  If the source is missing,
@@ -86,12 +100,20 @@ mtr-source: $(DL_DIR)/$(MTR_SOURCE) $(MTR_PATCHES)
 # If the compilation of the package requires other packages to be staged
 # first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
 #
-$(MTR_BUILD_DIR)/.configured: $(DL_DIR)/$(MTR_SOURCE) $(MTR_PATCHES)
+# If the package uses  GNU libtool, you should invoke $(PATCH_LIBTOOL) as
+# shown below to make various patches to it.
+#
+$(MTR_BUILD_DIR)/.configured: $(DL_DIR)/$(MTR_SOURCE) $(MTR_PATCHES) make/mtr.mk
 	$(MAKE) ncurses-stage
 	rm -rf $(BUILD_DIR)/$(MTR_DIR) $(MTR_BUILD_DIR)
 	$(MTR_UNZIP) $(DL_DIR)/$(MTR_SOURCE) | tar -C $(BUILD_DIR) -xvf -
-#	cat $(MTR_PATCHES) | patch -d $(BUILD_DIR)/$(MTR_DIR) -p1
-	mv $(BUILD_DIR)/$(MTR_DIR) $(MTR_BUILD_DIR)
+	if test -n "$(MTR_PATCHES)" ; \
+		then cat $(MTR_PATCHES) | \
+		patch -d $(BUILD_DIR)/$(MTR_DIR) -p0 ; \
+	fi
+	if test "$(BUILD_DIR)/$(MTR_DIR)" != "$(MTR_BUILD_DIR)" ; \
+		then mv $(BUILD_DIR)/$(MTR_DIR) $(MTR_BUILD_DIR) ; \
+	fi
 	(cd $(MTR_BUILD_DIR); \
 		$(TARGET_CONFIGURE_OPTS) \
 		CPPFLAGS="$(STAGING_CPPFLAGS) $(MTR_CPPFLAGS)" \
@@ -101,9 +123,11 @@ $(MTR_BUILD_DIR)/.configured: $(DL_DIR)/$(MTR_SOURCE) $(MTR_PATCHES)
 		--host=$(GNU_TARGET_NAME) \
 		--target=$(GNU_TARGET_NAME) \
 		--prefix=/opt \
-		--disable-nls \
 		--without-gtk \
+		--disable-nls \
+		--disable-static \
 	)
+#	$(PATCH_LIBTOOL) $(MTR_BUILD_DIR)/libtool
 	touch $(MTR_BUILD_DIR)/.configured
 
 mtr-unpack: $(MTR_BUILD_DIR)/.configured
@@ -135,6 +159,25 @@ $(MTR_BUILD_DIR)/.staged: $(MTR_BUILD_DIR)/.built
 mtr-stage: $(MTR_BUILD_DIR)/.staged
 
 #
+# This rule creates a control file for ipkg.  It is no longer
+# necessary to create a seperate control file under sources/mtr
+#
+$(MTR_IPK_DIR)/CONTROL/control:
+	@install -d $(@D)
+	@rm -f $@
+	@echo "Package: mtr" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(MTR_PRIORITY)" >>$@
+	@echo "Section: $(MTR_SECTION)" >>$@
+	@echo "Version: $(MTR_VERSION)-$(MTR_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(MTR_MAINTAINER)" >>$@
+	@echo "Source: $(MTR_SITE)/$(MTR_SOURCE)" >>$@
+	@echo "Description: $(MTR_DESCRIPTION)" >>$@
+	@echo "Depends: $(MTR_DEPENDS)" >>$@
+	@echo "Suggests: $(MTR_SUGGESTS)" >>$@
+	@echo "Conflicts: $(MTR_CONFLICTS)" >>$@
+
+#
 # This builds the IPK file.
 #
 # Binaries should be installed into $(MTR_IPK_DIR)/opt/sbin or $(MTR_IPK_DIR)/opt/bin
@@ -149,15 +192,15 @@ mtr-stage: $(MTR_BUILD_DIR)/.staged
 $(MTR_IPK): $(MTR_BUILD_DIR)/.built
 	rm -rf $(MTR_IPK_DIR) $(BUILD_DIR)/mtr_*_$(TARGET_ARCH).ipk
 	$(MAKE) -C $(MTR_BUILD_DIR) DESTDIR=$(MTR_IPK_DIR) install
+	$(STRIP_COMMAND) $(MTR_IPK_DIR)/opt/sbin/mtr
 #	install -d $(MTR_IPK_DIR)/opt/etc/
-#	install -m 755 $(MTR_SOURCE_DIR)/mtr.conf $(MTR_IPK_DIR)/opt/etc/mtr.conf
+#	install -m 644 $(MTR_SOURCE_DIR)/mtr.conf $(MTR_IPK_DIR)/opt/etc/mtr.conf
 #	install -d $(MTR_IPK_DIR)/opt/etc/init.d
 #	install -m 755 $(MTR_SOURCE_DIR)/rc.mtr $(MTR_IPK_DIR)/opt/etc/init.d/SXXmtr
-	install -d $(MTR_IPK_DIR)/CONTROL
-	install -m 644 $(MTR_SOURCE_DIR)/control $(MTR_IPK_DIR)/CONTROL/control
-#	install -m 644 $(MTR_SOURCE_DIR)/postinst $(MTR_IPK_DIR)/CONTROL/postinst
-#	install -m 644 $(MTR_SOURCE_DIR)/prerm $(MTR_IPK_DIR)/CONTROL/prerm
-#	echo $(MTR_CONFFILES) | sed -e 's/ /\n/g' > $(MTR_IPK_DIR)/CONTROL/conffiles
+	$(MAKE) $(MTR_IPK_DIR)/CONTROL/control
+#	install -m 755 $(MTR_SOURCE_DIR)/postinst $(MTR_IPK_DIR)/CONTROL/postinst
+#	install -m 755 $(MTR_SOURCE_DIR)/prerm $(MTR_IPK_DIR)/CONTROL/prerm
+	echo $(MTR_CONFFILES) | sed -e 's/ /\n/g' > $(MTR_IPK_DIR)/CONTROL/conffiles
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(MTR_IPK_DIR)
 
 #
@@ -169,6 +212,7 @@ mtr-ipk: $(MTR_IPK)
 # This is called from the top level makefile to clean all of the built files.
 #
 mtr-clean:
+	rm -f $(MTR_BUILD_DIR)/.built
 	-$(MAKE) -C $(MTR_BUILD_DIR) clean
 
 #
@@ -177,3 +221,9 @@ mtr-clean:
 #
 mtr-dirclean:
 	rm -rf $(BUILD_DIR)/$(MTR_DIR) $(MTR_BUILD_DIR) $(MTR_IPK_DIR) $(MTR_IPK)
+#
+#
+# Some sanity check for the package.
+#
+mtr-check:
+	perl scripts/optware-check-package.pl --target=$(OPTWARE_TARGET) $(MTR_IPK)
