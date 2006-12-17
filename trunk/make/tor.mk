b8dcfadf15b8eb7f904ea6947a824bf09a649400
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2006-12-17 21:37:10 +0000

    tor: 0.1.1.25 -> 0.1.1.26

--- trunk/make/tor.mk
+++ trunk/make/tor.mk
@@ -21,7 +21,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 TOR_SITE=http://tor.eff.org/dist
-TOR_VERSION=0.1.1.25
+TOR_VERSION=0.1.1.26
 TOR_SOURCE=tor-$(TOR_VERSION).tar.gz
 TOR_DIR=tor-$(TOR_VERSION)
 TOR_UNZIP=zcat
