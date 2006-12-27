d0bea85a622bfba3e60381c1d2de53c6081e3a50
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2006-12-27 06:18:39 +0000

    strace builds fine on slugosbe; fixed textutils for slugosbe

--- trunk/make/textutils.mk
+++ trunk/make/textutils.mk
@@ -41,7 +41,7 @@ TEXTUTILS_CONFLICTS=busybox-links
 #
 # TEXTUTILS_IPK_VERSION should be incremented when the ipk changes.
 #
-TEXTUTILS_IPK_VERSION=3
+TEXTUTILS_IPK_VERSION=4
 
 #
 # TEXTUTILS_CONFFILES should be a list of user-editable files
@@ -109,6 +109,9 @@ $(TEXTUTILS_BUILD_DIR)/.configured: $(DL_DIR)/$(TEXTUTILS_SOURCE) $(TEXTUTILS_PA
 	$(TEXTUTILS_UNZIP) $(DL_DIR)/$(TEXTUTILS_SOURCE) | tar -C $(BUILD_DIR) -xvf -
 #	cat $(TEXTUTILS_PATCHES) | patch -d $(BUILD_DIR)/$(TEXTUTILS_DIR) -p1
 	mv $(BUILD_DIR)/$(TEXTUTILS_DIR) $(TEXTUTILS_BUILD_DIR)
+ifeq ($(OPTWARE_TARGET), slugosbe)
+	sed -ie '/\*malloc *()/d' $(TEXTUTILS_BUILD_DIR)/lib/putenv.c
+endif
 	(cd $(TEXTUTILS_BUILD_DIR); \
 		$(TARGET_CONFIGURE_OPTS) \
 		CPPFLAGS="$(STAGING_CPPFLAGS) $(TEXTUTILS_CPPFLAGS)" \
