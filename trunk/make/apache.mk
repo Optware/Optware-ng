9c47747f1039dac793835f25cc9ef86c785312c9
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2006-12-27 18:04:44 +0000

    apache: staged /opt/libexec/mod_*.so were read-only on some platforms

--- trunk/make/apache.mk
+++ trunk/make/apache.mk
@@ -213,6 +213,7 @@ apache: $(APACHE_BUILD_DIR)/.built
 #
 $(APACHE_BUILD_DIR)/.staged: $(APACHE_BUILD_DIR)/.built
 	rm -f $(APACHE_BUILD_DIR)/.staged
+	rm -f $(STAGING_PREFIX)/libexec/mod_*.so
 	$(MAKE) -C $(APACHE_BUILD_DIR) install installbuilddir=/opt/share/apache2/build DESTDIR=$(STAGING_DIR)
 	sed -i -e 's!includedir = .*!includedir = $(STAGING_DIR)/opt/include/apache2!' $(STAGING_PREFIX)/share/apache2/build/config_vars.mk
 	touch $(APACHE_BUILD_DIR)/.staged
