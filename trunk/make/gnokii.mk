b7bffe71676c042fc0260a07231eec5d436d6556
marceln <marceln@1f1dca69-5516-0410-b834-e485effd04c3>
2006-12-29 10:31:48 +0000

    OPTWARE: Added gnokii

---
+++ trunk/make/gnokii.mk
@@ -0,0 +1,238 @@
+###########################################################
+#
+# gnokii
+#
+###########################################################
+#
+# $Id$
+#
+# I have placed my name as maintainer so that people can ask
+# questions. But feel free to update or change this package
+# if there are reasons.
+#
+http://www.gnokii.org/download/gnokii/gnokii-0.6.14.tar.gz
+GNOKII_SITE=http://www.gnokii.org/download/gnokii
+GNOKII_VERSION=0.6.14
+GNOKII_SOURCE=gnokii-$(GNOKII_VERSION).tar.bz2
+GNOKII_DIR=gnokii-$(GNOKII_VERSION)
+GNOKII_UNZIP=bzcat
+GNOKII_MAINTAINER=Marcel Nijenhof <nslu2@pion.xs4all.nl>
+GNOKII_DESCRIPTION=a multisystem tool suite and modem driver for the mobile phones
+GNOKII_SECTION=misc
+GNOKII_PRIORITY=optional
+GNOKII_DEPENDS=libusb, bluez-libs
+GNOKII_SUGGESTS=
+GNOKII_CONFLICTS=
+
+#
+# GNOKII_IPK_VERSION should be incremented when the ipk changes.
+#
+GNOKII_IPK_VERSION=1
+
+#
+# GNOKII_CONFFILES should be a list of user-editable files
+# GNOKII_CONFFILES=/opt/etc/gnokii.conf /opt/etc/init.d/SXXgnokii
+
+#
+# GNOKII_PATCHES should list any patches, in the the order in
+# which they should be applied to the source code.
+#
+# GNOKII_PATCHES=$(GNOKII_SOURCE_DIR)/configure.patch
+
+#
+# If the compilation of the package requires additional
+# compilation or linking flags, then list them here.
+#
+GNOKII_CPPFLAGS=
+GNOKII_LDFLAGS=
+
+#
+# GNOKII_BUILD_DIR is the directory in which the build is done.
+# GNOKII_SOURCE_DIR is the directory which holds all the
+# patches and ipkg control files.
+# GNOKII_IPK_DIR is the directory in which the ipk is built.
+# GNOKII_IPK is the name of the resulting ipk files.
+#
+# You should not change any of these variables.
+#
+GNOKII_BUILD_DIR=$(BUILD_DIR)/gnokii
+GNOKII_SOURCE_DIR=$(SOURCE_DIR)/gnokii
+GNOKII_IPK_DIR=$(BUILD_DIR)/gnokii-$(GNOKII_VERSION)-ipk
+GNOKII_IPK=$(BUILD_DIR)/gnokii_$(GNOKII_VERSION)-$(GNOKII_IPK_VERSION)_$(TARGET_ARCH).ipk
+
+.PHONY: gnokii-source gnokii-unpack gnokii gnokii-stage gnokii-ipk gnokii-clean gnokii-dirclean gnokii-check
+
+#
+# This is the dependency on the source code.  If the source is missing,
+# then it will be fetched from the site using wget.
+#
+$(DL_DIR)/$(GNOKII_SOURCE):
+	$(WGET) -P $(DL_DIR) $(GNOKII_SITE)/$(GNOKII_SOURCE)
+
+#
+# The source code depends on it existing within the download directory.
+# This target will be called by the top level Makefile to download the
+# source code's archive (.tar.gz, .bz2, etc.)
+#
+gnokii-source: $(DL_DIR)/$(GNOKII_SOURCE) $(GNOKII_PATCHES)
+
+#
+# This target unpacks the source code in the build directory.
+# If the source archive is not .tar.gz or .tar.bz2, then you will need
+# to change the commands here.  Patches to the source code are also
+# applied in this target as required.
+#
+# This target also configures the build within the build directory.
+# Flags such as LDFLAGS and CPPFLAGS should be passed into configure
+# and NOT $(MAKE) below.  Passing it to configure causes configure to
+# correctly BUILD the Makefile with the right paths, where passing it
+# to Make causes it to override the default search paths of the compiler.
+#
+# If the compilation of the package requires other packages to be staged
+# first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
+#
+# If the package uses  GNU libtool, you should invoke $(PATCH_LIBTOOL) as
+# shown below to make various patches to it.
+#
+$(GNOKII_BUILD_DIR)/.configured: $(DL_DIR)/$(GNOKII_SOURCE) $(GNOKII_PATCHES) make/gnokii.mk
+	$(MAKE) libusb-stage bluez-libs-stage
+	rm -rf $(BUILD_DIR)/$(GNOKII_DIR) $(GNOKII_BUILD_DIR)
+	$(GNOKII_UNZIP) $(DL_DIR)/$(GNOKII_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+	if test -n "$(GNOKII_PATCHES)" ; \
+		then cat $(GNOKII_PATCHES) | \
+		patch -d $(BUILD_DIR)/$(GNOKII_DIR) -p0 ; \
+	fi
+	if test "$(BUILD_DIR)/$(GNOKII_DIR)" != "$(GNOKII_BUILD_DIR)" ; \
+		then mv $(BUILD_DIR)/$(GNOKII_DIR) $(GNOKII_BUILD_DIR) ; \
+	fi
+	(cd $(GNOKII_BUILD_DIR); \
+		$(TARGET_CONFIGURE_OPTS) \
+		CPPFLAGS="$(STAGING_CPPFLAGS) $(GNOKII_CPPFLAGS)" \
+		LDFLAGS="$(STAGING_LDFLAGS) $(GNOKII_LDFLAGS)" \
+		./configure \
+		--build=$(GNU_HOST_NAME) \
+		--host=$(GNU_TARGET_NAME) \
+		--target=$(GNU_TARGET_NAME) \
+		--prefix=/opt \
+		--disable-nls \
+		--disable-static \
+	)
+	$(PATCH_LIBTOOL) $(GNOKII_BUILD_DIR)/libtool
+	touch $(GNOKII_BUILD_DIR)/.configured
+
+gnokii-unpack: $(GNOKII_BUILD_DIR)/.configured
+
+#
+# This builds the actual binary.
+#
+$(GNOKII_BUILD_DIR)/.built: $(GNOKII_BUILD_DIR)/.configured
+	rm -f $(GNOKII_BUILD_DIR)/.built
+	$(MAKE) -C $(GNOKII_BUILD_DIR)
+	touch $(GNOKII_BUILD_DIR)/.built
+
+#
+# This is the build convenience target.
+#
+gnokii: $(GNOKII_BUILD_DIR)/.built
+
+#
+# If you are building a library, then you need to stage it too.
+#
+$(GNOKII_BUILD_DIR)/.staged: $(GNOKII_BUILD_DIR)/.built
+	rm -f $(GNOKII_BUILD_DIR)/.staged
+	$(MAKE) -C $(GNOKII_BUILD_DIR) DESTDIR=$(STAGING_DIR) install
+	touch $(GNOKII_BUILD_DIR)/.staged
+
+gnokii-stage: $(GNOKII_BUILD_DIR)/.staged
+
+#
+# This rule creates a control file for ipkg.  It is no longer
+# necessary to create a seperate control file under sources/gnokii
+#
+$(GNOKII_IPK_DIR)/CONTROL/control:
+	@install -d $(@D)
+	@rm -f $@
+	@echo "Package: gnokii" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(GNOKII_PRIORITY)" >>$@
+	@echo "Section: $(GNOKII_SECTION)" >>$@
+	@echo "Version: $(GNOKII_VERSION)-$(GNOKII_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(GNOKII_MAINTAINER)" >>$@
+	@echo "Source: $(GNOKII_SITE)/$(GNOKII_SOURCE)" >>$@
+	@echo "Description: $(GNOKII_DESCRIPTION)" >>$@
+	@echo "Depends: $(GNOKII_DEPENDS)" >>$@
+	@echo "Suggests: $(GNOKII_SUGGESTS)" >>$@
+	@echo "Conflicts: $(GNOKII_CONFLICTS)" >>$@
+
+#
+# This builds the IPK file.
+#
+# Binaries should be installed into $(GNOKII_IPK_DIR)/opt/sbin or $(GNOKII_IPK_DIR)/opt/bin
+# (use the location in a well-known Linux distro as a guide for choosing sbin or bin).
+# Libraries and include files should be installed into $(GNOKII_IPK_DIR)/opt/{lib,include}
+# Configuration files should be installed in $(GNOKII_IPK_DIR)/opt/etc/gnokii/...
+# Documentation files should be installed in $(GNOKII_IPK_DIR)/opt/doc/gnokii/...
+# Daemon startup scripts should be installed in $(GNOKII_IPK_DIR)/opt/etc/init.d/S??gnokii
+#
+# You may need to patch your application to make it use these locations.
+#
+$(GNOKII_IPK): $(GNOKII_BUILD_DIR)/.built
+	rm -rf $(GNOKII_IPK_DIR) $(BUILD_DIR)/gnokii_*_$(TARGET_ARCH).ipk
+	$(MAKE) -C $(GNOKII_BUILD_DIR) DESTDIR=$(GNOKII_IPK_DIR) install-strip
+	chmod 755 $(GNOKII_IPK_DIR)/opt/*bin/*
+	$(TARGET_STRIP) $(GNOKII_IPK_DIR)/opt/bin/gnokii
+	$(TARGET_STRIP) $(GNOKII_IPK_DIR)/opt/sbin/*iid*
+	$(TARGET_STRIP) $(GNOKII_IPK_DIR)/opt/lib/libgnokii.so.3.0.0
+#
+# Programs script which don't work
+#
+	rm $(GNOKII_IPK_DIR)/opt/bin/todologo	# Depends on /usr/bin/perl
+	rm $(GNOKII_IPK_DIR)/opt/bin/ppm2nokia	# Doesn't work in busybox sh
+	rm $(GNOKII_IPK_DIR)/opt/bin/sendsms	# Needs dialog
+#
+# Remove documentation and development files
+#
+	rm $(GNOKII_IPK_DIR)/opt/lib/pkgconfig/*
+	rmdir $(GNOKII_IPK_DIR)/opt/lib/pkgconfig
+	rm -rf $(GNOKII_IPK_DIR)/opt/share/doc/gnokii 
+	rmdir $(GNOKII_IPK_DIR)/opt/share/doc
+	rmdir $(GNOKII_IPK_DIR)/opt/share
+	rm -rf $(GNOKII_IPK_DIR)/opt/include/gnokii*
+	rmdir  $(GNOKII_IPK_DIR)/opt/include
+	install -d $(GNOKII_IPK_DIR)/opt/etc/
+#	install -m 644 $(GNOKII_SOURCE_DIR)/gnokii.conf $(GNOKII_IPK_DIR)/opt/etc/gnokii.conf
+#	install -d $(GNOKII_IPK_DIR)/opt/etc/init.d
+#	install -m 755 $(GNOKII_SOURCE_DIR)/rc.gnokii $(GNOKII_IPK_DIR)/opt/etc/init.d/SXXgnokii
+#	sed -i -e '/^#!/aOPTWARE_TARGET=${OPTWARE_TARGET}' $(XINETD_IPK_DIR)/opt/etc/init.d/SXXgnokii
+	$(MAKE) $(GNOKII_IPK_DIR)/CONTROL/control
+#	install -m 755 $(GNOKII_SOURCE_DIR)/postinst $(GNOKII_IPK_DIR)/CONTROL/postinst
+	sed -i -e '/^#!/aOPTWARE_TARGET=${OPTWARE_TARGET}' $(XINETD_IPK_DIR)/CONTROL/postinst
+#	install -m 755 $(GNOKII_SOURCE_DIR)/prerm $(GNOKII_IPK_DIR)/CONTROL/prerm
+#	sed -i -e '/^#!/aOPTWARE_TARGET=${OPTWARE_TARGET}' $(XINETD_IPK_DIR)/CONTROL/prerm
+	echo $(GNOKII_CONFFILES) | sed -e 's/ /\n/g' > $(GNOKII_IPK_DIR)/CONTROL/conffiles
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(GNOKII_IPK_DIR)
+
+#
+# This is called from the top level makefile to create the IPK file.
+#
+gnokii-ipk: $(GNOKII_IPK)
+
+#
+# This is called from the top level makefile to clean all of the built files.
+#
+gnokii-clean:
+	rm -f $(GNOKII_BUILD_DIR)/.built
+	-$(MAKE) -C $(GNOKII_BUILD_DIR) clean
+
+#
+# This is called from the top level makefile to clean all dynamically created
+# directories.
+#
+gnokii-dirclean:
+	rm -rf $(BUILD_DIR)/$(GNOKII_DIR) $(GNOKII_BUILD_DIR) $(GNOKII_IPK_DIR) $(GNOKII_IPK)
+#
+#
+# Some sanity check for the package.
+#
+gnokii-check: $(GNOKII_IPK)
+	perl scripts/optware-check-package.pl --target=$(OPTWARE_TARGET) $(GNOKII_IPK)
