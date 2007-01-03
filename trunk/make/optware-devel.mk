61c1e7ea023ff33b545d46e2fda38396ddd3b783
oleo <oleo@1f1dca69-5516-0410-b834-e485effd04c3>
2007-01-03 13:24:09 +0000

    optware: native toolchain for uClibc

--- trunk/make/optware-devel.mk
+++ trunk/make/optware-devel.mk
@@ -10,11 +10,15 @@ OPTWARE-DEVEL_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
 OPTWARE-DEVEL_DESCRIPTION=This is a meta package that bundles all the packages required for optware native development.
 OPTWARE-DEVEL_SECTION=util
 OPTWARE-DEVEL_PRIORITY=optional
+ifeq ($(LIBC_STYLE), glibc)
 OPTWARE-DEVEL_DEPENDS=autoconf, automake, bash, bison, bzip2, coreutils, crosstool-native, diffutils, file, findutils, flex, gawk, libstdc++, groff, libtool, make, m4, ncurses, openssl, patch, perl, pkgconfig, python, rsync, sed, svn, tar, wget-ssl
+else
+OPTWARE-DEVEL_DEPENDS=autoconf, automake, bash, bison, buildroot, bzip2, coreutils, diffutils, file, findutils, flex, gawk, libuclibc++, groff, libtool, make, m4, ncurses, openssl, patch, pkgconfig, python, rsync, sed, svn, tar, wget-ssl
+endif
 OPTWARE-DEVEL_SUGGESTS=
 OPTWARE-DEVEL_CONFLICTS=
 
-OPTWARE-DEVEL_IPK_VERSION=4
+OPTWARE-DEVEL_IPK_VERSION=5
 
 OPTWARE-DEVEL_IPK_DIR=$(BUILD_DIR)/optware-devel-$(OPTWARE-DEVEL_VERSION)-ipk
 OPTWARE-DEVEL_IPK=$(BUILD_DIR)/optware-devel_$(OPTWARE-DEVEL_VERSION)-$(OPTWARE-DEVEL_IPK_VERSION)_$(TARGET_ARCH).ipk
