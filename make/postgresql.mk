63a984dd10e8973b46e26b2a7b40bd843353c2d8
oleo <oleo@1f1dca69-5516-0410-b834-e485effd04c3>
2007-01-07 13:36:59 +0000

    postgresql: source fix

--- make/postgresql.mk
+++ make/postgresql.mk
@@ -26,7 +26,7 @@
 # from your name or email address.  If you leave MAINTAINER set to
 # "NSLU2 Linux" other developers will feel free to edit.
 #
-POSTGRESQL_SITE=ftp://ftp.postgresql.org/pub/source/v8.2
+POSTGRESQL_SITE=ftp://ftp.postgresql.org/pub/source/v$(POSTGRESQL_VERSION)
 POSTGRESQL_VERSION=8.2.0
 POSTGRESQL_SOURCE=postgresql-base-$(POSTGRESQL_VERSION).tar.bz2
 POSTGRESQL_DIR=postgresql-$(POSTGRESQL_VERSION)
