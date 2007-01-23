5df8b2f2726dfdd424e10a4c1798f7fb09702b51
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2007-01-23 19:28:45 +0000

    mpd: added and promoted pre request, lacking init.d/rc file for now

--- trunk/make/mpd.mk
+++ trunk/make/mpd.mk
@@ -3,12 +3,6 @@
 # mpd
 #
 ###########################################################
-
-# You must replace "mpd" and "MPD" with the lower case name and
-# upper case name of your new package.  Some places below will say
-# "Do not change this" - that does not include this global change,
-# which must always be done to ensure we have unique names.
-
 #
 # MPD_VERSION, MPD_SITE and MPD_SOURCE define
 # the upstream location of the source code for the package.
@@ -18,12 +12,26 @@
 # It is usually "zcat" (for .gz) or "bzcat" (for .bz2)
 #
 # You should change all these variables to suit your package.
+# Please make sure that you add a description, and that you
+# list all your packages' dependencies, seperated by commas.
+# 
+# If you list yourself as MAINTAINER, please give a valid email
+# address, and indicate your irc nick if it cannot be easily deduced
+# from your name or email address.  If you leave MAINTAINER set to
+# "NSLU2 Linux" other developers will feel free to edit.
 #
-MPD_SITE=http://mercury.chem.pitt.edu/~shank
-MPD_VERSION=0.11.5
-MPD_SOURCE=mpd-$(MPD_VERSION).tar.gz
+MPD_SITE=http://www.musicpd.org/uploads/files
+MPD_VERSION=0.12.1
+MPD_SOURCE=mpd-$(MPD_VERSION).tar.bz2
 MPD_DIR=mpd-$(MPD_VERSION)
-MPD_UNZIP=zcat
+MPD_UNZIP=bzcat
+MPD_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
+MPD_DESCRIPTION=Music Player Daemon (MPD) allows remote access for playing music.
+MPD_SECTION=audio
+MPD_PRIORITY=optional
+MPD_DEPENDS=audiofile, libid3tag, libmad, libao, flac, libvorbisidec
+MPD_SUGGESTS=
+MPD_CONFLICTS=
 
 #
 # MPD_IPK_VERSION should be incremented when the ipk changes.
@@ -32,13 +40,13 @@ MPD_IPK_VERSION=1
 
 #
 # MPD_CONFFILES should be a list of user-editable files
-MPD_CONFFILES=
+#MPD_CONFFILES=/opt/etc/mpd.conf /opt/etc/init.d/SXXmpd
 
 #
 # MPD_PATCHES should list any patches, in the the order in
 # which they should be applied to the source code.
 #
-MPD_PATCHES=/dev/null
+#MPD_PATCHES=$(MPD_SOURCE_DIR)/configure.patch
 
 #
 # If the compilation of the package requires additional
@@ -60,6 +68,8 @@ MPD_BUILD_DIR=$(BUILD_DIR)/mpd
 MPD_SOURCE_DIR=$(SOURCE_DIR)/mpd
 MPD_IPK_DIR=$(BUILD_DIR)/mpd-$(MPD_VERSION)-ipk
 MPD_IPK=$(BUILD_DIR)/mpd_$(MPD_VERSION)-$(MPD_IPK_VERSION)_$(TARGET_ARCH).ipk
+
+.PHONY: mpd-source mpd-unpack mpd mpd-stage mpd-ipk mpd-clean mpd-dirclean mpd-check
 
 #
 # This is the dependency on the source code.  If the source is missing,
@@ -90,12 +100,25 @@ mpd-source: $(DL_DIR)/$(MPD_SOURCE) $(MPD_PATCHES)
 # If the compilation of the package requires other packages to be staged
 # first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
 #
-$(MPD_BUILD_DIR)/.configured: $(DL_DIR)/$(MPD_SOURCE) $(MPD_PATCHES)
-	$(MAKE) libao-stage libid3tag-stage libmad-stage libvorbis-stage libogg-stage
+# If the package uses  GNU libtool, you should invoke $(PATCH_LIBTOOL) as
+# shown below to make various patches to it.
+#
+$(MPD_BUILD_DIR)/.configured: $(DL_DIR)/$(MPD_SOURCE) $(MPD_PATCHES) make/mpd.mk
+	$(MAKE) audiofile-stage
+	$(MAKE) flac-stage
+	$(MAKE) libao-stage
+	$(MAKE) libid3tag-stage
+	$(MAKE) libmad-stage
+	$(MAKE) libvorbisidec-stage
 	rm -rf $(BUILD_DIR)/$(MPD_DIR) $(MPD_BUILD_DIR)
 	$(MPD_UNZIP) $(DL_DIR)/$(MPD_SOURCE) | tar -C $(BUILD_DIR) -xvf -
-	cat $(MPD_PATCHES) | patch -d $(BUILD_DIR)/$(MPD_DIR) -p1
-	mv $(BUILD_DIR)/$(MPD_DIR) $(MPD_BUILD_DIR)
+	if test -n "$(MPD_PATCHES)" ; \
+		then cat $(MPD_PATCHES) | \
+		patch -d $(BUILD_DIR)/$(MPD_DIR) -p0 ; \
+	fi
+	if test "$(BUILD_DIR)/$(MPD_DIR)" != "$(MPD_BUILD_DIR)" ; \
+		then mv $(BUILD_DIR)/$(MPD_DIR) $(MPD_BUILD_DIR) ; \
+	fi
 	(cd $(MPD_BUILD_DIR); \
 		$(TARGET_CONFIGURE_OPTS) \
 		CPPFLAGS="$(STAGING_CPPFLAGS) $(MPD_CPPFLAGS)" \
@@ -105,29 +128,28 @@ $(MPD_BUILD_DIR)/.configured: $(DL_DIR)/$(MPD_SOURCE) $(MPD_PATCHES)
 		--host=$(GNU_TARGET_NAME) \
 		--target=$(GNU_TARGET_NAME) \
 		--prefix=/opt \
-		--disable-ipv6 \
-		--disable-flac \
-		--disable-aac \
-		--disable-audiofile \
-		--disable-mod \
+		\
+		--enable-ao \
+		--enable-audiofile \
+		--enable-flac \
+		--enable-id3 \
+		--enable-mp3 \
+		--enable-oggvorbis \
+		\
+		--with-ao=$(STAGING_PREFIX) \
+		--with-audiofile-prefix=$(STAGING_PREFIX) \
+		--with-id3tag=$(STAGING_PREFIX) \
+		--with-libFLAC=$(STAGING_PREFIX) \
+		--with-mad=$(STAGING_PREFIX) \
+		--with-flac=$(STAGING_PREFIX) \
+		--with-tremor=$(STAGING_PREFIX) \
+		\
 		--disable-nls \
-		--disable-aotest \
-		--disable-oggtest \
-		--disable-vorbistest \
-		--disable-libFLACtest \
-		--disable-audiofiletest \
-		--disable-libmikmodtest \
-		--enable-mpd-mad \
-		--enable-mpd-id3tag \
-		--with-ao=$(STAGING_DIR)/opt \
-		--with-iconv=$(STAGING_DIR)/opt \
-		--with-id3tag=$(STAGING_DIR)/opt \
-		--with-mad=$(STAGING_DIR)/opt \
-		--with-ao=$(STAGING_DIR)/opt \
-		--with-ogg=$(STAGING_DIR)/opt \
-		--with-vorbis=$(STAGING_DIR)/opt \
+		--disable-static \
 	)
-	touch $(MPD_BUILD_DIR)/.configured
+	sed -i -e '/^MPD_CFLAGS/s| -I$${prefix}/include||;' $(MPD_BUILD_DIR)/src/Makefile
+	$(PATCH_LIBTOOL) $(MPD_BUILD_DIR)/libtool
+	touch $@
 
 mpd-unpack: $(MPD_BUILD_DIR)/.configured
 
@@ -135,9 +157,9 @@ mpd-unpack: $(MPD_BUILD_DIR)/.configured
 # This builds the actual binary.
 #
 $(MPD_BUILD_DIR)/.built: $(MPD_BUILD_DIR)/.configured
-	rm -f $(MPD_BUILD_DIR)/.built
+	rm -f $@
 	$(MAKE) -C $(MPD_BUILD_DIR)
-	touch $(MPD_BUILD_DIR)/.built
+	touch $@
 
 #
 # This is the build convenience target.
@@ -148,11 +170,30 @@ mpd: $(MPD_BUILD_DIR)/.built
 # If you are building a library, then you need to stage it too.
 #
 $(MPD_BUILD_DIR)/.staged: $(MPD_BUILD_DIR)/.built
-	rm -f $(MPD_BUILD_DIR)/.staged
+	rm -f $@
 	$(MAKE) -C $(MPD_BUILD_DIR) DESTDIR=$(STAGING_DIR) install
-	touch $(MPD_BUILD_DIR)/.staged
+	touch $@
 
 mpd-stage: $(MPD_BUILD_DIR)/.staged
+
+#
+# This rule creates a control file for ipkg.  It is no longer
+# necessary to create a seperate control file under sources/mpd
+#
+$(MPD_IPK_DIR)/CONTROL/control:
+	@install -d $(@D)
+	@rm -f $@
+	@echo "Package: mpd" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(MPD_PRIORITY)" >>$@
+	@echo "Section: $(MPD_SECTION)" >>$@
+	@echo "Version: $(MPD_VERSION)-$(MPD_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(MPD_MAINTAINER)" >>$@
+	@echo "Source: $(MPD_SITE)/$(MPD_SOURCE)" >>$@
+	@echo "Description: $(MPD_DESCRIPTION)" >>$@
+	@echo "Depends: $(MPD_DEPENDS)" >>$@
+	@echo "Suggests: $(MPD_SUGGESTS)" >>$@
+	@echo "Conflicts: $(MPD_CONFLICTS)" >>$@
 
 #
 # This builds the IPK file.
@@ -168,16 +209,18 @@ mpd-stage: $(MPD_BUILD_DIR)/.staged
 #
 $(MPD_IPK): $(MPD_BUILD_DIR)/.built
 	rm -rf $(MPD_IPK_DIR) $(BUILD_DIR)/mpd_*_$(TARGET_ARCH).ipk
-	$(MAKE) -C $(MPD_BUILD_DIR) DESTDIR=$(MPD_IPK_DIR) install
-	install -d $(MPD_IPK_DIR)/opt/etc/
-	install -m 644 $(MPD_SOURCE_DIR)/mpd.conf $(MPD_IPK_DIR)/opt/etc/mpd.conf
-	install -d $(MPD_IPK_DIR)/opt/etc/init.d
-	install -m 755 $(MPD_SOURCE_DIR)/rc.mpd $(MPD_IPK_DIR)/opt/etc/init.d/S61mpd
-	install -d $(MPD_IPK_DIR)/CONTROL
-	install -m 644 $(MPD_SOURCE_DIR)/control $(MPD_IPK_DIR)/CONTROL/control
-	install -m 755 $(MPD_SOURCE_DIR)/postinst $(MPD_IPK_DIR)/CONTROL/postinst
-	install -m 755 $(MPD_SOURCE_DIR)/prerm $(MPD_IPK_DIR)/CONTROL/prerm
-	echo $(MPD_CONFFILES) | sed -e 's/ /\n/g' > $(MPD_IPK_DIR)/CONTROL/conffiles
+	$(MAKE) -C $(MPD_BUILD_DIR) DESTDIR=$(MPD_IPK_DIR) install-strip
+#	install -d $(MPD_IPK_DIR)/opt/etc/
+#	install -m 644 $(MPD_SOURCE_DIR)/mpd.conf $(MPD_IPK_DIR)/opt/etc/mpd.conf
+#	install -d $(MPD_IPK_DIR)/opt/etc/init.d
+#	install -m 755 $(MPD_SOURCE_DIR)/rc.mpd $(MPD_IPK_DIR)/opt/etc/init.d/SXXmpd
+#	sed -i -e '/^#!/aOPTWARE_TARGET=${OPTWARE_TARGET}' $(XINETD_IPK_DIR)/opt/etc/init.d/SXXmpd
+	$(MAKE) $(MPD_IPK_DIR)/CONTROL/control
+#	install -m 755 $(MPD_SOURCE_DIR)/postinst $(MPD_IPK_DIR)/CONTROL/postinst
+#	sed -i -e '/^#!/aOPTWARE_TARGET=${OPTWARE_TARGET}' $(XINETD_IPK_DIR)/CONTROL/postinst
+#	install -m 755 $(MPD_SOURCE_DIR)/prerm $(MPD_IPK_DIR)/CONTROL/prerm
+#	sed -i -e '/^#!/aOPTWARE_TARGET=${OPTWARE_TARGET}' $(XINETD_IPK_DIR)/CONTROL/prerm
+#	echo $(MPD_CONFFILES) | sed -e 's/ /\n/g' > $(MPD_IPK_DIR)/CONTROL/conffiles
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(MPD_IPK_DIR)
 
 #
@@ -189,6 +232,7 @@ mpd-ipk: $(MPD_IPK)
 # This is called from the top level makefile to clean all of the built files.
 #
 mpd-clean:
+	rm -f $(MPD_BUILD_DIR)/.built
 	-$(MAKE) -C $(MPD_BUILD_DIR) clean
 
 #
@@ -197,3 +241,9 @@ mpd-clean:
 #
 mpd-dirclean:
 	rm -rf $(BUILD_DIR)/$(MPD_DIR) $(MPD_BUILD_DIR) $(MPD_IPK_DIR) $(MPD_IPK)
+#
+#
+# Some sanity check for the package.
+#
+mpd-check: $(MPD_IPK)
+	perl scripts/optware-check-package.pl --target=$(OPTWARE_TARGET) $(MPD_IPK)
