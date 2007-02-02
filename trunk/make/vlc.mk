545952293066c67772b67d62789e9022f54e965f
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2007-02-02 05:58:28 +0000

    vlc: enable-x264

--- trunk/make/vlc.mk
+++ trunk/make/vlc.mk
@@ -30,13 +30,13 @@ VLC_DESCRIPTION=VLC is a cross-platform media player and streaming server.
 VLC_SECTION=video
 VLC_PRIORITY=optional
 VLC_DEPENDS=ffmpeg, libmad, libmpeg2, libpng, libxml2, ncurses
-VLC_SUGGESTS=faad2, flac, freetype, liba52, libogg, libvorbis, libdvbpsi, speex
+VLC_SUGGESTS=faad2, flac, freetype, liba52, libogg, libvorbis, libdvbpsi, speex, x264
 VLC_CONFLICTS=
 
 #
 # VLC_IPK_VERSION should be incremented when the ipk changes.
 #
-VLC_IPK_VERSION=4
+VLC_IPK_VERSION=5
 
 #
 # VLC_CONFFILES should be a list of user-editable files
@@ -118,6 +118,7 @@ $(VLC_BUILD_DIR)/.configured: $(DL_DIR)/$(VLC_SOURCE) $(VLC_PATCHES) make/vlc.mk
 	$(MAKE) libxml2-stage
 	$(MAKE) ncurses-stage
 	$(MAKE) speex-stage
+	$(MAKE) x264-stage
 	rm -rf $(BUILD_DIR)/$(VLC_DIR) $(VLC_BUILD_DIR)
 	$(VLC_UNZIP) $(DL_DIR)/$(VLC_SOURCE) | tar -C $(BUILD_DIR) -xvf -
 	if test -n "$(VLC_PATCHES)" ; \
@@ -149,6 +150,7 @@ $(VLC_BUILD_DIR)/.configured: $(DL_DIR)/$(VLC_SOURCE) $(VLC_PATCHES) make/vlc.mk
 		--enable-png \
 		--enable-speex \
 		--enable-vorbis \
+		--enable-x264 \
 		--disable-dts \
 		--disable-dvdnav \
 		--disable-glx \
@@ -160,7 +162,6 @@ $(VLC_BUILD_DIR)/.configured: $(DL_DIR)/$(VLC_SOURCE) $(VLC_PATCHES) make/vlc.mk
 		--disable-sdl \
 		--disable-wxwidgets --disable-skins2 \
 		--disable-x11 \
-		--disable-x264 \
 		--disable-nls \
 		--disable-static \
 	)
