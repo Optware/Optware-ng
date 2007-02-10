fc2c11b21c523c56bcb6d6bdafd06fbd11d157fc
Brian Zhou <bzhou@users.noreply.github.com>
2007-02-10 16:28:29 +0000

    postgresql: 8.2.2 -> 8.2.3

--- make/postgresql.mk
+++ make/postgresql.mk
@@ -26,7 +26,7 @@
 # from your name or email address.  If you leave MAINTAINER set to
 # "NSLU2 Linux" other developers will feel free to edit.
 #
-POSTGRESQL_VERSION=8.2.2
+POSTGRESQL_VERSION=8.2.3
 POSTGRESQL_SITE=ftp://ftp.postgresql.org/pub/source/v$(POSTGRESQL_VERSION)
 POSTGRESQL_SOURCE=postgresql-base-$(POSTGRESQL_VERSION).tar.bz2
 POSTGRESQL_DIR=postgresql-$(POSTGRESQL_VERSION)
