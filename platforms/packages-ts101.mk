bba0029f0562c1776ba2df9f9b2707951574c636
Brian Zhou <bzhou@users.noreply.github.com>
2007-02-24 23:29:49 +0000

    promoted tons of packages for ts101, all of which just need clean rebuild, also removed duplicate uclibc_broken

--- platforms/packages-ts101.mk
+++ platforms/packages-ts101.mk
@@ -6,22 +6,16 @@ SPECIFIC_PACKAGES = \
 
 BROKEN_PACKAGES = \
 	$(UCLIBC_BROKEN_PACKAGES) \
-         appweb atop bitlbee \
-         bzflag bluez-utils chillispot cdargs \
-	 cups cyrus-imapd dansguardian eaccelerator \
-	 ecl fcgi fish ficy freeze ftpcopy gnupg \
-	 gtk gphoto2 libgphoto2 ice \
-	 jabberd jamvm kismet kissdx ldconfig libdvb \
- 	 mediatomb metalog memtester monotone mrtg \
-         mtr mysql-connector-odbc nfs-server nfs-utils nget \
-         nload noip obexftp openser \
-         pango par2cmdline \
-         pound qemu qemu-libc-i386 quagga quickie \
-         rrdcollect rrdtool sablevm screen sendmail \
-         ser sm snort squeak taglib \
-         tethereal ushare vsftpd vte \
-         xauth xaw xchat xmu xt \
-         xterm perl-spamassassin spamassassin \
-	 Mail-SpamAssassin microcom \
-	 mod-python perl-dbd-sqlite uclibc-opt \
+         atop bitlbee \
+         bluez-utils chillispot \
+	 ecl freeze ftpcopy gnupg \
+	 gtk ice \
+	 kissdx \
+ 	 mediatomb \
+         obexftp openser \
+         pango \
+         screen \
+         ser squeak \
+         tethereal ushare vsftpd \
+	 uclibc-opt \
 	 buildroot
