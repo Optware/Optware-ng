d47a8710298782eda802c7357d8d6dbfb967b4c1
Brian Zhou <bzhou@users.noreply.github.com>
2007-02-24 04:03:01 +0000

    promoted for ts101: ed, flip, id3lib, irssi, lsof, man python25, py-simpy, vim lha; all builds fine on nudi

--- platforms/packages-ts101.mk
+++ platforms/packages-ts101.mk
@@ -7,20 +7,20 @@ BROKEN_PACKAGES = \
 	$(UCLIBC_BROKEN_PACKAGES) \
          appweb asterisk14 asterisk14-chan-capi atop bitlbee \
          bzflag bluez-utils chillispot cdargs cherokee \
-	 coreutils cups cyrus-imapd dansguardian dcraw eaccelerator ed \
-	 ecl fcgi fish flip ficy flac freeze ftpcopy git-core gnupg \
-	 gnutls gtk gphoto2 gzip libgphoto2 hexcurse ice id3lib iksemel \
-	 irssi jabberd jamvm kismet kissdx ldconfig libdvb libgcrypt \
- 	 lighttpd lsof loudmouth man mediatomb metalog memtester monotone mpd mrtg \
+	 coreutils cups cyrus-imapd dansguardian dcraw eaccelerator \
+	 ecl fcgi fish ficy flac freeze ftpcopy git-core gnupg \
+	 gnutls gtk gphoto2 libgphoto2 hexcurse ice iksemel \
+	 jabberd jamvm kismet kissdx ldconfig libdvb libgcrypt \
+ 	 lighttpd loudmouth mediatomb metalog memtester monotone mpd mrtg \
          mtr mysql-connector-odbc netcat nfs-server nfs-utils nget nmap \
          nload noip ntop nzbget obexftp opencdk openser \
          pango par2cmdline pcre php php-apache php-fcgi postfix \
-         pound python25 py-simpy qemu qemu-libc-i386 quagga quickie \
+         pound qemu qemu-libc-i386 quagga quickie \
          rc rrdcollect rrdtool ruby sablevm screen sendmail \
          ser sm snort squeak taglib \
          tethereal transcode upslug2 ushare vlc vsftpd vte \
          wput xauth xaw xchat xmu xt \
          xterm zip perl-spamassassin spamassassin \
-	 lsof Mail-SpamAssassin microcom \
-	 mod-python perl-dbd-sqlite rcs vim uclibc-opt \
-	 buildroot lha
+	 Mail-SpamAssassin microcom \
+	 mod-python perl-dbd-sqlite uclibc-opt \
+	 buildroot
