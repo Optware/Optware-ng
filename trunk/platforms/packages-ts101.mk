29b5cd1699513d1f16b168591e6ae78ed34a21f1
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2007-02-24 18:35:52 +0000

    promoted for ts101: libgcrypt, gnutls, cherokee, lighttpd, loudmouth, ikselmel, asterisk14, wput

--- trunk/platforms/packages-ts101.mk
+++ trunk/platforms/packages-ts101.mk
@@ -6,21 +6,21 @@ SPECIFIC_PACKAGES = \
 
 BROKEN_PACKAGES = \
 	$(UCLIBC_BROKEN_PACKAGES) \
-         appweb asterisk14 asterisk14-chan-capi atop bitlbee \
-         bzflag bluez-utils chillispot cdargs cherokee \
+         appweb atop bitlbee \
+         bzflag bluez-utils chillispot cdargs \
 	 cups cyrus-imapd dansguardian dcraw eaccelerator \
 	 ecl fcgi fish ficy freeze ftpcopy gnupg \
-	 gnutls gtk gphoto2 libgphoto2 ice iksemel \
-	 jabberd jamvm kismet kissdx ldconfig libdvb libgcrypt \
- 	 lighttpd loudmouth mediatomb metalog memtester monotone mpd mrtg \
+	 gtk gphoto2 libgphoto2 ice \
+	 jabberd jamvm kismet kissdx ldconfig libdvb \
+ 	 mediatomb metalog memtester monotone mpd mrtg \
          mtr mysql-connector-odbc nfs-server nfs-utils nget \
-         nload noip nzbget obexftp opencdk openser \
+         nload noip nzbget obexftp openser \
          pango par2cmdline pcre postfix \
          pound qemu qemu-libc-i386 quagga quickie \
          rrdcollect rrdtool sablevm screen sendmail \
          ser sm snort squeak taglib \
          tethereal ushare vsftpd vte \
-         wput xauth xaw xchat xmu xt \
+         xauth xaw xchat xmu xt \
          xterm perl-spamassassin spamassassin \
 	 Mail-SpamAssassin microcom \
 	 mod-python perl-dbd-sqlite uclibc-opt \
