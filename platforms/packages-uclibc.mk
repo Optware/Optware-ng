3bb3b299e9c9c860b79ee733f3616ad85d0bb444
Brian Zhou <bzhou@users.noreply.github.com>
2007-02-24 23:28:00 +0000

    moved gambit-c, htop & par2cmdline from uclibc_broken to ddwrt & oleg broken, since these work on ts101

--- platforms/packages-uclibc.mk
+++ platforms/packages-uclibc.mk
@@ -1,14 +1,14 @@
 # Packages that do not work for uclibc
 UCLIBC_BROKEN_PACKAGES = \
 	 bzflag dansguardian \
-	 fcgi fish gambit-c ggrab \
+	 fcgi fish ggrab \
 	 gphoto2 libgphoto2 \
-	 gtk htop ice iperf iptables jabberd \
+	 gtk ice iperf iptables jabberd \
 	 jamvm ldconfig libstdc++ libdvb monotone \
 	 mtr nfs-server nfs-utils nget \
-	 pango par2cmdline \
+	 pango \
 	 qemu qemu-libc-i386 quickie sm \
-	 taglib transcode vte xauth xaw xchat xcursor \
+	 taglib vte xauth xaw xchat xcursor \
 	 xfixes xft xrender xmu xt xterm
 
 # Packages that *only* work for uclibc - do not just put new packages here.
