ffa591fc18e5ddd1a3fee50c4bf0c1a58cb26dad
nail <nail@1f1dca69-5516-0410-b834-e485effd04c3>
2007-02-28 18:00:53 +0000

    add broken packages

--- platforms/packages-ts101.mk
+++ platforms/packages-ts101.mk
@@ -6,15 +6,21 @@ SPECIFIC_PACKAGES = \
 
 BROKEN_PACKAGES = \
 	$(UCLIBC_BROKEN_PACKAGES) \
-         atop \
-         chillispot \
-	 ecl \
-	 freeze \
-	 ftpcopy \
-         openser \
-         ser \
-	 squeak \
-         ushare \
-	 vsftpd \
-	 uclibc-opt \
-	 buildroot
+         amule at atk avahi bitlbee bsdmainutils bzflag \
+         bluez-utils buildroot chillispot coreutils cups cyrus-imapd dcraw ecl \
+         erl-escript erl-yaws esound fcgi fish ffmpeg ficy \
+         fontconfig freetds glib gnet gnokii gnupg gsnmp \
+         gtk gphoto2 libgphoto2 gift giftcurs gift-ares gift-fasttrack \
+         gift-gnutella gift-openft gift-opennap hexcurse htop ice icecast \
+         id3lib irssi ivorbis-tools jabberd jamvm kissdx ldconfig \
+         libao libcdio libnsl libopensync libshout libsigc++ libtorrent \
+         libxml2 libuclibc++ lighttpd loudmouth madplay mc mcabber mediatomb \
+         memtester motion monotone mpd mrtg msynctool mtr \
+         netcat nfs-server nfs-utils noip ntop obexftp openser \
+         pango postfix psmisc python25 py-simpy qemu qemu-libc-i386 \
+         quagga quickie rc rrdcollect rrdtool rtorrent ruby \
+         sablevm samba scli sendmail ser sm snownews \
+         speex squeak streamripper swi-prolog tethereal transcode unixodbc \
+         ushare uclibc-opt vlc vorbis-tools vsftpd vte wget wget-ssl \
+         wxbase xauth xaw xchat xmu xt xterm \
+         zip zsh perl-business-isbn-data perl-business-isbn \
