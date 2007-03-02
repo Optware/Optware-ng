4ec7214cb717e7b3fa25fc68de0653094a9357ac
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2007-03-02 05:53:30 +0000

    promoted glib and friends for ts101

--- trunk/platforms/packages-ts101.mk
+++ trunk/platforms/packages-ts101.mk
@@ -6,21 +6,21 @@ SPECIFIC_PACKAGES = \
 
 BROKEN_PACKAGES = \
 	$(UCLIBC_BROKEN_PACKAGES) \
-         amule at atk avahi bitlbee bzflag \
-         buildroot chillispot ctrlproxy cups ecl \
+         amule at bzflag \
+         buildroot chillispot cups ecl \
          erl-escript erl-yaws fcgi fish ffmpeg ficy \
-         fontconfig gdb glib gnet gnokii gsnmp \
+         fontconfig gdb \
          gtk gphoto2 libgphoto2 gift giftcurs gift-ares gift-fasttrack \
          gift-gnutella gift-openft gift-opennap htop ice icecast \
-         id3lib irssi ivorbis-tools jabberd jamvm ldconfig \
+         id3lib ivorbis-tools jabberd jamvm ldconfig \
          libao libcdio libnsl libopensync libshout libsigc++ libtorrent \
-         libuclibc++ loudmouth madplay mc mcabber \
+         libuclibc++ loudmouth madplay \
          motion monotone mpd mrtg msynctool mtr \
          nfs-server nfs-utils ntop obexftp openser \
          pango postfix psmisc qemu qemu-libc-i386 \
          quagga quickie rc rrdcollect rrdtool rtorrent \
-         samba scli sendmail ser sm snownews \
-         speex squeak streamripper tethereal transcode \
+         samba sendmail ser sm snownews \
+         speex squeak streamripper transcode \
          ushare uclibc-opt vlc vorbis-tools vsftpd vte wget wget-ssl \
          xauth xaw xchat xmu xt xterm \
          zip zsh \
