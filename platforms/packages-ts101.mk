871b45810de12f2b13bab524155a3ec7e4002eaf
nail <nail@1f1dca69-5516-0410-b834-e485effd04c3>
2007-03-02 20:45:01 +0000

    Enable dcraw for ts101

--- platforms/packages-ts101.mk
+++ platforms/packages-ts101.mk
@@ -6,11 +6,12 @@ SPECIFIC_PACKAGES = \
 
 BROKEN_PACKAGES = \
 	$(UCLIBC_BROKEN_PACKAGES) \
-	amule at chillispot dcraw ecl erl-escript erl-yaws ffmpeg \
+	nft-utils ecl nfs-server erl-escript erl-yaws \
+	amule at chillispot ecl ffmpeg \
 	ficy freeradius gdb gift giftcurs gift-ares gift-fasttrack \
 	gift-gnutella gift-openft gift-opennap htop icecast id3lib \
 	ivorbis-tools libcdio libopensync libshout libsigc++ libtorrent \
 	mediatomb motion msynctool mysql-connector-odbc obexftp openser \
 	quagga rtorrent ruby sablevm squeak streamripper swi-prolog svn \
-	transcode ushare vlc vorbis-tools vsftpd wget wget-ssl zip \
+	transcode ushare vlc vorbis-tools vsftpd wget wget-ssl \
 	buildroot uclibc-opt ipkg-opt perl-business-isbn \
