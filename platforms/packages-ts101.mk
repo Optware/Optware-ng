2ea4af0bd4729f0b2c275b384cfe4354941b53cf
nail <nail@1f1dca69-5516-0410-b834-e485effd04c3>
2007-03-02 19:58:48 +0000

    clean up list of broken packages

--- platforms/packages-ts101.mk
+++ platforms/packages-ts101.mk
@@ -6,14 +6,11 @@ SPECIFIC_PACKAGES = \
 
 BROKEN_PACKAGES = \
 	$(UCLIBC_BROKEN_PACKAGES) \
-	amule at chillispot \
-	dcraw ecl erl-escript erl-yaws ffmpeg ficy freeradius \
-	gdb gift giftcurs gift-ares gift-fasttrack gift-gnutella \
-	gift-openft gift-opennap htop icecast id3lib \
+	amule at chillispot dcraw ecl erl-escript erl-yaws ffmpeg \
+	ficy freeradius gdb gift giftcurs gift-ares gift-fasttrack \
+	gift-gnutella gift-openft gift-opennap htop icecast id3lib \
 	ivorbis-tools libcdio libopensync libshout libsigc++ libtorrent \
-	mediatomb motion msynctool \
-	mysql-connector-odbc obexftp openser \
-	quagga rtorrent ruby sablevm \
-	squeak streamripper swi-prolog svn transcode ushare vlc \
-	vorbis-tools vsftpd wget wget-ssl zip buildroot uclibc-opt \
-	ipkg-opt perl-business-isbn \
+	mediatomb motion msynctool mysql-connector-odbc obexftp openser \
+	quagga rtorrent ruby sablevm squeak streamripper swi-prolog svn \
+	transcode ushare vlc vorbis-tools vsftpd wget wget-ssl zip \
+	buildroot uclibc-opt ipkg-opt perl-business-isbn \
