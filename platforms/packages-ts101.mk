ac77fe33d598131334f258d69f1ece6062930b33
Brian Zhou <bzhou@users.noreply.github.com>
2007-03-02 06:38:55 +0000

    ts101: promoted libnsl postfix rc snownews zip zsh

--- platforms/packages-ts101.mk
+++ platforms/packages-ts101.mk
@@ -13,14 +13,13 @@ BROKEN_PACKAGES = \
          gtk gphoto2 libgphoto2 gift giftcurs gift-ares gift-fasttrack \
          gift-gnutella gift-openft gift-opennap htop ice icecast \
          id3lib ivorbis-tools jabberd jamvm ldconfig \
-         libao libcdio libnsl libopensync libshout libsigc++ libtorrent \
+         libao libcdio libopensync libshout libsigc++ libtorrent \
          libuclibc++ loudmouth madplay \
          motion monotone mpd mrtg msynctool mtr \
          nfs-server nfs-utils ntop obexftp openser \
-         pango postfix psmisc qemu qemu-libc-i386 \
-         quagga quickie rc rrdcollect rrdtool rtorrent \
-         samba sendmail ser sm snownews \
+         pango psmisc qemu qemu-libc-i386 \
+         quagga quickie rrdcollect rrdtool rtorrent \
+         samba sendmail ser sm \
          squeak streamripper transcode \
          ushare uclibc-opt vlc vorbis-tools vsftpd vte wget wget-ssl \
          xauth xaw xchat xmu xt xterm \
-         zip zsh \
