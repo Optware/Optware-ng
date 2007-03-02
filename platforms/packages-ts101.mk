93e0320baf6e4fdce3fdfe3cc02e47e3dacd8a44
nail <nail@1f1dca69-5516-0410-b834-e485effd04c3>
2007-03-02 19:52:26 +0000

    Enable bsdmainutils, cyrus-imapd, hexcurse, lighttpd, memtester, mrtg, netcat, ntop, py-simpy, rc, rrdcollect, rrdtool, sendmail, ser for ts101

--- platforms/packages-ts101.mk
+++ platforms/packages-ts101.mk
@@ -6,14 +6,14 @@ SPECIFIC_PACKAGES = \
 
 BROKEN_PACKAGES = \
 	$(UCLIBC_BROKEN_PACKAGES) \
-	amule at bsdmainutils chillispot cyrus-imapd \
+	amule at chillispot \
 	dcraw ecl erl-escript erl-yaws ffmpeg ficy freeradius \
 	gdb gift giftcurs gift-ares gift-fasttrack gift-gnutella \
-	gift-openft gift-opennap hexcurse htop icecast id3lib \
+	gift-openft gift-opennap htop icecast id3lib \
 	ivorbis-tools libcdio libopensync libshout libsigc++ libtorrent \
-	lighttpd mediatomb memtester motion mrtg msynctool \
-	mysql-connector-odbc netcat ntop obexftp openser py-simpy \
-	quagga rc rrdcollect rrdtool rtorrent ruby sablevm sendmail \
-	ser squeak streamripper swi-prolog svn transcode ushare vlc \
+	mediatomb motion msynctool \
+	mysql-connector-odbc obexftp openser \
+	quagga rtorrent ruby sablevm \
+	squeak streamripper swi-prolog svn transcode ushare vlc \
 	vorbis-tools vsftpd wget wget-ssl zip buildroot uclibc-opt \
 	ipkg-opt perl-business-isbn \
