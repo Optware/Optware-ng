09fd307ce5202f321b94fe82a1c4c6f9cbbeab9a
nail <nail@1f1dca69-5516-0410-b834-e485effd04c3>
2007-03-02 20:47:13 +0000

    Cleanup ts101-packages again

--- trunk/platforms/packages-ts101.mk
+++ trunk/platforms/packages-ts101.mk
@@ -6,12 +6,11 @@ SPECIFIC_PACKAGES = \
 
 BROKEN_PACKAGES = \
 	$(UCLIBC_BROKEN_PACKAGES) \
-	nft-utils ecl nfs-server erl-escript erl-yaws \
-	amule at chillispot ecl ffmpeg \
+	amule at chillispot ecl erl-escript erl-yaws ffmpeg \
 	ficy freeradius gdb gift giftcurs gift-ares gift-fasttrack \
 	gift-gnutella gift-openft gift-opennap htop icecast id3lib \
 	ivorbis-tools libcdio libopensync libshout libsigc++ libtorrent \
-	mediatomb motion msynctool mysql-connector-odbc obexftp openser \
-	quagga rtorrent ruby sablevm squeak streamripper svn \
+	mediatomb motion msynctool mysql-connector-odbc nfs-server nfs-utils \
+	obexftp openser quagga rtorrent ruby sablevm squeak streamripper svn \
 	transcode ushare vlc vorbis-tools vsftpd wget wget-ssl \
 	buildroot uclibc-opt ipkg-opt perl-business-isbn \
