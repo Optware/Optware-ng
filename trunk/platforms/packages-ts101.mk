6ccfc0f1cf009f96c9b360b7d14045f31c865dd2
nail <nail@1f1dca69-5516-0410-b834-e485effd04c3>
2007-03-02 18:56:29 +0000

    Update broken packages list for ts101

--- trunk/platforms/packages-ts101.mk
+++ trunk/platforms/packages-ts101.mk
@@ -6,20 +6,14 @@ SPECIFIC_PACKAGES = \
 
 BROKEN_PACKAGES = \
 	$(UCLIBC_BROKEN_PACKAGES) \
-         amule at bzflag \
-         buildroot chillispot ecl \
-         erl-escript erl-yaws fcgi fish ffmpeg ficy \
-         fontconfig gdb \
-         gtk gphoto2 libgphoto2 gift giftcurs gift-ares gift-fasttrack \
-         gift-gnutella gift-openft gift-opennap htop ice icecast \
-         id3lib ivorbis-tools jabberd jamvm ldconfig \
-         libao libcdio libopensync libshout libsigc++ libtorrent \
-         libuclibc++ loudmouth madplay \
-         motion monotone mpd mrtg msynctool mtr \
-         nfs-server nfs-utils ntop obexftp openser \
-         pango psmisc qemu qemu-libc-i386 \
-         quagga quickie rrdcollect rrdtool rtorrent \
-         sendmail ser sm \
-         squeak streamripper transcode \
-         ushare uclibc-opt vlc vorbis-tools vsftpd vte wget wget-ssl \
-         xauth xaw xchat xmu xt xterm \
+	amule at bsdmainutils chillispot cyrus-imapd \
+	dcraw ecl erl-escript erl-yaws ffmpeg ficy freeradius \
+	gdb gift giftcurs gift-ares gift-fasttrack gift-gnutella \
+	gift-openft gift-opennap hexcurse htop icecast id3lib \
+	ivorbis-tools libcdio libopensync libshout libsigc++ libtorrent \
+	lighttpd mediatomb memtester motion mrtg msynctool \
+	mysql-connector-odbc netcat ntop obexftp openser py-simpy \
+	quagga rc rrdcollect rrdtool rtorrent ruby sablevm sendmail \
+	ser squeak streamripper swi-prolog svn transcode ushare vlc \
+	vorbis-tools vsftpd wget wget-ssl zip buildroot uclibc-opt \
+	ipkg-opt perl-business-isbn \
