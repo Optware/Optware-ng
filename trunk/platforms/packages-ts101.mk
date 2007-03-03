1b41f49dbc39a4da92c61f1b7bde1e2f50b860e7
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2007-03-03 05:32:28 +0000

    promoted libcdio for ts101

--- trunk/platforms/packages-ts101.mk
+++ trunk/platforms/packages-ts101.mk
@@ -9,7 +9,7 @@ BROKEN_PACKAGES = \
 	amule chillispot ecl erl-escript erl-yaws ffmpeg \
 	ficy gdb gift giftcurs gift-ares gift-fasttrack \
 	gift-gnutella gift-openft gift-opennap htop icecast \
-	ivorbis-tools libcdio libopensync libshout \
+	ivorbis-tools libopensync libshout \
 	motion msynctool nfs-server nfs-utils \
 	obexftp quagga squeak streamripper \
 	transcode ushare vlc vorbis-tools vsftpd wget wget-ssl \
