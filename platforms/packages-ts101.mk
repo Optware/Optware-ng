50391b73a9487823af40ab3580f12affa58a23e0
nail <nail@1f1dca69-5516-0410-b834-e485effd04c3>
2007-03-03 13:06:22 +0000

    Clean up list, promote vlc

--- platforms/packages-ts101.mk
+++ platforms/packages-ts101.mk
@@ -6,10 +6,8 @@ SPECIFIC_PACKAGES = \
 
 BROKEN_PACKAGES = \
 	$(UCLIBC_BROKEN_PACKAGES) \
-	amule chillispot ecl erl-escript erl-yaws \
-	ficy gdb gift giftcurs gift-ares gift-fasttrack \
-	gift-gnutella gift-openft gift-opennap htop \
-	libopensync motion msynctool nfs-server nfs-utils \
-	obexftp quagga squeak \
-	transcode ushare vlc vsftpd wget wget-ssl \
-	buildroot uclibc-opt ipkg-opt \
+	amule chillispot ecl erl-escript erl-yaws ficy gdb gift \
+	giftcurs gift-ares gift-fasttrack gift-gnutella gift-openft \
+	gift-opennap htop libopensync motion msynctool nfs-server \
+	nfs-utils obexftp quagga squeak transcode ushare vsftpd \
+	wget wget-ssl buildroot uclibc-opt ipkg-opt \
