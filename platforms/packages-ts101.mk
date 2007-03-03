7f519e5c58f0f951074582b22ea4a73b0c709a6f
nail <nail@1f1dca69-5516-0410-b834-e485effd04c3>
2007-03-03 00:52:56 +0000

    Promote freeradius for ts101

--- platforms/packages-ts101.mk
+++ platforms/packages-ts101.mk
@@ -7,7 +7,7 @@ SPECIFIC_PACKAGES = \
 BROKEN_PACKAGES = \
 	$(UCLIBC_BROKEN_PACKAGES) \
 	amule at chillispot ecl erl-escript erl-yaws ffmpeg \
-	ficy freeradius gdb gift giftcurs gift-ares gift-fasttrack \
+	ficy gdb gift giftcurs gift-ares gift-fasttrack \
 	gift-gnutella gift-openft gift-opennap htop icecast id3lib \
 	ivorbis-tools libcdio libopensync libshout libsigc++ libtorrent \
 	motion msynctool nfs-server nfs-utils \
