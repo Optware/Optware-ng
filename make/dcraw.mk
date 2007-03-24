ae0d6ca88359d7e46c10d1c04f2e58d06dac990e
Brian Zhou <bzhou@users.noreply.github.com>
2007-03-24 01:13:55 +0000

    dcraw: 1.368 -> 1.376

--- make/dcraw.mk
+++ make/dcraw.mk
@@ -22,7 +22,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 DCRAW_SITE=http://www.cybercom.net/~dcoffin/dcraw
-DCRAW_VERSION=1.368
+DCRAW_VERSION=1.376
 DCRAW_SOURCE=dcraw.c,v
 DCRAW_DIR=dcraw-$(DCRAW_VERSION)
 DCRAW_UNZIP=zcat
