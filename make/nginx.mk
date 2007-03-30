1bdaa7205e2e1359495e90a518bdfd0d690b4528
Brian Zhou <bzhou@users.noreply.github.com>
2007-03-30 05:58:50 +0000

    nginx: 0.5.15 -> 0.5.16

--- make/nginx.mk
+++ make/nginx.mk
@@ -21,7 +21,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 NGINX_SITE=http://sysoev.ru/nginx
-NGINX_VERSION=0.5.15
+NGINX_VERSION=0.5.16
 NGINX_SOURCE=nginx-$(NGINX_VERSION).tar.gz
 NGINX_DIR=nginx-$(NGINX_VERSION)
 NGINX_UNZIP=zcat
