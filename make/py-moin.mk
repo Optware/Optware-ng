2c47f626c7348eb3429a3c9a09d9e6061bce83cb
Brian Zhou <bzhou@users.noreply.github.com>
2007-05-13 23:57:14 +0000

    py-moin: 1.5.7 -> 1.5.8

--- make/py-moin.mk
+++ make/py-moin.mk
@@ -22,7 +22,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 PY-MOIN_SITE=http://$(SOURCEFORGE_MIRROR)/sourceforge/moin
-PY-MOIN_VERSION=1.5.7
+PY-MOIN_VERSION=1.5.8
 PY-MOIN_SOURCE=moin-$(PY-MOIN_VERSION).tar.gz
 PY-MOIN_DIR=moin-$(PY-MOIN_VERSION)
 PY-MOIN_UNZIP=zcat
