1133bfd8719a54c066787b12b0b85a1c8a43ee20
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2007-05-15 03:18:55 +0000

    nginx: 0.5.19 -> 0.5.20

--- trunk/make/nginx.mk
+++ trunk/make/nginx.mk
@@ -21,7 +21,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 NGINX_SITE=http://sysoev.ru/nginx
-NGINX_VERSION=0.5.19
+NGINX_VERSION=0.5.20
 NGINX_SOURCE=nginx-$(NGINX_VERSION).tar.gz
 NGINX_DIR=nginx-$(NGINX_VERSION)
 NGINX_UNZIP=zcat
