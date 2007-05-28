3fa46b29c1143480c1fd8d8a4f1ebd8ddacc1778
Brian Zhou <bzhou@users.noreply.github.com>
2007-05-28 16:25:51 +0000

    packages-gumstix1151: initial SPECIFIC & BROKEN

---
+++ platforms/packages-gumstix1151.mk
@@ -0,0 +1,18 @@
+SPECIFIC_PACKAGES = \
+	$(UCLIBC_SPECIFIC_PACKAGES) \
+
+# openldap: daemon.o: In function `slapd_daemon_task':
+# 	servers/slapd/daemon.c:1973: undefined reference to `in6addr_any'
+BROKEN_PACKAGES = \
+	$(UCLIBC_BROKEN_PACKAGES) \
+	dansguardian \
+	gambit-c \
+	htop \
+	libdvb \
+	ltrace \
+	nget \
+	oleo \
+	openldap \
+	par2cmdline \
+	zsh \
+
