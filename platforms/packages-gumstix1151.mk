0f7e27503ad5a62b21bf4ad0aa6ea322ceaa8ef5
Brian Zhou <bzhou@users.noreply.github.com>
2007-05-29 02:26:53 +0000

    packages-gumstix1151: added ipkg-opt and PERL_PACKAGES to SPECIFIC_PACKAGES

--- platforms/packages-gumstix1151.mk
+++ platforms/packages-gumstix1151.mk
@@ -1,5 +1,7 @@
 SPECIFIC_PACKAGES = \
+	ipkg-opt \
 	libiconv \
+	$(PERL_PACKAGES) \
 	$(UCLIBC_SPECIFIC_PACKAGES) \
 
 # openldap: daemon.o: In function `slapd_daemon_task':
@@ -7,7 +9,6 @@ SPECIFIC_PACKAGES = \
 BROKEN_PACKAGES = \
 	$(UCLIBC_BROKEN_PACKAGES) \
 	dansguardian \
-	gambit-c \
 	htop \
 	libdvb \
 	ltrace \
@@ -17,3 +18,13 @@ BROKEN_PACKAGES = \
 	par2cmdline \
 	zsh \
 
+# BROKEN_PACKAGES = \
+	amule asterisk asterisk14-chan-capi bluez-hcidump cherokee chillispot \
+	cyrus-imapd dovecot dump esound fcgi fish ficy \
+	gdb git-core gnupg gnuplot gtk htop ice \
+	inetutils iptables ircd-hybrid irssi ivorbis-tools jamvm kissdx \
+	launchtool ldconfig libao libopensync lsof madplay mc \
+	mdadm monotone mpd msynctool mtr netatalk nfs-server \
+	nfs-utils obexftp openldap pango phoneme-advanced portmap puppy \
+	py-duplicity qemu quagga quickie sendmail sm strace \
+	swi-prolog
