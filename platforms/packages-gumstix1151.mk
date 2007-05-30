dd060e2cda37e2cd2b5664a74019d5fb1b2bb2e5
Brian Zhou <bzhou@users.noreply.github.com>
2007-05-30 02:32:08 +0000

    gumstix: latest BROKEN_PACKAGES list

--- platforms/packages-gumstix1151.mk
+++ platforms/packages-gumstix1151.mk
@@ -6,22 +6,12 @@ SPECIFIC_PACKAGES = \
 
 BROKEN_PACKAGES = \
 	$(UCLIBC_BROKEN_PACKAGES) \
-	dansguardian \
-	htop \
-	libdvb \
-	ltrace \
-	nget \
-	oleo \
-	par2cmdline \
-	zsh \
-
-# BROKEN_PACKAGES = \
-	amule asterisk asterisk14-chan-capi bluez-hcidump cherokee chillispot \
-	cyrus-imapd dovecot dump esound fcgi fish ficy \
-	gdb git-core gnupg gnuplot gtk htop ice \
-	inetutils iptables ircd-hybrid irssi ivorbis-tools jamvm kissdx \
-	launchtool ldconfig libao libopensync lsof madplay mc \
-	mdadm monotone mpd msynctool mtr netatalk nfs-server \
-	nfs-utils obexftp openldap pango phoneme-advanced portmap puppy \
-	py-duplicity qemu quagga quickie sendmail sm strace \
-	swi-prolog
+	amule asterisk asterisk14-chan-capi \
+	bluez-hcidump chillispot \
+	dovecot dump ficy gdb \
+	gnuplot htop inetutils \
+	ircd-hybrid kissdx \
+	libopensync lsof mdadm \
+	msynctool netatalk obexftp \
+	portmap puppy quagga sendmail \
+	strace unfs3 usbutils xinetd
