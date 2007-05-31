c4e6de0b520685cd22c8040a7271f814170ef8ae
Brian Zhou <bzhou@users.noreply.github.com>
2007-05-31 16:20:53 +0000

    py-bittorrent: 5.0.4 -> 5.0.7

--- make/py-bittorrent.mk
+++ make/py-bittorrent.mk
@@ -27,7 +27,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 PY-BITTORRENT_SITE=http://download.bittorrent.com/dl
-PY-BITTORRENT_VERSION=5.0.4
+PY-BITTORRENT_VERSION=5.0.7
 PY-BITTORRENT_SOURCE=BitTorrent-$(PY-BITTORRENT_VERSION).tar.gz
 PY-BITTORRENT_DIR=BitTorrent-$(PY-BITTORRENT_VERSION)
 PY-BITTORRENT_UNZIP=zcat
