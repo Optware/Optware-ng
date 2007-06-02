794ac014a6a66e8d498a30b1b8c5d269245a075f
Brian Zhou <bzhou@users.noreply.github.com>
2007-06-02 15:57:25 +0000

    libidn: 0.6.12 -> 0.6.14

--- make/libidn.mk
+++ make/libidn.mk
@@ -27,7 +27,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 LIBIDN_SITE=ftp://alpha.gnu.org/pub/gnu/libidn
-LIBIDN_VERSION=0.6.12
+LIBIDN_VERSION=0.6.14
 LIBIDN_SOURCE=libidn-$(LIBIDN_VERSION).tar.gz
 LIBIDN_DIR=libidn-$(LIBIDN_VERSION)
 LIBIDN_UNZIP=zcat
