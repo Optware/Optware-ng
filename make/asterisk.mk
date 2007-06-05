a37a46bb845fd25ca7451b58134483b556d605d1
osas <osas@1f1dca69-5516-0410-b834-e485effd04c3>
2007-06-05 01:31:04 +0000

    asterisk: 1.2.17 -> 1.2.18

--- make/asterisk.mk
+++ make/asterisk.mk
@@ -27,7 +27,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 ASTERISK_SITE=http://ftp.digium.com/pub/asterisk/releases/
-ASTERISK_VERSION=1.2.17
+ASTERISK_VERSION=1.2.18
 ASTERISK_SOURCE=asterisk-$(ASTERISK_VERSION).tar.gz
 ASTERISK_DIR=asterisk-$(ASTERISK_VERSION)
 ASTERISK_UNZIP=zcat
