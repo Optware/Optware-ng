e99ebaf5e1aef37e10ae853ea8df06fbbf500387
Brian Zhou <bzhou@users.noreply.github.com>
2007-06-11 14:41:18 +0000

    zile: 2.2.33 -> 2.2.34

--- make/zile.mk
+++ make/zile.mk
@@ -21,7 +21,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 ZILE_SITE=http://$(SOURCEFORGE_MIRROR)/sourceforge/zile
-ZILE_VERSION=2.2.33
+ZILE_VERSION=2.2.34
 ZILE_SOURCE=zile-$(ZILE_VERSION).tar.gz
 ZILE_DIR=zile-$(ZILE_VERSION)
 ZILE_UNZIP=zcat
