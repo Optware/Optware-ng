5020831570afe7a2eae10d7b8523ac93e9c91b3f
oleo <oleo@1f1dca69-5516-0410-b834-e485effd04c3>
2007-06-19 19:58:09 +0000

    optware: add brcm24 target - OpenWRT Kamikaze based toolchain for Broadcom mipsel wireless routers

---
+++ platforms/packages-brcm24.mk
@@ -0,0 +1,26 @@
+SPECIFIC_PACKAGES = \
+	$(UCLIBC_SPECIFIC_PACKAGES) \
+
+BROKEN_PACKAGES = \
+	$(UCLIBC_BROKEN_PACKAGES) \
+	uclibc-opt buildroot libuclibc++ \
+	appweb asterisk14-chan-capi atk avahi bitlbee bogofilter bsdmainutils coreutils \
+	eaccelerator ecl erlang erl-yaws esound ffmpeg ficy finch \
+	fixesext gambit-c git glib gnet gnokii gnugo gnuplot gsnmp gphoto2 libgphoto2 gift giftcurs\
+	gift-ares gift-fasttrack gift-gnutella gift-openft gift-opennap hpijs icecast id3lib irssi \
+	ivorbis-tools jed joe kissdx libao libcdio libextractor libmtp libopensync libshout libsigc++ \
+	libtorrent loudmouth ltrace madplay mc mcabber modutils most motion mpc mpd msynctool multitail \
+	mutt ncmpc ntop phoneme-advanced php php-apache php-fcgi php-thttpd player psmisc py-duplicity \
+	py-lxml qemacs quagga renderext rrdcollect rrdtool rtorrent samba scli screen sdl ser slang \
+	snownews speex squeak streamripper tethereal tcsh vlc vorbis-tools weechat x11 xdpyinfo xext xpm xtst \
+	dansguardian \
+	gambit-c \
+	htop \
+	libdvb \
+	ltrace \
+	nget \
+	oleo \
+	par2cmdline \
+	player \
+	zsh \
+
