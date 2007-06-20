dd504f17e8adbec5f967a5d6ff5da7e45ddea9b5
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2007-06-20 05:23:16 +0000

    clean-workdir: improved to clean workdir of non-staging packages

--- trunk/scripts/clean-workdir.sh
+++ trunk/scripts/clean-workdir.sh
@@ -2,7 +2,8 @@
 
 # This script will
 #	rm -rf builds/$p/* builds/$p_*.ipk
-# if both builds/$p/.staged and builds/$p_*.ipk exist
+# if P_IPK built, and
+# either $p-stage is not used, or $p staged already
 
 if test "x$1" = "x-d"
 then shift; dry_run=1
@@ -28,14 +29,21 @@ do
 	ipk=`make query-${P}_IPK`
 	ipk_dir=`make query-${P}_IPK_DIR`
 	build_dir=`make query-${P}_BUILD_DIR`
-        if test -d "$build_dir" && test -f "$build_dir/.staged" && test -f "$ipk"; then
+        staging_count=`grep -l ${p}-stage make/*.mk | wc -l`
+        todo=""
+        if test -d "$build_dir" -a -f "$ipk"; then
+            if test $staging_count -le 1 -o -f "$build_dir/.staged"; then
         	if ! ls $build_dir/* > /dev/null 2>&1; then
-                	echo " already clean"
+                	todo="already clean"
         	elif test -n "$dry_run"; then
-                	echo " dry run"
+                	todo="dry run"
                 else
-                	echo " clean"; echo $build_dir/* $ipk_dir | xargs rm -rf
+                	todo="clean"
         	fi
-        else echo
+            fi
+        fi
+        echo " $todo"
+        if test "$todo" = "clean"; then
+               echo $build_dir/* $ipk_dir | xargs rm -rf
         fi
 done
