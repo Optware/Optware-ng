c00229c61e08dd709ac1056f69249b81312c9009
oleo <oleo@1f1dca69-5516-0410-b834-e485effd04c3>
2007-06-20 14:23:57 +0000

    optware: brcm24 fixes for non implemented functions

--- trunk/platforms/packages-brcm24.mk
+++ trunk/platforms/packages-brcm24.mk
@@ -1,17 +1,19 @@
 SPECIFIC_PACKAGES = \
 	$(UCLIBC_SPECIFIC_PACKAGES) \
+	uclibcnotimpl \
 
 BROKEN_PACKAGES = \
 	$(UCLIBC_BROKEN_PACKAGES) \
-	uclibc-opt buildroot libuclibc++ \
-	appweb asterisk14-chan-capi atk avahi bitlbee bogofilter bsdmainutils coreutils \
+	bluez-utils ctrlproxy cups fuppes mediatomb netatalk newt openobex \
+	obexftp ffmpeg transcode uclibc-opt buildroot libuclibc++ \
+	appweb asterisk14-chan-capi atk avahi bitlbee bogofilter bsdmainutils \
 	eaccelerator ecl erlang erl-yaws esound ffmpeg ficy finch \
-	fixesext gambit-c git glib gnet gnokii gnugo gnuplot gsnmp gphoto2 libgphoto2 gift giftcurs\
-	gift-ares gift-fasttrack gift-gnutella gift-openft gift-opennap hpijs icecast id3lib irssi \
+	fixesext gambit-c git glib gnet gnokii gnugo gsnmp gphoto2 libgphoto2 giftcurs\
+	hpijs icecast id3lib irssi \
 	ivorbis-tools jed joe kissdx libao libcdio libextractor libmtp libopensync libshout libsigc++ \
-	libtorrent loudmouth ltrace madplay mc mcabber modutils most motion mpc mpd msynctool multitail \
+	loudmouth ltrace madplay mc mcabber modutils most motion mpc mpd msynctool multitail \
 	mutt ncmpc ntop phoneme-advanced php php-apache php-fcgi php-thttpd player psmisc py-duplicity \
-	py-lxml qemacs quagga renderext rrdcollect rrdtool rtorrent samba scli screen sdl ser slang \
+	py-lxml qemacs quagga renderext samba scli sdl ser slang \
 	snownews speex squeak streamripper tethereal tcsh vlc vorbis-tools weechat x11 xdpyinfo xext xpm xtst \
 	dansguardian \
 	gambit-c \
