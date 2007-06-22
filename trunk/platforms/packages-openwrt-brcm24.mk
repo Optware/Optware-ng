9587feb04658dbbdcca52e1156241d2a38f20e35
oleo <oleo@1f1dca69-5516-0410-b834-e485effd04c3>
2007-06-22 10:00:47 +0000

    openwrt-brcm24: packages update

--- trunk/platforms/packages-openwrt-brcm24.mk
+++ trunk/platforms/packages-openwrt-brcm24.mk
@@ -1,29 +1,23 @@
 SPECIFIC_PACKAGES = \
+	libiconv \
 	$(UCLIBC_SPECIFIC_PACKAGES) \
 	uclibcnotimpl libuclibc++ \
 
 BROKEN_PACKAGES = \
 	$(UCLIBC_BROKEN_PACKAGES) \
-	amule apache openldap aspell asterisk bash \
-	bluez-utils ctrlproxy cups fuppes mediatomb netatalk newt openobex \
-	obexftp ffmpeg transcode uclibc-opt buildroot  \
-	appweb asterisk14-chan-capi atk avahi bitlbee bogofilter bsdmainutils \
-	eaccelerator ecl erlang erl-yaws esound ffmpeg ficy finch \
-	fixesext gambit-c git glib gnet gnokii gnugo gsnmp gphoto2 libgphoto2 giftcurs\
-	hpijs icecast id3lib irssi \
-	ivorbis-tools jed joe kissdx libao libcdio libextractor libmtp libopensync libshout libsigc++ \
-	loudmouth ltrace madplay mc mcabber modutils most motion mpc mpd msynctool multitail \
-	mutt ncmpc ntop phoneme-advanced php php-apache php-fcgi php-thttpd player psmisc py-duplicity \
-	py-lxml qemacs quagga renderext samba scli sdl ser slang \
-	snownews speex squeak streamripper tethereal tcsh vlc vorbis-tools weechat x11 xdpyinfo xext xpm xtst \
-	dansguardian \
-	gambit-c \
-	htop \
-	libdvb \
-	ltrace \
-	nget \
-	oleo \
-	par2cmdline \
-	player \
-	zsh \
+	buildroot uclibc-opt \
+	appweb aspell asterisk14-chan-capi bogofilter \
+	bsdmainutils bluez-utils cups eaccelerator ecl erlang \
+	erl-yaws fixesext fuppes gambit-c gcal git \
+	gnugo gsnmp gphoto2 libgphoto2 joe libcdio \
+	libdvb libextractor libmtp libopensync loudmouth ltrace \
+	mediatomb mpc mpd msynctool multitail netatalk newt nget \
+	openobex obexftp phoneme-advanced php php-apache php-fcgi \
+	php-thttpd player psmisc py-duplicity py-lxml py-psycopg \
+	py-psycopg2 py-pygresql py-pudge py-pylons py-pyro \
+	py-quixote py-rdiff-backup py-reportlab py-routes \
+	py-roundup py-ruledispatch py-scgi py-selector py-serial \
+	py-silvercity py-simplejson quagga renderext rhtvision ruby \
+	rubygems samba scli sdl ser slsc snownews squeak \
+	tcsh tethereal vlc weechat x11 xdpyinfo xext xpm xtst zile
 
