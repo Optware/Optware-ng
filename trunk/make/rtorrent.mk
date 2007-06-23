8528097a1b59c041a1b949031f890684c15ce7f0
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2007-06-23 00:29:28 +0000

    rtorrent: adding unistd.h for mss

--- trunk/make/rtorrent.mk
+++ trunk/make/rtorrent.mk
@@ -39,7 +39,7 @@ RTORRENT_CONFFILES=/opt/etc/rtorrent.conf
 # RTORRENT_PATCHES should list any patches, in the the order in
 # which they should be applied to the source code.
 #
-#RTORRENT_PATCHES=
+RTORRENT_PATCHES=$(RTORRENT_SOURCE_DIR)/symlink_unlink.patch
 
 #
 # If the compilation of the package requires additional
