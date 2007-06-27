43ba3a17797661b681b72ba7f74fd58671b3359b
Brian Zhou <bzhou@users.noreply.github.com>
2007-06-27 03:54:48 +0000

    packages-openwrt-brcm24: update from official build machine, feed ready

--- platforms/packages-openwrt-brcm24.mk
+++ platforms/packages-openwrt-brcm24.mk
@@ -7,17 +7,15 @@ BROKEN_PACKAGES = \
 	$(UCLIBC_BROKEN_PACKAGES) \
 	buildroot uclibc-opt \
 	appweb aspell asterisk14-chan-capi bogofilter \
-	bsdmainutils bluez-utils cups eaccelerator ecl erlang \
-	erl-yaws fixesext fuppes gambit-c gcal git \
+	bluez-utils cups eaccelerator \
+	ecl erlang erl-yaws \
+	fixesext fuppes gambit-c gdb \
 	gnugo gsnmp gphoto2 libgphoto2 joe libcdio \
-	libdvb libextractor libmtp libopensync loudmouth ltrace \
-	mediatomb mpc mpd msynctool multitail netatalk newt nget \
+	libdvb libextractor libmtp libnsl libopensync loudmouth ltrace \
+	mediatomb msynctool multitail netatalk nget \
 	openobex obexftp phoneme-advanced php php-apache php-fcgi \
-	php-thttpd player psmisc py-duplicity py-lxml py-psycopg \
-	py-psycopg2 py-pygresql py-pudge py-pylons py-pyro \
-	py-quixote py-rdiff-backup py-reportlab py-routes \
-	py-roundup py-ruledispatch py-scgi py-selector py-serial \
-	py-silvercity py-simplejson quagga renderext rhtvision ruby \
-	rubygems samba scli sdl ser slsc snownews squeak \
-	tcsh tethereal vlc weechat x11 xdpyinfo xext xpm xtst zile
-
+	php-thttpd player psmisc \
+	quagga \
+	recordext renderext \
+	rhtvision samba scli sdl ser squeak \
+	tcsh tethereal vlc x11 xdpyinfo xext xpm xtst zile
