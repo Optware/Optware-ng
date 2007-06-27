8f83602c1760cad7acf3056f435916293a10a349
oleo <oleo@1f1dca69-5516-0410-b834-e485effd04c3>
2007-06-27 12:02:08 +0000

    php: 5.2.2->5.2.3 - uclibc and arm patch change

--- platforms/packages-openwrt-brcm24.mk
+++ platforms/packages-openwrt-brcm24.mk
@@ -6,15 +6,15 @@ SPECIFIC_PACKAGES = \
 BROKEN_PACKAGES = \
 	$(UCLIBC_BROKEN_PACKAGES) \
 	buildroot uclibc-opt \
-	appweb aspell asterisk14-chan-capi bogofilter \
-	bluez-utils eaccelerator \
+	aspell asterisk14-chan-capi bogofilter \
+	bluez-utils \
 	ecl erlang erl-yaws \
 	fixesext fuppes gambit-c gdb \
 	gnugo gsnmp gphoto2 libgphoto2 joe libcdio \
 	libdvb libextractor libmtp libnsl libopensync loudmouth ltrace \
 	msynctool multitail netatalk nget \
-	openobex obexftp phoneme-advanced php php-apache php-fcgi \
-	php-thttpd player psmisc \
+	openobex obexftp phoneme-advanced \
+	php-fcgi player psmisc \
 	quagga \
 	recordext renderext \
 	rhtvision scli sdl ser slsc squeak \
