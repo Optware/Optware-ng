e48a5901dc47a135c21332ee136888530ac12596
Brian Zhou <bzhou@users.noreply.github.com>
2007-07-21 19:06:33 +0000

    sudo: 1.6.8p12 -> 1.6.9

--- make/sudo.mk
+++ make/sudo.mk
@@ -3,9 +3,9 @@
 #
 # $Id$
 
-SUDO_SITE=http://probsd.org/sudoftp
+SUDO_SITE=http://www.gratisoft.us/sudo/dist
 #SUDO_SITE=http://sources.nslu2-linux.org/sources/
-SUDO_VERSION=1.6.8p12
+SUDO_VERSION=1.6.9
 SUDO_SOURCE=sudo-$(SUDO_VERSION).tar.gz
 SUDO_DIR=sudo-$(SUDO_VERSION)
 SUDO_UNZIP=zcat
@@ -17,14 +17,17 @@ SUDO_DEPENDS=
 SUDO_SUGGESTS=
 SUDO_CONFLICTS=
 
-SUDO_IPK_VERSION=7
+SUDO_IPK_VERSION=1
 
 SUDO_CONFFILES=/opt/etc/sudoers
 
-ifeq ($(OPTWARE_TARGET),wl500g)
-SUDO_PATCHES=$(SUDO_SOURCE_DIR)/configure.patch $(SUDO_SOURCE_DIR)/configure_wl500g.patch
-else
-SUDO_PATCHES=$(SUDO_SOURCE_DIR)/configure.patch
+#SUDO_PATCHES=
+
+ifneq ($(TARGET_CC), $(HOSTCC))
+SUDO_CONFIGURE_ENV=sudo_cv_uid_t_len=10
+endif
+ifeq ($(OPTWARE_TARGET), wl500g)
+SUDO_CONFIGURE_ENV+=ac_cv_header_err_h=no
 endif
 
 SUDO_BUILD_DIR:=$(BUILD_DIR)/sudo
@@ -34,34 +37,43 @@ SUDO_IPK=$(BUILD_DIR)/sudo_$(SUDO_VERSION)-$(SUDO_IPK_VERSION)_$(TARGET_ARCH).ip
 
 
 $(DL_DIR)/$(SUDO_SOURCE):
-	cd $(DL_DIR) && $(WGET) $(SUDO_SITE)/$(SUDO_SOURCE)
+	$(WGET) -P $(DL_DIR) $(SUDO_SITE)/$(SUDO_SOURCE) || \
+	$(WGET) -P $(DL_DIR) $(SOURCES_NLO_SITE)/$(SUDO_SOURCE)
 
 sudo-source: $(DL_DIR)/$(SUDO_SOURCE) $(SUDO_PATCHES)
 
-$(SUDO_BUILD_DIR)/.configured: $(DL_DIR)/$(SUDO_SOURCE) $(SUDO_PATCHES)
+
+$(SUDO_BUILD_DIR)/.configured: $(DL_DIR)/$(SUDO_SOURCE) $(SUDO_PATCHES) make/sudo.mk
 	rm -rf $(BUILD_DIR)/$(SUDO_DIR) $(SUDO_BUILD_DIR)
 	$(SUDO_UNZIP) $(DL_DIR)/$(SUDO_SOURCE) | tar -C $(BUILD_DIR) -xvf -
-	cat $(SUDO_PATCHES) | patch -d $(BUILD_DIR)/$(SUDO_DIR) -p1
+	if test -n "$(SUDO_PATCHES)"; then \
+		cat $(SUDO_PATCHES) | patch -d $(BUILD_DIR)/$(SUDO_DIR) -p1; \
+	fi
 	mv $(BUILD_DIR)/$(SUDO_DIR) $(SUDO_BUILD_DIR)
 	cd $(SUDO_BUILD_DIR) && \
 		$(TARGET_CONFIGURE_OPTS) \
+		$(SUDO_CONFIGURE_ENV) \
 		./configure \
 			--host=$(GNU_TARGET_NAME) \
 			--target=$(GNU_TARGET_NAME) \
 			--build=$(GNU_HOST_NAME) \
 			--prefix=/opt \
 			--enable-authentication \
+			--without-pam \
 			--without-insults \
 			--with-editor=/bin/vi \
+			--with-env-editor \
 			--sysconfdir=/opt/etc
 	touch $(SUDO_BUILD_DIR)/.configured
 
 sudo-unpack: $(SUDO_BUILD_DIR)/.configured
 
-$(SUDO_BUILD_DIR)/sudo: $(SUDO_BUILD_DIR)/.configured
+$(SUDO_BUILD_DIR)/.built: $(SUDO_BUILD_DIR)/.configured
+	rm -f $@
 	make -C $(SUDO_BUILD_DIR)
+	touch $@
 
-sudo: $(SUDO_BUILD_DIR)/sudo
+sudo: $(SUDO_BUILD_DIR)/.built
 
 #
 # This rule creates a control file for ipkg.  It is no longer
@@ -82,7 +94,7 @@ $(SUDO_IPK_DIR)/CONTROL/control:
 	@echo "Suggests: $(SUDO_SUGGESTS)" >>$@
 	@echo "Conflicts: $(SUDO_CONFLICTS)" >>$@
 
-$(SUDO_IPK): $(SUDO_BUILD_DIR)/sudo
+$(SUDO_IPK): $(SUDO_BUILD_DIR)/.built
 	rm -rf $(SUDO_IPK_DIR) $(BUILD_DIR)/sudo_*_$(TARGET_ARCH).ipk
 	install -d $(SUDO_IPK_DIR)/opt/bin
 	$(STRIP_COMMAND) $(SUDO_BUILD_DIR)/sudo -o $(SUDO_IPK_DIR)/opt/bin/sudo
@@ -104,3 +116,6 @@ sudo-clean:
 
 sudo-dirclean:
 	rm -rf $(BUILD_DIR)/$(SUDO_DIR) $(SUDO_BUILD_DIR) $(SUDO_IPK_DIR) $(SUDO_IPK)
+
+sudo-check: $(SUDO_IPK)
+	perl scripts/optware-check-package.pl --target=$(OPTWARE_TARGET) $(SUDO_IPK)
