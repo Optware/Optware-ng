65384ab010a8e1417d4c05ad4370c8ee2ac64dd0
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2007-07-22 04:49:03 +0000

    Disabled packages that don't build on our new Debian Etch autobuilder

--- trunk/platforms/packages-openwrt-ixp4xx.mk
+++ trunk/platforms/packages-openwrt-ixp4xx.mk
@@ -1,7 +1,30 @@
 SPECIFIC_PACKAGES = \
 	libiconv \
 	$(UCLIBC_SPECIFIC_PACKAGES) \
-	$(PERL_PACKAGES) \
+
+#	$(PERL_PACKAGES) \
 
 BROKEN_PACKAGES = \
 	buildroot uclibc-opt \
+	asterisk \
+	chillispot cups \
+	erlang erl-yaws \
+	fcgi ficy fish ftpd-topfield \
+	gdb gnu-smalltalk gnugo gnuplot gtk \
+	hpijs \
+	ice imagemagick \
+	jamvm \
+	kismet \
+	lame launchtool ldconfig libnsl libopensync \
+	moc monotone msynctool mtr multitail \
+	nfs-utils nginx	ntop \
+	oleo obexftp \
+	pango par2cmdline phoneme-advanced player postfix \
+	qemu qemu-libc-i386 quagga quickie \
+	rrdcollect rrdtool \
+	samba sm squeak \
+	transcode tzwatch \
+	uemacs usbutils util-linux \
+	vte \
+	xt xmu xauth xaw xchat xterm
+
