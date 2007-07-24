30b04a5f67919c3735e144b7945c6415000af719
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2007-07-24 03:26:02 +0000

    Started on fsg3v4 bootstrap, and demoted packages that don't build so far

--- platforms/packages-fsg3v4.mk
+++ platforms/packages-fsg3v4.mk
@@ -1,7 +1,19 @@
 # Packages that *only* work for fsg3v4 - do not just put new packages here.
 SPECIFIC_PACKAGES = \
+	fsg3v4-bootstrap \
 	$(PERL_PACKAGES) \
 
 # Packages that do not work for fsg3v4.
 BROKEN_PACKAGES = \
 	abook adns \
+	bzflag bzip2 \
+	calc ccxstream cherokee chillispot clearsilver \
+	cron ctag ctrlproxy cups cvs cyrus-imapd \
+	dspam dtach dhcp digitemp dnsmasq dump \
+	eaccelerator ecl eggdrop elinks emacs22 erlang erl-yaws esound extract-xiso \
+	ffmpeg ficy finch freeradius \
+	gnutls \
+	libgcrypt libgpg-error \
+	opencdk openldap \
+	php python24 \
+	tcl
---
+++ sources/fsg3v4-bootstrap/bootstrap.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+BSDIR="/home/.optware/ipkg-bootstrap"
+
+echo "Creating temporary ipkg repository..."
+rm -rf $BSDIR
+mkdir -p $BSDIR
+ln -s $BSDIR /tmp/ipkg
+cat >>$BSDIR/ipkg.conf <<EOF
+dest root /
+lists_dir ext $BSDIR/ipkg
+EOF
+
+export IPKG_CONF_DIR=$BSDIR 
+export IPKG_DIR_PREFIX=$BSDIR 
+
+echo "Installing FSG-3 V4 bootstrap package..."
+mkdir -p /usr/lib/ipkg/info/
+ipkg install bootstrap.ipk
+
+echo "Removing temporary ipkg repository..."
+rm -rf $BSDIR
+rm /tmp/ipkg
+rm -rf /usr/lib/ipkg
+
+echo "Installing OpenSSL..."
+ipkg install openssl.ipk || exit 1
+
+echo "Installing wget..."
+ipkg install wget-ssl.ipk || exit 1
+
+echo "Overwriting /etc/ipkg.conf..."
+echo "src/gz cross http://ipkg.nslu2-linux.org/feeds/optware/fsg3v4/cross/stable" >/etc/ipkg.conf
+
+echo "Setup complete."
