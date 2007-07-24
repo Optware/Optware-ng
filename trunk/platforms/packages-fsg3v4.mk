5ab28c6c15bce0b8580346b0c604a209f5a7a201
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2007-07-24 21:17:49 +0000

    More broken fsg3v4 packages

--- trunk/platforms/packages-fsg3v4.mk
+++ trunk/platforms/packages-fsg3v4.mk
@@ -5,23 +5,26 @@ SPECIFIC_PACKAGES = \
 # Packages that do not work for fsg3v4.
 BROKEN_PACKAGES = \
 	abook antinat asterisk \
-	asterisk14 asterisk14-chan-capi atftp \
+	asterisk14 asterisk14-chan-capi atftp audiofile \
 	bitlbee busybox bzflag \
-	calc cherokee chillispot clearsilver \
-	ctrlproxy cups cyrus-imapd \
-	dspam dtach dhcp digitemp dnsmasq dump \
+	calc cherokee chillispot chrpath clearsilver \
+	ctrlproxy cups cyrus-imapd cyrus-sasl \
+	diffutils dspam dtach dhcp digitemp dnsmasq dump \
 	eaccelerator ecl eggdrop elinks emacs22 erlang erl-yaws esound extract-xiso \
-	ffmpeg ficy finch freeradius \
-	gdb \
-	gnuplot gnutls gphoto2 \
-	icecast irssi ivorbis-tools \
+	fcgi ffmpeg ficy finch freeradius \
+	gconv-modules gdb \
+	gnupg gnuplot gnutls gphoto2 grep \
+	hexcurse \
+	icecast iksemel irssi ivorbis-tools \
 	jabberd \
-	launchtool libao libgcrypt libgphoto2 libgpg-error \
-	libpcap librsync libshout logrotate loudmouth ltrace \
-	madplay	moc mpack mpd mutt \
-	net-snmp net-tools netatalk newt nget nmap \
-	opencdk openldap \
-	perl popt python24 \
+	launchtool ldconfig libao libgc libgcrypt libgphoto2 libgpg-error \
+	libnsl libpcap librsync libshout logrotate loudmouth ltrace \
+	madplay	mktemp moc monotone most mpack mpd mutt \
+	nemesis net-snmp net-tools netatalk newt nget nmap nzbget \
+	oleo opencdk openldap openser \
+	perl phoneme-advanced popt postfix python24 \
+	py-duplicity py-rdiff-backup \
+	qemu qemu-libc-i386 quagga quickie \
 	speex \
 	tcl \
 	$(PERL_PACKAGES) \
