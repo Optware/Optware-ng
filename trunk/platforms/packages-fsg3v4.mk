ffb02b87af287851946bb1666904bcc1d0453c28
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2007-07-24 20:42:31 +0000

    Updated broken fsg3v4 packages

--- trunk/platforms/packages-fsg3v4.mk
+++ trunk/platforms/packages-fsg3v4.mk
@@ -1,19 +1,27 @@
 # Packages that *only* work for fsg3v4 - do not just put new packages here.
 SPECIFIC_PACKAGES = \
 	fsg3v4-bootstrap \
-	$(PERL_PACKAGES) \
 
 # Packages that do not work for fsg3v4.
 BROKEN_PACKAGES = \
-	abook adns \
-	bzflag bzip2 \
-	calc ccxstream cherokee chillispot clearsilver \
-	cron ctag ctrlproxy cups cvs cyrus-imapd \
+	abook antinat asterisk \
+	asterisk14 asterisk14-chan-capi atftp \
+	bitlbee busybox bzflag \
+	calc cherokee chillispot clearsilver \
+	ctrlproxy cups cyrus-imapd \
 	dspam dtach dhcp digitemp dnsmasq dump \
 	eaccelerator ecl eggdrop elinks emacs22 erlang erl-yaws esound extract-xiso \
 	ffmpeg ficy finch freeradius \
-	gnutls \
-	libgcrypt libgpg-error \
+	gdb \
+	gnuplot gnutls gphoto2 \
+	icecast irssi ivorbis-tools \
+	jabberd \
+	launchtool libao libgcrypt libgphoto2 libgpg-error \
+	libpcap librsync libshout logrotate loudmouth ltrace \
+	madplay	moc mpack mpd mutt \
+	net-snmp net-tools netatalk newt nget nmap \
 	opencdk openldap \
-	php python24 \
-	tcl
+	perl popt python24 \
+	speex \
+	tcl \
+	$(PERL_PACKAGES) \
