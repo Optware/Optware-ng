89b723ff8135d7ce29d135dfe5cfc2d8445d0296
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2007-07-24 00:15:58 +0000

    Added support for fsg3v4

---
+++ platforms/packages-fsg3v4.mk
@@ -0,0 +1,7 @@
+# Packages that *only* work for fsg3v4 - do not just put new packages here.
+SPECIFIC_PACKAGES = \
+	$(PERL_PACKAGES) \
+
+# Packages that do not work for fsg3v4.
+BROKEN_PACKAGES = \
+
