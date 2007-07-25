46bfa3799c95feb3b2fe51b0202572d7a0d9ed13
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2007-07-25 03:19:08 +0000

    More building fsg3v4 packages

--- platforms/packages-fsg3v4.mk
+++ platforms/packages-fsg3v4.mk
@@ -4,30 +4,38 @@ SPECIFIC_PACKAGES = \
 	$(PERL_PACKAGES)
 
 # Packages that do not work for fsg3v4.
+# First group is unsorted.
+# Second group is config.sub related.
+# Third group are sorted dependency trees.
 BROKEN_PACKAGES = \
 	antinat asterisk \
-	asterisk14 asterisk14-chan-capi atftp audiofile \
+	asterisk14 asterisk14-chan-capi atftp \
 	bitlbee busybox \
 	chillispot chrpath clearsilver \
 	cyrus-imapd \
-	diffutils dspam dtach dhcp digitemp dnsmasq dump \
-	eaccelerator ecl eggdrop elinks erlang erl-yaws esound extract-xiso \
-	fcgi ffmpeg ficy finch freeradius \
+	dhcp \
+	ecl erlang erl-yaws esound \
+	fcgi ffmpeg freeradius \
 	gconv-modules gdb \
-	gnupg gnuplot grep \
-	hexcurse \
-	icecast iksemel ivorbis-tools \
-	ldconfig libao libgc \
-	libnsl libpcap libshout loudmouth ltrace \
+	gnuplot \
+	ldconfig \
+	libnsl libpcap libshout loudmouth \
 	madplay	mktemp moc monotone most mpack mpd mutt \
-	nemesis net-snmp net-tools netatalk newt nmap nzbget \
-	oleo openser \
-	phoneme-advanced postfix \
+	net-snmp net-tools netatalk newt nmap nzbget \
+	phoneme-advanced \
 	py-duplicity py-rdiff-backup \
 	qemu qemu-libc-i386 quagga quickie \
-	scponly snort speex sqsh strace streamripper swi-prolog \
-	tcl textutils \
+	\
+	diffutils hexcurse nemesis scponly snort sqsh streamripper xinetd \
+	\
+	audiofile libao ivorbis-tools vorbis-tools \
+	libgc w3m \
+	ltrace \
+	oleo \
+	openser \
+	postfix \
+	speex icecast vlc \
+	strace \
+	swi-prolog \
+	textutils \
 	uemacs \
-	vlc vorbis-tools \
-	w3m weechat wput \
-	xinetd
