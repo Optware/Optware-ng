85d51777098ece795400d8fddc969a3f8d290e5d
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2007-07-25 02:15:45 +0000

    More building fsg3v4 packages

--- platforms/packages-fsg3v4.mk
+++ platforms/packages-fsg3v4.mk
@@ -1,6 +1,7 @@
 # Packages that *only* work for fsg3v4 - do not just put new packages here.
 SPECIFIC_PACKAGES = \
 	fsg3v4-bootstrap \
+	$(PERL_PACKAGES)
 
 # Packages that do not work for fsg3v4.
 BROKEN_PACKAGES = \
@@ -13,15 +14,15 @@ BROKEN_PACKAGES = \
 	eaccelerator ecl eggdrop elinks emacs22 erlang erl-yaws esound extract-xiso \
 	fcgi ffmpeg ficy finch freeradius \
 	gconv-modules gdb \
-	gnupg gnuplot gnutls gphoto2 grep \
+	gnupg gnuplot gnutls grep \
 	hexcurse \
 	icecast iksemel irssi ivorbis-tools \
-	launchtool ldconfig libao libgc libgcrypt libgphoto2 libgpg-error \
-	libnsl libpcap librsync libshout logrotate loudmouth ltrace \
+	ldconfig libao libgc libgcrypt libgpg-error \
+	libnsl libpcap libshout loudmouth ltrace \
 	madplay	mktemp moc monotone most mpack mpd mutt \
-	nemesis net-snmp net-tools netatalk newt nget nmap nzbget \
+	nemesis net-snmp net-tools netatalk newt nmap nzbget \
 	oleo opencdk openldap openser \
-	perl phoneme-advanced postfix python24 \
+	phoneme-advanced postfix python24 \
 	py-duplicity py-rdiff-backup \
 	qemu qemu-libc-i386 quagga quickie \
 	samba scponly snort speex sqsh strace streamripper swi-prolog \
@@ -29,5 +30,4 @@ BROKEN_PACKAGES = \
 	uemacs \
 	vlc vorbis-tools \
 	w3m weechat wput \
-	xinetd \
-	$(PERL_PACKAGES) \
+	xinetd
