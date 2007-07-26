38a346b1eb17c1d6a3ad4c6d77ba7137a7c99767
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2007-07-26 00:45:56 +0000

    Added back ipkg-opt into the bootstrap, since the built-in one doesn't have update-alternatives

--- trunk/sources/fsg3v4-bootstrap/bootstrap.sh
+++ trunk/sources/fsg3v4-bootstrap/bootstrap.sh
@@ -12,6 +12,9 @@ fi
 echo "Installing FSG-3 V4 bootstrap package..."
 ipkg install bootstrap.ipk
 
+echo "Installing full-featured ipkg..."
+ipkg install ipkg.ipk
+
 echo "Overwriting /etc/ipkg.conf..."
 echo "src/gz cross http://ipkg.nslu2-linux.org/feeds/optware/fsg3v4/cross/stable" \
 		>/etc/ipkg.conf
