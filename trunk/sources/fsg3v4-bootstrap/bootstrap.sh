79278268677fc9ea3b1a0e979a520149f317c164
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2007-07-26 01:01:18 +0000

    Update the bootstrap script to install diffutils

--- trunk/sources/fsg3v4-bootstrap/bootstrap.sh
+++ trunk/sources/fsg3v4-bootstrap/bootstrap.sh
@@ -4,6 +4,7 @@ if [ -e /home/.optware ] ; then
     echo "Backup your configuration settings, then type:"
     echo "  rm -rf /home/.optware"
     echo "  rm -rf /usr/lib/ipkg"
+    echo "This will remove all existing optware packages."
     echo
     echo "Then you must *reboot* and then restart the bootstrap script."
     exit 1
@@ -14,6 +15,9 @@ ipkg install bootstrap.ipk
 
 echo "Installing full-featured ipkg..."
 ipkg install ipkg.ipk
+
+echo "Installing diffutils..."
+ipkg install diffutils.ipk
 
 echo "Overwriting /etc/ipkg.conf..."
 echo "src/gz cross http://ipkg.nslu2-linux.org/feeds/optware/fsg3v4/cross/stable" \
