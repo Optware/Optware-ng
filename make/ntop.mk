941129bdc1436f89a2b4c495e3297dd94c2680b9
Brian Zhou <bzhou@users.noreply.github.com>
2007-08-11 19:51:05 +0000

    ntop: changed "-u root" to -u0 in rc.ntop

--- make/ntop.mk
+++ make/ntop.mk
@@ -42,7 +42,7 @@ NTOP_DEPENDS=openssl, zlib, gdbm, libgd, libxml2, rrdtool, pcre
 #
 # NTOP_IPK_VERSION should be incremented when the ipk changes.
 #
-NTOP_IPK_VERSION=1
+NTOP_IPK_VERSION=2
 
 #
 # NTOP_CONFFILES should be a list of user-editable files
