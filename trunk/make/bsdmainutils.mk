00bab71ae0db59df46f756b8f5349c86159c4fe5
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2007-08-24 04:46:03 +0000

    bsdmainutils: 6.1.5 -> 6.1.6

--- trunk/make/bsdmainutils.mk
+++ trunk/make/bsdmainutils.mk
@@ -21,7 +21,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 BSDMAINUTILS_SITE=http://ftp.debian.org/debian/pool/main/b/bsdmainutils
-BSDMAINUTILS_VERSION=6.1.5
+BSDMAINUTILS_VERSION=6.1.6
 BSDMAINUTILS_SOURCE=bsdmainutils_$(BSDMAINUTILS_VERSION).tar.gz
 BSDMAINUTILS_DIR=bsdmainutils-$(BSDMAINUTILS_VERSION)
 BSDMAINUTILS_UNZIP=zcat
@@ -40,7 +40,7 @@ BSDMAINUTILS_CONFLICTS=
 #
 # BSDMAINUTILS_IPK_VERSION should be incremented when the ipk changes.
 #
-BSDMAINUTILS_IPK_VERSION=3
+BSDMAINUTILS_IPK_VERSION=1
 
 #
 # BSDMAINUTILS_CONFFILES should be a list of user-editable files
@@ -83,7 +83,8 @@ BSDMAINUTILS_IPK=$(BUILD_DIR)/bsdmainutils_$(BSDMAINUTILS_VERSION)-$(BSDMAINUTIL
 # then it will be fetched from the site using wget.
 #
 $(DL_DIR)/$(BSDMAINUTILS_SOURCE):
-	$(WGET) -P $(DL_DIR) $(BSDMAINUTILS_SITE)/$(BSDMAINUTILS_SOURCE)
+	$(WGET) -P $(DL_DIR) $(BSDMAINUTILS_SITE)/$(BSDMAINUTILS_SOURCE) || \
+	$(WGET) -P $(DL_DIR) $(SOURCES_NLO_SITE)/$(BSDMAINUTILS_SOURCE)
 
 #
 # The source code depends on it existing within the download directory.
@@ -210,7 +211,8 @@ $(BSDMAINUTILS_IPK): $(BSDMAINUTILS_BUILD_DIR)/.built
 	$(MAKE) -C $(BSDMAINUTILS_BUILD_DIR) install \
 		DESTDIR=$(BSDMAINUTILS_IPK_DIR) \
 		sysconfdir=$(BSDMAINUTILS_IPK_DIR)/opt/etc
-	$(STRIP_COMMAND) `ls $(BSDMAINUTILS_IPK_DIR)/opt/{bin,games}/* | egrep -v bin/lorder`
+	rm -rf $(BSDMAINUTILS_IPK_DIR)/opt/games $(BSDMAINUTILS_IPK_DIR)/opt/share/man/man6
+	$(STRIP_COMMAND) `ls $(BSDMAINUTILS_IPK_DIR)/opt/bin/* | egrep -v bin/lorder`
 #	install -d $(BSDMAINUTILS_IPK_DIR)/opt/etc/
 #	install -m 644 $(BSDMAINUTILS_SOURCE_DIR)/bsdmainutils.conf $(BSDMAINUTILS_IPK_DIR)/opt/etc/bsdmainutils.conf
 #	install -d $(BSDMAINUTILS_IPK_DIR)/opt/etc/init.d
