4e1756f6b61a5f66d5509b197279eaeedf89da82
Brian Zhou <bzhou@users.noreply.github.com>
2007-08-30 19:49:28 +0000

    samba: 3.0.25b -> 3.0.25c

--- make/samba.mk
+++ make/samba.mk
@@ -21,7 +21,7 @@
 #
 SAMBA_SITE=http://www.samba.org/samba/ftp/stable
 ifneq ($(OPTWARE_TARGET),wl500g)
-SAMBA_VERSION=3.0.25b
+SAMBA_VERSION=3.0.25c
 SAMBA_IPK_VERSION=1
 else
 SAMBA_VERSION=3.0.14a
