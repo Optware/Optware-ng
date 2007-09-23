9def922aa7090c14f280d7b9e522c5f74ccf7cf2
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2007-09-23 02:54:39 +0000

    perl is working on mssii

---
+++ trunk/platforms/packages-mssii.mk
@@ -0,0 +1,7 @@
+# Packages that *only* work for mssii - do not just put new packages here.
+SPECIFIC_PACKAGES = \
+	$(PERL_PACKAGES)
+
+# Packages that do not work for mssii.
+BROKEN_PACKAGES = \
+	chillispot \
