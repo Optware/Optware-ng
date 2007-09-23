a5392ac8a1e82c0b5f9b7fe5c86c2629eba6087e
Brian Zhou <bzhou@users.noreply.github.com>
2007-09-23 15:07:10 +0000

    mssii: broken list on first build

--- platforms/packages-mssii.mk
+++ platforms/packages-mssii.mk
@@ -1,9 +1,18 @@
 # Packages that *only* work for mssii - do not just put new packages here.
 SPECIFIC_PACKAGES = \
+	ipkg-opt \
 	$(PERL_PACKAGES)
 
 # Packages that do not work for mssii.
 BROKEN_PACKAGES = \
-	asterisk14-chan-capi \
-	gnuplot \
-	gtk \
+	iptraf \
+	ldconfig \
+	monotone \
+	player \
+	puppy \
+	qemu qemu-libc-i386 \
+	quagga \
+	socat \
+	uemacs \
+	usbutils \
+	gtk vte xchat \
