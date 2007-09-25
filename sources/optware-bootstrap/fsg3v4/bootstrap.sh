5e77d8f82085b6326d97c769e282e87f1aa2ca73
Brian Zhou <bzhou@users.noreply.github.com>
2007-09-25 03:17:37 +0000

    optware-bootstrap: initial check-in

---
+++ sources/optware-bootstrap/fsg3v4/bootstrap.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ -e /home/.optware ] ; then
+    echo "Backup your configuration settings, then type:"
+    echo "  rm -rf /home/.optware"
+    echo "  rm -rf /usr/lib/ipkg"
+    echo "This will remove all existing optware packages."
+    echo
+    echo "Then you must *reboot* and then restart the bootstrap script."
+    exit 1
+fi
+
+echo "Installing FSG-3 V4 bootstrap package..."
+ipkg install bootstrap.ipk
+
+echo "Installing diffutils..."
+ipkg install diffutils.ipk
+
+echo "Overwriting /etc/ipkg.conf..."
+echo "src/gz cross http://ipkg.nslu2-linux.org/feeds/optware/fsg3v4/cross/stable" \
+		>/etc/ipkg.conf
+
+echo "Setup complete."
---
+++ sources/optware-bootstrap/slugosbe/bootstrap.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+BSDIR="/.optware/ipkg-bootstrap"
+
+echo "Creating temporary ipkg repository..."
+rm -rf $BSDIR
+mkdir -p $BSDIR
+ln -s $BSDIR /tmp/ipkg
+cat >>$BSDIR/ipkg.conf <<EOF
+dest root /
+lists_dir ext $BSDIR/ipkg
+EOF
+
+export IPKG_CONF_DIR=$BSDIR 
+export IPKG_DIR_PREFIX=$BSDIR 
+
+echo "Installing optware-bootstrap package..."
+sh ./ipkg.sh install optware-bootstrap.ipk
+
+echo "Installing ipkg..."
+sh ./ipkg.sh install ipkg.ipk
+
+echo "Removing temporary ipkg repository..."
+rm -rf $BSDIR
+rm /tmp/ipkg
+
+echo "Installing OpenSSL..."
+/opt/bin/ipkg install openssl.ipk || exit 1
+
+echo "Installing wget..."
+/opt/bin/ipkg install wget-ssl.ipk || exit 1
+
+[ ! -d /opt/etc/ipkg ] && mkdir -p /opt/etc/ipkg
+if [ ! -e /opt/etc/ipkg/cross-feed.conf ]
+then
+	echo "Creating /opt/etc/ipkg/cross-feed.conf..."
+	echo "src/gz cross http://ipkg.nslu2-linux.org/feeds/optware/slguosbe/cross/stable" >/opt/etc/ipkg/cross-feed.conf
+fi
+
+echo "Setup complete."
