c916660be7f4aa1f66f8a81825cb7696082568f9
Brian Zhou <bzhou@users.noreply.github.com>
2007-10-07 04:52:21 +0000

    adduser: CONFIG_MONOTONIC_SYSCALL off unless linux 2.6

--- make/adduser.mk
+++ make/adduser.mk
@@ -107,6 +107,11 @@ $(ADDUSER_BUILD_DIR)/.configured:
 	cat $(ADDUSER_PATCHES) | patch -d $(BUILD_DIR)/$(BUSYBOX_DIR) -p1
 	mv $(BUILD_DIR)/$(BUSYBOX_DIR) $(ADDUSER_BUILD_DIR)
 	cp $(ADDUSER_SOURCE_DIR)/defconfig $(ADDUSER_BUILD_DIR)/.config
+ifeq (module-init-tools, $(filter module-init-tools, $(PACKAGES)))
+# default off, turn on if linux 2.6
+	sed -i -e "s/^.*CONFIG_MONOTONIC_SYSCALL.*/CONFIG_MONOTONIC_SYSCALL=y/" \
+		$(BUSYBOX_BUILD_DIR)/.config
+endif
 	sed -i -e 's/-strip /-$$(STRIP) /' $(ADDUSER_BUILD_DIR)/scripts/Makefile.IMA
 	$(MAKE) HOSTCC=$(HOSTCC) CC=$(TARGET_CC) CROSS=$(TARGET_CROSS) \
 		-C $(ADDUSER_BUILD_DIR) oldconfig
