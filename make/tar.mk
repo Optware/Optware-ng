6496707dace153b6629baf00205c1b34d3e06f15
Brian Zhou <bzhou@users.noreply.github.com>
2007-10-08 06:18:37 +0000

    tar: use update-alternatives

--- make/tar.mk
+++ make/tar.mk
@@ -22,10 +22,10 @@
 TAR_SITE=http://ftp.gnu.org/gnu/tar
 ifneq ($(OPTWARE_TARGET), wl500g)
 TAR_VERSION=1.18
-TAR_IPK_VERSION=1
+TAR_IPK_VERSION=2
 else
 TAR_VERSION=1.16.1
-TAR_IPK_VERSION=1
+TAR_IPK_VERSION=2
 endif
 TAR_SOURCE=tar-$(TAR_VERSION).tar.bz2
 TAR_DIR=tar-$(TAR_VERSION)
@@ -35,7 +35,7 @@ TAR_DESCRIPTION=heavyweight version of the Tape ARchiver
 TAR_SECTION=util
 TAR_PRIORITY=optional
 TAR_DEPENDS=bzip2
-TAR_CONFLICTS=busybox-links
+TAR_CONFLICTS=
 
 
 #
@@ -108,7 +108,7 @@ $(TAR_BUILD_DIR)/.configured: $(DL_DIR)/$(TAR_SOURCE) $(TAR_PATCHES)
 		--prefix=/opt \
 		--disable-nls \
 	)
-	touch $(TAR_BUILD_DIR)/.configured
+	touch $@
 
 tar-unpack: $(TAR_BUILD_DIR)/.configured
 
@@ -117,9 +117,9 @@ tar-unpack: $(TAR_BUILD_DIR)/.configured
 # directly to the main binary which is built.
 #
 $(TAR_BUILD_DIR)/.built: $(TAR_BUILD_DIR)/.configured
-	rm -f $(TAR_BUILD_DIR)/.built
+	rm -f $@
 	$(MAKE) -C $(TAR_BUILD_DIR)
-	touch $(TAR_BUILD_DIR)/.built
+	touch $@
 
 #
 # You should change the dependency to refer directly to the main binary
@@ -132,7 +132,7 @@ tar: $(TAR_BUILD_DIR)/.built
 # necessary to create a seperate control file under sources/tar
 #
 $(TAR_IPK_DIR)/CONTROL/control:
-	@install -d $(TAR_IPK_DIR)/CONTROL
+	@install -d $(@D)
 	@rm -f $@
 	@echo "Package: tar" >>$@
 	@echo "Architecture: $(TARGET_ARCH)" >>$@
@@ -160,10 +160,16 @@ $(TAR_IPK_DIR)/CONTROL/control:
 $(TAR_IPK): $(TAR_BUILD_DIR)/.built
 	rm -rf $(TAR_IPK_DIR) $(BUILD_DIR)/tar_*_$(TARGET_ARCH).ipk
 	install -d $(TAR_IPK_DIR)/opt/bin
-	$(STRIP_COMMAND) $(TAR_BUILD_DIR)/src/tar -o $(TAR_IPK_DIR)/opt/bin/tar
+	$(STRIP_COMMAND) $(TAR_BUILD_DIR)/src/tar -o $(TAR_IPK_DIR)/opt/bin/gnutar
 	install -d $(TAR_IPK_DIR)/opt/libexec
 #	$(STRIP_COMMAND) $(TAR_BUILD_DIR)/src/rmt -o $(TAR_IPK_DIR)/opt/libexec/rmt
 	$(MAKE) $(TAR_IPK_DIR)/CONTROL/control
+	(echo "#!/bin/sh"; \
+	 echo "update-alternatives --install /opt/bin/tar tar /opt/bin/gnutar 80"; \
+	) > $(TAR_IPK_DIR)/CONTROL/postinst
+	(echo "#!/bin/sh"; \
+	 echo "update-alternatives --remove tar /opt/bin/gnutar"; \
+	) > $(TAR_IPK_DIR)/CONTROL/prerm
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(TAR_IPK_DIR)
 
 #
