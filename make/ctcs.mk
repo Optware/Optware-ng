9a23efb6fada8cbb8426fbc35166f375229a1e72
Brian Zhou <bzhou@users.noreply.github.com>
2007-10-19 16:58:55 +0000

    ctcs: s/return 0/exit 0/ in postinst/prerm

--- make/ctcs.mk
+++ make/ctcs.mk
@@ -27,7 +27,7 @@ CTCS_CONFLICTS=
 #
 # CTCS_IPK_VERSION should be incremented when the ipk changes.
 #
-CTCS_IPK_VERSION=8
+CTCS_IPK_VERSION=9
 
 #
 # CTCS_CONFFILES should be a list of user-editable files
