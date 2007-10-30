49ec3647012ee3b9e080b9c4dcce303343d31497
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2007-10-30 02:36:24 +0000

    Support for the Freecom Datatank (dt2)

---
+++ trunk/platforms/packages-dt2.mk
@@ -0,0 +1,19 @@
+# Packages that *only* work for dt2 - do not just put new packages here.
+SPECIFIC_PACKAGES = \
+	dt2-optware-bootstrap \
+	$(PERL_PACKAGES) \
+	$(PACKAGES_REQUIRE_LINUX26) \
+
+# Packages that do not work for dt2.
+BROKEN_PACKAGES = \
+	$(PACKAGES_ONLY_WORK_ON_LINUX24) \
+	asterisk14-chan-capi libcapi20 \
+	gnuplot \
+	iptraf \
+	ldconfig \
+	monotone \
+	player \
+	puppy \
+	qemu qemu-libc-i386 \
+	socat \
+	uemacs \
