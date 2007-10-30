9fdc5e63eaf6c4e2b58829474bc38ec8e6bde1e6
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2007-10-30 02:36:24 +0000

    Support for the Freecom Datatank (dt2)

---
+++ sources/optware-bootstrap/dt2/bootstrap.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ -e /home/.optware ] ; then
+    echo "Backup your configuration settings, then type:"
+    echo "  rm -rf /home/.optware"
+    echo "  rm -rf /usr/lib/ipkg"
+    echo "This will remove all existing optware packages."
+    echo
+    echo "Then you must *reboot* and then restart the bootstrap script."
+    exit 1
+fi
+
+echo "Installing DataTank bootstrap package..."
+ipkg install bootstrap.ipk
+
+echo "Installing diffutils..."
+ipkg install diffutils.ipk
+
+echo "Overwriting /etc/ipkg.conf..."
+echo "src/gz cross http://ipkg.nslu2-linux.org/feeds/optware/dt2/cross/stable" \
+		>/etc/ipkg.conf
+
+echo "Setup complete."
--- sources/optware-bootstrap/target-specific.mk
+++ sources/optware-bootstrap/target-specific.mk
@@ -1,6 +1,6 @@
 OPTWARE-BOOTSTRAP_REAL_OPT_DIR=$(strip \
 	$(if $(filter ds101 ds101g, $(OPTWARE_TARGET)), /volume1/opt, \
-	$(if $(filter fsg3 fsg3v4, $(OPTWARE_TARGET)), /home/.optware, \
+	$(if $(filter fsg3 fsg3v4 dt2, $(OPTWARE_TARGET)), /home/.optware, \
 	$(if $(filter mssii, $(OPTWARE-BOOTSTRAP_TARGET)), /share/.optware, \
 	$(if $(filter lspro, $(OPTWARE-BOOTSTRAP_TARGET)), /mnt/disk1/.optware, \
 	$(if $(filter teraprov2, $(OPTWARE-BOOTSTRAP_TARGET)), /mnt/array1/.optware, \
@@ -11,7 +11,7 @@ OPTWARE-BOOTSTRAP_RC=$(strip \
 	/etc/init.d/optware))
 
 OPTWARE-BOOTSTRAP_CONTAINS=$(strip \
-	$(if $(filter fsg3 fsg3v4, $(OPTWARE_TARGET)), diffutils, \
+	$(if $(filter fsg3 fsg3v4 dt2, $(OPTWARE_TARGET)), diffutils, \
 	ipkg-opt openssl wget-ssl))
 
 # Ideally the following stanza would work
@@ -26,6 +26,11 @@ fsg3v4-optware-bootstrap-ipk:
 	$(MAKE) optware-bootstrap-ipk OPTWARE-BOOTSTRAP_TARGET=fsg3v4
 fsg3v4-optware-bootstrap-dirclean:
 	$(MAKE) optware-bootstrap-dirclean OPTWARE-BOOTSTRAP_TARGET=fsg3v4
+
+dt2-optware-bootstrap-ipk:
+	$(MAKE) optware-bootstrap-ipk OPTWARE-BOOTSTRAP_TARGET=dt2
+dt2-optware-bootstrap-dirclean:
+	$(MAKE) optware-bootstrap-dirclean OPTWARE-BOOTSTRAP_TARGET=dt2
 
 lspro-optware-bootstrap-ipk:
 	$(MAKE) optware-bootstrap-ipk OPTWARE-BOOTSTRAP_TARGET=lspro
