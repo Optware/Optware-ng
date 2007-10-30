9704e644b2d76f7d82d0d592dd645889ae320ad4
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2007-10-30 06:57:29 +0000

    SPECIFIC_PACKAGES is required for the index to work

--- platforms/packages-dt2.mk
+++ platforms/packages-dt2.mk
@@ -1,5 +1,7 @@
 # Packages that *only* work for dt2 - do not just put new packages here.
-PACKAGES = \
+SPECIFIC_PACKAGES = \
 	dt2-optware-bootstrap
+
+PACKAGES = ${SPECIFIC_PACKAGES}
 
 # The rest of the packages for the DataTank come from the cs05q3armel feed.
