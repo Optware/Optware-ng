2af258d7ae07e6733e1b7e164e5a87829e7fe2c4
Brian Zhou <bzhou@users.noreply.github.com>
2007-10-31 23:55:31 +0000

    oleg: demoted firmware-oleg & lirc

--- platforms/packages-oleg.mk
+++ platforms/packages-oleg.mk
@@ -2,6 +2,8 @@ SPECIFIC_PACKAGES = \
 	firmware-oleg \
 	$(UCLIBC_SPECIFIC_PACKAGES) \
 
+# firmware-oleg 1) needs specific version of old make; 2) requires wl500g toolchain
+# lirc depends on firmware-oleg
 BROKEN_PACKAGES = \
 	$(UCLIBC_BROKEN_PACKAGES) \
 	dansguardian \
@@ -12,4 +14,7 @@ BROKEN_PACKAGES = \
 	nget \
 	player \
 	zsh \
+	\
+	firmware-oleg \
+	lirc \
 
