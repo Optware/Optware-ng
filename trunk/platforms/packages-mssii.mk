b8b7ea77b2e6312dd2477459ddd5149f0af3f9e0
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2007-10-31 02:18:30 +0000

    mv mssii cs05q3armel; mv mssii-kernel mssii

--- trunk/platforms/packages-mssii.mk
+++ trunk/platforms/packages-mssii.mk
@@ -1,21 +1,4 @@
-# Packages that *only* work for mssii - do not just put new packages here.
-SPECIFIC_PACKAGES = \
+PACKAGES = \
 	mssii-optware-bootstrap \
-	lspro-optware-bootstrap \
-	teraprov2-optware-bootstrap \
-	$(PERL_PACKAGES) \
-	$(PACKAGES_REQUIRE_LINUX26) \
+	mssii-kernel-modules \
 
-# Packages that do not work for mssii.
-BROKEN_PACKAGES = \
-	$(PACKAGES_ONLY_WORK_ON_LINUX24) \
-	asterisk14-chan-capi libcapi20 \
-	gnuplot \
-	iptraf \
-	ldconfig \
-	monotone \
-	player \
-	puppy \
-	qemu qemu-libc-i386 \
-	socat \
-	uemacs \
