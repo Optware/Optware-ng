d034a47e10219f2102234d040a377b0be92fdcfa
Brian Zhou <bzhou@users.noreply.github.com>
2007-11-04 06:36:13 +0000

    rsnapshot: 1.2.9 -> 1.3.0; native to cross

--- make/rsnapshot.mk
+++ make/rsnapshot.mk
@@ -5,7 +5,7 @@
 ###########################################################
 
 RSNAPSHOT_SITE=http://www.rsnapshot.org/downloads
-RSNAPSHOT_VERSION=1.2.9
+RSNAPSHOT_VERSION=1.3.0
 RSNAPSHOT_SOURCE=rsnapshot-$(RSNAPSHOT_VERSION).tar.gz
 RSNAPSHOT_DIR=rsnapshot-$(RSNAPSHOT_VERSION)
 RSNAPSHOT_UNZIP=zcat
@@ -31,7 +31,7 @@ RSNAPSHOT_IPK_DIR=$(BUILD_DIR)/rsnapshot-$(RSNAPSHOT_VERSION)-ipk
 RSNAPSHOT_IPK=$(BUILD_DIR)/rsnapshot_$(RSNAPSHOT_VERSION)-$(RSNAPSHOT_IPK_VERSION)_$(TARGET_ARCH).ipk
 
 $(RSNAPSHOT_IPK_DIR)/CONTROL/control:
-	@install -d $(RSNAPSHOT_IPK_DIR)/CONTROL
+	@install -d $(@D)
 	@rm -f $@
 	@echo "Package: rsnapshot" >>$@
 	@echo "Architecture: $(TARGET_ARCH)" >>$@
@@ -48,61 +48,58 @@ $(DL_DIR)/$(RSNAPSHOT_SOURCE):
 
 rsnapshot-source: $(DL_DIR)/$(RSNAPSHOT_SOURCE) $(RSNAPSHOT_PATCHES)
 
-$(RSNAPSHOT_BUILD_DIR)/.configured: $(DL_DIR)/$(RSNAPSHOT_SOURCE) $(RSNAPSHOT_PATCHES)
-	$(MAKE) rsync-stage
+$(RSNAPSHOT_BUILD_DIR)/.configured: $(DL_DIR)/$(RSNAPSHOT_SOURCE) $(RSNAPSHOT_PATCHES) make/rsnapshot.mk
+#	$(MAKE) rsync-stage
 	rm -rf $(BUILD_DIR)/$(RSNAPSHOT_DIR) $(RSNAPSHOT_BUILD_DIR)
 	$(RSNAPSHOT_UNZIP) $(DL_DIR)/$(RSNAPSHOT_SOURCE) | tar -C $(BUILD_DIR) -xvf -
 #	cat $(RSNAPSHOT_PATCHES) | patch -d $(BUILD_DIR)/$(RSNAPSHOT_DIR) -p1
-	mv $(BUILD_DIR)/$(RSNAPSHOT_DIR) $(RSNAPSHOT_BUILD_DIR)
-	sed -i 's#/usr/bin/pod2man#pod2man#' $(RSNAPSHOT_BUILD_DIR)/Makefile.in
-	(cd $(RSNAPSHOT_BUILD_DIR); \
+	mv $(BUILD_DIR)/$(RSNAPSHOT_DIR) $(@D)
+	sed -i 's#/usr/bin/pod2man#pod2man#' $(@D)/Makefile.in
+	(cd $(@D); \
 		$(TARGET_CONFIGURE_OPTS) \
 		CPPFLAGS="$(STAGING_CPPFLAGS) $(RSNAPSHOT_CPPFLAGS)" \
 		LDFLAGS="$(STAGING_LDFLAGS) $(RSNAPSHOT_LDFLAGS)" \
+		ac_cv_path_PERL=/opt/bin/perl \
+		ac_cv_path_RSYNC=/opt/bin/rsync \
+		ac_cv_path_SSH=/opt/bin/ssh \
+		ac_cv_path_CP=/opt/bin/cp \
+		ac_cv_path_RM=/opt/bin/rm \
+		ac_cv_path_DU=/opt/bin/du \
 		./configure \
 		--build=$(GNU_HOST_NAME) \
 		--host=$(GNU_TARGET_NAME) \
 		--target=$(GNU_TARGET_NAME) \
 		--prefix=/opt \
 		--disable-nls \
-		--with-perl=/opt/bin/perl \
-		--with-rsync=$(STAGING_DIR)/opt/bin/rsync \
-		--with-ssh=/opt/bin/ssh \
-		--with-cp=/opt/bin/cp \
-		--with-rm=/opt/bin/rm \
-		--with-du=/opt/bin/du \
 		--without-logger \
 	)
-	touch $(RSNAPSHOT_BUILD_DIR)/.configured
+	touch $@
 
 rsnapshot-unpack: $(RSNAPSHOT_BUILD_DIR)/.configured
 
 $(RSNAPSHOT_BUILD_DIR)/.built: $(RSNAPSHOT_BUILD_DIR)/.configured
-	rm -f $(RSNAPSHOT_BUILD_DIR)/.built
+	rm -f $@
 #	$(MAKE) -C $(RSNAPSHOT_BUILD_DIR)
-	echo "Nothing to do for target 'rsnapshot'."
-	touch $(RSNAPSHOT_BUILD_DIR)/.built
+	touch $@
 
 rsnapshot: $(RSNAPSHOT_BUILD_DIR)/.built
 
 $(RSNAPSHOT_BUILD_DIR)/.staged: $(RSNAPSHOT_BUILD_DIR)/.built
-	rm -f $(RSNAPSHOT_BUILD_DIR)/.staged
+#	rm -f $@
 #	$(MAKE) -C $(RSNAPSHOT_BUILD_DIR) DESTDIR=$(STAGING_DIR) install
-	echo "Warning: the makefile target 'rsnapshot-stage' is not available."
-	touch $(RSNAPSHOT_BUILD_DIR)/.staged
+#	touch $@
 
 rsnapshot-stage: $(RSNAPSHOT_BUILD_DIR)/.staged
 
 $(RSNAPSHOT_IPK): $(RSNAPSHOT_BUILD_DIR)/.built
 	rm -rf $(RSNAPSHOT_IPK_DIR) $(BUILD_DIR)/rsnapshot_*_$(TARGET_ARCH).ipk
 	$(MAKE) -C $(RSNAPSHOT_BUILD_DIR) DESTDIR=$(RSNAPSHOT_IPK_DIR) install
+	sed -i -e '/\/usr\/bin\/perl -w/d' -e 's|/usr/local/|/opt/|g' $(RSNAPSHOT_IPK_DIR)/opt/bin/*
 	find $(RSNAPSHOT_IPK_DIR)/opt -type d -exec chmod go+rx {} \;
-	rm -rf $(RSNAPSHOT_IPK_DIR)/opt/etc/rsnapshot.conf.default
 	install -m 644 $(RSNAPSHOT_SOURCE_DIR)/rsnapshot.conf $(RSNAPSHOT_IPK_DIR)/opt/etc/rsnapshot.conf
 	install -d $(RSNAPSHOT_IPK_DIR)/opt/var/rsnapshot/
 	install -d $(RSNAPSHOT_IPK_DIR)/opt/var/run/
 	install -d $(RSNAPSHOT_IPK_DIR)/opt/var/log/
-
 	$(MAKE) $(RSNAPSHOT_IPK_DIR)/CONTROL/control
 #	install -m 755 $(RSNAPSHOT_SOURCE_DIR)/postinst $(RSNAPSHOT_IPK_DIR)/CONTROL/postinst
 #	install -m 755 $(RSNAPSHOT_SOURCE_DIR)/prerm $(RSNAPSHOT_IPK_DIR)/CONTROL/prerm
@@ -116,3 +113,6 @@ rsnapshot-clean:
 
 rsnapshot-dirclean:
 	rm -rf $(BUILD_DIR)/$(RSNAPSHOT_DIR) $(RSNAPSHOT_BUILD_DIR) $(RSNAPSHOT_IPK_DIR) $(RSNAPSHOT_IPK)
+
+rsnapshot-check: $(RSNAPSHOT_IPK)
+	perl scripts/optware-check-package.pl --target=$(OPTWARE_TARGET) $(RSNAPSHOT_IPK)
