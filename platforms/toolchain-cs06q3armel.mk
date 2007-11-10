aaeebfa56900614258abcc1ce09bf46ef80adaab
Brian Zhou <bzhou@users.noreply.github.com>
2007-11-10 17:36:52 +0000

    gcc 4.1.1 toolchain for armel

---
+++ platforms/toolchain-cs06q3armel.mk
@@ -0,0 +1,37 @@
+# This toolchain is gcc 4.1.1 on glibc 2.3.6
+
+TARGET_ARCH=arm
+TARGET_OS=linux
+LIBC_STYLE=glibc
+
+HOSTCC = gcc
+GNU_HOST_NAME = $(HOST_MACHINE)-pc-linux-gnu
+GNU_TARGET_NAME = arm-none-linux-gnueabi
+TARGET_CROSS_TOP = $(BASE_DIR)/toolchain/$(GNU_TARGET_NAME)/gcc-2006q3-glibc-2.3.6
+TARGET_CROSS = $(TARGET_CROSS_TOP)/bin/$(GNU_TARGET_NAME)-
+TARGET_LIBDIR = $(TARGET_CROSS_TOP)/$(GNU_TARGET_NAME)/lib
+TARGET_USRLIBDIR = $(TARGET_CROSS_TOP)/$(GNU_TARGET_NAME)/libc/usr/lib
+TARGET_INCDIR = $(TARGET_CROSS_TOP)/$(GNU_TARGET_NAME)/libc/usr/include
+TARGET_LDFLAGS =
+TARGET_CUSTOM_FLAGS= -pipe
+TARGET_CFLAGS=$(TARGET_OPTIMIZATION) $(TARGET_DEBUGGING) $(TARGET_CUSTOM_FLAGS)
+
+TOOLCHAIN_SITE=http://www.codesourcery.com/public/gnu_toolchain/arm-none-linux-gnueabi
+TOOLCHAIN_BINARY=arm-2006q3-27-arm-none-linux-gnueabi-i686-pc-linux-gnu.tar.bz2
+TOOLCHAIN_SOURCE=arm-2006q3-27-arm-none-linux-gnueabi.src.tar.bz2
+
+toolchain: $(TARGET_CROSS_TOP)/.unpacked
+
+$(DL_DIR)/$(TOOLCHAIN_BINARY):
+	$(WGET) -P $(DL_DIR) $(TOOLCHAIN_SITE)/$(@F) || \
+	$(WGET) -P $(DL_DIR) $(SOURCES_NLO_SITE)/$(@F)
+
+$(DL_DIR)/$(TOOLCHAIN_SOURCE):
+	$(WGET) -P $(DL_DIR) $(TOOLCHAIN_SITE)/$(@F) || \
+	$(WGET) -P $(DL_DIR) $(SOURCES_NLO_SITE)/$(@F)
+
+$(TARGET_CROSS_TOP)/.unpacked: $(DL_DIR)/$(TOOLCHAIN_BINARY) # $(OPTWARE_TOP)/platforms/toolchain-$(OPTWARE_TARGET).mk
+	rm -rf $(TARGET_CROSS_TOP)
+	mkdir -p $(TARGET_CROSS_TOP)
+	tar -xj -C $(TARGET_CROSS_TOP) -f $(DL_DIR)/$(TOOLCHAIN_BINARY)
+	touch $@
