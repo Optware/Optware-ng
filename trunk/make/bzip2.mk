0763d565df7012d549542939200c9be083b7361f
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2007-11-17 06:37:41 +0000

    bzip2: use update-alternatives to co-exist with busybox

--- trunk/make/bzip2.mk
+++ trunk/make/bzip2.mk
@@ -17,7 +17,7 @@ BZIP2_PRIORITY=optional
 BZIP2_DEPENDS=
 BZIP2_CONFLICTS=
 
-BZIP2_IPK_VERSION=1
+BZIP2_IPK_VERSION=2
 
 BZIP2_BUILD_DIR=$(BUILD_DIR)/bzip2
 BZIP2_SOURCE_DIR=$(SOURCE_DIR)/bzip2
@@ -35,24 +35,27 @@ $(BZIP2_BUILD_DIR)/.configured: $(DL_DIR)/$(BZIP2_SOURCE)
 	rm -rf $(BUILD_DIR)/$(BZIP2_DIR) $(BZIP2_BUILD_DIR)
 	$(BZIP2_UNZIP) $(DL_DIR)/$(BZIP2_SOURCE) | tar -C $(BUILD_DIR) -xvf -
 	mv $(BUILD_DIR)/$(BZIP2_DIR) $(BZIP2_BUILD_DIR)
-	touch $(BZIP2_BUILD_DIR)/.configured
+	touch $@
 
 bzip2-unpack: $(BZIP2_BUILD_DIR)/.configured
 
-$(BZIP2_BUILD_DIR)/bzip2: $(BZIP2_BUILD_DIR)/.configured
-	$(MAKE) -C $(BZIP2_BUILD_DIR) \
+$(BZIP2_BUILD_DIR)/.built: $(BZIP2_BUILD_DIR)/.configured
+	rm -f $@
+	$(MAKE) -C $(@D) \
 		PREFIX=/opt \
 		$(TARGET_CONFIGURE_OPTS) \
 		-f Makefile \
 		libbz2.a bzip2 bzip2recover
-	$(MAKE) -C $(BZIP2_BUILD_DIR) \
+	$(MAKE) -C $(@D) \
 		PREFIX=/opt \
 		$(TARGET_CONFIGURE_OPTS) \
 		-f Makefile-libbz2_so
+	touch $@
 
-bzip2: $(BZIP2_BUILD_DIR)/bzip2
+bzip2: $(BZIP2_BUILD_DIR)/.built
 
-$(STAGING_LIB_DIR)/libbz2.a: $(BZIP2_BUILD_DIR)/bzip2
+$(BZIP2_BUILD_DIR)/.staged: $(BZIP2_BUILD_DIR)/.built
+	rm -f $@
 	install -d $(STAGING_INCLUDE_DIR)
 	install -m 644 $(BZIP2_BUILD_DIR)/bzlib.h $(STAGING_INCLUDE_DIR)
 	install -d $(STAGING_LIB_DIR)
@@ -60,15 +63,16 @@ $(STAGING_LIB_DIR)/libbz2.a: $(BZIP2_BUILD_DIR)/bzip2
 	install -m 644 $(BZIP2_BUILD_DIR)/libbz2.so.$(BZIP2_LIB_VERSION) $(STAGING_LIB_DIR)
 	cd $(STAGING_DIR)/opt/lib && ln -fs libbz2.so.$(BZIP2_LIB_VERSION) libbz2.so.1.0
 	cd $(STAGING_DIR)/opt/lib && ln -fs libbz2.so.$(BZIP2_LIB_VERSION) libbz2.so
+	touch $@
 
-bzip2-stage: $(STAGING_LIB_DIR)/libbz2.a
+bzip2-stage: $(BZIP2_BUILD_DIR)/.staged
 
 #
 # This rule creates a control file for ipkg.  It is no longer
 # necessary to create a seperate control file under sources/bzip2
 #
 $(BZIP2_IPK_DIR)/CONTROL/control:
-	@install -d $(BZIP2_IPK_DIR)/CONTROL
+	@install -d $(@D)
 	@rm -f $@
 	@echo "Package: bzip2" >>$@
 	@echo "Architecture: $(TARGET_ARCH)" >>$@
@@ -81,10 +85,10 @@ $(BZIP2_IPK_DIR)/CONTROL/control:
 	@echo "Depends: $(BZIP2_DEPENDS)" >>$@
 	@echo "Conflicts: $(BZIP2_CONFLICTS)" >>$@
 
-$(BZIP2_IPK): $(BZIP2_BUILD_DIR)/bzip2
+$(BZIP2_IPK): $(BZIP2_BUILD_DIR)/.built
 	rm -rf $(BZIP2_IPK_DIR) $(BUILD_DIR)/bzip2_*_$(TARGET_ARCH).ipk
 	install -d $(BZIP2_IPK_DIR)/opt/bin
-	$(STRIP_COMMAND) $(BZIP2_BUILD_DIR)/bzip2 -o $(BZIP2_IPK_DIR)/opt/bin/bzip2
+	$(STRIP_COMMAND) $(BZIP2_BUILD_DIR)/bzip2 -o $(BZIP2_IPK_DIR)/opt/bin/bzip2-bzip2
 	$(STRIP_COMMAND) $(BZIP2_BUILD_DIR)/bzip2recover -o $(BZIP2_IPK_DIR)/opt/bin/bzip2recover
 	install -d $(BZIP2_IPK_DIR)/opt/include
 	install -m 644 $(BZIP2_BUILD_DIR)/bzlib.h $(BZIP2_IPK_DIR)/opt/include
@@ -97,6 +101,16 @@ $(BZIP2_IPK): $(BZIP2_BUILD_DIR)/bzip2
 	install -m 644 $(BZIP2_BUILD_DIR)/manual*.html $(BZIP2_IPK_DIR)/opt/doc/bzip2
 	cd $(BZIP2_IPK_DIR)/opt/bin && ln -fs bzip2 bzcat
 	$(MAKE) $(BZIP2_IPK_DIR)/CONTROL/control
+	(echo "#!/bin/sh"; \
+	 echo "update-alternatives --install /opt/bin/bzip2 bzip2 /opt/bin/bzip2-bzip2 80" \
+	) > $(BZIP2_IPK_DIR)/CONTROL/postinst
+	(echo "#!/bin/sh"; \
+	 echo "update-alternatives --remove bzip2 /opt/bin/bzip2-bzip2" \
+	) > $(BZIP2_IPK_DIR)/CONTROL/prerm
+	if test -n "$(UPD-ALT_PREFIX)"; then \
+		sed -i -e '/^[ 	]*update-alternatives /s|update-alternatives|$(UPD-ALT_PREFIX)/bin/&|' \
+			$(BZIP2_IPK_DIR)-links/CONTROL/postinst $(BZIP2_IPK_DIR)-links/CONTROL/prerm; \
+	fi
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(BZIP2_IPK_DIR)
 
 bzip2-ipk: bzip2-stage $(BZIP2_IPK)
