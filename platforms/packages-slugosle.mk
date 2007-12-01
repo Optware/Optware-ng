a82c8c8d4575dfce10e7c7b4534bcedb4e045eba
Brian Zhou <bzhou@users.noreply.github.com>
2007-12-01 23:03:48 +0000

    new target platform per request of ka6sox

---
+++ platforms/packages-slugosle.mk
@@ -0,0 +1,22 @@
+# Packages that *only* work for slugosbe - do not just put new packages here.
+SPECIFIC_PACKAGES = \
+	ipkg-opt \
+	$(PERL_PACKAGES) \
+	$(PACKAGES_REQUIRE_LINUX26) \
+
+# Packages that do not work for slugosbe.
+# puppy: usb_io.h:33:23: error: linux/usb.h: No such file or directory
+# heyu: xwrite.c:34:30: error: linux/serial_reg.h: No such file or directory
+# iptraf: sys/types.h and linux/types.h conflicting, the ipk for unslung seems to work though
+BROKEN_PACKAGES = \
+	$(PACKAGES_ONLY_WORK_ON_LINUX24) \
+	heyu \
+	iptraf \
+	ldconfig \
+	monotone \
+	netatalk \
+	puppy \
+	py-psycopg \
+	qemu \
+	ushare \
+
