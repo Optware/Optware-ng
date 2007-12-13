a749297b19304aca13e8ce735398ed7e87a3cb6b
Brian Zhou <bzhou@users.noreply.github.com>
2007-12-13 17:13:31 +0000

    flex: 2.5.33 -> 2.5.34

--- make/flex.mk
+++ make/flex.mk
@@ -5,10 +5,9 @@
 ###########################################################
 
 FLEX_SITE=http://$(SOURCEFORGE_MIRROR)/sourceforge/flex
-FLEX_VERSION=2.5.33
-FLEX_LIB_VERSION=2.5.33
+FLEX_VERSION=2.5.34
 FLEX_SOURCE=flex-$(FLEX_VERSION).tar.bz2
-FLEX_DIR=flex-2.5.33
+FLEX_DIR=flex-$(FLEX_VERSION)
 FLEX_UNZIP=bzcat
 FLEX_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
 FLEX_DESCRIPTION=Generates programs that perform pattern-matching on text.
@@ -17,7 +16,7 @@ FLEX_PRIORITY=optional
 FLEX_DEPENDS=m4
 FLEX_CONFLICTS=
 
-FLEX_IPK_VERSION=4
+FLEX_IPK_VERSION=1
 
 FLEX_BUILD_DIR=$(BUILD_DIR)/flex
 FLEX_SOURCE_DIR=$(SOURCE_DIR)/flex
@@ -29,21 +28,25 @@ $(DL_DIR)/$(FLEX_SOURCE):
 
 flex-source: $(DL_DIR)/$(FLEX_SOURCE)
 
-$(FLEX_BUILD_DIR)/.configured: $(DL_DIR)/$(FLEX_SOURCE)
-	rm -rf $(BUILD_DIR)/$(FLEX_DIR) $(FLEX_BUILD_DIR)
+$(FLEX_BUILD_DIR)/.configured: $(DL_DIR)/$(FLEX_SOURCE) make/flex.mk
+	rm -rf $(BUILD_DIR)/$(FLEX_DIR) $(@D)
 	$(FLEX_UNZIP) $(DL_DIR)/$(FLEX_SOURCE) | tar -C $(BUILD_DIR) -xvf -
-	mv $(BUILD_DIR)/$(FLEX_DIR) $(FLEX_BUILD_DIR)
-	(cd $(FLEX_BUILD_DIR); \
+	mv $(BUILD_DIR)/$(FLEX_DIR) $(@D)
+	(cd $(@D); \
 		$(TARGET_CONFIGURE_OPTS) \
+		ac_cv_func_malloc_0_nonnull=yes \
+		ac_cv_func_realloc_0_nonnull=yes \
 		./configure \
 		--build=$(GNU_HOST_NAME) \
 		--host=$(GNU_TARGET_NAME) \
 		--target=$(GNU_TARGET_NAME) \
 		--prefix=/opt \
+		--enable-shared \
+		--disable-static \
+		--disable-nls \
 	)
-	sed -i -e 's|/usr/bin|/opt/bin|'  $(FLEX_BUILD_DIR)/config.h
-	sed -i -e 's|-I$${prefix}/include|-I$(STAGING_INCLUDE_DIR)/include|' $(FLEX_BUILD_DIR)/Makefile
-	touch $(FLEX_BUILD_DIR)/.configured
+	sed -i -e 's|/usr/bin|/opt/bin|'  $(@D)/config.h
+	touch $@
 
 flex-unpack: $(FLEX_BUILD_DIR)/.configured
 
@@ -51,9 +54,9 @@ flex-unpack: $(FLEX_BUILD_DIR)/.configured
 # This builds the actual binary.
 #
 $(FLEX_BUILD_DIR)/.built: $(FLEX_BUILD_DIR)/.configured
-	rm -f $(FLEX_BUILD_DIR)/.built
-	$(MAKE) -C $(FLEX_BUILD_DIR)
-	touch $(FLEX_BUILD_DIR)/.built
+	rm -f $@
+	$(MAKE) -C $(@D)
+	touch $@
 
 #
 # This is the build convenience target.
@@ -64,19 +67,19 @@ flex: $(FLEX_BUILD_DIR)/.built
 # If you are building a library, then you need to stage it too.
 #
 $(FLEX_BUILD_DIR)/.staged: $(FLEX_BUILD_DIR)/.built
-	rm -f $(FLEX_BUILD_DIR)/.staged
-	$(MAKE) -C $(FLEX_BUILD_DIR) DESTDIR=$(STAGING_DIR) install
+	rm -f $@
+	$(MAKE) -C $(@D) DESTDIR=$(STAGING_DIR) install
 ifneq ($(HOSTCC), $(TARGET_CC)) # prevent PATH=staging/opt/bin problems
 	if test -x $(STAGING_DIR)/opt/bin/flex ;\
 		 then rm $(STAGING_DIR)/opt/bin/flex ;\
 	fi
 endif
-	touch $(FLEX_BUILD_DIR)/.staged
+	touch $@
 
 flex-stage: $(FLEX_BUILD_DIR)/.staged
 
 $(FLEX_IPK_DIR)/CONTROL/control:
-	@install -d $(FLEX_IPK_DIR)/CONTROL
+	@install -d $(@D)
 	@rm -f $@
 	@echo "Package: flex" >>$@
 	@echo "Architecture: $(TARGET_ARCH)" >>$@
@@ -104,3 +107,5 @@ flex-clean:
 flex-dirclean:
 	rm -rf $(BUILD_DIR)/$(FLEX_DIR) $(FLEX_BUILD_DIR) $(FLEX_IPK_DIR) $(FLEX_IPK)
 
+flex-check: $(FLEX_IPK)
+	perl scripts/optware-check-package.pl --target=$(OPTWARE_TARGET) $(FLEX_IPK)
