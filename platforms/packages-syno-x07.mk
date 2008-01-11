bb171a1e0825ca9e7110c3cea49a981ea20dbffd
Brian Zhou <bzhou@users.noreply.github.com>
2008-01-11 21:43:11 +0000

    packages-syno-x07: first pass

---
+++ platforms/packages-syno-x07.mk
@@ -0,0 +1,29 @@
+SPECIFIC_PACKAGES = \
+	ipkg-opt \
+	$(PERL_PACKAGES) \
+	$(PACKAGES_REQUIRE_LINUX26) \
+
+LIBTIFF_PACKAGES = \
+	libtiff cups ghostscript imagemagick samba sane-backends spandsp \
+	gtk gutenprint \
+
+BROKEN_PACKAGES = \
+	asterisk asterisk14 asterisk14-chan-capi \
+	busybox \
+	chillispot classpath \
+	fuppes \
+	gambit-c gconv-modules gnu-smalltalk \
+	gift giftcurs gift-ares gift-fasttrack gift-gnutella gift-openft gift-opennap \
+	icecast iptraf ivorbis-tools \
+	ldconfig libcapi20 libextractor \
+	libshout libvorbis \
+	moc modutils \
+	monotone mt-daapd \
+	ncftp net-snmp ntop \
+	openser p7zip \
+	player puppy \
+	qemu sablevm \
+	socat \
+	streamripper transcode uemacs \
+	vlc vorbis-tools vte \
+	xchat \
