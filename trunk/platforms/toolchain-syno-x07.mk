1044454851545dcf5a5d82c0dccd17b379c0cf02
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2008-01-11 06:28:58 +0000

    syno-x07: added kernel headers

--- trunk/platforms/toolchain-syno-x07.mk
+++ trunk/platforms/toolchain-syno-x07.mk
@@ -20,10 +20,18 @@ TARGET_CFLAGS=$(TARGET_OPTIMIZATION) $(TARGET_DEBUGGING) $(TARGET_CUSTOM_FLAGS)
 TOOLCHAIN_BINARY_SITE=http://download.synology.com/toolchain
 TOOLCHAIN_BINARY=gcc343_glibc232_88f5281.tbz
 
+TOOLCHAIN_KERNEL_SITE=ftp://ftp.kernel.org/pub/linux/kernel/v2.6
+TOOLCHAIN_KERNEL_VERSION=2.6.15
+TOOLCHAIN_KERNEL_SOURCE=linux-$(TOOLCHAIN_KERNEL_VERSION).tar.bz2
+
 toolchain: $(TARGET_CROSS_TOP)/.unpacked
 
 $(DL_DIR)/$(TOOLCHAIN_BINARY):
 	$(WGET) -P $(DL_DIR) $(TOOLCHAIN_BINARY_SITE)/$(@F) || \
+	$(WGET) -P $(DL_DIR) $(SOURCES_NLO_SITE)/$(@F)
+
+$(DL_DIR)/$(TOOLCHAIN_KERNEL_SOURCE):
+	$(WGET) -P $(DL_DIR) $(TOOLCHAIN_KERNEL_SITE)/$(@F) || \
 	$(WGET) -P $(DL_DIR) $(SOURCES_NLO_SITE)/$(@F)
 
 $(TARGET_CROSS_TOP)/.unpacked: $(DL_DIR)/$(TOOLCHAIN_BINARY) # $(OPTWARE_TOP)/platforms/toolchain-$(OPTWARE_TARGET).mk
@@ -32,8 +40,9 @@ $(TARGET_CROSS_TOP)/.unpacked: $(DL_DIR)/$(TOOLCHAIN_BINARY) # $(OPTWARE_TOP)/pl
 	tar -xj -C $(BASE_DIR)/toolchain -f $(DL_DIR)/$(TOOLCHAIN_BINARY)
 	mv $(BASE_DIR)/toolchain/usr/local/$(GNU_TARGET_NAME)/* $(@D)
 	rm -rf $(BASE_DIR)/toolchain/usr
+	tar -xj -C $(BASE_DIR)/toolchain -f $(DL_DIR)/$(TOOLCHAIN_KERNEL_SOURCE)
+	ln -s $(BASE_DIR)/toolchain/linux-$(TOOLCHAIN_KERNEL_VERSION)/include/linux $(TARGET_INCDIR)/
+	ln -s $(BASE_DIR)/toolchain/linux-$(TOOLCHAIN_KERNEL_VERSION)/include/asm-arm $(TARGET_INCDIR)/asm
+	ln -s $(BASE_DIR)/toolchain/linux-$(TOOLCHAIN_KERNEL_VERSION)/include/asm-generic $(TARGET_INCDIR)/
+	cp $(OPTWARE_TOP)/sources/toolchain-$(OPTWARE_TARGET)/autoconf.h $(BASE_DIR)/toolchain/linux-$(TOOLCHAIN_KERNEL_VERSION)/include/linux/
 	touch $@
-
-# ln -s toolchain/linux-2.6.15/include/linux $(TARGET_INCDIR)/
-# ln -s toolchain/linux-2.6.15/include/asm-arm $(TARGET_INCDIR)/asm
-# ln -s toolchain/linux-2.6.15/include/asm-generic $(TARGET_INCDIR)/
