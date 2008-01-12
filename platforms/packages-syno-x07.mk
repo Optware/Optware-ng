63f4931a5fb45330a187673ce3f5f57df1803b6f
Brian Zhou <bzhou@users.noreply.github.com>
2008-01-12 02:01:38 +0000

    packages-syno-x07: promoted a few more that depend on libvorbis

--- platforms/packages-syno-x07.mk
+++ platforms/packages-syno-x07.mk
@@ -4,15 +4,14 @@ SPECIFIC_PACKAGES = \
 	$(PACKAGES_REQUIRE_LINUX26) \
 
 BROKEN_PACKAGES = \
+	$(PACKAGES_ONLY_WORK_ON_LINUX24) \
 	asterisk asterisk14 asterisk14-chan-capi \
 	busybox \
 	chillispot classpath \
 	gambit-c gconv-modules gnu-smalltalk \
 	gift giftcurs gift-ares gift-fasttrack gift-gnutella gift-openft gift-opennap \
-	icecast iptraf \
+	iptraf \
 	ldconfig libcapi20 libextractor \
-	libshout \
-	moc modutils \
 	monotone \
 	ncftp net-snmp ntop \
 	openser p7zip \
