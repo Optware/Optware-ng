9e611f02280cfeacce6c5eb5b3985aee17854fb5
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2008-01-16 19:05:25 +0000

    py-bazaar-ng: 1.0 -> 1.1

--- trunk/make/py-bazaar-ng.mk
+++ trunk/make/py-bazaar-ng.mk
@@ -21,7 +21,7 @@
 # from your name or email address.  If you leave MAINTAINER set to
 # "NSLU2 Linux" other developers will feel free to edit.
 #
-PY-BAZAAR-NG_VERSION=1.0
+PY-BAZAAR-NG_VERSION=1.1
 PY-BAZAAR-NG_SITE=http://bazaar-vcs.org/releases/src
 PY-BAZAAR-NG_SOURCE=bzr-$(PY-BAZAAR-NG_VERSION).tar.gz
 PY-BAZAAR-NG_DIR=bzr-$(PY-BAZAAR-NG_VERSION)
