f0e6f34d187c2fe3c2cc93b89b02b2f88a78e8ab
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2008-01-19 08:43:26 +0000

    Initial starting point for vt4

---
+++ trunk/platforms/packages-vt4.mk
@@ -0,0 +1,7 @@
+# Packages that *only* work for vt4 - do not just put new packages here.
+SPECIFIC_PACKAGES = \
+
+#	vt4-optware-bootstrap
+
+# Packages that do not work for vt4.
+BROKEN_PACKAGES = \
