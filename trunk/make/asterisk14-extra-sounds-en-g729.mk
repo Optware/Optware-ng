218eedc2398fa259b32fd13637ed3e9a87ecd609
osas <osas@1f1dca69-5516-0410-b834-e485effd04c3>
2008-01-23 14:24:46 +0000

    asterisk14-extra-sounds-en: 1.4.6 -> 1.4.7

--- trunk/make/asterisk14-extra-sounds-en-g729.mk
+++ trunk/make/asterisk14-extra-sounds-en-g729.mk
@@ -21,7 +21,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 ASTERISK14_EXTRA_SOUNDS_EN_G729_SITE=http://downloads.digium.com/pub/telephony/sounds/releases
-ASTERISK14_EXTRA_SOUNDS_EN_G729_VERSION=1.4.6
+ASTERISK14_EXTRA_SOUNDS_EN_G729_VERSION=1.4.7
 ASTERISK14_EXTRA_SOUNDS_EN_G729_SOURCE=asterisk-extra-sounds-en-g729-$(ASTERISK14_EXTRA_SOUNDS_EN_G729_VERSION).tar.gz
 ASTERISK14_EXTRA_SOUNDS_EN_G729_DIR=asterisk-extra-sounds-en-g729-$(ASTERISK14_EXTRA_SOUNDS_EN_G729_VERSION)
 ASTERISK14_EXTRA_SOUNDS_EN_G729_UNZIP=zcat
@@ -175,12 +175,8 @@ $(ASTERISK14_EXTRA_SOUNDS_EN_G729_IPK): $(ASTERISK14_EXTRA_SOUNDS_EN_G729_BUILD_
 	$(MAKE) $(ASTERISK14_EXTRA_SOUNDS_EN_G729_IPK_DIR)/CONTROL/control
 	install -d $(ASTERISK14_EXTRA_SOUNDS_EN_G729_IPK_DIR)/opt/var/lib/asterisk/sounds
 	install -m 644 $(ASTERISK14_EXTRA_SOUNDS_EN_G729_BUILD_DIR)/*.g729 $(ASTERISK14_EXTRA_SOUNDS_EN_G729_IPK_DIR)/opt/var/lib/asterisk/sounds
-	rm $(ASTERISK14_EXTRA_SOUNDS_EN_G729_IPK_DIR)/opt/var/lib/asterisk/sounds/conf-thereare.g729
-	rm $(ASTERISK14_EXTRA_SOUNDS_EN_G729_IPK_DIR)/opt/var/lib/asterisk/sounds/invalid.g729
 	install -d $(ASTERISK14_EXTRA_SOUNDS_EN_G729_IPK_DIR)/opt/var/lib/asterisk/sounds/ha
 	install -m 644 $(ASTERISK14_EXTRA_SOUNDS_EN_G729_BUILD_DIR)/ha/*.g729 $(ASTERISK14_EXTRA_SOUNDS_EN_G729_IPK_DIR)/opt/var/lib/asterisk/sounds/ha
-	install -d $(ASTERISK14_EXTRA_SOUNDS_EN_G729_IPK_DIR)/opt/var/lib/asterisk/sounds/phonetic
-	install -m 644 $(ASTERISK14_EXTRA_SOUNDS_EN_G729_BUILD_DIR)/phonetic/*.g729 $(ASTERISK14_EXTRA_SOUNDS_EN_G729_IPK_DIR)/opt/var/lib/asterisk/sounds/phonetic
 	install -d $(ASTERISK14_EXTRA_SOUNDS_EN_G729_IPK_DIR)/opt/var/lib/asterisk/sounds/wx
 	install -m 644 $(ASTERISK14_EXTRA_SOUNDS_EN_G729_BUILD_DIR)/wx/*.g729 $(ASTERISK14_EXTRA_SOUNDS_EN_G729_IPK_DIR)/opt/var/lib/asterisk/sounds/wx
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(ASTERISK14_EXTRA_SOUNDS_EN_G729_IPK_DIR)
