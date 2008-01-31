c82aad404bedbb1c4ed77d56fcd9c5afb5d8496f
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2008-01-31 01:27:08 +0000

    Enable vt4-optware-bootstrap

--- platforms/packages-vt4.mk
+++ platforms/packages-vt4.mk
@@ -1,7 +1,6 @@
 # Packages that *only* work for vt4 - do not just put new packages here.
 SPECIFIC_PACKAGES = \
-
-#	vt4-optware-bootstrap
+	vt4-optware-bootstrap
 
 # Packages that do not work for vt4.
 BROKEN_PACKAGES = \
