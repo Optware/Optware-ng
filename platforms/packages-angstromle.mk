ce8c7ce89aaa0a7c3f7937a5989ccdec79491d92
Brian Zhou <bzhou@users.noreply.github.com>
2008-02-01 22:50:01 +0000

    packages-angstromle: first pass

--- platforms/packages-angstromle.mk
+++ platforms/packages-angstromle.mk
@@ -7,3 +7,10 @@ SPECIFIC_PACKAGES = \
 # Packages that do not work for angstromle.
 BROKEN_PACKAGES = \
 	$(PACKAGES_ONLY_WORK_ON_LINUX24) \
+	asterisk asterisk14 asterisk14-chan-capi \
+	atftp bitchx bzflag \
+	chillispot coreutils gconv-modules ipac-ng \
+	iptables iptraf ivorbis-tools ldconfig madplay monotone mt-daapd \
+	netatalk nfs-utils nget phoneme-advanced player puppy py-psycopg \
+	qemu squeak util-linux \
+	atk gtk vte xchat
