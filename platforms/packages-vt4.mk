1d643a614cc0b8db462b0c4f68470ff09ccc7825
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2008-02-09 09:55:11 +0000

    Updated broken packages for vt4

--- platforms/packages-vt4.mk
+++ platforms/packages-vt4.mk
@@ -4,3 +4,38 @@ SPECIFIC_PACKAGES = \
 
 # Packages that do not work for vt4.
 BROKEN_PACKAGES = \
+	amule appweb apache aspell asterisk asterisk14 asterisk14-chan-capi \
+	atftp avahi \
+	bitlbee bsdgames btpd bzflag \
+	castget cdargs cherokee chillispot clamav ctorrent ctrlproxy cups \
+	dansguardian dspam \
+	eaccelerator emacs22 enhanced-ctorrent esniper erlang erl-yaws \
+	ficy finch firedrill-httptunnel flac flip freeradius fuppes \
+	gconv-modules ggrab ghostscript git gnokii gnupg \
+	gnuplot gnutls grep gtk gutenprint \
+	haproxy hplip \
+	icecast id3lib iksemel imagemagick iptraf ivorbis-tools \
+	jabberd jed jikes \
+	kismet \
+	launchtool ldconfig leafnode lftp liba52 libbt libcapi20 libcdio \
+	libcurl libdvb libextractor libmrss libnsl libnxml libopensync \
+	libpar2 libsigc++ libsoup libsndfile libstdc++ libtiff libtorrent \
+	lighttpd loudmouth \
+	mc mediatomb metalog moc modutils most motion mod-fastcgi moe \
+	monotone mp3blaster mpd mpdscribble mpop msmtp msynctool \
+	mysql mysql-connector-odbc mod-python mod-wsgi \
+	newsbeuter newt nget nginx nmap nload ntop nzbget \
+	open2300 obexftp openser oww \
+	p7zip par2cmdline pcapsipdump pcre php php-apache php-fcgi \
+	picolisp player postfix pound puppy py-curl py-mysql \
+	qemu quickie \
+	re2c rhtvision rrdcollect rsstail rtorrent \
+	sablevm samba sane-backends simh slang snort socat spandsp \
+	swi-prolog svn \
+	taglib tshark transcode \
+	unrar upslug2 upx \
+	vlc vnstat vorbis-tools vte \
+	weechat wget wput wxbase \
+	xmlrpc-c xchat \
+	yougrabber
+
