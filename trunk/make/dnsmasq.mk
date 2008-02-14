e61ff032310d5d8ed260d0eae7e128a64c72433a
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2008-02-14 16:04:39 +0000

    dnsmasq: 2.40 -> 2.41

--- trunk/make/dnsmasq.mk
+++ trunk/make/dnsmasq.mk
@@ -5,7 +5,7 @@
 #############################################################
 
 DNSMASQ_SITE=http://www.thekelleys.org.uk/dnsmasq
-DNSMASQ_VERSION=2.40
+DNSMASQ_VERSION=2.41
 DNSMASQ_SOURCE:=dnsmasq-$(DNSMASQ_VERSION).tar.gz
 DNSMASQ_DIR:=dnsmasq-$(DNSMASQ_VERSION)
 DNSMASQ_UNZIP=zcat
@@ -21,8 +21,7 @@ DNSMASQ_IPK_VERSION=1
 # DNSMASQ_CONFFILES should be a list of user-editable files
 DNSMASQ_CONFFILES=/opt/etc/dnsmasq.conf
 
-DNSMASQ_PATCHES=$(DNSMASQ_SOURCE_DIR)/conffile.patch \
-$(DNSMASQ_SOURCE_DIR)/src-dnsmasq.h.patch
+DNSMASQ_PATCHES=$(DNSMASQ_SOURCE_DIR)/conffile.patch $(DNSMASQ_SOURCE_DIR)/src-dnsmasq.h.patch
 
 DNSMASQ_BUILD_DIR=$(BUILD_DIR)/dnsmasq
 DNSMASQ_SOURCE_DIR=$(SOURCE_DIR)/dnsmasq
@@ -32,30 +31,33 @@ DNSMASQ_IPK=$(BUILD_DIR)/dnsmasq_$(DNSMASQ_VERSION)-$(DNSMASQ_IPK_VERSION)_$(TAR
 .PHONY: dnsmasq-source dnsmasq-unpack dnsmasq dnsmasq-stage dnsmasq-ipk dnsmasq-clean dnsmasq-dirclean dnsmasq-check
 
 $(DL_DIR)/$(DNSMASQ_SOURCE):
-	$(WGET) -P $(DL_DIR) $(DNSMASQ_SITE)/$(DNSMASQ_SOURCE)
+	$(WGET) -P $(DL_DIR) $(DNSMASQ_SITE)/$(@F) || \
+	$(WGET) -P $(DL_DIR) $(SOURCES_NLO_SITE)/$(@F)
 
 dnsmasq-source: $(DL_DIR)/$(DNSMASQ_SOURCE)
 
 $(DNSMASQ_BUILD_DIR)/.configured: $(DL_DIR)/$(DNSMASQ_SOURCE) $(DNSMASQ_PATCHES)
-	rm -rf $(BUILD_DIR)/$(DNSMASQ_DIR) $(DNSMASQ_BUILD_DIR)
+	rm -rf $(BUILD_DIR)/$(DNSMASQ_DIR) $(@D)
 	$(DNSMASQ_UNZIP) $(DL_DIR)/$(DNSMASQ_SOURCE) | tar -C $(BUILD_DIR) -xvf -
 	cat $(DNSMASQ_PATCHES) | patch -d $(BUILD_DIR)/$(DNSMASQ_DIR) -p0
-	mv $(BUILD_DIR)/$(DNSMASQ_DIR) $(DNSMASQ_BUILD_DIR)
-	touch $(DNSMASQ_BUILD_DIR)/.configured
+	mv $(BUILD_DIR)/$(DNSMASQ_DIR) $(@D)
+	touch $@
 
 dnsmasq-unpack: $(DNSMASQ_BUILD_DIR)/.configured
 
-$(DNSMASQ_BUILD_DIR)/src/dnsmasq: $(DNSMASQ_BUILD_DIR)/.configured
-	$(MAKE) -C $(DNSMASQ_BUILD_DIR) $(TARGET_CONFIGURE_OPTS)
+$(DNSMASQ_BUILD_DIR)/.built: $(DNSMASQ_BUILD_DIR)/.configured
+	rm -f $@
+	$(MAKE) -C $(@D) $(TARGET_CONFIGURE_OPTS)
+	touch $@
 
-dnsmasq: $(DNSMASQ_BUILD_DIR)/src/dnsmasq
+dnsmasq: $(DNSMASQ_BUILD_DIR)/.built
 
 #
 # This rule creates a control file for ipkg.  It is no longer
 # necessary to create a seperate control file under sources/dnsmasq
 #
 $(DNSMASQ_IPK_DIR)/CONTROL/control:
-	@install -d $(DNSMASQ_IPK_DIR)/CONTROL
+	@install -d $(@D)
 	@rm -f $@
 	@echo "Package: dnsmasq" >>$@
 	@echo "Architecture: $(TARGET_ARCH)" >>$@
@@ -68,7 +70,7 @@ $(DNSMASQ_IPK_DIR)/CONTROL/control:
 	@echo "Depends: $(DNSMASQ_DEPENDS)" >>$@
 	@echo "Conflicts: $(DNSMASQ_CONFLICTS)" >>$@
 
-$(DNSMASQ_IPK): $(DNSMASQ_BUILD_DIR)/src/dnsmasq
+$(DNSMASQ_IPK): $(DNSMASQ_BUILD_DIR)/.built
 	rm -rf $(DNSMASQ_IPK_DIR) $(BUILD_DIR)/dnsmasq_*_$(TARGET_ARCH).ipk
 	install -d $(DNSMASQ_IPK_DIR)/opt/sbin $(DNSMASQ_IPK_DIR)/opt/etc/init.d
 	$(STRIP_COMMAND) $(DNSMASQ_BUILD_DIR)/src/dnsmasq -o $(DNSMASQ_IPK_DIR)/opt/sbin/dnsmasq
