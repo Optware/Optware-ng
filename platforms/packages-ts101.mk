f54780562cb7c13383a2099b8ce3b4eeb0b178ed
Brian Zhou <bzhou@users.noreply.github.com>
2008-02-17 20:44:33 +0000

    chillispot: promoted on gumstix1151, syno-x07, and ts101

--- platforms/packages-ts101.mk
+++ platforms/packages-ts101.mk
@@ -7,7 +7,7 @@ SPECIFIC_PACKAGES = \
 
 BROKEN_PACKAGES = \
 	$(filter-out libstdc++ newsbeuter, $(UCLIBC_BROKEN_PACKAGES)) \
-	amule chillispot ecl erl-escript erl-yaws ficy \
+	amule ecl erl-escript erl-yaws ficy \
 	gift giftcurs \
 	gift-ares gift-fasttrack gift-gnutella \
 	gift-openft gift-opennap \
