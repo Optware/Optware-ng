0ea622087acc710ee439cadf44b71e1bc6c34208
dyoung <dyoung@1f1dca69-5516-0410-b834-e485effd04c3>
2008-02-17 03:15:32 +0000

    Add optware for angstrom big endian setup files

---
+++ trunk/platforms/packages-angstrombe.mk
@@ -0,0 +1,17 @@
+# Packages that *only* work for angstrombe - do not just put new packages here.
+SPECIFIC_PACKAGES = \
+	ipkg-opt \
+	$(PERL_PACKAGES) \
+	$(PACKAGES_REQUIRE_LINUX26) \
+
+# Packages that do not work for angstrombe.
+BROKEN_PACKAGES = \
+	$(PACKAGES_ONLY_WORK_ON_LINUX24) 
+#	$(PACKAGES_ONLY_WORK_ON_LINUX24) \
+#	asterisk asterisk14 asterisk14-chan-capi \
+#	atftp bitchx bzflag \
+#	chillispot coreutils gconv-modules ipac-ng \
+#	iptables iptraf ivorbis-tools ldconfig madplay monotone mt-daapd \
+#	netatalk nfs-utils nget phoneme-advanced player puppy \
+#	qemu squeak \
+#	atk gtk vte xchat
