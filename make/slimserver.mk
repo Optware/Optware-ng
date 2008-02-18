8480efc7c05002013c4b992e46037ead4bb8bc23
oleo <oleo@1f1dca69-5516-0410-b834-e485effd04c3>
2008-02-18 07:31:32 +0000

    slimserver: install and remove fixes

--- make/slimserver.mk
+++ make/slimserver.mk
@@ -46,7 +46,7 @@ endif
 #
 # SLIMSERVER_IPK_VERSION should be incremented when the ipk changes.
 #
-SLIMSERVER_IPK_VERSION=9
+SLIMSERVER_IPK_VERSION=10
 
 #
 # SLIMSERVER_CONFFILES should be a list of user-editable files
