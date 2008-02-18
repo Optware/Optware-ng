2454c7d06447e6eee86164aea3bf53bf8ab12ff1
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2008-02-18 07:34:04 +0000

    coreutils: promoted on angstrom?e

--- trunk/platforms/packages-angstromle.mk
+++ trunk/platforms/packages-angstromle.mk
@@ -9,7 +9,7 @@ BROKEN_PACKAGES = \
 	$(PACKAGES_ONLY_WORK_ON_LINUX24) \
 	asterisk asterisk14 asterisk14-chan-capi \
 	atftp bitchx bzflag \
-	coreutils gconv-modules ipac-ng \
+	gconv-modules ipac-ng \
 	iptables iptraf ivorbis-tools ldconfig madplay monotone mt-daapd \
 	netatalk nfs-utils nget phoneme-advanced player puppy \
 	qemu squeak \
