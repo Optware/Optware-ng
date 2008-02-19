4b3c6094c7676662a35f6a5dbad75337dc1dac54
dyoung <dyoung@1f1dca69-5516-0410-b834-e485effd04c3>
2008-02-19 03:18:39 +0000

    Still plowing through whole packages list, adding more as it proceeds...

--- trunk/platforms/packages-angstrombe.mk
+++ trunk/platforms/packages-angstrombe.mk
@@ -9,10 +9,10 @@ BROKEN_PACKAGES = \
 	$(PACKAGES_ONLY_WORK_ON_LINUX24) \
 	antinat asterisk asterisk14 asterisk14-chan-capi \
 	atftp atk bitchx bzflag \
-	cabextract chillispot connect \
-	gconv-modules gtk \
-	ipac-ng iptables iptraf ivorbis-tools \
-	ldconfig madplay monotone mt-daapd \
-	netatalk nget \
+	cabextract chillispot connect coreutils dircproxy \
+	ecl eggdrop fcgi gconv-modules gtk \
+	inadyn ipac-ng iptables iptraf ivorbis-tools \
+	ldconfig libcapi20 madplay microcom monotone mt-daapd \
+	netatalk nfs-utils nget ntop \
 	phoneme-advanced player puppy \
 	qemu squeak vte xchat 
