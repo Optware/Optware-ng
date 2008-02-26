cdf0f7b7d1734061948689b0fa5707a4927a4d6f
dyoung <dyoung@1f1dca69-5516-0410-b834-e485effd04c3>
2008-02-26 23:33:06 +0000

    updated post python-2.5 fix

--- trunk/platforms/packages-ts72xx.mk
+++ trunk/platforms/packages-ts72xx.mk
@@ -6,13 +6,6 @@ BROKEN_PACKAGES = \
 	asterisk \
 	ldconfig \
 	mod-python \
-	py-4suite py-apsw py-axiom py-bazaar-ng py-bittorrent py-bluez \
-	py-celementtree py-cheetah py-cjson py-clips py-crypto \
-	py-curl py-duplicity py-epsilon py-gdchart2 py-gd py-lxml \
-	py-mantissa py-mercurial py-mssql py-mx-base py-mysql \
-	py-nevow py-openssl py-pil py-pgsql \
-	py-psycopg py-psycopg2 py-pygresql py-quixote py-rdiff-backup \
-	py-reportlab py-scgi py-silvercity py-simplejson py-twisted \
-	py-urwid py-usb py-xml py-yenc py-zope-interface \
-	qemu qemu-libc-i386 
-	
+	py-bazaar-ng py-simplejson \
+	qemu qemu-libc-i386 slrn spandsp speex # vte xchat
+
