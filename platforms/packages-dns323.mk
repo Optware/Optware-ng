a02668cb14ab73521692c6101d4d33fce7685fe8
Brian Zhou <bzhou@users.noreply.github.com>
2008-03-08 19:34:12 +0000

    packages-dns323: promoted bash, php*, openssh, util-linux-ng after toolchain symlink fix

--- platforms/packages-dns323.mk
+++ platforms/packages-dns323.mk
@@ -6,19 +6,20 @@ SPECIFIC_PACKAGES = \
 
 BROKEN_PACKAGES = \
 	$(PACKAGES_ONLY_WORK_ON_LINUX24) \
-	9base appweb asterisk asterisk14 asterisk14-chan-capi asterisk16 asterisk16-addons \
-	bash btpd castget dspam eaccelerator fcgi \
+	9base appweb \
+	asterisk asterisk14 asterisk14-chan-capi asterisk16 asterisk16-addons \
+	btpd castget dspam fcgi \
 	ficy fish freeradius fuppes ghostscript gnu-smalltalk gpsd \
 	gtmess gtk ice id3lib ipac-ng iptraf kismet \
 	launchtool ldconfig liba52 lighttpd mcabber mediatomb monotone \
-	mt-daapd mtr mysql-connector-odbc nagios-plugins nail net-snmp nfs-server \
-	nfs-utils nrpe ntop openser openssh pango pcapsipdump \
+	mt-daapd mtr mysql-connector-odbc nagios-plugins \
+	nail net-snmp nfs-server nfs-utils \
+	nrpe ntop openser pango pcapsipdump \
 	phoneme-advanced \
-	php php-apache php-thttpd \
 	picolisp privoxy hplip py-mysql \
 	qemu quagga \
 	rssh sablevm simh sm srelay tcpwrappers \
-	transcode tz uemacs util-linux util-linux-ng vlc vnstat \
+	transcode tz uemacs util-linux vlc vnstat \
 	vte xauth xaw xchat xmu xt xterm \
 	slimserver \
 
