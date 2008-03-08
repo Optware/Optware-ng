22c083741c51b2e4877dbcd7d30f41c3f96d90f4
Brian Zhou <bzhou@users.noreply.github.com>
2008-03-08 21:01:00 +0000

    promoted more packages with toolchain-dns323 symlink fix

--- platforms/packages-dns323.mk
+++ platforms/packages-dns323.mk
@@ -7,19 +7,17 @@ SPECIFIC_PACKAGES = \
 BROKEN_PACKAGES = \
 	$(PACKAGES_ONLY_WORK_ON_LINUX24) \
 	9base appweb \
-	asterisk asterisk14 asterisk14-chan-capi asterisk16 asterisk16-addons \
-	btpd castget dspam fcgi \
+	castget dspam fcgi \
 	ficy fish freeradius fuppes ghostscript gnu-smalltalk gpsd \
 	gtmess gtk ice id3lib ipac-ng iptraf kismet \
 	launchtool ldconfig liba52 lighttpd mcabber mediatomb monotone \
-	mt-daapd mtr mysql-connector-odbc nagios-plugins \
-	nail net-snmp nfs-server nfs-utils \
+	mtr mysql-connector-odbc nagios-plugins \
+	nfs-server nfs-utils \
 	nrpe ntop openser pango pcapsipdump \
-	phoneme-advanced \
 	picolisp privoxy hplip py-mysql \
 	qemu quagga \
-	rssh sablevm simh sm srelay tcpwrappers \
-	transcode tz uemacs util-linux vlc vnstat \
+	rssh sablevm simh sm srelay \
+	transcode uemacs util-linux vlc vnstat \
 	vte xauth xaw xchat xmu xt xterm \
 	slimserver \
 
