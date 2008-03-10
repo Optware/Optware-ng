08d8138818a9655c13a63f41c51c78780d3e8b7a
Brian Zhou <bzhou@users.noreply.github.com>
2008-03-10 02:04:31 +0000

    packages-dns323: promoted bunch of packages

--- platforms/packages-dns323.mk
+++ platforms/packages-dns323.mk
@@ -7,16 +7,16 @@ SPECIFIC_PACKAGES = \
 BROKEN_PACKAGES = \
 	$(PACKAGES_ONLY_WORK_ON_LINUX24) \
 	9base appweb \
-	castget dspam fcgi \
-	ficy fish freeradius fuppes ghostscript gnu-smalltalk gpsd \
+	castget fcgi \
+	ficy fish freeradius fuppes gnu-smalltalk \
 	gtmess gtk ice id3lib ipac-ng iptraf kismet \
-	launchtool ldconfig liba52 lighttpd mcabber mediatomb monotone \
-	mtr mysql-connector-odbc nagios-plugins \
+	launchtool ldconfig liba52 lighttpd mediatomb monotone \
+	mtr mysql-connector-odbc \
 	nfs-server nfs-utils \
-	nrpe ntop openser pango pcapsipdump \
-	picolisp privoxy hplip py-mysql \
+	ntop openser pango pcapsipdump \
+	py-mysql \
 	qemu quagga \
-	rssh sablevm simh sm srelay \
+	rssh sablevm sm \
 	transcode uemacs util-linux vlc vnstat \
 	vte xauth xaw xchat xmu xt xterm \
 	slimserver \
