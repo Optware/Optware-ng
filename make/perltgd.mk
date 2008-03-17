df76b8d3412e3a0664516ad8f284b9220e54939c
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2008-03-17 03:39:06 +0000

    perltgd: Updated to betaM

--- make/perltgd.mk
+++ make/perltgd.mk
@@ -27,7 +27,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 PERLTGD_SITE=http://jim16.110mb.com/
-PERLTGD_VERSION=1.0BetaL
+PERLTGD_VERSION=1.0BetaM
 PERLTGD_SOURCE=perlTGDSlug-$(PERLTGD_VERSION).tar.gz
 PERLTGD_DIR=perltgd-$(PERLTGD_VERSION)
 PERLTGD_UNZIP=zcat
