41043aa3bb4dbe88a9a82e11a7a796dd6638876b
Brian Zhou <bzhou@users.noreply.github.com>
2008-03-21 18:02:02 +0000

    bzip2: 1.0.4 -> 1.0.5

--- make/bzip2.mk
+++ make/bzip2.mk
@@ -4,7 +4,7 @@
 #
 ###########################################################
 
-BZIP2_VERSION=1.0.4
+BZIP2_VERSION=1.0.5
 BZIP2_SITE=http://www.bzip.org/$(BZIP2_VERSION)
 BZIP2_LIB_VERSION=$(BZIP2_VERSION)
 BZIP2_SOURCE=bzip2-$(BZIP2_VERSION).tar.gz
@@ -17,7 +17,7 @@ BZIP2_PRIORITY=optional
 BZIP2_DEPENDS=
 BZIP2_CONFLICTS=
 
-BZIP2_IPK_VERSION=3
+BZIP2_IPK_VERSION=1
 
 BZIP2_BUILD_DIR=$(BUILD_DIR)/bzip2
 BZIP2_SOURCE_DIR=$(SOURCE_DIR)/bzip2
@@ -27,7 +27,8 @@ BZIP2_IPK_DIR=$(BUILD_DIR)/bzip2-$(BZIP2_VERSION)-ipk
 .PHONY: bzip2-source bzip2-unpack bzip2 bzip2-stage bzip2-ipk bzip2-clean bzip2-dirclean bzip2-check
 
 $(DL_DIR)/$(BZIP2_SOURCE):
-	$(WGET) -P $(DL_DIR) $(BZIP2_SITE)/$(BZIP2_SOURCE)
+	$(WGET) -P $(@D) $(BZIP2_SITE)/$(@F) || \
+	$(WGET) -P $(@D) $(SOURCES_NLO_SITE)/$(@F)
 
 bzip2-source: $(DL_DIR)/$(BZIP2_SOURCE)
 
@@ -35,7 +36,7 @@ $(BZIP2_BUILD_DIR)/.configured: $(DL_DIR)/$(BZIP2_SOURCE)
 	rm -rf $(BUILD_DIR)/$(BZIP2_DIR) $(@D)
 	$(BZIP2_UNZIP) $(DL_DIR)/$(BZIP2_SOURCE) | tar -C $(BUILD_DIR) -xvf -
 	mv $(BUILD_DIR)/$(BZIP2_DIR) $(@D)
-	sed -i -e 's/^CFLAGS *=/&$$(CPPFLAGS) /' $(@D)/Makefile*
+	sed -i -e 's/^CFLAGS *=/&$$(CPPFLAGS) /; s|1.0.4|$(BZIP2_VERSION)|' $(@D)/Makefile*
 	touch $@
 
 bzip2-unpack: $(BZIP2_BUILD_DIR)/.configured
@@ -65,7 +66,7 @@ $(BZIP2_BUILD_DIR)/.staged: $(BZIP2_BUILD_DIR)/.built
 	install -m 644 $(BZIP2_BUILD_DIR)/libbz2.a $(STAGING_LIB_DIR)
 	install -m 644 $(BZIP2_BUILD_DIR)/libbz2.so.$(BZIP2_LIB_VERSION) $(STAGING_LIB_DIR)
 	cd $(STAGING_DIR)/opt/lib && ln -fs libbz2.so.$(BZIP2_LIB_VERSION) libbz2.so.1.0
-	cd $(STAGING_DIR)/opt/lib && ln -fs libbz2.so.$(BZIP2_LIB_VERSION) libbz2.so
+	cd $(STAGING_DIR)/opt/lib && ln -fs libbz2.so.1.0 libbz2.so
 	touch $@
 
 bzip2-stage: $(BZIP2_BUILD_DIR)/.staged
@@ -98,7 +99,7 @@ $(BZIP2_IPK): $(BZIP2_BUILD_DIR)/.built
 	install -d $(BZIP2_IPK_DIR)/opt/lib
 	install -m 644 $(BZIP2_BUILD_DIR)/libbz2.so.$(BZIP2_LIB_VERSION) $(BZIP2_IPK_DIR)/opt/lib
 	cd $(BZIP2_IPK_DIR)/opt/lib && ln -fs libbz2.so.$(BZIP2_LIB_VERSION) libbz2.so.1.0
-	cd $(BZIP2_IPK_DIR)/opt/lib && ln -fs libbz2.so.$(BZIP2_LIB_VERSION) libbz2.so
+	cd $(BZIP2_IPK_DIR)/opt/lib && ln -fs libbz2.so.1.0 libbz2.so
 	$(STRIP_COMMAND) $(BZIP2_IPK_DIR)/opt/lib/libbz2.so.$(BZIP2_LIB_VERSION)
 	install -d $(BZIP2_IPK_DIR)/opt/doc/bzip2
 	install -m 644 $(BZIP2_BUILD_DIR)/manual*.html $(BZIP2_IPK_DIR)/opt/doc/bzip2
