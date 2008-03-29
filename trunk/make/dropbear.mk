e14501885aaf130cfbbfbe7d78e2e6b40164e1e2
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2008-03-29 04:48:22 +0000

    dropbear: 0.48.1 -> 0.51

--- trunk/make/dropbear.mk
+++ trunk/make/dropbear.mk
@@ -8,10 +8,10 @@
 #
 
 DROPBEAR_SITE=http://matt.ucc.asn.au/dropbear/releases
-DROPBEAR_VERSION=0.48.1
-DROPBEAR_SOURCE=dropbear-$(DROPBEAR_VERSION).tar.gz
+DROPBEAR_VERSION=0.51
+DROPBEAR_SOURCE=dropbear-$(DROPBEAR_VERSION).tar.bz2
 DROPBEAR_DIR=dropbear-$(DROPBEAR_VERSION)
-DROPBEAR_UNZIP=zcat
+DROPBEAR_UNZIP=bzcat
 DROPBEAR_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
 DROPBEAR_DESCRIPTION=Lightweight SSH client and server system
 DROPBEAR_SECTION=net
@@ -28,6 +28,8 @@ DROPBEAR_PATCHES=$(DROPBEAR_SOURCE_DIR)/configure.patch \
 		 $(DROPBEAR_SOURCE_DIR)/ssh-path.patch \
 		 $(DROPBEAR_SOURCE_DIR)/shell-path.patch
 
+DROPBEAR_CONFFILES=/opt/etc/default/dropbear /opt/etc/init.d/S51dropbear
+
 DROPBEAR_CPPFLAGS=
 DROPBEAR_LDFLAGS=
 
@@ -43,7 +45,8 @@ endif
 endif
 
 $(DL_DIR)/$(DROPBEAR_SOURCE):
-	$(WGET) -P $(DL_DIR) $(DROPBEAR_SITE)/$(DROPBEAR_SOURCE)
+	$(WGET) -P $(@D) $(DROPBEAR_SITE)/$(@F) || \
+	$(WGET) -P $(@D) $(SOURCES_NLO_SITE)/$(@F)
 
 dropbear-source: $(DL_DIR)/$(DROPBEAR_SOURCE) $(DROPBEAR_PATCHES)
 
@@ -53,8 +56,10 @@ $(DROPBEAR_BUILD_DIR)/.configured: $(DL_DIR)/$(DROPBEAR_SOURCE) $(DROPBEAR_PATCH
 	cat $(DROPBEAR_PATCHES) | patch -d $(BUILD_DIR)/$(DROPBEAR_DIR) -p1
 	mv $(BUILD_DIR)/$(DROPBEAR_DIR) $(DROPBEAR_BUILD_DIR)
 	(cd $(DROPBEAR_BUILD_DIR) && \
+		autoreconf && \
 		$(TARGET_CONFIGURE_OPTS) \
 		CFLAGS="$(TARGET_CFLAGS)" LD="" \
+		LDFLAGS="$(STAGING_LDFLAGS) $(DROPBEAR_LDFLAGS)" \
 		./configure \
 		--build=$(GNU_HOST_NAME) \
 		--host=$(GNU_TARGET_NAME) \
@@ -66,15 +71,15 @@ $(DROPBEAR_BUILD_DIR)/.configured: $(DL_DIR)/$(DROPBEAR_SOURCE) $(DROPBEAR_PATCH
 		--disable-wtmpx --disable-libutil \
 		$(DROPBEAR_DISABLE_SHADOW) \
 	)
-	touch $(DROPBEAR_BUILD_DIR)/.configured
+	touch $@
 
 dropbear-unpack: $(DROPBEAR_BUILD_DIR)/.configured
 
 $(DROPBEAR_BUILD_DIR)/.built: $(DROPBEAR_BUILD_DIR)/.configured
-	rm -f $(DROPBEAR_BUILD_DIR)/.built
-	$(MAKE) -C $(DROPBEAR_BUILD_DIR) MULTI=1 SCPPROGRESS=1 \
+	rm -f $@
+	$(MAKE) -C $(@D) MULTI=1 SCPPROGRESS=1 \
 		PROGRAMS="dropbear dropbearkey dropbearconvert dbclient ssh scp"
-	touch $(DROPBEAR_BUILD_DIR)/.built
+	touch $@
 
 dropbear: $(DROPBEAR_BUILD_DIR)/.built
 
@@ -83,7 +88,7 @@ dropbear: $(DROPBEAR_BUILD_DIR)/.built
 # necessary to create a seperate control file under sources/dropbear
 #
 $(DROPBEAR_IPK_DIR)/CONTROL/control:
-	@install -d $(DROPBEAR_IPK_DIR)/CONTROL
+	@install -d $(@D)
 	@rm -f $@
 	@echo "Package: dropbear" >>$@
 	@echo "Architecture: $(TARGET_ARCH)" >>$@
@@ -105,13 +110,16 @@ $(DROPBEAR_IPK): $(DROPBEAR_BUILD_DIR)/.built
 	cd $(DROPBEAR_IPK_DIR)/opt/sbin && ln -sf dropbearmulti dropbearkey
 	cd $(DROPBEAR_IPK_DIR)/opt/sbin && ln -sf dropbearmulti dropbearconvert
 	cd $(DROPBEAR_IPK_DIR)/opt/bin && ln -sf ../sbin/dropbearmulti dbclient
-	cd $(DROPBEAR_IPK_DIR)/opt/bin && ln -sf ../sbin/dropbearmulti ssh
-	cd $(DROPBEAR_IPK_DIR)/opt/bin && ln -sf ../sbin/dropbearmulti scp
+#	cd $(DROPBEAR_IPK_DIR)/opt/bin && ln -sf ../sbin/dropbearmulti ssh
+#	cd $(DROPBEAR_IPK_DIR)/opt/bin && ln -sf ../sbin/dropbearmulti scp
 	install -d $(DROPBEAR_IPK_DIR)/opt/etc/init.d
 	install -m 755 $(DROPBEAR_SOURCE_DIR)/rc.dropbear $(DROPBEAR_IPK_DIR)/opt/etc/init.d/S51dropbear
+	install -d $(DROPBEAR_IPK_DIR)/opt/etc/default
+	install -m 755 $(DROPBEAR_SOURCE_DIR)/dropbear.default $(DROPBEAR_IPK_DIR)/opt/etc/default/dropbear
 	$(MAKE) $(DROPBEAR_IPK_DIR)/CONTROL/control
 	install -m 644 $(DROPBEAR_SOURCE_DIR)/postinst $(DROPBEAR_IPK_DIR)/CONTROL/postinst
 	install -m 644 $(DROPBEAR_SOURCE_DIR)/prerm    $(DROPBEAR_IPK_DIR)/CONTROL/prerm
+	echo $(DROPBEAR_CONFFILES) | sed -e 's/ /\n/g' > $(DROPBEAR_IPK_DIR)/CONTROL/conffiles
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(DROPBEAR_IPK_DIR)
 
 dropbear-ipk: $(DROPBEAR_IPK)
@@ -121,3 +129,9 @@ dropbear-clean:
 
 dropbear-dirclean:
 	rm -rf $(BUILD_DIR)/$(DROPBEAR_DIR) $(DROPBEAR_BUILD_DIR) $(DROPBEAR_IPK_DIR) $(DROPBEAR_IPK)
+
+#
+# Some sanity check for the package.
+#
+dropbear-check: $(DROPBEAR_IPK)
+	perl scripts/optware-check-package.pl --target=$(OPTWARE_TARGET) $(DROPBEAR_IPK)
