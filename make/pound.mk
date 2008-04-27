339846eb2747499a93fa6fd21a4e881a55ab5312
Brian Zhou <bzhou@users.noreply.github.com>
2008-04-27 15:19:34 +0000

    pound: 2.4 -> 2.4.1

--- make/pound.mk
+++ make/pound.mk
@@ -21,7 +21,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 POUND_SITE=http://www.apsis.ch/pound
-POUND_VERSION=2.4
+POUND_VERSION=2.4.1
 POUND_SOURCE=Pound-$(POUND_VERSION).tgz
 POUND_DIR=Pound-$(POUND_VERSION)
 POUND_UNZIP=zcat
