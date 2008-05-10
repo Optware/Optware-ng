bfa9ea47040ae62723f758c47d8ea9f68cf44096
Brian Zhou <bzhou@users.noreply.github.com>
2008-05-10 18:07:58 +0000

    libtorrent: 0.12.1 -> 0.12.2; rtorrent: 0.8.1 -> 0.8.2

--- make/rtorrent.mk
+++ make/rtorrent.mk
@@ -16,7 +16,7 @@ RTORRENT_SITE=http://libtorrent.rakshasa.no/downloads
 
 RTORRENT_VERSION=$(strip \
 	$(if $(filter gumstix1151 openwrt-brcm24, $(OPTWARE_TARGET)), 0.8.0, \
-	0.8.1))
+	0.8.2))
 RTORRENT_IPK_VERSION=1
 
 RTORRENT_SVN=svn://rakshasa.no/libtorrent/trunk/rtorrent
