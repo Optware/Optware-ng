c36ce179ea522d9484ae7c13d36208402cf72542
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2008-05-22 03:00:25 +0000

    tsx09: platform specific packages

---
+++ trunk/platforms/packages-tsx09.mk
@@ -0,0 +1,7 @@
+# Packages that *only* work for tsx09 - do not just put new packages here.
+SPECIFIC_PACKAGES = \
+	tsx09-optware-bootstrap tsx09-kernel-modules
+
+PACKAGES = ${SPECIFIC_PACKAGES}
+
+# The rest of the packages for the TS-x09 come from the cs05q3armel feed.
