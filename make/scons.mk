5063e6e5ffb82f00935fb8c7f3161436ef9f012f
Brian Zhou <bzhou@users.noreply.github.com>
2008-05-30 06:00:50 +0000

    scons: 0.98.1 -> 0.98.4

--- make/scons.mk
+++ make/scons.mk
@@ -27,7 +27,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 SCONS_SITE=http://$(SOURCEFORGE_MIRROR)/sourceforge/scons
-SCONS_VERSION=0.98.1
+SCONS_VERSION=0.98.4
 SCONS_SOURCE=scons-$(SCONS_VERSION).tar.gz
 SCONS_DIR=scons-$(SCONS_VERSION)
 SCONS_UNZIP=zcat
