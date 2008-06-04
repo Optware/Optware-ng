e1233475f5924171cc1049e53c58b1e0f9251460
Brian Zhou <bzhou@users.noreply.github.com>
2008-06-04 18:23:24 +0000

    cherokee: 0.7.0 -> 0.7.1

--- make/cherokee.mk
+++ make/cherokee.mk
@@ -21,7 +21,7 @@
 # from your name or email address.  If you leave MAINTAINER set to
 # "NSLU2 Linux" other developers will feel free to edit.
 #
-CHEROKEE_VERSION=0.7.0
+CHEROKEE_VERSION=0.7.1
 CHEROKEE_SITE=http://www.0x50.org/download/0.7/$(CHEROKEE_VERSION)
 CHEROKEE_SOURCE=cherokee-$(CHEROKEE_VERSION).tar.gz
 CHEROKEE_DIR=cherokee-$(CHEROKEE_VERSION)
@@ -39,7 +39,7 @@ CHEROKEE_CONFLICTS=
 #
 # CHEROKEE_IPK_VERSION should be incremented when the ipk changes.
 #
-CHEROKEE_IPK_VERSION=2
+CHEROKEE_IPK_VERSION=1
 
 #
 # CHEROKEE_CONFFILES should be a list of user-editable files
