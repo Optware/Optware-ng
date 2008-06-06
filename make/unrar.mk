34bc995466f15111107fe40e80f0875367b52806
Brian Zhou <bzhou@users.noreply.github.com>
2008-06-06 22:00:08 +0000

    unrar: 3.6.8 -> 3.7.8

--- make/unrar.mk
+++ make/unrar.mk
@@ -5,7 +5,7 @@
 #############################################################
 
 UNRAR_SITE=http://www.rarlab.com/rar
-UNRAR_VERSION:=3.6.8
+UNRAR_VERSION:=3.7.8
 UNRAR_SOURCE=unrarsrc-$(UNRAR_VERSION).tar.gz
 UNRAR_DIR=unrarsrc-$(UNRAR_VERSION)
 UNRAR_UNZIP=gunzip
@@ -16,7 +16,7 @@ UNRAR_PRIORITY=optional
 UNRAR_DEPENDS=libstdc++
 UNRAR_CONFLICTS=
 
-UNRAR_IPK_VERSION=2
+UNRAR_IPK_VERSION=1
 
 UNRAR_CFLAGS=$(TARGET_CFLAGS)
 
@@ -28,33 +28,36 @@ UNRAR_IPK=$(BUILD_DIR)/unrar_$(UNRAR_VERSION)-$(UNRAR_IPK_VERSION)_$(TARGET_ARCH
 .PHONY: unrar-source unrar-unpack unrar unrar-stage unrar-ipk unrar-clean unrar-dirclean unrar-check
 
 $(DL_DIR)/$(UNRAR_SOURCE):
-	$(WGET) -P $(DL_DIR) $(UNRAR_SITE)/$(UNRAR_SOURCE)
+	$(WGET) -P $(@D) $(UNRAR_SITE)/$(@F) || \
+	$(WGET) -P $(@D) $(SOURCES_NLO_SITE)/$(@F)
 
 unrar-source: $(DL_DIR)/$(UNRAR_SOURCE)
 
 $(UNRAR_BUILD_DIR)/.configured: $(DL_DIR)/$(UNRAR_SOURCE)
 	$(MAKE) libstdc++-stage
-	rm -rf $(BUILD_DIR)/$(UNRAR_DIR) $(UNRAR_BUILD_DIR)
+	rm -rf $(BUILD_DIR)/$(UNRAR_DIR) $(@D)
 	tar -C $(BUILD_DIR) -xzvf $(DL_DIR)/$(UNRAR_SOURCE)
-	ln $(UNRAR_BUILD_DIR)/makefile.unix $(UNRAR_BUILD_DIR)/Makefile
-	touch $(UNRAR_BUILD_DIR)/.configured
+	ln $(@D)/makefile.unix $(@D)/Makefile
+	touch $@
 
 unrar-unpack: $(UNRAR_BUILD_DIR)/.configured
 
-$(UNRAR_BUILD_DIR)/unrar: $(UNRAR_BUILD_DIR)/.configured
+$(UNRAR_BUILD_DIR)/.built: $(UNRAR_BUILD_DIR)/.configured
+	rm -f $@
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) \
 		CFLAGS="$(UNRAR_CFLAGS)" CXXFLAGS="$(UNRAR_CFLAGS)" \
-		-C $(UNRAR_BUILD_DIR) \
+		-C $(@D) \
 		LDFLAGS="$(STAGING_LDFLAGS)"
+	touch $@
 
-unrar: $(UNRAR_BUILD_DIR)/unrar
+unrar: $(UNRAR_BUILD_DIR)/.built
 
 #
 # This rule creates a control file for ipkg.  It is no longer
 # necessary to create a seperate control file under sources/nylon
 #
 $(UNRAR_IPK_DIR)/CONTROL/control:
-	@install -d $(UNRAR_IPK_DIR)/CONTROL
+	@install -d $(@D)
 	@rm -f $@
 	@echo "Package: unrar" >>$@
 	@echo "Architecture: $(TARGET_ARCH)" >>$@
@@ -67,7 +70,7 @@ $(UNRAR_IPK_DIR)/CONTROL/control:
 	@echo "Depends: $(UNRAR_DEPENDS)" >>$@
 	@echo "Conflicts: $(UNRAR_CONFLICTS)" >>$@
 
-$(UNRAR_IPK): $(UNRAR_BUILD_DIR)/unrar
+$(UNRAR_IPK): $(UNRAR_BUILD_DIR)/.built
 	rm -rf $(UNRAR_IPK_DIR) $(BUILD_DIR)/unrar_*_$(TARGET_ARCH).ipk
 	install -d $(UNRAR_IPK_DIR)/opt/bin
 	install -m 755 $(UNRAR_BUILD_DIR)/unrar $(UNRAR_IPK_DIR)/opt/bin
