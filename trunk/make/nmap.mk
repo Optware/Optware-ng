d6833662cf02e00d67e11bc17dc725a2458d0c6f
engy <engy@1f1dca69-5516-0410-b834-e485effd04c3>
2008-06-08 18:06:12 +0000

    nmap: 4.62->4.65

--- trunk/make/nmap.mk
+++ trunk/make/nmap.mk
@@ -7,7 +7,7 @@
 # $Header$
 #
 NMAP_SITE=http://download.insecure.org/nmap/dist
-NMAP_VERSION=4.62
+NMAP_VERSION=4.65
 NMAP_SOURCE=nmap-$(NMAP_VERSION).tar.bz2
 NMAP_DIR=nmap-$(NMAP_VERSION)
 NMAP_UNZIP=bzcat
