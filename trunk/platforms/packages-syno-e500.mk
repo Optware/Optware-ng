2db75b1ad4e101a446bc6a6d355da116bf3cbd26
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2008-06-24 18:11:24 +0000

    packages-syno-e500: first pass

--- trunk/platforms/packages-syno-e500.mk
+++ trunk/platforms/packages-syno-e500.mk
@@ -1,6 +1,15 @@
 SPECIFIC_PACKAGES = \
 	ipkg-opt \
 	$(PACKAGES_REQUIRE_LINUX26) \
+	py-ctypes \
+	$(PERL_PACKAGES) \
 
 BROKEN_PACKAGES = \
 	$(PACKAGES_ONLY_WORK_ON_LINUX24) \
+	asterisk14 asterisk14-chan-capi asterisk16 asterisk16-addons \
+	atftp \
+	finch flac gconv-modules gnuplot iptraf \
+	ldconfig libcapi20 mediatomb moc monotone mpd openser \
+	pciutils procps puppy ser socat transcode vlc \
+	vsftpd x264 lm-sensors \
+	slimserver \
