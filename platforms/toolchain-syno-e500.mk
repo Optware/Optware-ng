fc2af8bcba6b343f5132745ebf885aff40688cc4
Brian Zhou <bzhou@users.noreply.github.com>
2008-07-08 17:10:55 +0000

    toolchain-syno-e500: cleaned up TARGET_INCDIR, setup for native devel

--- platforms/toolchain-syno-e500.mk
+++ platforms/toolchain-syno-e500.mk
@@ -5,9 +5,25 @@ LIBC_STYLE=glibc
 LIBSTDC++_VERSION=6.0.3
 LIBNSL_VERSION=2.3.4
 
+GNU_TARGET_NAME = powerpc-linux-gnuspe
+
+ifeq (ppc, $(HOST_MACHINE))
+
+HOSTCC = $(TARGET_CC)
+GNU_HOST_NAME = $(GNU_TARGET_NAME)
+TARGET_CROSS = /opt/bin/
+TARGET_LIBDIR = /opt/lib
+TARGET_INCDIR = /opt/include
+TARGET_LDFLAGS = -L/opt/lib
+TARGET_CUSTOM_FLAGS= -O2 -pipe
+TARGET_CFLAGS= -I/opt/include $(TARGET_OPTIMIZATION) $(TARGET_DEBUGGING) $(TARGET_CUSTOM_FLAGS)
+
+toolchain:
+
+else
+
 HOSTCC = gcc
 GNU_HOST_NAME = $(HOST_MACHINE)-pc-linux-gnu
-GNU_TARGET_NAME = powerpc-linux-gnuspe
 TARGET_CROSS_TOP = $(BASE_DIR)/toolchain/gcc-3.4.3-glibc-2.3.4
 TARGET_CROSS = $(TARGET_CROSS_TOP)/$(GNU_TARGET_NAME)/bin/$(GNU_TARGET_NAME)-
 TARGET_LIBDIR = $(TARGET_CROSS_TOP)/$(GNU_TARGET_NAME)/lib
@@ -17,7 +33,7 @@ TARGET_LDFLAGS =
 TARGET_CUSTOM_FLAGS= -O2 -pipe
 TARGET_CFLAGS=$(TARGET_OPTIMIZATION) $(TARGET_DEBUGGING) $(TARGET_CUSTOM_FLAGS)
 
-#STAGING_CPPFLAGS+= -DPATH_MAX=4096 -DLINE_MAX=2048 -DMB_LEN_MAX=16
+NATIVE_GCC_VERSION=3.4.6
 
 TOOLCHAIN_BINARY_SITE=http://download.synology.com/toolchain
 TOOLCHAIN_BINARY=gcc343_glibc234_854x.tar.gz
@@ -30,8 +46,13 @@ $(DL_DIR)/$(TOOLCHAIN_BINARY):
 
 $(TARGET_CROSS_TOP)/.unpacked: \
 $(DL_DIR)/$(TOOLCHAIN_BINARY) \
-# $(OPTWARE_TOP)/platforms/toolchain-$(OPTWARE_TARGET).mk
+$(OPTWARE_TOP)/platforms/toolchain-$(OPTWARE_TARGET).mk
 	rm -rf $(@D)
 	mkdir -p $(@D)
 	tar -xz -C $(@D) -f $(DL_DIR)/$(TOOLCHAIN_BINARY)
+	cd $(TARGET_INCDIR); \
+	rm -rf ext2fs et mtd security; \
+	rm -rf `find . -newer stdio.h -a ! -newer pam_client.h`
 	touch $@
+
+endif
