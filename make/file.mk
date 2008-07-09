715d31d748235e4c3b8f27cafb14868f59636745
Brian Zhou <bzhou@users.noreply.github.com>
2008-07-09 02:31:30 +0000

    file: 4.24 -> 4.25

--- make/file.mk
+++ make/file.mk
@@ -20,7 +20,7 @@
 # You should change all these variables to suit your package.
 #
 FILE_SITE=ftp://ftp.astron.com/pub/file
-FILE_VERSION=4.24
+FILE_VERSION=4.25
 FILE_SOURCE=file-$(FILE_VERSION).tar.gz
 FILE_DIR=file-$(FILE_VERSION)
 FILE_UNZIP=zcat
