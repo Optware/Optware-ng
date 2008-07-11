53f9fc5a494cc37eb0895eca9d773ab18586b73e
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2008-07-11 20:02:30 +0000

    tshark: 1.0.1 -> 1.0.2

--- trunk/make/tshark.mk
+++ trunk/make/tshark.mk
@@ -27,7 +27,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 TSHARK_SITE=http://www.wireshark.org/download/src
-TSHARK_VERSION=1.0.1
+TSHARK_VERSION=1.0.2
 TSHARK_SOURCE=wireshark-$(TSHARK_VERSION).tar.bz2
 TSHARK_DIR=wireshark-$(TSHARK_VERSION)
 TSHARK_UNZIP=bzcat
