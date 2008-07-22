7e0bfd1017196c125ce8dc0244fde21755ea5721
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2008-07-22 21:38:44 +0000

    openssh: 5.0p1 -> 5.1p1

--- trunk/make/openssh.mk
+++ trunk/make/openssh.mk
@@ -5,7 +5,7 @@
 #########################################################
 
 OPENSSH_SITE=ftp://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable
-OPENSSH_VERSION=5.0p1
+OPENSSH_VERSION=5.1p1
 OPENSSH_SOURCE=openssh-$(OPENSSH_VERSION).tar.gz
 OPENSSH_DIR=openssh-$(OPENSSH_VERSION)
 OPENSSH_UNZIP=zcat
