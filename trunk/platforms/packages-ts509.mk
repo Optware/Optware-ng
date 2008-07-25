8464d9cbae5cc1c2b0b134241e247ab87a3e04d5
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2008-07-25 16:31:51 +0000

    packages-ts509: 1st pass

--- trunk/platforms/packages-ts509.mk
+++ trunk/platforms/packages-ts509.mk
@@ -5,3 +5,6 @@ SPECIFIC_PACKAGES = \
 
 BROKEN_PACKAGES = \
 	$(PACKAGES_ONLY_WORK_ON_LINUX24) \
+	asterisk bitchx bzflag gnupg iptables ivorbis-tools lcd4linux \
+	ldconfig ltrace nget openser phoneme-advanced proftpd puppy \
+	mod-python py-curl py-reportlab qemu ser taged transcode \
