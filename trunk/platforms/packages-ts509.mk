e22f42ac1c2066e9308a90413bf309700f08d943
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2008-07-26 01:39:16 +0000

    nget: fixed build under gcc 4.2 or 4.3

--- trunk/platforms/packages-ts509.mk
+++ trunk/platforms/packages-ts509.mk
@@ -6,5 +6,5 @@ SPECIFIC_PACKAGES = \
 BROKEN_PACKAGES = \
 	$(PACKAGES_ONLY_WORK_ON_LINUX24) \
 	asterisk bitchx bzflag iptables ivorbis-tools lcd4linux \
-	ldconfig nget phoneme-advanced puppy \
+	ldconfig phoneme-advanced puppy \
 	mod-python py-reportlab qemu taged transcode \
