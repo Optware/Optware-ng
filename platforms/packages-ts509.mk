4bf97fd129290e82b69040064c78949b93e3bbfb
Brian Zhou <bzhou@users.noreply.github.com>
2008-07-26 16:48:57 +0000

    packages-ts509: py-reportlab build fixed

--- platforms/packages-ts509.mk
+++ platforms/packages-ts509.mk
@@ -8,4 +8,4 @@ BROKEN_PACKAGES = \
 	$(PACKAGES_ONLY_WORK_ON_LINUX24) \
 	asterisk bitchx bzflag iptables ivorbis-tools lcd4linux \
 	ldconfig phoneme-advanced puppy \
-	mod-python py-reportlab qemu taged transcode \
+	mod-python qemu taged transcode \
