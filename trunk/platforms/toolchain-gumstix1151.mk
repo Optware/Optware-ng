3f9be7afa754b09caa8e0ea4a5df93289020014c
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2008-08-12 18:57:13 +0000

    toolchain-gumstix1151: added username/password in svn co

--- trunk/platforms/toolchain-gumstix1151.mk
+++ trunk/platforms/toolchain-gumstix1151.mk
@@ -52,7 +52,7 @@ toolchain: $(TARGET_CROSS)gcc
 $(TARGET_CROSS)gcc: # $(OPTWARE_TOP)/platforms/toolchain-gumstix1151.mk
 	cd toolchain; \
 	rm -rf gumstix-buildroot; \
-	svn co -r1151 http://svn.gumstix.com/gumstix-buildroot/trunk gumstix-buildroot
+	svn co -r1151 --username root --password root http://svn.gumstix.com/gumstix-buildroot/trunk gumstix-buildroot
 	$(MAKE) -C toolchain/gumstix-buildroot defconfig DL_DIR=$(DL_DIR)
 	sed -i.orig \
 	    -e '/BR2_INSTALL_LIBSTDCPP/s/^.*/BR2_INSTALL_LIBSTDCPP=y/' \
