5842f21991bf5986f961221e42aa18b2edeb0540
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2008-08-21 23:42:34 +0000

    transmission: 1.33b1 -> 1.33

--- trunk/make/transmission.mk
+++ trunk/make/transmission.mk
@@ -25,7 +25,7 @@
 # SVN releases also include transmissiond-dbg while official releases does not.
 #
 TRANSMISSION_SITE=http://download.transmissionbt.com/transmission/files
-TRANSMISSION_VERSION=1.33b1
+TRANSMISSION_VERSION=1.33
 #TRANSMISSION_SVN=svn://svn.transmissionbt.com/Transmission/trunk
 #TRANSMISSION_SVN_REV=6245
 ifdef TRANSMISSION_SVN_REV
@@ -46,7 +46,7 @@ TRANSMISSION_CONFLICTS=
 #
 # TRANSMISSION_IPK_VERSION should be incremented when the ipk changes.
 #
-TRANSMISSION_IPK_VERSION=2
+TRANSMISSION_IPK_VERSION=1
 
 #
 # TRANSMISSION_CONFFILES should be a list of user-editable files
