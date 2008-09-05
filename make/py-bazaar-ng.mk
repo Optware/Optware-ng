9fca9d14ad981cadf1d592b308824a5e868683d5
Brian Zhou <bzhou@users.noreply.github.com>
2008-09-05 23:58:02 +0000

    bzr: 1.6 -> 1.6.1

--- make/py-bazaar-ng.mk
+++ make/py-bazaar-ng.mk
@@ -21,8 +21,8 @@
 # from your name or email address.  If you leave MAINTAINER set to
 # "NSLU2 Linux" other developers will feel free to edit.
 #
-PY-BAZAAR-NG_VERSION=1.6
-PY-BAZAAR-NG_SITE=https://launchpad.net/bzr/$(PY-BAZAAR-NG_VERSION)/$(PY-BAZAAR-NG_VERSION)/+download
+PY-BAZAAR-NG_VERSION=1.6.1
+PY-BAZAAR-NG_SITE=https://launchpad.net/bzr/1.6/$(PY-BAZAAR-NG_VERSION)/+download
 PY-BAZAAR-NG_SOURCE=bzr-$(PY-BAZAAR-NG_VERSION).tar.gz
 PY-BAZAAR-NG_DIR=bzr-$(PY-BAZAAR-NG_VERSION)
 PY-BAZAAR-NG_UNZIP=zcat
