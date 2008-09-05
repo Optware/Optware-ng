13372d929e9d9802e6b1dfe16bfd32e24af67c3b
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2008-09-05 00:56:06 +0000

    clamav: Create user as a system user and system group

--- trunk/make/clamav.mk
+++ trunk/make/clamav.mk
@@ -42,7 +42,7 @@ CLAMAV_CONFLICTS=
 #
 # CLAMAV_IPK_VERSION should be incremented when the ipk changes.
 #
-CLAMAV_IPK_VERSION=1
+CLAMAV_IPK_VERSION=2
 
 #
 # CLAMAV_CONFFILES should be a list of user-editable files
