d4ad9ce44fad34b2ebb901e86d9184835793b190
Brian Zhou <bzhou@users.noreply.github.com>
2008-09-17 18:54:29 +0000

    nginx: 0.7.14 -> 0.7.17

--- make/nginx.mk
+++ make/nginx.mk
@@ -21,7 +21,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 NGINX_SITE=http://sysoev.ru/nginx
-NGINX_VERSION=0.7.14
+NGINX_VERSION=0.7.17
 NGINX_SOURCE=nginx-$(NGINX_VERSION).tar.gz
 NGINX_DIR=nginx-$(NGINX_VERSION)
 NGINX_UNZIP=zcat
@@ -36,7 +36,7 @@ NGINX_CONFLICTS=
 #
 # NGINX_IPK_VERSION should be incremented when the ipk changes.
 #
-NGINX_IPK_VERSION=2
+NGINX_IPK_VERSION=1
 
 #
 # NGINX_CONFFILES should be a list of user-editable files
