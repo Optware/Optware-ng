90187bea3c920913e81f61f6e5f0e425f8f442b7
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2008-09-27 16:23:10 +0000

    openssl: 0.9.8h -> 0.9.8i on 3 platforms

--- trunk/make/openssl.mk
+++ trunk/make/openssl.mk
@@ -5,7 +5,7 @@
 OPENSSL_SITE=http://www.openssl.org/source
 
 ifeq ($(OPTWARE_TARGET), $(filter syno-e500 cs08q1armel ts509, $(OPTWARE_TARGET)))
-OPENSSL_VERSION=0.9.8h
+OPENSSL_VERSION=0.9.8i
 OPENSSL_LIB_VERSION=0.9.8
 OPENSSL_IPK_VERSION=1
 else
