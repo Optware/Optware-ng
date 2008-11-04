f7bca447a395e6a1b11c12f6893a4a97b11596bd
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2008-11-04 05:01:58 +0000

    tree: 1.5.1.2 if uclibc

--- trunk/make/tree.mk
+++ trunk/make/tree.mk
@@ -21,7 +21,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 TREE_SITE=ftp://mama.indstate.edu/linux/tree
-TREE_VERSION=$(if $(filter uclibc, $(LIBC_STYLE)), 1.5.1.1, 1.5.2.1)
+TREE_VERSION=$(strip $(if $(filter uclibc, $(LIBC_STYLE)), 1.5.1.2, 1.5.2.1))
 TREE_SOURCE=tree-$(TREE_VERSION).tgz
 TREE_DIR=tree-$(TREE_VERSION)
 TREE_UNZIP=zcat
@@ -77,6 +77,7 @@ TREE_IPK=$(BUILD_DIR)/tree_$(TREE_VERSION)-$(TREE_IPK_VERSION)_$(TARGET_ARCH).ip
 #
 $(DL_DIR)/$(TREE_SOURCE):
 	$(WGET) -P $(@D) $(TREE_SITE)/$(@F) || \
+	$(WGET) -P $(@D) $(TREE_SITE)/old/$(@F) || \
 	$(WGET) -P $(@D) $(SOURCES_NLO_SITE)/$(@F)
 
 #
