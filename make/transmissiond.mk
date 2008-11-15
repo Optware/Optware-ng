1395d179828961fba4ecb30b34bf7bd07d3ee318
oleo <oleo@1f1dca69-5516-0410-b834-e485effd04c3>
2008-11-15 21:22:38 +0000

    transmissiond: sync with 1.40

--- make/transmissiond.mk
+++ make/transmissiond.mk
@@ -25,9 +25,9 @@
 # SVN releases also include transmissiond-dbg while official releases does not.
 #
 TRANSMISSIOND_SITE=http://download.transmissionbt.com/transmission/files
-TRANSMISSIOND_VERSION=1.34
+TRANSMISSIOND_VERSION=1.40
 TRANSMISSIOND_SVN=svn://svn.transmissionbt.com/Transmission/trunk
-#TRANSMISSIOND_SVN_REV=6401
+#TRANSMISSIOND_SVN_REV=7069
 ifdef TRANSMISSIOND_SVN_REV
 TRANSMISSIOND_SOURCE=transmission-svn-$(TRANSMISSIOND_SVN_REV).tar.bz2
 else
