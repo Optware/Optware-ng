0b880fbb3f2df1ea59ce4b2a9d1dcf0b3d4e21f2
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2008-11-25 14:32:49 +0000

    Some wrp400 broken packages

---
+++ trunk/platforms/packages-wrp400.mk
@@ -0,0 +1,17 @@
+# Packages that *only* work for wrp400 - do not just put new packages here.
+SPECIFIC_PACKAGES = \
+
+
+# Packages that do not work for wrp400.
+BROKEN_PACKAGES = \
+	9base \
+	antinat \
+	aria2 \
+	arpwatch \
+	asterisk asterisk16 asterisk16-addons \
+	atk \
+	avahi \
+	bash \
+	gdbm apr-util apache \
+        glib bitlbee castget ctrlproxy finch fuppes giftcurs gnet gnokii gtk irssi libopensync libsoup mc mcabber ncmpc pal pango syslog-ng tshark you grabber \
+	libdb appweb bogofilter cyrus-imapd mutt openldap perl-berkeleydb perl-db-file php-fcgi php postfix python24 python25 python26 sendmail xemacs \
