018a332b7fb3f01d7d9364f0f9a9a9d78f87532a
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2009-01-05 17:45:54 +0000

    py-pylons: 0.9.6.1 -> 0.9.6.2, (py24,py25) -> (py25,py26)

--- trunk/make/py-pylons.mk
+++ trunk/make/py-pylons.mk
@@ -21,8 +21,8 @@
 # from your name or email address.  If you leave MAINTAINER set to
 # "NSLU2 Linux" other developers will feel free to edit.
 #
-PY-PYLONS_SITE=http://cheeseshop.python.org/packages/source/P/Pylons
-PY-PYLONS_VERSION=0.9.6.1
+PY-PYLONS_SITE=http://pypi.python.org/packages/source/P/Pylons
+PY-PYLONS_VERSION=0.9.6.2
 PY-PYLONS_SOURCE=Pylons-$(PY-PYLONS_VERSION).tar.gz
 PY-PYLONS_DIR=Pylons-$(PY-PYLONS_VERSION)
 PY-PYLONS_UNZIP=zcat
@@ -32,39 +32,37 @@ PY-PYLONS_SECTION=web
 PY-PYLONS_PRIORITY=optional
 PY-PYLONS_CONFLICTS=
 
-PY24-PYLONS_DEPENDS=\
-	py24-beaker (>=0.7.5), \
-	py24-decorator (>=2.1.0),
-	py24-formencode (>=0.7),
-	py24-mako (>=0.1.8), \
-	py24-myghty (>=1.1), \
-	py24-nose (>=0.9.3), \
-	py24-paste (>=1.4), \
-	py24-pastedeploy (>=1.3.1), \
-	py24-pastescript (>=1.3.6), \
-	py24-routes (>=1.7), \
-	py24-simplejson (>=1.7.1), \
-	py24-webhelpers (>=0.3.2)
-
 PY25-PYLONS_DEPENDS=\
-	py25-beaker (>=0.7.5), \
+	py25-beaker (>=0.8.1), \
 	py25-decorator (>=2.1.0),
 	py25-formencode (>=0.7),
 	py25-mako (>=0.1.8), \
-	py25-myghty (>=1.1), \
 	py25-nose (>=0.9.3), \
 	py25-paste (>=1.4), \
 	py25-pastedeploy (>=1.3.1), \
 	py25-pastescript (>=1.3.6), \
-	py25-routes (>=1.7), \
+	py25-routes (>=1.7.3), \
 	py25-simplejson (>=1.7.1), \
 	py25-webhelpers (>=0.3.2)
+
+PY26-PYLONS_DEPENDS=\
+	py26-beaker (>=0.8.1), \
+	py26-decorator (>=2.1.0),
+	py26-formencode (>=0.7),
+	py26-mako (>=0.1.8), \
+	py26-nose (>=0.9.3), \
+	py26-paste (>=1.4), \
+	py26-pastedeploy (>=1.3.1), \
+	py26-pastescript (>=1.3.6), \
+	py26-routes (>=1.7.3), \
+	py26-simplejson (>=1.7.1), \
+	py26-webhelpers (>=0.3.2)
 
 
 #
 # PY-PYLONS_IPK_VERSION should be incremented when the ipk changes.
 #
-PY-PYLONS_IPK_VERSION=2
+PY-PYLONS_IPK_VERSION=1
 
 #
 # PY-PYLONS_CONFFILES should be a list of user-editable files
@@ -95,11 +93,11 @@ PY-PYLONS_LDFLAGS=
 PY-PYLONS_BUILD_DIR=$(BUILD_DIR)/py-pylons
 PY-PYLONS_SOURCE_DIR=$(SOURCE_DIR)/py-pylons
 
-PY24-PYLONS_IPK_DIR=$(BUILD_DIR)/py24-pylons-$(PY-PYLONS_VERSION)-ipk
-PY24-PYLONS_IPK=$(BUILD_DIR)/py24-pylons_$(PY-PYLONS_VERSION)-$(PY-PYLONS_IPK_VERSION)_$(TARGET_ARCH).ipk
-
 PY25-PYLONS_IPK_DIR=$(BUILD_DIR)/py25-pylons-$(PY-PYLONS_VERSION)-ipk
 PY25-PYLONS_IPK=$(BUILD_DIR)/py25-pylons_$(PY-PYLONS_VERSION)-$(PY-PYLONS_IPK_VERSION)_$(TARGET_ARCH).ipk
+
+PY26-PYLONS_IPK_DIR=$(BUILD_DIR)/py26-pylons-$(PY-PYLONS_VERSION)-ipk
+PY26-PYLONS_IPK=$(BUILD_DIR)/py26-pylons_$(PY-PYLONS_VERSION)-$(PY-PYLONS_IPK_VERSION)_$(TARGET_ARCH).ipk
 
 .PHONY: py-pylons-source py-pylons-unpack py-pylons py-pylons-stage py-pylons-ipk py-pylons-clean py-pylons-dirclean py-pylons-check
 
@@ -108,7 +106,8 @@ PY25-PYLONS_IPK=$(BUILD_DIR)/py25-pylons_$(PY-PYLONS_VERSION)-$(PY-PYLONS_IPK_VE
 # then it will be fetched from the site using wget.
 #
 $(DL_DIR)/$(PY-PYLONS_SOURCE):
-	$(WGET) -P $(DL_DIR) $(PY-PYLONS_SITE)/$(PY-PYLONS_SOURCE)
+	$(WGET) -P $(@D) $(PY-PYLONS_SITE)/$(@F) || \
+	$(WGET) -P $(@D) $(SOURCES_NLO_SITE)/$(@F)
 
 #
 # The source code depends on it existing within the download directory.
@@ -134,33 +133,16 @@ py-pylons-source: $(DL_DIR)/$(PY-PYLONS_SOURCE) $(PY-PYLONS_PATCHES)
 #
 $(PY-PYLONS_BUILD_DIR)/.configured: $(DL_DIR)/$(PY-PYLONS_SOURCE) $(PY-PYLONS_PATCHES)
 	$(MAKE) py-setuptools-stage
-	rm -rf $(PY-PYLONS_BUILD_DIR)
-	mkdir -p $(PY-PYLONS_BUILD_DIR)
-	# 2.4
-	rm -rf $(BUILD_DIR)/$(PY-PYLONS_DIR)
-	$(PY-PYLONS_UNZIP) $(DL_DIR)/$(PY-PYLONS_SOURCE) | tar -C $(BUILD_DIR) -xvf -
-	if test -n "$(PY-PYLONS_PATCHES)"; \
-		then cat $(PY-PYLONS_PATCHES) | patch -d $(BUILD_DIR)/$(PY-PYLONS_DIR) -p1; \
-	fi
-	mv $(BUILD_DIR)/$(PY-PYLONS_DIR) $(PY-PYLONS_BUILD_DIR)/2.4
-	(cd $(PY-PYLONS_BUILD_DIR)/2.4; \
-	    ( \
-		echo "[build_ext]"; \
-	        echo "include-dirs=$(STAGING_INCLUDE_DIR):$(STAGING_INCLUDE_DIR)/python2.4"; \
-	        echo "library-dirs=$(STAGING_LIB_DIR)"; \
-	        echo "rpath=/opt/lib"; \
-		echo "[build_scripts]"; \
-		echo "executable=/opt/bin/python2.4" \
-	    ) >> setup.cfg; \
-	)
+	rm -rf $(@D)
+	mkdir -p $(@D)
 	# 2.5
 	rm -rf $(BUILD_DIR)/$(PY-PYLONS_DIR)
 	$(PY-PYLONS_UNZIP) $(DL_DIR)/$(PY-PYLONS_SOURCE) | tar -C $(BUILD_DIR) -xvf -
 	if test -n "$(PY-PYLONS_PATCHES)"; \
 		then cat $(PY-PYLONS_PATCHES) | patch -d $(BUILD_DIR)/$(PY-PYLONS_DIR) -p1; \
 	fi
-	mv $(BUILD_DIR)/$(PY-PYLONS_DIR) $(PY-PYLONS_BUILD_DIR)/2.5
-	(cd $(PY-PYLONS_BUILD_DIR)/2.5; \
+	mv $(BUILD_DIR)/$(PY-PYLONS_DIR) $(@D)/2.5
+	(cd $(@D)/2.5; \
 	    ( \
 		echo "[build_ext]"; \
 	        echo "include-dirs=$(STAGING_INCLUDE_DIR):$(STAGING_INCLUDE_DIR)/python2.5"; \
@@ -168,6 +150,23 @@ $(PY-PYLONS_BUILD_DIR)/.configured: $(DL_DIR)/$(PY-PYLONS_SOURCE) $(PY-PYLONS_PA
 	        echo "rpath=/opt/lib"; \
 		echo "[build_scripts]"; \
 		echo "executable=/opt/bin/python2.5" \
+	    ) >> setup.cfg; \
+	)
+	# 2.6
+	rm -rf $(BUILD_DIR)/$(PY-PYLONS_DIR)
+	$(PY-PYLONS_UNZIP) $(DL_DIR)/$(PY-PYLONS_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+	if test -n "$(PY-PYLONS_PATCHES)"; \
+		then cat $(PY-PYLONS_PATCHES) | patch -d $(BUILD_DIR)/$(PY-PYLONS_DIR) -p1; \
+	fi
+	mv $(BUILD_DIR)/$(PY-PYLONS_DIR) $(@D)/2.6
+	(cd $(@D)/2.6; \
+	    ( \
+		echo "[build_ext]"; \
+	        echo "include-dirs=$(STAGING_INCLUDE_DIR):$(STAGING_INCLUDE_DIR)/python2.6"; \
+	        echo "library-dirs=$(STAGING_LIB_DIR)"; \
+	        echo "rpath=/opt/lib"; \
+		echo "[build_scripts]"; \
+		echo "executable=/opt/bin/python2.6" \
 	    ) >> setup.cfg; \
 	)
 	touch $@
@@ -179,14 +178,14 @@ py-pylons-unpack: $(PY-PYLONS_BUILD_DIR)/.configured
 #
 $(PY-PYLONS_BUILD_DIR)/.built: $(PY-PYLONS_BUILD_DIR)/.configured
 	rm -f $@
-	cd $(PY-PYLONS_BUILD_DIR)/2.4; \
-	    PYTHONPATH=$(STAGING_LIB_DIR)/python2.4/site-packages \
-	    CC='$(TARGET_CC)' LDSHARED='$(TARGET_CC) -shared' \
-	    $(HOST_STAGING_PREFIX)/bin/python2.4 setup.py build
-	cd $(PY-PYLONS_BUILD_DIR)/2.5; \
+	cd $(@D)/2.5; \
 	    PYTHONPATH=$(STAGING_LIB_DIR)/python2.5/site-packages \
 	    CC='$(TARGET_CC)' LDSHARED='$(TARGET_CC) -shared' \
 	    $(HOST_STAGING_PREFIX)/bin/python2.5 setup.py build
+	cd $(@D)/2.6; \
+	    PYTHONPATH=$(STAGING_LIB_DIR)/python2.6/site-packages \
+	    CC='$(TARGET_CC)' LDSHARED='$(TARGET_CC) -shared' \
+	    $(HOST_STAGING_PREFIX)/bin/python2.6 setup.py build
 	touch $@
 
 #
@@ -197,31 +196,17 @@ py-pylons: $(PY-PYLONS_BUILD_DIR)/.built
 #
 # If you are building a library, then you need to stage it too.
 #
-$(PY-PYLONS_BUILD_DIR)/.staged: $(PY-PYLONS_BUILD_DIR)/.built
-	rm -f $@
-#	$(MAKE) -C $(PY-PYLONS_BUILD_DIR) DESTDIR=$(STAGING_DIR) install
-	touch $@
+#$(PY-PYLONS_BUILD_DIR)/.staged: $(PY-PYLONS_BUILD_DIR)/.built
+#	rm -f $@
+#	$(MAKE) -C $(@D) DESTDIR=$(STAGING_DIR) install
+#	touch $@
 
-py-pylons-stage: $(PY-PYLONS_BUILD_DIR)/.staged
+#py-pylons-stage: $(PY-PYLONS_BUILD_DIR)/.staged
 
 #
 # This rule creates a control file for ipkg.  It is no longer
 # necessary to create a seperate control file under sources/py-pylons
 #
-$(PY24-PYLONS_IPK_DIR)/CONTROL/control:
-	@install -d $(@D)
-	@rm -f $@
-	@echo "Package: py24-pylons" >>$@
-	@echo "Architecture: $(TARGET_ARCH)" >>$@
-	@echo "Priority: $(PY-PYLONS_PRIORITY)" >>$@
-	@echo "Section: $(PY-PYLONS_SECTION)" >>$@
-	@echo "Version: $(PY-PYLONS_VERSION)-$(PY-PYLONS_IPK_VERSION)" >>$@
-	@echo "Maintainer: $(PY-PYLONS_MAINTAINER)" >>$@
-	@echo "Source: $(PY-PYLONS_SITE)/$(PY-PYLONS_SOURCE)" >>$@
-	@echo "Description: $(PY-PYLONS_DESCRIPTION)" >>$@
-	@echo "Depends: $(PY24-PYLONS_DEPENDS)" >>$@
-	@echo "Conflicts: $(PY-PYLONS_CONFLICTS)" >>$@
-
 $(PY25-PYLONS_IPK_DIR)/CONTROL/control:
 	@install -d $(@D)
 	@rm -f $@
@@ -236,6 +221,20 @@ $(PY25-PYLONS_IPK_DIR)/CONTROL/control:
 	@echo "Depends: $(PY25-PYLONS_DEPENDS)" >>$@
 	@echo "Conflicts: $(PY-PYLONS_CONFLICTS)" >>$@
 
+$(PY26-PYLONS_IPK_DIR)/CONTROL/control:
+	@install -d $(@D)
+	@rm -f $@
+	@echo "Package: py26-pylons" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(PY-PYLONS_PRIORITY)" >>$@
+	@echo "Section: $(PY-PYLONS_SECTION)" >>$@
+	@echo "Version: $(PY-PYLONS_VERSION)-$(PY-PYLONS_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(PY-PYLONS_MAINTAINER)" >>$@
+	@echo "Source: $(PY-PYLONS_SITE)/$(PY-PYLONS_SOURCE)" >>$@
+	@echo "Description: $(PY-PYLONS_DESCRIPTION)" >>$@
+	@echo "Depends: $(PY26-PYLONS_DEPENDS)" >>$@
+	@echo "Conflicts: $(PY-PYLONS_CONFLICTS)" >>$@
+
 #
 # This builds the IPK file.
 #
@@ -248,19 +247,8 @@ $(PY25-PYLONS_IPK_DIR)/CONTROL/control:
 #
 # You may need to patch your application to make it use these locations.
 #
-$(PY24-PYLONS_IPK): $(PY-PYLONS_BUILD_DIR)/.built
-	rm -rf $(BUILD_DIR)/py-pylons_*_$(TARGET_ARCH).ipk
-	rm -rf $(PY24-PYLONS_IPK_DIR) $(BUILD_DIR)/py24-pylons_*_$(TARGET_ARCH).ipk
-	cd $(PY-PYLONS_BUILD_DIR)/2.4; \
-	    PYTHONPATH=$(STAGING_LIB_DIR)/python2.4/site-packages \
-	    $(HOST_STAGING_PREFIX)/bin/python2.4 setup.py install \
-	    --root=$(PY24-PYLONS_IPK_DIR) --prefix=/opt
-#	$(STRIP_COMMAND) `find $(PY24-PYLONS_IPK_DIR)/opt/lib/python2.4/site-packages -name '*.so'`
-	$(MAKE) $(PY24-PYLONS_IPK_DIR)/CONTROL/control
-	cd $(BUILD_DIR); $(IPKG_BUILD) $(PY24-PYLONS_IPK_DIR)
-
 $(PY25-PYLONS_IPK): $(PY-PYLONS_BUILD_DIR)/.built
-	rm -rf $(PY25-PYLONS_IPK_DIR) $(BUILD_DIR)/py25-pylons_*_$(TARGET_ARCH).ipk
+	rm -rf $(PY25-PYLONS_IPK_DIR) $(BUILD_DIR)/py2[45]-pylons_*_$(TARGET_ARCH).ipk
 	cd $(PY-PYLONS_BUILD_DIR)/2.5; \
 	    PYTHONPATH=$(STAGING_LIB_DIR)/python2.5/site-packages \
 	    $(HOST_STAGING_PREFIX)/bin/python2.5 setup.py install \
@@ -269,10 +257,20 @@ $(PY25-PYLONS_IPK): $(PY-PYLONS_BUILD_DIR)/.built
 	$(MAKE) $(PY25-PYLONS_IPK_DIR)/CONTROL/control
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(PY25-PYLONS_IPK_DIR)
 
+$(PY26-PYLONS_IPK): $(PY-PYLONS_BUILD_DIR)/.built
+	rm -rf $(PY26-PYLONS_IPK_DIR) $(BUILD_DIR)/py26-pylons_*_$(TARGET_ARCH).ipk
+	cd $(PY-PYLONS_BUILD_DIR)/2.6; \
+	    PYTHONPATH=$(STAGING_LIB_DIR)/python2.6/site-packages \
+	    $(HOST_STAGING_PREFIX)/bin/python2.6 setup.py install \
+	    --root=$(PY26-PYLONS_IPK_DIR) --prefix=/opt
+#	$(STRIP_COMMAND) `find $(PY26-PYLONS_IPK_DIR)/opt/lib/python2.6/site-packages -name '*.so'`
+	$(MAKE) $(PY26-PYLONS_IPK_DIR)/CONTROL/control
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(PY26-PYLONS_IPK_DIR)
+
 #
 # This is called from the top level makefile to create the IPK file.
 #
-py-pylons-ipk: $(PY24-PYLONS_IPK) $(PY25-PYLONS_IPK)
+py-pylons-ipk: $(PY25-PYLONS_IPK) $(PY26-PYLONS_IPK)
 
 #
 # This is called from the top level makefile to clean all of the built files.
@@ -286,11 +284,11 @@ py-pylons-clean:
 #
 py-pylons-dirclean:
 	rm -rf $(BUILD_DIR)/$(PY-PYLONS_DIR) $(PY-PYLONS_BUILD_DIR)
-	rm -rf $(PY24-PYLONS_IPK_DIR) $(PY24-PYLONS_IPK)
 	rm -rf $(PY25-PYLONS_IPK_DIR) $(PY25-PYLONS_IPK)
+	rm -rf $(PY26-PYLONS_IPK_DIR) $(PY26-PYLONS_IPK)
 
 #
 # Some sanity check for the package.
 #
-py-pylons-check: $(PY24-PYLONS_IPK) $(PY25-PYLONS_IPK)
-	perl scripts/optware-check-package.pl --target=$(OPTWARE_TARGET) $(PY24-PYLONS_IPK) $(PY25-PYLONS_IPK)
+py-pylons-check: $(PY25-PYLONS_IPK) $(PY26-PYLONS_IPK)
+	perl scripts/optware-check-package.pl --target=$(OPTWARE_TARGET) $^
