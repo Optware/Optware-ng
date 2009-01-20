ab14a8e83717c79d51dadd0db5f140b65ed56ced
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2009-01-20 19:06:55 +0000

    nginx: 0.7.30 -> 0.7.31

--- trunk/make/nginx.mk
+++ trunk/make/nginx.mk
@@ -21,7 +21,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 NGINX_SITE=http://sysoev.ru/nginx
-NGINX_VERSION=0.7.30
+NGINX_VERSION=0.7.31
 NGINX_SOURCE=nginx-$(NGINX_VERSION).tar.gz
 NGINX_DIR=nginx-$(NGINX_VERSION)
 NGINX_UNZIP=zcat
@@ -288,4 +288,4 @@ nginx-dirclean:
 # Some sanity check for the package.
 #
 nginx-check: $(NGINX_IPK)
-	perl scripts/optware-check-package.pl --target=$(OPTWARE_TARGET) $(NGINX_IPK)
+	perl scripts/optware-check-package.pl --target=$(OPTWARE_TARGET) $^
