1aa52ddfb4266d29b072fa92af71f2e59aff40ab
Brian Zhou <bzhou@users.noreply.github.com>
2009-01-23 06:21:53 +0000

    mpd: added libshout to DEPENDS

--- make/mpd.mk
+++ make/mpd.mk
@@ -33,7 +33,7 @@ MPD_DESCRIPTION=Music Player Daemon (MPD) allows remote access for playing music
 MPD_SECTION=audio
 MPD_PRIORITY=optional
 MPD_DEPENDS=audiofile, faad2, ffmpeg, flac, lame, libao, libcurl
-MPD_DEPENDS+=, libid3tag, libmad, libmpcdec, libsamplerate, wavpack
+MPD_DEPENDS+=, libid3tag, libmad, libmpcdec, libsamplerate, libshout, wavpack
 ifneq (, $(filter i686, $(TARGET_ARCH)))
 MPD_DEPENDS+=, libvorbis
 else
@@ -48,7 +48,7 @@ MPD_CONFLICTS=
 #
 # MPD_IPK_VERSION should be incremented when the ipk changes.
 #
-MPD_IPK_VERSION=1
+MPD_IPK_VERSION=2
 
 #
 # MPD_CONFFILES should be a list of user-editable files
