5f59ec9d6338cd2565818d0477749d717b4ae5d2
marceln <marceln@1f1dca69-5516-0410-b834-e485effd04c3>
2009-02-14 13:22:00 +0000

    dovecot: Upstream update to 1.2.beta1

--- trunk/make/dovecot.mk
+++ trunk/make/dovecot.mk
@@ -15,26 +15,15 @@
 # TODO:
 #	- wl500g doesn't work
 #
-# DOVECOT_VERSION, DOVECOT_SITE and DOVECOT_SOURCE define
-# the upstream location of the source code for the package.
-# DOVECOT_DIR is the directory which is created when the source
-# archive is unpacked.
-# DOVECOT_UNZIP is the command used to unzip the source.
-# It is usually "zcat" (for .gz) or "bzcat" (for .bz2)
+DOVECOT_SITE=http://dovecot.org/releases/1.2/beta
+DOVECOT_VERSION=1.2.0.beta1
+DOVECOT_SRC_VERSION=1.2.beta1
 #
-# You should change all these variables to suit your package.
-# Please make sure that you add a description, and that you
-# list all your packages' dependencies, seperated by commas.
-# 
-# If you list yourself as MAINTAINER, please give a valid email
-# address, and indicate your irc nick if it cannot be easily deduced
-# from your name or email address.  If you leave MAINTAINER set to
-# "NSLU2 Linux" other developers will feel free to edit.
+# Version 1.2.0 should be greater then 1.2.beta1 so i place
+# a extra "0" in the ipk version.
 #
-DOVECOT_SITE=http://www.dovecot.org/releases/1.0
-DOVECOT_VERSION=1.0.15
-DOVECOT_SOURCE=dovecot-$(DOVECOT_VERSION).tar.gz
-DOVECOT_DIR=dovecot-$(DOVECOT_VERSION)
+DOVECOT_SOURCE=dovecot-$(DOVECOT_SRC_VERSION).tar.gz
+DOVECOT_DIR=dovecot-$(DOVECOT_SRC_VERSION)
 DOVECOT_UNZIP=zcat
 DOVECOT_MAINTAINER=Marcel Nijenhof <nslu2@pion.xs4all.nl>
 DOVECOT_DESCRIPTION=Dovecot secure IMAP server
@@ -54,7 +43,7 @@ DOVECOT_DOC_CONFLICTS=
 #
 # DOVECOT_IPK_VERSION should be incremented when the ipk changes.
 #
-DOVECOT_IPK_VERSION=2
+DOVECOT_IPK_VERSION=1
 
 #
 # DOVECOT_CONFFILES should be a list of user-editable files
@@ -67,15 +56,7 @@ DOVECOT_CONFFILES= \
 # DOVECOT_PATCHES should list any patches, in the the order in
 # which they should be applied to the source code.
 #
-DOVECOT_PATCHES=sources/dovecot/configure.in.patch
-DOVECOT_CONFIGURE=
-ifeq ($(OPTWARE_TARGET),wl500g)
-DOVECOT_PATCHES+=sources/dovecot/config.h.in_wl500g.patch
-DOVECOT_CONFIGURE+=--disable-ipv6
-endif
-ifeq ($(IPV6),no)
-DOVECOT_CONFIGURE+=--disable-ipv6
-endif
+DOVECOT_PATCHES=$(DOVECOT_SOURCE_DIR)/configure.in.patch
 
 #
 # If the compilation of the package requires additional
@@ -83,6 +64,14 @@ endif
 #
 DOVECOT_CPPFLAGS=
 DOVECOT_LDFLAGS=
+
+ifeq ($(OPTWARE_TARGET),wl500g)
+DOVECOT_CONFIGURE+=--disable-ipv6
+endif
+
+ifeq ($(IPV6),no)
+DOVECOT_CONFIGURE+=--disable-ipv6
+endif
 
 #
 # DOVECOT_BUILD_DIR is the directory in which the build is done.
@@ -101,12 +90,15 @@ DOVECOT_IPK=$(BUILD_DIR)/dovecot_$(DOVECOT_VERSION)-$(DOVECOT_IPK_VERSION)_$(TAR
 DOVECOT_DOC_IPK_DIR=$(BUILD_DIR)/dovecot-doc-$(DOVECOT_VERSION)-ipk
 DOVECOT_DOC_IPK=$(BUILD_DIR)/dovecot-doc_$(DOVECOT_VERSION)-$(DOVECOT_IPK_VERSION)_$(TARGET_ARCH).ipk
 
+.PHONY: dovecot-source dovecot-unpack dovecot dovecot-stage dovecot-ipk dovecot-clean dovecot-dirclean dovecot-check
+
 #
 # This is the dependency on the source code.  If the source is missing,
 # then it will be fetched from the site using wget.
 #
 $(DL_DIR)/$(DOVECOT_SOURCE):
-	$(WGET) -P $(DL_DIR) $(DOVECOT_SITE)/$(DOVECOT_SOURCE)
+	$(WGET) -P $(@D) $(DOVECOT_SITE)/$(@F) || \
+	$(WGET) -P $(@D) $(SOURCES_NLO_SITE)/$(@F)
 
 #
 # The source code depends on it existing within the download directory.
@@ -135,40 +127,36 @@ dovecot-source: $(DL_DIR)/$(DOVECOT_SOURCE) $(DOVECOT_PATCHES)
 #
 $(DOVECOT_BUILD_DIR)/.configured: $(DL_DIR)/$(DOVECOT_SOURCE) $(DOVECOT_PATCHES) make/dovecot.mk
 	$(MAKE) openssl-stage
-	rm -rf $(BUILD_DIR)/$(DOVECOT_DIR) $(DOVECOT_BUILD_DIR)
+	rm -rf $(BUILD_DIR)/$(DOVECOT_DIR) $(@D)
 	$(DOVECOT_UNZIP) $(DL_DIR)/$(DOVECOT_SOURCE) | tar -C $(BUILD_DIR) -xvf -
 	if test -n "$(DOVECOT_PATCHES)" ; \
 		then cat $(DOVECOT_PATCHES) | \
 		patch -d $(BUILD_DIR)/$(DOVECOT_DIR) -p0 ; \
 	fi
-	if test "$(BUILD_DIR)/$(DOVECOT_DIR)" != "$(DOVECOT_BUILD_DIR)" ; \
-		then mv $(BUILD_DIR)/$(DOVECOT_DIR) $(DOVECOT_BUILD_DIR) ; \
+	if test "$(BUILD_DIR)/$(DOVECOT_DIR)" != "$(@D)" ; \
+		then mv $(BUILD_DIR)/$(DOVECOT_DIR) $(@D) ; \
 	fi
-	(cd $(DOVECOT_BUILD_DIR); \
-		rm -rf config.cache; \
-		ACLOCAL=aclocal-1.9 AUTOMAKE=automake-1.9 autoreconf -vif; \
+	(cd $(@D); \
+		autoconf; \
 		$(TARGET_CONFIGURE_OPTS) \
 		CPPFLAGS="$(STAGING_CPPFLAGS) $(DOVECOT_CPPFLAGS)" \
 		LDFLAGS="$(STAGING_LDFLAGS) $(DOVECOT_LDFLAGS)" \
-		PKG_CONFIG_PATH=$(STAGING_LIB_DIR)/pkgconfig \
-		PKG_CONFIG_LIBDIR=$(STAGING_LIB_DIR)/pkgconfig \
 		./configure \
-		$(DOVECOT_CONFIGURE) \
-		--without-gssapi \
-		--without-pam \
-		--with-notify=dnotify \
 		--build=$(GNU_HOST_NAME) \
 		--host=$(GNU_TARGET_NAME) \
 		--target=$(GNU_TARGET_NAME) \
 		--prefix=/opt \
+		--disable-static \
+		--without-gssapi \
+		--without-pam \
+		--with-notify=dnotify \
 		--sysconfdir=/opt/etc/dovecot \
 		--localstatedir=/opt/var \
 		--with-ssldir=/opt/etc/dovecot \
-		--disable-nls \
-		--disable-static \
-		--without-sql-drivers; \
+		--without-sql-drivers \
+		--with-ioloop=poll; \
 	)
-	#$(PATCH_LIBTOOL) $(DOVECOT_BUILD_DIR)/libtool
+	$(PATCH_LIBTOOL) $(@D)/libtool
 	touch $@
 
 dovecot-unpack: $(DOVECOT_BUILD_DIR)/.configured
@@ -245,7 +233,7 @@ $(DOVECOT_DOC_IPK_DIR)/CONTROL/control:
 $(DOVECOT_IPK): $(DOVECOT_BUILD_DIR)/.built
 	rm -rf $(DOVECOT_IPK_DIR) $(BUILD_DIR)/dovecot_*_$(TARGET_ARCH).ipk
 	$(MAKE) -C $(DOVECOT_BUILD_DIR) DESTDIR=$(DOVECOT_IPK_DIR) install-strip
-	install -d $(DOVECOT_IPK_DIR)/opt/etc/dovecot/
+	install -d $(DOVECOT_IPK_DIR)/opt/etc/dovecot
 	install -m 644 $(DOVECOT_SOURCE_DIR)/dovecot.conf $(DOVECOT_IPK_DIR)/opt/etc/dovecot/
 	install -m 644 $(DOVECOT_BUILD_DIR)/doc/dovecot-openssl.cnf $(DOVECOT_IPK_DIR)/opt/etc/dovecot/
 	install -m 755 $(DOVECOT_BUILD_DIR)/doc/mkcert.sh $(DOVECOT_IPK_DIR)/opt/etc/dovecot/
@@ -257,6 +245,7 @@ $(DOVECOT_IPK): $(DOVECOT_BUILD_DIR)/.built
 	install -m 755 $(DOVECOT_SOURCE_DIR)/prerm $(DOVECOT_IPK_DIR)/CONTROL/prerm
 	echo $(DOVECOT_CONFFILES) | sed -e 's/ /\n/g' > $(DOVECOT_IPK_DIR)/CONTROL/conffiles
 	rm -rf $(DOVECOT_IPK_DIR)/opt/share/doc/dovecot
+	echo $(DOVECOT_CONFFILES) | sed -e 's/ /\n/g' > $(DOVECOT_IPK_DIR)/CONTROL/conffiles
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(DOVECOT_IPK_DIR)
 
 $(DOVECOT_DOC_IPK): $(DOVECOT_BUILD_DIR)/.built
@@ -284,9 +273,9 @@ dovecot-clean:
 #
 dovecot-dirclean:
 	rm -rf $(BUILD_DIR)/$(DOVECOT_DIR) $(DOVECOT_BUILD_DIR) $(DOVECOT_IPK_DIR) $(DOVECOT_IPK)
-
+#
 #
 # Some sanity check for the package.
 #
 dovecot-check: $(DOVECOT_IPK)
-	perl scripts/optware-check-package.pl --target=$(OPTWARE_TARGET) $(DOVECOT_IPK)
+	perl scripts/optware-check-package.pl --target=$(OPTWARE_TARGET) $^
