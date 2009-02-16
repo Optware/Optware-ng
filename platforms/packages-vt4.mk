c9bd84a92452c155f3b11be10182762388cc0537
Brian Zhou <bzhou@users.noreply.github.com>
2009-02-16 04:23:55 +0000

    packages-vt4: demoted subvertpy and bzr-svn on vt4

--- platforms/packages-vt4.mk
+++ platforms/packages-vt4.mk
@@ -37,7 +37,8 @@ BROKEN_PACKAGES = \
 	qemu quickie \
 	re2c rhtvision rsstail rtorrent \
 	sablevm samba sane-backends simh smartmontools snort \
-	spandsp splix sqsh swi-prolog svn \
+	spandsp splix sqsh swi-prolog \
+	subvertpy svn bzr-svn \
 	taglib tshark transcode \
 	unrar upslug2 upx \
 	vlc vnstat vorbis-tools vte \
