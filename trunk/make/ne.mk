974b03e763b1b33a626de7108dfa3361a9468738
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2009-02-20 06:26:54 +0000

    ne: 2.0 -> 2.0.2

--- trunk/make/ne.mk
+++ trunk/make/ne.mk
@@ -21,7 +21,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 NE_SITE=http://ne.dsi.unimi.it
-NE_VERSION=2.0
+NE_VERSION=2.0.2
 NE_SOURCE=ne-$(NE_VERSION).tar.gz
 NE_DIR=ne-$(NE_VERSION)
 NE_UNZIP=zcat
