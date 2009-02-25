c327ca0417bf076e1995320d1044de5a5a24e03a
Brian Zhou <bzhou@users.noreply.github.com>
2009-02-25 19:17:46 +0000

    packages-wdtv: fixed and promoted asterisk16 & asterisk16-addons

--- platforms/packages-wdtv.mk
+++ platforms/packages-wdtv.mk
@@ -8,7 +8,7 @@ SPECIFIC_PACKAGES = \
 
 BROKEN_PACKAGES = \
 	$(PACKAGES_ONLY_WORK_ON_LINUX24) \
-	9base asterisk16 asterisk16-addons cdrtools fcgi ficy fish \
+	9base cdrtools fcgi ficy fish \
 	fuppes ghostscript gnu-smalltalk gtmess gtk gutenprint \
 	ice iptraf launchtool ldconfig ltrace microdc2 moc \
 	monotone mtr newsbeuter nfs-server nfs-utils pango pinentry \
