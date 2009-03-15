44bbc620c8c2dbd0d7de6ea31fd7ef73d051edc8
Brian Zhou <bzhou@users.noreply.github.com>
2009-03-15 05:31:58 +0000

    lftp: 3.7.8 -> 3.7.9

--- make/lftp.mk
+++ make/lftp.mk
@@ -21,7 +21,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 LFTP_SITE=http://ftp.yars.free.net/pub/source/lftp
-LFTP_VERSION=3.7.8
+LFTP_VERSION=3.7.9
 LFTP_SOURCE=lftp-$(LFTP_VERSION).tar.gz
 LFTP_DIR=lftp-$(LFTP_VERSION)
 LFTP_UNZIP=zcat
