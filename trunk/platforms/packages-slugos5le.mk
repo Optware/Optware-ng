fcdb10488e60486b667d2aac159cfcc0083957a1
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2009-03-18 20:26:27 +0000

    added essential files for optware/slugos5le

---
+++ trunk/platforms/packages-slugos5le.mk
@@ -0,0 +1,15 @@
+PERL_MAJOR_VER = 5.10
+LIBNSL_SO_DIR = $(TARGET_TOP)/staging/armv5te-linux-gnueabi/lib
+
+SPECIFIC_PACKAGES = \
+	optware-bootstrap \
+	$(PERL_PACKAGES) \
+	$(PACKAGES_REQUIRE_LINUX26) \
+
+BROKEN_PACKAGES = \
+	$(PACKAGES_ONLY_WORK_ON_LINUX24) \
+	bitchx bzflag \
+	gtk ipac-ng iptables iptraf ldconfig \
+	microdc2 netatalk nfs-utils \
+	player puppy mod-python qemu softflowd \
+	taged transcode vte xchat \
