7dcd16afc24da5aba48ad942368c6e17c615cc13
Brian Zhou <bzhou@users.noreply.github.com>
2009-03-21 19:37:00 +0000

    git: 1.6.2 -> 1.6.2.1

--- make/git.mk
+++ make/git.mk
@@ -22,7 +22,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 GIT_SITE=http://www.kernel.org/pub/software/scm/git
-GIT_VERSION=1.6.2
+GIT_VERSION=1.6.2.1
 GIT_SOURCE=git-$(GIT_VERSION).tar.gz
 GIT_DIR=git-$(GIT_VERSION)
 GIT_UNZIP=zcat
