9fc4b6a0bf3045667307b5ab1986bac779ca82b1
Brian Zhou <bzhou@users.noreply.github.com>
2009-03-22 22:51:10 +0000

    setup-optware.sh modified to work with both slugos5be and slugos5le

--- sources/optware-bootstrap/slugos5be/setup-optware.sh
+++ sources/optware-bootstrap/slugos5be/setup-optware.sh
@@ -5,7 +5,12 @@ if test 0 != `id -u`; then
     exit 1
 fi
  
-optware_target=slugos5be
+if test `uname -m` = armv5teb ; then
+    optware_target=slugos5be
+else
+    optware_target=slugos5le
+fi
+
 feed=http://ipkg.nslu2-linux.org/feeds/optware/${optware_target}/cross/unstable
 latest_xsh=`wget -q -O- ${feed} | grep '\.xsh' | sed -e 's/.*xsh">//' -e 's/<.*//'`
 
