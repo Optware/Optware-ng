0ed4599b96f86804f074d3e82f13aa99bd1b3fcb
Brian Zhou <bzhou@users.noreply.github.com>
2009-03-24 20:31:09 +0000

    nginx: 0.7.41 -> 0.7.44

--- make/nginx.mk
+++ make/nginx.mk
@@ -21,7 +21,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 NGINX_SITE=http://sysoev.ru/nginx
-NGINX_VERSION=0.7.41
+NGINX_VERSION=0.7.44
 NGINX_SOURCE=nginx-$(NGINX_VERSION).tar.gz
 NGINX_DIR=nginx-$(NGINX_VERSION)
 NGINX_UNZIP=zcat
@@ -52,7 +52,7 @@ NGINX_CONFFILES=\
 # NGINX_PATCHES should list any patches, in the the order in
 # which they should be applied to the source code.
 #
-NGINX_PATCHES=
+NGINX_PATCHES=$(NGINX_SOURCE_DIR)/ngx_log_abort.patch
 
 ifneq ($(HOSTCC), $(TARGET_CC))
 NGINX_PATCHES+=$(NGINX_SOURCE_DIR)/cross-configure.patch
