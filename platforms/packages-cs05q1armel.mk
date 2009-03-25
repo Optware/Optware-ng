46e14c24acace2d564701944b5c3ed30c5e884b2
Brian Zhou <bzhou@users.noreply.github.com>
2009-03-25 18:37:19 +0000

    cs05q1armel: added configuration file for cs05q1 toolchain

---
+++ platforms/packages-cs05q1armel.mk
@@ -0,0 +1,19 @@
+PERL_MAJOR_VER := 5.10
+
+# Packages that *only* work for mssii - do not just put new packages here.
+SPECIFIC_PACKAGES = \
+	$(PERL_PACKAGES) \
+	$(PACKAGES_REQUIRE_LINUX26) \
+
+# Packages that do not work for mssii.
+BROKEN_PACKAGES = \
+	$(PACKAGES_ONLY_WORK_ON_LINUX24) \
+	amule \
+	asterisk14-chan-capi libcapi20 \
+	iptraf \
+	ldconfig \
+	player \
+	qemu qemu-libc-i386 \
+
+E2FSPROGS_VERSION := 1.40.3
+E2FSPROGS_IPK_VERSION := 1
