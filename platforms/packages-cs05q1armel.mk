5f206b24509530f62b4a5f870abcddecad6381b2
Brian Zhou <bzhou@users.noreply.github.com>
2009-03-26 14:40:53 +0000

    packages-cs05q1armel: fixed libnsl location, a few more BROKEN_PACKAGES after 1st build

--- platforms/packages-cs05q1armel.mk
+++ platforms/packages-cs05q1armel.mk
@@ -10,11 +10,14 @@ BROKEN_PACKAGES = \
 	$(PACKAGES_ONLY_WORK_ON_LINUX24) \
 	amule \
 	asterisk14-chan-capi libcapi20 \
+	bluez-utils bluez-hcidump \
 	iptraf \
 	ldconfig \
 	player \
 	qemu qemu-libc-i386 \
 	gtk vte xchat \
+	microdc2 mod-python subvertpy \
+	taged transcode slimserver \
 
 E2FSPROGS_VERSION := 1.40.3
 E2FSPROGS_IPK_VERSION := 1
