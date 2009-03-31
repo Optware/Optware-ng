8574742839916fdf9e721e439229f2acc111ee0b
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2009-03-31 03:11:11 +0000

    hiawatha: 6.11 -> 6.12

--- trunk/make/hiawatha.mk
+++ trunk/make/hiawatha.mk
@@ -21,7 +21,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 HIAWATHA_SITE=http://www.hiawatha-webserver.org/files
-HIAWATHA_VERSION=6.11
+HIAWATHA_VERSION=6.12
 HIAWATHA_SOURCE=hiawatha-$(HIAWATHA_VERSION).tar.gz
 HIAWATHA_DIR=hiawatha-$(HIAWATHA_VERSION)
 HIAWATHA_UNZIP=zcat
