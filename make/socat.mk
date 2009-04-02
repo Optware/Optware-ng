1bc8c3c3dc1f5f85963c3b969563b3fbdf75078c
Brian Zhou <bzhou@users.noreply.github.com>
2009-04-02 23:33:18 +0000

    socat: 1.7.0.0 -> 1.7.0.1

--- make/socat.mk
+++ make/socat.mk
@@ -21,7 +21,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 SOCAT_SITE=http://www.dest-unreach.org/socat/download
-SOCAT_VERSION=1.7.0.0
+SOCAT_VERSION=1.7.0.1
 SOCAT_SOURCE=socat-$(SOCAT_VERSION).tar.bz2
 SOCAT_DIR=socat-$(SOCAT_VERSION)
 SOCAT_UNZIP=bzcat
@@ -233,4 +233,4 @@ socat-dirclean:
 # Some sanity check for the package.
 #
 socat-check: $(SOCAT_IPK)
-	perl scripts/optware-check-package.pl --target=$(OPTWARE_TARGET) $(SOCAT_IPK)
+	perl scripts/optware-check-package.pl --target=$(OPTWARE_TARGET) $^
