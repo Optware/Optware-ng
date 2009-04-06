7003ac191d0a52ed1e335c75bf9f99d2daae310d
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2009-04-06 19:52:33 +0000

    heyu: upgraded to 2.5.1, promoted on slugos4{be,le}

--- trunk/platforms/packages-slugosle.mk
+++ trunk/platforms/packages-slugosle.mk
@@ -7,12 +7,10 @@ SPECIFIC_PACKAGES = \
 
 # Packages that do not work for slugosbe.
 # puppy: usb_io.h:33:23: error: linux/usb.h: No such file or directory
-# heyu: xwrite.c:34:30: error: linux/serial_reg.h: No such file or directory
 # iptraf: sys/types.h and linux/types.h conflicting, the ipk for unslung seems to work though
 # softflowd: staging/opt/include/pcap/pcap.h:339: error: conflicting types for 'bpf_filter' (with arm-linux/include/net/bpf.h:779)
 BROKEN_PACKAGES = \
 	$(PACKAGES_ONLY_WORK_ON_LINUX24) \
-	heyu \
 	iptraf \
 	ldconfig \
 	netatalk \
