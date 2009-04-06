d7f978d60bb5a1331a36a1077eae5522e18c561b
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2009-04-06 19:52:15 +0000

    bzflag: upgrade to 2.0.12

--- trunk/make/bzflag.mk
+++ trunk/make/bzflag.mk
@@ -15,7 +15,8 @@
 # You should change all these variables to suit your package.
 #
 BZFLAG_SITE=http://ftp.bzflag.org/bzflag
-BZFLAG_VERSION=2.0.0.20050117
+BZFLAG_SITE=http://$(SOURCEFORGE_MIRROR)/sourceforge/bzflag
+BZFLAG_VERSION=2.0.12
 BZFLAG_SOURCE=bzflag-$(BZFLAG_VERSION).tar.gz
 BZFLAG_DIR=bzflag-$(BZFLAG_VERSION)
 BZFLAG_UNZIP=zcat
@@ -23,14 +24,14 @@ BZFLAG_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
 BZFLAG_DESCRIPTION=bzflag server
 BZFLAG_SECTION=games
 BZFLAG_PRIORITY=optional
-BZFLAG_DEPENDS=zlib, adns, libcurl, ncurses, openssl
+BZFLAG_DEPENDS=libstdc++, c-ares, libcurl, ncurses, openssl, zlib
 BZFLAG_SUGGESTS=
 BZFLAG_CONFLICTS=
 
 #
 # BZFLAG_IPK_VERSION should be incremented when the ipk changes.
 #
-BZFLAG_IPK_VERSION=3
+BZFLAG_IPK_VERSION=1
 
 #
 # BZFLAG_CONFFILES should be a list of user-editable files
@@ -40,7 +41,7 @@ BZFLAG_CONFFILES=
 # BZFLAG_PATCHES should list any patches, in the the order in
 # which they should be applied to the source code.
 #
-BZFLAG_PATCHES=$(BZFLAG_SOURCE_DIR)/bzflag-configure.patch
+#BZFLAG_PATCHES=$(BZFLAG_SOURCE_DIR)/bzflag-configure.patch
 
 #
 # If the compilation of the package requires additional
@@ -70,7 +71,8 @@ BZFLAG_IPK=$(BUILD_DIR)/bzflag_$(BZFLAG_VERSION)-$(BZFLAG_IPK_VERSION)_$(TARGET_
 # then it will be fetched from the site using wget.
 #
 $(DL_DIR)/$(BZFLAG_SOURCE):
-	$(WGET) -P $(DL_DIR) $(BZFLAG_SITE)/$(BZFLAG_SOURCE)
+	$(WGET) -P $(@D) $(BZFLAG_SITE)/$(@F) || \
+	$(WGET) -P $(@D) $(SOURCES_NLO_SITE)/$(@F)
 
 #
 # The source code depends on it existing within the download directory.
@@ -94,16 +96,17 @@ bzflag-source: $(DL_DIR)/$(BZFLAG_SOURCE) $(BZFLAG_PATCHES)
 # If the compilation of the package requires other packages to be staged
 # first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
 #
-$(BZFLAG_BUILD_DIR)/.configured: $(DL_DIR)/$(BZFLAG_SOURCE) $(BZFLAG_PATCHES)
-	$(MAKE) zlib-stage libcurl-stage adns-stage ncurses-stage openssl-stage
+$(BZFLAG_BUILD_DIR)/.configured: $(DL_DIR)/$(BZFLAG_SOURCE) $(BZFLAG_PATCHES) make/bzflag.mk
+	$(MAKE) c-ares-stage libstdc++-stage
+	$(MAKE) zlib-stage libcurl-stage ncurses-stage openssl-stage
 	rm -rf $(BUILD_DIR)/$(BZFLAG_DIR) $(BZFLAG_BUILD_DIR)
 	$(BZFLAG_UNZIP) $(DL_DIR)/$(BZFLAG_SOURCE) | tar -C $(BUILD_DIR) -xvf -
-	cat $(BZFLAG_PATCHES) | patch -d $(BUILD_DIR)/$(BZFLAG_DIR) -p1
-	mv $(BUILD_DIR)/$(BZFLAG_DIR) $(BZFLAG_BUILD_DIR)
+#	cat $(BZFLAG_PATCHES) | patch -d $(BUILD_DIR)/$(BZFLAG_DIR) -p1
+	mv $(BUILD_DIR)/$(BZFLAG_DIR) $(@D)
 	# install gl headers needed by bzadmin
-	install -d $(BZFLAG_BUILD_DIR)/include/GL
-	install -m 644 $(BZFLAG_SOURCE_DIR)/*.h $(BZFLAG_BUILD_DIR)/include/GL
-	(cd $(BZFLAG_BUILD_DIR); \
+#	install -d $(@D)/include/GL
+#	install -m 644 $(BZFLAG_SOURCE_DIR)/*.h $(@D)/include/GL
+	(cd $(@D); \
 		$(TARGET_CONFIGURE_OPTS) \
 		PATH="$(STAGING_DIR)/bin:$(PATH)" \
 		CPPFLAGS="$(STAGING_CPPFLAGS) $(BZFLAG_CPPFLAGS)" \
@@ -118,7 +121,8 @@ $(BZFLAG_BUILD_DIR)/.configured: $(DL_DIR)/$(BZFLAG_SOURCE) $(BZFLAG_PATCHES)
 		--disable-client \
 		--without-SDL \
 	)
-	touch $(BZFLAG_BUILD_DIR)/.configured
+	$(PATCH_LIBTOOL) $(@D)/libtool
+	touch $@
 
 bzflag-unpack: $(BZFLAG_BUILD_DIR)/.configured
 
@@ -126,9 +130,9 @@ bzflag-unpack: $(BZFLAG_BUILD_DIR)/.configured
 # This builds the actual binary.
 #
 $(BZFLAG_BUILD_DIR)/.built: $(BZFLAG_BUILD_DIR)/.configured
-	rm -f $(BZFLAG_BUILD_DIR)/.built
-	$(MAKE) -C $(BZFLAG_BUILD_DIR)
-	touch $(BZFLAG_BUILD_DIR)/.built
+	rm -f $@
+	$(MAKE) -C $(@D)
+	touch $@
 
 #
 # This is the build convenience target.
@@ -138,19 +142,19 @@ bzflag: $(BZFLAG_BUILD_DIR)/.built
 #
 # If you are building a library, then you need to stage it too.
 #
-$(BZFLAG_BUILD_DIR)/.staged: $(BZFLAG_BUILD_DIR)/.built
-	rm -f $(BZFLAG_BUILD_DIR)/.staged
-	$(MAKE) -C $(BZFLAG_BUILD_DIR) DESTDIR=$(STAGING_DIR) install
-	touch $(BZFLAG_BUILD_DIR)/.staged
-
-bzflag-stage: $(BZFLAG_BUILD_DIR)/.staged
+#$(BZFLAG_BUILD_DIR)/.staged: $(BZFLAG_BUILD_DIR)/.built
+#	rm -f $@
+#	$(MAKE) -C $(@D) DESTDIR=$(STAGING_DIR) install
+#	touch $@
+#
+#bzflag-stage: $(BZFLAG_BUILD_DIR)/.staged
 
 #
 # This rule creates a control file for ipkg.  It is no longer
 # necessary to create a seperate control file under sources/bzflag
 #
 $(BZFLAG_IPK_DIR)/CONTROL/control:
-	@install -d $(BZFLAG_IPK_DIR)/CONTROL
+	@install -d $(@D)
 	@rm -f $@
 	@echo "Package: bzflag" >>$@
 	@echo "Architecture: $(TARGET_ARCH)" >>$@
@@ -215,4 +219,4 @@ bzflag-dirclean:
 # Some sanity check for the package.
 #
 bzflag-check: $(BZFLAG_IPK)
-	perl scripts/optware-check-package.pl --target=$(OPTWARE_TARGET) $(BZFLAG_IPK)
+	perl scripts/optware-check-package.pl --target=$(OPTWARE_TARGET) $^
