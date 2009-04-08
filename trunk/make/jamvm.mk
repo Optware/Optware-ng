664291855247b1331559b092fb594a39b785cae0
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2009-04-08 21:47:57 +0000

    jamvm: revert 1.5.2 -> 1.5.1

--- trunk/make/jamvm.mk
+++ trunk/make/jamvm.mk
@@ -20,7 +20,7 @@
 # You should change all these variables to suit your package.
 #
 JAMVM_SITE=http://$(SOURCEFORGE_MIRROR)/sourceforge/jamvm
-JAMVM_VERSION ?= 1.5.2
+JAMVM_VERSION ?= 1.5.1
 JAMVM_IPK_VERSION ?= 1
 JAMVM_SOURCE=jamvm-$(JAMVM_VERSION).tar.gz
 JAMVM_DIR=jamvm-$(JAMVM_VERSION)
