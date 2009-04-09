c90a92af3ae31fc2691e8c1109655d7e407ec9a9
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2009-04-09 17:44:08 +0000

    hdparm: 9.13 -> 9.14

--- trunk/make/hdparm.mk
+++ trunk/make/hdparm.mk
@@ -13,7 +13,7 @@
 # It is usually "zcat" (for .gz) or "bzcat" (for .bz2)
 #
 HDPARM_SITE=http://$(SOURCEFORGE_MIRROR)/sourceforge/hdparm
-HDPARM_VERSION=9.13
+HDPARM_VERSION=9.14
 HDPARM_SOURCE=hdparm-$(HDPARM_VERSION).tar.gz
 HDPARM_DIR=hdparm-$(HDPARM_VERSION)
 HDPARM_UNZIP=zcat
