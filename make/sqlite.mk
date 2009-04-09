0bdfd3ba074dcadac7c60eaae56844be1e80a707
Brian Zhou <bzhou@users.noreply.github.com>
2009-04-09 02:40:55 +0000

    sqlite: 3.6.11 -> 3.6.12

--- make/sqlite.mk
+++ make/sqlite.mk
@@ -21,7 +21,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 SQLITE_SITE=http://www.sqlite.org
-SQLITE_VERSION=3.6.11
+SQLITE_VERSION=3.6.12
 SQLITE_SOURCE=sqlite-$(SQLITE_VERSION).tar.gz
 SQLITE_DIR=sqlite-$(SQLITE_VERSION)
 SQLITE_UNZIP=zcat
