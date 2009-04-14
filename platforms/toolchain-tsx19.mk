d99d12470e2a81029f0164e0b20b6971541e3676
Brian Zhou <bzhou@users.noreply.github.com>
2009-04-14 03:06:24 +0000

    toolchain-tsx19: enable native build

--- platforms/toolchain-tsx19.mk
+++ platforms/toolchain-tsx19.mk
@@ -5,6 +5,21 @@ LIBC_STYLE=glibc
 LIBSTDC++_VERSION=6.0.9
 LIBNSL_VERSION=2.5
 
+ifeq ($(HOST_MACHINE), $(filter armv5tel armv5tejl, $(HOST_MACHINE)))
+
+HOSTCC = $(TARGET_CC)
+GNU_HOST_NAME = $(GNU_TARGET_NAME)
+TARGET_CROSS = /opt/bin/
+TARGET_LIBDIR = /opt/lib
+TARGET_INCDIR = /opt/include
+TARGET_LDFLAGS =
+TARGET_CUSTOM_FLAGS=
+TARGET_CFLAGS= $(TARGET_OPTIMIZATION) $(TARGET_DEBUGGING) $(TARGET_CUSTOM_FLAGS)
+
+toolchain:
+
+else
+
 HOSTCC = gcc
 GNU_HOST_NAME = $(HOST_MACHINE)-pc-linux-gnu
 GNU_TARGET_NAME = arm-none-linux-gnueabi
@@ -28,3 +43,5 @@ toolchain/$(GNU_COMBO)/.unpacked: ../cs08q1armel/toolchain/arm-2008q1/.unpacked
 ../cs08q1armel/toolchain/arm-2008q1/.unpacked:
 	$(MAKE) -C .. cs08q1armel-target
 	$(MAKE) -C ../cs08q1armel toolchain
+
+endif
