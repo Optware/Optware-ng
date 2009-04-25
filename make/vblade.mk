ba59b3f3220d0ae7da541e8df846f5a574c79314
Brian Zhou <bzhou@users.noreply.github.com>
2009-04-25 04:27:57 +0000

    vblade: 16 -> 19

--- make/vblade.mk
+++ make/vblade.mk
@@ -11,7 +11,7 @@
 # if there are reasons.
 #
 VBLADE_SITE=http://$(SOURCEFORGE_MIRROR)/sourceforge/aoetools
-VBLADE_VERSION=16
+VBLADE_VERSION=19
 VBLADE_SOURCE=vblade-$(VBLADE_VERSION).tgz
 VBLADE_DIR=vblade-$(VBLADE_VERSION)
 VBLADE_UNZIP=zcat
@@ -30,7 +30,7 @@ VBLADE_IPK_VERSION=1
 
 #
 # VBLADE_CONFFILES should be a list of user-editable files
-VBLADE_CONFFILES=""
+VBLADE_CONFFILES=
 
 #
 # VBLADE_PATCHES should list any patches, in the the order in
@@ -64,7 +64,8 @@ VBLADE_IPK=$(BUILD_DIR)/vblade_$(VBLADE_VERSION)-$(VBLADE_IPK_VERSION)_$(TARGET_
 # then it will be fetched from the site using wget.
 #
 $(DL_DIR)/$(VBLADE_SOURCE):
-	$(WGET) -P $(DL_DIR) $(VBLADE_SITE)/$(VBLADE_SOURCE)
+	$(WGET) -P $(@D) $(VBLADE_SITE)/$(@F) || \
+	$(WGET) -P $(@D) $(SOURCES_NLO_SITE)/$(@F)
 
 #
 # The source code depends on it existing within the download directory.
@@ -88,25 +89,26 @@ vblade-source: $(DL_DIR)/$(VBLADE_SOURCE) $(VBLADE_PATCHES)
 # If the compilation of the package requires other packages to be staged
 # first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
 #
-$(VBLADE_BUILD_DIR)/.configured: $(DL_DIR)/$(VBLADE_SOURCE) $(VBLADE_PATCHES)
+$(VBLADE_BUILD_DIR)/.configured: $(DL_DIR)/$(VBLADE_SOURCE) $(VBLADE_PATCHES) make/vblade.mk
 #	$(MAKE) <bar>-stage <baz>-stage
-	rm -rf $(BUILD_DIR)/$(VBLADE_DIR) $(VBLADE_BUILD_DIR)
+	rm -rf $(BUILD_DIR)/$(VBLADE_DIR) $(@D)
 	$(VBLADE_UNZIP) $(DL_DIR)/$(VBLADE_SOURCE) | tar -C $(BUILD_DIR) -xvf -
-	# cat $(VBLADE_PATCHES) | patch -d $(BUILD_DIR)/$(VBLADE_DIR) -p1
-	mv $(BUILD_DIR)/$(VBLADE_DIR) $(VBLADE_BUILD_DIR)
-	cd $(VBLADE_BUILD_DIR); sed -i '/CC = gcc/d' makefile
-	#(cd $(VBLADE_BUILD_DIR); \
-	#$(TARGET_CONFIGURE_OPTS) \
-	#	CPPFLAGS="$(STAGING_CPPFLAGS) $(VBLADE_CPPFLAGS)" \
-	#	LDFLAGS="$(STAGING_LDFLAGS) $(VBLADE_LDFLAGS)" \
-	#	./configure \
-	#	--build=$(GNU_HOST_NAME) \
-	#	--host=$(GNU_TARGET_NAME) \
-	#	--target=$(GNU_TARGET_NAME) \
-	#	--prefix=/opt \
-	#	--disable-nls \
-	#)
-	touch $(VBLADE_BUILD_DIR)/.configured
+	if test -n "$(VBLADE_PATCHES)"; then \
+		cat $(VBLADE_PATCHES) | patch -d $(BUILD_DIR)/$(VBLADE_DIR) -p1; \
+	fi
+	mv $(BUILD_DIR)/$(VBLADE_DIR) $(@D)
+#	(cd $(@D); \
+		$(TARGET_CONFIGURE_OPTS) \
+		CPPFLAGS="$(STAGING_CPPFLAGS) $(VBLADE_CPPFLAGS)" \
+		LDFLAGS="$(STAGING_LDFLAGS) $(VBLADE_LDFLAGS)" \
+		./configure \
+		--build=$(GNU_HOST_NAME) \
+		--host=$(GNU_TARGET_NAME) \
+		--target=$(GNU_TARGET_NAME) \
+		--prefix=/opt \
+		--disable-nls \
+	)
+	touch $@
 
 vblade-unpack: $(VBLADE_BUILD_DIR)/.configured
 
@@ -114,12 +116,13 @@ vblade-unpack: $(VBLADE_BUILD_DIR)/.configured
 # This builds the actual binary.
 #
 $(VBLADE_BUILD_DIR)/.built: $(VBLADE_BUILD_DIR)/.configured
-	rm -f $(VBLADE_BUILD_DIR)/.built
-	$(TARGET_CONFIGURE_OPTS) \
+	rm -f $@
+	$(MAKE) -C $(@D) \
+		$(TARGET_CONFIGURE_OPTS) \
 		CPPFLAGS="$(STAGING_CPPFLAGS) $(VBLADE_CPPFLAGS)" \
 		LDFLAGS="$(STAGING_LDFLAGS) $(VBLADE_LDFLAGS)" \
-		$(MAKE) -C $(VBLADE_BUILD_DIR)
-	touch $(VBLADE_BUILD_DIR)/.built
+		;
+	touch $@
 
 #
 # This is the build convenience target.
@@ -130,9 +133,9 @@ vblade: $(VBLADE_BUILD_DIR)/.built
 # If you are building a library, then you need to stage it too.
 #
 $(VBLADE_BUILD_DIR)/.staged: $(VBLADE_BUILD_DIR)/.built
-	rm -f $(VBLADE_BUILD_DIR)/.staged
-	$(MAKE) -C $(VBLADE_BUILD_DIR) DESTDIR=$(STAGING_DIR) install
-	touch $(VBLADE_BUILD_DIR)/.staged
+	rm -f $@
+	$(MAKE) -C $(@D) DESTDIR=$(STAGING_DIR) install
+	touch $@
 
 vblade-stage: $(VBLADE_BUILD_DIR)/.staged
 
@@ -141,7 +144,7 @@ vblade-stage: $(VBLADE_BUILD_DIR)/.staged
 # necessary to create a seperate control file under sources/vblade
 #
 $(VBLADE_IPK_DIR)/CONTROL/control:
-	@install -d $(VBLADE_IPK_DIR)/CONTROL
+	@install -d $(@D)
 	@rm -f $@
 	@echo "Package: vblade" >>$@
 	@echo "Architecture: $(TARGET_ARCH)" >>$@
@@ -173,6 +176,10 @@ $(VBLADE_IPK): $(VBLADE_BUILD_DIR)/.built
 	install -m 755 $(VBLADE_BUILD_DIR)/vblade $(VBLADE_IPK_DIR)/opt/sbin
 	$(MAKE) $(VBLADE_IPK_DIR)/CONTROL/control
 	$(STRIP_COMMAND) $(VBLADE_IPK_DIR)/opt/sbin/vblade
+	install -d $(VBLADE_IPK_DIR)/opt/share/man/man8
+	install -m644 $(VBLADE_BUILD_DIR)/vblade.8 $(VBLADE_IPK_DIR)/opt/share/man/man8
+	install -d $(VBLADE_IPK_DIR)/opt/share/doc/vblade
+	install -m644 $(VBLADE_BUILD_DIR)/[CNR]* $(VBLADE_IPK_DIR)/opt/share/doc/vblade
 	echo $(VBLADE_CONFFILES) | sed -e 's/ /\n/g' > $(VBLADE_IPK_DIR)/CONTROL/conffiles
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(VBLADE_IPK_DIR)
 
@@ -193,3 +200,9 @@ vblade-clean:
 #
 vblade-dirclean:
 	rm -rf $(BUILD_DIR)/$(VBLADE_DIR) $(VBLADE_BUILD_DIR) $(VBLADE_IPK_DIR) $(VBLADE_IPK)
+
+#
+# Some sanity check for the package.
+#
+vblade-check: $(VBLADE_IPK)
+	perl scripts/optware-check-package.pl --target=$(OPTWARE_TARGET) $^
