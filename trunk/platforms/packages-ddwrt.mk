20858da73ece2673fc6cb163650bea10e410f3c7
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2009-04-25 16:36:29 +0000

    renamed UCLIBC_{BROKEN,SPECIFIC} to UCLIBC++_{BROKEN,SPECIFIC}, demoted sane-backends on ddwrt,oleg,openwrt-brcm24

--- trunk/platforms/packages-ddwrt.mk
+++ trunk/platforms/packages-ddwrt.mk
@@ -1,12 +1,12 @@
 SPECIFIC_PACKAGES = \
 	firmware-oleg \
 	$(PERL_PACKAGES) \
-	$(UCLIBC_SPECIFIC_PACKAGES) \
+	$(UCLIBC++_SPECIFIC_PACKAGES) \
 
 # firmware-oleg 1) needs specific version of old make; 2) requires wl500g toolchain
 # rssh: rssh needs wordexp() to compile
 BROKEN_PACKAGES = \
-	$(UCLIBC_BROKEN_PACKAGES) \
+	$(UCLIBC++_BROKEN_PACKAGES) \
 	cdrtools \
 	centerim \
 	dansguardian \
--- trunk/platforms/packages-oleg.mk
+++ trunk/platforms/packages-oleg.mk
@@ -1,13 +1,13 @@
 SPECIFIC_PACKAGES = \
 	firmware-oleg \
 	$(PERL_PACKAGES) \
-	$(UCLIBC_SPECIFIC_PACKAGES) \
+	$(UCLIBC++_SPECIFIC_PACKAGES) \
 
 # firmware-oleg 1) needs specific version of old make; 2) requires wl500g toolchain
 # lirc depends on firmware-oleg
 # rssh: rssh needs wordexp() to compile
 BROKEN_PACKAGES = \
-	$(UCLIBC_BROKEN_PACKAGES) \
+	$(UCLIBC++_BROKEN_PACKAGES) \
 	cdrtools \
 	centerim \
 	dansguardian \
