4996c8552e0ec73de98f22bce06a87a6c8ca2d9e
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2009-05-10 16:23:39 +0000

    btg: demoted on {ds101g,openwrt-ixp4xx,slugosbe,slugosle}

--- trunk/platforms/packages-slugosbe.mk
+++ trunk/platforms/packages-slugosbe.mk
@@ -10,6 +10,7 @@ SPECIFIC_PACKAGES = \
 # softflowd: staging/opt/include/pcap/pcap.h:339: error: conflicting types for 'bpf_filter' (with armeb-linux/include/net/bpf.h:779)
 BROKEN_PACKAGES = \
 	$(PACKAGES_ONLY_WORK_ON_LINUX24) \
+	btg \
 	iptraf \
 	ldconfig \
 	netatalk \
