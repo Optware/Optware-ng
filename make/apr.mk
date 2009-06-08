67e3054730b130b02c37e1c209a2ad4a1f69e093
Brian Zhou <bzhou@users.noreply.github.com>
2009-06-08 21:42:05 +0000

    apr: 1.3.3 -> 1.3.5

--- make/apr.mk
+++ make/apr.mk
@@ -14,7 +14,7 @@
 # It is usually "zcat" (for .gz) or "bzcat" (for .bz2)
 #
 APR_SITE=http://www.apache.org/dist/apr
-APR_VERSION=1.3.3
+APR_VERSION=1.3.5
 APR_SOURCE=apr-$(APR_VERSION).tar.bz2
 APR_DIR=apr-$(APR_VERSION)
 APR_UNZIP=bzcat
@@ -218,4 +218,4 @@ apr-dirclean:
 # Some sanity check for the package.
 #
 apr-check: $(APR_IPK)
-	perl scripts/optware-check-package.pl --target=$(OPTWARE_TARGET) $(APR_IPK)
+	perl scripts/optware-check-package.pl --target=$(OPTWARE_TARGET) $^
