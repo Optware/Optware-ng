a44f173044ea71fe682ae4e274e3810eef517f62
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2009-06-14 16:47:05 +0000

    sheevaplug kmods: some structural change

--- trunk/make/kernel-modules.mk
+++ trunk/make/kernel-modules.mk
@@ -1,3 +1,9 @@
-ifeq (sheevaplug, $(OPTWARE_TARGET))
-include sources/kernel-modules/kirkwood/Makefile
+#KERNEL_RECIPE_DIR=sources/kernel-modules/$(strip \
+$(if $(filter sheevaplug, $(OPTWARE_TARGET)), kirkwood, \
+$(if $(filter syno0844mv6281, $(OPTWARE_TARGET)), syno0844/mv6281, \
+$(if $(filter syno0844ppc854x, $(OPTWARE_TARGET)), syno0844/ppc854x, \
+))))
+
+ifdef KERNEL_RECIPE_DIR
+include $(KERNEL_RECIPE_DIR)/Makefile
 endif
