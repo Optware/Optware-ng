186e95165ed2c0dfcc8e3b9a60a3928031db52a4
Brian Zhou <bzhou@users.noreply.github.com>
2009-06-28 06:31:19 +0000

    new i686g25 optware target

---
+++ platforms/packages-i686g25.mk
@@ -0,0 +1,16 @@
+PERL_MAJOR_VER = 5.10
+
+SPECIFIC_PACKAGES = \
+	$(PACKAGES_REQUIRE_LINUX26) \
+	$(PERL_PACKAGES) \
+	ipkg-opt \
+	cacao \
+	binutils gcc libc-dev \
+
+BROKEN_PACKAGES = \
+	$(PACKAGES_ONLY_WORK_ON_LINUX24) \
+	asterisk \
+	bitchx \
+	iptables ivorbis-tools lcd4linux \
+	ldconfig puppy \
+	qemu transcode \
