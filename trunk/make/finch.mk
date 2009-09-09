c1ae30e42e7f5d64ad1cac9f5545fd0f83e7471f
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2009-09-09 04:26:25 +0000

    finch: 2.5.8 -> 2.6.2

--- trunk/make/finch.mk
+++ trunk/make/finch.mk
@@ -7,7 +7,7 @@
 # when we have a second client also uses libpurple, we should separate it into its own ipk, and make sure stage works
 #
 FINCH_SITE=http://$(SOURCEFORGE_MIRROR)/sourceforge/pidgin
-FINCH_VERSION=2.5.8
+FINCH_VERSION=2.6.2
 FINCH_SOURCE=pidgin-$(FINCH_VERSION).tar.bz2
 FINCH_DIR=pidgin-$(FINCH_VERSION)
 FINCH_UNZIP=bzcat
@@ -133,6 +133,7 @@ endif
 		--disable-dbus \
 		--disable-perl \
 		--disable-tcl \
+		--disable-vv \
 		--disable-nls \
 		--disable-static \
 	)
