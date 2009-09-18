2d1af5336c4d533cf24b23e085ea65e4150dcc25
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2009-09-18 03:30:59 +0000

    bzr: 1.17 -> 1.18; bzrtools: 1.17.0 -> 1.18.0; bzr-svn: 0.6.4 -> 0.6.5

--- trunk/make/bzr.mk
+++ trunk/make/bzr.mk
@@ -21,8 +21,8 @@
 # from your name or email address.  If you leave MAINTAINER set to
 # "NSLU2 Linux" other developers will feel free to edit.
 #
-BZR_VERSION=1.17
-BZR_SITE=https://launchpad.net/bzr/1.17/$(BZR_VERSION)/+download
+BZR_VERSION=1.18
+BZR_SITE=https://launchpad.net/bzr/1.18/$(BZR_VERSION)/+download
 BZR_SOURCE=bzr-$(BZR_VERSION).tar.gz
 BZR_DIR=bzr-$(BZR_VERSION)
 BZR_UNZIP=zcat
