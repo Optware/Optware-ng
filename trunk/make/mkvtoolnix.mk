c425beba55f6cb4a8dc6fde3ee158a130f9d08eb
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2009-12-13 18:15:59 +0000

    mkvtoolnix: 2.9.9 -> 3.0.0

--- trunk/make/mkvtoolnix.mk
+++ trunk/make/mkvtoolnix.mk
@@ -21,7 +21,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 MKVTOOLNIX_SITE=http://bunkus.org/videotools/mkvtoolnix/sources
-MKVTOOLNIX_VERSION ?= 2.9.9
+MKVTOOLNIX_VERSION ?= 3.0.0
 MKVTOOLNIX_SOURCE=mkvtoolnix-$(MKVTOOLNIX_VERSION).tar.bz2
 MKVTOOLNIX_DIR=mkvtoolnix-$(MKVTOOLNIX_VERSION)
 MKVTOOLNIX_UNZIP=bzcat
