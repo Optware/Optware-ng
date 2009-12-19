e525058f939089be6b338dd788bcfb1013b70f63
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2009-12-19 09:23:36 +0000

    Populate pre and pre-emulator feeds with packages required for mobi.optware.*

--- trunk/platforms/packages-pre.mk
+++ trunk/platforms/packages-pre.mk
@@ -1,4 +1,18 @@
 PACKAGES = \
+	bash \
+	dropbear \
+	ez-ipupdate \
+	ipkg-opt \
 	kernel-modules \
+	libstdc++ \
+	lighttpd \
+	nano \
+	openssh \
+	openssh-sftp-server \
+	pcre \
+	pwgen \
+	readline \
+	tar \
+	zlib
 
 KERNEL_RECIPE_DIR=sources/kernel-modules/pre
