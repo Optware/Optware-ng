1a329513de1b241aac89a0b3086e9743bf8ad167
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2009-12-19 10:38:28 +0000

    Only build those that need to be different

--- platforms/packages-pre-emulator.mk
+++ platforms/packages-pre-emulator.mk
@@ -1,15 +1,4 @@
+# Only list those that need to be different from i686g25
+
 PACKAGES = \
-	bash \
-	dropbear \
-	ez-ipupdate \
-	ipkg-opt \
-	libstdc++ \
-	lighttpd \
-	nano \
-	openssh \
-	openssh-sftp-server \
-	pcre \
-	pwgen \
-	readline \
-	tar \
-	zlib
+	lighttpd
