fe5bf63fc29aacd95e0b2bd5e244e843a2f7c502
Brian Zhou <bzhou@users.noreply.github.com>
2009-12-25 19:53:46 +0000

    msort: chase tre shlib upgrade

--- make/msort.mk
+++ make/msort.mk
@@ -39,7 +39,7 @@ MSORT_CONFLICTS=
 #
 # MSORT_IPK_VERSION should be incremented when the ipk changes.
 #
-MSORT_IPK_VERSION=1
+MSORT_IPK_VERSION=2
 
 #
 # MSORT_CONFFILES should be a list of user-editable files
@@ -128,6 +128,7 @@ endif
 		$(TARGET_CONFIGURE_OPTS) \
 		CPPFLAGS="$(STAGING_CPPFLAGS) $(MSORT_CPPFLAGS)" \
 		LDFLAGS="$(STAGING_LDFLAGS) $(MSORT_LDFLAGS)" \
+		ac_cv_lib_tre_regwcomp=yes \
 		./configure \
 		--build=$(GNU_HOST_NAME) \
 		--host=$(GNU_TARGET_NAME) \
