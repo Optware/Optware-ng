15124164c2cc268499db061c20a6a99c716bedf4
Brian Zhou <bzhou@users.noreply.github.com>
2009-12-25 04:11:03 +0000

    cherokee: 0.99.37 -> 0.99.38

--- make/cherokee.mk
+++ make/cherokee.mk
@@ -21,7 +21,7 @@
 # from your name or email address.  If you leave MAINTAINER set to
 # "NSLU2 Linux" other developers will feel free to edit.
 #
-CHEROKEE_VERSION=0.99.37
+CHEROKEE_VERSION=0.99.38
 CHEROKEE_SITE=http://www.cherokee-project.com/download/0.99/$(CHEROKEE_VERSION)
 CHEROKEE_SOURCE=cherokee-$(CHEROKEE_VERSION).tar.gz
 CHEROKEE_DIR=cherokee-$(CHEROKEE_VERSION)
