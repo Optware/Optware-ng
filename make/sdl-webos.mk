54884c841390ff0bda99b94d08e5b8e62dfffe28
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2010-01-02 08:05:42 +0000

    Patched configure.in for sdl-webos

--- make/sdl-webos.mk
+++ make/sdl-webos.mk
@@ -46,7 +46,7 @@ SDL-WEBOS_CONFFILES=
 # SDL-WEBOS_PATCHES should list any patches, in the the order in
 # which they should be applied to the source code.
 #
-SDL-WEBOS_PATCHES=$(SDL-WEBOS_SOURCE_DIR)/libsdl-1.2-patch
+SDL-WEBOS_PATCHES=$(SDL-WEBOS_SOURCE_DIR)/libsdl-1.2-patch $(SDL-WEBOS_SOURCE_DIR)/configure-in.patch
 
 #
 # If the compilation of the package requires additional
@@ -113,6 +113,7 @@ $(SDL-WEBOS_BUILD_DIR)/.configured: $(DL_DIR)/$(SDL-WEBOS_SOURCE) $(SDL-WEBOS_PA
 	$(SDL-WEBOS_UNZIP) $(DL_DIR)/$(SDL-WEBOS_SOURCE) | tar -C $(BUILD_DIR) -xvf -
 	cat $(SDL-WEBOS_PATCHES) | patch -d $(BUILD_DIR)/$(SDL-WEBOS_DIR) -p1
 	mv $(BUILD_DIR)/$(SDL-WEBOS_DIR) $(SDL-WEBOS_BUILD_DIR)
+	(cd $(SDL-WEBOS_BUILD_DIR); ./autogen.sh )
 	(cd $(SDL-WEBOS_BUILD_DIR); \
 		$(TARGET_CONFIGURE_OPTS) \
 		CPPFLAGS="$(STAGING_CPPFLAGS) $(SDL-WEBOS_CPPFLAGS)" \
