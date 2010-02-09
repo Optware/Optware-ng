f3cd0c92cd9d505e428afb18a273ea27caafcafa
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2010-02-09 03:36:32 +0000

    lighttpd: 1.4.25 -> 1.4.26

--- trunk/make/lighttpd.mk
+++ trunk/make/lighttpd.mk
@@ -27,7 +27,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 LIGHTTPD_SITE=http://download.lighttpd.net/lighttpd/releases-1.4.x
-LIGHTD_VERSION=1.4.25
+LIGHTD_VERSION=1.4.26
 LIGHTTPD_SOURCE=lighttpd-$(LIGHTD_VERSION).tar.bz2
 LIGHTTPD_DIR=lighttpd-$(LIGHTD_VERSION)
 LIGHTTPD_UNZIP=bzcat
