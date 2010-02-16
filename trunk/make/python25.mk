b2e5e49e51598784957f9250fbd028fedb14dacf
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2010-02-16 19:25:49 +0000

    python25: ucs2 -> ucs4

--- trunk/make/python25.mk
+++ trunk/make/python25.mk
@@ -42,7 +42,7 @@ PYTHON25_SUGGESTS=
 #
 # PYTHON25_IPK_VERSION should be incremented when the ipk changes.
 #
-PYTHON25_IPK_VERSION=1
+PYTHON25_IPK_VERSION=2
 
 #
 # PYTHON25_CONFFILES should be a list of user-editable files
@@ -160,7 +160,7 @@ endif
 		--prefix=/opt \
 		--mandir=/opt/man \
 		--enable-shared \
-		--enable-unicode=ucs2 \
+		--enable-unicode=ucs4 \
 	)
 	touch $@
 
