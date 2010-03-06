e9380f5367705add796dbc0035dcd1c18ec26e52
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2010-03-06 07:27:04 +0000

    golang: 20100217 -> 20100304

--- trunk/make/golang.mk
+++ trunk/make/golang.mk
@@ -21,8 +21,8 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 GOLANG_HG_REPO=https://go.googlecode.com/hg
-GOLANG_HG_DATE=20100217
-GOLANG_HG_REV=53fec18b83e2
+GOLANG_HG_DATE=20100304
+GOLANG_HG_REV=a215d03e7ee1
 GOLANG_SITE=http://$(SOURCEFORGE_MIRROR)/sourceforge/golang
 GOLANG_VERSION=0.hg$(GOLANG_HG_DATE)
 GOLANG_SOURCE=golang-$(GOLANG_VERSION).tar.gz
