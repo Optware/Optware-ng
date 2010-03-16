3fbb475ef5fdf9c8faf8955e9781804638d72f1e
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2010-03-16 15:50:06 +0000

    transmission: 1.91 -> 1.92

--- trunk/make/transmission.mk
+++ trunk/make/transmission.mk
@@ -25,7 +25,7 @@
 # SVN releases also include transmissiond-dbg while official releases does not.
 #
 TRANSMISSION_SITE=http://mirrors.m0k.org/transmission/files
-TRANSMISSION_VERSION=1.91
+TRANSMISSION_VERSION=1.92
 
 #TRANSMISSION_SVN_REV=8696
 
