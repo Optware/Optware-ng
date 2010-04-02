f0c52c7a5570b004d56e049a92a888a7d79c437f
Brian Zhou <bzhou@users.noreply.github.com>
2010-04-02 04:29:16 +0000

    mpg123: 1.11.0 -> 1.12.0

--- make/mpg123.mk
+++ make/mpg123.mk
@@ -21,7 +21,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 MPG123_SITE=http://$(SOURCEFORGE_MIRROR)/sourceforge/mpg123
-MPG123_VERSION=1.11.0
+MPG123_VERSION=1.12.0
 MPG123_SOURCE=mpg123-$(MPG123_VERSION).tar.bz2
 MPG123_DIR=mpg123-$(MPG123_VERSION)
 MPG123_UNZIP=bzcat
@@ -46,7 +46,7 @@ MPG123_IPK_VERSION=1
 # MPG123_PATCHES should list any patches, in the the order in
 # which they should be applied to the source code.
 #
-MPG123_PATCHES=$(MPG123_SOURCE_DIR)/AI_ADDRCONFIG.patch
+#MPG123_PATCHES=$(MPG123_SOURCE_DIR)/AI_ADDRCONFIG.patch
 
 #
 # If the compilation of the package requires additional
