c4da3a3e3e855c881fcc2aebabeb5b2a9d25ca68
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2010-06-10 05:17:05 +0000

    Initial lists of broken packages for syno1142 targets

--- trunk/platforms/packages-syno1142mv6281.mk
+++ trunk/platforms/packages-syno1142mv6281.mk
@@ -1,8 +1,7 @@
-PACKAGES = \
-	kernel-modules \
-	squid \
-
-KERNEL_RECIPE_DIR=sources/kernel-modules/syno0844/mv6281
-
-SQUID_EPOLL := --disable-epoll
-SQUID_IPK_VERSION := 3
+BROKEN_PACKAGES = \
+         amule asterisk14-chan-capi atftp delegate emacs22 ffmpeg finch \
+         fuppes gconv-modules ghostscript gphoto2 libgphoto2 iptables iptraf \
+         ldconfig libcapi20 libdlna make minidlna motion mpd \
+         mt-daapd-svn nfs-utils nget ntop opendchub openser opensips \
+         phoneme-advanced puppy qemu sox transcode transmission ushare \
+         vlc vsftpd
