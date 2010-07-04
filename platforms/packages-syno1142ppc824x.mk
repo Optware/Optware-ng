55ea626e452e9192bb9bf5f7ffee08af086e5c6b
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2010-07-04 22:26:50 +0000

    Broken packages for syno1142ppx824x

---
+++ platforms/packages-syno1142ppc824x.mk
@@ -0,0 +1,5 @@
+BROKEN_PACKAGES = \
+         asterisk14 asterisk14-chan-capi asterisk16 asterisk16-addons btg cairo erl-ejabberd \
+         gconv-modules iptables iptraf ldconfig libcapi20 mt-daapd net-snmp \
+         ntop nut opendchub openser opensips pango pixman \
+         puppy hplip sandbox vsftpd
