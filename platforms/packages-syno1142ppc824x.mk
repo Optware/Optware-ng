c46b855ac520e1d8c7a75cf90de86ec94a9f1c3b
rwhitby <rwhitby@1f1dca69-5516-0410-b834-e485effd04c3>
2010-07-04 05:59:14 +0000

    syno1142ppc824x toolchain

--- platforms/packages-syno1142ppc824x.mk
+++
@@ -1,7 +0,0 @@
-PACKAGES = \
-	kernel-modules \
-	module-init-tools \
-
-KERNEL_RECIPE_DIR=sources/kernel-modules/syno0844/ppc824x
-
-MODULE_INIT_TOOLS_CONFIGURE_OPTIONS := --with-moddir=/opt/lib/modules
