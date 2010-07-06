2e40a5aca4b5ae8590d2df6add7eb928bd6f7dd2
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2010-07-06 20:37:46 +0000

    pcre: 8.02 -> 8.10

--- trunk/make/pcre.mk
+++ trunk/make/pcre.mk
@@ -22,7 +22,7 @@
 #
 
 PCRE_SITE=ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre
-PCRE_VERSION=8.02
+PCRE_VERSION=8.10
 PCRE_SOURCE=pcre-$(PCRE_VERSION).tar.bz2
 PCRE_DIR=pcre-$(PCRE_VERSION)
 PCRE_UNZIP=bzcat
