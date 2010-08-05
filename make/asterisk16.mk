8208e642b78d4bc74890e6e3202338691a1c9eec
osas <osas@1f1dca69-5516-0410-b834-e485effd04c3>
2010-08-05 14:23:29 +0000

    asterisk16: 1.6.9 -> 1.6.10

--- make/asterisk16.mk
+++ make/asterisk16.mk
@@ -24,7 +24,7 @@ ASTERISK16_SOURCE_TYPE=tarball
 #ASTERISK16_SOURCE_TYPE=svn
 
 ASTERISK16_SITE=http://downloads.digium.com/pub/asterisk/releases
-ASTERISK16_BASE_VERSION=1.6.2.9
+ASTERISK16_BASE_VERSION=1.6.2.10
 
 ifeq ($(ASTERISK16_SOURCE_TYPE), svn)
 ASTERISK16_SVN=http://svn.digium.com/svn/asterisk/branches/1.6.1
