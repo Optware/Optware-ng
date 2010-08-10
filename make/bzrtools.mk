48542f137676bff1f4f6eca2d8fe48881b8fc332
Brian Zhou <bzhou@users.noreply.github.com>
2010-08-10 23:55:56 +0000

    bzrtools: 2.1.0 -> 2.2.0

--- make/bzrtools.mk
+++ make/bzrtools.mk
@@ -21,8 +21,8 @@
 # from your name or email address.  If you leave MAINTAINER set to
 # "NSLU2 Linux" other developers will feel free to edit.
 #
-BZRTOOLS_VERSION=2.1.0
-BZRTOOLS_SITE=http://launchpad.net/bzrtools/trunk/2.1.0/+download
+BZRTOOLS_VERSION=2.2.0
+BZRTOOLS_SITE=http://launchpad.net/bzrtools/stable/2.2.0/+download
 BZRTOOLS_SOURCE=bzrtools-$(BZRTOOLS_VERSION).tar.gz
 BZRTOOLS_DIR=bzrtools
 BZRTOOLS_UNZIP=zcat
