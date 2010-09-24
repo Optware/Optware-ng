633fb7ec68565c1121f8f0d300ccc8b0bbf3fc27
osas <osas@1f1dca69-5516-0410-b834-e485effd04c3>
2010-09-24 14:34:11 +0000

    asterisk14-extra-sounds-en-g729: 1.4.7 > 1.4.11

--- trunk/make/asterisk14-extra-sounds-en-g729.mk
+++ trunk/make/asterisk14-extra-sounds-en-g729.mk
@@ -20,8 +20,8 @@
 # from your name or email address.  If you leave MAINTAINER set to
 # "NSLU2 Linux" other developers will feel free to edit.
 #
-ASTERISK14_EXTRA_SOUNDS_EN_G729_SITE=http://downloads.digium.com/pub/telephony/sounds/releases
-ASTERISK14_EXTRA_SOUNDS_EN_G729_VERSION=1.4.7
+ASTERISK14_EXTRA_SOUNDS_EN_G729_SITE=http://downloads.asterisk.org/pub/telephony/sounds/releases
+ASTERISK14_EXTRA_SOUNDS_EN_G729_VERSION=1.4.11
 ASTERISK14_EXTRA_SOUNDS_EN_G729_SOURCE=asterisk-extra-sounds-en-g729-$(ASTERISK14_EXTRA_SOUNDS_EN_G729_VERSION).tar.gz
 ASTERISK14_EXTRA_SOUNDS_EN_G729_DIR=asterisk-extra-sounds-en-g729-$(ASTERISK14_EXTRA_SOUNDS_EN_G729_VERSION)
 ASTERISK14_EXTRA_SOUNDS_EN_G729_UNZIP=zcat
