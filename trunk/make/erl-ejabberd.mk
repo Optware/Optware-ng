51d24b86803d0fead6958d5c7c3e0354c60cc413
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2010-09-30 22:38:28 +0000

    erl-ejabberd: 2.1.3 -> 2.1.5

--- trunk/make/erl-ejabberd.mk
+++ trunk/make/erl-ejabberd.mk
@@ -20,7 +20,7 @@
 # from your name or email address.  If you leave MAINTAINER set to
 # "NSLU2 Linux" other developers will feel free to edit.
 #
-ERL_EJABBERD_VERSION=2.1.3
+ERL_EJABBERD_VERSION=2.1.5
 ERL_EJABBERD_SITE=http://www.process-one.net/downloads/ejabberd/$(ERL_EJABBERD_VERSION)
 ERL_EJABBERD_SOURCE=ejabberd-$(ERL_EJABBERD_VERSION).tar.gz
 ERL_EJABBERD_DIR=ejabberd-$(ERL_EJABBERD_VERSION)
