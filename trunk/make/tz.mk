68714ad65eb48c2806e8084b28357f0a35f30eaf
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2010-10-25 21:24:10 +0000

    updated tzcode and tzdata to 2010n

--- trunk/make/tz.mk
+++ trunk/make/tz.mk
@@ -21,8 +21,8 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 TZ_SITE=ftp://elsie.nci.nih.gov/pub
-TZ_CODE_VERSION=2010f
-TZ_DATA_VERSION=2010i
+TZ_CODE_VERSION=2010n
+TZ_DATA_VERSION=2010n
 TZ_VERSION=$(TZ_DATA_VERSION)
 TZ_CODE_SOURCE=tzcode$(TZ_CODE_VERSION).tar.gz
 TZ_DATA_SOURCE=tzdata$(TZ_DATA_VERSION).tar.gz
