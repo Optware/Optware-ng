49e78d51bf045ec974629c801329d120f8ea15a2
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2010-12-11 23:59:27 +0000

    c-ares: 1.7.3 -> 1.7.4

--- trunk/make/c-ares.mk
+++ trunk/make/c-ares.mk
@@ -21,7 +21,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 C_ARES_SITE=http://daniel.haxx.se/projects/c-ares
-C_ARES_VERSION=1.7.3
+C_ARES_VERSION=1.7.4
 C_ARES_SOURCE=c-ares-$(C_ARES_VERSION).tar.gz
 C_ARES_DIR=c-ares-$(C_ARES_VERSION)
 C_ARES_UNZIP=zcat
