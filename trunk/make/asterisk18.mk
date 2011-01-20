1606dd14cc06b767d81a4ea169abcb1754960bb2
osas <osas@1f1dca69-5516-0410-b834-e485effd04c3>
2011-01-20 22:56:41 +0000

    asterisk18: 1.8.2 -> 1.8.2.2

--- trunk/make/asterisk18.mk
+++ trunk/make/asterisk18.mk
@@ -24,7 +24,7 @@ ASTERISK18_SOURCE_TYPE=tarball
 #ASTERISK18_SOURCE_TYPE=svn
 
 ASTERISK18_SITE=http://downloads.digium.com/pub/asterisk/releases
-ASTERISK18_BASE_VERSION=1.8.2
+ASTERISK18_BASE_VERSION=1.8.2.2
 
 ifeq ($(ASTERISK18_SOURCE_TYPE), svn)
 ASTERISK18_SVN=http://svn.digium.com/svn/asterisk/branches/1.8.0
