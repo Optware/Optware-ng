a1fe5f28d85411ae133ca536e2de328ea4841cad
Brian Zhou <bzhou@users.noreply.github.com>
2011-03-26 15:22:50 +0000

    tor: 0.2.1.29 -> 0.2.1.30

--- make/tor.mk
+++ make/tor.mk
@@ -21,7 +21,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 TOR_SITE=http://www.torproject.org/dist
-TOR_VERSION=0.2.1.29
+TOR_VERSION=0.2.1.30
 TOR_SOURCE=tor-$(TOR_VERSION).tar.gz
 TOR_DIR=tor-$(TOR_VERSION)
 TOR_UNZIP=zcat
