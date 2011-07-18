6c5cac23513294df351327aa03c2d035fde84cdb
Brian Zhou <bzhou@users.noreply.github.com>
2011-07-18 04:06:17 +0000

    discount: 2.0.8 -> 2.1.0

--- make/discount.mk
+++ make/discount.mk
@@ -21,7 +21,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 DISCOUNT_SITE=http://www.pell.portland.or.us/~orc/Code/markdown
-DISCOUNT_VERSION=2.0.8
+DISCOUNT_VERSION=2.1.0
 DISCOUNT_SOURCE=discount-$(DISCOUNT_VERSION).tar.bz2
 DISCOUNT_DIR=discount-$(DISCOUNT_VERSION)
 DISCOUNT_UNZIP=bzcat
