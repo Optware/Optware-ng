ff3f020f2042d79975e358e1ad4d7432ede71353
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2011-07-25 04:11:10 +0000

    nginx: 1.0.2 -> 1.0.5

--- trunk/make/nginx.mk
+++ trunk/make/nginx.mk
@@ -21,7 +21,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 NGINX_SITE=http://sysoev.ru/nginx
-NGINX_VERSION?=1.0.2
+NGINX_VERSION?=1.0.5
 NGINX_SOURCE=nginx-$(NGINX_VERSION).tar.gz
 NGINX_DIR=nginx-$(NGINX_VERSION)
 NGINX_UNZIP=zcat
