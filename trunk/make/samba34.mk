d1e064da34bb38926c46a242cbd59cccb5f5f15f
Sebastian <Sebastian@1f1dca69-5516-0410-b834-e485effd04c3>
2011-08-04 19:35:34 +0000

    samba34: 3.4.13 -> 3.4.14

--- trunk/make/samba34.mk
+++ trunk/make/samba34.mk
@@ -20,8 +20,8 @@
 # You should change all these variables to suit your package.
 #
 SAMBA34_SITE=http://www.samba.org/samba/ftp/stable
-SAMBA34_VERSION ?= 3.4.13
-SAMBA34_IPK_VERSION ?= 2
+SAMBA34_VERSION ?= 3.4.14
+SAMBA34_IPK_VERSION ?= 1
 SAMBA34_SOURCE=samba-$(SAMBA34_VERSION).tar.gz
 SAMBA34_DIR=samba-$(SAMBA34_VERSION)
 SAMBA34_UNZIP=zcat
