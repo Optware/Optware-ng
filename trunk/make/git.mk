a5fd9aefd91bee9f0c9f52ee5ae1eae9d0cd0cbf
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2011-11-10 02:46:13 +0000

    git: 1.7.7.2 -> 1.7.7.3

--- trunk/make/git.mk
+++ trunk/make/git.mk
@@ -22,7 +22,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 GIT_SITE=http://git-core.googlecode.com/files
-GIT_VERSION=1.7.7.2
+GIT_VERSION=1.7.7.3
 GIT_IPK_VERSION=1
 GIT_SOURCE=git-$(GIT_VERSION).tar.gz
 GIT_DIR=git-$(GIT_VERSION)
