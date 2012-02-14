b4f8063b05135a75a93a127c919edd023d168781
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2012-02-14 07:04:02 +0000

    python27: same _sha256 _sha512 change for python27

--- trunk/make/python27.mk
+++ trunk/make/python27.mk
@@ -42,7 +42,7 @@ PYTHON27_SUGGESTS=
 #
 # PYTHON27_IPK_VERSION should be incremented when the ipk changes.
 #
-PYTHON27_IPK_VERSION=1
+PYTHON27_IPK_VERSION=2
 
 #
 # PYTHON27_CONFFILES should be a list of user-editable files
