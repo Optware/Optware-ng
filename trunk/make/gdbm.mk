719cc2c8060fa4fe14996499165a1ccc74fc5e0a
bzhou <bzhou@1f1dca69-5516-0410-b834-e485effd04c3>
2012-02-17 19:36:04 +0000

    gdbm: mt-daapd needs libgdbm.a, do not disable-static for now

--- trunk/make/gdbm.mk
+++ trunk/make/gdbm.mk
@@ -18,7 +18,7 @@ GDBM_DEPENDS=
 GDBM_SUGGESTS=
 GDBM_CONFLICTS=
 
-GDBM_IPK_VERSION=2
+GDBM_IPK_VERSION=3
 
 GDBM_PATCHES=$(GDBM_SOURCE_DIR)/Makefile.patch
 
@@ -47,9 +47,8 @@ $(GDBM_BUILD_DIR)/.configured: $(DL_DIR)/$(GDBM_SOURCE) $(GDBM_PATCHES) make/gdb
 		--target=$(GNU_TARGET_NAME) \
 		--prefix=/opt \
 		--disable-nls \
-		--disable-static \
 	);
-	touch $(GDBM_BUILD_DIR)/.configured
+	touch $@
 
 gdbm-unpack: $(GDBM_BUILD_DIR)/.configured
 
