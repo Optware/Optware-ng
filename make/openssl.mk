8e9c156468309e117f75e92241fda1deec434eee
BobNovas <BobNovas@1f1dca69-5516-0410-b834-e485effd04c3>
2012-04-16 14:23:33 +0000

    Tweaked openssl.mk to take an optional make argument OPENSSL_VERSION. If OPENSSL_VERSION is not specified, then openssl.mk does exactly what it did previously, e.g., makes openssl 0.9.7 or 0.9.8 depending on target. If OPENSSL_VERSION is specified, like this:
       make OPENSSL_VERSION=1.0.1 openssl[-stage | -ipk]
    
    then openssl.mk will make that version of openssl. 
    
    Note that if you use the ipk file to install openssl 1.0.x, it will remove any older version of libcrypto and 
    libssl. So, copy libcrypto.so.0.9.x and libssl.0.9.x to a safe place, install openssl 1.0.x, copy libcrypto.so.0.9.x
    and libssl.so.0.9.x back again and manually fix up the symbolic links.
    
    Also note that the openssl 1.0.1 Makefile names the libraries 1.0.0.

--- make/openssl.mk
+++ make/openssl.mk
@@ -4,17 +4,25 @@
 
 OPENSSL_SITE=http://www.openssl.org/source
 
-ifneq ($(OPTWARE_TARGET), $(filter \
+ifeq ($(OPENSSL_VERSION), 1.0.1)
+export OPENSSL_VERSION := 1.0.1
+export OPENSSL_LIB_VERSION := 1.0.0
+export OPENSSL_IPK_VERSION := 101
+else ifeq ($(OPENSSL_VERSION), 1.0.0)
+export OPENSSL_VERSION := 1.0.0h
+export OPENSSL_LIB_VERSION := 1.0.0
+export OPENSSL_IPK_VERSION := 100
+else ifneq ($(OPTWARE_TARGET), $(filter \
 	cs04q3armel cs05q3armel cs06q3armel ddwrt dns323 ds101 ds101g fsg3 fsg3v4 gumstix1151 mss \
 	nslu2 oleg openwrt-brcm24 openwrt-ixp4xx slugosbe slugosle syno-x07 ts101 ts72xx vt4 wl500g, \
 	$(OPTWARE_TARGET)))
-OPENSSL_VERSION=0.9.8p
-OPENSSL_LIB_VERSION=0.9.8
-OPENSSL_IPK_VERSION=1
+export OPENSSL_VERSION := 0.9.8p
+export OPENSSL_LIB_VERSION := 0.9.8
+export OPENSSL_IPK_VERSION := 1
 else
-OPENSSL_VERSION=0.9.7m
-OPENSSL_LIB_VERSION=0.9.7
-OPENSSL_IPK_VERSION=5
+export OPENSSL_VERSION := 0.9.7m
+export OPENSSL_LIB_VERSION := 0.9.7
+export OPENSSL_IPK_VERSION := 5
 endif
 
 OPENSSL_SOURCE=openssl-$(OPENSSL_VERSION).tar.gz
