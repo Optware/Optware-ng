c14ea0f20e67cd7bb351f5ec10dca085e535cfdb
osas <osas@1f1dca69-5516-0410-b834-e485effd04c3>
2013-02-03 23:01:54 +0000

    opensips: promoted on syno1142mv5281

--- trunk/platforms/packages-syno1142mv5281.mk
+++ trunk/platforms/packages-syno1142mv5281.mk
@@ -1,5 +1,5 @@
 BROKEN_PACKAGES = \
-         9base amule apache apr-util asterisk14-chan-capi asterisk16 asterisk16-addons \
+         9base amule apache apr-util \
          avahi btg btpd busybox bluez-utils bluez-hcidump centerim \
          classpath cups cyrus-imapd davtools dbus dbus-glib dbus-python \
          dump e2fsprogs e2tools finch fuppes gambit-c gconv-modules \
@@ -8,7 +8,7 @@ BROKEN_PACKAGES = \
          iptraf ivorbis-tools jamvm ldconfig libcapi20 libextractor libshout \
          libtiff libtorrent-rasterbar libvorbis linphone mc mimms minidlna \
          mkvtoolnix moc modutils mod-fastcgi mpd mt-daapd mt-daapd-svn \
-         net-snmp nfs-utils ntop opendchub openser opensips p7zip \
+         net-snmp nfs-utils ntop opendchub p7zip \
          php-apache puppy hplip mod-python mod-wsgi subvertpy py-lxml \
          qemu sablevm samba samba34 samba35 sandbox sane-backends \
          socat sox spandsp splix streamripper svn tesseract-ocr \
