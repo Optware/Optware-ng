b153a0daff2125bc30440963a76c0907f2bc509e
osas <osas@1f1dca69-5516-0410-b834-e485effd04c3>
2013-02-03 23:01:31 +0000

    opensips: promoted on syno1142ppc824x

--- trunk/platforms/packages-syno1142ppc824x.mk
+++ trunk/platforms/packages-syno1142ppc824x.mk
@@ -1,5 +1,5 @@
 BROKEN_PACKAGES = \
-         asterisk14 asterisk14-chan-capi asterisk16 asterisk16-addons btg cairo erl-ejabberd \
+         btg cairo erl-ejabberd \
          gconv-modules iptraf ldconfig libcapi20 mt-daapd net-snmp \
-         ntop nut opendchub openser opensips pango pixman \
+         ntop nut opendchub pango pixman \
          puppy hplip sandbox vsftpd
