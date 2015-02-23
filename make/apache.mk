b9698b9231a899b8644f69746e4edcbf2112ae0a
alllexx88 <opotapenko@gmail.com>
2015-02-23 22:30:54 +0200

    apache: 2.4.9 -> 2.4.12

--- make/apache.mk
+++ make/apache.mk
@@ -14,7 +14,7 @@
 # It is usually "zcat" (for .gz) or "bzcat" (for .bz2)
 #
 APACHE_SITE=http://archive.apache.org/dist/httpd
-APACHE_VERSION=2.4.9
+APACHE_VERSION=2.4.12
 APACHE_SOURCE=httpd-$(APACHE_VERSION).tar.bz2
 APACHE_DIR=httpd-$(APACHE_VERSION)
 APACHE_UNZIP=bzcat
