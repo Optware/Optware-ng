9e50df634a008153a2ae631c5be228e55ee7150f
alllexx88 <opotapenko@gmail.com>
2015-04-19 12:05:51 +0300

    ipython: add python27 and python3 extensnions

--- make/ipython.mk
+++ make/ipython.mk
@@ -21,10 +21,13 @@
 # from your name or email address.  If you leave MAINTAINER set to
 # "NSLU2 Linux" other developers will feel free to edit.
 #
-IPYTHON_SITE=http://ipython.scipy.org/dist
-IPYTHON_VERSION=0.10.2
+IPYTHON_SITE=https://pypi.python.org/packages/source/i/ipython
+IPYTHON_VERSION=3.1.0
+IPYTHON_VERSION_OLD=0.10.2
 IPYTHON_SOURCE=ipython-$(IPYTHON_VERSION).tar.gz
+IPYTHON_SOURCE_OLD=ipython-$(IPYTHON_VERSION_OLD).tar.gz
 IPYTHON_DIR=ipython-$(IPYTHON_VERSION)
+IPYTHON_DIR_OLD=ipython-$(IPYTHON_VERSION_OLD)
 IPYTHON_UNZIP=zcat
 IPYTHON_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
 IPYTHON_DESCRIPTION=An enhanced interactive Python shell
@@ -32,7 +35,14 @@ IPYTHON_SECTION=misc
 IPYTHON_PRIORITY=optional
 IPYTHON_PY25_DEPENDS=python25, py25-setuptools
 IPYTHON_PY26_DEPENDS=python26, py26-setuptools
+IPYTHON_PY26_DEPENDS=python27, py27-setuptools
+IPYTHON_PY3_DEPENDS=python3, py3-setuptools
 IPYTHON_SUGGESTS=ipython-common
+ifneq ($(IPYTHON_VERSION), $(IPYTHON_VERSION_OLD))
+IPYTHON_SUGGESTS_OLD=ipython-common-old
+else
+IPYTHON_SUGGESTS_OLD=ipython-common
+endif
 IPYTHON_CONFLICTS=
 
 #
@@ -72,11 +82,22 @@ IPYTHON_SOURCE_DIR=$(SOURCE_DIR)/ipython
 IPYTHON-COMMON_IPK_DIR=$(BUILD_DIR)/ipython-common-$(IPYTHON_VERSION)-ipk
 IPYTHON-COMMON_IPK=$(BUILD_DIR)/ipython-common_$(IPYTHON_VERSION)-$(IPYTHON_IPK_VERSION)_$(TARGET_ARCH).ipk
 
-IPYTHON_PY25_IPK_DIR=$(BUILD_DIR)/py25-ipython-$(IPYTHON_VERSION)-ipk
-IPYTHON_PY25_IPK=$(BUILD_DIR)/py25-ipython_$(IPYTHON_VERSION)-$(IPYTHON_IPK_VERSION)_$(TARGET_ARCH).ipk
+ifneq ($(IPYTHON_VERSION), $(IPYTHON_VERSION_OLD))
+IPYTHON-COMMON-OLD_IPK_DIR=$(BUILD_DIR)/ipython-common-old-$(IPYTHON_VERSION_OLD)-ipk
+IPYTHON-COMMON-OLD_IPK=$(BUILD_DIR)/ipython-common-old_$(IPYTHON_VERSION_OLD)-$(IPYTHON_IPK_VERSION)_$(TARGET_ARCH).ipk
+endif
 
-IPYTHON_PY26_IPK_DIR=$(BUILD_DIR)/py26-ipython-$(IPYTHON_VERSION)-ipk
-IPYTHON_PY26_IPK=$(BUILD_DIR)/py26-ipython_$(IPYTHON_VERSION)-$(IPYTHON_IPK_VERSION)_$(TARGET_ARCH).ipk
+IPYTHON_PY25_IPK_DIR=$(BUILD_DIR)/py25-ipython-$(IPYTHON_VERSION_OLD)-ipk
+IPYTHON_PY25_IPK=$(BUILD_DIR)/py25-ipython_$(IPYTHON_VERSION_OLD)-$(IPYTHON_IPK_VERSION)_$(TARGET_ARCH).ipk
+
+IPYTHON_PY26_IPK_DIR=$(BUILD_DIR)/py26-ipython-$(IPYTHON_VERSION_OLD)-ipk
+IPYTHON_PY26_IPK=$(BUILD_DIR)/py26-ipython_$(IPYTHON_VERSION_OLD)-$(IPYTHON_IPK_VERSION)_$(TARGET_ARCH).ipk
+
+IPYTHON_PY27_IPK_DIR=$(BUILD_DIR)/py27-ipython-$(IPYTHON_VERSION)-ipk
+IPYTHON_PY27_IPK=$(BUILD_DIR)/py27-ipython_$(IPYTHON_VERSION)-$(IPYTHON_IPK_VERSION)_$(TARGET_ARCH).ipk
+
+IPYTHON_PY3_IPK_DIR=$(BUILD_DIR)/py3-ipython-$(IPYTHON_VERSION)-ipk
+IPYTHON_PY3_IPK=$(BUILD_DIR)/py3-ipython_$(IPYTHON_VERSION)-$(IPYTHON_IPK_VERSION)_$(TARGET_ARCH).ipk
 
 .PHONY: ipython-source ipython-unpack ipython ipython-stage ipython-ipk ipython-clean ipython-dirclean ipython-check
 
@@ -86,7 +107,13 @@ IPYTHON_PY26_IPK=$(BUILD_DIR)/py26-ipython_$(IPYTHON_VERSION)-$(IPYTHON_IPK_VERS
 #
 $(DL_DIR)/$(IPYTHON_SOURCE):
 	$(WGET) -P $(@D) $(IPYTHON_SITE)/$(@F) || \
-	$(WGET) -P $(@D) $(IPYTHON_SITE)/old/$(@F)
+	$(WGET) -P $(@D) $(SOURCES_NLO_SITE)/$(@F)
+
+ifneq ($(IPYTHON_VERSION), $(IPYTHON_VERSION_OLD))
+$(DL_DIR)/$(IPYTHON_SOURCE_OLD):
+	$(WGET) -P $(@D) $(IPYTHON_SITE)/$(@F) || \
+	$(WGET) -P $(@D) $(SOURCES_NLO_SITE)/$(@F)
+endif
 
 #
 # The source code depends on it existing within the download directory.
@@ -110,28 +137,49 @@ ipython-source: $(DL_DIR)/$(IPYTHON_SOURCE) $(IPYTHON_PATCHES)
 # If the compilation of the package requires other packages to be staged
 # first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
 #
-$(IPYTHON_BUILD_DIR)/.configured: $(DL_DIR)/$(IPYTHON_SOURCE) $(IPYTHON_PATCHES)
-	$(MAKE) py-setuptools-stage
+$(IPYTHON_BUILD_DIR)/.configured: $(DL_DIR)/$(IPYTHON_SOURCE) $(DL_DIR)/$(IPYTHON_SOURCE_OLD) \
+								$(IPYTHON_PATCHES) make/ipython.mk
+	$(MAKE) py-setuptools-host-stage
 	rm -rf $(IPYTHON_BUILD_DIR)
 	mkdir -p $(IPYTHON_BUILD_DIR)
 	# 2.5
 	rm -rf $(BUILD_DIR)/$(IPYTHON_DIR)
-	$(IPYTHON_UNZIP) $(DL_DIR)/$(IPYTHON_SOURCE) | tar -C $(BUILD_DIR) -xvf -
-#	cat $(IPYTHON_PATCHES) | patch -d $(BUILD_DIR)/$(IPYTHON_DIR) -p1
-	mv $(BUILD_DIR)/$(IPYTHON_DIR) $(@D)/2.5
+	$(IPYTHON_UNZIP) $(DL_DIR)/$(IPYTHON_SOURCE_OLD) | tar -C $(BUILD_DIR) -xvf -
+#	cat $(IPYTHON_PATCHES) | patch -d $(BUILD_DIR)/$(IPYTHON_DIR_OLD) -p1
+	mv $(BUILD_DIR)/$(IPYTHON_DIR_OLD) $(@D)/2.5
 	(cd $(@D)/2.5; \
 	    (echo "[build_scripts]"; \
 	    echo "executable=/opt/bin/python2.5") >> setup.cfg \
 	)
 	# 2.6
 	rm -rf $(BUILD_DIR)/$(IPYTHON_DIR)
-	$(IPYTHON_UNZIP) $(DL_DIR)/$(IPYTHON_SOURCE) | tar -C $(BUILD_DIR) -xvf -
-#	cat $(IPYTHON_PATCHES) | patch -d $(BUILD_DIR)/$(IPYTHON_DIR) -p1
-	mv $(BUILD_DIR)/$(IPYTHON_DIR) $(@D)/2.6
+	$(IPYTHON_UNZIP) $(DL_DIR)/$(IPYTHON_SOURCE_OLD) | tar -C $(BUILD_DIR) -xvf -
+#	cat $(IPYTHON_PATCHES) | patch -d $(BUILD_DIR)/$(IPYTHON_DIR_OLD) -p1
+	mv $(BUILD_DIR)/$(IPYTHON_DIR_OLD) $(@D)/2.6
 	(cd $(@D)/2.6; \
 	    (echo "[build_scripts]"; \
 	    echo "executable=/opt/bin/python2.6") >> setup.cfg \
 	)
+	# 2.7
+	rm -rf $(BUILD_DIR)/$(IPYTHON_DIR)
+	$(IPYTHON_UNZIP) $(DL_DIR)/$(IPYTHON_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+#	cat $(IPYTHON_PATCHES) | patch -d $(BUILD_DIR)/$(IPYTHON_DIR) -p1
+	mv $(BUILD_DIR)/$(IPYTHON_DIR) $(@D)/2.7
+	(cd $(@D)/2.7; \
+	    (echo "[build_scripts]"; \
+	    echo "executable=/opt/bin/python2.7") >> setup.cfg \
+	)
+	# 3
+	rm -rf $(BUILD_DIR)/$(IPYTHON_DIR)
+	$(IPYTHON_UNZIP) $(DL_DIR)/$(IPYTHON_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+#	cat $(IPYTHON_PATCHES) | patch -d $(BUILD_DIR)/$(IPYTHON_DIR) -p1
+	mv $(BUILD_DIR)/$(IPYTHON_DIR) $(@D)/3
+	(cd $(@D)/3; \
+	    (echo "[build_scripts]"; \
+	    echo "executable=/opt/bin/python$(PYTHON3_VERSION_MAJOR)") >> setup.cfg \
+	)
+#	see http://stackoverflow.com/questions/16771894/python-nameerror-global-name-file-is-not-defined
+	sed -i -e 's/__file__/"&"/' $(@D)/2.7/setup.py $(@D)/3/setup.py
 	touch $@
 
 ipython-unpack: $(IPYTHON_BUILD_DIR)/.configured
@@ -142,11 +190,13 @@ ipython-unpack: $(IPYTHON_BUILD_DIR)/.configured
 $(IPYTHON_BUILD_DIR)/.built: $(IPYTHON_BUILD_DIR)/.configured
 	rm -f $@
 	(cd $(@D)/2.5; \
-		PYTHONPATH=$(STAGING_LIB_DIR)/python2.5/site-packages \
 		$(HOST_STAGING_PREFIX)/bin/python2.5 -c "import setuptools; execfile('setup.py')" build)
 	(cd $(@D)/2.6; \
-		PYTHONPATH=$(STAGING_LIB_DIR)/python2.6/site-packages \
 		$(HOST_STAGING_PREFIX)/bin/python2.6 -c "import setuptools; execfile('setup.py')" build)
+	(cd $(@D)/2.7; \
+		$(HOST_STAGING_PREFIX)/bin/python2.7 -c "import setuptools; execfile('setup.py')" build)
+	(cd $(@D)/3; \
+		$(HOST_STAGING_PREFIX)/bin/python$(PYTHON3_VERSION_MAJOR) setup.py build)
 	touch $@
 
 #
@@ -178,9 +228,25 @@ $(IPYTHON-COMMON_IPK_DIR)/CONTROL/control:
 	@echo "Version: $(IPYTHON_VERSION)-$(IPYTHON_IPK_VERSION)" >>$@
 	@echo "Maintainer: $(IPYTHON_MAINTAINER)" >>$@
 	@echo "Source: $(IPYTHON_SITE)/$(IPYTHON_SOURCE)" >>$@
-	@echo "Description: $(IPYTHON_DESCRIPTION)" >>$@
+	@echo "Description: $(IPYTHON_DESCRIPTION). Common files for $(IPYTHON_VERSION)" >>$@
 	@echo "Depends: " >>$@
 	@echo "Conflicts: $(IPYTHON_CONFLICTS)" >>$@
+
+ifneq ($(IPYTHON_VERSION), $(IPYTHON_VERSION_OLD))
+$(IPYTHON-COMMON-OLD_IPK_DIR)/CONTROL/control:
+	@install -d $(@D)
+	@rm -f $@
+	@echo "Package: ipython-common-old" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(IPYTHON_PRIORITY)" >>$@
+	@echo "Section: $(IPYTHON_SECTION)" >>$@
+	@echo "Version: $(IPYTHON_VERSION_OLD)-$(IPYTHON_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(IPYTHON_MAINTAINER)" >>$@
+	@echo "Source: $(IPYTHON_SITE)/$(IPYTHON_SOURCE_OLD)" >>$@
+	@echo "Description: $(IPYTHON_DESCRIPTION). Common files for $(IPYTHON_VERSION_OLD)" >>$@
+	@echo "Depends: " >>$@
+	@echo "Conflicts: $(IPYTHON_CONFLICTS)" >>$@
+endif
 
 $(IPYTHON_PY25_IPK_DIR)/CONTROL/control:
 	@install -d $(@D)
@@ -189,12 +255,12 @@ $(IPYTHON_PY25_IPK_DIR)/CONTROL/control:
 	@echo "Architecture: $(TARGET_ARCH)" >>$@
 	@echo "Priority: $(IPYTHON_PRIORITY)" >>$@
 	@echo "Section: $(IPYTHON_SECTION)" >>$@
-	@echo "Version: $(IPYTHON_VERSION)-$(IPYTHON_IPK_VERSION)" >>$@
+	@echo "Version: $(IPYTHON_VERSION_OLD)-$(IPYTHON_IPK_VERSION)" >>$@
 	@echo "Maintainer: $(IPYTHON_MAINTAINER)" >>$@
-	@echo "Source: $(IPYTHON_SITE)/$(IPYTHON_SOURCE)" >>$@
+	@echo "Source: $(IPYTHON_SITE)/$(IPYTHON_SOURCE_OLD)" >>$@
 	@echo "Description: $(IPYTHON_DESCRIPTION)" >>$@
 	@echo "Depends: $(IPYTHON_PY25_DEPENDS)" >>$@
-	@echo "Suggests: $(IPYTHON_SUGGESTS)" >>$@
+	@echo "Suggests: $(IPYTHON_SUGGESTS_OLD)" >>$@
 	@echo "Conflicts: $(IPYTHON_CONFLICTS)" >>$@
 
 $(IPYTHON_PY26_IPK_DIR)/CONTROL/control:
@@ -204,11 +270,41 @@ $(IPYTHON_PY26_IPK_DIR)/CONTROL/control:
 	@echo "Architecture: $(TARGET_ARCH)" >>$@
 	@echo "Priority: $(IPYTHON_PRIORITY)" >>$@
 	@echo "Section: $(IPYTHON_SECTION)" >>$@
+	@echo "Version: $(IPYTHON_VERSION_OLD)-$(IPYTHON_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(IPYTHON_MAINTAINER)" >>$@
+	@echo "Source: $(IPYTHON_SITE)/$(IPYTHON_SOURCE_OLD)" >>$@
+	@echo "Description: $(IPYTHON_DESCRIPTION)" >>$@
+	@echo "Depends: $(IPYTHON_PY26_DEPENDS)" >>$@
+	@echo "Suggests: $(IPYTHON_SUGGESTS_OLD)" >>$@
+	@echo "Conflicts: $(IPYTHON_CONFLICTS)" >>$@
+
+$(IPYTHON_PY27_IPK_DIR)/CONTROL/control:
+	@install -d $(@D)
+	@rm -f $@
+	@echo "Package: py27-ipython" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(IPYTHON_PRIORITY)" >>$@
+	@echo "Section: $(IPYTHON_SECTION)" >>$@
 	@echo "Version: $(IPYTHON_VERSION)-$(IPYTHON_IPK_VERSION)" >>$@
 	@echo "Maintainer: $(IPYTHON_MAINTAINER)" >>$@
 	@echo "Source: $(IPYTHON_SITE)/$(IPYTHON_SOURCE)" >>$@
 	@echo "Description: $(IPYTHON_DESCRIPTION)" >>$@
-	@echo "Depends: $(IPYTHON_PY26_DEPENDS)" >>$@
+	@echo "Depends: $(IPYTHON_PY27_DEPENDS)" >>$@
+	@echo "Suggests: $(IPYTHON_SUGGESTS)" >>$@
+	@echo "Conflicts: $(IPYTHON_CONFLICTS)" >>$@
+
+$(IPYTHON_PY3_IPK_DIR)/CONTROL/control:
+	@install -d $(@D)
+	@rm -f $@
+	@echo "Package: py3-ipython" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(IPYTHON_PRIORITY)" >>$@
+	@echo "Section: $(IPYTHON_SECTION)" >>$@
+	@echo "Version: $(IPYTHON_VERSION)-$(IPYTHON_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(IPYTHON_MAINTAINER)" >>$@
+	@echo "Source: $(IPYTHON_SITE)/$(IPYTHON_SOURCE)" >>$@
+	@echo "Description: $(IPYTHON_DESCRIPTION)" >>$@
+	@echo "Depends: $(IPYTHON_PY3_DEPENDS)" >>$@
 	@echo "Suggests: $(IPYTHON_SUGGESTS)" >>$@
 	@echo "Conflicts: $(IPYTHON_CONFLICTS)" >>$@
 
@@ -228,7 +324,6 @@ $(IPYTHON_PY25_IPK): $(IPYTHON_BUILD_DIR)/.built
 	rm -rf $(BUILD_DIR)/ipython_*_$(TARGET_ARCH).ipk
 	rm -rf $(IPYTHON_PY25_IPK_DIR) $(BUILD_DIR)/py25-ipython_*_$(TARGET_ARCH).ipk
 	(cd $(IPYTHON_BUILD_DIR)/2.5; \
-		PYTHONPATH=$(STAGING_LIB_DIR)/python2.5/site-packages \
 		$(HOST_STAGING_PREFIX)/bin/python2.5 -c "import setuptools; execfile('setup.py')" \
 		install --root=$(IPYTHON_PY25_IPK_DIR) --prefix=/opt)
 	rm -rf $(IPYTHON_PY25_IPK_DIR)/opt/share
@@ -242,7 +337,6 @@ $(IPYTHON_PY26_IPK): $(IPYTHON_BUILD_DIR)/.built
 	rm -rf $(IPYTHON_PY26_IPK_DIR) $(BUILD_DIR)/py26-ipython_*_$(TARGET_ARCH).ipk
 	rm -rf $(IPYTHON-COMMON_IPK_DIR) $(BUILD_DIR)/ipython-common_*_$(TARGET_ARCH).ipk
 	(cd $(IPYTHON_BUILD_DIR)/2.6; \
-		PYTHONPATH=$(STAGING_LIB_DIR)/python2.6/site-packages \
 		$(HOST_STAGING_PREFIX)/bin/python2.6 -c "import setuptools; execfile('setup.py')" \
 		install --root=$(IPYTHON_PY26_IPK_DIR) --prefix=/opt)
 	rm -rf $(IPYTHON_PY26_IPK_DIR)/opt/share
@@ -250,20 +344,54 @@ $(IPYTHON_PY26_IPK): $(IPYTHON_BUILD_DIR)/.built
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(IPYTHON_PY26_IPK_DIR)
 	$(WHAT_TO_DO_WITH_IPK_DIR) $(IPYTHON_PY26_IPK_DIR)
 
+$(IPYTHON_PY27_IPK): $(IPYTHON_BUILD_DIR)/.built
+	rm -rf $(IPYTHON_PY27_IPK_DIR) $(BUILD_DIR)/py27-ipython_*_$(TARGET_ARCH).ipk
+	rm -rf $(IPYTHON-COMMON_IPK_DIR) $(BUILD_DIR)/ipython-common_*_$(TARGET_ARCH).ipk
+	(cd $(IPYTHON_BUILD_DIR)/2.7; \
+		$(HOST_STAGING_PREFIX)/bin/python2.7 -c "import setuptools; execfile('setup.py')" \
+		install --root=$(IPYTHON_PY27_IPK_DIR) --prefix=/opt)
+	rm -rf $(IPYTHON_PY27_IPK_DIR)/opt/share
+	$(MAKE) $(IPYTHON_PY27_IPK_DIR)/CONTROL/control
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(IPYTHON_PY27_IPK_DIR)
+	$(WHAT_TO_DO_WITH_IPK_DIR) $(IPYTHON_PY27_IPK_DIR)
+
+$(IPYTHON_PY3_IPK): $(IPYTHON_BUILD_DIR)/.built
+	rm -rf $(IPYTHON_PY3_IPK_DIR) $(BUILD_DIR)/py3-ipython_*_$(TARGET_ARCH).ipk
+	rm -rf $(IPYTHON-COMMON_IPK_DIR) $(BUILD_DIR)/ipython-common_*_$(TARGET_ARCH).ipk
+	(cd $(IPYTHON_BUILD_DIR)/3; \
+		$(HOST_STAGING_PREFIX)/bin/python$(PYTHON3_VERSION_MAJOR) setup.py \
+		install --root=$(IPYTHON_PY3_IPK_DIR) --prefix=/opt)
+	rm -rf $(IPYTHON_PY3_IPK_DIR)/opt/share
+	$(MAKE) $(IPYTHON_PY3_IPK_DIR)/CONTROL/control
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(IPYTHON_PY3_IPK_DIR)
+	$(WHAT_TO_DO_WITH_IPK_DIR) $(IPYTHON_PY3_IPK_DIR)
+
 $(IPYTHON-COMMON_IPK): $(IPYTHON_BUILD_DIR)/.built
-	(cd $(IPYTHON_BUILD_DIR)/2.6; \
-		PYTHONPATH=$(STAGING_LIB_DIR)/python2.6/site-packages \
-		$(HOST_STAGING_PREFIX)/bin/python2.6 setup.py \
+	(cd $(IPYTHON_BUILD_DIR)/2.7; \
+		$(HOST_STAGING_PREFIX)/bin/python2.7 setup.py \
 		install --root=$(IPYTHON-COMMON_IPK_DIR) --prefix=/opt)
 	rm -rf $(IPYTHON-COMMON_IPK_DIR)/opt/bin $(IPYTHON-COMMON_IPK_DIR)/opt/lib
 	$(MAKE) $(IPYTHON-COMMON_IPK_DIR)/CONTROL/control
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(IPYTHON-COMMON_IPK_DIR)
 	$(WHAT_TO_DO_WITH_IPK_DIR) $(IPYTHON-COMMON_IPK_DIR)
 
+ifneq ($(IPYTHON_VERSION), $(IPYTHON_VERSION_OLD))
+$(IPYTHON-COMMON-OLD_IPK): $(IPYTHON_BUILD_DIR)/.built
+	(cd $(IPYTHON_BUILD_DIR)/2.6; \
+		$(HOST_STAGING_PREFIX)/bin/python2.6 setup.py \
+		install --root=$(IPYTHON-COMMON-OLD_IPK_DIR) --prefix=/opt)
+	rm -rf $(IPYTHON-COMMON-OLD_IPK_DIR)/opt/bin $(IPYTHON-COMMON-OLD_IPK_DIR)/opt/lib
+	$(MAKE) $(IPYTHON-COMMON-OLD_IPK_DIR)/CONTROL/control
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(IPYTHON-COMMON-OLD_IPK_DIR)
+	$(WHAT_TO_DO_WITH_IPK_DIR) $(IPYTHON-COMMON-OLD_IPK_DIR)
+endif
+
 #
 # This is called from the top level makefile to create the IPK file.
 #
-ipython-ipk: $(IPYTHON_PY25_IPK) $(IPYTHON_PY26_IPK) $(IPYTHON-COMMON_IPK)
+ipython-ipk: $(IPYTHON_PY25_IPK) $(IPYTHON_PY26_IPK) \
+		$(IPYTHON_PY27_IPK) $(IPYTHON_PY3_IPK) \
+		$(IPYTHON-COMMON_IPK) $(IPYTHON-COMMON-OLD_IPK)
 
 #
 # This is called from the top level makefile to clean all of the built files.
@@ -278,11 +406,16 @@ ipython-clean:
 ipython-dirclean:
 	rm -rf $(BUILD_DIR)/$(IPYTHON_DIR) $(IPYTHON_BUILD_DIR)
 	rm -rf $(IPYTHON-COMMON_IPK_DIR) $(IPYTHON-COMMON_IPK)
+	rm -rf $(IPYTHON-COMMON-OLD_IPK_DIR) $(IPYTHON-COMMON-OLD_IPK)
 	rm -rf $(IPYTHON_PY25_IPK_DIR) $(IPYTHON_PY25_IPK)
 	rm -rf $(IPYTHON_PY26_IPK_DIR) $(IPYTHON_PY26_IPK)
+	rm -rf $(IPYTHON_PY27_IPK_DIR) $(IPYTHON_PY27_IPK)
+	rm -rf $(IPYTHON_PY3_IPK_DIR) $(IPYTHON_PY3_IPK)
 
 #
 # Some sanity check for the package.
 #
-ipython-check: $(IPYTHON_PY25_IPK) $(IPYTHON_PY26_IPK) $(IPYTHON-COMMON_IPK)
+ipython-check: $(IPYTHON_PY25_IPK) $(IPYTHON_PY26_IPK) \
+		$(IPYTHON_PY27_IPK) $(IPYTHON_PY3_IPK) \
+		$(IPYTHON-COMMON_IPK) $(IPYTHON-COMMON-OLD_IPK)
 	perl scripts/optware-check-package.pl --target=$(OPTWARE_TARGET) $^
