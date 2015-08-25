8d898b81a537bedfadaecadb315e6b7d575ea610
alllexx88 <opotapenko@gmail.com>
2015-08-25 10:29:39 +0300

    minidlna: build 2 versions: - minidlna (without thumbnail generation support) - minidlna-thumbnail (with thumbnail generation support)

--- make/minidlna.mk
+++ make/minidlna.mk
@@ -23,11 +23,14 @@ MINIDLNA_DIR=minidlna-$(MINIDLNA_VERSION)
 MINIDLNA_UNZIP=zcat
 MINIDLNA_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
 MINIDLNA_DESCRIPTION=MiniDLNA (aka ReadyDLNA) is server software with the aim of being fully compliant with DLNA/UPnP-AV clients.
+MINIDLNA_THUMBNAIL_DESCRIPTION=MiniDLNA (aka ReadyDLNA) is server software with the aim of being fully compliant with DLNA/UPnP-AV clients. Version with thumbnail generation support.
 MINIDLNA_SECTION=media
 MINIDLNA_PRIORITY=optional
-MINIDLNA_DEPENDS=libexif, libid3tag, libjpeg, libvorbis, e2fslibs, ffmpeg, flac, sqlite, ffmpegthumbnailer
+MINIDLNA_DEPENDS=libexif, libid3tag, libjpeg, libvorbis, e2fslibs, ffmpeg, flac, sqlite
+MINIDLNA_THUMBNAIL_DEPENDS=libexif, libid3tag, libjpeg, libvorbis, e2fslibs, ffmpeg, flac, sqlite, ffmpegthumbnailer
 ifneq (, $(filter libiconv, $(PACKAGES)))
 MINIDLNA_DEPENDS +=, libiconv
+MINIDLNA_THUMBNAIL_DEPENDS +=, libiconv
 endif
 MINIDLNA_SUGGESTS=
 MINIDLNA_CONFLICTS=
@@ -35,7 +38,7 @@ MINIDLNA_CONFLICTS=
 #
 # MINIDLNA_IPK_VERSION should be incremented when the ipk changes.
 #
-MINIDLNA_IPK_VERSION=1
+MINIDLNA_IPK_VERSION=2
 
 #
 # MINIDLNA_CONFFILES should be a list of user-editable files
@@ -65,8 +68,12 @@ MINIDLNA_LDFLAGS=
 #
 MINIDLNA_BUILD_DIR=$(BUILD_DIR)/minidlna
 MINIDLNA_SOURCE_DIR=$(SOURCE_DIR)/minidlna
+
 MINIDLNA_IPK_DIR=$(BUILD_DIR)/minidlna-$(MINIDLNA_VERSION)-ipk
 MINIDLNA_IPK=$(BUILD_DIR)/minidlna_$(MINIDLNA_VERSION)-$(MINIDLNA_IPK_VERSION)_$(TARGET_ARCH).ipk
+
+MINIDLNA_THUMBNAIL_IPK_DIR=$(BUILD_DIR)/minidlna-thumbnail-$(MINIDLNA_VERSION)-ipk
+MINIDLNA_THUMBNAIL_IPK=$(BUILD_DIR)/minidlna-thumbnail_$(MINIDLNA_VERSION)-$(MINIDLNA_IPK_VERSION)_$(TARGET_ARCH).ipk
 
 .PHONY: minidlna-source minidlna-unpack minidlna minidlna-stage minidlna-ipk minidlna-clean minidlna-dirclean minidlna-check
 
@@ -119,18 +126,61 @@ ifneq (, $(filter libiconv, $(PACKAGES)))
 endif
 	$(MAKE) libexif-stage libid3tag-stage libjpeg-stage libvorbis-stage \
 		e2fsprogs-stage ffmpeg-stage flac-stage sqlite-stage ffmpegthumbnailer-stage
-	rm -rf $(BUILD_DIR)/$(MINIDLNA_DIR) $(MINIDLNA_BUILD_DIR)
-	tar -C $(BUILD_DIR) -xzf $(DL_DIR)/minidlna-$(MINIDLNA_VERSION).tar.gz
+	rm -rf $(BUILD_DIR)/$(MINIDLNA_DIR) $(@D)
+	install -d $(@D)
+	tar -C $(@D) -xzf $(DL_DIR)/minidlna-$(MINIDLNA_VERSION).tar.gz
 	if test -n "$(MINIDLNA_PATCHES)" ; \
 		then cat $(MINIDLNA_PATCHES) | \
-		patch -bd $(BUILD_DIR)/$(MINIDLNA_DIR) -p1 ; \
+		patch -bd $(@D)/$(MINIDLNA_DIR) -p1 ; \
 	fi
-	if test "$(BUILD_DIR)/$(MINIDLNA_DIR)" != "$(@D)" ; \
-		then mv $(BUILD_DIR)/$(MINIDLNA_DIR) $(@D) ; \
+	mv $(@D)/$(MINIDLNA_DIR) $(@D)/nothumbs
+	tar -C $(@D) -xzf $(DL_DIR)/minidlna-$(MINIDLNA_VERSION).tar.gz
+	if test -n "$(MINIDLNA_PATCHES)" ; \
+		then cat $(MINIDLNA_PATCHES) | \
+		patch -bd $(@D)/$(MINIDLNA_DIR) -p1 ; \
 	fi
-	sed -i -e '/^AM_SILENT_RULES/s/^/dnl /' $(@D)/configure.ac
-	autoreconf -vif $(@D)
-	(cd $(@D); \
+	mv $(@D)/$(MINIDLNA_DIR) $(@D)/thumbs
+	### configure version without thumbnails
+	sed -i -e '/^AM_SILENT_RULES/s/^/dnl /' $(@D)/nothumbs/configure.ac
+	autoreconf -vif $(@D)/nothumbs
+	(cd $(@D)/nothumbs; \
+		$(TARGET_CONFIGURE_OPTS) \
+		CPPFLAGS="$(STAGING_CPPFLAGS) $(MINIDLNA_CPPFLAGS)" \
+		LDFLAGS="$(STAGING_LDFLAGS) $(MINIDLNA_LDFLAGS)" \
+		PKG_CONFIG_PATH="$(STAGING_LIB_DIR)/pkgconfig" \
+		PKG_CONFIG_LIBDIR="$(STAGING_LIB_DIR)/pkgconfig" \
+		./configure \
+		--build=$(GNU_HOST_NAME) \
+		--host=$(GNU_TARGET_NAME) \
+		--target=$(GNU_TARGET_NAME) \
+		--prefix=/opt \
+		--disable-nls \
+		--program-prefix='' \
+	)
+	sed -i.orig \
+		-e 's|-I/usr/include|-I$(STAGING_INCLUDE_DIR)|g' \
+		-e '/$$(CFLAGS).*-c/s|$$(CFLAGS) |&$$(CPPFLAGS) |' \
+		-e '/$$(CFLAGS).*$$(LIBS)/s|$$(CFLAGS) |$$(LDFLAGS) |' \
+		-e '/^minidlna:/s| $$(LIBS)||' \
+		$(@D)/nothumbs/Makefile
+	if ! $(TARGET_CC) -E sources/common/test_sendfile.c >/dev/null 2>&1; then \
+		sed -i -e 's/-D_FILE_OFFSET_BITS=64 //' $(@D)/nothumbs/Makefile; \
+	fi
+#	sed -i.orig \
+		-e 's|\[ *-f /usr/include/sys/inotify.h *\]|$(TARGET_CC) -E $(SOURCE_DIR)/common/have_inotify.c >/dev/null 2>\&1|' \
+		-e 's|/usr/include/|$(STAGING_INCLUDE_DIR)/|g' \
+		-e '/^echo.*#define/s|$$OS_NAME|Linux|' \
+		-e '/^echo.*#define/s|$$OS_VERSION|Cross_compiled|' \
+		-e '/^echo.*#define/s|$${OS_URL}|http://www.kernel.org/|' \
+		$(@D)/nothumbs/genconfig.sh
+	sed -i.orig \
+		 -e 's|/etc/|/opt&|' \
+		 -e 's|/usr/|/opt/|' \
+		$(@D)/nothumbs/minidlna.c
+	### configure version with thumbnails
+	sed -i -e '/^AM_SILENT_RULES/s/^/dnl /' $(@D)/thumbs/configure.ac
+	autoreconf -vif $(@D)/thumbs
+	(cd $(@D)/thumbs; \
 		$(TARGET_CONFIGURE_OPTS) \
 		CPPFLAGS="$(STAGING_CPPFLAGS) $(MINIDLNA_CPPFLAGS)" \
 		LDFLAGS="$(STAGING_LDFLAGS) $(MINIDLNA_LDFLAGS)" \
@@ -150,9 +200,9 @@ endif
 		-e '/$$(CFLAGS).*-c/s|$$(CFLAGS) |&$$(CPPFLAGS) |' \
 		-e '/$$(CFLAGS).*$$(LIBS)/s|$$(CFLAGS) |$$(LDFLAGS) |' \
 		-e '/^minidlna:/s| $$(LIBS)||' \
-		$(@D)/Makefile
+		$(@D)/thumbs/Makefile
 	if ! $(TARGET_CC) -E sources/common/test_sendfile.c >/dev/null 2>&1; then \
-		sed -i -e 's/-D_FILE_OFFSET_BITS=64 //' $(@D)/Makefile; \
+		sed -i -e 's/-D_FILE_OFFSET_BITS=64 //' $(@D)/thumbs/Makefile; \
 	fi
 #	sed -i.orig \
 		-e 's|\[ *-f /usr/include/sys/inotify.h *\]|$(TARGET_CC) -E $(SOURCE_DIR)/common/have_inotify.c >/dev/null 2>\&1|' \
@@ -160,11 +210,11 @@ endif
 		-e '/^echo.*#define/s|$$OS_NAME|Linux|' \
 		-e '/^echo.*#define/s|$$OS_VERSION|Cross_compiled|' \
 		-e '/^echo.*#define/s|$${OS_URL}|http://www.kernel.org/|' \
-		$(@D)/genconfig.sh
+		$(@D)/thumbs/genconfig.sh
 	sed -i.orig \
 		 -e 's|/etc/|/opt&|' \
 		 -e 's|/usr/|/opt/|' \
-		$(@D)/minidlna.c
+		$(@D)/thumbs/minidlna.c
 	touch $@
 
 minidlna-unpack: $(MINIDLNA_BUILD_DIR)/.configured
@@ -174,7 +224,12 @@ minidlna-unpack: $(MINIDLNA_BUILD_DIR)/.configured
 #
 $(MINIDLNA_BUILD_DIR)/.built: $(MINIDLNA_BUILD_DIR)/.configured
 	rm -f $@
-	$(MAKE) -C $(@D) \
+	$(MAKE) -C $(@D)/nothumbs \
+		$(TARGET_CONFIGURE_OPTS) \
+		CPPFLAGS="$(STAGING_CPPFLAGS) $(MINIDLNA_CPPFLAGS)" \
+		LDFLAGS="$(STAGING_LDFLAGS) $(MINIDLNA_LDFLAGS)" \
+		;
+	$(MAKE) -C $(@D)/thumbs \
 		$(TARGET_CONFIGURE_OPTS) \
 		CPPFLAGS="$(STAGING_CPPFLAGS) $(MINIDLNA_CPPFLAGS)" \
 		LDFLAGS="$(STAGING_LDFLAGS) $(MINIDLNA_LDFLAGS)" \
@@ -191,7 +246,7 @@ minidlna: $(MINIDLNA_BUILD_DIR)/.built
 #
 $(MINIDLNA_BUILD_DIR)/.staged: $(MINIDLNA_BUILD_DIR)/.built
 	rm -f $@
-	$(MAKE) -C $(@D) DESTDIR=$(STAGING_DIR) install
+#	$(MAKE) -C $(@D) DESTDIR=$(STAGING_DIR) install
 	touch $@
 
 minidlna-stage: $(MINIDLNA_BUILD_DIR)/.staged
@@ -219,6 +274,25 @@ endif
 	@echo "Suggests: $(MINIDLNA_SUGGESTS)" >>$@
 	@echo "Conflicts: $(MINIDLNA_CONFLICTS)" >>$@
 
+$(MINIDLNA_THUMBNAIL_IPK_DIR)/CONTROL/control:
+	@install -d $(@D)
+	@rm -f $@
+	@echo "Package: minidlna-thumbnail" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(MINIDLNA_PRIORITY)" >>$@
+	@echo "Section: $(MINIDLNA_SECTION)" >>$@
+	@echo "Version: $(MINIDLNA_VERSION)-$(MINIDLNA_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(MINIDLNA_MAINTAINER)" >>$@
+ifndef MINIDLNA_REPOSITORY
+	@echo "Source: $(MINIDLNA_SITE)/$(MINIDLNA_SOURCE)" >>$@
+else
+	@echo "Source: $(MINIDLNA_REPOSITORY)" >>$@
+endif
+	@echo "Description: $(MINIDLNA_THUMBNAIL_DESCRIPTION)" >>$@
+	@echo "Depends: $(MINIDLNA_THUMBNAIL_DEPENDS)" >>$@
+	@echo "Suggests: $(MINIDLNA_SUGGESTS)" >>$@
+	@echo "Conflicts: $(MINIDLNA_CONFLICTS)" >>$@
+
 #
 # This builds the IPK file.
 #
@@ -233,7 +307,7 @@ endif
 #
 $(MINIDLNA_IPK): $(MINIDLNA_BUILD_DIR)/.built
 	rm -rf $(MINIDLNA_IPK_DIR) $(BUILD_DIR)/minidlna_*_$(TARGET_ARCH).ipk
-	$(MAKE) -C $(MINIDLNA_BUILD_DIR) install \
+	$(MAKE) -C $(MINIDLNA_BUILD_DIR)/nothumbs install \
 		DESTDIR=$(MINIDLNA_IPK_DIR) \
 		PREFIX=$(MINIDLNA_IPK_DIR) \
 		INSTALLPREFIX=$(MINIDLNA_IPK_DIR)/opt \
@@ -249,10 +323,28 @@ $(MINIDLNA_IPK): $(MINIDLNA_BUILD_DIR)/.built
 	echo $(MINIDLNA_CONFFILES) | sed -e 's/ /\n/g' > $(MINIDLNA_IPK_DIR)/CONTROL/conffiles
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(MINIDLNA_IPK_DIR)
 
+$(MINIDLNA_THUMBNAIL_IPK): $(MINIDLNA_BUILD_DIR)/.built
+	rm -rf $(MINIDLNA_THUMBNAIL_IPK_DIR) $(BUILD_DIR)/minidlna-thumbnail_*_$(TARGET_ARCH).ipk
+	$(MAKE) -C $(MINIDLNA_BUILD_DIR)/thumbs install \
+		DESTDIR=$(MINIDLNA_THUMBNAIL_IPK_DIR) \
+		PREFIX=$(MINIDLNA_THUMBNAIL_IPK_DIR) \
+		INSTALLPREFIX=$(MINIDLNA_THUMBNAIL_IPK_DIR)/opt \
+		ETCINSTALLDIR=$(MINIDLNA_THUMBNAIL_IPK_DIR)/opt/etc \
+		;
+	$(STRIP_COMMAND) $(MINIDLNA_THUMBNAIL_IPK_DIR)/opt/sbin/*
+#	install -m 644 $(MINIDLNA_SOURCE_DIR)/minidlna.conf $(MINIDLNA_IPK_DIR)/opt/etc/minidlna.conf
+#	install -d $(MINIDLNA_IPK_DIR)/opt/etc/init.d
+#	install -m 755 $(MINIDLNA_BUILD_DIR)/linux/minidlna.init.d.script $(MINIDLNA_IPK_DIR)/opt/etc/init.d/S98minidlna
+	$(MAKE) $(MINIDLNA_THUMBNAIL_IPK_DIR)/CONTROL/control
+#	install -m 755 $(MINIDLNA_SOURCE_DIR)/postinst $(MINIDLNA_IPK_DIR)/CONTROL/postinst
+#	install -m 755 $(MINIDLNA_SOURCE_DIR)/prerm $(MINIDLNA_IPK_DIR)/CONTROL/prerm
+	echo $(MINIDLNA_CONFFILES) | sed -e 's/ /\n/g' > $(MINIDLNA_THUMBNAIL_IPK_DIR)/CONTROL/conffiles
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(MINIDLNA_THUMBNAIL_IPK_DIR)
+
 #
 # This is called from the top level makefile to create the IPK file.
 #
-minidlna-ipk: $(MINIDLNA_IPK)
+minidlna-ipk: $(MINIDLNA_IPK) $(MINIDLNA_THUMBNAIL_IPK)
 
 #
 # This is called from the top level makefile to clean all of the built files.
@@ -266,10 +358,12 @@ minidlna-clean:
 # directories.
 #
 minidlna-dirclean:
-	rm -rf $(BUILD_DIR)/$(MINIDLNA_DIR) $(MINIDLNA_BUILD_DIR) $(MINIDLNA_IPK_DIR) $(MINIDLNA_IPK)
+	rm -rf $(BUILD_DIR)/$(MINIDLNA_DIR) $(MINIDLNA_BUILD_DIR) \
+		$(MINIDLNA_IPK_DIR) $(MINIDLNA_IPK) \
+		$(MINIDLNA_THUMBNAIL_IPK_DIR) $(MINIDLNA_THUMBNAIL_IPK)
 
 #
 # Some sanity check for the package.
 #
-minidlna-check: $(MINIDLNA_IPK)
+minidlna-check: $(MINIDLNA_IPK) $(MINIDLNA_THUMBNAIL_IPK)
 	perl scripts/optware-check-package.pl --target=$(OPTWARE_TARGET) $^
