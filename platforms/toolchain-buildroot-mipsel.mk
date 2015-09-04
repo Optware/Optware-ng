dd1894651e57ca652384d5a439c1ccf0489f518e
alllexx88 <opotapenko@gmail.com>
2015-09-04 22:07:03 +0300

    buildroot-mipsel: upgrade toolchain to gcc-5.2.0

--- platforms/toolchain-buildroot-mipsel.mk
+++ platforms/toolchain-buildroot-mipsel.mk
@@ -1,4 +1,4 @@
-# This toolchain is gcc 4.9.2 on uClibc 0.9.33.2
+# This toolchain is gcc 5.2.0 on uClibc 0.9.33.2
 
 GNU_TARGET_NAME = mipsel-linux
 EXACT_TARGET_NAME = mipsel-buildroot-linux-uclibc
@@ -7,13 +7,15 @@ LIBC_STYLE=uclibc
 TARGET_ARCH=mipsel
 TARGET_OS=linux-uclibc
 
-LIBSTDC++_VERSION=6.0.20
+LIBSTDC++_VERSION=6.0.21
+
+LIBC-DEV_IPK_VERSION=9
 
 GETTEXT_NLS=enable
 #NO_BUILTIN_MATH=true
 IPV6=yes
 
-CROSS_CONFIGURATION_GCC_VERSION=4.9.2
+CROSS_CONFIGURATION_GCC_VERSION=5.2.0
 CROSS_CONFIGURATION_UCLIBC_VERSION=0.9.33.2
 
 ifeq ($(HOST_MACHINE),mips)
@@ -36,8 +38,8 @@ GNU_HOST_NAME = $(HOST_MACHINE)-pc-linux-gnu
 CROSS_CONFIGURATION_GCC=gcc-$(CROSS_CONFIGURATION_GCC_VERSION)
 CROSS_CONFIGURATION_UCLIBC=uclibc-$(CROSS_CONFIGURATION_UCLIBC_VERSION)
 CROSS_CONFIGURATION=$(CROSS_CONFIGURATION_GCC)-$(CROSS_CONFIGURATION_UCLIBC)
-TARGET_CROSS_BUILD_DIR = $(BASE_DIR)/toolchain/buildroot-2015.02
-TARGET_CROSS_TOP = $(BASE_DIR)/toolchain/buildroot-mipsel-linux-2.6.36-uclibc-4.9.2
+TARGET_CROSS_BUILD_DIR = $(BASE_DIR)/toolchain/buildroot-2015.08
+TARGET_CROSS_TOP = $(BASE_DIR)/toolchain/buildroot-mipsel-linux-2.6.36-uclibc-5.2.0
 TARGET_CROSS = $(TARGET_CROSS_TOP)/bin/mipsel-buildroot-linux-uclibc-
 TARGET_LIBDIR = $(TARGET_CROSS_TOP)/mipsel-buildroot-linux-uclibc/sysroot/usr/lib
 TARGET_INCDIR = $(TARGET_CROSS_TOP)/mipsel-buildroot-linux-uclibc/sysroot/usr/include
@@ -52,10 +54,11 @@ TARGET_CUSTOM_FLAGS= -pipe
 TARGET_CFLAGS=$(TARGET_OPTIMIZATION) $(TARGET_DEBUGGING) $(TARGET_CUSTOM_FLAGS)
 
 TOOLCHAIN_SITE=http://buildroot.uclibc.org/downloads
-TOOLCHAIN_SOURCE=buildroot-2015.02.tar.bz2
+TOOLCHAIN_SOURCE=buildroot-2015.08.tar.bz2
 
 UCLIBC-OPT_VERSION = 0.9.33.2
-UCLIBC-OPT_IPK_VERSION = 2
+UCLIBC-OPT_IPK_VERSION = 3
+LIBNSL_IPK_VERSION = 6
 UCLIBC-OPT_LIBS_SOURCE_DIR = $(TARGET_CROSS_TOP)/mipsel-buildroot-linux-uclibc/sysroot/lib
 
 BUILDROOT-MIPSEL_SOURCE_DIR=$(SOURCE_DIR)/buildroot-mipsel
@@ -71,7 +74,7 @@ $(TARGET_CROSS_TOP)/.configured: $(DL_DIR)/$(TOOLCHAIN_SOURCE) #$(OPTWARE_TOP)/p
 	mkdir -p $(TARGET_CROSS_TOP)/mipsel-buildroot-linux-uclibc/sysroot
 	tar -xjvf $(DL_DIR)/$(TOOLCHAIN_SOURCE) -C $(BASE_DIR)/toolchain
 	sed 's|^BR2_DL_DIR=.*|BR2_DL_DIR="$(DL_DIR)"|' $(BUILDROOT-MIPSEL_SOURCE_DIR)/config > $(TARGET_CROSS_BUILD_DIR)/.config
-	cp -f $(BUILDROOT-MIPSEL_SOURCE_DIR)/gcc-4.9.2-patches/*.patch $(TARGET_CROSS_BUILD_DIR)/package/gcc/4.9.2/
+	cp -f $(BUILDROOT-MIPSEL_SOURCE_DIR)/uclibc.config $(TARGET_CROSS_BUILD_DIR)/package/uclibc/my.uclibc.config
 	cp -f $(BUILDROOT-MIPSEL_SOURCE_DIR)/uClibc-0.9.33-patches/*.patch $(TARGET_CROSS_BUILD_DIR)/package/uclibc/0.9.33.2/
 	touch $@
 
@@ -80,6 +83,7 @@ $(TARGET_CROSS_TOP)/.built: $(TARGET_CROSS_TOP)/.configured
 	$(MAKE) STAGING_DIR=$(TARGET_CROSS_TOP)/mipsel-buildroot-linux-uclibc/sysroot -C $(TARGET_CROSS_BUILD_DIR)
 	cp -af $(TARGET_CROSS_BUILD_DIR)/output/host/usr/* $(TARGET_CROSS_TOP)/
 	cp -f $(UCLIBC-OPT_LIBS_SOURCE_DIR)/libnsl-0.9.33.2.so $(TARGET_LIBDIR)/
+	cp -f $(TARGET_CROSS_TOP)/lib/gcc/mipsel-buildroot-linux-uclibc/5.2.0/*.a $(UCLIBC-OPT_LIBS_SOURCE_DIR)/
 	touch $@
 
 GCC_TARGET_NAME := mipsel-buildroot-linux-uclibc
@@ -88,8 +92,10 @@ GCC_CPPFLAGS := -D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64
 
 GCC_EXTRA_CONF_ENV := ac_cv_lbl_unaligned_fail=yes ac_cv_func_mmap_fixed_mapped=yes ac_cv_func_memcmp_working=yes ac_cv_have_decl_malloc=yes gl_cv_func_malloc_0_nonnull=yes ac_cv_func_malloc_0_nonnull=yes ac_cv_func_calloc_0_nonnull=yes ac_cv_func_realloc_0_nonnull=yes lt_cv_sys_lib_search_path_spec="" ac_cv_c_bigendian=no
 
-NATIVE_GCC_EXTRA_CONFIG_ARGS=--with-gxx-include-dir=/opt/include/c++/4.9.2 --disable-__cxa_atexit --with-gnu-ld --disable-libssp --enable-target-optspace --disable-libsanitizer --disable-tls --disable-libmudflap --enable-threads --without-isl --without-cloog --with-float=soft --disable-decimal-float --with-arch=mips32r2 --with-abi=32 --enable-poison-system-directories --enable-shared --disable-libgomp --with-gmp=$(STAGING_PREFIX) --with-mpfr=$(STAGING_PREFIX) --with-mpc=$(STAGING_PREFIX)
+NATIVE_GCC_EXTRA_CONFIG_ARGS=--with-gxx-include-dir=/opt/include/c++/5.2.0 --disable-__cxa_atexit --with-gnu-ld --disable-libssp --disable-libsanitizer --disable-tls --disable-libmudflap --enable-threads --without-isl --without-cloog --with-float=soft --disable-decimal-float --with-arch=mips32r2 --with-abi=32 --enable-shared --disable-libgomp --with-gmp=$(STAGING_PREFIX) --with-mpfr=$(STAGING_PREFIX) --with-mpc=$(STAGING_PREFIX) --with-default-libstdcxx-abi=gcc4-compatible --with-system-zlib
 
-NATIVE_GCC_EXTRA_PATCHES=$(wildcard $(BUILDROOT-MIPSEL_SOURCE_DIR)/gcc-4.9.2-patches/*.patch)
+NATIVE_GCC_ADDITIONAL_DEPS=zlib
+
+NATIVE_GCC_ADDITIONAL_STAGE=zlib-stage
 
 endif
