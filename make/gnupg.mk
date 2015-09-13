dbf7fdd2080d40e304a3c290efd96c96d2c2860f
alllexx88 <opotapenko@gmail.com>
2015-09-13 16:56:09 +0300

    gnupg: 2.0.18 -> 2.0.29; fix GCC 5+ build

--- make/gnupg.mk
+++ make/gnupg.mk
@@ -22,7 +22,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 GNUPG_SITE=ftp://ftp.gnupg.org/gcrypt/gnupg
-GNUPG_VERSION=2.0.18
+GNUPG_VERSION=2.0.29
 GNUPG_SOURCE=gnupg-$(GNUPG_VERSION).tar.bz2
 GNUPG_DIR=gnupg-$(GNUPG_VERSION)
 GNUPG_UNZIP=bzcat
@@ -56,7 +56,7 @@ GNUPG_IPK_VERSION=1
 # If the compilation of the package requires additional
 # compilation or linking flags, then list them here.
 #
-GNUPG_CPPFLAGS=
+GNUPG_CPPFLAGS=-std=gnu89
 GNUPG_LDFLAGS=
 
 #
