e4035d217be1f04a7c7e25692cd3df5ab20177a3
alllexx88 <opotapenko@gmail.com>
2015-10-29 19:06:10 +0200

    aclocal.sh: allow to skip aclocal stage when using autoreconf Set SKIP_ACLOCAL env variable to "1" for this

--- scripts/aclocal.sh
+++ scripts/aclocal.sh
@@ -15,6 +15,10 @@ if [ -z "${TOP}" ]; then
 	TOP=`dirname $0`/..
 fi
 
+if [ "x${SKIP_ACLOCAL}" == "x1" ]; then
+	exit 0
+fi
+
 ${ACLOCAL} "$@" || exit 1
 
 ( (cd "${TOP}/host/staging/opt/share/aclocal"; cat libtool.m4 ltoptions.m4 ltversion.m4 ltsugar.m4 lt~obsolete.m4) >> aclocal.m4) || exit 1
