e3040be08e1c29efd283e37b79d70591d1c1f8d7
alllexx88 <opotapenko@gmail.com>
2015-12-25 13:35:30 +0200

    whois: 5.2.10 -> 5.2.11 & update link

--- make/whois.mk
+++ make/whois.mk
@@ -21,8 +21,8 @@
 # from your name or email address.  If you leave MAINTAINER set to
 # "NSLU2 Linux" other developers will feel free to edit.
 #
-WHOIS_SITE=http://ftp.debian.org/debian/pool/main/w/whois
-WHOIS_VERSION=5.2.10
+WHOIS_SITE=http://ftp.lfs-matrix.net/pub/blfs/conglomeration/whois
+WHOIS_VERSION=5.2.11
 WHOIS_SOURCE=whois_$(WHOIS_VERSION).tar.xz
 WHOIS_DIR=whois-$(WHOIS_VERSION)
 WHOIS_UNZIP=$(HOST_STAGING_PREFIX)/bin/xzcat
