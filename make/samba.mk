e4fc7f32d15f22ff744b057653447245d77539e9
alllexx88 <opotapenko@gmail.com>
2016-02-21 19:42:03 +0200

    samba: fix race condition

--- make/samba.mk
+++ make/samba.mk
@@ -205,7 +205,7 @@ samba-source: $(DL_DIR)/$(SAMBA_SOURCE) $(SAMBA_PATCHES)
 # If the compilation of the package requires other packages to be staged
 # first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
 #
-$(SAMBA_BUILD_DIR)/.configured: $(DL_DIR)/$(SAMBA_SOURCE) $(SAMBA_PATCHES)
+$(SAMBA_BUILD_DIR)/.configured: $(DL_DIR)/$(SAMBA_SOURCE) $(SAMBA_PATCHES) make/samba.mk
 ifeq (openldap, $(filter openldap, $(PACKAGES)))
 	$(MAKE) openldap-stage 
 endif
@@ -284,6 +284,7 @@ samba-unpack: $(SAMBA_BUILD_DIR)/.configured
 #
 $(SAMBA_BUILD_DIR)/.built: $(SAMBA_BUILD_DIR)/.configured
 	rm -f $@
+	$(MAKE) -C $(@D) proto_exists
 	$(MAKE) -C $(@D)
 	touch $@
 
