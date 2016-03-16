e9425697eb1eb0134610cfb7dda22ead6b1b6fb1
Jonathon Padfield <jonathon.padfield@gmail.com>
2016-03-16 08:24:54 +0800

    Added perl libs SOAP::Lite and Class::Inspector
    Basic addition of perl module SOAP::Lite and dependency
    Class::Inspector.
    
    Testing: SOAP::Lite can be loaded with a perl -MSOAP::Lite -e1
    
    Added on request https://github.com/Optware/Optware-ng/issues/99

---
+++ make/perl-class-inspector.mk
@@ -0,0 +1,103 @@
+###########################################################
+#
+# perl-class-inspector
+#
+###########################################################
+
+PERL-CLASS-INSPECTOR_SITE=http://$(PERL_CPAN_SITE)/CPAN/authors/id/A/AD/ADAMK
+PERL-CLASS-INSPECTOR_VERSION=1.28
+PERL-CLASS-INSPECTOR_SOURCE=Class-Inspector-$(PERL-CLASS-INSPECTOR_VERSION).tar.gz
+PERL-CLASS-INSPECTOR_DIR=Class-Inspector-$(PERL-CLASS-INSPECTOR_VERSION)
+PERL-CLASS-INSPECTOR_UNZIP=zcat
+PERL-CLASS-INSPECTOR_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
+PERL-CLASS-INSPECTOR_DESCRIPTION=Get information about a class and its structure
+PERL-CLASS-INSPECTOR_SECTION=util
+PERL-CLASS-INSPECTOR_PRIORITY=optional
+PERL-CLASS-INSPECTOR_DEPENDS=
+PERL-CLASS-INSPECTOR_SUGGESTS=
+PERL-CLASS-INSPECTOR_CONFLICTS=
+
+PERL-CLASS-INSPECTOR_IPK_VERSION=1
+
+PERL-CLASS-INSPECTOR_CONFFILES=
+
+PERL-CLASS-INSPECTOR_BUILD_DIR=$(BUILD_DIR)/perl-class-inspector
+PERL-CLASS-INSPECTOR_SOURCE_DIR=$(SOURCE_DIR)/perl-class-inspector
+PERL-CLASS-INSPECTOR_IPK_DIR=$(BUILD_DIR)/perl-class-inspector-$(PERL-CLASS-INSPECTOR_VERSION)-ipk
+PERL-CLASS-INSPECTOR_IPK=$(BUILD_DIR)/perl-class-inspector_$(PERL-CLASS-INSPECTOR_VERSION)-$(PERL-CLASS-INSPECTOR_IPK_VERSION)_$(TARGET_ARCH).ipk
+
+$(DL_DIR)/$(PERL-CLASS-INSPECTOR_SOURCE):
+	$(WGET) -P $(@D) $(PERL-CLASS-INSPECTOR_SITE)/$(@F) || \
+	$(WGET) -P $(@D) $(FREEBSD_DISTFILES)/$(@F) || \
+	$(WGET) -P $(@D) $(SOURCES_NLO_SITE)/$(@F)
+
+perl-class-inspector-source: $(DL_DIR)/$(PERL-CLASS-INSPECTOR_SOURCE) $(PERL-CLASS-INSPECTOR_PATCHES)
+
+$(PERL-CLASS-INSPECTOR_BUILD_DIR)/.configured: $(DL_DIR)/$(PERL-CLASS-INSPECTOR_SOURCE) $(PERL-CLASS-INSPECTOR_PATCHES) make/perl-class-inspector.mk
+	rm -rf $(BUILD_DIR)/$(PERL-CLASS-INSPECTOR_DIR) $(PERL-CLASS-INSPECTOR_BUILD_DIR)
+	$(PERL-CLASS-INSPECTOR_UNZIP) $(DL_DIR)/$(PERL-CLASS-INSPECTOR_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+#	cat $(PERL-CLASS-INSPECTOR_PATCHES) | $(PATCH) -d $(BUILD_DIR)/$(PERL-CLASS-INSPECTOR_DIR) -p1
+	mv $(BUILD_DIR)/$(PERL-CLASS-INSPECTOR_DIR) $(@D)
+	(cd $(@D); \
+		$(TARGET_CONFIGURE_OPTS) \
+		CPPFLAGS="$(STAGING_CPPFLAGS)" \
+		LDFLAGS="$(STAGING_LDFLAGS)" \
+		PERL5LIB="$(STAGING_LIB_DIR)/perl5/site_perl" \
+		$(PERL_HOSTPERL) Makefile.PL \
+		PREFIX=$(TARGET_PREFIX) \
+	)
+	touch $@
+
+perl-class-inspector-unpack: $(PERL-CLASS-INSPECTOR_BUILD_DIR)/.configured
+
+$(PERL-CLASS-INSPECTOR_BUILD_DIR)/.built: $(PERL-CLASS-INSPECTOR_BUILD_DIR)/.configured
+	rm -f $@
+	$(MAKE) -C $(@D) \
+	PERL5LIB="$(STAGING_LIB_DIR)/perl5/site_perl"
+	touch $@
+
+perl-class-inspector: $(PERL-CLASS-INSPECTOR_BUILD_DIR)/.built
+
+$(PERL-CLASS-INSPECTOR_BUILD_DIR)/.staged: $(PERL-CLASS-INSPECTOR_BUILD_DIR)/.built
+	rm -f $@
+	$(MAKE) -C $(@D) DESTDIR=$(STAGING_DIR) install
+	touch $@
+
+perl-class-inspector-stage: $(PERL-CLASS-INSPECTOR_BUILD_DIR)/.staged
+
+$(PERL-CLASS-INSPECTOR_IPK_DIR)/CONTROL/control:
+	@$(INSTALL) -d $(@D)
+	@rm -f $@
+	@echo "Package: perl-class-inspector" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(PERL-CLASS-INSPECTOR_PRIORITY)" >>$@
+	@echo "Section: $(PERL-CLASS-INSPECTOR_SECTION)" >>$@
+	@echo "Version: $(PERL-CLASS-INSPECTOR_VERSION)-$(PERL-CLASS-INSPECTOR_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(PERL-CLASS-INSPECTOR_MAINTAINER)" >>$@
+	@echo "Source: $(PERL-CLASS-INSPECTOR_SITE)/$(PERL-CLASS-INSPECTOR_SOURCE)" >>$@
+	@echo "Description: $(PERL-CLASS-INSPECTOR_DESCRIPTION)" >>$@
+	@echo "Depends: $(PERL-CLASS-INSPECTOR_DEPENDS)" >>$@
+	@echo "Suggests: $(PERL-CLASS-INSPECTOR_SUGGESTS)" >>$@
+	@echo "Conflicts: $(PERL-CLASS-INSPECTOR_CONFLICTS)" >>$@
+
+$(PERL-CLASS-INSPECTOR_IPK): $(PERL-CLASS-INSPECTOR_BUILD_DIR)/.built
+	rm -rf $(PERL-CLASS-INSPECTOR_IPK_DIR) $(BUILD_DIR)/perl-class-inspector_*_$(TARGET_ARCH).ipk
+	$(MAKE) -C $(PERL-CLASS-INSPECTOR_BUILD_DIR) DESTDIR=$(PERL-CLASS-INSPECTOR_IPK_DIR) install
+	find $(PERL-CLASS-INSPECTOR_IPK_DIR)$(TARGET_PREFIX) -name 'perllocal.pod' -exec rm -f {} \;
+	(cd $(PERL-CLASS-INSPECTOR_IPK_DIR)$(TARGET_PREFIX)/lib/perl5 ; \
+		find . -name '*.so' -exec chmod +w {} \; ; \
+		find . -name '*.so' -exec $(STRIP_COMMAND) {} \; ; \
+		find . -name '*.so' -exec chmod -w {} \; ; \
+	)
+	find $(PERL-CLASS-INSPECTOR_IPK_DIR)$(TARGET_PREFIX) -type d -exec chmod go+rx {} \;
+	$(MAKE) $(PERL-CLASS-INSPECTOR_IPK_DIR)/CONTROL/control
+	echo $(PERL-CLASS-INSPECTOR_CONFFILES) | sed -e 's/ /\n/g' > $(PERL-CLASS-INSPECTOR_IPK_DIR)/CONTROL/conffiles
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(PERL-CLASS-INSPECTOR_IPK_DIR)
+
+perl-class-inspector-ipk: $(PERL-CLASS-INSPECTOR_IPK)
+
+perl-class-inspector-clean:
+	-$(MAKE) -C $(PERL-CLASS-INSPECTOR_BUILD_DIR) clean
+
+perl-class-inspector-dirclean:
+	rm -rf $(BUILD_DIR)/$(PERL-CLASS-INSPECTOR_DIR) $(PERL-CLASS-INSPECTOR_BUILD_DIR) $(PERL-CLASS-INSPECTOR_IPK_DIR) $(PERL-CLASS-INSPECTOR_IPK)
---
+++ make/perl-soap-lite.mk
@@ -0,0 +1,103 @@
+###########################################################
+#
+# perl-soap-lite
+#
+###########################################################
+
+PERL-SOAP-LITE_SITE=http://$(PERL_CPAN_SITE)/CPAN/authors/id/P/PH/PHRED
+PERL-SOAP-LITE_VERSION=1.19
+PERL-SOAP-LITE_SOURCE=SOAP-Lite-$(PERL-SOAP-LITE_VERSION).tar.gz
+PERL-SOAP-LITE_DIR=SOAP-Lite-$(PERL-SOAP-LITE_VERSION)
+PERL-SOAP-LITE_UNZIP=zcat
+PERL-SOAP-LITE_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
+PERL-SOAP-LITE_DESCRIPTION=Perls Web Services Toolkit
+PERL-SOAP-LITE_SECTION=util
+PERL-SOAP-LITE_PRIORITY=optional
+PERL-SOAP-LITE_DEPENDS=perl-class-inspector
+PERL-SOAP-LITE_SUGGESTS=
+PERL-SOAP-LITE_CONFLICTS=
+
+PERL-SOAP-LITE_IPK_VERSION=1
+
+PERL-SOAP-LITE_CONFFILES=
+
+PERL-SOAP-LITE_BUILD_DIR=$(BUILD_DIR)/perl-soap-lite
+PERL-SOAP-LITE_SOURCE_DIR=$(SOURCE_DIR)/perl-soap-lite
+PERL-SOAP-LITE_IPK_DIR=$(BUILD_DIR)/perl-soap-lite-$(PERL-SOAP-LITE_VERSION)-ipk
+PERL-SOAP-LITE_IPK=$(BUILD_DIR)/perl-soap-lite_$(PERL-SOAP-LITE_VERSION)-$(PERL-SOAP-LITE_IPK_VERSION)_$(TARGET_ARCH).ipk
+
+$(DL_DIR)/$(PERL-SOAP-LITE_SOURCE):
+	$(WGET) -P $(@D) $(PERL-SOAP-LITE_SITE)/$(@F) || \
+	$(WGET) -P $(@D) $(FREEBSD_DISTFILES)/$(@F) || \
+	$(WGET) -P $(@D) $(SOURCES_NLO_SITE)/$(@F)
+
+perl-soap-lite-source: $(DL_DIR)/$(PERL-SOAP-LITE_SOURCE) $(PERL-SOAP-LITE_PATCHES)
+
+$(PERL-SOAP-LITE_BUILD_DIR)/.configured: $(DL_DIR)/$(PERL-SOAP-LITE_SOURCE) $(PERL-SOAP-LITE_PATCHES) make/perl-soap-lite.mk
+	rm -rf $(BUILD_DIR)/$(PERL-SOAP-LITE_DIR) $(PERL-SOAP-LITE_BUILD_DIR)
+	$(PERL-SOAP-LITE_UNZIP) $(DL_DIR)/$(PERL-SOAP-LITE_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+#	cat $(PERL-SOAP-LITE_PATCHES) | $(PATCH) -d $(BUILD_DIR)/$(PERL-SOAP-LITE_DIR) -p1
+	mv $(BUILD_DIR)/$(PERL-SOAP-LITE_DIR) $(@D)
+	(cd $(@D); \
+		$(TARGET_CONFIGURE_OPTS) \
+		CPPFLAGS="$(STAGING_CPPFLAGS)" \
+		LDFLAGS="$(STAGING_LDFLAGS)" \
+		PERL5LIB="$(STAGING_LIB_DIR)/perl5/site_perl" \
+		$(PERL_HOSTPERL) Makefile.PL \
+		PREFIX=$(TARGET_PREFIX) \
+	)
+	touch $@
+
+perl-soap-lite-unpack: $(PERL-SOAP-LITE_BUILD_DIR)/.configured
+
+$(PERL-SOAP-LITE_BUILD_DIR)/.built: $(PERL-SOAP-LITE_BUILD_DIR)/.configured
+	rm -f $@
+	$(MAKE) -C $(@D) \
+	PERL5LIB="$(STAGING_LIB_DIR)/perl5/site_perl"
+	touch $@
+
+perl-soap-lite: $(PERL-SOAP-LITE_BUILD_DIR)/.built
+
+$(PERL-SOAP-LITE_BUILD_DIR)/.staged: $(PERL-SOAP-LITE_BUILD_DIR)/.built
+	rm -f $@
+	$(MAKE) -C $(@D) DESTDIR=$(STAGING_DIR) install
+	touch $@
+
+perl-soap-lite-stage: $(PERL-SOAP-LITE_BUILD_DIR)/.staged
+
+$(PERL-SOAP-LITE_IPK_DIR)/CONTROL/control:
+	@$(INSTALL) -d $(@D)
+	@rm -f $@
+	@echo "Package: perl-soap-lite" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(PERL-SOAP-LITE_PRIORITY)" >>$@
+	@echo "Section: $(PERL-SOAP-LITE_SECTION)" >>$@
+	@echo "Version: $(PERL-SOAP-LITE_VERSION)-$(PERL-SOAP-LITE_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(PERL-SOAP-LITE_MAINTAINER)" >>$@
+	@echo "Source: $(PERL-SOAP-LITE_SITE)/$(PERL-SOAP-LITE_SOURCE)" >>$@
+	@echo "Description: $(PERL-SOAP-LITE_DESCRIPTION)" >>$@
+	@echo "Depends: $(PERL-SOAP-LITE_DEPENDS)" >>$@
+	@echo "Suggests: $(PERL-SOAP-LITE_SUGGESTS)" >>$@
+	@echo "Conflicts: $(PERL-SOAP-LITE_CONFLICTS)" >>$@
+
+$(PERL-SOAP-LITE_IPK): $(PERL-SOAP-LITE_BUILD_DIR)/.built
+	rm -rf $(PERL-SOAP-LITE_IPK_DIR) $(BUILD_DIR)/perl-soap-lite_*_$(TARGET_ARCH).ipk
+	$(MAKE) -C $(PERL-SOAP-LITE_BUILD_DIR) DESTDIR=$(PERL-SOAP-LITE_IPK_DIR) install
+	find $(PERL-SOAP-LITE_IPK_DIR)$(TARGET_PREFIX) -name 'perllocal.pod' -exec rm -f {} \;
+	(cd $(PERL-SOAP-LITE_IPK_DIR)$(TARGET_PREFIX)/lib/perl5 ; \
+		find . -name '*.so' -exec chmod +w {} \; ; \
+		find . -name '*.so' -exec $(STRIP_COMMAND) {} \; ; \
+		find . -name '*.so' -exec chmod -w {} \; ; \
+	)
+	find $(PERL-SOAP-LITE_IPK_DIR)$(TARGET_PREFIX) -type d -exec chmod go+rx {} \;
+	$(MAKE) $(PERL-SOAP-LITE_IPK_DIR)/CONTROL/control
+	echo $(PERL-SOAP-LITE_CONFFILES) | sed -e 's/ /\n/g' > $(PERL-SOAP-LITE_IPK_DIR)/CONTROL/conffiles
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(PERL-SOAP-LITE_IPK_DIR)
+
+perl-soap-lite-ipk: $(PERL-SOAP-LITE_IPK)
+
+perl-soap-lite-clean:
+	-$(MAKE) -C $(PERL-SOAP-LITE_BUILD_DIR) clean
+
+perl-soap-lite-dirclean:
+	rm -rf $(BUILD_DIR)/$(PERL-SOAP-LITE_DIR) $(PERL-SOAP-LITE_BUILD_DIR) $(PERL-SOAP-LITE_IPK_DIR) $(PERL-SOAP-LITE_IPK)
