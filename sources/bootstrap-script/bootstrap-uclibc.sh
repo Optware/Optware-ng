57ba9c868470d40995d0f1708fb73d3a4e76c3b0
alllexx88 <opotapenko@gmail.com>
2016-04-26 22:05:13 -0700

    Update bootstrap scripts
    * don't change working dir
    
    * make sure that we extract the downloaded ipk
    
    * clean downloaded files after extracting

--- sources/bootstrap-script/bootstrap-uclibc.sh
+++ sources/bootstrap-script/bootstrap-uclibc.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 feed=http://ipkg.nslu2-linux.org/optware-ng/%TARGET%
-cd /tmp 
-ipk_name=$(wget -qO- $feed/Packages | awk '/^Filename: ipkg-static/ {print $2}') 
-wget $feed/$ipk_name 
-tar -xvzf $ipk_name ./data.tar.gz 
-tar -C / -xzvf data.tar.gz 
-echo "src/gz alllexx $feed" > %OPTWARE_TARGET_PREFIX%/etc/ipkg.conf 
+ipk_name=$(wget -qO- $feed/Packages | awk '/^Filename: ipkg-static/ {print $2}')
+wget -O /tmp/$ipk_name $feed/$ipk_name
+tar -C /tmp -xvzf /tmp/$ipk_name ./data.tar.gz
+tar -C / -xzvf /tmp/data.tar.gz
+rm -f /tmp/$ipk_name /tmp/data.tar.gz
+echo "src/gz optware-ng $feed" > %OPTWARE_TARGET_PREFIX%/etc/ipkg.conf
 echo "dest %OPTWARE_TARGET_PREFIX%/ /" >> %OPTWARE_TARGET_PREFIX%/etc/ipkg.conf
 
 echo "Bootstraping done"
