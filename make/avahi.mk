49213c284426b981de9299f4060db9cdf0d43372
alllexx88 <opotapenko@gmail.com>
2017-08-13 10:46:20 +0000

    avahi: 0.6.32 -> 0.7 Also build libdns_sd: see #166

--- make/avahi.mk
+++ make/avahi.mk
@@ -21,7 +21,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 AVAHI_SITE=https://github.com/lathiat/avahi/releases/download/v$(AVAHI_VERSION)
-AVAHI_VERSION=0.6.32
+AVAHI_VERSION=0.7
 AVAHI_SOURCE=avahi-$(AVAHI_VERSION).tar.gz
 AVAHI_DIR=avahi-$(AVAHI_VERSION)
 AVAHI_UNZIP=zcat
@@ -32,6 +32,7 @@ LIBAVAHI_CLIENT_DESCRIPTION=Avahi client library
 LIBAVAHI_CORE_DESCRIPTION=Avahi core library
 LIBAVAHI_GLIB_DESCRIPTION=Avahi glib library
 LIBAVAHI_GOBJECT_DESCRIPTION=Avahi gobject library
+LIBAVAHI_COMPAT_LIBDNS_SD_DESCRIPTION=Avahi compat libdns_sd library
 AVAHI_SECTION=net
 LIBAVAHI_COMMON_SECTION=libs
 LIBAVAHI_CLIENT_SECTION=libs
@@ -54,13 +55,14 @@ LIBAVAHI_CLIENT_DEPENDS=libavahi-common, libdbus
 LIBAVAHI_CORE_DEPENDS=libavahi-common
 LIBAVAHI_GLIB_DEPENDS=libavahi-common, glib
 LIBAVAHI_GOBJECT_DEPENDS=libavahi-common, libavahi-client, libavahi-glib
+LIBAVAHI_COMPAT_LIBDNS_SD_DEPENDS=libavahi-client, libdbus, libavahi-common
 AVAHI_SUGGESTS=
 AVAHI_CONFLICTS=
 
 #
 # AVAHI_IPK_VERSION should be incremented when the ipk changes.
 #
-AVAHI_IPK_VERSION=2
+AVAHI_IPK_VERSION=1
 
 #
 # AVAHI_CONFFILES should be a list of user-editable files
@@ -121,6 +123,9 @@ LIBAVAHI_GLIB_IPK=$(BUILD_DIR)/libavahi-glib_$(AVAHI_VERSION)-$(AVAHI_IPK_VERSIO
 
 LIBAVAHI_GOBJECT_IPK_DIR=$(BUILD_DIR)/libavahi-gobject-$(AVAHI_VERSION)-ipk
 LIBAVAHI_GOBJECT_IPK=$(BUILD_DIR)/libavahi-gobject_$(AVAHI_VERSION)-$(AVAHI_IPK_VERSION)_$(TARGET_ARCH).ipk
+
+LIBAVAHI_COMPAT_LIBDNS_SD_IPK_DIR=$(BUILD_DIR)/libavahi-compat-libdns-sd-$(AVAHI_VERSION)-ipk
+LIBAVAHI_COMPAT_LIBDNS_SD_IPK=$(BUILD_DIR)/libavahi-compat-libdns-sd_$(AVAHI_VERSION)-$(AVAHI_IPK_VERSION)_$(TARGET_ARCH).ipk
 
 .PHONY: avahi-source avahi-unpack avahi avahi-stage avahi-ipk avahi-clean avahi-dirclean avahi-check
 
@@ -208,6 +213,7 @@ endif
 		--disable-nls \
 		--disable-static \
 		--disable-introspection \
+		--enable-compat-libdns_sd \
 	)
 	$(PATCH_LIBTOOL) $(@D)/libtool
 	touch $@
@@ -258,6 +264,21 @@ $(AVAHI_IPK_DIR)/CONTROL/control:
 	@echo "Suggests: $(AVAHI_SUGGESTS)" >>$@
 	@echo "Conflicts: $(AVAHI_CONFLICTS)" >>$@
 
+$(LIBAVAHI_COMPAT_LIBDNS_SD_IPK_DIR)/CONTROL/control:
+	@$(INSTALL) -d $(@D)
+	@rm -f $@
+	@echo "Package: libavahi-compat-libdns-sd" >>$@
+	@echo "Architecture: $(TARGET_ARCH)" >>$@
+	@echo "Priority: $(AVAHI_PRIORITY)" >>$@
+	@echo "Section: libs" >>$@
+	@echo "Version: $(AVAHI_VERSION)-$(AVAHI_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(AVAHI_MAINTAINER)" >>$@
+	@echo "Source: $(AVAHI_SITE)/$(AVAHI_SOURCE)" >>$@
+	@echo "Description: $(LIBAVAHI_COMPAT_LIBDNS_SD_DESCRIPTION)" >>$@
+	@echo "Depends: $(LIBAVAHI_COMPAT_LIBDNS_SD_DEPENDS)" >>$@
+	@echo "Suggests: $(LIBAVAHI_COMPAT_LIBDNS_SD_SUGGESTS)" >>$@
+	@echo "Conflicts: $(LIBAVAHI_COMPAT_LIBDNS_SD_CONFLICTS)" >>$@
+
 $(BUILD_DIR)/libavahi-%-$(AVAHI_VERSION)-ipk/CONTROL/control:
 	@$(INSTALL) -d $(@D)
 	@rm -f $@
@@ -286,7 +307,7 @@ $(BUILD_DIR)/libavahi-%-$(AVAHI_VERSION)-ipk/CONTROL/control:
 # You may need to patch your application to make it use these locations.
 #
 $(AVAHI_IPK) $(LIBAVAHI_COMMON_IPK) $(LIBAVAHI_CLIENT_IPK) $(LIBAVAHI_CORE_IPK) \
-$(LIBAVAHI_GLIB_IPK) $(LIBAVAHI_GOBJECT_IPK): $(AVAHI_BUILD_DIR)/.built
+$(LIBAVAHI_GLIB_IPK) $(LIBAVAHI_GOBJECT_IPK) $(LIBAVAHI_COMPAT_LIBDNS_SD_IPK): $(AVAHI_BUILD_DIR)/.built
 	rm -rf  $(AVAHI_IPK_DIR) $(BUILD_DIR)/avahi_*_$(TARGET_ARCH).ipk \
 		$(LIBAVAHI_COMMON_IPK_DIR) $(BUILD_DIR)/libavahi-common_*_$(TARGET_ARCH).ipk \
 		$(LIBAVAHI_CLIENT_IPK_DIR) $(BUILD_DIR)/libavahi-client_*_$(TARGET_ARCH).ipk \
@@ -294,8 +315,9 @@ $(LIBAVAHI_GLIB_IPK) $(LIBAVAHI_GOBJECT_IPK): $(AVAHI_BUILD_DIR)/.built
 		$(LIBAVAHI_GLIB_IPK_DIR) $(BUILD_DIR)/libavahi-glib_*_$(TARGET_ARCH).ipk \
 		$(LIBAVAHI_GOBJECT_IPK_DIR) $(BUILD_DIR)/libavahi-gobject_*_$(TARGET_ARCH).ipk
 	$(MAKE) -C $(AVAHI_BUILD_DIR) DESTDIR=$(AVAHI_IPK_DIR) install-strip
+	rm -rf $(AVAHI_IPK_DIR)/run
 	sed -i -e '/rlimit-nproc/s/^/#/' $(AVAHI_IPK_DIR)$(TARGET_PREFIX)/etc/avahi/avahi-daemon.conf
-	rm -f $(AVAHI_IPK_DIR)$(TARGET_PREFIX)/lib/libavahi*.la
+	rm -f $(AVAHI_IPK_DIR)$(TARGET_PREFIX)/lib/*.la
 	for lib in common client core glib gobject; do \
 		$(INSTALL) -d $(BUILD_DIR)/libavahi-$$lib-$(AVAHI_VERSION)-ipk$(TARGET_PREFIX)/lib; \
 		mv -f $(AVAHI_IPK_DIR)$(TARGET_PREFIX)/lib/libavahi-$${lib}.so* $(BUILD_DIR)/libavahi-$$lib-$(AVAHI_VERSION)-ipk$(TARGET_PREFIX)/lib; \
@@ -304,6 +326,11 @@ $(LIBAVAHI_GLIB_IPK) $(LIBAVAHI_GOBJECT_IPK): $(AVAHI_BUILD_DIR)/.built
 		popd; \
 		$(WHAT_TO_DO_WITH_IPK_DIR) $(BUILD_DIR)/libavahi-$$lib-$(AVAHI_VERSION)-ipk; \
 	done
+	$(INSTALL) -d $(LIBAVAHI_COMPAT_LIBDNS_SD_IPK_DIR)$(TARGET_PREFIX)/lib
+	mv -f $(AVAHI_IPK_DIR)$(TARGET_PREFIX)/lib/libdns_sd.so* $(LIBAVAHI_COMPAT_LIBDNS_SD_IPK_DIR)$(TARGET_PREFIX)/lib
+	$(MAKE) $(LIBAVAHI_COMPAT_LIBDNS_SD_IPK_DIR)/CONTROL/control
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(LIBAVAHI_COMPAT_LIBDNS_SD_IPK_DIR)
+	$(WHAT_TO_DO_WITH_IPK_DIR) $(LIBAVAHI_COMPAT_LIBDNS_SD_IPK_DIR)
 #	$(INSTALL) -d $(AVAHI_IPK_DIR)$(TARGET_PREFIX)/etc/
 #	$(INSTALL) -m 644 $(AVAHI_SOURCE_DIR)/avahi.conf $(AVAHI_IPK_DIR)$(TARGET_PREFIX)/etc/avahi.conf
 	$(INSTALL) -d $(AVAHI_IPK_DIR)$(TARGET_PREFIX)/etc/init.d
@@ -342,11 +369,12 @@ avahi-dirclean:
 		$(LIBAVAHI_CLIENT_IPK_DIR) $(LIBAVAHI_CLIENT_IPK) \
 		$(LIBAVAHI_CORE_IPK_DIR) $(LIBAVAHI_CORE_IPK) \
 		$(LIBAVAHI_GLIB_IPK_DIR) $(LIBAVAHI_GLIB_IPK) \
-		$(LIBAVAHI_GOBJECT_IPK_DIR) $(LIBAVAHI_GOBJECT_IPK)
+		$(LIBAVAHI_GOBJECT_IPK_DIR) $(LIBAVAHI_GOBJECT_IPK) \
+		$(LIBAVAHI_COMPAT_LIBDNS_SD_IPK_DIR) $(LIBAVAHI_COMPAT_LIBDNS_SD_IPK)
 #
 #
 # Some sanity check for the package.
 #
 avahi-check: $(AVAHI_IPK) $(LIBAVAHI_COMMON_IPK) $(LIBAVAHI_CLIENT_IPK) $(LIBAVAHI_CORE_IPK) \
-	     $(LIBAVAHI_GLIB_IPK) $(LIBAVAHI_GOBJECT_IPK)
+	     $(LIBAVAHI_GLIB_IPK) $(LIBAVAHI_GOBJECT_IPK) $(LIBAVAHI_COMPAT_LIBDNS_SD_IPK)
 	perl scripts/optware-check-package.pl --target=$(OPTWARE_TARGET) $^
