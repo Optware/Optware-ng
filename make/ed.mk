e0775c790a24699c33c3c038f47169c36564b0d5
alllexx88 <opotapenko@gmail.com>
2017-09-15 17:32:49 +0000

    ed: remove share/info/dir

--- make/ed.mk
+++ make/ed.mk
@@ -35,7 +35,7 @@ ED_CONFLICTS=
 #
 # ED_IPK_VERSION should be incremented when the ipk changes.
 #
-ED_IPK_VERSION=1
+ED_IPK_VERSION=2
 
 #
 # ED_PATCHES should list any patches, in the the order in
@@ -167,6 +167,7 @@ $(ED_IPK): $(ED_BUILD_DIR)/.built
 	$(MAKE) -C $(ED_BUILD_DIR) prefix=$(ED_IPK_DIR)$(TARGET_PREFIX) install INSTALL_DATA=:
 	$(STRIP_COMMAND) $(ED_IPK_DIR)$(TARGET_PREFIX)/bin/ed
 	$(MAKE) -C $(ED_BUILD_DIR) prefix=$(ED_IPK_DIR)$(TARGET_PREFIX) install-man
+	rm -f $(ED_IPK_DIR)$(TARGET_PREFIX)/share/info/dir
 	$(MAKE) $(ED_IPK_DIR)/CONTROL/control
 #	echo $(ED_CONFFILES) | sed -e 's/ /\n/g' > $(ED_IPK_DIR)/CONTROL/conffiles
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(ED_IPK_DIR)
