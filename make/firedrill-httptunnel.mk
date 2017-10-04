62dae7586f5cd524d2d43684b6abafb895e1ab44
alllexx88 <opotapenko@gmail.com>
2017-10-04 09:16:52 +0000

    firedrill-httptunnel: 1.3.2 -> 1.4.0 Also build with -std=c++98

--- make/firedrill-httptunnel.mk
+++ make/firedrill-httptunnel.mk
@@ -21,7 +21,7 @@
 # "NSLU2 Linux" other developers will feel free to edit.
 #
 FIREDRILL-HTTPTUNNEL_SITE=http://the-linux-academy.co.uk/downloads
-FIREDRILL-HTTPTUNNEL_VERSION=1.3.2
+FIREDRILL-HTTPTUNNEL_VERSION=1.4.0
 FIREDRILL-HTTPTUNNEL_UPSTREAM_SOURCE=httptunnel-$(FIREDRILL-HTTPTUNNEL_VERSION).tgz
 FIREDRILL-HTTPTUNNEL_SOURCE=firedrill-$(FIREDRILL-HTTPTUNNEL_UPSTREAM_SOURCE)
 FIREDRILL-HTTPTUNNEL_DIR=httptunnel
@@ -37,7 +37,7 @@ FIREDRILL-HTTPTUNNEL_CONFLICTS=
 #
 # FIREDRILL-HTTPTUNNEL_IPK_VERSION should be incremented when the ipk changes.
 #
-FIREDRILL-HTTPTUNNEL_IPK_VERSION=2
+FIREDRILL-HTTPTUNNEL_IPK_VERSION=1
 
 #
 # FIREDRILL-HTTPTUNNEL_CONFFILES should be a list of user-editable files
@@ -54,7 +54,7 @@ $(FIREDRILL-HTTPTUNNEL_SOURCE_DIR)/find_if.patch \
 # If the compilation of the package requires additional
 # compilation or linking flags, then list them here.
 #
-FIREDRILL-HTTPTUNNEL_CPPFLAGS=-I$(STAGING_INCLUDE_DIR)/openssl
+FIREDRILL-HTTPTUNNEL_CPPFLAGS=-I$(STAGING_INCLUDE_DIR)/openssl -std=c++98
 FIREDRILL-HTTPTUNNEL_LDFLAGS=-lcrypto
 
 #
