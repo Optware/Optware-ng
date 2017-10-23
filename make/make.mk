d70f852a927aec671f04083a1108d0ba4cfce1d6
alllexx88 <opotapenko@gmail.com>
2017-10-23 04:12:30 +0000

    make: fix packaging

--- make/make.mk
+++ make/make.mk
@@ -177,7 +177,7 @@ $(MAKE_IPK_DIR)/CONTROL/control:
 # You may need to patch your application to make it use these locations.
 #
 $(MAKE_IPK): $(MAKE_BUILD_DIR)/.built
-	rm -rf $(MAKE_IPK_DIR) $(MAKE_IPK)
+	rm -rf $(MAKE_IPK_DIR) $(BUILD_DIR)/make_*_$(TARGET_ARCH).ipk
 	$(MAKE) -C $(MAKE_BUILD_DIR) DESTDIR=$(MAKE_IPK_DIR) install-strip
 	rm -f $(MAKE_IPK_DIR)$(TARGET_PREFIX)/share/info/dir
 #	$(INSTALL) -d $(MAKE_IPK_DIR)$(TARGET_PREFIX)/etc/init.d
