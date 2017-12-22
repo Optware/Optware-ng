c305a44a74be71f95be573f515b90bfed45cbbf9
alllexx88 <opotapenko@gmail.com>
2017-12-22 14:24:02 +0000

    perl-mozilla-ca: upgrade cacert.pem, add perl, perl-libwww as suggestions

--- make/perl-mozilla-ca.mk
+++ make/perl-mozilla-ca.mk
@@ -4,40 +4,45 @@
 #
 ###########################################################
 
-PERL-MOZILLA-CA_SITE=http://$(PERL_CPAN_SITE)/CPAN/authors/id/A/AB/ABH
-PERL-MOZILLA-CA_VERSION=20160104
-PERL-MOZILLA-CA_SOURCE=Mozilla-CA-$(PERL-MOZILLA-CA_VERSION).tar.gz
-PERL-MOZILLA-CA_DIR=Mozilla-CA-$(PERL-MOZILLA-CA_VERSION)
-PERL-MOZILLA-CA_UNZIP=zcat
-PERL-MOZILLA-CA_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
-PERL-MOZILLA-CA_DESCRIPTION=Mozillas CA cert bundle in PEM format
-PERL-MOZILLA-CA_SECTION=util
-PERL-MOZILLA-CA_PRIORITY=optional
-PERL-MOZILLA-CA_DEPENDS=
-PERL-MOZILLA-CA_SUGGESTS=
-PERL-MOZILLA-CA_CONFLICTS=
+PERL_MOZILLA_CA_SITE=http://$(PERL_CPAN_SITE)/CPAN/authors/id/A/AB/ABH
+PERL_MOZILLA_CA_UPSTREAM_VERSION=20160104
+PERL_MOZILLA_CA_CERTS_VERSION=20171222
+PERL_MOZILLA_CA_VERSION=$(PERL_MOZILLA_CA_UPSTREAM_VERSION)-certs$(PERL_MOZILLA_CA_CERTS_VERSION)
+PERL_MOZILLA_CA_SOURCE=Mozilla-CA-$(PERL_MOZILLA_CA_UPSTREAM_VERSION).tar.gz
+PERL_MOZILLA_CA_DIR=Mozilla-CA-$(PERL_MOZILLA_CA_UPSTREAM_VERSION)
+PERL_MOZILLA_CA_UNZIP=zcat
+PERL_MOZILLA_CA_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
+PERL_MOZILLA_CA_DESCRIPTION=Mozillas CA cert bundle in PEM format
+PERL_MOZILLA_CA_SECTION=util
+PERL_MOZILLA_CA_PRIORITY=optional
+PERL_MOZILLA_CA_DEPENDS=
+PERL_MOZILLA_CA_SUGGESTS=perl, perl-libwww
+PERL_MOZILLA_CA_CONFLICTS=
 
-PERL-MOZILLA-CA_IPK_VERSION=3
+PERL_MOZILLA_CA_IPK_VERSION=1
 
-PERL-MOZILLA-CA_CONFFILES=
+PERL_MOZILLA_CA_CONFFILES=
 
-PERL-MOZILLA-CA_BUILD_DIR=$(BUILD_DIR)/perl-mozilla-ca
-PERL-MOZILLA-CA_SOURCE_DIR=$(SOURCE_DIR)/perl-mozilla-ca
-PERL-MOZILLA-CA_IPK_DIR=$(BUILD_DIR)/perl-mozilla-ca-$(PERL-MOZILLA-CA_VERSION)-ipk
-PERL-MOZILLA-CA_IPK=$(BUILD_DIR)/perl-mozilla-ca_$(PERL-MOZILLA-CA_VERSION)-$(PERL-MOZILLA-CA_IPK_VERSION)_$(TARGET_ARCH).ipk
+PERL_MOZILLA_CA_BUILD_DIR=$(BUILD_DIR)/perl-mozilla-ca
+PERL_MOZILLA_CA_SOURCE_DIR=$(SOURCE_DIR)/perl-mozilla-ca
+PERL_MOZILLA_CA_IPK_DIR=$(BUILD_DIR)/perl-mozilla-ca-$(PERL_MOZILLA_CA_VERSION)-ipk
+PERL_MOZILLA_CA_IPK=$(BUILD_DIR)/perl-mozilla-ca_$(PERL_MOZILLA_CA_VERSION)-$(PERL_MOZILLA_CA_IPK_VERSION)_$(TARGET_ARCH).ipk
 
-$(DL_DIR)/$(PERL-MOZILLA-CA_SOURCE):
-	$(WGET) -P $(@D) $(PERL-MOZILLA-CA_SITE)/$(@F) || \
+$(DL_DIR)/$(PERL_MOZILLA_CA_SOURCE):
+	$(WGET) -P $(@D) $(PERL_MOZILLA_CA_SITE)/$(@F) || \
 	$(WGET) -P $(@D) $(FREEBSD_DISTFILES)/$(@F) || \
 	$(WGET) -P $(@D) $(SOURCES_NLO_SITE)/$(@F)
 
-perl-mozilla-ca-source: $(DL_DIR)/$(PERL-MOZILLA-CA_SOURCE) $(PERL-MOZILLA-CA_PATCHES)
+perl-mozilla-ca-source: $(DL_DIR)/$(PERL_MOZILLA_CA_SOURCE) $(PERL_MOZILLA_CA_PATCHES)
 
-$(PERL-MOZILLA-CA_BUILD_DIR)/.configured: $(DL_DIR)/$(PERL-MOZILLA-CA_SOURCE) $(PERL-MOZILLA-CA_PATCHES) make/perl-mozilla-ca.mk
-	rm -rf $(BUILD_DIR)/$(PERL-MOZILLA-CA_DIR) $(PERL-MOZILLA-CA_BUILD_DIR)
-	$(PERL-MOZILLA-CA_UNZIP) $(DL_DIR)/$(PERL-MOZILLA-CA_SOURCE) | tar -C $(BUILD_DIR) -xvf -
-#	cat $(PERL-MOZILLA-CA_PATCHES) | $(PATCH) -d $(BUILD_DIR)/$(PERL-MOZILLA-CA_DIR) -p1
-	mv $(BUILD_DIR)/$(PERL-MOZILLA-CA_DIR) $(@D)
+$(PERL_MOZILLA_CA_BUILD_DIR)/.configured: $(DL_DIR)/$(PERL_MOZILLA_CA_SOURCE) \
+		$(PERL_MOZILLA_CA_PATCHES) make/perl-mozilla-ca.mk \
+		$(PERL_MOZILLA_CA_SOURCE_DIR)/cacert.pem
+	rm -rf $(BUILD_DIR)/$(PERL_MOZILLA_CA_DIR) $(@D)
+	$(PERL_MOZILLA_CA_UNZIP) $(DL_DIR)/$(PERL_MOZILLA_CA_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+#	cat $(PERL_MOZILLA_CA_PATCHES) | $(PATCH) -d $(BUILD_DIR)/$(PERL_MOZILLA_CA_DIR) -p1
+	mv $(BUILD_DIR)/$(PERL_MOZILLA_CA_DIR) $(@D)
+	cp -f $(PERL_MOZILLA_CA_SOURCE_DIR)/cacert.pem $(@D)/lib/Mozilla/CA/cacert.pem
 	(cd $(@D); \
 		$(TARGET_CONFIGURE_OPTS) \
 		CPPFLAGS="$(STAGING_CPPFLAGS)" \
@@ -48,56 +53,58 @@ $(PERL-MOZILLA-CA_BUILD_DIR)/.configured: $(DL_DIR)/$(PERL-MOZILLA-CA_SOURCE) $(
 	)
 	touch $@
 
-perl-mozilla-ca-unpack: $(PERL-MOZILLA-CA_BUILD_DIR)/.configured
+perl-mozilla-ca-unpack: $(PERL_MOZILLA_CA_BUILD_DIR)/.configured
 
-$(PERL-MOZILLA-CA_BUILD_DIR)/.built: $(PERL-MOZILLA-CA_BUILD_DIR)/.configured
+$(PERL_MOZILLA_CA_BUILD_DIR)/.built: $(PERL_MOZILLA_CA_BUILD_DIR)/.configured
 	rm -f $@
 	$(MAKE) -C $(@D) \
-	PERL5LIB="$(STAGING_LIB_DIR)/perl5/site_perl"
+		PERL5LIB="$(STAGING_LIB_DIR)/perl5/site_perl"
 	touch $@
 
-perl-mozilla-ca: $(PERL-MOZILLA-CA_BUILD_DIR)/.built
+perl-mozilla-ca: $(PERL_MOZILLA_CA_BUILD_DIR)/.built
 
-$(PERL-MOZILLA-CA_BUILD_DIR)/.staged: $(PERL-MOZILLA-CA_BUILD_DIR)/.built
+$(PERL_MOZILLA_CA_BUILD_DIR)/.staged: $(PERL_MOZILLA_CA_BUILD_DIR)/.built
 	rm -f $@
 	$(MAKE) -C $(@D) DESTDIR=$(STAGING_DIR) install
 	touch $@
 
-perl-mozilla-ca-stage: $(PERL-MOZILLA-CA_BUILD_DIR)/.staged
+perl-mozilla-ca-stage: $(PERL_MOZILLA_CA_BUILD_DIR)/.staged
 
-$(PERL-MOZILLA-CA_IPK_DIR)/CONTROL/control:
+$(PERL_MOZILLA_CA_IPK_DIR)/CONTROL/control:
 	@$(INSTALL) -d $(@D)
 	@rm -f $@
 	@echo "Package: perl-mozilla-ca" >>$@
 	@echo "Architecture: $(TARGET_ARCH)" >>$@
-	@echo "Priority: $(PERL-MOZILLA-CA_PRIORITY)" >>$@
-	@echo "Section: $(PERL-MOZILLA-CA_SECTION)" >>$@
-	@echo "Version: $(PERL-MOZILLA-CA_VERSION)-$(PERL-MOZILLA-CA_IPK_VERSION)" >>$@
-	@echo "Maintainer: $(PERL-MOZILLA-CA_MAINTAINER)" >>$@
-	@echo "Source: $(PERL-MOZILLA-CA_SITE)/$(PERL-MOZILLA-CA_SOURCE)" >>$@
-	@echo "Description: $(PERL-MOZILLA-CA_DESCRIPTION)" >>$@
-	@echo "Depends: $(PERL-MOZILLA-CA_DEPENDS)" >>$@
-	@echo "Suggests: $(PERL-MOZILLA-CA_SUGGESTS)" >>$@
-	@echo "Conflicts: $(PERL-MOZILLA-CA_CONFLICTS)" >>$@
+	@echo "Priority: $(PERL_MOZILLA_CA_PRIORITY)" >>$@
+	@echo "Section: $(PERL_MOZILLA_CA_SECTION)" >>$@
+	@echo "Version: $(PERL_MOZILLA_CA_VERSION)-$(PERL_MOZILLA_CA_IPK_VERSION)" >>$@
+	@echo "Maintainer: $(PERL_MOZILLA_CA_MAINTAINER)" >>$@
+	@echo "Source: $(PERL_MOZILLA_CA_SITE)/$(PERL_MOZILLA_CA_SOURCE)" >>$@
+	@echo "Description: $(PERL_MOZILLA_CA_DESCRIPTION)" >>$@
+	@echo "Depends: $(PERL_MOZILLA_CA_DEPENDS)" >>$@
+	@echo "Suggests: $(PERL_MOZILLA_CA_SUGGESTS)" >>$@
+	@echo "Conflicts: $(PERL_MOZILLA_CA_CONFLICTS)" >>$@
 
-$(PERL-MOZILLA-CA_IPK): $(PERL-MOZILLA-CA_BUILD_DIR)/.built
-	rm -rf $(PERL-MOZILLA-CA_IPK_DIR) $(BUILD_DIR)/perl-mozilla-ca_*_$(TARGET_ARCH).ipk
-	$(MAKE) -C $(PERL-MOZILLA-CA_BUILD_DIR) DESTDIR=$(PERL-MOZILLA-CA_IPK_DIR) install
-	find $(PERL-MOZILLA-CA_IPK_DIR)$(TARGET_PREFIX) -name 'perllocal.pod' -exec rm -f {} \;
-	(cd $(PERL-MOZILLA-CA_IPK_DIR)$(TARGET_PREFIX)/lib/perl5 ; \
-		find . -name '*.so' -exec chmod +w {} \; ; \
-		find . -name '*.so' -exec $(STRIP_COMMAND) {} \; ; \
-		find . -name '*.so' -exec chmod -w {} \; ; \
-	)
-	find $(PERL-MOZILLA-CA_IPK_DIR)$(TARGET_PREFIX) -type d -exec chmod go+rx {} \;
-	$(MAKE) $(PERL-MOZILLA-CA_IPK_DIR)/CONTROL/control
-	echo $(PERL-MOZILLA-CA_CONFFILES) | sed -e 's/ /\n/g' > $(PERL-MOZILLA-CA_IPK_DIR)/CONTROL/conffiles
-	cd $(BUILD_DIR); $(IPKG_BUILD) $(PERL-MOZILLA-CA_IPK_DIR)
+$(PERL_MOZILLA_CA_IPK): $(PERL_MOZILLA_CA_BUILD_DIR)/.built
+	rm -rf $(PERL_MOZILLA_CA_IPK_DIR) $(BUILD_DIR)/perl-mozilla-ca_*_$(TARGET_ARCH).ipk
+	$(MAKE) -C $(PERL_MOZILLA_CA_BUILD_DIR) DESTDIR=$(PERL_MOZILLA_CA_IPK_DIR) install
+	find $(PERL_MOZILLA_CA_IPK_DIR)$(TARGET_PREFIX) -name 'perllocal.pod' -exec rm -f {} \;
+	find $(PERL_MOZILLA_CA_IPK_DIR)$(TARGET_PREFIX) -type d -exec chmod go+rx {} \;
+	$(MAKE) $(PERL_MOZILLA_CA_IPK_DIR)/CONTROL/control
+	echo $(PERL_MOZILLA_CA_CONFFILES) | sed -e 's/ /\n/g' > $(PERL_MOZILLA_CA_IPK_DIR)/CONTROL/conffiles
+	cd $(BUILD_DIR); $(IPKG_BUILD) $(PERL_MOZILLA_CA_IPK_DIR)
+	$(WHAT_TO_DO_WITH_IPK_DIR) $(PERL_MOZILLA_CA_IPK_DIR)
 
-perl-mozilla-ca-ipk: $(PERL-MOZILLA-CA_IPK)
+perl-mozilla-ca-ipk: $(PERL_MOZILLA_CA_IPK)
 
 perl-mozilla-ca-clean:
-	-$(MAKE) -C $(PERL-MOZILLA-CA_BUILD_DIR) clean
+	-$(MAKE) -C $(PERL_MOZILLA_CA_BUILD_DIR) clean
 
 perl-mozilla-ca-dirclean:
-	rm -rf $(BUILD_DIR)/$(PERL-MOZILLA-CA_DIR) $(PERL-MOZILLA-CA_BUILD_DIR) $(PERL-MOZILLA-CA_IPK_DIR) $(PERL-MOZILLA-CA_IPK)
+	rm -rf $(BUILD_DIR)/$(PERL_MOZILLA_CA_DIR) $(PERL_MOZILLA_CA_BUILD_DIR) $(PERL_MOZILLA_CA_IPK_DIR) $(PERL_MOZILLA_CA_IPK)
+#
+#
+# Some sanity check for the package.
+#
+perl-mozilla-ca-check: $(PERL_MOZILLA_CA_IPK)
+	perl scripts/optware-check-package.pl --target=$(OPTWARE_TARGET) $^
