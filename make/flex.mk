c795e91f257f9b27a0bb81b465a8b917f48a3978
Anthon Pang <anthon.pang@gmail.com>
2018-01-22 03:05:33 +0000

    flex 2.5.35 => 2.5.39 (last of 2.5.x)

--- make/flex.mk
+++ make/flex.mk
@@ -5,10 +5,10 @@
 ###########################################################
 
 FLEX_SITE=http://$(SOURCEFORGE_MIRROR)/sourceforge/flex
-FLEX_VERSION=2.5.35
-FLEX_SOURCE=flex-$(FLEX_VERSION).tar.bz2
+FLEX_VERSION=2.5.39
+FLEX_SOURCE=flex-$(FLEX_VERSION).tar.xz
 FLEX_DIR=flex-$(FLEX_VERSION)
-FLEX_UNZIP=bzcat
+FLEX_UNZIP=xzcat
 FLEX_MAINTAINER=NSLU2 Linux <nslu2-linux@yahoogroups.com>
 FLEX_DESCRIPTION=Generates programs that perform pattern-matching on text.
 FLEX_SECTION=devel
@@ -16,18 +16,47 @@ FLEX_PRIORITY=optional
 FLEX_DEPENDS=m4
 FLEX_CONFLICTS=
 
-FLEX_IPK_VERSION=3
+FLEX_IPK_VERSION=4
 
 FLEX_BUILD_DIR=$(BUILD_DIR)/flex
 FLEX_SOURCE_DIR=$(SOURCE_DIR)/flex
 FLEX_IPK_DIR=$(BUILD_DIR)/flex-$(FLEX_VERSION)-ipk
 FLEX_IPK=$(BUILD_DIR)/flex_$(FLEX_VERSION)-$(FLEX_IPK_VERSION)_$(TARGET_ARCH).ipk
 
+.PHONY: flex-source flex-unpack flex flex-stage flex-ipk flex-clean flex-dirclean flex-check
+
+#
+# This is the dependency on the source code.  If the source is missing,
+# then it will be fetched from the site using wget.
+#
 $(DL_DIR)/$(FLEX_SOURCE):
 	$(WGET) -P $(DL_DIR) $(FLEX_SITE)/$(FLEX_SOURCE)
 
+#
+# The source code depends on it existing within the download directory.
+# This target will be called by the top level Makefile to download the
+# source code's archive (.tar.gz, .bz2, etc.)
+#
 flex-source: $(DL_DIR)/$(FLEX_SOURCE)
 
+#
+# This target unpacks the source code in the build directory.
+# If the source archive is not .tar.gz or .tar.bz2, then you will need
+# to change the commands here.  Patches to the source code are also
+# applied in this target as required.
+#
+# This target also configures the build within the build directory.
+# Flags such as LDFLAGS and CPPFLAGS should be passed into configure
+# and NOT $(MAKE) below.  Passing it to configure causes configure to
+# correctly BUILD the Makefile with the right paths, where passing it
+# to Make causes it to override the default search paths of the compiler.
+#
+# If the compilation of the package requires other packages to be staged
+# first, then do that first (e.g. "$(MAKE) <bar>-stage <baz>-stage").
+#
+# If the package uses  GNU libtool, you should invoke $(PATCH_LIBTOOL) as
+# shown below to make various patches to it.
+#
 $(FLEX_BUILD_DIR)/.configured: $(DL_DIR)/$(FLEX_SOURCE) make/flex.mk
 	rm -rf $(BUILD_DIR)/$(FLEX_DIR) $(@D)
 	$(FLEX_UNZIP) $(DL_DIR)/$(FLEX_SOURCE) | tar -C $(BUILD_DIR) -xvf -
@@ -80,6 +109,9 @@ endif
 
 flex-stage: $(FLEX_BUILD_DIR)/.staged
 
+#
+# This rule creates a control file for ipkg.
+#
 $(FLEX_IPK_DIR)/CONTROL/control:
 	@$(INSTALL) -d $(@D)
 	@rm -f $@
@@ -94,6 +126,9 @@ $(FLEX_IPK_DIR)/CONTROL/control:
 	@echo "Depends: $(FLEX_DEPENDS)" >>$@
 	@echo "Conflicts: $(FLEX_CONFLICTS)" >>$@
 
+#
+# This builds the IPK file.
+#
 $(FLEX_IPK): $(FLEX_BUILD_DIR)/.built
 	rm -rf $(FLEX_IPK_DIR) $(BUILD_DIR)/flex_*_$(TARGET_ARCH).ipk
 	$(MAKE) -C $(FLEX_BUILD_DIR) DESTDIR=$(FLEX_IPK_DIR) install-strip
@@ -102,13 +137,26 @@ $(FLEX_IPK): $(FLEX_BUILD_DIR)/.built
 	$(MAKE) $(FLEX_IPK_DIR)/CONTROL/control
 	cd $(BUILD_DIR); $(IPKG_BUILD) $(FLEX_IPK_DIR)
 
+#
+# This is called from the top level makefile to create the IPK file.
+#
 flex-ipk: $(FLEX_IPK)
 
+#
+# This is called from the top level makefile to clean all of the built files.
+#
 flex-clean:
 	-$(MAKE) -C $(FLEX_BUILD_DIR) clean
 
+#
+# This is called from the top level makefile to clean all dynamically created
+# directories.
+#
 flex-dirclean:
 	rm -rf $(BUILD_DIR)/$(FLEX_DIR) $(FLEX_BUILD_DIR) $(FLEX_IPK_DIR) $(FLEX_IPK)
 
+#
+# Some sanity check for the package.
+#
 flex-check: $(FLEX_IPK)
 	perl scripts/optware-check-package.pl --target=$(OPTWARE_TARGET) $(FLEX_IPK)
